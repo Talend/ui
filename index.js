94515d9db0ecf7b708a00531490b299da053ff5e
David Jensen <david.lgj@gmail.com>
2016-03-28 17:09:58 +0200

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-
-export * from './lib/sfPath.js';
+export * from './lib/canonicalTitleMap';
+export * from './lib/merge.js';
+export * from './lib/schemaDefaults.js';
 export * from './lib/select.js';
+export * from './lib/sfPath.js';
+export * from './lib/traverse.js';
