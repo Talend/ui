62dfcc1debb53a4121129b1c8e5a81e6539284e3
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2016-09-21 15:57:23 +0200

    test: add for AppHeaderBar

--- src/AppHeaderBar/AppHeaderBar.test.js
+++ src/AppHeaderBar/AppHeaderBar.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import renderer from 'react-test-renderer';
+import stubContext from 'react-stub-context';
 import AppHeaderBar from './AppHeaderBar.component';
+
+jest.mock('react-dom');
 
 describe('AppHeaderBar', () => {
 	it('should render its name', () => {
-		const wrapper = shallow(<AppHeaderBar name="Hello world" />);
-		expect(wrapper.containsMatchingElement(<div>Hello world</div>)).toBe(true);
+		const appheaderbar = renderer.create(
+			<AppHeaderBar name="Hello world" />
+		).toJSON();
+		expect(appheaderbar).toMatchSnapshot();
 	});
 });
