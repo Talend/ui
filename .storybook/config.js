f6ccafab56e2338e4886be1a5d79859d603131b0
Fabien Rassinier <frassinier@talend.com>
2016-10-21 21:50:36 +0200

    chore: add react-a11y non intrusive warning while starting storybook (#46)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import a11y from 'react-a11y';
+
 import { configure, setAddon } from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
+
+a11y(React);
 
 setAddon(infoAddon);
 
 function loadStories() {
-  require('../stories');
+	require('../stories');
 }
 
 configure(loadStories, module);
