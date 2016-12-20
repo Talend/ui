a88d152780855a2261cee32a524770cf785cb7aa
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2016-12-20 10:44:57 +0100

    feat(middleware/router): support routerReplace option (#59)

--- __tests__/middlewares/cmf.test.js
+++ __tests__/middlewares/cmf.test.js
@@ -43,7 +43,6 @@ describe('CMF middleware', () => {
 		expect(arg.type).toBe('@@router/CALL_HISTORY_METHOD');
 		expect(arg.payload.method).toBe('push');
 		expect(arg.payload.args[0]).toBe('/route');
-
 	});
 	it('should dispatch router push if cmf.routerPush is a function', () => {
 		const action = {
@@ -59,6 +58,36 @@ describe('CMF middleware', () => {
 		const arg = store.dispatch.mock.calls[0][0];
 		expect(arg.type).toBe('@@router/CALL_HISTORY_METHOD');
 		expect(arg.payload.method).toBe('push');
+		expect(arg.payload.args[0]).toBe('/route/28');
+	});
+	it('should dispatch router replace if cmf.routerReplace is a string', () => {
+		const action = {
+			cmf: {
+				routerReplace: '/route',
+			},
+			response: { id: 28 },
+		};
+		middleware(action);
+		expect(store.dispatch).toHaveBeenCalled();
+		const arg = store.dispatch.mock.calls[0][0];
+		expect(arg.type).toBe('@@router/CALL_HISTORY_METHOD');
+		expect(arg.payload.method).toBe('replace');
+		expect(arg.payload.args[0]).toBe('/route');
+	});
+	it('should dispatch router replace if cmf.routerReplace is a function', () => {
+		const action = {
+			cmf: {
+				routerReplace(data) {
+					return `/route/${data.response.id}`;
+				},
+			},
+			response: { id: 28 },
+		};
+		middleware(action);
+		expect(store.dispatch).toHaveBeenCalled();
+		const arg = store.dispatch.mock.calls[0][0];
+		expect(arg.type).toBe('@@router/CALL_HISTORY_METHOD');
+		expect(arg.payload.method).toBe('replace');
 		expect(arg.payload.args[0]).toBe('/route/28');
 	});
 });
--- src/middlewares/cmf/index.js
+++ src/middlewares/cmf/index.js
@@ -12,15 +12,15 @@ const cmfMiddleware = store => next => (action) => {
 			),
 		);
 	}
-	if (config.routerPush) {
-		let route = config.routerPush;
+	if (config.routerPush || config.routerReplace) {
+		let route = config.routerPush || config.routerReplace;
 		if (typeof route === 'function') {
 			route = route(action);
 		}
 		store.dispatch({
 			type: '@@router/CALL_HISTORY_METHOD',
 			payload: {
-				method: 'push',
+				method: config.routerPush ? 'push' : 'replace',
 				args: [route],
 			},
 		});
