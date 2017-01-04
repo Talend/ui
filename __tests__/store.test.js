5f0ad802cb342a4d6f178e46e093499580307ad6
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-01-04 13:45:01 +0100

    feat(store): add pre reducers support (#63)
    It's now possible to register reducer that will be executed before all others
    
    Add something like this in the index.js of your app where you declare the store.
    
    import { store } from 'react-cmf';
    
    function myReducer(state, action) {
      //do what ever you want
      return state
    }
    // only one
    store.addPreReducer(myReducer);
    // or multiple
    store.addPreReducer([myReducer]);

--- __tests__/store.test.js
+++ __tests__/store.test.js
@@ -43,3 +43,32 @@ describe('CMF store', () => {
 		expect(typeof state.heyImRoot).toBe('object');
 	});
 });
+
+describe('addPreReducer', () => {
+	it('should add a reducer called by the cmf reducer', () => {
+		const myreducer = jest.fn();
+		store.addPreReducer(myreducer);
+		const reducer = store.getReducer();
+		reducer(undefined, {});
+		expect(myreducer.mock.calls.length).toBe(1);
+	});
+});
+
+describe('getMiddlewares', () => {
+	it('should return array of middleware', () => {
+		const middlewares = store.getMiddlewares();
+		expect(Array.isArray(middlewares)).toBe(true);
+	});
+	it('should support first attr as function', () => {
+		const fn = jest.fn();
+		const middlewares = store.getMiddlewares(fn);
+		expect(middlewares).toContain(fn);
+	});
+	it('should support first attr as array', () => {
+		const fn1 = jest.fn();
+		const fn2 = jest.fn();
+		const middlewares = store.getMiddlewares([fn1, fn2]);
+		expect(middlewares).toContain(fn1);
+		expect(middlewares).toContain(fn2);
+	});
+});
--- src/store.js
+++ src/store.js
@@ -12,8 +12,10 @@ import cmfReducers from './reducers';
 import httpMiddleware from './middlewares/http';
 import cmfMiddleware from './middlewares/cmf';
 
+const preReducers = [];
 const enhancers = [];
 const middlewares = [thunk, httpMiddleware, cmfMiddleware];
+
 if (window) {
 	if (window.devToolsExtension) {
 		enhancers.push(window.devToolsExtension());
@@ -33,22 +35,37 @@ function setRouterMiddleware(middleware) {
 	defaultRouterOverwrite = true;
 }
 
+function addPreReducer(reducers) {
+	if (typeof reducers === 'function') {
+		preReducers.push(reducers);
+	} else if (Array.isArray(reducers)) {
+		preReducers.push(...reducers);
+	}
+}
+
+function preApplyReducer(reducer) {
+	if (preReducers.length === 0) {
+		return reducer;
+	}
+	const newReducer = (state, action) => {
+		const newState = preReducers.reduce(
+			(accumulatedState, r) => r(
+				accumulatedState,
+				action
+			),
+			state
+		);
+		return reducer(newState, action);
+	};
+	return newReducer;
+}
+
 /**
- * helper to create the store with all the things needed by CMF
- * the store look like this:
- * - root
- * |- app (with appReducer)
- * |- routing (for react-router)
- * |- cmf (for the internals)
- *
- * @param  {function} appReducer   the reducer for your app.
- * @param  {any} preloadedState if you want to create your state tree with initial values.
- *                              This is usefull for server side renderring
- * @param  {function} enhancer     The store enhancer
- * @param  {Array|function} middleware   redux middleware: http://redux.js.org/docs/api/applyMiddleware.html
- * @return {Object}              The created store
+ * Return the CMF reducer
+ * @param  {function|Object} appReducer [description]
+ * @return {function}            [description]
  */
-function initialize(appReducer, preloadedState, enhancer, middleware) {
+function getReducer(appReducer) {
 	let reducerObject = {};
 	if (appReducer) {
 		if (typeof appReducer === 'object') {
@@ -65,7 +82,16 @@ function initialize(appReducer, preloadedState, enhancer, middleware) {
 	if (!reducerObject.routing) {
 		reducerObject.routing = routerReducer;
 	}
-	const rootReducer = combineReducers(reducerObject);
+	return enableBatching(
+		preApplyReducer(
+			combineReducers(
+				reducerObject
+			)
+		)
+	);
+}
+
+function getMiddlewares(middleware) {
 	if (Array.isArray(middleware)) {
 		middleware.forEach((mid) => {
 			if (middlewares.indexOf(mid) === -1) {
@@ -75,22 +101,46 @@ function initialize(appReducer, preloadedState, enhancer, middleware) {
 	} else if (middleware) {
 		middlewares.push(middleware);
 	}
-	if (typeof enhancer === 'function') {
-		enhancers.push(enhancer);
-	}
 	if (!defaultRouterOverwrite) {
 		setRouterMiddleware(routerMiddleware(hashHistory));
 	}
+	return middlewares;
+}
 
+/**
+ * helper to create the store with all the things needed by CMF
+ * the store look like this:
+ * - root
+ * |- app (with appReducer)
+ * |- routing (for react-router)
+ * |- cmf (for the internals)
+ *
+ * @param  {function} appReducer   the reducer for your app.
+ * @param  {any} preloadedState if you want to create your state tree with initial values.
+ *                              This is usefull for server side renderring
+ * @param  {function} enhancer     The store enhancer
+ * @param  {Array|function} middleware   redux middleware: http://redux.js.org/docs/api/applyMiddleware.html
+ * @return {Object}              The created store
+ */
+function initialize(appReducer, preloadedState, enhancer, middleware) {
+	const reducer = getReducer(appReducer);
+	if (typeof enhancer === 'function') {
+		enhancers.push(enhancer);
+	}
+	const middles = getMiddlewares(middleware);
 	const store = compose(
-		applyMiddleware(...middlewares),
+		applyMiddleware(...middles),
 		...enhancers
-	)(createStore)(enableBatching(rootReducer), preloadedState);
+	)(createStore)(reducer, preloadedState);
 
 	return store;
 }
 
 export default {
+	addPreReducer,
 	setRouterMiddleware,
 	initialize,
+	// for testing purepose only
+	getReducer,
+	getMiddlewares,
 };
