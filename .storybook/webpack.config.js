bf438de84576e4322a218d067f1644c8fb5d5af8
Geoffroy Lorieux <glorieux@talend.com>
2017-01-19 13:22:01 +0100

    feat(styles): Adds styles support (#67)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,29 @@
-// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// you can use this file to add your custom webpack plugins, loaders and
+// anything you like.
 // This is just the basic way to add addional webpack configurations.
 // For more information refer the docs: https://goo.gl/qPbSyX
 
 // IMPORTANT
-// When you add this file, we won't add the default configurations which is similar
+// When you add this file, we won't add the default configurations which is
+// similar
 // to "React Create App". This only has babel loader to load JavaScript.
+
+const SASS_DATA = `@import '~bootstrap-talend-theme/src/theme/guidelines';`;
+
 module.exports = {
 	plugins: [
 		// your custom plugins
 	],
 	module: {
 		loaders: [
+			{
+				test: /\.scss$/,
+				loaders: ['style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass'],
+			},
 			{ test: /\.json$/, loader: 'json' },
 		],
+	},
+	sassLoader: {
+		data: SASS_DATA,
 	},
 };
