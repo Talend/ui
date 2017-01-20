c51d8905d7994af9df5920d3611b401bc8e7765e
Fabien Rassinier <frassinier@talend.com>
2017-01-20 11:37:39 +0100

    chore(webpack): switch to webpack 2 (#121)

--- webpack.config.js
+++ webpack.config.js
@@ -1,104 +1,78 @@
+const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
-
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const extractFonts = new ExtractTextPlugin('fonts.css', {
-	allChunks: true,
-});
-const extractFontsMin = new ExtractTextPlugin('fonts.min.css', {
-	allChunks: true,
-});
-const extractBootstrap = new ExtractTextPlugin('bootstrap.css', {
-	allChunks: true,
-});
-const extractBootstrapMin = new ExtractTextPlugin('bootstrap.min.css', {
-	allChunks: true,
-});
-
-const autoprefixerConfig = autoprefixer({ browsers: ['last 2 versions'] });
-
-const BASE_CONF = {
-	entry: './src/index.js',
-	output: {
-		path: `${__dirname}/dist`,
-		filename: 'bootstrap.js',
-	},
-	resolve: ['', '.scss', '.css', '.js'],
-	module: {
-		loaders: [
-			{
-				test: /\.woff(2)?(\?[a-z0-9=&.]+)?$/,
-				loader: 'url',
-				query: {
-					limit: 50000,
-					mimetype: 'application/font-woff',
-					name: './fonts/[name].[ext]'
+module.exports = () => {
+	return {
+		entry: './src/index.js',
+		output: {
+			path: `${__dirname}/dist`,
+			filename: 'bootstrap.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.woff(2)?(\?[a-z0-9=&.]+)?$/,
+					loader: 'url-loader',
+					options: {
+						limit: 50000,
+						mimetype: 'application/font-woff',
+						name: './fonts/[name].[ext]'
+					}
+				},
+				{
+					test: /bootstrap\.scss$/,
+					loader: ExtractTextPlugin.extract({
+						fallbackLoader: 'style-loader',
+						loader: [
+							{
+								loader: 'css-loader',
+								query: {
+									importLoaders: 3,
+									minimize: true,
+									sourceMap: false
+								}
+							},
+							{
+								loader: 'postcss-loader'
+							},
+							{
+								loader: 'sass-loader',
+								query: {
+									sourceMap: true
+								}
+							},
+						],
+					}),
 				}
-			},
-			{
-				test: /\.(ttf|otf|eot|svg)(\?[a-z0-9=&.]+)?$/,
-				loader: 'file',
-				query: {
-					name: './fonts/[name].[ext]'
+			],
+		},
+		devtool: 'source-map',
+		plugins: [
+			/**
+			 * Loader options plugin helps people move from webpack 1 to webpack 2.
+			 * @reference https://webpack.js.org/plugins/loader-options-plugin
+			 */
+			new webpack.LoaderOptionsPlugin({
+				options: {
+					context: __dirname,
+					postcss: [
+						require('autoprefixer')({
+							browsers: ['last 2 versions'],
+							cascade: true,
+							remove: true
+						}),
+					]
 				}
-			},
-			{
-				test: /\.css$/,
-				loader: extractFonts.extract('style', 'css'),
-			},
-			{
-				test: /bootstrap\.scss$/,
-				loader: extractBootstrap.extract('style', 'css!postcss!sass'),
-			}
+			}),
+			new ExtractTextPlugin({
+				filename: 'bootstrap.css',
+				allChunks: true
+			}),
 		],
-	},
-	postcss: function () {
-		return [
-			autoprefixerConfig
-		];
-	},
-	plugins: [
-		extractFonts,
-		extractBootstrap
-	],
-	devServer: {
-		contentBase: './example',
-	},
+		devServer: {
+			contentBase: './example',
+		},
+	}
 };
 
-const MINIFIED = Object.assign({}, BASE_CONF, {
-	module: {
-		loaders: [
-			{
-				test: /\.woff(2)?(\?[a-z0-9=&.]+)?$/,
-				loader: 'url',
-				query: {
-					limit: 50000,
-					mimetype: 'application/font-woff',
-					name: './fonts/[hash].[ext]'
-				}
-			},
-			{
-				test: /\.(ttf|otf|eot|svg)(\?[a-z0-9=&.]+)?$/,
-				loader: 'file',
-				query: {
-					name: './fonts/[hash].[ext]'
-				}
-			},
-			{
-				test: /source-sans-pro\.css$/,
-				loader: extractFontsMin.extract('style', 'css?minimize'),
-			},
-			{
-				test: /bootstrap\.scss$/,
-				loader: extractBootstrapMin.extract('style', 'css?minimize!sass'),
-			}
-		],
-	},
-	plugins: [
-		extractFontsMin,
-		extractBootstrapMin,
-	],
-});
-
-module.exports = [BASE_CONF, MINIFIED];
