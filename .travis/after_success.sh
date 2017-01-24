864b22b1554c73e2059737357545a7f9e3680589
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-01-24 23:34:39 +0100

    chore: add lerna-semantic-release scripts

---
+++ .travis/after_success.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#!/bin/bash
+set -e
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "We are in a pull request, not releasing"
+  exit 0
+fi
+
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  npm run semantic-release
+fi
