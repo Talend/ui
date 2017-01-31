9fc1e982b8c79347004783d207aafe1dcabeca25
Fabien Rassinier <frassinier@talend.com>
2017-01-31 14:43:18 +0100

    chore(travis): change icons docs path

--- .travis/after_success_deploy.sh
+++ .travis/after_success_deploy.sh
@@ -7,10 +7,8 @@ if [ "$TRAVIS_PULL_REQUEST" == false ] && [ "$TRAVIS_BRANCH" == 'master' ]; then
 	echo "✓ Deploy showcases to somewhere"
 else
     lerna exec --scope=talend-icons -- yarn run docs
-	echo "✓ Run icons docs with Lerna"
-	cd packages/icons/docs
-	echo "✓ Move to icons docs dir"
-	surge --domain "talend-ui.$TRAVIS_PULL_REQUEST.surge.sh"
+	echo "✓ Generate icons showcase"
+	surge --project ./packages/icons/docs --domain "talend-ui.$TRAVIS_PULL_REQUEST.surge.sh"
 	echo "✓ Deploy PR#$TRAVIS_PULL_REQUEST to talend-ui.$TRAVIS_PULL_REQUEST.surge.sh"
 fi
 cd "$TRAVIS_BUILD_DIR"
