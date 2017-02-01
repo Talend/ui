f36894bf4d0d62396970d0394b71b25e942ef4d9
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-02-01 12:55:50 +0100

    style: use tabs to indent

--- .travis/after_success_deploy.sh
+++ .travis/after_success_deploy.sh
@@ -5,33 +5,33 @@ cd "$TRAVIS_BUILD_DIR"
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_BRANCH" == 'master' ]; then
 	echo "✓ Deploy showcases to somewhere"
 else
-    if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
-        mkdir .tmp
-        cp .surge/index.html .tmp
-	    echo "✓ Copy showcase index.html"
-        mkdir -p .tmp/components
-        lerna exec --scope=react-talend-components -- yarn run build-storybook
-        mv -v packages/components/storybook-static/* .tmp/components
-	    echo "✓ Move components showcase"
-        mkdir -p .tmp/containers
-        lerna exec --scope=react-talend-containers -- yarn run build-storybook
-        mv -v packages/containers/storybook-static/* .tmp/containers
-	    echo "✓ Move containers showcase"
-        mkdir -p .tmp/forms
-        lerna exec --scope=react-talend-forms -- yarn run build-storybook
-        mv -v packages/forms/storybook-static/* .tmp/forms
-	    echo "✓ Move forms showcase"
-        mkdir -p .tmp/icons
-        lerna exec --scope=talend-icons -- yarn run docs
-        mv -v packages/icons/docs/index.html .tmp/icons
-	    echo "✓ Move icons showcase"
-        mkdir -p .tmp/theme
-        mv -v packages/theme/example/index.html .tmp/theme
-        mv -v packages/theme/dist .tmp/theme
-	    echo "✓ Move theme showcase"
-	    find .tmp/
-	    surge --project .tmp --domain "talend-ui.$TRAVIS_PULL_REQUEST.surge.sh"
-	    echo "✓ Deploy PR#$TRAVIS_PULL_REQUEST to talend-ui.$TRAVIS_PULL_REQUEST.surge.sh"
-    fi
+	if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
+		mkdir .tmp
+		cp .surge/index.html .tmp
+		echo "✓ Copy showcase index.html"
+		mkdir -p .tmp/components
+		lerna exec --scope=react-talend-components -- yarn run build-storybook
+		mv -v packages/components/storybook-static/* .tmp/components
+		echo "✓ Move components showcase"
+		mkdir -p .tmp/containers
+		lerna exec --scope=react-talend-containers -- yarn run build-storybook
+		mv -v packages/containers/storybook-static/* .tmp/containers
+		echo "✓ Move containers showcase"
+		mkdir -p .tmp/forms
+		lerna exec --scope=react-talend-forms -- yarn run build-storybook
+		mv -v packages/forms/storybook-static/* .tmp/forms
+		echo "✓ Move forms showcase"
+		mkdir -p .tmp/icons
+		lerna exec --scope=talend-icons -- yarn run docs
+		mv -v packages/icons/docs/index.html .tmp/icons
+		echo "✓ Move icons showcase"
+		mkdir -p .tmp/theme
+		mv -v packages/theme/example/index.html .tmp/theme
+		mv -v packages/theme/dist .tmp/theme
+		echo "✓ Move theme showcase"
+		find .tmp/
+		surge --project .tmp --domain "talend-ui.$TRAVIS_PULL_REQUEST.surge.sh"
+		echo "✓ Deploy PR#$TRAVIS_PULL_REQUEST to talend-ui.$TRAVIS_PULL_REQUEST.surge.sh"
+	fi
 fi
 cd "$TRAVIS_BUILD_DIR"
--- .travis/after_success_screenshots.sh
+++ .travis/after_success_screenshots.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" != 'master' ]; then
-    lerna exec --scope=react-talend-components -- yarn run build-storybook
+	lerna exec --scope=react-talend-components -- yarn run build-storybook
 
-    mkdir .static
-    cp packages/components/storybook-static .static/components
-    nohup http-server .static/ -p 1337 >/dev/null 2>&1 &
-    sleep 5
-    echo "✓ Start static server"
+	mkdir .static
+	cp -R packages/components/storybook-static .static/components
+	nohup http-server .static/ -p 1337 >/dev/null 2>&1 &
+	sleep 5
+	echo "✓ Start static server"
 
-    lerna exec --scope=react-talend-components -- yarn run test:slimerjs
-    echo "✓ Run yarn test:slimerjs script for components"
+	lerna exec --scope=react-talend-components -- yarn run test:slimerjs
+	echo "✓ Run yarn test:slimerjs script for components"
 
-    rm -rf .static/*
+	rm -rf .static/*
 
-    cp packages/theme/dist .static
-    cp packages/theme/example/index.html .static
+	cp -R packages/theme/dist .static
+	cp -R packages/theme/example/index.html .static
 
-    lerna exec --scope=bootstrap-talend-theme -- yarn run test:slimerjs
-    echo "✓ Run yarn test:slimerjs script for theme"
+	lerna exec --scope=bootstrap-talend-theme -- yarn run test:slimerjs
+	echo "✓ Run yarn test:slimerjs script for theme"
 fi
