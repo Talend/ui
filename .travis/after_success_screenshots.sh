47f1564b68151679938d6cf7ea8d6c8ea4f401ee
Fabien Rassinier <frassinier@talend.com>
2017-02-01 02:10:02 +0100

    chore(travis): fix screenshots generation

--- .travis/after_success_screenshots.sh
+++ .travis/after_success_screenshots.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_BRANCH" != 'master' ]; then
-    #lerna exec --scope=react-talend-components -- yarn run build-storybook
-    #lerna exec --scope=react-talend-containers -- yarn run build-storybook
+    lerna exec --scope=react-talend-components -- yarn run build-storybook
 
-    cp packages/theme/example/index.html packages/theme/dist
-    nohup http-server packages/theme/dist -p 1337 >/dev/null 2>&1 &
+    mkdir .static
+    cp packages/components/storybook-static .static/components
+    nohup http-server .static/ -p 1337 >/dev/null 2>&1 &
     echo "✓ Start static server"
 
+    lerna exec --scope=react-talend-components -- yarn run test:slimerjs
+    echo "✓ Run yarn test:slimerjs script for components"
+
+    rm -rf .static/*
+
+    cp packages/theme/dist .static
+    cp packages/theme/example/index.html .static
+
     lerna exec --scope=bootstrap-talend-theme -- yarn run test:slimerjs
-    #lerna exec --scope=react-talend-components -- yarn run test:slimerjs
-    echo "✓ Run yarn test:slimerjs script"
+    echo "✓ Run yarn test:slimerjs script for theme"
 fi
