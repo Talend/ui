e7cc1b412ad4a3fc5b304ea8a6ae098fb52d1ac6
Fabien Rassinier <frassinier@talend.com>
2017-02-01 01:02:30 +0100

    chore(travis): fix git commands

--- .travis/after_success_git.sh
+++ .travis/after_success_git.sh
@@ -9,30 +9,22 @@ if [ -n "$GH_TOKEN" ]; then
     echo "✓ GH Token is here"
     cd "$TRAVIS_BUILD_DIR"
     echo "✓ Move to Travis build dir"
-    if [ "$TRAVIS_PULL_REQUEST_BRANCH" != "" ]; then
-        echo "git status"
-        git status
-        echo "git remote -v"
-        git remote -v
-        echo "git fetch origin"
-        git fetch origin
-        echo "git checkout $TRAVIS_PULL_REQUEST_BRANCH ($TRAVIS_BRANCH)"
-        git checkout $TRAVIS_PULL_REQUEST_BRANCH
-        echo "✓ Checkout $TRAVIS_PULL_REQUEST_BRANCH"
-        echo "git status"
-        git status
-
+    if [ "$TRAVIS_BRANCH" != 'master' ]; then
+        git checkout $TRAVIS_BRANCH
+        echo "✓ Checkout $TRAVIS_BRANCH"
         git add packages/theme/screenshots/
         git add packages/components/screenshots/
+        echo "git status"
+        git status
         git -c user.name='travis' -c user.email='travis' commit -m 'tests: update screenshots from CI'
-        echo "✓ Commit updated screenshots to $TRAVIS_PULL_REQUEST_BRANCH"
+        echo "✓ Commit updated screenshots to $TRAVIS_BRANCH"
 
-        git add output
+        git add output/
         git -c user.name='travis' -c user.email='travis' commit -m 'tests: udate code style outputs from CI'
-        echo "✓ Commit updated lint output to $TRAVIS_PULL_REQUEST_BRANCH"
+        echo "✓ Commit updated lint output to $TRAVIS_BRANCH"
 
-        git push -f -q https://jmfrancois:$GH_TOKEN@github.com/Talend/ui $TRAVIS_PULL_REQUEST_BRANCH &> /dev/null
-        echo "✓ Push to $TRAVIS_PULL_REQUEST_BRANCH"
+        git push -f -q https://jmfrancois:$GH_TOKEN@github.com/Talend/ui $TRAVIS_BRANCH &> /dev/null
+        echo "✓ Push to $TRAVIS_BRANCH"
     fi
     cd "$TRAVIS_BUILD_DIR"
     echo "✓ Move to Travis build dir"
