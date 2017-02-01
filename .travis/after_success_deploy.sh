f336bbaae67d5f2f394502028413eefb3051a7fa
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-02-01 18:07:19 +0100

    fix: add http to make an URL

--- .travis/after_success_deploy.sh
+++ .travis/after_success_deploy.sh
@@ -8,7 +8,7 @@ else
 	if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
 		surge --project .static --domain "$TRAVIS_PULL_REQUEST.talend.surge.sh"
 		echo "✓ Deploy PR#$TRAVIS_PULL_REQUEST to $TRAVIS_PULL_REQUEST.talend.surge.sh"
-		curl -f -XPOST -H "Authorization: token $GH_TOKEN" https://api.github.com/repos/Talend/ui/issues/$TRAVIS_PULL_REQUEST/comments -d "{\"body\": \":octocat: Demo is available [here]($TRAVIS_PULL_REQUEST.talend.surge.sh) \"}"
+		curl -f -XPOST -H "Authorization: token $GH_TOKEN" https://api.github.com/repos/Talend/ui/issues/$TRAVIS_PULL_REQUEST/comments -d "{\"body\": \":octocat: Demo is available [here](http://$TRAVIS_PULL_REQUEST.talend.surge.sh) \"}"
 	fi
 fi
 cd "$TRAVIS_BUILD_DIR"
