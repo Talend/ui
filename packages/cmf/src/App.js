8ae17c078792732b9134f41d94264b3eb779abd9
sromain@talend.com <8hyrmetlI8!t>
2017-02-03 23:22:16 +0100

    change syntax

--- packages/cmf/src/App.js
+++ packages/cmf/src/App.js
@@ -18,7 +18,7 @@ export default function App(props) {
 	return (
 		<Provider store={props.store}>
 			<RegistryProvider>
-				{props.children !== undefined ? props.children : <UIRouter history={hist} />}
+				{props.children || <UIRouter history={hist} />}
 			</RegistryProvider>
 		</Provider>
 	);
