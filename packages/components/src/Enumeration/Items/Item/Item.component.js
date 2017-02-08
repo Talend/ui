5692ae2e3bc74c44ccca69e49863b8c490fdb265
Jean-Alain Ré <jare@talend.com>
2017-02-08 17:11:27 +0100

    modify the ObjectFieldEnumeration

--- packages/components/src/Enumeration/Items/Item/Item.component.js
+++ packages/components/src/Enumeration/Items/Item/Item.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
- import Action from '../../../Actions/Action';
+import Action from '../../../Actions/Action';
 import theme from './Item.scss';
 import ItemPropTypes from './Item.propTypes';
 
