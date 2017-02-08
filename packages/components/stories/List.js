87925328fd9d95fa2fb6c7d0fb04ce06aeebceef
Jimmy Somsanith <jsomsanith@talend.com>
2017-02-08 16:05:10 +0100

    feat(List): filter dock mode (#74)
    The filter is a button that reveal the input on click

--- packages/components/stories/List.js
+++ packages/components/stories/List.js
@@ -201,6 +201,13 @@ const props = {
 			totalResults: 10,
 			onChange: action('pagination.onChange'),
 		},
+		filter: {
+			docked: true,
+			onBlur: action('filter.onBlur'),
+			onFocus: action('filter.onFocus'),
+			onFilter: action('filter.onFilter'),
+			onToggle: action('filter.onToggle'),
+		},
 	},
 };
 const columnsForItems = [
@@ -281,26 +288,30 @@ const itemPropsForItems = {
 	onChange: action('onTitleChange'),
 	onSubmit: action('onTitleEditSubmit'),
 };
+
 const sort = {
 	field: 'name',
 	isDescending: false,
 	onChange: action('sort.onChange'),
 };
-const getPropsFor = displayMode => ({
-	id: props.id,
-	displayMode,
-	list: {
-		columns: columnsForItems,
-		actions: actionsForItems,
-		items: itemsForItems,
-		itemProps: itemPropsForItems,
-	},
-	toolbar: props.toolbar,
-	useContent: true,
-});
+
+function getPropsFor(displayMode) {
+	return {
+		id: props.id,
+		displayMode,
+		list: {
+			columns: columnsForItems,
+			actions: actionsForItems,
+			items: itemsForItems,
+			itemProps: itemPropsForItems,
+		},
+		toolbar: props.toolbar,
+		useContent: true,
+	};
+}
 
 storiesOf('List', module)
-	.add('table (default)', () => (
+	.add('Table (default)', () => (
 		<div className="display-table tc-list-fixed-name-column">
 			<h1>List</h1>
 			<p>Display a list by defining your.</p>
@@ -308,21 +319,25 @@ storiesOf('List', module)
 			<List {...props} />
 		</div>
 	))
-	.add('large', () => {
-		const eprops = Object.assign({}, props);
-		eprops.displayMode = 'large';
+	.add('Large', () => {
+		const tprops = {
+			...props,
+			displayMode: 'large',
+		};
 		return (
 			<div>
 				<h1>List</h1>
 				<p>Display the list in large mode</p>
 				<IconsProvider defaultIcons={icons} />
-				<List {...eprops} />
+				<List {...tprops} />
 			</div>
 		);
 	})
-	.add('tile', () => {
-		const tprops = Object.assign({}, props);
-		tprops.displayMode = 'tile';
+	.add('Tile', () => {
+		const tprops = {
+			...props,
+			displayMode: 'tile',
+		};
 		return (
 			<div>
 				<h1>List</h1>
@@ -332,7 +347,53 @@ storiesOf('List', module)
 			</div>
 		);
 	})
-	.add('table with column actions', () => {
+	.add('No toolbar', () => {
+		const tprops = {
+			...props,
+			toolbar: undefined,
+		};
+		return (
+			<div>
+				<h1>List</h1>
+				<p>Display a list without toolbar</p>
+				<IconsProvider />
+				<div className="list-container">
+					<List {...tprops} />
+				</div>
+			</div>
+		);
+	})
+	.add('Toolbar with filter', () => {
+		const dockedProps = Immutable.fromJS(props).toJS();
+		dockedProps.list.items = [dockedProps.list.items[0]];
+		dockedProps.toolbar.actionBar = null;
+
+		const inputProps = Immutable.fromJS(dockedProps).toJS();
+		inputProps.toolbar.filter.docked = false;
+
+		const highlightedProps = Immutable.fromJS(inputProps).toJS();
+		highlightedProps.toolbar.filter.highlight = true;
+
+		const inputDebounceProps = Immutable.fromJS(inputProps).toJS();
+		inputDebounceProps.toolbar.filter.debounceTimeout = 300;
+
+		return (<div>
+			<IconsProvider />
+
+			<h1>List</h1>
+			<h2>Definition</h2>
+			<p>Toolbar Filter</p>
+			<h2>Docked</h2>
+			<List {...dockedProps} />
+			<h2>Input</h2>
+			<List {...inputProps} />
+			<h2>Highlighted</h2>
+			<List {...highlightedProps} />
+			<h2>Input with 300ms debounce</h2>
+			<List {...inputDebounceProps} />
+		</div>);
+	})
+	.add('Table with column actions', () => {
 		const columnActionsProps = Immutable.fromJS(props).toJS();
 		columnActionsProps.list.columns.splice(2, 0, { key: 'columnActions', label: '' });// label should be empty as the cell will appear only when item is hovered
 		columnActionsProps.list.items = columnActionsProps.list.items.map(item => ({
@@ -358,7 +419,7 @@ storiesOf('List', module)
 			<List {...columnActionsProps} />
 		</div>);
 	})
-	.add('table with selected items', () => {
+	.add('Table with selected items', () => {
 		const selectedItemsProps = Immutable.fromJS(props).toJS();
 		selectedItemsProps.toolbar.actionBar.selected = 1;
 		selectedItemsProps.toolbar.actionBar.multiSelectActions = {
@@ -381,21 +442,7 @@ storiesOf('List', module)
 			</div>
 		);
 	})
-	.add('table without toolbar', () => {
-		const tprops = {
-			...props,
-			toolbar: undefined,
-		};
-		return (
-			<div>
-				<h1>List</h1>
-				<p>Display a list without toolbar</p>
-				<IconsProvider defaultIcons={icons} />
-				<List {...tprops} />
-			</div>
-		);
-	})
-	.add('table with custom selected class', () => {
+	.add('Table with custom selected class', () => {
 		const selectedClassProps = Immutable.fromJS(props).toJS();
 		selectedClassProps.list.itemProps.selectedClass = 'tc-list-custom-style';
 		selectedClassProps.list.itemProps.isSelected = item => selected.find(next => next.id === item.id);
