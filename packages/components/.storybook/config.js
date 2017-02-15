50ec5b1f7a83281ee47a7e4739d0ad43840e3cab
Fabien Rassinier <frassinier@talend.com>
2017-02-15 11:49:27 +0100

    fix(storybook): import Bootstrap theme instead link it

--- packages/components/.storybook/config.js
+++ packages/components/.storybook/config.js
@@ -5,6 +5,8 @@ import a11y from 'react-a11y';
 import { configure, setAddon } from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
 
+import '!style!css!sass!bootstrap-talend-theme/src/theme/theme.scss';
+
 a11y(ReactDOM);
 
 setAddon(infoAddon);
