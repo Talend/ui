cf8ccd0a2b861a5adc9e25e868b1cc87a2a1ba18
amaalej <amaalej@talend.com>
2017-02-23 20:53:20 +0100

    feat(forms): datalist widget (#134)
    * feat(TDP-3433): pure typeahead
    
    * feat(forms): datalist widget supporting both select and typeahead widgets with cleaned CSS
    
    * feat(form): statefull datalist with required flag, default value, submit and caseInsensitive
    
    * Fix some linter errors and reorganise unit tests
    
    * test(ci): update screenshots
    
    * Remove non used propTypes
    
    * test(ci): update screenshots
    
    * Refactor
    
    * test(ci): update screenshots
    
    * Fix linter error
    
    * test(ci): update screenshots
    
    * test(ci): update screenshots
    
    * Unit tests
    
    * test(ci): update screenshots
    
    * Fix label placement
    
    * test(ci): update screenshots
    
    * feat(forms): datalist should have 100% width like select (#188)
    
    * Clean style
    
    * test(ci): update screenshots
    
    * Fix initial value
    
    * test(ci): update screenshots

---
+++ packages/forms/src/widgets/DatalistWidget/DatalistWidget.test.js
@@ -0,0 +1,187 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import DatalistWidget, { escapeRegexCharacters, getMatchingSuggestions } from './DatalistWidget';
+
+describe('escapeRegexCharacters', () => {
+	it('should escape all regex chars', () => {
+		// given
+		const unescapedString = 'wxc.*+?^${}()|[]\\aze';
+		const expectedString = 'wxc\\.\\*\\+\\?\\^\\$\\{\\}\\(\\)\\|\\[\\]\\\\aze';
+
+		// when
+		const escapedString = escapeRegexCharacters(unescapedString);
+
+		// then
+		expect(escapedString).toBe(expectedString);
+	});
+});
+
+describe('getMatchingSuggestions', () => {
+	it('should filter in case insensitive mode', () => {
+		// given
+		const value = 'aze';
+		const suggestions = [
+			'aze',
+			'banane',
+			'bananAze',
+			'   AzErTy   ',
+			'Toto'
+		];
+
+		// when
+		const filteredSuggestions = getMatchingSuggestions(suggestions, value);
+
+		// then
+		expect(filteredSuggestions).toEqual([
+			'aze',
+			'bananAze',
+			'   AzErTy   ',
+		]);
+	});
+
+	it('should filter with special chars', () => {
+		// given
+		const value = 'az[e';
+		const suggestions = [
+			'aze',
+			'banane',
+			'bananaz[e',
+			'   az[erty   ',
+		];
+
+		// when
+		const filteredSuggestions = getMatchingSuggestions(suggestions, value);
+
+		// then
+		expect(filteredSuggestions).toEqual([
+			'bananaz[e',
+			'   az[erty   ',
+		]);
+	});
+});
+
+describe('DatalistWidget', () => {
+	const schema = {
+		enum: [
+			'aze',
+			'banane',
+			'bananAze',
+			'   AzErTy   ',
+			'Toto'
+		],
+	};
+
+	it('should render input', () => {
+		// when
+		const wrapper = renderer.create(
+			<DatalistWidget
+				id="myWidget"
+				required={true}
+				schema={{}}
+				onChange={jest.fn()}
+			/>
+		).toJSON();
+
+		// then
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('should render "no match" message', () => {
+		// given
+		const wrapper = mount(
+			<DatalistWidget
+				id="myWidget"
+				required={true}
+			    schema={schema}
+				onChange={jest.fn()}
+			/>
+		);
+
+		// when
+		wrapper.find('input').at(0).simulate('change', {  target: { value: 'noMatchingValue' } });
+
+		// then
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('should render all suggestions on focus', () => {
+		// given
+		const wrapper = mount(
+			<DatalistWidget
+				id="myWidget"
+				required={true}
+				schema={schema}
+				onChange={jest.fn()}
+			/>
+		);
+
+		// when
+		wrapper.find('input').at(0).simulate('focus');
+
+		// then
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('should render hightlighted matching suggestions on value change', () => {
+		// given
+		const wrapper = mount(
+			<DatalistWidget
+				id="myWidget"
+				required={true}
+				schema={schema}
+				onChange={jest.fn()}
+			/>
+		);
+
+		// when
+		wrapper.find('input').at(0).simulate('change', {  target: { value: 'aze' } });
+
+		// then
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('should set value and reset suggestions on suggestion selection', () => {
+		// given
+		const onChange = jest.fn();
+		const wrapper = mount(
+			<DatalistWidget
+				id="myWidget"
+				required={true}
+				schema={schema}
+			    onChange={onChange}
+			/>
+		);
+		wrapper.find('input').at(0).simulate('focus'); // to display suggestions
+
+		// when
+		wrapper.find('#react-autowhatever-myWidget--item-0').simulate('mouseDown');
+
+		// then
+		expect(onChange).toBeCalledWith('aze');
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+
+	it('should reset value on unknown value input blur', () => {
+		// given
+		const onChange = jest.fn();
+		const wrapper = mount(
+			<DatalistWidget
+				id="myWidget"
+				required={true}
+				schema={schema}
+				onChange={onChange}
+			/>
+		);
+		const input = wrapper.find('input').at(0);
+		input.simulate('change', { target: { value: 'unknown' } });
+
+		// when
+		input.simulate('blur');
+
+		// then
+		expect(onChange).not.toBeCalled();
+		expect(toJson(wrapper)).toMatchSnapshot();
+	});
+});
