168dbce36af087862ddb372dfd9f00a0d5d3863a
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-02-28 16:35:42 +0100

    fix(containers/sidepanel): get the routerReplace info under payload
    this issue is here since the action structure is under payload #135

--- packages/containers/src/SidePanel/SidePanel.container.js
+++ packages/containers/src/SidePanel/SidePanel.container.js
@@ -1,3 +1,4 @@
+import get from 'lodash/get';
 import React, { PropTypes } from 'react';
 import { SidePanel as Component } from 'react-talend-components';
 import { api } from 'react-cmf';
@@ -37,13 +38,11 @@ class SidePanel extends React.Component {
 		const actions = actionIds.map((id) => {
 			const info = api.action.getActionInfo(this.context, id);
 			info.onClick = () => this.context.store.dispatch(info.payload);
-			if (info.cmf) {
-				if (info.cmf.routerReplace) {
-					const route = info.cmf.routerReplace;
-					const currentRoute = this.context.router.location.pathname;
-					if (currentRoute.indexOf(route) !== -1) {
-						info.active = true;
-					}
+			const route = get(info, 'payload.cmf.routerReplace');
+			if (route) {
+				const currentRoute = this.context.router.location.pathname;
+				if (currentRoute.indexOf(route) !== -1) {
+					info.active = true;
 				}
 			}
 			return info;
