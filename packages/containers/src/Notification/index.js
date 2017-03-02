a8a05ae204a7c928878af780c88ac1527fcc965d
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-03-02 10:28:54 +0100

    feat(containers/Notification): attach the reducer to add notification

--- packages/containers/src/Notification/index.js
+++ packages/containers/src/Notification/index.js
@@ -1,3 +1,15 @@
+import objectId from 'bson-objectid';
 import Notification from './Notification.connect';
+
+Notification.push = function pushNotification(state, notification) {
+	const path = ['Notification', 'Notification', 'notifications'];
+	let notifs = state.cmf.components.getIn(path);
+	notifs = notifs.push(Object.assign({
+		id: objectId(),
+	}, notification));
+	const newState = Object.assign({}, state);
+	newState.cmf.components = state.cmf.components.setIn(path, notifs);
+	return newState;
+};
 
 export default Notification;
