8530bba1c96bc8733b05b1fa61b74b0c1d41d209
Jean-Alain Ré <jare@talend.com>
2017-03-06 18:40:36 +0100

    add check if notifications exist

--- packages/containers/src/Notification/clearNotifications.js
+++ packages/containers/src/Notification/clearNotifications.js
@@ -1,6 +1,11 @@
 export default function clearNotifications(state) {
 	const path = ['Notification', 'Notification', 'notifications'];
 	let notifs = state.cmf.components.getIn(path);
+
+	if (!notifs) {
+		return state;
+	}
+
 	notifs = notifs.clear();
 	const newState = Object.assign({}, state);
 	newState.cmf.components = state.cmf.components.setIn(path, notifs);
