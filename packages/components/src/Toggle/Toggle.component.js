705e31840a49e8174c22c9ebcd043acf9c312566
Geoffroy Lorieux <glorieux@talend.com>
2017-03-10 11:19:11 +0100

    Update props based on @jsomsanith review

--- packages/components/src/Toggle/Toggle.component.js
+++ packages/components/src/Toggle/Toggle.component.js
@@ -4,17 +4,17 @@ import React, { PropTypes } from 'react';
  * The Toggle component is basically a fancy checkbox like you have in your iphone.
  * Properties:
  * @param id {string}: the id to be used for htmlFor and maybe QA
+ * @param onChange {function}: an event callback called each time the state is changed
  * @param checked {boolean}: sets the state of the toggle to be On or Off
  * @param disabled {boolean}: enables the read-only no-interaction mode
- * @param onChange {function}: an event callback called each time the state is changed
  * @param label {string}: a status label to be shown near toggle
  *
  * Required: [ id, onChange ]
- * Defaults: { checked: false, disabled: false }
+ * Defaults: { checked: false, disabled: false, label: '' }
  *
  * @return XML(JSX) React pure component
  * **/
-function Toggle({ id, onChange, label, children, ...props }) {
+function Toggle({ id, onChange, label, checked, disabled }) {
 	return (
 		<div className="tc-toggle switch checkbox">
 			<label htmlFor={id}>
@@ -22,19 +22,27 @@ function Toggle({ id, onChange, label, children, ...props }) {
 					type="checkbox"
 					id={id}
 					onChange={onChange}
-					{...props}
+					checked={checked}
+					disabled={disabled}
 				/>
-				<span>{label || children}</span>
+				<span>{label}</span>
 			</label>
 		</div>
 	);
 }
 
+Toggle.defaultProps = {
+	disabled: false,
+	checked: false,
+	label: '',
+};
+
 Toggle.propTypes = {
 	id: PropTypes.string.isRequired,
 	label: PropTypes.string,
 	onChange: PropTypes.func.isRequired,
-	children: PropTypes.node,
+	checked: PropTypes.bool,
+	disabled: PropTypes.bool,
 };
 
 export default Toggle;
