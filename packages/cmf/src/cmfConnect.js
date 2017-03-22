f5bc8735e8e390a59343883db3c919fa3fc354f7
jlame <jlame@talend.com>
2017-03-22 12:08:34 +0100

    adding js comment

--- packages/cmf/src/cmfConnect.js
+++ packages/cmf/src/cmfConnect.js
@@ -95,6 +95,7 @@ export function getDispatchToProps({
  * - didMountActionCreator (id or array of id)
  * - willUnMountActionCreator (id or array of id)
  * - componentId (or will use uuid)
+ * - keepComponentState (boolean, overrides the keepComponentState defined in container)
  * @return {ReactComponent}
  */
 export default function cmfConnect({
