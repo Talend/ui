417f1a1f145984ccb7d00e1ae7dbf3007aff4f3e
sromain@talend.com <8hyrmetlI8!t>
2017-03-23 14:28:01 +0100

    Add tests & externalize function

--- packages/components/src/Enumeration/Items/Item/Item.component.js
+++ packages/components/src/Enumeration/Items/Item/Item.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import { removeDuplicates, allIndexOf } from './utils';
 import Action from '../../../Actions/Action';
 import theme from './Item.scss';
 import ItemPropTypes from './Item.propTypes';
@@ -56,40 +57,6 @@ function Item({ id, item, searchCriteria }) {
 				link
 			/>
 		);
-	}
-
-	/**
-	 * This function allow to return all the indexof string of a given string
-	 * @param str string to search in
-	 * @param toSearch value to search
-	 */
-	function allIndexOf(str, toSearch) {
-		const indices = [];
-		for (let pos = str.indexOf(toSearch); pos !== -1; pos = str.indexOf(toSearch, pos + 1)) {
-			indices.push(pos);
-		}
-		return indices;
-	}
-
-	/**
-	 * This function allow to remove 'duplicates' indexes ( when same character is typed for example
-	 * : label : "aaaaa" for searched string "aa")
-	 *
-	 * @param indexes list of indexes
-	 * @param search searchCriteria
-	 */
-	function removeDuplicates(indexes, search) {
-		const array = [];
-
-		indexes.forEach((matchingIndex) => {
-			if (array.length === 0) {
-				array.push(matchingIndex);
-			} else if (matchingIndex >= array[array.length - 1] + search.length) {
-				array.push(matchingIndex);
-			}
-		});
-
-		return array;
 	}
 
 	/**
