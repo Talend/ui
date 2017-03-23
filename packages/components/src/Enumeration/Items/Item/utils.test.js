954e1e31278430b9e24264fd4a93f3a03e28877f
sromain@talend.com <8hyrmetlI8!t>
2017-03-23 14:49:21 +0100

    Update tests

--- packages/components/src/Enumeration/Items/Item/utils.test.js
+++ packages/components/src/Enumeration/Items/Item/utils.test.js
@@ -2,20 +2,20 @@ import { allIndexOf, removeDuplicates } from './utils';
 
 describe('allIndexOf', () => {
 	it('should get 2 indexes with allIndexOf', () => {
-		const indexes = allIndexOf('AaBbCcAa', 'aa');
+		const indexes = allIndexOf('AaBbCcAa'.toLowerCase(), 'Aa'.toLowerCase());
 		expect(indexes.length).toBe(2);
 		expect(indexes[0]).toBe(0);
 		expect(indexes[1]).toBe(6);
 	});
 
 	it('should get 1 indexes with allIndexOf', () => {
-		const indexes = allIndexOf('AaBbCcAa', 'bb');
+		const indexes = allIndexOf('AaBbCcAa'.toLowerCase(), 'bb'.toLowerCase());
 		expect(indexes.length).toBe(1);
 		expect(indexes[0]).toBe(2);
 	});
 
 	it('should get empty array from allIndexOf', () => {
-		const indexes = allIndexOf('AaBbCcAa', 'ee');
+		const indexes = allIndexOf('AaBbCcAa'.toLowerCase(), 'ee'.toLowerCase());
 		expect(indexes.length).toBe(0);
 	});
 });
@@ -23,10 +23,9 @@ describe('allIndexOf', () => {
 describe('removeDuplicates', () => {
 	it('should remove indexes with short search', () => {
 		const indexes = removeDuplicates([0, 1, 2, 3], 'aa');
-		expect(indexes.length).toBe(3);
+		expect(indexes.length).toBe(2);
 		expect(indexes[0]).toBe(0);
 		expect(indexes[1]).toBe(2);
-		expect(indexes[2]).toBe(3);
 	});
 
 	it('should not remove indexes when not in searchLenght', () => {
