567268c3c466a47a5b93426834156622573a058b
Sylvain-Talend <smaestri@talend.com>
2017-03-27 14:13:14 +0200

    fix(components): Enumeration component : change tooltip values (#315)
    * fix(components): Enumeration component : change tooltip values
    
    * test(ci): update code style outputs
    
    * fix(components): Enumeration component : change tooltip values
    
    * fix(components): Enumeration component : fix wording
    
    * test(ci): update code style outputs

--- packages/forms/src/widgets/EnumerationWidget/EnumerationWidget.js
+++ packages/forms/src/widgets/EnumerationWidget/EnumerationWidget.js
@@ -44,7 +44,7 @@ class EnumerationWidget extends React.Component {
 			id: 'edit',
 			onClick: this.onEnterEditModeItem.bind(this),
 		}, {
-			label: 'Delete',
+			label: 'Remove value',
 			icon: 'talend-trash',
 			id: 'delete',
 			onClick: this.onDeleteItem.bind(this),
@@ -65,7 +65,7 @@ class EnumerationWidget extends React.Component {
 				onClick: this.changeDisplayToSearchMode.bind(this),
 			}],
 			headerSelected: [{
-				label: 'Delete items',
+				label: 'Remove selected values',
 				icon: 'talend-trash',
 				id: 'delete',
 				onClick: this.onDeleteItems.bind(this),
