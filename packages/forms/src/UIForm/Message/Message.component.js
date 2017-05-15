1ded597c090b039215e06e6af3710e520de3210c
jsomsanith <jimmy.somsanith@gmail.com>
2017-05-15 09:34:57 +0200

    Add role on message

--- packages/forms/src/UIForm/Message/Message.component.js
+++ packages/forms/src/UIForm/Message/Message.component.js
@@ -10,7 +10,10 @@ export default function Message(props) {
 	const message = isValid ? description : errorMessage;
 	return message ?
 		(
-			<div className="help-block">
+			<div
+				className="help-block"
+				role="status"
+			>
 				{ message }
 			</div>
 		) :
