43933d26e0424baa76fe9ad677b44255f3745137
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-05-18 11:23:43 +0200

    feat(cmf): add redux-storage support

---
+++ packages/cmf/src/reduxstorage/redux-storage-decorator-immutablejs.js
@@ -0,0 +1,27 @@
+// FIXME: should be contribution
+import { fromJS } from 'immutable';
+
+export default (engine, whitelist = []) => {
+	return {
+		...engine,
+
+		load() {
+			return engine.load().then((result) => {
+				whitelist.forEach((keys) => {
+					if (typeof keys === 'string') {
+						keys = [keys]; // eslint-disable-line no-param-reassign
+					}
+					let tmp = result;
+					keys.forEach((key, index) => {
+						if (tmp && index === keys.length - 1) {
+							tmp[key] = fromJS(tmp[key]);
+						} else if (tmp) {
+							tmp = tmp[key];
+						}
+					});
+				});
+				return result;
+			});
+		},
+	};
+};
