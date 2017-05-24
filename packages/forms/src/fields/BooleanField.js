3158f06470b4bdbf72b667d633793b114b2aeed3
jlame <jlame@talend.com>
2017-05-24 17:49:17 +0200

    fixing boolean field

--- packages/forms/src/fields/BooleanField.js
+++ packages/forms/src/fields/BooleanField.js
@@ -1,78 +1,98 @@
-import React, {PropTypes} from "react";
+import React, { PropTypes } from 'react';
 
 import {
-  getWidget,
-  getUiOptions,
-  optionsList,
-  getDefaultRegistry
+	getWidget,
+	getUiOptions,
+	optionsList,
+	getDefaultRegistry,
 } from 'react-jsonschema-form/lib/utils';
+import Toggle from 'react-talend-components/lib/Toggle';
 
-function BooleanField(props) {
-  const {
-    schema,
-    name,
-    uiSchema,
-    idSchema,
-    formData,
-    registry,
-    required,
-    disabled,
-    readonly,
-    autofocus,
-    onChange
-  } = props;
-  const {title} = schema;
-  const {widgets, formContext} = registry;
-  const {widget="checkbox", ...options} = getUiOptions(uiSchema);
-  const Widget = getWidget(schema, widget, widgets);
-  const enumOptions = optionsList({
-    enum: [true, false],
-    enumNames: schema.enumNames || ["yes", "no"]
-  });
-  return <Widget
-    options={{...options, enumOptions}}
-    schema={schema}
-    id={idSchema && idSchema.$id}
-    onChange={onChange}
-    label={title === undefined ? name : title}
-    value={formData}
-    required={required}
-    disabled={disabled}
-    readonly={readonly}
-    registry={registry}
-    formContext={formContext}
-    autofocus={autofocus}/>;
+
+function buildOptions(schema) {
+	return {
+		enumOptions: optionsList(Object.assign({
+			enumNames: ['true', 'false'],
+			enum: [true, false],
+		}, { enumNames: schema.enumNames })),
+	};
 }
 
-if (process.env.NODE_ENV !== "production") {
-  BooleanField.propTypes = {
-    schema: PropTypes.object.isRequired,
-    uiSchema: PropTypes.object,
-    idSchema: PropTypes.object,
-    onChange: PropTypes.func.isRequired,
-    formData: PropTypes.bool,
-    required: PropTypes.bool,
-    disabled: PropTypes.bool,
-    readonly: PropTypes.bool,
-    autofocus: PropTypes.bool,
-    registry: PropTypes.shape({
-      widgets: PropTypes.objectOf(PropTypes.oneOfType([
-        PropTypes.func,
-        PropTypes.object,
-      ])).isRequired,
-      fields: PropTypes.objectOf(PropTypes.func).isRequired,
-      definitions: PropTypes.object.isRequired,
-      formContext: PropTypes.object.isRequired,
-    })
-  };
+function BooleanField(props) {
+	const {
+		schema,
+		name,
+		uiSchema,
+		idSchema,
+		formData,
+		registry,
+		required,
+		disabled,
+		readonly,
+		onChange,
+		onBlur,
+	} = props;
+	const { title } = schema;
+	const { widgets, formContext } = registry;
+	const widget = uiSchema['ui:widget'];
+	const uiOptions = getUiOptions(uiSchema);
+	const onChangeHandler = () => {
+		onChange(!formData, uiOptions);
+	};
+	const onBlurHandler = () => {
+		if (onBlur) {
+			onBlur(idSchema && idSchema.$id, formData);
+		}
+	};
+	const commonProps = {
+		schema,
+		id: idSchema && idSchema.$id,
+		onChange: onChangeHandler,
+		onBlur: onBlurHandler,
+		label: (title === undefined) ? name : title,
+		value: formData, 
+		checked: formData,
+		required,
+		disabled,
+		readonly,
+		registry,
+		formContext,
+	};
+	if (widget) {
+		const Widget = getWidget(schema, widget, widgets);
+		return <Widget options={buildOptions(schema)} {... commonProps} />;
+	}
+	return <Toggle options={buildOptions(schema)} {...commonProps} />;
+}
+
+if (process.env.NODE_ENV !== 'production') {
+	BooleanField.propTypes = {
+		schema: PropTypes.object.isRequired,
+		uiSchema: PropTypes.object,
+		idSchema: PropTypes.object,
+		onChange: PropTypes.func.isRequired,
+		onBlur: PropTypes.func,
+		formData: PropTypes.bool,
+		required: PropTypes.bool,
+		disabled: PropTypes.bool,
+		readonly: PropTypes.bool,
+		registry: PropTypes.shape({
+			widgets: PropTypes.objectOf(PropTypes.oneOfType([
+				PropTypes.func,
+				PropTypes.object,
+			])).isRequired,
+			fields: PropTypes.objectOf(PropTypes.func).isRequired,
+			definitions: PropTypes.object.isRequired,
+			formContext: PropTypes.object.isRequired,
+		}),
+	};
 }
 
 BooleanField.defaultProps = {
-  uiSchema: {},
-  registry: getDefaultRegistry(),
-  disabled: false,
-  readonly: false,
-  autofocus: false,
+	uiSchema: {},
+	registry: getDefaultRegistry(),
+	disabled: false,
+	readonly: false,
 };
 
 export default BooleanField;
