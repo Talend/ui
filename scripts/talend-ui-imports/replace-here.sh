edbfdb91a2c6897a4debabb19a42ebf4a8d14f0a
jsomsanith <jsomsanith@talend.com>
2017-05-30 10:40:59 +0200

    Script to replace this repo

---
+++ scripts/talend-ui-imports/replace-here.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./replace-imports.sh /home/jsomsanith/Documents/workspace/ui/packages/cmf
+./replace-imports.sh /home/jsomsanith/Documents/workspace/ui/packages/components
+./replace-imports.sh /home/jsomsanith/Documents/workspace/ui/packages/containers
+./replace-imports.sh /home/jsomsanith/Documents/workspace/ui/packages/forms
+./replace-imports.sh /home/jsomsanith/Documents/workspace/ui/packages/icons
+./replace-imports.sh /home/jsomsanith/Documents/workspace/ui/packages/logging
+./replace-imports.sh /home/jsomsanith/Documents/workspace/ui/packages/theme
