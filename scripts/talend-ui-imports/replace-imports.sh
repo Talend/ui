0e7071ea353dc3c2d65c14debc2dc6da4b482ec8
jsomsanith <jsomsanith@talend.com>
2017-05-31 11:22:19 +0200

    Add logs in script

--- scripts/talend-ui-imports/replace-imports.sh
+++ scripts/talend-ui-imports/replace-imports.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
+source "${BASEDIR}/../utils.sh"
+
+# From version 0.77.0, the packages are published in the @talend organisation.
+# This scripts :
+#   * install jscodeshift globally if needed
+#   * replace js imports in all the js files in src/, /examples, /stories
+#   * replace bootstrap-talend-theme imports in scss
+#   * replace imports in src/**/*js
+#   * replace references in configurations /*.js and /config/*.js
+
 # install jscodeshift if needed
 if ! type jscodeshift > /dev/null
 then
-    echo "You will need jscodeshift to run this script, but it is not installed."
+    print_title "You will need jscodeshift to run this script, but it is not installed."
     echo "Let's install it : sudo npm install -g jscodeshift"
     sudo npm install -g jscodeshift
 fi
@@ -22,15 +33,21 @@ then
 fi
 
 # replace js imports
-BASEDIR=$(dirname "$0")
+print_title "Execute codemod on $1/examples"
 jscodeshift -t "${BASEDIR}/codemod.js" $1/examples
+
+print_title "Execute codemod on $1/stories"
 jscodeshift -t "${BASEDIR}/codemod.js" $1/stories
+
+print_title "Execute codemod on $1/src"
 jscodeshift -t "${BASEDIR}/codemod.js" $1/src
 
 # replace bootstrap-talend-theme in all scss
+print_title "Replace bootstrap-talend-theme in all scss"
 find $1/src -iregex '.*\.scss' | xargs sed -i 's/bootstrap-talend-theme/@talend\/bootstrap-theme/g'
 
 # replace modules in package.json
+print_title "Replace dependencies in package.json"
 REPLACE_THEME_SCHEMA='s/bootstrap-talend-theme/@talend\/bootstrap-theme/g'
 REPLACE_ICON_SCHEMA='s/talend-icons/@talend\/icons/g'
 REPLACE_LOG_SCHEMA='s/talend-log/@talend\/log/g'
@@ -42,6 +59,7 @@ ALL_REPLACE="$REPLACE_THEME_SCHEMA; $REPLACE_ICON_SCHEMA; $REPLACE_LOG_SCHEMA; $
 sed -i "$ALL_REPLACE" $1/package.json
 
 # replace webpack sass data config
+print_title "Replace references in configurations (webpack)"
 REPLACE_TALEND_MODULES='s/react-talend-/@talend\\\//g'
 ALL_REPLACE_WITH_WILDCARD="$ALL_REPLACE; $REPLACE_TALEND_MODULES"
 sed -i "$ALL_REPLACE_WITH_WILDCARD" $1/*.js
