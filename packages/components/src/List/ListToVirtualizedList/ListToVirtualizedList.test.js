300e016c34dfabcbfabbb177bad44a8951888e39
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-06-02 16:56:33 +0200

    test: fix

--- packages/components/src/List/ListToVirtualizedList/ListToVirtualizedList.test.js
+++ packages/components/src/List/ListToVirtualizedList/ListToVirtualizedList.test.js
@@ -5,9 +5,17 @@ import ListToVirtualizedList from './ListToVirtualizedList.component';
 
 describe('ListToVirtualizedList', () => {
 	it('should render', () => {
+		const props = {
+			id: 'mylistid',
+			items: [{ id: 3 }],
+			columns: [{ key: 'id', label: 'Id' }],
+			titleProps: {
+
+			},
+		};
 		const wrapper = shallow(
-			<ListToVirtualizedList />
+			<ListToVirtualizedList {...props} />
 		);
-		expect(wrapper).toMatchSnapshot();
+		expect(wrapper.root.node).toMatchSnapshot();
 	});
 });
