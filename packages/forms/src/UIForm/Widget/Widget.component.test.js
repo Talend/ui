2ef3c5e0bc06bfc502359862434f9ac7f5a33245
jsomsanith <jsomsanith@talend.com>
2017-06-05 15:36:06 +0200

    Widget unit tests

---
+++ packages/forms/src/UIForm/Widget/Widget.component.test.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Widget from './Widget.component';
+
+describe('Widget component', () => {
+	const schema = {
+		key: ['user', 'firstname'],
+		type: 'text',
+	};
+	const errors = {
+		'user,firstname': 'This is not ok',
+	};
+	const properties = {
+		user: {
+			firstname: 'my firstname',
+			lastname: 'my lastname',
+		},
+		comment: '',
+	};
+
+	it('should render widget', () => {
+		// when
+		const wrapper = shallow(
+			<Widget
+				formName={'myForm'}
+				onChange={jest.fn('onChange')}
+				onTrigger={jest.fn('onTrigger')}
+				properties={properties}
+				schema={schema}
+				errors={errors}
+			/>
+		);
+
+		// then
+		expect(wrapper.node).toMatchSnapshot();
+	});
+
+	it('should render nothing if widget does not exist', () => {
+		// given
+		const unknownWidgetSchema = {
+			...schema,
+			type: 'unknown',
+		};
+
+		// when
+		const wrapper = shallow(
+			<Widget
+				properties={properties}
+				schema={unknownWidgetSchema}
+				errors={errors}
+			/>
+		);
+
+		// then
+		expect(wrapper.node).toMatchSnapshot();
+	});
+
+	it('should render custom widget', () => {
+		// given
+		const widgets = {
+			customWidget() {
+				return (<div>my widget</div>);
+			},
+		};
+		const customWidgetSchema = {
+			...schema,
+			type: 'customWidget',
+		};
+
+		// when
+		const wrapper = shallow(
+			<Widget
+				formName={'myForm'}
+				onChange={jest.fn('onChange')}
+				onTrigger={jest.fn('onTrigger')}
+				properties={properties}
+				schema={customWidgetSchema}
+				errors={errors}
+				widgets={widgets}
+			/>
+		);
+
+		// then
+		expect(wrapper.node).toMatchSnapshot();
+	});
+
+	it('should pass validation message from schema over message from errors', () => {
+		// given
+		const customValidationMessageSchema = {
+			...schema,
+			validationMessage: 'My custom validation message',
+		};
+
+		// when
+		const wrapper = shallow(
+			<Widget
+				formName={'myForm'}
+				onChange={jest.fn('onChange')}
+				onTrigger={jest.fn('onTrigger')}
+				properties={properties}
+				schema={customValidationMessageSchema}
+				errors={errors}
+			/>
+		);
+
+		// then
+		expect(wrapper.node.props.errorMessage).toBe('My custom validation message');
+	});
+
+	it('should pass message from errors when there is no validation message in schema', () => {
+		// when
+		const wrapper = shallow(
+			<Widget
+				formName={'myForm'}
+				onChange={jest.fn('onChange')}
+				onTrigger={jest.fn('onTrigger')}
+				properties={properties}
+				schema={schema}
+				errors={errors}
+			/>
+		);
+
+		// then
+		expect(wrapper.node.props.errorMessage).toBe('This is not ok');
+	});
+});
