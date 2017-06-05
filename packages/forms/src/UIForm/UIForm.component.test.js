985c923287647ee97ba27cfcdd98bf6fefeb7ef7
Jimmy Somsanith <JSO.Technologies@gmail.com>
2017-06-05 23:20:41 +0200

    Continue UIForm component unit tests

--- packages/forms/src/UIForm/UIForm.component.test.js
+++ packages/forms/src/UIForm/UIForm.component.test.js
@@ -1,50 +1,92 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import UIForm from './UIForm.component';
 
-describe('UIForm component', () => {
-	const props = {
-		autocomplete: 'off',
-		customValidation: jest.fn(),
-		formName: 'myForm',
-		onChange: jest.fn(),
-		onTrigger: jest.fn(),
-		onSubmit: jest.fn(),
-	};
+const props = {
+	autocomplete: 'off',
+	customValidation: jest.fn(),
+	formName: 'myForm',
+	onChange: jest.fn(),
+	onTrigger: jest.fn(),
+	onSubmit: jest.fn(),
+	setError: jest.fn(),
+	setErrors: jest.fn(),
+	updateForm: jest.fn(),
+};
 
-	const data = {
-		jsonSchema: {
-			type: 'object',
-			title: 'Comment',
-			properties: {
-				lastname: {
-					type: 'string',
-				},
-				firstname: {
-					type: 'string',
-				},
+const data = {
+	jsonSchema: {
+		type: 'object',
+		title: 'Comment',
+		properties: {
+			lastname: {
+				type: 'string',
+				minLength: 10,
 			},
-			required: [
-				'firstname',
-			],
+			firstname: {
+				type: 'string',
+			},
+			check: {},
 		},
-		uiSchema: [
-			{
-				key: 'lastname',
-				title: 'Last Name (with description)',
-				description: 'Hint: this is the last name',
-				autoFocus: true,
-			},
-			{
-				key: 'firstname',
-				title: 'First Name (with placeholder)',
-				placeholder: 'Enter your firstname here',
-			},
+		required: [
+			'firstname',
 		],
-		properties: {},
-	};
+	},
+	uiSchema: [
+		{
+			key: 'lastname',
+			title: 'Last Name (with description)',
+			description: 'Hint: this is the last name',
+			autoFocus: true,
+		},
+		{
+			key: 'firstname',
+			title: 'First Name (with placeholder)',
+			placeholder: 'Enter your firstname here',
+			triggers: ['after'],
+		},
+		{
+			key: 'check',
+			type: 'button',
+			title: 'Check the thing',
+			triggers: ['after'],
+		},
+	],
+	properties: {},
+	errors: {},
+};
 
+const mergedSchema = [
+	{
+		autoFocus: true,
+		description: 'Hint: this is the last name',
+		key: ['lastname'],
+		minlength: 10,
+		ngModelOptions: {},
+		schema: { minLength: 10, type: 'string' },
+		title: 'Last Name (with description)',
+		type: 'text',
+	},
+	{
+		key: ['firstname'],
+		ngModelOptions: {},
+		placeholder: 'Enter your firstname here',
+		required: true,
+		schema: { type: 'string' },
+		title: 'First Name (with placeholder)',
+		triggers: ['after'],
+		type: 'text',
+	},
+	{
+		key: ['check'],
+		title: 'Check the thing',
+		triggers: ['after'],
+		type: 'button',
+	},
+];
+
+describe('UIForm component', () => {
 	it('should render form', () => {
 		// when
 		const wrapper = shallow(<UIForm {...data} {...props} />);
@@ -54,34 +96,93 @@ describe('UIForm component', () => {
 	});
 
 	describe('#onChange', () => {
-		it('should validate change', () => {
+		it('should call onChange callback', () => {
 			// given
-			const wrapper = shallow(<UIForm {...data} {...props} />);
+			const wrapper = mount(<UIForm {...data} {...props} />);
+			const newValue = 'toto';
+			const event = { target: { value: newValue } };
+			const inputValidationError = 'String is too short (4 chars), minimum 10';
+			expect(props.onChange).not.toBeCalled();
 
 			// when
-			// renderer._instance._instance.onChange
-		});
+			wrapper.find('input').at(0).simulate('change', event);
 
-		it('should call onChange callback', () => {
-
+			// then
+			expect(props.onChange).toBeCalledWith(
+				props.formName,
+				mergedSchema[0],
+				newValue,
+				inputValidationError,
+			);
+			expect(props.onTrigger).not.toBeCalled();
 		});
 
 		it('should trigger "after" trigger', () => {
+			// given
+			const wrapper = mount(<UIForm {...data} {...props} />);
+			const newValue = 'toto';
+			const event = { target: { value: newValue } };
+			expect(props.onTrigger).not.toHaveBeenCalled();
+			props.onTrigger.mockReturnValueOnce(Promise.resolve({}));
 
+			// when
+			wrapper.find('input').at(1).simulate('change', event);
+
+			// then
+			expect(props.onTrigger).toHaveBeenCalledWith(
+				'after',
+				mergedSchema[1],
+				newValue,
+				data.properties,
+			);
 		});
 	});
 
 	describe('#onTrigger', () => {
-		it('should do nothing if there is no trigger callback', () => {
-
-		});
-
 		it('should call trigger callback', () => {
+			// given
+			const wrapper = mount(<UIForm {...data} {...props} />);
+			props.onTrigger.mockReturnValueOnce(Promise.resolve({}));
 
+			// when
+			wrapper.find('button').at(0).simulate('click');
+
+			// then
+			expect(props.onTrigger).toHaveBeenCalledWith(
+				'after',
+				mergedSchema[2],
+				undefined,
+				data.properties,
+			);
 		});
 
 		it('should updateForm on trigger success', () => {
-
+			// given
+			// const wrapper = mount(<UIForm {...data} {...props} />);
+			// const nextData = {
+			// 	jsonSchema: {
+			// 		type: 'object',
+			// 		title: 'User',
+			// 		properties: {
+			// 			name: { type: 'string' },
+			// 		},
+			// 	},
+			// 	uiSchema: ['name'],
+			// 	properties: { name: 'toto' },
+			// 	errors: { name: 'This field is required' },
+			// };
+			// props.onTrigger.mockReturnValueOnce(Promise.resolve(nextData).then(done));
+			//
+			// // when
+			// wrapper.find('button').at(0).simulate('click');
+			//
+			// // then
+			// expect(props.updateForm).toHaveBeenCalledWith(
+			// 	'after',
+			// 	mergedSchema[2],
+			// 	undefined,
+			// 	data.properties,
+			// );
 		});
 
 		it('should setError after trigger failure', () => {
