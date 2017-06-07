1e3e2f56e9731c635520a95673d88533b84213e9
Jimmy Somsanith <JSO.Technologies@gmail.com>
2017-06-07 09:43:44 +0200

    Suppress unwanted lint error

--- packages/forms/src/UIForm/actions/model.actions.js
+++ packages/forms/src/UIForm/actions/model.actions.js
@@ -1,5 +1,6 @@
 import { TF_UPDATE_FORM_DATA } from './constants';
 
+// eslint-disable-next-line import/prefer-default-export
 export function updateFormData(formName, schema, value, error) {
 	return {
 		type: TF_UPDATE_FORM_DATA,
