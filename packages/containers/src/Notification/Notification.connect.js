01cf117d134e643136fbab7b3b8e7d7b73cca2b4
Cateland <acateland@talend.com>
2017-06-12 16:55:16 +0200

    update component.setState to accept mutator - with prevState as parameter update Notification connect to use the mutator version of setState

--- packages/containers/src/Notification/Notification.connect.js
+++ packages/containers/src/Notification/Notification.connect.js
@@ -8,18 +8,24 @@ export function componentId(ownProps) {
 }
 
 export function mergeProps(stateProps, dispatchProps, ownProps) {
-	return Object.assign({
-		deleteNotification(i) {
-			const notifications = stateProps.state.get('notifications');
-			const index = notifications.indexOf(i);
-			if (index === -1) {
-				invariant(true, `notification not found ${JSON.stringify(i)}`);
-			}
-			const newNotif = notifications.delete(index);
-			const newState = stateProps.state.set('notifications', newNotif);
-			dispatchProps.setState(newState);
+	return Object.assign(
+		{
+			deleteNotification(i) {
+				dispatchProps.setState((prevStateProps) => {
+					const notifications = prevStateProps.state.get('notifications');
+					const index = notifications.indexOf(i);
+					if (index === -1) {
+						invariant(true, `notification not found ${JSON.stringify(i)}`);
+					}
+					const newNotif = notifications.delete(index);
+					return prevStateProps.state.set('notifications', newNotif);
+				});
+			},
 		},
-	}, ownProps, stateProps, dispatchProps);
+		ownProps,
+		stateProps,
+		dispatchProps,
+	);
 }
 
 export default cmfConnect({
