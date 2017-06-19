c41d296d0744fde703bf40bc2900d894edca8ccb
Nicolas Comont <ncomont@talend.com>
2017-06-19 10:19:34 +0200

    fix(components/typeahead): use onmousedown instead of onclick

--- packages/components/src/Typeahead/Typeahead.test.js
+++ packages/components/src/Typeahead/Typeahead.test.js
@@ -165,7 +165,7 @@ describe('Typeahead', () => {
 
 			// when
 			const typeaheadInstance = mount(typeahead);
-			typeaheadInstance.find('Item').at(0).simulate('click');
+			typeaheadInstance.find('Item').at(0).simulate('mouseDown');
 
 			// then
 			expect(onSelect).toBeCalled();
