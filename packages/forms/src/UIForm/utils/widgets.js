da506b6ab20ca51d8d48f369f7b1a10ff84909e3
Jimmy Somsanith <jsomsanith@talend.com>
2017-06-26 16:18:12 +0200

    feat(UIForm): buttons (#558)

--- packages/forms/src/UIForm/utils/widgets.js
+++ packages/forms/src/UIForm/utils/widgets.js
@@ -1,7 +1,8 @@
 import Fieldset from '../fieldsets/Fieldset';
 import Tabs from '../fieldsets/Tabs';
 
-import Button from '../fields/Button';
+import Button from '../fields/Button/Button.component';
+import Buttons from '../fields/Button/Buttons.component';
 import CheckBox from '../fields/CheckBox/CheckBox.component';
 import CheckBoxes from '../fields/CheckBox/CheckBoxes.component';
 import Radios from '../fields/Radios';
@@ -15,10 +16,13 @@ const widgets = {
 
 	// fields
 	button: Button,
+	buttons: Buttons,
 	checkbox: CheckBox,
 	checkboxes: CheckBoxes,
 	number: Text,
 	radios: Radios,
+	reset: Button,
+	submit: Button,
 	text: Text,
 	textarea: TextArea,
 };
