2079440a7dc94236a07cf8ce4c8edb68f232f8d0
jsomsanith <jsomsanith@talend.com>
2017-07-03 11:29:24 +0200

    Deploy only on master and PR, extract .static folder management in a script

--- .travis/after_success_deploy.sh
+++ .travis/after_success_deploy.sh
@@ -5,9 +5,11 @@ cd "$TRAVIS_BUILD_DIR"
 if [ "$TRAVIS_BRANCH" == 'master' ]; then
 	surge --project .static --domain "talend.surge.sh"
 	echo "✓ Deploy to talend.surge.sh"
-else
+elif [ "$TRAVIS_PULL_REQUEST" != 'false' ]
     surge --project .static --domain "$TRAVIS_PULL_REQUEST.talend.surge.sh"
     echo "✓ Deploy PR#$TRAVIS_PULL_REQUEST to $TRAVIS_PULL_REQUEST.talend.surge.sh"
     curl -f -XPOST -H "Authorization: token $GH_TOKEN" https://api.github.com/repos/Talend/ui/issues/$TRAVIS_PULL_REQUEST/comments -d "{\"body\": \":octocat: [Demo is available here](http://$TRAVIS_PULL_REQUEST.talend.surge.sh) \"}"
+else
+    echo "✓ No storybook to publish"
 fi
 cd "$TRAVIS_BUILD_DIR"
--- .travis/after_success_static.sh
+++ .travis/after_success_static.sh
@@ -11,25 +11,7 @@ mkdir .static/forms
 mkdir .static/icons
 mkdir .static/theme
 cp .surge/index.html .static
-
-lerna exec --scope=react-talend-components -- yarn run build-storybook
-cp -R packages/components/storybook-static/* .static/components
-echo "✓ Copy components showcase to .static"
-lerna exec --scope=react-talend-containers -- yarn run build-storybook
-cp -R packages/containers/storybook-static/* .static/containers
-echo "✓ Copy containers showcase to .static"
-
-lerna exec --scope=react-talend-forms -- yarn run build-storybook
-cp -R packages/forms/storybook-static/* .static/forms
-echo "✓ Copy forms showcase to .static"
-
-lerna exec --scope=talend-icons -- yarn run docs
-cp -R packages/icons/docs/index.html .static/icons
-echo "✓ Copy icons showcase to .static"
-
-cp -R packages/theme/example/index.html .static/theme
-cp -R packages/theme/dist .static/theme
-echo "✓ Copy theme showcase to .static"
+echo "✓ Initialized .static"
 
 find .static/
 
