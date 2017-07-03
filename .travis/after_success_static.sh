b1e877df042a1423a9c64d7cbf4ff434efd87729
jsomsanith <jsomsanith@talend.com>
2017-07-03 14:00:42 +0200

    Fix scripts

--- .travis/after_success_static.sh
+++ .travis/after_success_static.sh
@@ -5,10 +5,12 @@ cd "$TRAVIS_BUILD_DIR"
 
 rm -rf .static
 mkdir .static
+mkdir .static/cmf
 mkdir .static/components
 mkdir .static/containers
 mkdir .static/forms
 mkdir .static/icons
+mkdir .static/logging
 mkdir .static/theme
 cp .surge/index.html .static
 echo "✓ Initialized .static"
