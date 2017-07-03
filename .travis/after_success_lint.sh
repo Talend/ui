a5535915bf81e51ff04eb3f3b29b2f7a79e54345
jsomsanith <jsomsanith@talend.com>
2017-07-03 17:20:41 +0200

    Triggers lint and screeshots only on PRs, trigger build only on master or PR

--- .travis/after_success_lint.sh
+++ .travis/after_success_lint.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 #!/bin/bash
 
-echo "Linting JavaScript files"
-lerna exec --scope=react-cmf -- npm run lint:es 1> output/cmf.eslint.txt 2>/dev/null
-lerna exec --scope=react-talend-components -- npm run lint:es 1> output/components.eslint.txt 2>/dev/null
-lerna exec --scope=react-talend-containers -- npm run lint:es 1> output/containers.eslint.txt 2>/dev/null
-lerna exec --scope=react-talend-forms -- npm run lint:es 1> output/forms.eslint.txt 2>/dev/null
-lerna exec --scope=talend-log -- npm run lint:es 1> output/logging.eslint.txt 2>/dev/null
+if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
+	echo "Linting JavaScript files"
+	lerna exec --scope=react-cmf -- npm run lint:es 1> output/cmf.eslint.txt 2>/dev/null
+	lerna exec --scope=react-talend-components -- npm run lint:es 1> output/components.eslint.txt 2>/dev/null
+	lerna exec --scope=react-talend-containers -- npm run lint:es 1> output/containers.eslint.txt 2>/dev/null
+	lerna exec --scope=react-talend-forms -- npm run lint:es 1> output/forms.eslint.txt 2>/dev/null
+	lerna exec --scope=talend-log -- npm run lint:es 1> output/logging.eslint.txt 2>/dev/null
 
-echo "Linting Sass files"
-lerna exec --scope=bootstrap-talend-theme -- npm run lint:style 1> output/theme.sasslint.txt 2>/dev/null
-lerna exec --scope=react-talend-components -- npm run lint:style 1> output/components.sasslint.txt 2>/dev/null
-lerna exec --scope=react-talend-forms -- npm run lint:style 1> output/forms.sasslint.txt 2>/dev/null
+	echo "Linting Sass files"
+	lerna exec --scope=bootstrap-talend-theme -- npm run lint:style 1> output/theme.sasslint.txt 2>/dev/null
+	lerna exec --scope=react-talend-components -- npm run lint:style 1> output/components.sasslint.txt 2>/dev/null
+	lerna exec --scope=react-talend-forms -- npm run lint:style 1> output/forms.sasslint.txt 2>/dev/null
+fi
--- .travis/after_success_screenshots.sh
+++ .travis/after_success_screenshots.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-nohup http-server .static/ -p 1337 >/dev/null 2>&1 &
-sleep 5
-echo "✓ Start static server"
+if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
+    nohup http-server .static/ -p 1337 >/dev/null 2>&1 &
+    sleep 5
+    echo "✓ Start static server"
 
-if [ "$TRAVIS_BRANCH" != 'master' ]; then
 	#lerna exec --scope=react-talend-components -- yarn run test:slimerjs
 	#echo "✓ Run yarn test:slimerjs script for components"
 
