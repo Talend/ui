5151dadcd2db52f6768bbac36d71952ca50f735b
jsomsanith <jsomsanith@talend.com>
2017-07-12 13:28:57 +0200

    Changes after review

--- scripts/talend-ui-imports/replace-imports.sh
+++ scripts/talend-ui-imports/replace-imports.sh
@@ -5,7 +5,7 @@ APP_DIR=$1
 
 # From version 0.77.0, the packages are published in the @talend organisation.
 # This scripts :
-#   * install jscodeshift globally if needed
+#   * check if jscodeshift is installed and available
 #   * replace js imports in all the js files in src/, /examples, /stories
 #   * replace bootstrap-talend-theme imports in scss
 #   * replace imports in src/**/*js
@@ -18,12 +18,17 @@ print_title () {
   echo "----------------------------------------------------------------------"
 }
 
+print_success () {
+  echo " ✓ done"
+}
+
 # install jscodeshift if needed
 if ! type jscodeshift > /dev/null
 then
     print_title "You will need jscodeshift to run this script, but it is not installed."
-    echo "Let's install it : sudo npm install -g jscodeshift"
-    sudo npm install -g jscodeshift
+    echo "Please install it and run this script again"
+    echo "Execute : sudo npm install -g jscodeshift"
+    exit 1
 fi
 
 # ensure that a project directory is provided
@@ -56,12 +61,6 @@ jscodeshift -t "${BASEDIR}/codemod.js" $APP_DIR/config
 print_title "Execute codemod on $APP_DIR/*.js (for webpack config)"
 jscodeshift -t "${BASEDIR}/codemod.js" $APP_DIR/*.js
 
-# replace bootstrap-talend-theme in all scss
-print_title "Replace bootstrap-talend-theme in all scss"
-find $APP_DIR/src -iregex '.*\.scss' | xargs sed -i '' 's/bootstrap-talend-theme/@talend\/bootstrap-theme/g'
-
-# replace modules in package.json
-print_title "Replace dependencies in package.json"
 REPLACE_THEME_SCHEMA='s/bootstrap-talend-theme/@talend\/bootstrap-theme/g'
 REPLACE_ICON_SCHEMA='s/talend-icons/@talend\/icons/g'
 REPLACE_LOG_SCHEMA='s/talend-log/@talend\/log/g'
@@ -70,4 +69,13 @@ REPLACE_COMPONENTS_SCHEMA='s/react-talend-components/@talend\/react-components/g
 REPLACE_CONTAINERS_SCHEMA='s/react-talend-containers/@talend\/react-containers/g'
 REPLACE_FORMS_SCHEMA='s/react-talend-forms/@talend\/react-forms/g'
 ALL_REPLACE="$REPLACE_THEME_SCHEMA; $REPLACE_ICON_SCHEMA; $REPLACE_LOG_SCHEMA; $REPLACE_CMF_SCHEMA; $REPLACE_COMPONENTS_SCHEMA; $REPLACE_CONTAINERS_SCHEMA; $REPLACE_FORMS_SCHEMA"
-sed -i '' "$ALL_REPLACE" $APP_DIR/package.json
+
+# replace modules names in all scss files
+print_title "Replace imports in all scss"
+find $APP_DIR/src -iregex '.*\.scss' -exec sed -i "$ALL_REPLACE" {} ";"
+print_success
+
+# replace modules names in package.json
+print_title "Replace dependencies in package.json"
+sed -i "$ALL_REPLACE" $APP_DIR/package.json
+print_success
