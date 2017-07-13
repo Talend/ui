6a2f8617dc476b9111e934d9ee89cda08b0df748
frassinier <frassinier@talend.com>
2017-07-13 15:08:07 +0200

    fix: lint issues

--- packages/forms/src/widgets/ListViewWidget/ListViewWidget.test.js
+++ packages/forms/src/widgets/ListViewWidget/ListViewWidget.test.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { mount } from "enzyme";
-import ListViewWidget from "./ListViewWidget";
+import React from 'react';
+import { mount } from 'enzyme';
+import ListViewWidget from './ListViewWidget';
 
 jest.mock(
 	'../../../node_modules/react-virtualized/dist/commonjs/AutoSizer/AutoSizer', () => props =>
