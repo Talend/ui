cece4b89b9309239655dd21997e5f2dd75cbc48d
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-07-21 09:33:27 +0200

    test: add test case for error

--- packages/cmf/__tests__/Inject.test.js
+++ packages/cmf/__tests__/Inject.test.js
@@ -17,4 +17,18 @@ describe('Inject', () => {
 		, { context });
 		expect(wrapper.equals(<MyComponent extra-props />)).toBe(true);
 	});
+	it('should render error if component not found', () => {
+		const MyComponent = jest.fn();
+		MyComponent.displayName = 'MyComponent';
+		const context = { registry: {} };
+		const wrapper = shallow(
+			<Inject component="MyComponent" extra-props />
+		, { context });
+		expect(wrapper.equals(
+			<Inject.NotFoundComponent
+				extra-props
+				error="component not found in the registry: MyComponent"
+			/>
+		)).toBe(true);
+	});
 });
