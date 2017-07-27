2ace2950f371d1d616b38bd0bda1bc71aaf97d57
Jean-Alain Ré <jare@talend.com>
2017-07-27 18:03:19 +0200

    remove locale

--- packages/components/locales/components/list.js
+++
@@ -1,9 +0,0 @@
-import { UI_COMPONENTS_LIST } from '../../src/List/List.component';
-
-export default {
-	en: {
-		[UI_COMPONENTS_LIST]: {
-			TOOLBAR_DISPLAY: 'Display toolbar:',
-		},
-	},
-};
--- packages/components/locales/index.js
+++
@@ -1,6 +0,0 @@
-import translateComponents from './components';
-
-
-export default {
-	...translateComponents,
-};
