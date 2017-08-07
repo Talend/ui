f7fbc056c25d68a55666c26c357c307499befed0
acateland <acateland@talend.com>
2017-08-07 11:18:21 +0200

    update container package to use prop-types package

--- packages/containers/src/Action/Action.component.js
+++ packages/containers/src/Action/Action.component.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { Action as PureAction } from 'react-talend-components';
 import actions from '../actionAPI';
 
