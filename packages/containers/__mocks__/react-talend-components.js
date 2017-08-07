232ce0172438c4b210a07618cbc030cb0cded720
acateland <acateland@talend.com>
2017-08-07 11:25:59 +0200

    updating example and some missing files

--- packages/containers/__mocks__/react-talend-components.js
+++ packages/containers/__mocks__/react-talend-components.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const mock = {
 	AppHeaderBar: props => (<div className="tc-appheaderbar" {...props} />),
