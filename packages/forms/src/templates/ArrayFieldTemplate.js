897f0795c66b3e5c5a68e78221e4ce704b5af6fd
Geoffroy Lorieux <glorieux@talend.com>
2017-08-09 14:30:54 +0200

    feat(forms): Adds support for ArrayField min/max items (#677)

--- packages/forms/src/templates/ArrayFieldTemplate.js
+++ packages/forms/src/templates/ArrayFieldTemplate.js
@@ -4,12 +4,14 @@ import { Icon, IconsProvider } from 'react-talend-components';
 
 import theme from './ArrayFieldTemplate.scss';
 
-function FieldTemplate({ element }) {
+function FieldTemplate({ element, cantDelete }) {
 	return (
 		<div className={theme.arrayElement}>
 			{!element.itemData.isClosed &&
 				<div className={theme.control}>
 					<button
+						name={`btn-delete-element-${element.index}`}
+						disabled={cantDelete}
 						className={theme.delete}
 						onClick={element.onDropIndexClick(element.index)}
 						title="Delete"
@@ -17,6 +19,7 @@ function FieldTemplate({ element }) {
 						<Icon name="talend-trash" />
 					</button>
 					<button
+						name={`btn-move-element-up-${element.index}`}
 						disabled={!element.hasMoveUp}
 						onClick={element.onReorderClick(element.index, element.index - 1)}
 						title="Move Up"
@@ -24,6 +27,7 @@ function FieldTemplate({ element }) {
 						<Icon name="talend-caret-down" transform="flip-vertical" />
 					</button>
 					<button
+						name={`btn-move-element-down-${element.index}`}
 						disabled={!element.hasMoveDown}
 						onClick={element.onReorderClick(element.index, element.index + 1)}
 						title="Move Down"
@@ -40,17 +44,21 @@ function FieldTemplate({ element }) {
 if (process.env.NODE_ENV !== 'production') {
 	FieldTemplate.propTypes = {
 		element: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+		cantDelete: PropTypes.bool.isRequired,
 	};
 }
 
 function ArrayFieldTemplate(props) {
-	const { items, canAdd, onAddClick } = props;
+	const { items, canAdd, onAddClick, minItems, maxItems } = props;
 	return (
 		<div className={theme.ArrayFieldTemplate}>
 			<IconsProvider />
-			{items && items.map(element => <FieldTemplate element={element} />)}
+			{items && items.map(element => <FieldTemplate
+				element={element}
+				cantDelete={items.length <= minItems}
+			/>)}
 			{canAdd &&
-				<button className="btn btn-info" type="button" onClick={onAddClick}>
+				<button className="btn btn-info" type="button" name="btn-new-element" disabled={items.length >= maxItems} onClick={onAddClick}>
 					NEW ELEMENT
 				</button>}
 		</div>
@@ -61,6 +69,8 @@ if (process.env.NODE_ENV !== 'production') {
 		items: PropTypes.arrayOf(PropTypes.object).isRequired,
 		canAdd: PropTypes.func.isRequired,
 		onAddClick: PropTypes.func.isRequired,
+		minItems: PropTypes.number.isRequired,
+		maxItems: PropTypes.number.isRequired,
 	};
 }
 export default ArrayFieldTemplate;
