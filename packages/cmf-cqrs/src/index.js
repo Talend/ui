b49354906b45c58d9c1d81e2e596b2573b8c9672
sromain@talend.com <8hyrmetlI8!t>
2017-08-10 11:42:01 +0200

    Add reducers in exports

--- packages/cmf-cqrs/src/index.js
+++ packages/cmf-cqrs/src/index.js
@@ -1,9 +1,13 @@
 import actions from './actions';
 import ACKDispatcher from './components/ACKDispatcher';
 import middlewares from './middleware';
+import reducers from './reducers';
+import { ackProcessed } from './reducers/ack';
 
 export {
 	actions,
 	ACKDispatcher,
 	middlewares,
+	reducers,
+	ackProcessed,
 };
