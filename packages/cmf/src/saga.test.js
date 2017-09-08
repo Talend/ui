81d46b45d9c1604dcd705755d14791f9bc09fcd0
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-09-08 14:17:14 +0200

    test: add some

--- packages/cmf/src/saga.test.js
+++ packages/cmf/src/saga.test.js
@@ -1,8 +1,35 @@
 import { put, select } from 'redux-saga/effects';
+import registry from './registry';
 import saga from './saga';
 
 describe('saga', () => {
-	it('should putActionCreator call put of a registred actionCreator', () => {
+	it('should putActionCreator call put of a registred actionCreator without context', () => {
+		// given
+		const testAction = { type: 'TEST' };
+		const actionCreator = jest.fn(() => testAction);
+		const reg = registry.getRegistry();
+		reg['actionCreator:myActionCreator'] = actionCreator;
+		const data = { foo: 'bar' };
+		const event = { type: 'click', source: 'MyComponent' };
+
+		// when
+		const gen = saga.putActionCreator('myActionCreator', event, data);
+
+		// then
+		expect(gen.next().value).toEqual(select());
+		const puttedAction = gen.next().value;
+		expect(actionCreator).toHaveBeenCalled();
+		const args = actionCreator.mock.calls[0];
+		expect(args[0]).toBe(event);
+		expect(args[1]).toBe(data);
+		expect(args[2].registry).toBe(reg);
+		expect(typeof args[2].store.getState).toBe('function');
+		expect(args[2].store.getState()).toBeUndefined();
+		expect(puttedAction).toEqual(put(testAction));
+		expect(gen.next).toThrow();
+	});
+	it('should putActionCreator call put of a registred actionCreator or using context', () => {
+		// given
 		const testAction = { type: 'TEST' };
 		const actionCreator = jest.fn(() => testAction);
 		const context = {
@@ -12,10 +39,18 @@ describe('saga', () => {
 		};
 		const data = { foo: 'bar' };
 		const event = { type: 'click', source: 'MyComponent' };
+
+		// when
 		const gen = saga.putActionCreator('myActionCreator', event, data, context);
+
+		// then
 		expect(gen.next().value).toEqual(select());
 		const puttedAction = gen.next().value;
 		expect(actionCreator).toHaveBeenCalled();
+		const args = actionCreator.mock.calls[0];
+		expect(args[0]).toBe(event);
+		expect(args[1]).toBe(data);
+		expect(args[2]).toBe(context);
 		expect(puttedAction).toEqual(put(testAction));
 		expect(gen.next).toThrow();
 	});
