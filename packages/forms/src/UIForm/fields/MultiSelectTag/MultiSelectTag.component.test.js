5fa176dcef3cd77636bc6eb308fc6ac421728e2b
Jimmy Somsanith <JSO.Technologies@gmail.com>
2017-09-08 13:02:29 +0200

    Unit tests

--- packages/forms/src/UIForm/fields/MultiSelectTag/MultiSelectTag.component.test.js
+++ packages/forms/src/UIForm/fields/MultiSelectTag/MultiSelectTag.component.test.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import MultiSelectTag from './MultiSelectTag.component';
 
 describe('MultiSelectTag field', () => {
-	it('should ', () => {
+	const props = {
+		id: 'my-select-tag',
+		isValid: true,
+		errorMessage: 'This is wrong',
+		onChange: jest.fn(),
+		onFinish: jest.fn(),
+		schema: {
+			autoFocus: true,
+			description: 'This is the MultiSelectTag field',
+			disabled: false,
+			placeholder: 'Type here',
+			readOnly: false,
+			restricted: false,
+			title: 'Tags',
+			titleMap: [{ name: 'toto', value: 'titi' }, { name: 'tata', value: 'tutu' }],
+		},
+		value: ['aze', 'tutu'],
+	};
+
+	it('should render MultiSelectTag', () => {
 		// when
+		const wrapper = shallow(<MultiSelectTag {...props} />);
 
 		// then
+		expect(wrapper.node).toMatchSnapshot();
 	});
 });
