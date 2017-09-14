c84bcce7308ca7f468bd50eea57bdb6bce95449d
jsomsanith <jsomsanith@talend.com>
2017-09-14 12:21:28 +0200

    fix(UIForms): create properties nexted objects when mutated

--- packages/forms/src/UIForm/utils/properties.js
+++ packages/forms/src/UIForm/utils/properties.js
@@ -58,12 +58,12 @@ export function convertValue(type, value) {
 
 /**
  * Mutate the properties, setting the value in the path identified by key
- * @param {object} properties The original properties store
+ * @param {object | array} properties The original properties store
  * @param {array} key The key chain (array of strings) to identify the path
  * @param {any} value The value to set
  * @returns {object} The new mutated properties store.
  */
-export function mutateValue(properties, key, value) {
+export function mutateValue(properties = {}, key, value) {
 	if (!key.length) {
 		return value;
 	}
