65c3a9f05c2206407be65d92085a1f5cbb61b524
axel CATELAND <acateland@talend.com>
2017-09-20 16:28:29 +0200

    fix(cmf/saga-router): fix cancel/spawn order (#733)

--- packages/cmf/src/sagaRouter/router.js
+++ packages/cmf/src/sagaRouter/router.js
@@ -30,30 +30,45 @@ function shouldRestartSaga(maybeSaga, match) {
 	return false;
 }
 
+function parseSagaState(routeFragments, sagas, currentLocation, index) {
+	const routeFragment = routeFragments[index];
+	return {
+		routeFragment,
+		match: matchPath(currentLocation.pathname, { path: routeFragment }),
+		maybeSaga: sagas[routeFragment],
+	};
+}
+
 export default function* routerSaga(history, routes) {
 	const sagas = {};
+	const routeFragments = Object.keys(routes);
 	while (true) {
 		yield take('@@router/LOCATION_CHANGE');
+		const shouldStart = [];
 		const currentLocation = history.getCurrentLocation();
-		const routeFragments = Object.keys(routes);
 		for (let index = 0; index < routeFragments.length;) {
-			const routeFragment = routeFragments[index];
-			const match = matchPath(currentLocation.pathname, { path: routeFragment });
-			const maybeSaga = sagas[routeFragment];
-			if (shouldStartSaga(maybeSaga, match)) {
-				sagas[routeFragment] = {
-					saga: yield spawn(routes[routeFragment], match.params),
-					match,
-				};
-			} else if (shouldCancelSaga(maybeSaga, match)) {
+			const { routeFragment, match, maybeSaga } = parseSagaState(
+				routeFragments,
+				sagas,
+				currentLocation,
+				index,
+			);
+			if (shouldCancelSaga(maybeSaga, match)) {
 				yield cancel(maybeSaga.saga);
 			} else if (shouldRestartSaga(maybeSaga, match)) {
 				yield cancel(maybeSaga.saga);
-				sagas[routeFragment] = {
-					saga: yield spawn(routes[routeFragment], match.params),
-					match,
-				};
+				shouldStart.push({ routeFragment, match });
+			} else if (shouldStartSaga(maybeSaga, match)) {
+				shouldStart.push({ routeFragment, match });
 			}
+			index += 1;
+		}
+		for (let index = 0; index < shouldStart.length;) {
+			const { routeFragment, match } = shouldStart[index];
+			sagas[routeFragment] = {
+				saga: yield spawn(routes[routeFragment], match.params),
+				match,
+			};
 			index += 1;
 		}
 	}
