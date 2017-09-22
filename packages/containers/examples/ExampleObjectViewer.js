aa9719e031cb19a737a4b18e983a6e206e05e765
sgendre <sgendre@talend.com>
2017-09-22 11:59:38 +0200

    Review : use of functionnal way to cmf setState

--- packages/containers/examples/ExampleObjectViewer.js
+++ packages/containers/examples/ExampleObjectViewer.js
@@ -2,21 +2,183 @@ import React from 'react';
 import { IconsProvider } from '@talend/react-components';
 import { ObjectViewer } from '../src';
 
-const veryLongDatasetLabel = 'Dataset of something that I cant\'t imagine; Dataset of something that I cant\'t imagine; Dataset of something that I cant\'t imagine';
-const veryLongCafeName = 'Betty\'s Cafe witha  veryyyyyyy veryyyyyyyyyy looong name';
+const veryLongDatasetLabel =
+	"Dataset of something that I cant't imagine; Dataset of something that I cant't imagine; Dataset of something that I cant't imagine";
+const veryLongCafeName = "Betty's Cafe witha  veryyyyyyy veryyyyyyyyyy looong name";
 const data = [
 	{
-		business_id: 0, name: veryLongCafeName, category: 'Club', rating: 4, num_of_reviews: 2647, attributes: { good_for: { dessert: false, kids: true, drinks: false, breakfast: false, lunch: false, dinner: true }, parking: { lot: false, valet: false, garage: false }, take_reservations: true, noise_level: 'quiet' }, location: { zipType: 'STANDARD', zip: 72132, decommisioned: false, taxReturnsFiled: 1400, location: 'NA- US - AR - REDFIELD', estimatedPopulation: 2653, locationType: 'PRIMARY', totalWages: 56190766, state: 'AR', longitude: -92.18, latitude: 34.44, city: 'REDFIELD' }
+		business_id: 0,
+		name: veryLongCafeName,
+		category: 'Club',
+		rating: 4,
+		num_of_reviews: 2647,
+		attributes: {
+			good_for: {
+				dessert: false,
+				kids: true,
+				drinks: false,
+				breakfast: false,
+				lunch: false,
+				dinner: true,
+			},
+			parking: { lot: false, valet: false, garage: false },
+			take_reservations: true,
+			noise_level: 'quiet',
+		},
+		location: {
+			zipType: 'STANDARD',
+			zip: 72132,
+			decommisioned: false,
+			taxReturnsFiled: 1400,
+			location: 'NA- US - AR - REDFIELD',
+			estimatedPopulation: 2653,
+			locationType: 'PRIMARY',
+			totalWages: 56190766,
+			state: 'AR',
+			longitude: -92.18,
+			latitude: 34.44,
+			city: 'REDFIELD',
+		},
 	},
 	{
-		business_id: 1, name: `${veryLongCafeName} ${veryLongCafeName} ${veryLongCafeName} ${veryLongCafeName}`, category: 'Club', rating: 4, num_of_reviews: 2647, attributes: { good_for: { dessert: false, kids: true, drinks: false, breakfast: false, lunch: false, dinner: true }, parking: { lot: false, valet: false, garage: false }, take_reservations: true, noise_level: 'quiet' }, location: { zipType: 'STANDARD', zip: 72132, decommisioned: false, taxReturnsFiled: 1400, location: 'NA- US - AR - REDFIELD', estimatedPopulation: 2653, locationType: 'PRIMARY', totalWages: 56190766, state: 'AR', longitude: -92.18, latitude: 34.44, city: 'REDFIELD' }
+		business_id: 1,
+		name: `${veryLongCafeName} ${veryLongCafeName} ${veryLongCafeName} ${veryLongCafeName}`,
+		category: 'Club',
+		rating: 4,
+		num_of_reviews: 2647,
+		attributes: {
+			good_for: {
+				dessert: false,
+				kids: true,
+				drinks: false,
+				breakfast: false,
+				lunch: false,
+				dinner: true,
+			},
+			parking: { lot: false, valet: false, garage: false },
+			take_reservations: true,
+			noise_level: 'quiet',
+		},
+		location: {
+			zipType: 'STANDARD',
+			zip: 72132,
+			decommisioned: false,
+			taxReturnsFiled: 1400,
+			location: 'NA- US - AR - REDFIELD',
+			estimatedPopulation: 2653,
+			locationType: 'PRIMARY',
+			totalWages: 56190766,
+			state: 'AR',
+			longitude: -92.18,
+			latitude: 34.44,
+			city: 'REDFIELD',
+		},
 	},
-	{ business_id: 2, name: 'Nancy\'s Club', category: 'Club', rating: 2, num_of_reviews: 3779, attributes: { good_for: { dessert: false, kids: true, drinks: false, breakfast: false, lunch: false, dinner: true }, parking: { lot: true, valet: true, garage: false }, take_reservations: true, noise_level: 'average' }, location: { zipType: 'PO BOX', zip: 88221, decommisioned: false, taxReturnsFiled: 967, location: 'NA-US - NM - CARLSBAD', estimatedPopulation: 1638, locationType: 'PRIMARY', totalWages: 37060120, state: 'NM', longitude: -104.23, latitude: 32.40, city: 'CARLSBAD' } },
-	{ business_id: 3, name: 'Cecelia\'s Club', category: 'Cafe', rating: 4, num_of_reviews: 16547, attributes: { good_for: { dessert: true, kids: false, drinks: false, breakfast: true, lunch: false, dinner: false }, parking: { lot: true, valet: true, garage: false }, take_reservations: false, noise_level: 'noisy' }, location: { zipType: 'PO BOX', zip: 47445, decommisioned: false, taxReturnsFiled: 123, location: 'NA-US - IN - MIDLAND', estimatedPopulation: 123, locationType: 'PRIMARY', totalWages: 456, state: 'IN', longitude: -87.16, latitude: 39.09, city: 'MIDLAND' } },
-	{ business_id: 4, name: 'Gordon\'s Bar', category: 'Cafe', rating: 1, num_of_reviews: 152, attributes: { good_for: { dessert: false, kids: false, drinks: true, breakfast: true, lunch: true, dinner: true }, parking: { lot: true, valet: false, garage: true }, take_reservations: true, noise_level: 'noisy' }, location: { zipType: 'STANDARD', zip: 65638, decommisioned: false, taxReturnsFiled: 123, location: 'NA- US - MO - DRURY', estimatedPopulation: 123, locationType: 'PRIMARY', totalWages: 456, state: 'MO', longitude: -92.32, latitude: 36.92, city: 'DRURY' } }
+	{
+		business_id: 2,
+		name: "Nancy's Club",
+		category: 'Club',
+		rating: 2,
+		num_of_reviews: 3779,
+		attributes: {
+			good_for: {
+				dessert: false,
+				kids: true,
+				drinks: false,
+				breakfast: false,
+				lunch: false,
+				dinner: true,
+			},
+			parking: { lot: true, valet: true, garage: false },
+			take_reservations: true,
+			noise_level: 'average',
+		},
+		location: {
+			zipType: 'PO BOX',
+			zip: 88221,
+			decommisioned: false,
+			taxReturnsFiled: 967,
+			location: 'NA-US - NM - CARLSBAD',
+			estimatedPopulation: 1638,
+			locationType: 'PRIMARY',
+			totalWages: 37060120,
+			state: 'NM',
+			longitude: -104.23,
+			latitude: 32.4,
+			city: 'CARLSBAD',
+		},
+	},
+	{
+		business_id: 3,
+		name: "Cecelia's Club",
+		category: 'Cafe',
+		rating: 4,
+		num_of_reviews: 16547,
+		attributes: {
+			good_for: {
+				dessert: true,
+				kids: false,
+				drinks: false,
+				breakfast: true,
+				lunch: false,
+				dinner: false,
+			},
+			parking: { lot: true, valet: true, garage: false },
+			take_reservations: false,
+			noise_level: 'noisy',
+		},
+		location: {
+			zipType: 'PO BOX',
+			zip: 47445,
+			decommisioned: false,
+			taxReturnsFiled: 123,
+			location: 'NA-US - IN - MIDLAND',
+			estimatedPopulation: 123,
+			locationType: 'PRIMARY',
+			totalWages: 456,
+			state: 'IN',
+			longitude: -87.16,
+			latitude: 39.09,
+			city: 'MIDLAND',
+		},
+	},
+	{
+		business_id: 4,
+		name: "Gordon's Bar",
+		category: 'Cafe',
+		rating: 1,
+		num_of_reviews: 152,
+		attributes: {
+			good_for: {
+				dessert: false,
+				kids: false,
+				drinks: true,
+				breakfast: true,
+				lunch: true,
+				dinner: true,
+			},
+			parking: { lot: true, valet: false, garage: true },
+			take_reservations: true,
+			noise_level: 'noisy',
+		},
+		location: {
+			zipType: 'STANDARD',
+			zip: 65638,
+			decommisioned: false,
+			taxReturnsFiled: 123,
+			location: 'NA- US - MO - DRURY',
+			estimatedPopulation: 123,
+			locationType: 'PRIMARY',
+			totalWages: 456,
+			state: 'MO',
+			longitude: -92.32,
+			latitude: 36.92,
+			city: 'DRURY',
+		},
+	},
 ];
 
-let selectedJsonpath = "$[0][\'name\']";
+let selectedJsonpath = "$[0]['name']";
 const kTrue = true;
 
 const ExampleObjectViewer = {
@@ -29,7 +191,7 @@ const ExampleObjectViewer = {
 	'JsonLike with hightlight': () => (
 		<div>
 			<IconsProvider />
-			<ObjectViewer data={data} />
+			<ObjectViewer data={data} selectedJsonpath={selectedJsonpath} />
 		</div>
 	),
 	'JsonLike with types': () => (
