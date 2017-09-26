200e7b43be1b23b0d631b6d4987dff3b537d074c
nicomaligne <nmaligne@gmail.com>
2017-09-26 17:50:15 +0200

    changes after pr comment

--- packages/cmf/src/sagaRouter/documentTitle.js
+++ packages/cmf/src/sagaRouter/documentTitle.js
@@ -2,18 +2,26 @@ import { take } from 'redux-saga/effects';
 
 import matchPath from './matchPath';
 
+/**
+ * Format the current full path
+ * @param {string} path current path name (ex: foo)
+ * @param {string} parentPath parent path name (ex: /foo)
+ */
 export function formatPath(path, parentPath) {
-	let fPath = '';
-	if (parentPath && parentPath === '/') {
-		fPath = `${parentPath}${path}`;
-	} else if (parentPath) {
-		fPath = `${parentPath}/${path}`;
-	} else {
-		fPath = path;
+	let fPath = path;
+	if (parentPath) {
+		fPath = parentPath.endsWith('/') ? `${parentPath}${path}` : `${parentPath}/${path}`;
 	}
+	// Convert optional url parameters to React Router V2 --> V4 (:param) --> :param?
 	return fPath.replace(/[(]/g, '').replace(/[)]/g, '?');
 }
 
+/**
+ * Build a map from the settings routes object recursively
+ * @param {object} routes
+ * @param {array} mapRoutes
+ * @param {string} parentPath
+ */
 export function buildMapFromRoutes(routes, mapRoutes, parentPath) {
 	const path = formatPath(routes.path, parentPath);
 	if (routes.documentTitle) {
@@ -28,6 +36,14 @@ export function buildMapFromRoutes(routes, mapRoutes, parentPath) {
 	return mapRoutes;
 }
 
+/**
+ * Iterate over the routes map to find a match with the given location.
+ * The value map return the correct document title.
+ * If none is found we return the default document title.
+ * @param {array} mapRoutes
+ * @param {string} location
+ * @param {string} defaultDocTitle
+ */
 export function getTitleFromRoutes(mapRoutes, location, defaultDocTitle) {
 	let title = defaultDocTitle;
 	mapRoutes.forEach((value, key) => {
@@ -37,12 +53,22 @@ export function getTitleFromRoutes(mapRoutes, location, defaultDocTitle) {
 	return title;
 }
 
+/**
+ * If not null|undefined we assign a new title to the document.
+ * @param {string} title
+ */
 export function assignDocTitle(title) {
 	if (title) {
 		document.title = title;
 	}
 }
 
+/**
+ * A saga which listen to the REQUEST_SETTINGS_OK.
+ * We use the routes of the settings to build a map [route: documentTitle].
+ * We use the root path '/' to get the default document title and we assign it.
+ * When the location changes @@router/LOCATION_CHANGE we update the document title.
+ */
 export default function* changeDocumentTitle() {
 	const { settings } = yield take('REACT_CMF.REQUEST_SETTINGS_OK');
 	const mapRoutes = buildMapFromRoutes(settings.routes, new Map());
--- packages/cmf/src/sagaRouter/index.js
+++ packages/cmf/src/sagaRouter/index.js
@@ -1,8 +1,7 @@
-import { all, fork, call } from 'redux-saga/effects';
+import { all, fork } from 'redux-saga/effects';
 import { browserHistory as history } from 'react-router';
 import routerSaga from './router';
-import changeDocumentTitle from './documentTitle';
 
 export default function* initSagaRouter(routes) {
-	yield all([fork(routerSaga, history, routes), call(changeDocumentTitle)]);
+	yield all([fork(routerSaga, history, routes)]);
 }
