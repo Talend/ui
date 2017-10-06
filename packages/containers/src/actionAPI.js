830e3b9b560dde679d3ab61264f016ac9a271700
nicomaligne <nmaligne@gmail.com>
2017-10-06 09:10:07 +0200

    wip recursive actionApi

--- packages/containers/src/actionAPI.js
+++ packages/containers/src/actionAPI.js
@@ -10,13 +10,58 @@ import { api } from '@talend/react-cmf';
  * * labelExpression
  */
 function evalExpressions(action, context, payload = {}) {
-	const newAction = api.expression.getProps(action, ['active', 'available', 'disabled', 'inProgress'], context, payload);
+	const newAction = api.expression.getProps(
+		action,
+		['active', 'available', 'disabled', 'inProgress'],
+		context,
+		payload,
+	);
 	if (action.labelExpression) {
 		delete newAction.labelExpression;
 		newAction.label = api.expression.call(action.labelExpression, context, newAction);
 	}
 	return newAction;
 }
+
+function getActionsPropsRecursive(context, ids, model, props) {
+	const infos = ids.map(id => {
+		if (typeof id === 'string') {
+			return api.action.getActionInfo(context, id);
+		}
+		return id;
+	});
+	const dispatch = context.store.dispatch;
+	const getActionObj = api.action.getActionObject;
+	const actionsProps = infos.map(info =>
+		Object.assign(
+			{
+				onClick(event, data) {
+					if (info.actionCreator) {
+						dispatch(getActionObj(context, info.id, event, data));
+					} else {
+						dispatch(Object.assign({ model }, info.payload));
+					}
+				},
+			},
+			evalExpressions(info, context, { model }),
+		),
+	);
+	actionsProps.forEach((action, index) => {
+		if (action.items && Array.isArray(action.items)) {
+			const ret = action.items.map(item => {
+				let id = [];
+				if (typeof item.id === 'string') {
+					id = [item.id];
+				}
+				return getActionsPropsRecursive(context, id, model, action.items);
+			});
+			const test = ret.reduce((a, b) => [...a, ...b], []);
+			actionsProps[index].items = test;
+		}
+	});
+	return actionsProps;
+}
+// }
 
 export function getActionsProps(context, ids, model) {
 	if (!ids) {
@@ -27,33 +72,11 @@ export function getActionsProps(context, ids, model) {
 	if (onlyOne) {
 		tmpIds = [ids];
 	}
-
-	const infos = tmpIds.map((id) => {
-		if (typeof id === 'string') {
-			return api.action.getActionInfo(context, id);
-		}
-		return id;
-	});
-
-	const props = infos.map(info => Object.assign({
-		onClick(event, data) {
-			if (info.actionCreator) {
-				context.store.dispatch(
-					api.action.getActionObject(context, info.id, event, data)
-				);
-			} else {
-				context.store.dispatch(Object.assign({
-					model,
-				}, info.payload));
-			}
-		},
-	}, evalExpressions(info, context, { model })));
-
+	const actionsProps = getActionsPropsRecursive(context, tmpIds, model, []);
 	if (onlyOne) {
-		return props[0];
+		return actionsProps[0];
 	}
-
-	return props;
+	return actionsProps;
 }
 
 export default {
