27cd5530bbf00e00da935d2be7c13f1c6f23207f
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-10-19 18:15:32 +0200

    test: add last one

--- packages/cmf/__tests__/component.test.js
+++ packages/cmf/__tests__/component.test.js
@@ -23,4 +23,25 @@ describe('component', () => {
 		expect(context.registry['expression:baz']).toBe(foo.expressions.baz);
 		expect(context.registry['actionCreator:bar']).toBe(foo.actions.bar);
 	});
+	it('get should return component registred', () => {
+		function foo() {}
+		const context = {
+			registry: {
+				'_.route.component:foo': foo,
+			},
+		};
+		expect(component.get('foo', context)).toBe(foo);
+	});
+	it('registerMany should register an index of component', () => {
+		const context = { registry: {} };
+		const components = {
+			foo: function foo() {},
+			bar: function bar() {},
+			baz: function baz() {},
+		};
+		component.registerMany(components, context);
+		expect(context.registry['_.route.component:foo']).toBe(components.foo);
+		expect(context.registry['_.route.component:bar']).toBe(components.bar);
+		expect(context.registry['_.route.component:baz']).toBe(components.baz);
+	});
 });
