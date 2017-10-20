3a09bccc8508988ff1d0a62b3a4377f69efeb69e
sgendre <31214611+sgendre@users.noreply.github.com>
2017-10-20 16:01:40 +0200

    feat(saga): Saga package initialization (#792)
    * create package
    * refactor saga pendingMaybeNeeded from data-streams to the new package

--- .travis/after_success_lint.sh
+++ .travis/after_success_lint.sh
@@ -3,6 +3,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
 	echo "Linting JavaScript files"
+	lerna exec --scope=@talend/react-sagas -- npm run lint:es 1> output/sagas.eslint.txt
 	lerna exec --scope=@talend/react-cmf -- npm run lint:es 1> output/cmf.eslint.txt 2>/dev/null
 	lerna exec --scope=@talend/react-components -- npm run lint:es 1> output/components.eslint.txt 2>/dev/null
 	lerna exec --scope=@talend/react-containers -- npm run lint:es 1> output/containers.eslint.txt 2>/dev/null
