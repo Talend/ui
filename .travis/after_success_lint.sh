6bafd386c71436cc00b271383b5dc7f15af3c5c4
sgendre <sgendre@talend.com>
2017-10-20 09:54:55 +0200

    Move sagas linter execution to first position

--- .travis/after_success_lint.sh
+++ .travis/after_success_lint.sh
@@ -3,8 +3,8 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
 	echo "Linting JavaScript files"
+	lerna exec --scope=@talend/react-sagas -- npm run lint:es 1> output/sagas.eslint.txt
 	lerna exec --scope=@talend/react-cmf -- npm run lint:es 1> output/cmf.eslint.txt 2>/dev/null
-	lerna exec --scope=@talend/react-sagas -- npm run lint:es 1> output/sagas.eslint.txt 2>/dev/null
 	lerna exec --scope=@talend/react-components -- npm run lint:es 1> output/components.eslint.txt 2>/dev/null
 	lerna exec --scope=@talend/react-containers -- npm run lint:es 1> output/containers.eslint.txt 2>/dev/null
 	lerna exec --scope=@talend/react-forms -- npm run lint:es 1> output/forms.eslint.txt 2>/dev/null
@@ -12,6 +12,6 @@ if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
 
 	echo "Linting Sass files"
 	lerna exec --scope=@talend/bootstrap-theme -- npm run lint:style 1> output/theme.sasslint.txt 2>/dev/null
-	lerna exec --scope=@talend/react-components -- npm run lint:style 1> output/components.sasslint.txt 2>/dev/null
+	lerna exec --scope=@talend/react-components -- np	m run lint:style 1> output/components.sasslint.txt 2>/dev/null
 	lerna exec --scope=@talend/react-forms -- npm run lint:style 1> output/forms.sasslint.txt 2>/dev/null
 fi
