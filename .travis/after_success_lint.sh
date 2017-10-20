2e2333a81bd00edf47871dd15c7dc90df06ccacd
sgendre <sgendre@talend.com>
2017-10-20 09:57:07 +0200

    Fix tab typo

--- .travis/after_success_lint.sh
+++ .travis/after_success_lint.sh
@@ -12,6 +12,6 @@ if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
 
 	echo "Linting Sass files"
 	lerna exec --scope=@talend/bootstrap-theme -- npm run lint:style 1> output/theme.sasslint.txt 2>/dev/null
-	lerna exec --scope=@talend/react-components -- np	m run lint:style 1> output/components.sasslint.txt 2>/dev/null
+	lerna exec --scope=@talend/react-components -- npm run lint:style 1> output/components.sasslint.txt 2>/dev/null
 	lerna exec --scope=@talend/react-forms -- npm run lint:style 1> output/forms.sasslint.txt 2>/dev/null
 fi
