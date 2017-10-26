cf16cd5c5095c9a3a6574f3a93f2e5da6168908f
nicomaligne <nmaligne@gmail.com>
2017-10-26 11:29:56 +0200

    removing data from model actionProps

--- packages/containers/src/DeleteResource/DeleteResource.container.js
+++ packages/containers/src/DeleteResource/DeleteResource.container.js
@@ -65,7 +65,6 @@ export default class DeleteResource extends React.Component {
 	 */
 	getActions(resourceInfo) {
 		const actions = getActionsProps(this.context, this.props['form-actions'], {
-			props: this.props,
 			resourceInfo,
 		});
 		return {
