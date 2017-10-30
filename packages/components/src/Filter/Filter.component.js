86e8901c8eb18d91edd8f5102278830e43019b08
nicomaligne <nmaligne@gmail.com>
2017-10-30 09:44:17 +0100

    bootstrap filter as stand alone comp

---
+++ packages/components/src/Filter/Filter.component.js
@@ -0,0 +1,181 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+import DebounceInput from 'react-debounce-input';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import get from 'lodash/get';
+import keycode from 'keycode';
+
+import { getDefaultTranslate } from '../../../translate';
+import { Action } from '../../../Actions';
+import Icon from '../../../Icon';
+import theme from './Filter.scss';
+
+function onKeyDown(event, escAction, enterAction) {
+	switch (event.keyCode) {
+		case keycode.codes.enter:
+			if (enterAction) {
+				enterAction(event);
+			}
+			break;
+		case keycode.codes.esc:
+			if (escAction) {
+				escAction(event);
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+function FilterInput(props) {
+	const {
+		id,
+		debounceMinLength,
+		debounceTimeout,
+		onBlur,
+		onFocus,
+		onFilter,
+		onToggle,
+		placeholder,
+		value,
+	} = props;
+
+	const inputProps = {
+		id,
+		name: 'search',
+		type: 'search',
+		value,
+		placeholder,
+		className: theme.search,
+		'aria-label': 'Filter',
+		onBlur: onBlur && (event => onBlur(event, event.target.value)),
+		onFocus: onFocus && (event => onFocus(event, event.target.value)),
+		onChange: event => onFilter(event, event.target.value),
+		onKeyDown: event => onKeyDown(event, onToggle, onBlur),
+		autoFocus: true,
+	};
+
+	if (debounceMinLength || debounceTimeout) {
+		return (<DebounceInput
+			{...inputProps}
+			element={FormControl}
+			minLength={debounceMinLength}
+			debounceTimeout={debounceTimeout}
+		/>);
+	}
+	return (<FormControl {...inputProps} />);
+}
+
+FilterInput.propTypes = {
+	id: PropTypes.string,
+	debounceMinLength: PropTypes.number,
+	debounceTimeout: PropTypes.number,
+	onBlur: PropTypes.func,
+	onFocus: PropTypes.func,
+	onFilter: PropTypes.func.isRequired,
+	onToggle: PropTypes.func,
+	placeholder: PropTypes.string,
+	value: PropTypes.string,
+};
+
+/**
+ * @param {object} props react props
+ * @example
+ <Filter id="my-filter" docked="false" onFilter="filter()"></Filter>
+ */
+function Filter(props) {
+	const {
+		id,
+		debounceMinLength,
+		debounceTimeout,
+		docked,
+		highlight,
+		onBlur,
+		onFocus,
+		onFilter,
+		onToggle,
+		placeholder,
+		value,
+		t,
+	} = props;
+	if (docked) {
+		return (
+			<Action
+				id={id}
+				className="navbar-right"
+				onClick={onToggle}
+				label={t('LIST_FILTER_TOGGLE', { defaultValue: 'Toggle filter' })}
+				hideLabel
+				icon="talend-search"
+				bsStyle="link"
+			/>
+		);
+	}
+
+	function onSubmit(event) {
+		event.preventDefault();
+		return onFilter(event, get(event, 'target.search.value'));
+	}
+
+	const classes = classNames(
+		'navbar-form',
+		'navbar-right',
+		theme.filter,
+		{ [theme.highlight]: highlight },
+	);
+
+	return (
+		<form
+			className={classes}
+			role="search"
+			onSubmit={onSubmit}
+		>
+			<Icon name="talend-search" className={theme['search-icon']} />
+			<div className="form-group">
+				<FilterInput
+					id={id && `${id}-input`}
+					debounceMinLength={debounceMinLength}
+					debounceTimeout={debounceTimeout}
+					onBlur={onBlur}
+					onFocus={onFocus}
+					onFilter={onFilter}
+					onToggle={onToggle}
+					placeholder={placeholder}
+					value={value}
+				/>
+				<Action
+					id={id && `${id}-cross-icon`}
+					bsStyle="link"
+					icon="talend-cross"
+					label={t('LIST_FILTER_REMOVE', { defaultValue: 'Remove filter' })}
+					hideLabel
+					onClick={onToggle}
+				/>
+			</div>
+		</form>
+	);
+}
+
+Filter.propTypes = {
+	id: PropTypes.string,
+	debounceMinLength: PropTypes.number,
+	debounceTimeout: PropTypes.number,
+	docked: PropTypes.bool,
+	onBlur: PropTypes.func,
+	onFocus: PropTypes.func,
+	onFilter: PropTypes.func.isRequired,
+	onToggle: PropTypes.func.isRequired,
+	highlight: PropTypes.bool,
+	placeholder: PropTypes.string,
+	value: PropTypes.string,
+	t: PropTypes.func.isRequired,
+};
+
+Filter.defaultProps = {
+	docked: true,
+	placeholder: 'Filter',
+	t: getDefaultTranslate,
+};
+
+export default Filter;
---
+++ packages/containers/src/Filter/Filter.test.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Filter from './Filter.component';
+
+jest.useFakeTimers();
+
+let defaultProps = {};
+beforeEach(() => {
+	defaultProps = {
+		docked: false,
+		onBlur: jest.fn(),
+		onFilter: jest.fn(),
+		onToggle: jest.fn(),
+		ref: 'inputFilter',
+	};
+});
+
+describe('Filter', () => {
+	it('should call onToggle on search icon click', () => {
+		// given
+		const props = {
+			...defaultProps,
+			docked: true,
+		};
+		const filterInstance = mount(<Filter {...props} />);
+
+		// when
+		filterInstance.find('button').simulate('click');
+
+		// then
+		expect(props.onToggle).toBeCalled();
+	});
+
+	it('should call onToggle on cross icon click', () => {
+		// given
+		const props = {
+			...defaultProps,
+		};
+		const filterInstance = mount(<Filter {...props} />);
+
+		// when
+		filterInstance.find('button').simulate('click');
+
+		// then
+		expect(props.onToggle).toBeCalled();
+	});
+
+	it('should call onFilter when input value change', () => {
+		// given
+		const props = { ...defaultProps };
+		const filterInstance = mount(<Filter {...props} />);
+
+		// when
+		filterInstance.find('input').simulate('change');
+
+		// then
+		expect(props.onFilter).toBeCalled();
+	});
+
+	it('should call onBlur on input blur', () => {
+		// given
+		const props = { ...defaultProps };
+		const filterInstance = mount(<Filter {...props} />);
+
+		// when
+		filterInstance.find('input').simulate('blur');
+
+		// then
+		expect(props.onBlur).toBeCalled();
+	});
+
+	it('should call onToggle on ESC keydown', () => {
+		// given
+		const props = { ...defaultProps };
+		const filterInstance = mount(<Filter {...props} />);
+
+		// when
+		filterInstance.find('input').simulate('keydown', { keyCode: 27 });
+
+		// then
+		expect(props.onToggle).toBeCalled();
+	});
+
+	it('should call onToggle on ENTER keydown', () => {
+		// given
+		const props = { ...defaultProps };
+		const filterInstance = mount(<Filter {...props} />);
+
+		// when
+		filterInstance.find('input').simulate('keydown', { keyCode: 13 });
+
+		// then
+		expect(props.onBlur).toBeCalled();
+	});
+
+	it('should call onFilter with debounce options', () => {
+		// given
+		const initialTimeoutCount = setTimeout.mock.calls.length;
+		const debounceTimeout = 300;
+		const props = {
+			...defaultProps,
+			debounceTimeout,
+		};
+		const filterInstance = mount(<Filter {...props} />);
+
+		// when
+		filterInstance.find('input').simulate('change');
+
+		// then
+		expect(setTimeout.mock.calls.length).toBe(initialTimeoutCount + 1);
+		expect(setTimeout.mock.calls[0][1]).toBe(debounceTimeout);
+	});
+
+	it('should call onFilter with debounceMinLength options', () => {
+		// given
+		const initialTimeoutCount = setTimeout.mock.calls.length;
+		const debounceTimeout = 300;
+		const props = {
+			...defaultProps,
+			debounceTimeout,
+			debounceMinLength: 3,
+		};
+		const underMinLengthEvent = { target: { value: '2' } };
+		const overMinLengthEvent = { target: { value: 'toto' } };
+
+		const filterInstance = mount(<Filter {...props} />);
+
+		// when
+		filterInstance.find('input').simulate('change', underMinLengthEvent);
+
+		// then
+		expect(setTimeout.mock.calls.length).toBe(initialTimeoutCount);
+
+		// when
+		filterInstance.find('input').simulate('change', overMinLengthEvent);
+
+		// then
+		expect(setTimeout.mock.calls.length).toBe(initialTimeoutCount + 1);
+		expect(setTimeout.mock.calls[0][1]).toBe(debounceTimeout);
+	});
+});
