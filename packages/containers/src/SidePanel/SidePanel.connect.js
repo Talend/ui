40419ab783298a4ad627f59a71128717fe588e0f
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-11-02 15:16:32 +0100

    chore: refactor sidepanel to use mapstate to props

--- packages/containers/src/SidePanel/SidePanel.connect.js
+++ packages/containers/src/SidePanel/SidePanel.connect.js
@@ -1,7 +1,39 @@
-import { cmfConnect } from '@talend/react-cmf';
+import get from 'lodash/get';
+import { api, cmfConnect } from '@talend/react-cmf';
+import { withRouter } from 'react-router';
 import Container, { DEFAULT_STATE } from './SidePanel.container';
 
-export default cmfConnect({
+function mapStateToProps(state, ownProps) {
+	const props = {};
+	if (ownProps.actionIds) {
+		props.actions = ownProps.actionIds.map((id) => {
+			const info = api.action.getActionInfo({
+				registry: api.registry.getRegistry(),
+				store: {
+					getState: () => state,
+				},
+			}, id);
+			let route = get(info, 'payload.cmf.routerReplace');
+			if (!route) {
+				route = get(info, 'payload.cmf.routerPush');
+			}
+			if (!route) {
+				route = get(info, 'href');
+			}
+			if (route) {
+				const currentRoute = ownProps.location.pathname;
+				if (currentRoute.indexOf(route) !== -1) {
+					info.active = true;
+				}
+			}
+			return info;
+		});
+	}
+	return props;
+}
+
+export default withRouter(cmfConnect({
 	defaultState: DEFAULT_STATE,
 	keepComponentState: true,
-})(Container);
+	mapStateToProps,
+})(Container));
--- packages/containers/src/SidePanel/SidePanel.container.js
+++ packages/containers/src/SidePanel/SidePanel.container.js
@@ -1,35 +1,12 @@
-import get from 'lodash/get';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { SidePanel as Component } from '@talend/react-components';
 import { componentState } from '@talend/react-cmf';
 import { Map } from 'immutable';
 
-import { getActionsProps } from '../actionAPI';
-
 export const DEFAULT_STATE = new Map({
 	docked: false,
 });
-
-export function getActions(actionIds, context) {
-	return actionIds.map((id) => {
-		const info = getActionsProps(context, id);
-		let route = get(info, 'payload.cmf.routerReplace');
-		if (!route) {
-			route = get(info, 'payload.cmf.routerPush');
-		}
-		if (!route) {
-			route = get(info, 'href');
-		}
-		if (route) {
-			const currentRoute = context.router.location.pathname;
-			if (currentRoute.indexOf(route) !== -1) {
-				info.active = true;
-			}
-		}
-		return info;
-	});
-}
 
 /**
  * Checkout the {@link http://talend.surge.sh/containers/?selectedKind=SidePanelExample&selectedStory=Default|examples}
@@ -38,12 +15,6 @@ export function getActions(actionIds, context) {
 class SidePanel extends React.Component {
 	static displayName = 'Container(SidePanel)';
 	static propTypes = {
-		actionIds: PropTypes.arrayOf(
-			PropTypes.oneOfType([
-				PropTypes.string,
-				PropTypes.object,
-			]),
-		),
 		...componentState.propTypes,
 	};
 	static contextTypes = {
@@ -63,10 +34,8 @@ class SidePanel extends React.Component {
 	}
 
 	render() {
-		const { actionIds = [], state = DEFAULT_STATE, ...rest } = this.props;
-		const actions = getActions(actionIds, this.context);
+		const { state = DEFAULT_STATE, ...rest } = this.props;
 		const props = Object.assign({
-			actions,
 			docked: state.get('docked'),
 			onToggleDock: this.onToggleDock,
 		});
