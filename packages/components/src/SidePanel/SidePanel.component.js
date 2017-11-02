Talend/ui

commit 811bdd07ed1aa107b5fd2055627a431ec1fb5f11

feat(SidePanel): i18n (#818)
* feat(SidePanel): be i18n ready

* fix(SidePanel): doc

* fix(SidePanel): storybook

* style(SidePanel): prettify

* fix: prettier

* style(SidePanel): prettify

packages/components/src/SidePanel/SidePanel.component.js
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
+import { translate } from 'react-i18next';
+
+import I18N_DOMAIN_COMPONENTS from '../constants';
+import { DEFAULT_I18N } from '../translate';
 
 import Action from '../Actions/Action';
 
@@ -25,16 +29,7 @@ import theme from './SidePanel.scss';
  />
  *
  */
-function SidePanel({
-	id,
-	selected,
-	onSelect,
-	actions = [],
-	docked,
-	onToggleDock,
-	expandTitle = 'Expand',
-	collapseTitle = 'Collapse',
-}) {
+function SidePanel({ id, selected, onSelect, actions = [], docked, onToggleDock, t }) {
 	const dockedCSS = { [theme.docked]: docked };
 	const navCSS = classNames(theme['tc-side-panel'], dockedCSS, 'tc-side-panel');
 	const listCSS = classNames(
@@ -42,17 +37,19 @@ function SidePanel({
 		'tc-side-panel-list',
 		theme['action-list'],
 	);
-	const isActionSelected = action => {
+	const isActionSelected = (action) => {
 		if (selected) {
 			return action === selected;
 		}
 		return action.active;
 	};
 
-	const toggleButtonTitle = docked ? expandTitle : collapseTitle;
+	const expandLabel = t('SIDEPANEL_EXPAND', { defaultValue: 'Expand' });
+	const collapseTitle = t('SIDEPANEL_COLLAPSE', { defaultValue: 'Collapse' });
+	const toggleButtonTitle = docked ? expandLabel : collapseTitle;
 
 	return (
-		<nav className={navCSS}>
+		<nav className={navCSS} role="navigation">
 			<ul className={listCSS}>
 				<li className={theme['toggle-btn']} title={toggleButtonTitle}>
 					<Action
@@ -64,37 +61,43 @@ function SidePanel({
 						label=""
 					/>
 				</li>
-				{actions.map(action => {
-					const { active, ...actionModel } = { ...action };
-					const actionProps = Object.assign({}, actionModel, {
-						id:
-							id &&
-							`${id}-nav-${action.label
-								.toLowerCase()
-								.split(' ')
-								.join('-')}`,
-						bsStyle: 'link',
-						role: 'link',
-						className: classNames(theme.link, action.className),
-						onClick: event => {
-							if (onSelect) {
-								onSelect(event, action);
-							}
-							if (action.onClick) {
-								action.onClick(event);
-							}
-						},
-					});
-
+				{actions.map((action) => {
+					const isSelected = isActionSelected(action);
+					const a11y = {};
+					if (isSelected) {
+						a11y['aria-current'] = true;
+					}
 					return (
 						<li
 							title={action.label}
 							key={action.key || action.label}
 							className={classNames('tc-side-panel-list-item', {
-								active: isActionSelected(action),
+								active: isSelected,
 							})}
+							{...a11y}
 						>
-							<Action {...actionProps} />
+							<Action
+								id={
+									id &&
+									`${id}-nav-${action.label
+										.toLowerCase()
+										.split(' ')
+										.join('-')}`
+								}
+								bsStyle="link"
+								role="link"
+								className={theme.link}
+								onClick={(event) => {
+									if (onSelect) {
+										onSelect(event, action);
+									}
+									if (action.onClick) {
+										action.onClick(event);
+									}
+								}}
+								label={action.label}
+								icon={action.icon}
+							/>
 						</li>
 					);
 				})}
@@ -103,23 +106,24 @@ function SidePanel({
 	);
 }
 
-const actionPropType = PropTypes.shape({
-	active: PropTypes.bool,
-	icon: PropTypes.string,
-	key: PropTypes.string,
-	label: PropTypes.string,
-	onClick: PropTypes.func,
-});
+if (process.env.NODE_ENV !== 'production') {
+	const actionPropType = PropTypes.shape({
+		active: PropTypes.bool,
+		icon: PropTypes.string,
+		key: PropTypes.string,
+		label: PropTypes.string,
+		onClick: PropTypes.func,
+	});
 
-SidePanel.propTypes = {
-	id: PropTypes.string,
-	actions: PropTypes.arrayOf(actionPropType),
-	onSelect: PropTypes.func,
-	onToggleDock: PropTypes.func,
-	docked: PropTypes.bool,
-	selected: actionPropType,
-	expandTitle: PropTypes.string,
-	collapseTitle: PropTypes.string,
-};
+	SidePanel.propTypes = {
+		id: PropTypes.string,
+		actions: PropTypes.arrayOf(actionPropType),
+		onSelect: PropTypes.func,
+		onToggleDock: PropTypes.func,
+		docked: PropTypes.bool,
+		selected: actionPropType,
+		t: PropTypes.func,
+	};
+}
 
-export default SidePanel;
+export default translate(I18N_DOMAIN_COMPONENTS, { i18n: DEFAULT_I18N })(SidePanel);

packages/components/src/SidePanel/SidePanel.scss
@@ -18,7 +18,7 @@ $toggle-button-padding: $padding-small - 1;
 	transition: 0.2s width ease-out;
 	width: $tc-side-panel-width;
 
-	&>ul {
+	> ul {
 		width: $tc-side-panel-width;
 	}
 

packages/components/src/SidePanel/SidePanel.test.js
@@ -25,7 +25,10 @@ describe('SidePanel', () => {
 			/>
 		);
 		const wrapper = mount(sidePanel);
-		wrapper.find(Button).at(0).simulate('click');
+		wrapper
+			.find(Button)
+			.at(0)
+			.simulate('click');
 
 		// then
 		expect(onToggleDock).toBeCalled();
@@ -53,7 +56,11 @@ describe('SidePanel', () => {
 			/>
 		);
 		const wrapper = mount(sidePanel);
-		wrapper.find('nav').find(Button).at(2).simulate('click');
+		wrapper
+			.find('nav')
+			.find(Button)
+			.at(2)
+			.simulate('click');
 
 		// then
 		expect(onPreparationsClick).not.toBeCalled();

packages/components/stories/SidePanel.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
 import { storiesOf, action } from '@storybook/react';
 import talendIcons from '@talend/icons/dist/react';
-
+import i18n, { LanguageSwitcher } from './config/i18n';
 import { SidePanel, IconsProvider, Layout } from '../src/index';
 
 const icons = {
@@ -32,19 +33,23 @@ const actions = [
 	},
 ];
 
-const items = [{
-	key: 'preparations',
-	label: 'Preparations',
-	icon: 'talend-dataprep',
-}, {
-	key: 'datasets',
-	label: 'Datasets',
-	icon: 'talend-download',
-}, {
-	key: 'favorites',
-	label: 'Favorites',
-	icon: 'talend-star',
-}];
+const items = [
+	{
+		key: 'preparations',
+		label: 'Preparations',
+		icon: 'talend-dataprep',
+	},
+	{
+		key: 'datasets',
+		label: 'Datasets',
+		icon: 'talend-download',
+	},
+	{
+		key: 'favorites',
+		label: 'Favorites',
+		icon: 'talend-star',
+	},
+];
 
 const stories = storiesOf('SidePanel', module);
 if (!stories.addWithInfo) {
@@ -52,40 +57,38 @@ if (!stories.addWithInfo) {
 }
 
 stories
-	.addWithInfo('default', () => (
+	.addDecorator(story => (
 		<div>
+			<LanguageSwitcher />
 			<IconsProvider defaultIcons={icons} />
-			<SidePanel
-				id="context"
-				actions={actions}
-				onToggleDock={action('Toggle dock clicked')}
-				docked={false}
-				tooltipPlacement="top"
-			/>
+			<I18nextProvider i18n={i18n}>{story()}</I18nextProvider>
 		</div>
+	))
+	.addWithInfo('default', () => (
+		<SidePanel
+			id="context"
+			actions={actions}
+			onToggleDock={action('Toggle dock clicked')}
+			docked={false}
+			tooltipPlacement="top"
+		/>
 	))
 	.addWithInfo('docked', () => (
-		<div>
-			<IconsProvider defaultIcons={icons} />
-			<SidePanel
-				actions={actions}
-				onToggleDock={action('Toggle dock clicked')}
-				docked
-				tooltipPlacement="top"
-			/>
-		</div>
+		<SidePanel
+			actions={actions}
+			onToggleDock={action('Toggle dock clicked')}
+			docked
+			tooltipPlacement="top"
+		/>
 	))
 	.addWithInfo('with onSelect function', () => (
-		<div>
-			<IconsProvider defaultIcons={icons} />
-			<SidePanel
-				actions={items}
-				onSelect={action('onItemSelect')}
-				onToggleDock={action('onToggleDock')}
-				selected={items[1]}
-				tooltipPlacement="top"
-			/>
-		</div>
+		<SidePanel
+			actions={items}
+			onSelect={action('onItemSelect')}
+			onToggleDock={action('onToggleDock')}
+			selected={items[1]}
+			tooltipPlacement="top"
+		/>
 	))
 	.addWithInfo('With layout (toggle interactive)', () => {
 		class WithLayout extends React.Component {
@@ -93,6 +96,7 @@ stories
 				super();
 				this.state = { docked: false };
 			}
+
 			render() {
 				const panelItems = items.concat([
 					{
@@ -111,13 +115,11 @@ stories
 					/>
 				);
 				return (
-					<Layout	mode="TwoColumns" one={panel}>
+					<Layout mode="TwoColumns" one={panel}>
 						<ol>
-							{
-								new Array(100)
-									.fill('This is some random content')
-									.map((item, num) => <li key={num}>{item}</li>)
-							}
+							{new Array(100)
+								.fill('This is some random content')
+								.map((item, num) => <li key={num}>{item}</li>)}
 						</ol>
 						<IconsProvider defaultIcons={icons} />
 					</Layout>
@@ -125,8 +127,5 @@ stories
 			}
 		}
 
-		return (
-			<WithLayout />
-		);
+		return <WithLayout />;
 	});
-

packages/components/stories/config/i18n.js
@@ -1,47 +1,72 @@
+import React from 'react';
 import i18n from 'i18next';
 import I18N_DOMAIN_COMPONENTS from '../../src/constants';
 
-i18n
-	.init({
-		resources: {
-			fr: {
-				[I18N_DOMAIN_COMPONENTS]: {
-					LIST_TOOLBAR_DISPLAY: 'Affichage :',
-					LIST_TOOLBAR_SORT_BY: 'Trier par :',
-					LIST_TOOLBAR_PAGINATION_SHOW: 'Nombre :',
-					LIST_SELECT_ALL: 'Sélectionner',
-					LIST_SELECT_SORT_BY_ORDER_ASC: 'Croissant',
-					LIST_SELECT_SORT_BY_ORDER_DESC: 'Décroissant',
-					LIST_SELECT_DISPLAY_MODE_TABLE: 'Tableau',
-					LIST_SELECT_DISPLAY_MODE_LARGE: 'Large',
-					LIST_SELECT_DISPLAY_MODE_TILE: 'Tuile',
-					LIST_FILTER_TOGGLE: 'Afficher le filtre',
-					LIST_FILTER_REMOVE: 'Supprimer le filtre',
-					ENUMERATION_EMPTY_LIST: 'La liste est vide',
-					ENUMERATION_HEADER_LABEL: 'Valeurs',
-				},
-			},
-			it: {
-				[I18N_DOMAIN_COMPONENTS]: {
-					LIST_TOOLBAR_DISPLAY: 'Affissione :',
-					LIST_TOOLBAR_SORT_BY: 'Ordina per :',
-					LIST_TOOLBAR_PAGINATION_SHOW: 'Mostrare :',
-					LIST_SELECT_ALL: 'Seleziona tutto',
-					LIST_SELECT_SORT_BY_ORDER_ASC: 'Ascendente',
-					LIST_SELECT_SORT_BY_ORDER_DESC: 'Discendente',
-					LIST_SELECT_DISPLAY_MODE_TABLE: 'Tabella',
-					LIST_SELECT_DISPLAY_MODE_LARGE: 'Grande',
-					LIST_SELECT_DISPLAY_MODE_TILE: 'Piastrella',
-					LIST_FILTER_TOGGLE: 'Ginocchiera filtro',
-					LIST_FILTER_REMOVE: 'Rimuova il filtro',
-					ENUMERATION_EMPTY_LIST: 'L\'elenco è vuoto',
-					ENUMERATION_HEADER_LABEL: 'Valori',
-				},
+i18n.init({
+	resources: {
+		fr: {
+			[I18N_DOMAIN_COMPONENTS]: {
+				LIST_TOOLBAR_DISPLAY: 'Affichage :',
+				LIST_TOOLBAR_SORT_BY: 'Trier par :',
+				LIST_TOOLBAR_PAGINATION_SHOW: 'Nombre :',
+				LIST_SELECT_ALL: 'Sélectionner',
+				LIST_SELECT_SORT_BY_ORDER_ASC: 'Croissant',
+				LIST_SELECT_SORT_BY_ORDER_DESC: 'Décroissant',
+				LIST_SELECT_DISPLAY_MODE_TABLE: 'Tableau',
+				LIST_SELECT_DISPLAY_MODE_LARGE: 'Large',
+				LIST_SELECT_DISPLAY_MODE_TILE: 'Tuile',
+				LIST_FILTER_TOGGLE: 'Afficher le filtre',
+				LIST_FILTER_REMOVE: 'Supprimer le filtre',
+				ENUMERATION_EMPTY_LIST: 'La liste est vide',
+				ENUMERATION_HEADER_LABEL: 'Valeurs',
+				SIDEPANEL_EXPAND: 'Développer',
+				SIDEPANEL_COLLAPSE: 'Réduire',
 			},
 		},
-		debug: false,
-		wait: true, // globally set to wait for loaded translations in translate hoc
-	});
+		it: {
+			[I18N_DOMAIN_COMPONENTS]: {
+				LIST_TOOLBAR_DISPLAY: 'Affissione :',
+				LIST_TOOLBAR_SORT_BY: 'Ordina per :',
+				LIST_TOOLBAR_PAGINATION_SHOW: 'Mostrare :',
+				LIST_SELECT_ALL: 'Seleziona tutto',
+				LIST_SELECT_SORT_BY_ORDER_ASC: 'Ascendente',
+				LIST_SELECT_SORT_BY_ORDER_DESC: 'Discendente',
+				LIST_SELECT_DISPLAY_MODE_TABLE: 'Tabella',
+				LIST_SELECT_DISPLAY_MODE_LARGE: 'Grande',
+				LIST_SELECT_DISPLAY_MODE_TILE: 'Piastrella',
+				LIST_FILTER_TOGGLE: 'Ginocchiera filtro',
+				LIST_FILTER_REMOVE: 'Rimuova il filtro',
+				ENUMERATION_EMPTY_LIST: "L'elenco è vuoto",
+				ENUMERATION_HEADER_LABEL: 'Valori',
+				SIDEPANEL_EXPAND: 'Espandere',
+				SIDEPANEL_COLLAPSE: 'Collassare',
+			},
+		},
+	},
+	debug: false,
+	wait: true, // globally set to wait for loaded translations in translate hoc
+});
 
+export const LanguageSwitcher = () => {
+	const style = {
+		position: 'fixed',
+		bottom: 0,
+		width: '100vw',
+		textAlign: 'center',
+		zIndex: 1,
+	};
+	return (
+		<nav style={style}>
+			<div className="btn-group">
+				<button className="btn" onClick={() => i18n.changeLanguage('fr')}>
+					fr
+				</button>
+				<button className="btn" onClick={() => i18n.changeLanguage('it')}>
+					it
+				</button>
+			</div>
+		</nav>
+	);
+};
 
 export default i18n;

commit a56c9082761f1927e5f76eb23b8f62a3bac9bf29

fix(SidePanel): regression with #818

packages/components/src/SidePanel/SidePanel.component.js
@@ -67,6 +67,26 @@ function SidePanel({ id, selected, onSelect, actions = [], docked, onToggleDock,
 					if (isSelected) {
 						a11y['aria-current'] = true;
 					}
+					const { active, ...actionModel } = { ...action };
+					const actionProps = Object.assign({}, actionModel, {
+						id:
+						id &&
+						`${id}-nav-${action.label
+							.toLowerCase()
+							.split(' ')
+							.join('-')}`,
+						bsStyle: 'link',
+						role: 'link',
+						className: classNames(theme.link, action.className),
+						onClick: event => {
+							if (onSelect) {
+								onSelect(event, action);
+							}
+							if (action.onClick) {
+								action.onClick(event);
+							}
+						},
+					});
 					return (
 						<li
 							title={action.label}
@@ -76,28 +96,7 @@ function SidePanel({ id, selected, onSelect, actions = [], docked, onToggleDock,
 							})}
 							{...a11y}
 						>
-							<Action
-								id={
-									id &&
-									`${id}-nav-${action.label
-										.toLowerCase()
-										.split(' ')
-										.join('-')}`
-								}
-								bsStyle="link"
-								role="link"
-								className={theme.link}
-								onClick={(event) => {
-									if (onSelect) {
-										onSelect(event, action);
-									}
-									if (action.onClick) {
-										action.onClick(event);
-									}
-								}}
-								label={action.label}
-								icon={action.icon}
-							/>
+							<Action {...actionProps} />
 						</li>
 					);
 				})}