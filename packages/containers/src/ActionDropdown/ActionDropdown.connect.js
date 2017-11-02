03ec0e3ec6e7a6cf62461d494cd4a2f837c4444f
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-11-02 10:25:45 +0100

    fix: actiondropdown connect

--- packages/containers/src/ActionDropdown/ActionDropdown.connect.js
+++ packages/containers/src/ActionDropdown/ActionDropdown.connect.js
@@ -18,6 +18,33 @@ export function mapStateToProps(state, { actionId } = {}) {
 	return props;
 }
 
-export default cmfConnect(
+export function mergeProps(stateProps, dispatchProps, ownProps) {
+	const props = Object.assign({}, stateProps, dispatchProps, ownProps);
+	if (ownProps.actionId) {
+		delete props.actionId;
+	}
+	if (ownProps.actionIds) {
+		props.items = props.items.map(item => Object.assign({
+			onClick: (event, data) => {
+				if (item.actionCreator) {
+					props.dispatchActionCreator(item.actionCreator, event, data);
+				} else {
+					props.dispatch(
+						Object.assign(
+							{
+								model: props.model,
+							},
+							item.payload,
+						),
+					);
+				}
+			},
+		}, item));
+	}
+	return props;
+}
+
+export default cmfConnect({
 	mapStateToProps,
-)(ActionDropdown);
+	mergeProps,
+})(ActionDropdown);
