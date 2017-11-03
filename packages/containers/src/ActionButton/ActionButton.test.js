0cd3a45f031bb3ccb6f36875d54aebc0021913ca
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-11-03 15:04:59 +0100

    test: add some

--- packages/containers/src/ActionButton/ActionButton.test.js
+++ packages/containers/src/ActionButton/ActionButton.test.js
@@ -3,25 +3,40 @@ import { shallow } from 'enzyme';
 import { ActionButton } from '@talend/react-components';
 import mock from '@talend/react-cmf/lib/mock';
 
-import Connected, { mapStateToProps, mergeProps, ContainerActionButton } from './ActionButton.connect';
+import Connected, {
+	mapStateToProps,
+	mergeProps,
+	ContainerActionButton,
+} from './ActionButton.connect';
 
-describe('Connected ActionButton', () => {
-	it('should connect ActionButton', () => {
+describe('Connect(CMF(Container(ActionButton)))', () => {
+	it('should connect Container(ActionButton)', () => {
 		expect(Connected.displayName).toBe(`Connect(CMF(${ContainerActionButton.displayName}))`);
 		expect(Connected.WrappedComponent).toBe(ContainerActionButton);
 	});
+});
+
+describe('CMF(Container(ActionButton))', () => {
 	it('should map state to props', () => {
-		const state = {};
-		const props = mapStateToProps(state, {});
+		const state = {
+			cmf: {
+				settings: {
+					actions: {
+						foo: { id: 'foo', label: 'Foo!' },
+					},
+				},
+			},
+		};
+		const props = mapStateToProps(state, { actionId: 'foo' });
 		expect(typeof props).toBe('object');
+		expect(props.label).toBe('Foo!');
+		expect(props.id).toBe('foo');
 	});
 	it('should render', () => {
 		const context = mock.context();
-		const wrapper = shallow(
-			<ActionButton actionId="menu:article" extra="foo" />,
-			{ context }
-		);
+		const wrapper = shallow(<ContainerActionButton actionId="menu:article" extra="foo" />, { context });
 		expect(wrapper.getNode()).toMatchSnapshot();
+		expect(wrapper.find(ActionButton).length).toBe(1);
 	});
 });
 
