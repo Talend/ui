cdc447bd222c52583ce03991f21b049716ec5b7b
jare-talend <jare@talend.com>
2017-11-03 14:28:24 +0100

    feat(Virtualized): option to disable header on table (#775)

--- packages/components/src/VirtualizedList/PropTypes.js
+++ packages/components/src/VirtualizedList/PropTypes.js
@@ -8,6 +8,8 @@ export default {
 	children: PropTypes.arrayOf(PropTypes.element),
 	// The collection items
 	collection: PropTypes.arrayOf(PropTypes.object),
+	// Disable header on TABLE
+	disableHeader: PropTypes.boolean,
 	// The id. The sub-ids will be based on the ids as prefix
 	id: PropTypes.string,
 	// Highlight row on click
