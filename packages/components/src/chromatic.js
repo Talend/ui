aba2e2728a9d9492d7e70854d487742ca1b68af9
frassinier <frassinier@talend.com>
2017-11-21 14:11:35 +0100

    feat(Components): add Chromatic support

---
+++ packages/components/src/chromatic.js
@@ -0,0 +1,20 @@
+if (process.env.NODE_ENV === 'development') {
+	const chromatic = require('react-chromatic').default;
+	const storybookRuntime = require('react-chromatic/storybook').default;
+
+	require('../.storybook/config.js');
+
+	// @see http://docs.chromaticqa.com/configuration
+	chromatic({
+		// appCode - (required) the unique code for your app, get it from chromaticqa.com
+		appCode: process.env.CHROMATIC_APP_CODE,
+		// components - An array of components. You can simply import these components as you would ordinarily in your app. We will automatically detect their displayName.
+		// components: [],
+		// componentContext - a (array of) Webpack require context of files that export components.
+		componentContext: [
+			require.context('./', true, /\/[^.]*\.js/),
+		],
+		// runtimes - an array of runtime configurations (see Storybook).
+		runtimes: [storybookRuntime],
+	});
+}
