c9267c046731a825be4766d506262d7e5a176034
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2017-11-28 16:46:26 +0100

    fix(components/TreeView): make it keyboard compliant
    remove diff between select and open

--- packages/components/src/TreeView/TreeViewItem/TreeViewItem.component.js
+++ packages/components/src/TreeView/TreeViewItem/TreeViewItem.component.js
@@ -21,6 +21,20 @@ function getActionHandler(func, item) {
 }
 
 /**
+* return the default open or closed folder icon if non is specified on item
+* or if it is specified return the specified icon with `-closed` append if not toggled
+* @param {String} icon - icon name
+* @param {Boolean} toggled - state of the item
+* @return {String}
+*/
+function getItemIcon(itemIcon, itemStateToggled) {
+	if (itemIcon === DEFAULT_OPEN_ICON) {
+		return itemStateToggled ? DEFAULT_OPEN_ICON : DEFAULT_CLOSED_ICON;
+	}
+	return itemStateToggled ? itemIcon : `${itemIcon}-closed`;
+}
+
+/**
  *
  * Single item of TreeView component
  *
@@ -34,122 +48,119 @@ function getActionHandler(func, item) {
  * @returns XML, jsx to display
  */
 
-function TreeViewItem({ id, item, depth = 0, onClick, onSelect }) {
-	const {
-		toggled = false,
-		selected,
-		hidden,
-		name,
-		children = [],
-		showCounter,
-		actions,
-		icon = 'talend-folder',
-		counter = children.length,
-	} = item;
-	const toggleIconLabel = toggled ? 'Collapse' : 'Expand';
+class TreeViewItem extends React.Component {
+	static propTypes = {
+		id: PropTypes.string.isRequired,
+		item: PropTypes.shape({
+			name: PropTypes.string.isRequired,
+			toggled: PropTypes.bool,
+			selected: PropTypes.bool,
+			children: PropTypes.arrayOf(PropTypes.object),
+			icon: PropTypes.string,
+			actions: PropTypes.arrayOf(
+				PropTypes.shape({
+					action: PropTypes.func,
+					label: PropTypes.string,
+					icon: PropTypes.string,
+				}),
+			),
+			counter: PropTypes.number,
+			showCounter: PropTypes.bool,
+		}).isRequired,
+		onClick: PropTypes.func.isRequired,
+		onSelect: PropTypes.func.isRequired,
+		depth: PropTypes.number,
+	};
 
-	function onSelectWrapper() {
-		return onSelect(item);
+	static defaultProps = {
+		id: 'tc-treeview-item',
+		depth: 0,
+	};
+
+	constructor(props) {
+		super(props);
+		this.onSelect = this.onSelect.bind(this);
+		this.renderTreeViewItem = this.renderTreeViewItem.bind(this);
 	}
 
-	function getTreeViewItem(child, i) {
+	onSelect(event) {
+		this.elementRef.focus();
+		this.props.onClick(event, this.props.item);
+		return this.props.onSelect(this.props.item);
+	}
+
+	renderTreeViewItem(child, i) {
 		return (
 			<TreeViewItem
-				{...{
-					id: id && `${id}-${i}`,
-					item: child,
-					onSelect,
-					onClick,
-					depth: depth + 1,
-					key: i,
-				}}
+				id={`${this.props.id}-${i}`}
+				item={child}
+				onSelect={this.props.onSelect}
+				onClick={this.props.onClick}
+				depth={this.props.depth + 1}
+				key={i}
 			/>
 		);
 	}
 
-	function getIconAction(label, icon_, action, id_) {
+	renderIconAction(label, icon_, action, id_) {
 		return (
 			<Action
 				label={label}
 				icon={icon_}
-				onClick={getActionHandler(action, item)}
+				onClick={getActionHandler(action, this.props.item)}
 				tooltipPlacement="right"
 				hideLabel
 				key={label}
-				id={id_ || `${id}-${icon_}`}
+				id={id_ || `${this.props.id}-${icon_}`}
 				link
 			/>
 		);
 	}
 
-	/**
-	 * return the default open or closed folder icon if non is specified on item
-	 * or if it is specified return the specified icon with `-closed` append if not toggled
-	 * @param {String} icon - icon name
-	 * @param {Boolean} toggled - state of the item
-	 * @return {String}
-	 */
-	function getItemIcon(itemIcon, itemStateToggled) {
-		if (itemIcon === DEFAULT_OPEN_ICON) {
-			if (itemStateToggled) return DEFAULT_OPEN_ICON;
-			return DEFAULT_CLOSED_ICON;
-		}
-		if (itemStateToggled) return itemIcon;
-		return `${itemIcon}-closed`;
-	}
+	render() {
+		const { id, item, depth = 0 } = this.props;
+		const {
+			toggled = false,
+			selected,
+			hidden,
+			name,
+			children = [],
+			showCounter,
+			actions,
+			icon = 'talend-folder',
+			counter = children.length,
+		} = item;
+		const toggleIconLabel = toggled ? 'Collapse' : 'Expand';
 
-	const paddingLeft = `${depth * PADDING_NORMAL + PADDING_LARGE}px`;
+		const paddingLeft = `${depth * PADDING_NORMAL + PADDING_LARGE}px`;
 
-	return (
-		<li className={classNames('tc-treeview-item-li', css['tc-treeview-li'])} data-hidden={hidden}>
-			<div // eslint-disable-line jsx-a11y/no-static-element-interactions
-				className={classNames('tc-treeview-item', css['tc-treeview-item'])}
-				data-selected={selected}
-				onClick={onSelectWrapper}
-				id={id}
-				style={{ paddingLeft }}
-			>
-				{!children.length || (
-					<div className={css['tc-treeview-toggle']} data-toggled={toggled}>
-						{getIconAction(toggleIconLabel, 'talend-caret-down', onClick, `${id}-toggle`)}
+		return (
+			<li className={classNames('tc-treeview-item-li', css['tc-treeview-li'])} data-hidden={hidden}>
+				<div // eslint-disable-line jsx-a11y/no-static-element-interactions
+					className={classNames('tc-treeview-item', css['tc-treeview-item'])}
+					data-selected={selected}
+					onClick={this.onSelect}
+					id={id}
+					style={{ paddingLeft }}
+					role="button"
+					tabIndex="0"
+					ref={element => { this.elementRef = element; }}
+				>
+					{!children.length || (
+						<Icon className={css['tc-treeview-toggle']} name="talend-caret-down" transform={toggled ? undefined : 'rotate-270'} title={toggleIconLabel} />
+					)}
+					<Icon name={getItemIcon(icon, toggled)} key={icon} className={css['tc-treeview-folder']} />
+					<span className="tc-treeview-item-name">{name}</span>
+					<div className={css['tc-treeview-item-ctrl']}>
+						{showCounter && <Badge label={counter.toString()} />}
+						{actions && actions.map(a => this.renderIconAction(a.label, a.icon, a.action))}
 					</div>
-				)}
-				<span className={css['tc-treeview-folder']}>
-					<Icon name={getItemIcon(icon, toggled)} key={icon} />
-				</span>
-				<span className="tc-treeview-item-name">{name}</span>
-				<div className={css['tc-treeview-item-ctrl']}>
-					{showCounter && <Badge label={counter.toString()} />}
-					{actions && actions.map(a => getIconAction(a.label, a.icon, a.action))}
 				</div>
-			</div>
-			{children &&
-				toggled && <ul className={css['tc-treeview-ul']}>{children.map(getTreeViewItem)}</ul>}
-		</li>
-	);
+				{children &&
+					toggled && <ul className={css['tc-treeview-ul']}>{children.map(this.renderTreeViewItem)}</ul>}
+			</li>
+		);
+	}
 }
-
-TreeViewItem.propTypes = {
-	id: PropTypes.string,
-	item: PropTypes.shape({
-		name: PropTypes.string.isRequired,
-		toggled: PropTypes.bool,
-		selected: PropTypes.bool,
-		children: PropTypes.arrayOf(PropTypes.object),
-		icon: PropTypes.string,
-		actions: PropTypes.arrayOf(
-			PropTypes.shape({
-				action: PropTypes.func,
-				label: PropTypes.string,
-				icon: PropTypes.string,
-			}),
-		),
-		counter: PropTypes.number,
-		showCounter: PropTypes.bool,
-	}).isRequired,
-	onClick: PropTypes.func.isRequired,
-	onSelect: PropTypes.func.isRequired,
-	depth: PropTypes.number,
-};
 
 export default TreeViewItem;
