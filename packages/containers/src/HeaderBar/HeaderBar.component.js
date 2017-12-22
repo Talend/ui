1d40aaa9d99e9c211013a40ace07d445396fb74c
Yan Wang <ywang@talend.com>
2017-12-22 17:00:28 +0800

    remove ActionSplitDown renderer

--- packages/containers/src/HeaderBar/HeaderBar.component.js
+++ packages/containers/src/HeaderBar/HeaderBar.component.js
@@ -3,13 +3,11 @@ import { cmfConnect } from '@talend/react-cmf';
 import { HeaderBar as PureHeaderBar } from '@talend/react-components';
 
 import Action from '../Action';
-import ActionSplitDropdown from '../ActionSplitDropdown';
 import ActionDropdown from '../ActionDropdown';
 import getRenderers from '../renderers';
 
 const renderers = {
 	Action,
-	ActionSplitDropdown,
 	ActionDropdown,
 };
 
