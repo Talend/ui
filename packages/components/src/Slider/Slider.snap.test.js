da6fc1c75dd32895b71f3c9989d32568321dc87e
Sébastien Romain <sebastien.romain@gmail.com>
2017-12-22 16:48:53 +0100

    add a missing test

--- packages/components/src/Slider/Slider.snap.test.js
+++ packages/components/src/Slider/Slider.snap.test.js
@@ -63,5 +63,14 @@ describe('Slider component', () => {
 			// then
 			expect(result).toBe(2);
 		});
+
+		it('should return -1 when value is null', () => {
+			// given
+			const icons = ['icon1', 'icon2', 'icon3', 'icon4', 'icon5'];
+			// when
+			const result = getSelectedIconPosition(icons, null, 0, 100);
+			// then
+			expect(result).toBe(-1);
+		});
 	});
 });
