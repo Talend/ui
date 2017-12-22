89ba3b37f8755c703798186ecb456f9b101300bf
travis <travis>
2017-12-22 14:24:17 +0000

    test(ci): prettier

--- packages/components/src/VirtualizedList/utils/tablerow.js
+++ packages/components/src/VirtualizedList/utils/tablerow.js
@@ -26,10 +26,13 @@ export function insertSelectionConfiguration({ children, isSelected, selectionTo
 				cellDataGetter={({ rowData }) => isSelected(rowData)}
 				columnData={{ label: 'Select this element', onChange: selectionToggle }}
 				{...CellCheckboxRenderer}
-			/>);
+			/>
+		);
 		contentsConfiguration = [toggleColumn].concat(contentsConfiguration);
 	} else if (selectionToggle || isSelected) {
-		console.warn('VirtualizedList : props.selectionToggle goes with props.isSelected. You defined only one of those props, selection is ignored.');
+		console.warn(
+			'VirtualizedList : props.selectionToggle goes with props.isSelected. You defined only one of those props, selection is ignored.',
+		);
 	}
 	return contentsConfiguration;
 }
@@ -40,26 +43,17 @@ export function insertSelectionConfiguration({ children, isSelected, selectionTo
  * - parent id (via columnData)
  */
 export function toColumns(id, theme, children) {
-	return React.Children.toArray(children)
-		.map((field, index) => {
-			const colClassName = `tc-list-cell-${field.props.dataKey}`;
-			const colProps = {
-				...field.props,
-				headerClassName: classNames(
-					field.props.headerClassName,
-					theme.header,
-					colClassName
-				),
-				className: classNames(
-					field.props.className,
-					theme.cell,
-					colClassName
-				),
-				columnData: {
-					...field.props.columnData,
-					id,
-				},
-			};
-			return <Column key={index} {...colProps} />;
-		});
+	return React.Children.toArray(children).map((field, index) => {
+		const colClassName = `tc-list-cell-${field.props.dataKey}`;
+		const colProps = {
+			...field.props,
+			headerClassName: classNames(field.props.headerClassName, theme.header, colClassName),
+			className: classNames(field.props.className, theme.cell, colClassName),
+			columnData: {
+				...field.props.columnData,
+				id,
+			},
+		};
+		return <Column key={index} {...colProps} />;
+	});
 }
