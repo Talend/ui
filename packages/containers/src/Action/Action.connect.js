0e94de0584ef6a49512ba1bd8f62672a35b9287e
Yan Wang <ywang@talend.com>
2017-12-22 16:48:34 +0800

    add Action renders back. still used in HeaderBar

--- packages/containers/src/Action/Action.connect.js
+++ packages/containers/src/Action/Action.connect.js
@@ -1,8 +1,23 @@
 import { api, cmfConnect } from '@talend/react-cmf';
 import { Action } from '@talend/react-components';
 
+import ActionButton from '../ActionButton';
+import ActionFile from '../ActionFile';
+import ActionSplitDropdown from '../ActionSplitDropdown';
+import ActionDropdown from '../ActionDropdown';
+import getRenderers from '../renderers';
+
+export const renderers = {
+	ActionButton,
+	ActionFile,
+	ActionSplitDropdown,
+	ActionDropdown,
+};
+
 export function mapStateToProps(state, ownProps) {
-	const props = {};
+	const props = {
+		renderers: getRenderers(renderers),
+	};
 
 	if (!ownProps.actionId && !ownProps.name) {
 		return props;
