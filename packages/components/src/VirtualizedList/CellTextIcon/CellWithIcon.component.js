535a8b0ed18dc3f4dae98b6eb89942c3fb5aef46
Volodymyr Vyshko <vvyshko@talend.com>
2017-12-27 09:03:16 +0200

    Fix review issues

--- packages/components/src/VirtualizedList/CellTextIcon/CellWithIcon.component.js
+++ packages/components/src/VirtualizedList/CellTextIcon/CellWithIcon.component.js
@@ -1,19 +1,26 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import classnames from 'classnames';
 
 import Action from '../../Actions/Action';
 import styles from './CellWithIcon.scss';
 
 /**
- * Cell renderer that displays a badge
+ * Cell renderer that displays text + icon
  */
-function CellWithIcon(props) {
-	const action = props.columnData.getIcon(props.rowData);
+function CellWithIcon({
+	columnData,
+	rowData,
+	cellData,
+}) {
+	const action = columnData.getIcon(rowData);
 
 	return (
-		<div className={styles['cell-icon-container']}>
-			<div>{props.cellData}</div>
-			<div>{action && <Action {...action} hideLabel link />}</div>
+		<div className={classnames('cell-icon-container', styles['cell-icon-container'])}>
+			<div>{cellData}</div>
+			<div>
+				{action && <Action {...action} hideLabel link />}
+			</div>
 		</div>
 	);
 }
@@ -25,7 +32,7 @@ CellWithIcon.propTypes = {
 	// Can be any object
 	rowData: PropTypes.object,
 	columnData: PropTypes.shape({
-		getIcon: PropTypes.func,
+		getIcon: PropTypes.func.isRequired,
 	}).isRequired,
 };
 
