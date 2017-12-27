af12812eb44bb42bf1ec2e4ea63b8643f698afe5
Yan Wang <ywang@talend.com>
2017-12-27 11:05:01 +0800

    add tests

---
+++ packages/containers/src/HeaderBar/HeaderBar.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider, mock} from '@talend/react-cmf/lib/mock';
+import { HeaderBar } from '@talend/react-components';
+
+import Container from './HeaderBar.component';
+
+
+describe('Container HeaderBar', () => {
+	it('should pass props to component HeaderBar', () => {
+		const wrapper = mount(<Provider><Container /></Provider>);
+		expect(wrapper.find(HeaderBar).props()).toMatchSnapshot();
+})
+});
