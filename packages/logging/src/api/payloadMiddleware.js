8dae3778f21f17034a07dd31a4f4b201801c036c
travis <travis>
2017-12-28 15:07:04 +0000

    test(ci): prettier

--- packages/logging/src/api/payloadMiddleware.js
+++ packages/logging/src/api/payloadMiddleware.js
@@ -1,7 +1,7 @@
 export default function getStatePayloadMiddleware(getState) {
 	return function payloadMiddleware(payload) {
 		return {
-			time: (new Date()).toJSON(),
+			time: new Date().toJSON(),
 			appState: getState(),
 			userAgent: navigator.userAgent,
 			...payload,
