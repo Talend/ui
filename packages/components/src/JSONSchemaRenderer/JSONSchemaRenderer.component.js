dc76c83c97b484c0891a81e88b9a5088e5b5abbb
sgendre <sgendre@talend.com>
2017-12-29 16:31:09 +0100

    add unit tests

--- packages/components/src/JSONSchemaRenderer/JSONSchemaRenderer.component.js
+++ packages/components/src/JSONSchemaRenderer/JSONSchemaRenderer.component.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 import entries from 'lodash/entries';
+import get from 'lodash/get';
 
 import css from './JSONSchemaRenderer.scss';
 
@@ -72,7 +73,7 @@ function TextRenderer({ propertyKey, title, properties }) {
 	);
 }
 
-function PasswordRenderer({ propertyKey, title, properties }) {
+export function PasswordRenderer({ propertyKey, title, properties }) {
 	return (
 		<div className={classNames('text-renderer', `text-renderer-${propertyKey}`)} key={propertyKey}>
 			<dt>{title || propertyKey}</dt>
@@ -140,8 +141,8 @@ function isHidden(uiSchema, element) {
 	return uiSchema && uiSchema[element] && uiSchema[element]['ui:widget'] === 'hidden';
 }
 
-function isPassword(uiSchema, element) {
-	return uiSchema && uiSchema[element] && uiSchema[element]['ui:widget'] === 'password';
+export function isPassword(uiSchema, element) {
+	return get(uiSchema, [element, 'ui:widget'], '') === 'password';
 }
 
 /**
