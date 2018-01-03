4d29c7ceebf8cc217c4b81a4ad1eee0ba3c13a8a
travis <travis>
2018-01-03 11:20:34 +0000

    test(ci): prettier

--- packages/icons/src/svgs.js
+++ packages/icons/src/svgs.js
@@ -7,9 +7,11 @@ const dir = path.join(__dirname, './svg');
 const files = fs.readdirSync(dir);
 const svgs = {};
 //case insensitive sort for windows users
-files.sort(function (a, b) {
-	return a.localeCompare(b, {'sensitivity': 'base'});
-}).forEach((file) => {
-	svgs[file.split('.svg')[0]] = fs.readFileSync(path.resolve(dir, file));
-});
+files
+	.sort(function(a, b) {
+		return a.localeCompare(b, { sensitivity: 'base' });
+	})
+	.forEach(file => {
+		svgs[file.split('.svg')[0]] = fs.readFileSync(path.resolve(dir, file));
+	});
 exports.default = svgs;
