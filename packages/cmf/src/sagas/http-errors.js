3eb4ae394db4705d847386a43a9bafe8a8e018b3
Jean-Alain Ré <jare@talend.com>
2018-01-05 15:36:07 +0100

    add documentation

--- packages/cmf/src/sagas/http-errors.js
+++ packages/cmf/src/sagas/http-errors.js
@@ -12,6 +12,18 @@ const HTTPErrorsCodes = [HTTP_STATUS.UNAUTHORIZED, HTTP_STATUS.FORBIDDEN, HTTP_S
  * @param  {Function} errors.on403 saga to spawn when 403 errors
  * @param  {Function} errors.on404 saga to spawn when 404 errors
  * @return {Function}              a generator function
+ * @example
+ * import { sagas, store } from '@talend/react-cmf';
+ * import createSagaMiddleware from 'redux-saga'
+ * sagas.getHttpErrorsSaga({
+ *	 on401: function* on401() {...},
+ * 	 on403: function* on403() {...},
+ * 	 on404: function* on404() {...},
+ * });
+ * store.initialize({}, {}, undefined, [
+ * 	 createSagaMiddleware().run(helloSaga),
+ *  ]);
+ *
  */
 export default function getHttpErrorsSaga(errors = {}) {
 	return function* httpErrorSaga() {
