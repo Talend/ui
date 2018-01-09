b279c65a4090d0faf57664db3e80028fb396a61c
Sébastien Romain <sebastien.romain@gmail.com>
2018-01-09 14:13:44 +0100

    fix(containers/Redirect): add cmf connect to redirect container

--- packages/containers/src/Redirect/Redirect.component.js
+++ packages/containers/src/Redirect/Redirect.component.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { cmfConnect } from '@talend/react-cmf';
 import { CircularProgress } from '@talend/react-components';
 
 export const REDIRECT_ACTION_TYPE = 'REDIRECT';
@@ -9,7 +10,7 @@ export const REDIRECT_ACTION_TYPE = 'REDIRECT';
  * @example
 <Redirect name="Hello world"></Redirect>
  */
-function Redirect({ path }, context) {
+export function Redirect({ path }, context) {
 	context.store.dispatch({
 		type: REDIRECT_ACTION_TYPE,
 		cmf: {
@@ -31,4 +32,4 @@ Redirect.contextTypes = {
 	store: PropTypes.object.isRequired,
 };
 
-export default Redirect;
+export default cmfConnect({})(Redirect);
--- packages/containers/src/Redirect/Redirect.test.js
+++ packages/containers/src/Redirect/Redirect.test.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import Redirect from './Redirect.component';
+import { Redirect } from './Redirect.component';
 
-jest.mock(
-	'@talend/react-components',
-	() => ({ CircularProgress: props => (<div className="tc-circular-project" {...props} />) })
-);
+jest.mock('@talend/react-components', () => ({
+	CircularProgress: props => <div className="tc-circular-project" {...props} />,
+}));
 
 describe('Redirect', () => {
 	it('should dispatch a redirect action', () => {
 		const dispatch = jest.fn();
-		const result = Redirect(
-			{ path: '/hello-world' },
-			{ store: { dispatch } }
-		);
+		const result = new Redirect({ path: '/hello-world' }, { store: { dispatch } });
 		expect(dispatch.mock.calls.length).toBe(1);
 		const action = dispatch.mock.calls[0][0];
 		expect(action.type).toBe('REDIRECT');
