347333f7b26f336130bf7bbedfa099fcefdea1cd
sgendre <sgendre@talend.com>
2018-01-11 16:30:38 +0100

    remove useless comments

--- packages/cmf-cqrs/src/constants/socketMiddleware.js
+++ packages/cmf-cqrs/src/constants/socketMiddleware.js
@@ -1,8 +1,3 @@
-/*export const ACTION_TYPES = {
-	ON_OPEN: '@@SOCKET.ON_OPEN',
-	ON_CLOSE: '@@SOCKET.ON_CLOSE',
-	ON_ERROR: '@@SOCKET.ON_ERROR',
-};*/
 export const SOCKET_ON_OPEN = '@@SOCKET.ON_OPEN';
 export const SOCKET_ON_CLOSE = '@@SOCKET.ON_CLOSE';
 export const SOCKET_ON_ERROR = '@@SOCKET.ON_ERROR';
