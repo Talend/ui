5e1c9bfe94af261ce391ac48aa389c1a13424b1f
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-01-16 14:29:17 +0100

    fix: TFD-3229 id of datalist widget

--- packages/forms/src/UIForm/fields/Datalist/Datalist.component.js
+++ packages/forms/src/UIForm/fields/Datalist/Datalist.component.js
@@ -190,7 +190,7 @@ class Datalist extends Component {
 			>
 				<div className={theme['tf-datalist']}>
 					<Typeahead
-						id={this.props.id}
+						id={`react-autowhatever-${this.props.id}`}
 						autoFocus={this.props.schema.autoFocus || false}
 						disabled={this.props.schema.disabled || false}
 						focusedItemIndex={this.state.focusedItemIndex}
