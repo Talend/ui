7cd89188db72894d7fcc9acb347bcbb166204aaa
Nicolas Comont <ncomont@talend.com>
2018-01-23 11:46:35 +0100

    feat(components/loader): customize size

--- packages/components/stories/Loader.js
+++ packages/components/stories/Loader.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import { Loader } from '../src/';
-
+import { Loader, CIRCULAR_PROGRESS_SIZE as SIZE } from '../src/';
 
 storiesOf('Loader', module)
 	.addWithInfo('default', () => (
 		<section>
 			<Loader />
+		</section>
+	))
+	.addWithInfo('small', () => (
+		<section>
+			<Loader size={SIZE.small} />
+		</section>
+	))
+	.addWithInfo('large', () => (
+		<section>
+			<Loader size={SIZE.large} />
 		</section>
 	));
