425cf3c333b17b3ebd21ea9ebf5080044d1a3160
Nicolas Comont <ncomont@talend.com>
2018-01-24 11:16:15 +0100

    fix: default export for i18n domain components

--- packages/components/src/constants.js
+++ packages/components/src/constants.js
@@ -1,8 +1,8 @@
 const I18N_DOMAIN_COMPONENTS = 'tui-components';
-const CIRCULAR_PROGRESS_SIZE = {
+export const CIRCULAR_PROGRESS_SIZE = {
 	small: 'small',
 	default: 'default',
 	large: 'large',
 };
 
-export { I18N_DOMAIN_COMPONENTS, CIRCULAR_PROGRESS_SIZE };
+export default I18N_DOMAIN_COMPONENTS;
