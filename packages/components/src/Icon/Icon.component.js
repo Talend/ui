da23b57677b5c51ab86d008e8e8912afb0a7dcd5
jsomsanith <jsomsanith@talend.com>
2018-02-02 15:12:18 +0100

    Remove alt

--- packages/components/src/Icon/Icon.component.js
+++ packages/components/src/Icon/Icon.component.js
@@ -44,7 +44,7 @@ function Icon({ className, name, title, transform, onClick }) {
 	};
 	if (name.startsWith('src-')) {
 		const classNames = classnames(theme['tc-icon'], 'tc-icon', className);
-		return <img className={classNames} src={name.substring(4)} alt={title || ''} aria-hidden />;
+		return <img className={classNames} src={name.substring(4)} alt={''} aria-hidden />;
 	}
 	if (name.startsWith('fa-')) {
 		const classes = classnames('fa', name, className, transform && FA_TRANSFORMS[transform]);
