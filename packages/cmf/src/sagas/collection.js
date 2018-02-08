7fe710675abcb093a40e73c67dfc3ffaf529a061
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-02-08 16:47:11 +0100

    chore: refactor follow review

--- packages/cmf/src/sagas/collection.js
+++ packages/cmf/src/sagas/collection.js
@@ -1,11 +1,18 @@
 import { call, select } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 
-export function* waitFor(id, timeToWait = 10) {
-	let found = false;
-	while (!found) {
-		const state = yield select();
-		found = state.cmf.collections.has(id);
-		yield call(delay, timeToWait);
+/**
+ * this saga ends when the collection is available
+ * @param {string} id of the collection to wait for
+ * @param {number} interval in ms
+ */
+export function* waitFor(id, interval = 10) {
+	// eslint-disable-next-line no-constant-condition
+	while (true) {
+		const collection = yield select(state => state.cmf.collections.get(id));
+		if (collection !== undefined) {
+			break;
+		}
+		yield call(delay, interval);
 	}
 }
