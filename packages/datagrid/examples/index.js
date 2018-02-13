cc488f3fa3860eadfaab8615c558d000107eba8c
Jean-Alain Ré <jare@talend.com>
2018-02-13 15:18:23 +0100

    remove vanilla example

--- packages/datagrid/examples/index.js
+++ packages/datagrid/examples/index.js
@@ -1,9 +1,7 @@
 import ReactCellRenderer from './ReactCellRenderer';
-import VanillaCellRenderer from './VanillaCellRenderer';
 import ContainerDataGrid from './ContainerDataGrid';
 
 export default {
 	ContainerDataGrid,
 	ReactCellRenderer,
-	VanillaCellRenderer,
 };
