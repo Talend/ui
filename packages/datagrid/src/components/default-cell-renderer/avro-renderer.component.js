6c65a8e17797121aa50ef742ef428b7285928907
Jean-Alain Ré <jare@talend.com>
2018-02-13 11:36:55 +0100

    format code

--- packages/datagrid/src/components/default-cell-renderer/avro-renderer.component.js
+++ packages/datagrid/src/components/default-cell-renderer/avro-renderer.component.js
@@ -3,5 +3,6 @@ import { Inject } from '@talend/react-components';
 
 export default function DefaultTypeRenderer({ avroRenderer, colDef, data, getComponent }) {
 	const componentId = avroRenderer[`${colDef.avro.type.type}CellRenderer`];
+
 	return <Inject getComponent={getComponent} component={componentId} data={data} colDef={colDef} />;
 }
