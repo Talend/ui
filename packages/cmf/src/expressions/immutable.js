fa64df0d1f639ca9be6095f9eef9fa1a63eda8d9
travis <travis>
2018-02-15 10:19:45 +0000

    test(ci): prettier

--- packages/cmf/src/expressions/immutable.js
+++ packages/cmf/src/expressions/immutable.js
@@ -3,11 +3,10 @@ import Immutable from 'immutable';
 import curry from 'lodash/curry';
 
 function getInState(statePath, { context }, immutablePath, defaultValue) {
-	return _get(
-		context.store.getState(),
-		statePath,
-		new Immutable.Map()
-	).getIn(immutablePath.split('.'), defaultValue);
+	return _get(context.store.getState(), statePath, new Immutable.Map()).getIn(
+		immutablePath.split('.'),
+		defaultValue,
+	);
 }
 
 export default curry(getInState);
