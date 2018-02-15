44b11c89d09430cb0f7adc5cdbcfb1a894edd130
Sébastien Romain <sebastien.romain@gmail.com>
2018-02-15 11:14:57 +0100

    add comments here

--- packages/cmf/src/saga.js
+++ packages/cmf/src/saga.js
@@ -1,9 +1,20 @@
 import registry from './registry';
 
+/**
+ * This function register a saga in the cmf registry
+ * @param {string} id the saga id you want
+ * @param {generator} saga the saga generator
+ * @param {object} context optional context to get the registry
+ */
 export function register(id, saga, context) {
 	registry.addToRegistry(`SAGA:${id}`, saga, context);
 }
 
+/**
+ * This function allow to get a saga from the registry
+ * @param {string} id the saga id you want
+ * @param {object} context optional context to get the registry
+ */
 export function get(id, context) {
 	return registry.getFromRegistry(`SAGA:${id}`, context);
 }
