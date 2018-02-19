bb9a8387b024cbb8922ca22c1445fd7854062c2b
Jean-Alain Ré <jare@talend.com>
2018-02-19 16:16:23 +0100

    fix reviews and tests

--- packages/datagrid/src/components/default-header-renderer/quality-bar.component.js
+++ packages/datagrid/src/components/default-header-renderer/quality-bar.component.js
@@ -8,17 +8,17 @@ export default function QualityBar(props) {
 	return (
 		<div className={classNames(theme['td-quality-bar'], 'td-quality-bar')}>
 			<span
-				className={classNames(theme['td-quality-bar-invalid'])}
+				className={classNames(theme['td-quality-bar-invalid'], 'td-quality-bar-invalid')}
 				style={{ width: `${props.invalid}%` }}
 				title={`${props.invalid}%`}
 			/>
 			<span
-				className={classNames(theme['td-quality-bar-empty'])}
+				className={classNames(theme['td-quality-bar-empty'], 'td-quality-bar-empty')}
 				style={{ width: `${props.empty}%` }}
 				title={`${props.empty}%`}
 			/>
 			<span
-				className={classNames(theme['td-quality-bar-valid'])}
+				className={classNames(theme['td-quality-bar-valid'], 'td-quality-bar-valid')}
 				style={{ width: `${props.valid}%` }}
 				title={`${props.valid}%`}
 			/>
