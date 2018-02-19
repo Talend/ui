49428a0ffe9170b22b0d69c69eeff4e67469bc3b
Fabien Rassinier <frassinier@talend.com>
2018-02-19 11:49:41 +0100

    feat(cmf): add react-cmf-webpack-plugin

---
+++ packages/cmf-webpoack-plugin/README.md
@@ -0,0 +1,107 @@
+# React CMF Webpack Plugin (aka react-cmf-webpack-plugin)
+
+Simplifies merging of CMF settings files to serve your webpack bundles.
+
+[![NPM][npm-icon] ][npm-url]
+
+[![Travis CI][travis-ci-image] ][travis-ci-url]
+[![Quality][quality-badge] ][quality-url]
+[![dependencies][dependencies-image] ][dependencies-url]
+[![devdependencies][devdependencies-image] ][devdependencies-url]
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/f1e353b0f69c4cf99a4cb3f68b70ea7d)](https://www.codacy.com/app/Talend/ui/packages/cmf-cqrs)
+
+[npm-icon]: https://nodei.co/npm/react-cmf-webpack-plugin.png?downloads=true
+[npm-url]: https://npmjs.org/package/@talend/react-cmf-webpack-plugin
+[travis-ci-image]: https://travis-ci.org/Talend/react-cmf-webpack-plugin.svg?branch=master
+[travis-ci-url]: https://travis-ci.org/Talend/react-cmf-webpack-plugin
+[dependencies-image]: https://david-dm.org/Talend/react-cmf-webpack-plugin/status.svg
+[dependencies-url]: https://david-dm.org/Talend/react-cmf-webpack-plugin
+[devdependencies-image]: https://david-dm.org/Talend/react-cmf-webpack-plugin/dev-status.svg
+[devdependencies-url]: https://david-dm.org/Talend/react-cmf-webpack-plugin#info=devDependencies
+[quality-badge]: http://npm.packagequality.com/shield/react-cmf-webpack-plugin.svg
+[quality-url]: http://packagequality.com/#?package=react-cmf-webpack-plugin
+
+## Breaking changes log
+
+Before 1.0, `react-cmf-webpack-plugin` does NOT follow semver in releases.
+You will find a [list of breaking changes here](https://github.com/Talend/ui/blob/master/BREAKING_CHANGES_LOG.md).
+
+## Content
+
+This package provides Webpack plugin to deal with several React CMF settings files.
+
+## Installation
+
+Install dependency:
+
+```bash
+$> yarn add @talend/react-cmf-webpack-plugin
+```
+
+## Basic Usage
+
+You must have a _cmf.json_ file at root folder of your project:
+
+``json
+{
+	"settings": {
+		"sources": [
+            "src/settings",
+            "node_modules/another_package_name/lib/settings/"
+		],
+		"sources-dev": [
+			"src/settings",
+            "../../another_package_name/src/settings/"
+		],
+		"destination": "dist/settings.json"
+  }
+}
+```
+
+Edit your webpack.config.js file:
+
+```javascript
+const ReactCMFWebpackPlugin = require('react-cmf-webpack-plugin');
+
+const webpackConfig = {
+  entry: 'index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  plugins: [
+  	    new ReactCMFWebpackPlugin(),
+  ],
+};
+```
+
+## Configuration
+
+You can pass a bunch of configuration options to ReactCMFWebpackPlugin. Allowed values are as follows:
+
+| Option | Type | Default | Description |
+| --- | --- | --- | --- |
+| `dev` | Boolean | false | `devSource` entry will be used instead of `sources` one in _cmf.json_ file |
+| `quiet` | Boolean | false | No output at all |
+| `recursive` | Boolean | false | Recursive search for JSON files |
+
+Here's an example webpack config illustrating how to use these options:
+
+```javascript
+const ReactCMFWebpackPlugin = require('react-cmf-webpack-plugin');
+
+const webpackConfig = {
+  entry: 'index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new ReactCMFWebpackPlugin({
+      dev: false,
+      quiet: false,
+      recursive: false,
+    }),
+  ],
+};
+```
---
+++ packages/cmf-webpoack-plugin/src/index.js
@@ -0,0 +1,131 @@
+const fs = require('fs');
+const pathLib = require('path');
+const mkdirp = require('mkdirp');
+const deepmerge = require('deepmerge');
+
+/**
+ * React CMF Webpack Plugin
+ * @param options Plugin options
+ * @constructor
+ */
+function ReactCMFWebpackPlugin(options) {
+	this.options = options;
+}
+
+ReactCMFWebpackPlugin.prototype.apply = function (compiler) {
+	const {
+		dev, // dev sources instead of sources
+		quiet, // no output
+		recursive, // recursive search for json files
+	} = this.options;
+
+	compiler.plugin('emit', function (compilation, callback) {
+
+		function findJson(fileOrFolder, recursive) {
+			let jsonFiles = [];
+			if (fileOrFolder.endsWith('.json')) {
+				jsonFiles.push(fileOrFolder);
+			} else {
+				fs.readdirSync(fileOrFolder).forEach(path => {
+					const fullpath = pathLib.join(fileOrFolder, path);
+					if (path.endsWith('.json')) {
+						jsonFiles.push(fullpath);
+					} else if (recursive && fs.lstatSync(fullpath).isDirectory()) {
+						jsonFiles = jsonFiles.concat(...findJson(fullpath, recursive));
+					}
+				});
+			}
+			return jsonFiles;
+		}
+
+		function concatMerge(destinationArray, sourceArray) {
+			return destinationArray.concat(sourceArray);
+		}
+
+		function getChildRoutes(splitedPath, settings) {
+			let currentChild = settings.routes.childRoutes;
+			splitedPath.forEach(path => {
+				if (path !== '') {
+					currentChild.forEach(config => {
+						if (config.path === path) {
+							currentChild = config.childRoutes || [];
+						}
+					});
+				}
+			});
+			return currentChild;
+		}
+
+		function overrideRoutes(path, settings) {
+			const childRoutes = getChildRoutes(path.split('/'), settings);
+			settings.overrideRoutes[path].forEach(config => {
+				childRoutes.push(config);
+			});
+			delete settings.overrideRoutes[path]; // eslint-disable-line no-param-reassign
+		}
+
+		function overrideActions(id, settings) {
+			// eslint-disable-next-line no-param-reassign
+			settings.actions[id] = Object.assign({}, settings.actions[id], settings.overrideActions[id]);
+		}
+
+		function log() {
+			return somethingToLog => {
+				if (!quiet) {
+					console.error(somethingToLog);
+				}
+			};
+		}
+
+		const cmfconfig = (() => {
+			try {
+				return require(pathLib.join(process.cwd(), 'cmf.json')); // eslint-disable-line
+			} catch (e) {
+				console.error('cmf.json file is required to run this script');
+				process.exit();
+			}
+			return null;
+		})();
+
+		const sources = dev ? cmfconfig.settings['sources-dev'] : cmfconfig.settings.sources;
+		const destination = pathLib.join(process.cwd(), cmfconfig.settings.destination);
+
+		log('\nExtracting configuration from : \n');
+
+		const jsonFiles = sources.reduce(
+			(acc, source) =>
+				acc.concat([...findJson(pathLib.join(process.cwd(), source), recursive)]),
+			[]
+		);
+
+		log(jsonFiles);
+
+		log('\n');
+
+		const configurations = jsonFiles.map(path => require(`${path}`)); // eslint-disable-line global-require
+
+		const settings = deepmerge.all(configurations, {arrayMerge: concatMerge});
+
+		if (settings.overrideRoutes) {
+			Object.keys(settings.overrideRoutes).forEach(route => {
+				overrideRoutes(route, settings);
+			});
+		}
+		if (settings.overrideActions) {
+			Object.keys(settings.overrideActions).forEach(id => {
+				overrideActions(id, settings);
+			});
+		}
+
+		log(`Merge to ${destination}`);
+		mkdirp(pathLib.dirname(destination), () => {
+			fs.writeFile(destination, JSON.stringify(settings), () => {
+				log('Merged');
+			});
+		});
+
+		callback();
+	});
+};
+
+module.exports = ReactCMFWebpackPlugin;
