f09b310a7bccb5edb516747d3194c24e647bdbfd
travis <travis>
2018-02-20 15:23:24 +0000

    test(ci): prettier

--- packages/cmf/src/actions/http.js
+++ packages/cmf/src/actions/http.js
@@ -27,7 +27,7 @@ function onRequest(url, config) {
 }
 
 function onJSError(error, action) {
-	console.error(error);  // eslint-disable-line no-console
+	console.error(error); // eslint-disable-line no-console
 	return {
 		type: ACTION_TYPE_HTTP_REDUCER_ERROR,
 		error,
