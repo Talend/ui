29b5171bd969baa61454280e4db9a72f020770db
Fabien Rassinier <frassinier@talend.com>
2018-02-20 13:53:59 +0100

    feat(cmf): add watcher

--- packages/cmf-webpack-plugin/src/index.js
+++ packages/cmf-webpack-plugin/src/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const pathLib = require('path');
 const mkdirp = require('mkdirp');
+const chokidar = require('chokidar');
 const deepmerge = require('deepmerge');
 
 /**
@@ -12,97 +13,117 @@ function ReactCMFWebpackPlugin(options = {}) {
 	this.options = options;
 }
 
-ReactCMFWebpackPlugin.prototype.apply = function(compiler) {
+ReactCMFWebpackPlugin.prototype.apply = function (compiler) {
 	const {
 		dev,
 		quiet,
 		recursive,
 	} = this.options;
 
-	compiler.plugin('emit', function(compilation, callback) {
-		function findJson(fileOrFolder, recursive) {
-			let jsonFiles = [];
-			if (fileOrFolder.endsWith('.json')) {
-				jsonFiles.push(fileOrFolder);
-			} else {
-				fs.readdirSync(fileOrFolder).forEach(path => {
-					const fullpath = pathLib.join(fileOrFolder, path);
-					if (path.endsWith('.json')) {
-						jsonFiles.push(fullpath);
-					} else if (recursive && fs.lstatSync(fullpath).isDirectory()) {
-						jsonFiles = jsonFiles.concat(...findJson(fullpath, recursive));
+	let jsonFiles;
+
+	function findJson(fileOrFolder, recursive) {
+		let jsonFiles = [];
+		if (fileOrFolder.endsWith('.json')) {
+			jsonFiles.push(fileOrFolder);
+		} else {
+			fs.readdirSync(fileOrFolder).forEach(path => {
+				const fullpath = pathLib.join(fileOrFolder, path);
+				if (path.endsWith('.json')) {
+					jsonFiles.push(fullpath);
+				} else if (recursive && fs.lstatSync(fullpath).isDirectory()) {
+					jsonFiles = jsonFiles.concat(...findJson(fullpath, recursive));
+				}
+			});
+		}
+		return jsonFiles;
+	}
+
+	function concatMerge(destinationArray, sourceArray) {
+		return destinationArray.concat(sourceArray);
+	}
+
+	function getChildRoutes(splitedPath, settings) {
+		let currentChild = settings.routes.childRoutes;
+		splitedPath.forEach(path => {
+			if (path !== '') {
+				currentChild.forEach(config => {
+					if (config.path === path) {
+						currentChild = config.childRoutes || [];
 					}
 				});
 			}
-			return jsonFiles;
+		});
+		return currentChild;
+	}
+
+	function overrideRoutes(path, settings) {
+		const childRoutes = getChildRoutes(path.split('/'), settings);
+		settings.overrideRoutes[path].forEach(config => {
+			childRoutes.push(config);
+		});
+		delete settings.overrideRoutes[path]; // eslint-disable-line no-param-reassign
+	}
+
+	function overrideActions(id, settings) {
+		// eslint-disable-next-line no-param-reassign
+		settings.actions[id] = Object.assign({}, settings.actions[id], settings.overrideActions[id]);
+	}
+
+	// function log() {
+	// 	return somethingToLog => {
+	// 		if (!quiet) {
+	// 		    console.error(somethingToLog);
+	// 		}
+	// 	};
+	// }
+
+	compiler.plugin('emit', function (compilation, callback) {
+		var filelist = 'In this build:\n\n';
+
+		// Loop through all compiled assets,
+		// adding a new line item for each filename.
+		for (var filename in compilation.chunks) {
+			filelist += ('- '+ filename +'\n');
 		}
 
-		function concatMerge(destinationArray, sourceArray) {
-			return destinationArray.concat(sourceArray);
-		}
-
-		function getChildRoutes(splitedPath, settings) {
-			let currentChild = settings.routes.childRoutes;
-			splitedPath.forEach(path => {
-				if (path !== '') {
-					currentChild.forEach(config => {
-						if (config.path === path) {
-							currentChild = config.childRoutes || [];
-						}
-					});
-				}
-			});
-			return currentChild;
-		}
-
-		function overrideRoutes(path, settings) {
-			const childRoutes = getChildRoutes(path.split('/'), settings);
-			settings.overrideRoutes[path].forEach(config => {
-				childRoutes.push(config);
-			});
-			delete settings.overrideRoutes[path]; // eslint-disable-line no-param-reassign
-		}
-
-		function overrideActions(id, settings) {
-			// eslint-disable-next-line no-param-reassign
-			settings.actions[id] = Object.assign({}, settings.actions[id], settings.overrideActions[id]);
-		}
-
-		function log() {
-			return somethingToLog => {
-				if (!quiet) {
-					console.error(somethingToLog);
-				}
-			};
-		}
-
-		const cmfconfig = (() => {
-			try {
-				return require(pathLib.join(process.cwd(), 'cmf.json')); // eslint-disable-line
-			} catch (e) {
-				console.error('cmf.json file is required to run this script');
-				process.exit();
+		// Insert this list into the Webpack build as a new file asset:
+		compilation.assets['filelist.md'] = {
+			source: function() {
+				return filelist;
+			},
+			size: function() {
+				return filelist.length;
 			}
-			return null;
-		})();
+		};
+
+		let cmfconfig;
+		try {
+			cmfconfig = require(pathLib.join(process.cwd(), 'cmf.json')); // eslint-disable-line
+		} catch (e) {
+			console.error('cmf.json file is required to run this script');
+			process.exit();
+		}
 
 		const sources = dev ? cmfconfig.settings['sources-dev'] : cmfconfig.settings.sources;
+
 		const destination = pathLib.join(process.cwd(), cmfconfig.settings.destination);
 
-		log('\nExtracting configuration from : \n');
-
-		const jsonFiles = sources.reduce(
+		jsonFiles = sources.reduce(
 			(acc, source) => acc.concat([...findJson(pathLib.join(process.cwd(), source), recursive)]),
 			[],
 		);
 
-		log(jsonFiles);
+		console.error('Extracting configuration from:', jsonFiles);
 
-		log('\n');
+		const configurations = jsonFiles.map(path => {
+			delete require.cache[require.resolve(`${path}`)];
+			return require(`${path}`)
+		}); // eslint-disable-line global-require
 
-		const configurations = jsonFiles.map(path => require(`${path}`)); // eslint-disable-line global-require
+		console.error(JSON.stringify(configurations[0].debug));
 
-		const settings = deepmerge.all(configurations, { arrayMerge: concatMerge });
+		const settings = deepmerge.all(configurations, {arrayMerge: concatMerge});
 
 		if (settings.overrideRoutes) {
 			Object.keys(settings.overrideRoutes).forEach(route => {
@@ -115,14 +136,94 @@ ReactCMFWebpackPlugin.prototype.apply = function(compiler) {
 			});
 		}
 
-		log(`Merge to ${destination}`);
+		console.error(`Merge to ${destination}`);
 		mkdirp(pathLib.dirname(destination), () => {
 			fs.writeFile(destination, JSON.stringify(settings), () => {
-				log('Merged');
+				console.error('Merged');
 			});
 		});
 
 		callback();
+	});
+
+	compiler.plugin('done', function (compilation) {
+
+		const watcher = chokidar.watch(jsonFiles, {
+			persistent: true,
+			ignored: false,
+			ignoreInitial: false,
+			followSymlinks: true,
+			cwd: '.',
+			disableGlobbing: false,
+			usePolling: true,
+			interval: 100,
+			binaryInterval: 300,
+			alwaysStat: false,
+			depth: 99,
+			awaitWriteFinish: {
+				stabilityThreshold: 2000,
+				pollInterval: 100
+			},
+			ignorePermissionErrors: false,
+			atomic: true
+		});
+
+		watcher
+			.on(
+				'add',
+				function (path) {
+					return null;
+				}
+			)
+			.on(
+				'change',
+				function (path) {
+					console.error(`\n\n Compilation Started  after change of - ${path} \n\n`);
+					compiler.run(function (err) {
+						if (err) throw err;
+						watcher.close();
+					});
+					console.error(`\n\n Compilation ended  for change of - ${path} \n\n`);
+				}
+			)
+			.on(
+				'unlink',
+				function (path) {
+					console.error(`File ${path} has been removed`);
+				}
+			);
+
+		watcher
+			.on(
+				'addDir',
+				function (path) {
+					console.error(`Directory ${path} has been added`);
+				}
+			)
+			.on(
+				'unlinkDir',
+				function (path) {
+					console.error(`Directory ${path} has been removed`);
+				}
+			)
+			.on(
+				'error',
+				function (error) {
+					console.error(`Watcher error: ${error}`);
+				}
+			)
+			.on(
+				'ready',
+				function () {
+					console.error('Initial scan complete. Ready for changes');
+				}
+			)
+			.on(
+				'raw',
+				function (event, path, details) {
+					return null;
+				}
+			);
 	});
 };
 
