6b3c9bff510c510fffcd779221d0b849024c368a
Jean-Alain Ré <jare@talend.com>
2018-02-22 17:36:00 +0100

    ignore falsy to show the quality indicator

--- packages/datagrid/src/components/default-header-renderer/quality-bar.component.js
+++ packages/datagrid/src/components/default-header-renderer/quality-bar.component.js
@@ -9,21 +9,21 @@ const QUALITY_BAR_MIN_WIDTH = 5;
 export default function QualityBar(props) {
 	return (
 		<div className={classNames(theme['td-quality-bar'], 'td-quality-bar')}>
-			{props.invalid > 0 && (
+			{!!props.invalid && (
 				<span
 					className={classNames(theme['td-quality-bar-invalid'], 'td-quality-bar-invalid')}
 					style={{ width: `${props.invalid}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
 					title={`${props.invalid}%`}
 				/>
 			)}
-			{props.empty > 0 && (
+			{!!props.empty && (
 				<span
 					className={classNames(theme['td-quality-bar-empty'], 'td-quality-bar-empty')}
 					style={{ width: `${props.empty}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
 					title={`${props.empty}%`}
 				/>
 			)}
-			{props.valid > 0 && (
+			{!!props.valid && (
 				<span
 					className={classNames(theme['td-quality-bar-valid'], 'td-quality-bar-valid')}
 					style={{ width: `${props.valid}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
