6f08055747f973745b239f57f9e397bdae96e4b6
Fabien Rassinier <frassinier@talend.com>
2018-02-26 17:07:50 +0100

    fix(cmf): lock management

--- packages/cmf-webpack-plugin/src/index.js
+++ packages/cmf-webpack-plugin/src/index.js
@@ -28,6 +28,7 @@ const chokidarOptions = {
  * @constructor
  */
 function ReactCMFWebpackPlugin(options = {}) {
+	this._canRun = true;
 	this._lastRun;
 	this._lastWatch;
 	this._modifiedFiles = [];
@@ -47,12 +48,14 @@ ReactCMFWebpackPlugin.prototype.apply = function (compiler) {
 
 	compiler.plugin('emit', (compilation, callback) => {
 		this._log('emit', { lastRun: this._lastRun, lastWatch: this._lastWatch });
-		if (this._lastRun && this._lastWatch && this._lastRun > this._lastWatch) return;
+		if (!this._canRun || (this._lastRun && this._lastWatch && this._lastRun > this._lastWatch)) return;
+		this._canRun = false;
 		this._lastRun = new Date();
 		const startTime = Date.now();
 		this._modifiedFiles = mergeSettings(this._options, this._log, () => {
 			const endTime = Date.now();
 			this._log(`Files merged in ${(((endTime - startTime) % 60000) / 1000)}s`);
+			this._canRun = true;
 			callback();
 		}, callback);
 	});
@@ -60,7 +63,7 @@ ReactCMFWebpackPlugin.prototype.apply = function (compiler) {
 	if (this._options.watch) {
 		compiler.plugin('done', () => {
 			this._log('done', { lastRun: this._lastRun, lastWatch: this._lastWatch });
-			if (this._lastRun && this._lastWatch && this._lastWatch > this._lastRun) return;
+			if (!this._canRun || (this._lastRun && this._lastWatch && this._lastWatch > this._lastRun)) return;
 			this._lastWatch = new Date();
 			const watcher = chokidar.watch(this._modifiedFiles, chokidarOptions);
 			const run = () => {
