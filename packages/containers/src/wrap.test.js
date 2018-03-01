5269d5fdf43ec0c0fcd7466b80ee60895da28548
travis <travis>
2018-03-01 10:11:42 +0000

    test(ci): prettier

--- packages/containers/src/wrap.test.js
+++ packages/containers/src/wrap.test.js
@@ -11,15 +11,12 @@ describe('wrap', () => {
 	it('should create a component', () => {
 		const WrappedButton = wrap(Button, 'MyButton');
 		expect(WrappedButton.displayName).toBe('Connect(CMF(MyButton))');
-		const wrapper = mount(
-			<WrappedButton text="hello" />,
-			{
-				context,
-				childContextTypes: {
-					registry: React.PropTypes.object,
-				},
-			}
-		);
+		const wrapper = mount(<WrappedButton text="hello" />, {
+			context,
+			childContextTypes: {
+				registry: React.PropTypes.object,
+			},
+		});
 		expect(wrapper.find('button').props().children[1]).toBe('hello');
 	});
 });
