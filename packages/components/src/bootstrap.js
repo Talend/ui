b6f3d8122ffeb003339e5a73d494ee4d82f648db
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-03-03 09:07:54 +0100

    chore: fix lint

--- packages/components/src/bootstrap.js
+++ packages/components/src/bootstrap.js
@@ -3,6 +3,7 @@ import wrap from './wrap';
 
 const bootrapWrapped = Object.keys(bootstrap).reduce((acc, key) => {
 	if (bootstrap[key]) {
+		// eslint-disable-next-line no-param-reassign
 		acc[key] = wrap(bootstrap[key], key);
 	}
 	return acc;
