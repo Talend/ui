23e85863f6f80ae106f35a2ba545a12677aa1dbb
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-03-03 14:31:58 +0100

    test: remove the need of cmf

--- packages/components/src/wrap.test.js
+++ packages/components/src/wrap.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { mount } from 'enzyme';
-import mock from '@talend/react-cmf/lib/mock';
 import wrap from './wrap';
 
 describe('wrap', () => {
@@ -12,17 +11,10 @@ describe('wrap', () => {
 	};
 	Button.displayName = 'Button';
 	Button.foo = 'bar';
-	const context = mock.context();
-	context.registry = {};
 	it('should create a component', () => {
 		const WrappedButton = wrap(Button, 'MyButton');
-		expect(WrappedButton.displayName).toBe('Connect(CMF(MyButton))');
-		const wrapper = mount(<WrappedButton text="hello" />, {
-			context,
-			childContextTypes: {
-				registry: React.PropTypes.object,
-			},
-		});
+		expect(WrappedButton.displayName).toBe('MyButton');
+		const wrapper = mount(<WrappedButton text="hello" />);
 		expect(wrapper.find('button').props().children[1]).toBe('hello');
 	});
 
