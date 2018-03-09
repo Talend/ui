919bdd913ac5701e3ed2c105a693959dfa6c5a18
Jimmy Somsanith <jsomsanith@talend.com>
2018-03-09 14:43:32 +0100

    Revert "chore(e2e): ci execute e2e tests on browserstack (#1155)"
    This reverts commit 2dbe439724e15ed5e68c7bb22a681cf2cb893dbd.

--- packages/components/e2e/component-objects/src/test/java/org/talend/component/StorybookTest.java
+++ packages/components/e2e/component-objects/src/test/java/org/talend/component/StorybookTest.java
@@ -1,20 +1,26 @@
 package org.talend.component;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.utils.URIBuilder;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.talend.config.WebDriverTest;
-import org.talend.config.WebDriverTestFactory;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+import java.net.URLEncoder;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 public class StorybookTest {
-    private static final Logger LOGGER = LogManager.getLogger(StorybookTest.class);
 
     private static final String ACTION_LOGGER_CONSOLE_SELECTOR = ".horizontal.Pane2 > div > div > div:last-child";
+
+    private static final String STORY_CATEGORY_SELECTOR = "div[data-name='%s']";
+
+    private static final String STORY_MENU_SELECTOR = "a[data-name='%s']";
 
     private static final String DEFAULT_STORY_NAME = "default";
 
@@ -22,23 +28,17 @@ public class StorybookTest {
 
     private static final int STORYBOOK_PORT = 6006;
 
-    private static final String STORYBOOK_CONTEXT = "";
-
-    private static WebDriverTest webDriverTestConfiguration;
-
     protected static WebDriver driver;
 
     @BeforeClass
-    public static void before() throws Exception {
-        webDriverTestConfiguration = new WebDriverTestFactory().getWebDriverTestConfiguration();
-        webDriverTestConfiguration.setUp();
-        driver = webDriverTestConfiguration.driver;
-        LOGGER.info("WebDriver is running: " + driver);
+    public static void before() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @AfterClass
-    public static void after() throws Exception {
-        webDriverTestConfiguration.tearDown();
+    public static void after() {
+        driver.close();
     }
 
     protected void goToStory(final String categoryName) {
@@ -52,18 +52,9 @@ public class StorybookTest {
             URIBuilder builder = new URIBuilder();
 
             builder.setScheme("http");
-            final String storybookHost = System.getProperty("storybook.host", STORYBOOK_HOST);
-            builder.setHost(storybookHost);
-            final String storybookPort = System.getProperty("storybook.port");
-            if (StringUtils.isNotBlank(storybookPort)) {
-                builder.setPort(Integer.parseInt(storybookPort));
-            } else {
-                builder.setPort(STORYBOOK_PORT);
-            }
-            final String storybookContext = System.getProperty("storybook.context", STORYBOOK_CONTEXT);
-            if (StringUtils.isNotBlank(storybookContext)) {
-                builder.setPath(storybookContext);
-            }
+            builder.setHost(STORYBOOK_HOST);
+            builder.setPort(STORYBOOK_PORT);
+            builder.setPath("/");
             builder.addParameter("selectedKind", categoryName);
             builder.addParameter("selectedStory", storyName);
 
@@ -75,11 +66,11 @@ public class StorybookTest {
         this.goToStoryFrame();
     }
 
-    private void goToStoryFrame() {
+    protected void goToStoryFrame() {
         driver.switchTo().frame(driver.findElement(By.id("storybook-preview-iframe")));
     }
 
-    private void goToMainElement() {
+    protected void goToMainElement() {
         driver.switchTo().defaultContent();
     }
 
