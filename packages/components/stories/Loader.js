8ba2ec82c7eab418b3758aefdf3d3e426a2bb820
yatheesh <ybaskaran@talend.com>
2018-03-13 15:26:35 +0100

    feat(theme): css app loader for the application launch page

--- packages/components/stories/Loader.js
+++ packages/components/stories/Loader.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import theme from '../src/Loader/Loader.scss';
 
 import { Loader, CIRCULAR_PROGRESS_SIZE as SIZE } from '../src/';
-
+import { TALEND_T7_THEME_APPS as apps } from '../src/Layout/constants';
+import classNames from 'classnames';
+import appLoaderTheme from '@talend/bootstrap-theme/src/theme/_loader.scss';
 
 const containerStyle = {
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	height: '100vh',
-	width: '100vw',
+  alignItems: 'center',
+  justifyContent: 'center',
 };
-
 
 const decoratedStories = storiesOf('Loader', module)
 	.addDecorator(story => (
-		<div style={containerStyle}>
+		<div style={containerStyle} className={classNames(theme['tc-loader-container'])} >
 			{story()}
 		</div>
 	));
@@ -24,4 +22,16 @@ const decoratedStories = storiesOf('Loader', module)
 decoratedStories
 	.addWithInfo('default', () => <Loader />)
 	.addWithInfo('small', () => <Loader size={SIZE.small} />)
-	.addWithInfo('large', () => <Loader size={SIZE.large} />);
+	.addWithInfo('large', () => <Loader size={SIZE.large} />)
+	.addWithInfo('app loader', () =>
+		<div data-app-icon='tdp' className={appLoaderTheme['tc-app-icon']}>
+			<div className={appLoaderTheme['tc-app-loader']}><div></div></div>
+  </div>);
+
+	apps.forEach(app => {
+		decoratedStories.addWithInfo(`[${app.toUpperCase()}] app loader`, () => (
+			<div data-app-icon={app} className={appLoaderTheme['tc-app-icon']}>
+				<div className={appLoaderTheme['tc-app-loader']}><div></div></div>
+			</div>
+		));
+	});
