97fa37c9cb0d6132860a8f11f1af6fa69159e1b7
travis <travis>
2018-03-19 11:17:46 +0000

    test(ci): prettier

--- packages/forms/src/widgets/MultiSelectTagWidget/MultiSelectTagWidget.js
+++ packages/forms/src/widgets/MultiSelectTagWidget/MultiSelectTagWidget.js
@@ -6,8 +6,8 @@ import Typeahead from '@talend/react-components/lib/Typeahead';
 import classNames from 'classnames';
 import keycode from 'keycode';
 import theme from './MultiSelectTagWidget.scss';
-import I18N_DOMAIN_FORMS from "../../constants";
-import {DEFAULT_I18N} from "../../translate";
+import I18N_DOMAIN_FORMS from '../../constants';
+import { DEFAULT_I18N } from '../../translate';
 
 const DROP_DOWN_ITEM_HEIGHT = 49;
 
