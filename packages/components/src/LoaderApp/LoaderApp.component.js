4672586b32c72d4addfb653726e15036bc466843
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-03-19 16:55:20 +0100

    fix: add aria

--- packages/components/src/LoaderApp/LoaderApp.component.js
+++ packages/components/src/LoaderApp/LoaderApp.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function LoaderApp() {
 	return (
-		<div className="tc-loader-app-container">
+		<div className="tc-loader-app-container" aria-atomic="true" aria-busy="true">
 			<div className="tc-loader-app">
 				<div className="tc-loader-app-icon" />
 			</div>
