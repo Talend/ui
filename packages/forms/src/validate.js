569b537f41490ee78c40856db1007fcfe0a2b291
sgendre <sgendre@talend.com>
2018-03-20 17:34:08 +0100

    default format initialization

--- packages/forms/src/validate.js
+++ packages/forms/src/validate.js
@@ -1,9 +1,31 @@
 import Ajv from 'ajv';
+import tv4 from 'tv4';
 
-export default function validate(jsonSchema, data) {
+let isInitialized = false;
+
+function initialize(validatorName) {
+	if (!isInitialized && validatorName === 'tv4') {
+		tv4.addFormat('no-leading-trailing-space', fieldData => {
+			if (typeof fieldData === 'string' && /^\s|\s$/.test(fieldData)) {
+				return 'must be string without leading or trailing space';
+			}
+
+			return null;
+		});
+	}
+	isInitialized = true;
+}
+
+export default function validate(jsonSchema, data, { validatorName }) {
 	if (!data || !jsonSchema) {
 		return false;
 	}
+	initialize(validatorName);
+
+	if (validatorName === 'tv4') {
+		return tv4.validate(data, jsonSchema);
+	}
+
 	const validator = new Ajv().compile(jsonSchema);
 	return validator && validator(data);
 }
