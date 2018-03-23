d8012405ee86901e09277a380557449fb931353e
travis <travis>
2018-03-23 10:40:19 +0000

    test(ci): prettier

--- packages/cmf/src/sagas/component.js
+++ packages/cmf/src/sagas/component.js
@@ -27,7 +27,10 @@ export const registerMany = registry.getRegisterMany(register);
 export function* onSagaStart(action) {
 	const saga = get(action.saga);
 	if (!saga) {
-		invariant(process.env.NODE_ENV === 'production', `You cannot register undefined as saga for id "${action.saga}"`);
+		invariant(
+			process.env.NODE_ENV === 'production',
+			`You cannot register undefined as saga for id "${action.saga}"`,
+		);
 	} else {
 		const task = yield fork(saga, action.props);
 		yield take(`${CONST.WILL_UNMOUNT_SAGA_STOP}_${action.saga}`);
