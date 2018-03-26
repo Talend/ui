c566f6ca1a69a600d486a6f167dfad68e9a8bdfd
Jimmy Somsanith <jsomsanith@talend.com>
2018-03-26 14:53:34 +0200

    fix: ActionDropdown storybook

--- packages/components/stories/ActionDropdown.js
+++ packages/components/stories/ActionDropdown.js
@@ -39,7 +39,7 @@ const withComponents = {
 		} else if (key === 'FilterBar') {
 			return FilterBar;
 		}
-		return Action;
+		throw new Error('Component not found');
 	},
 	components: {
 		itemsDropdown: [
@@ -67,7 +67,7 @@ const mixItemsComponents = {
 		if (key === 'Action') {
 			return Action;
 		}
-		return Action;
+		throw new Error('Component not found');
 	},
 	items: [
 		{
