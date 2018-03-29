d16fc3500148e2f1e89bb2ff9cd1d59ea530a393
Jean-Alain Ré <jare@talend.com>
2018-03-29 14:31:31 +0200

    fix(datagrid): show the total invalid/valid/empty values

--- packages/datagrid/src/components/DefaultHeaderRenderer/QualityBar.component.js
+++ packages/datagrid/src/components/DefaultHeaderRenderer/QualityBar.component.js
@@ -9,25 +9,25 @@ const QUALITY_BAR_MIN_WIDTH = 5;
 export default function QualityBar(props) {
 	return (
 		<div className={classNames(theme['td-quality-bar'], 'td-quality-bar')}>
-			{!!props.invalid && (
+			{!!props.invalid.total && (
 				<span
 					className={classNames(theme['td-quality-bar-invalid'], 'td-quality-bar-invalid')}
-					style={{ width: `${props.invalid}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
-					title={`${props.invalid}%`}
+					style={{ width: `${props.invalid.percentage}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
+					title={`${props.invalid.total} invalid values (${props.invalid.percentage}%)`}
 				/>
 			)}
-			{!!props.empty && (
+			{!!props.empty.total && (
 				<span
 					className={classNames(theme['td-quality-bar-empty'], 'td-quality-bar-empty')}
-					style={{ width: `${props.empty}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
-					title={`${props.empty}%`}
+					style={{ width: `${props.empty.percentage}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
+					title={`${props.empty.total} empty values (${props.empty.percentage}%)`}
 				/>
 			)}
-			{!!props.valid && (
+			{!!props.valid.total && (
 				<span
 					className={classNames(theme['td-quality-bar-valid'], 'td-quality-bar-valid')}
-					style={{ width: `${props.valid}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
-					title={`${props.valid}%`}
+					style={{ width: `${props.valid.percentage}%`, minWidth: `${QUALITY_BAR_MIN_WIDTH}%` }}
+					title={`${props.valid.total} valid values (${props.valid.percentage}%)`}
 				/>
 			)}
 		</div>
