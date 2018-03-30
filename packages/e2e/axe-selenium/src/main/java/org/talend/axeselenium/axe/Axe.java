6eb93c112a50ebe15b5f73c4698595b78c5a74f1
Jimmy Somsanith <jsomsanith@talend.com>
2018-03-30 11:16:16 +0200

    Options

--- packages/e2e/axe-selenium/src/main/java/org/talend/axeselenium/axe/Axe.java
+++ packages/e2e/axe-selenium/src/main/java/org/talend/axeselenium/axe/Axe.java
@@ -1,20 +1,41 @@
 package org.talend.axeselenium.axe;
 
+import org.json.JSONObject;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.talend.axeselenium.Script;
 
-import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Axe {
     private WebDriver driver;
-    private final URL axeScript;
+    private Script script;
 
     public Axe(final WebDriver driver) {
         this.driver = driver;
-        this.axeScript = Axe.class.getResource("/axe.min.js");
+        this.script = new Script(driver, Axe.class.getResource("/axe.min.js"));
     }
 
-    public void run(final WebElement element, final Options options) {
+    public JSONObject runWCAG2a(final WebElement element) {
+        final Options options = Options.builder().runOnly("tags", "wcag2a").build();
+        return run(element, options);
+    }
 
+    public JSONObject run(final WebElement element, final Options options) {
+        this.driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+        this.script.runAxeScript();
+
+        final String command =
+                "cont done = arguments[arguments.length - 1];\n" +
+                "function handleResult(error, report) { if (error) { throw error; } return done(report); }\n" +
+                "const targetElement = arguments[0] || document;" +
+                "const options = arguments[1];" +
+                "axe.run(targetElement, options, handleResult)";
+        final String axeOptions = options == null ? "{}" : options.toString();
+        final Object response = ((JavascriptExecutor) this.driver).executeAsyncScript(command, element, axeOptions);
+
+        return new JSONObject((Map) response);
     }
 }
--- packages/e2e/axe-selenium/src/main/java/org/talend/axeselenium/axe/Options.java
+++ packages/e2e/axe-selenium/src/main/java/org/talend/axeselenium/axe/Options.java
@@ -1,4 +1,85 @@
 package org.talend.axeselenium.axe;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Options {
+    private String runOnlyType;
+    private String[] runOnlyValues;
+
+    private List<String> rulesToEnable;
+    private List<String> rulesToDisable;
+
+    private Options(final String runOnlyType, final String[] runOnlyValues, final List<String> rulesToEnable, final List<String> rulesToDisable) {
+        this.runOnlyType = runOnlyType;
+        this.runOnlyValues = runOnlyValues;
+        this.rulesToEnable = rulesToEnable;
+        this.rulesToDisable = rulesToDisable;
+    }
+
+    public JSONObject toJSONObject() {
+        final JSONObject jsonObject = new JSONObject();
+
+        if (runOnlyType!= null && runOnlyValues != null) {
+            final JSONObject runOnly = new JSONObject();
+            runOnly.put("type", runOnlyType);
+            runOnly.put("values", new JSONArray(runOnlyValues));
+            jsonObject.put("runOnly", runOnly);
+        }
+
+        if (rulesToDisable.size() > 0 || rulesToEnable.size() > 0) {
+            final JSONObject enabled = new JSONObject();
+            enabled.put("enabled", true);
+
+            final JSONObject disabled = new JSONObject();
+            enabled.put("enabled", false);
+
+            final JSONObject rules = new JSONObject();
+            rulesToEnable.forEach(nextRule -> rules.put(nextRule, enabled));
+            rulesToDisable.forEach(nextRule -> rules.put(nextRule, disabled));
+
+            jsonObject.put("rules", rules);
+        }
+
+        return jsonObject;
+    }
+
+    public String toString() {
+        return toJSONObject().toString();
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private static class Builder {
+        private String runOnlyType;
+        private String[] runOnlyValues;
+
+        private List<String> rulesToEnable = new ArrayList<>();
+        private List<String> rulesToDisable = new ArrayList<>();
+
+        public Builder runOnly(final String type, final String... values) {
+            runOnlyType = type;
+            runOnlyValues = values;
+            return this;
+        }
+
+        public Builder enableRule(final String rule) {
+            rulesToEnable.add(rule);
+            return this;
+        }
+
+        public Builder disableRule(final String rule) {
+            rulesToDisable.add(rule);
+            return this;
+        }
+
+        public Options build() {
+            return new Options(runOnlyType, runOnlyValues, rulesToEnable, rulesToDisable);
+        }
+    }
 }
