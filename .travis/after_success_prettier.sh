81183502469c692f1274d8b7daadb3d175fb35b6
Sébastien Romain <sebastien.romain@gmail.com>
2018-04-09 13:33:35 +0200

    let's format json with travis too

--- .travis/after_success_prettier.sh
+++ .travis/after_success_prettier.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
 	modified_files=($(git diff --name-only HEAD $(git merge-base HEAD master)))
 
 	for i in ${modified_files[@]}; do
-		if [[ $i == *.js || $i == *.scss ]]; then
+		if [[ $i == *.js || $i == *.scss || $i == *.json ]]; then
 			prettier --config .prettierrc ${i} --write
 		fi
 	done
