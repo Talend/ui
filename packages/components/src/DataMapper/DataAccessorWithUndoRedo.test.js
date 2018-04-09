1af271151ebff864eb9c643711e7e37f51bf5f50
imbault <timbault@talend.com>
2018-04-09 11:54:41 +0200

    feat(components): data mapper: - fix unit tests

--- packages/components/src/DataMapper/DataAccessorWithUndoRedo.test.js
+++ packages/components/src/DataMapper/DataAccessorWithUndoRedo.test.js
@@ -6,30 +6,23 @@ import DataAccessorWithUndoRedo from './DataAccessorWithUndoRedo';
 const dataAccessor = new DataAccessorWithUndoRedo(new DefaultDataAccessor());
 
 it('data-accessor-with-undo-redo', () => {
-	const inputSchema = TestData.schema1;
-	dataAccessor.populateCache(inputSchema, Constants.MappingSide.INPUT);
-	const outputSchema = TestData.schema2;
-	dataAccessor.populateCache(outputSchema, Constants.MappingSide.OUTPUT);
 
-	let mapping = TestData.mapping;
+  const inputSchema = TestData.schema1;
+  dataAccessor.populateCache(inputSchema, Constants.MappingSide.INPUT);
+  const outputSchema = TestData.schema2;
+  dataAccessor.populateCache(outputSchema, Constants.MappingSide.OUTPUT);
 
-	expect(dataAccessor.canUndo()).toBe(false);
-	expect(dataAccessor.canRedo()).toBe(false);
+  let mapping = TestData.mapping;
 
-	mapping = dataAccessor.addMapping(mapping, TestData.element3, TestData.element7);
-	expect(dataAccessor.canUndo()).toBe(true);
-	expect(dataAccessor.canRedo()).toBe(false);
+  expect(dataAccessor.canUndo()).toBe(false);
+  expect(dataAccessor.canRedo()).toBe(false);
 
-	expect(dataAccessor.getUndoLabel()).toBe(
-		`Undo "Map ${TestData.element3.name} to ${TestData.element7.name}"`,
-	);
+  mapping = dataAccessor.addMapping(mapping, TestData.element3, TestData.element7);
+  expect(dataAccessor.canUndo()).toBe(true);
+  expect(dataAccessor.canRedo()).toBe(false);
 
-	let cmd = dataAccessor.getACopyOfUndoCommand();
-	expect(cmd.code).toBe(Constants.Commands.ADD_MAPPING);
-	let source = dataAccessor.getSchemaElementFromId(inputSchema, cmd.sourceId);
-	expect(dataAccessor.areEquals(source, TestData.element3)).toBe(true);
-	let target = dataAccessor.getSchemaElementFromId(outputSchema, cmd.targetId);
-	expect(dataAccessor.areEquals(target, TestData.element7)).toBe(true);
+  expect(dataAccessor.getUndoLabel())
+    .toBe(`Undo "Map ${TestData.element3.name} to ${TestData.element7.name}"`);
 
   let cmd = dataAccessor.getACopyOfUndoCommand();
   expect(cmd.code).toBe(Constants.Commands.ADD_MAPPING);
@@ -38,31 +31,26 @@ it('data-accessor-with-undo-redo', () => {
   let target = dataAccessor.getSchemaElementFromId(outputSchema, cmd.targetId);
   expect(dataAccessor.areElementsEqual(target, TestData.element7)).toBe(true);
 
-	expect(dataAccessor.getRedoLabel()).toBe(
-		`Redo "Map ${TestData.element3.name} to ${TestData.element7.name}"`,
-	);
+  mapping = dataAccessor.undo(mapping);
+  expect(dataAccessor.canUndo()).toBe(false);
+  expect(dataAccessor.canRedo()).toBe(true);
 
-	cmd = dataAccessor.getACopyOfRedoCommand();
-	expect(cmd.code).toBe(Constants.Commands.ADD_MAPPING);
+  expect(dataAccessor.getRedoLabel())
+    .toBe(`Redo "Map ${TestData.element3.name} to ${TestData.element7.name}"`);
 
-	mapping = dataAccessor.redo(mapping);
-	expect(dataAccessor.canUndo()).toBe(true);
-	expect(dataAccessor.canRedo()).toBe(false);
+  cmd = dataAccessor.getACopyOfRedoCommand();
+  expect(cmd.code).toBe(Constants.Commands.ADD_MAPPING);
 
-	mapping = dataAccessor.removeMapping(mapping, TestData.element3, TestData.element7);
-	expect(dataAccessor.canUndo()).toBe(true);
-	expect(dataAccessor.canRedo()).toBe(false);
+  mapping = dataAccessor.redo(mapping);
+  expect(dataAccessor.canUndo()).toBe(true);
+  expect(dataAccessor.canRedo()).toBe(false);
 
-	expect(dataAccessor.getUndoLabel()).toBe(
-		`Undo "Clear mapping between ${TestData.element3.name} and ${TestData.element7.name}"`,
-	);
+  mapping = dataAccessor.removeMapping(mapping, TestData.element3, TestData.element7);
+  expect(dataAccessor.canUndo()).toBe(true);
+  expect(dataAccessor.canRedo()).toBe(false);
 
-	cmd = dataAccessor.getACopyOfUndoCommand();
-	expect(cmd.code).toBe(Constants.Commands.REMOVE_MAPPING);
-	source = dataAccessor.getSchemaElementFromId(inputSchema, cmd.sourceId);
-	expect(dataAccessor.areEquals(source, TestData.element3)).toBe(true);
-	target = dataAccessor.getSchemaElementFromId(outputSchema, cmd.targetId);
-	expect(dataAccessor.areEquals(target, TestData.element7)).toBe(true);
+  expect(dataAccessor.getUndoLabel())
+    .toBe(`Undo "Clear mapping between ${TestData.element3.name} and ${TestData.element7.name}"`);
 
   cmd = dataAccessor.getACopyOfUndoCommand();
   expect(cmd.code).toBe(Constants.Commands.REMOVE_MAPPING);
@@ -71,35 +59,39 @@ it('data-accessor-with-undo-redo', () => {
   target = dataAccessor.getSchemaElementFromId(outputSchema, cmd.targetId);
   expect(dataAccessor.areElementsEqual(target, TestData.element7)).toBe(true);
 
-	cmd = dataAccessor.getACopyOfUndoCommand();
-	expect(cmd.code).toBe(Constants.Commands.ADD_MAPPING);
-	cmd = dataAccessor.getACopyOfRedoCommand();
-	expect(cmd.code).toBe(Constants.Commands.REMOVE_MAPPING);
+  mapping = dataAccessor.undo(mapping);
+  expect(dataAccessor.canUndo()).toBe(true);
+  expect(dataAccessor.canRedo()).toBe(true);
 
-	mapping = dataAccessor.clearMapping(mapping);
-	expect(dataAccessor.canUndo()).toBe(true);
-	expect(dataAccessor.canRedo()).toBe(false);
+  cmd = dataAccessor.getACopyOfUndoCommand();
+  expect(cmd.code).toBe(Constants.Commands.ADD_MAPPING);
+  cmd = dataAccessor.getACopyOfRedoCommand();
+  expect(cmd.code).toBe(Constants.Commands.REMOVE_MAPPING);
 
-	expect(dataAccessor.getUndoLabel()).toBe('Undo "Clear all mapping"');
+  mapping = dataAccessor.clearMapping(mapping);
+  expect(dataAccessor.canUndo()).toBe(true);
+  expect(dataAccessor.canRedo()).toBe(false);
 
-	expect(dataAccessor.isMappingEmpty(mapping)).toBe(true);
+  expect(dataAccessor.getUndoLabel()).toBe('Undo "Clear all mapping"');
 
-	cmd = dataAccessor.getACopyOfUndoCommand();
-	expect(cmd.code).toBe(Constants.Commands.CLEAR_MAPPING);
+  expect(dataAccessor.isMappingEmpty(mapping)).toBe(true);
 
-	const storedMapping =
-		'[{"sourceId":"elem_2","targetId":"elem_5"},{"sourceId":"elem_4","targetId":"elem_8"},{"sourceId":"elem_3","targetId":"elem_7"}]';
-	expect(JSON.stringify(cmd.storedMapping)).toBe(storedMapping);
+  cmd = dataAccessor.getACopyOfUndoCommand();
+  expect(cmd.code).toBe(Constants.Commands.CLEAR_MAPPING);
 
-	mapping = dataAccessor.undo(mapping);
-	expect(dataAccessor.canUndo()).toBe(true);
-	expect(dataAccessor.canRedo()).toBe(true);
+  const storedMapping = '[{"sourceId":"elem_2","targetId":"elem_5"},{"sourceId":"elem_4","targetId":"elem_8"},{"sourceId":"elem_3","targetId":"elem_7"}]';
+  expect(JSON.stringify(cmd.storedMapping)).toBe(storedMapping);
 
-	expect(dataAccessor.isMappingEmpty(mapping)).toBe(false);
+  mapping = dataAccessor.undo(mapping);
+  expect(dataAccessor.canUndo()).toBe(true);
+  expect(dataAccessor.canRedo()).toBe(true);
 
-	const mappingItems = dataAccessor.getMappingItems(mapping);
-	expect(mappingItems.length).toBe(3);
+  expect(dataAccessor.isMappingEmpty(mapping)).toBe(false);
 
-	cmd = dataAccessor.getACopyOfRedoCommand();
-	expect(cmd.code).toBe(Constants.Commands.CLEAR_MAPPING);
+  const mappingItems = dataAccessor.getMappingItems(mapping);
+  expect(mappingItems.length).toBe(3);
+
+  cmd = dataAccessor.getACopyOfRedoCommand();
+  expect(cmd.code).toBe(Constants.Commands.CLEAR_MAPPING);
+
 });
