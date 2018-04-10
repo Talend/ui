154694c9c25d54c841a37351d7aff0b86000f2b9
travis <travis>
2018-04-10 10:17:55 +0000

    test(ci): prettier

--- packages/components/src/AppLoader/AppLoader.component.js
+++ packages/components/src/AppLoader/AppLoader.component.js
@@ -4,8 +4,8 @@ function AppLoader() {
 	return (
 		<div className="tc-app-loader-container" aria-atomic="true" aria-busy="true">
 			<div className="tc-app-loader">
-        <svg focusable="false" className="tc-app-loader-animate" viewBox="0 0 50 50">
-					<circle className="tc-app-loader-path" r="20" cx="25" cy="25" fill="none"></circle>
+				<svg focusable="false" className="tc-app-loader-animate" viewBox="0 0 50 50">
+					<circle className="tc-app-loader-path" r="20" cx="25" cy="25" fill="none" />
 				</svg>
 				<div className="tc-app-loader-icon" />
 			</div>
