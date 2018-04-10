a451031a56a69e0e18fe06c9cef7d43f29be6c12
Jimmy Somsanith <jsomsanith@talend.com>
2018-04-10 15:51:50 +0200

    Tests on CellTitleActions

--- packages/components/src/VirtualizedList/CellTitle/CellTitleActions.component.js
+++ packages/components/src/VirtualizedList/CellTitle/CellTitleActions.component.js
@@ -1,14 +1,25 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
+import { translate } from 'react-i18next';
 import { Actions, ActionDropdown } from '../../Actions';
 import { cellTitleDisplayModes } from '../utils/constants';
+import { DEFAULT_I18N } from '../../translate';
+import I18N_DOMAIN_COMPONENTS from '../../constants';
 
 import theme from './CellTitleActions.scss';
 
 const { TITLE_MODE_INPUT, TITLE_MODE_TEXT } = cellTitleDisplayModes;
 
-function CellTitleActions({ rowData, actionsKey, displayMode, persistentActionsKey, id }) {
+export function CellTitleActionsComponent(
+	{
+		rowData,
+		actionsKey,
+		displayMode,
+		persistentActionsKey,
+		id,
+		t,
+	}) {
 	const actions = [];
 
 	if (displayMode === TITLE_MODE_TEXT) {
@@ -24,7 +35,7 @@ function CellTitleActions({ rowData, actionsKey, displayMode, persistentActionsK
 					id={id}
 					className={classNames('cell-title-actions-menu', theme['cell-title-actions-menu'])}
 					items={actionDefinitions}
-					label={'TODO translate me : Open menu'}
+					label={t('LIST_OPEN_ACTION_MENU', { defaultValue: 'Open menu' })}
 					hideLabel
 					link
 					noCaret
@@ -55,8 +66,8 @@ function CellTitleActions({ rowData, actionsKey, displayMode, persistentActionsK
 	);
 }
 
-CellTitleActions.displayName = 'VirtualizedList(CellTitleActions)';
-CellTitleActions.propTypes = {
+CellTitleActionsComponent.displayName = 'VirtualizedList(CellTitleActions)';
+CellTitleActionsComponent.propTypes = {
 	id: PropTypes.string,
 	// The actions property key. Actions = props.rowData[props.actionsKey]
 	actionsKey: PropTypes.string,
@@ -65,7 +76,8 @@ CellTitleActions.propTypes = {
 	/** The display mode. */
 	displayMode: PropTypes.oneOf([TITLE_MODE_TEXT, TITLE_MODE_INPUT]),
 	// The collection item.
-	rowData: PropTypes.object, // eslint-disable-line
+	rowData: PropTypes.object,
+	t: PropTypes.func,
 };
 
-export default CellTitleActions;
+export default translate(I18N_DOMAIN_COMPONENTS, { i18n: DEFAULT_I18N })(CellTitleActionsComponent);
