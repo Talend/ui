3e797a9945018d439a35dab44c14e3be834a8bca
sgendre <sgendre@talend.com>
2018-04-13 12:42:00 +0200

    add DELET UT

--- packages/cmf/__tests__/sagas/http.test.js
+++ packages/cmf/__tests__/sagas/http.test.js
@@ -266,6 +266,40 @@ describe('#httpFetch', () => {
 		).toEqual(call(httpFetch, url, config, HTTP_METHODS.PUT, payload));
 		expect(gen.next().done).toBe(true);
 	});
+	it('should wrap the DELETE request and an undefined payload', () => {
+		const url = '/foo';
+		const config = {
+			'Content-Type': 'application/json',
+		};
+
+		const gen = wrapFetch(url, config, HTTP_METHODS.DELETE);
+
+		expect(
+			gen.next({
+				data: { ok: true },
+			}).value,
+		).toEqual(call(httpFetch, url, config, HTTP_METHODS.DELETE, undefined));
+		expect(gen.next().done).toBe(true);
+	});
+
+	it('should wrap the DELETE request when options are given', () => {
+		const url = '/foo';
+		const config = {
+			'Content-Type': 'application/json',
+		};
+		const options = {
+			aCmfOption: true,
+		};
+
+		const gen = wrapFetch(url, config, HTTP_METHODS.DELETE, undefined, options);
+
+		expect(
+			gen.next({
+				data: { ok: true },
+			}).value,
+		).toEqual(call(httpFetch, url, config, HTTP_METHODS.DELETE, undefined));
+		expect(gen.next().done).toBe(true);
+	});
 
 	it('should wrap the request and notify errors', () => {
 		const url = '/foo';
