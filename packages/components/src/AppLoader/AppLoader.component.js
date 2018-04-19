ead51e4ec0ee743490d1e77a2144100fd2cbc748
yatheesh <ybaskaran@talend.com>
2018-04-19 14:33:53 +0200

    chore(components): appLoader improving the spinner animation

--- packages/components/src/AppLoader/AppLoader.component.js
+++ packages/components/src/AppLoader/AppLoader.component.js
@@ -3,8 +3,11 @@ import React from 'react';
 function AppLoader() {
 	return (
 		<div className="tc-app-loader-container" aria-atomic="true" aria-busy="true">
-			<div className="tc-app-loader">
-				<div className="tc-app-loader-icon" />
+			<div className="tc-app-loader" >
+				<div className="spinner-wrapper">
+          <div className="spinner-left"></div>
+          <div className="spinner-right"></div>
+				</div>
 			</div>
 		</div>
 	);
