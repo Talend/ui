f921e2a336a4748f2494057a4685a052c036f6e8
travis <travis>
2018-04-23 13:10:12 +0000

    test(ci): prettier

--- packages/jsfc/src/lib/resolve.js
+++ packages/jsfc/src/lib/resolve.js
@@ -1,22 +1,27 @@
 import * as JsonRefs from './../../lib/json-refs-standalone';
 
 export function jsonref(schema, callBack) {
-  let promise = new Promise(
-    function(resolve, reject) {
-      JsonRefs.resolveRefs(schema, {
-        "filter": [ 'relative', 'local', 'remote' ]
-      })
-        .then((res) => { resolve(res.resolved); })
-        .catch((err) => { reject(new Error(err)); });
-    }
-  );
+	let promise = new Promise(function(resolve, reject) {
+		JsonRefs.resolveRefs(schema, {
+			filter: ['relative', 'local', 'remote'],
+		})
+			.then(res => {
+				resolve(res.resolved);
+			})
+			.catch(err => {
+				reject(new Error(err));
+			});
+	});
 
-  if(typeof(callBack) === 'function') {
-    promise
-      .then((resolved) => { callBack(null, resolved); })
-      .catch((error) => { callBack(error); });
-  }
-  else {
-    return promise;
-  }
-};
+	if (typeof callBack === 'function') {
+		promise
+			.then(resolved => {
+				callBack(null, resolved);
+			})
+			.catch(error => {
+				callBack(error);
+			});
+	} else {
+		return promise;
+	}
+}
--- packages/jsfc/src/module.js
+++ packages/jsfc/src/module.js
@@ -3,8 +3,8 @@ import * as sfPathImp from './lib/sf-path';
 import canonicalTitleMapImp from './lib/canonical-title-map';
 
 export { merge } from './lib/merge';
-export { select }  from './lib/select';
-export { jsonref }  from './lib/resolve';
+export { select } from './lib/select';
+export { jsonref } from './lib/resolve';
 export { traverseSchema, traverseForm } from './lib/traverse';
 export { validate } from './lib/validate';
 
