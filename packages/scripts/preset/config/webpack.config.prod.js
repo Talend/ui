3447a1f31e608a2028fe97b4f0975f74243ef484
Jimmy Somsanith <jsomsanith@talend.com>
2018-04-24 18:13:46 +0200

    chore(scripts): migrate to webpack 4

--- packages/scripts/preset/config/webpack.config.prod.js
+++ packages/scripts/preset/config/webpack.config.prod.js
@@ -1,17 +1,16 @@
-const webpack = require('webpack');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = () => ({
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-			},
-			comments: false,
-			mangle: true,
-			minimize: true,
-		}),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('production'),
-		}),
-	],
+	mode: 'production',
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				sourceMap: true,
+			}),
+			new OptimizeCSSAssetsPlugin({}),
+		],
+	},
 });
