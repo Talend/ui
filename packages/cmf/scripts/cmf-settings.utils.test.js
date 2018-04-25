c2ac26218dfc6fb136c03f36a15fee0c44db3b81
Jean-Alain Ré <jare@talend.com>
2018-04-25 16:31:46 +0200

    fix reviews

--- packages/cmf/scripts/cmf-settings.utils.test.js
+++ packages/cmf/scripts/cmf-settings.utils.test.js
@@ -3,7 +3,7 @@ const { sortObject } = require('./cmf-settings.utils');
 describe('cmf-settings.utils', () => {
 	describe('#sortObject', () => {
 		it('should sort the object', () => {
-			expect(sortObject({ c: 1, a: 2, b: 3 })).toEqual({ a: 2, b: 3, c: 1 });
+			expect(Object.keys(sortObject({ c: 1, a: 2, b: 3 }))).toEqual(['a', 'b', 'c']);
 		});
 	});
 });
