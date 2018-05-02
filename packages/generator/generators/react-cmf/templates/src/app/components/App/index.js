4df2b69c4859b85151de40c8e6344096a8eec287
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-05-02 21:14:49 +0200

    chore: update generator

---
+++ packages/generator/generators/react-cmf/templates/src/app/components/App/index.js
@@ -0,0 +1,3 @@
+import App from './App.component';
+
+export default App;
--- packages/generator/generators/react-cmf/templates/src/app/index.js
+++ packages/generator/generators/react-cmf/templates/src/app/index.js
@@ -4,41 +4,24 @@
  * and other style can override it
  */
 import '@talend/bootstrap-theme/src/theme/theme.scss';
-import React from 'react';
-import { render } from 'react-dom';
-import { App, store as cmfstore, actions } from '@talend/react-cmf';
-import { browserHistory } from 'react-router';
-import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
+import cmf from '@talend/react-cmf';
+import { registerAllContainers } from '@talend/react-containers/lib/register';
+import actions from './actions';
+import components from './components';
+import reducer from './reducers';
 
-import configure from './configure';
-import appReducer from './reducers';
+registerAllContainers();
 
 /**
  * Initialize CMF configuration
  * - Register your components in the CMF dictionary
  * - Register action creators in CMF actions dictionary
  */
-configure.initialize();
+const app = cmf.bootstrap({
+	components,
+	reducer,
+	settingsURL: '/settings.json',
+	actionCreators: actions,
+});
 
-/**
- * Register react-router-redux router reducer (see https://github.com/reactjs/react-router-redux)
- */
-cmfstore.setRouterMiddleware(routerMiddleware(browserHistory));
-
-/**
- * Register your app reducers
- */
-const store = cmfstore.initialize(appReducer);
-
-/**
- * Fetch the CMF settings and configure the CMF app
- */
-store.dispatch(actions.settings.fetchSettings('/settings.json'));
-
-/**
- * Render the CMF App
- */
-render(
-	<App store={store} history={syncHistoryWithStore(browserHistory, store)} />,
-	document.getElementById('app')
-);
+app.render();
