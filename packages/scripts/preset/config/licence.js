d77d5ba6ac858883bacecefc17bdccbcf3828b8d
Jimmy Somsanith <jsomsanith@talend.com>
2018-05-09 16:45:24 +0200

    Set licence url to tools instead of tdp one

--- packages/scripts/preset/config/licence.js
+++ packages/scripts/preset/config/licence.js
@@ -4,7 +4,7 @@ const LICENSE_BANNER = `
      Copyright (C) 2006-${new Date().getFullYear()} Talend Inc. - www.talend.com
     
      This source code is available under agreement available at
-     https://github.com/Talend/data-prep/blob/master/LICENSE
+     https://github.com/Talend/tools/blob/master/LICENSE
     
      You should have received a copy of the agreement
      along with this program; if not, write to Talend SA
