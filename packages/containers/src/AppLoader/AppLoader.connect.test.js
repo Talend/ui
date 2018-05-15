6fa280d28f82d1d7ac33f45b2f86ee8868c016a9
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-05-15 10:51:33 +0200

    test: update

--- packages/containers/src/AppLoader/AppLoader.connect.test.js
+++ packages/containers/src/AppLoader/AppLoader.connect.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Immutable from 'immutable';
 import { shallow } from 'enzyme';
+import { AppLoader } from '@talend/react-components';
 import { AppLoaderContainer, mapStateToProps } from './AppLoader.connect';
 
 describe('AppLoader container', () => {
@@ -11,7 +12,7 @@ describe('AppLoader container', () => {
 					<div className="child" />
 				</AppLoaderContainer>,
 			);
-			expect(wrapper.getElement()).toMatchSnapshot();
+			expect(wrapper.find('.child').length).toBe(1);
 		});
 
 		it('should render the app loader if loading', () => {
@@ -20,7 +21,8 @@ describe('AppLoader container', () => {
 					<div className="child" />
 				</AppLoaderContainer>,
 			);
-			expect(wrapper.getElement()).toMatchSnapshot();
+			expect(wrapper.find('.child').length).toBe(0);
+			expect(wrapper.getElement()).toEqual(<AppLoader />);
 		});
 	});
 
