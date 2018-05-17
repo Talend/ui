fedaf8f2bde1272639a66ada0793a8b80535fed7
Jimmy Somsanith <jsomsanith@talend.com>
2018-05-17 14:36:53 +0200

    Update versions in missed packages

--- packages/scripts/preset/config/test/test-setup.js
+++ packages/scripts/preset/config/test/test-setup.js
@@ -1,5 +1,5 @@
 require('babel-polyfill');
 const configure = require('enzyme').configure;
-const Adapter = require('enzyme-adapter-react-15');
+const Adapter = require('enzyme-adapter-react-16');
 
 configure({ adapter: new Adapter() });
