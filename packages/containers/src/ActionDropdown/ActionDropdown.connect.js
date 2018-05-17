ffe514ea13abf27085f5221f0c97eeee8262fb05
Jean-Alain Ré <jare@talend.com>
2018-05-17 16:55:35 +0200

    fix(Component): fix on click on a item

--- packages/containers/src/ActionDropdown/ActionDropdown.connect.js
+++ packages/containers/src/ActionDropdown/ActionDropdown.connect.js
@@ -42,7 +42,7 @@ export function ContainerActionDropdown({ items, ...props }) {
 	const safeProps = omit(props, cmfConnect.INJECTED_PROPS);
 	if (items) {
 		// keep initial object as it can be immutable and have a prototype
-		const clikableItems = items.map(item => Object.assign(item, ...getOnClick(item, props)));
+		const clikableItems = items.map(item => ({ ...getOnClick(item, props), ...item }));
 		return <ActionDropdown items={clikableItems} {...safeProps} />;
 	}
 	return <ActionDropdown {...safeProps} />;
