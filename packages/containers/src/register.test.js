55061745b90d634d7f42e7e7bc4606093c9336f3
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-05-21 11:29:03 +0200

    chore: apply cmf codeshift on all packages

--- packages/containers/src/register.test.js
+++ packages/containers/src/register.test.js
@@ -1,8 +1,8 @@
-import { api } from '@talend/react-cmf';
+import cmf from '@talend/react-cmf';
 import { registerAllContainers } from './register';
 
 jest.mock('@talend/react-cmf', () => ({
-	api: {
+	cmf: {
 		component: {
 			registerMany: () => {},
 			register: () => {},
@@ -15,11 +15,11 @@ jest.mock('@talend/react-cmf', () => ({
 
 describe('#register contianers', () => {
 	it('should register all component', () => {
-		api.component.registerMany = jest.fn();
-		api.component.register = jest.fn();
+		cmf.component.registerMany = jest.fn();
+		cmf.component.register = jest.fn();
 		registerAllContainers();
 
-		expect(api.component.registerMany.mock.calls.length).toBe(1);
-		expect(api.component.register).toHaveBeenCalled();
+		expect(cmf.component.registerMany.mock.calls.length).toBe(1);
+		expect(cmf.component.register).toHaveBeenCalled();
 	});
 });
