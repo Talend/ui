e80c49c4a6c1c9f0213a7f95625625d12d413944
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-05-30 12:13:21 +0200

    chore
    let the default it way more easy to maintain

--- packages/containers/src/Typeahead/Typeahead.connect.js
+++ packages/containers/src/Typeahead/Typeahead.connect.js
@@ -2,6 +2,5 @@ import { cmfConnect } from '@talend/react-cmf';
 import Typeahead, { DEFAULT_STATE } from './Typeahead.container';
 
 export default cmfConnect({
-	componentId: ownProps => ownProps.componentId || ownProps.id,
 	defaultState: DEFAULT_STATE,
 })(Typeahead);
