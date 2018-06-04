148be62a79bb9e19f801a352cfb8067c131c19a9
Sébastien Romain <sromain@tlnd-sromain.local>
2018-06-04 13:06:44 +0200

    rename

--- packages/cmf/src/expressions/includes.js
+++ packages/cmf/src/expressions/includes.js
@@ -1,7 +1,7 @@
 import _get from 'lodash/get';
 import Immutable from 'immutable';
 
-export default function getInIncludesClosure(statePath) {
+export default function getInIncludesCurry(statePath) {
 	return function getInIncludes({ context }, immutablePath, value) {
 		return _get(context.store.getState(), statePath, new Immutable.Map())
 			.getIn(immutablePath.split('.'), new Immutable.List())
