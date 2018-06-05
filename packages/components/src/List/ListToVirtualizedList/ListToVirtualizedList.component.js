a92205b9fa671338a923adbe636827ded0fd1e54
travis <travis>
2018-06-05 06:34:09 +0000

    test(ci): prettier

--- packages/components/src/List/ListToVirtualizedList/ListToVirtualizedList.component.js
+++ packages/components/src/List/ListToVirtualizedList/ListToVirtualizedList.component.js
@@ -72,28 +72,31 @@ export function ListToVirtualizedList(props) {
 			sortDirection={sort && sort.isDescending ? SORT_BY.DESC : SORT_BY.ASC}
 			type={props.displayMode.toUpperCase()}
 		>
-			{props.columns.sort(compareOrder).filter(item => !item.hidden).map((column, index) => {
-				const cProps = {
-					label: column.label,
-					dataKey: column.key,
-				};
-				if (titleProps && column.key === titleProps.key) {
-					Object.assign(cProps, CellTitle, {
-						columnData: titleProps,
-					});
-				} else if (supposedActions[column.key]) {
-					Object.assign(cProps, CellActions);
-				} else if (column.type && listCellDictionary[column.type]) {
-					Object.assign(cProps, listCellDictionary[column.type], {
-						columnData: column.data,
-					});
-				}
-				if (column.hideHeader) {
-					cProps.disableSort = true;
-					cProps.headerRenderer = HiddenHeader;
-				}
-				return <VirtualizedList.Content key={index} {...cProps} />;
-			})}
+			{props.columns
+				.sort(compareOrder)
+				.filter(item => !item.hidden)
+				.map((column, index) => {
+					const cProps = {
+						label: column.label,
+						dataKey: column.key,
+					};
+					if (titleProps && column.key === titleProps.key) {
+						Object.assign(cProps, CellTitle, {
+							columnData: titleProps,
+						});
+					} else if (supposedActions[column.key]) {
+						Object.assign(cProps, CellActions);
+					} else if (column.type && listCellDictionary[column.type]) {
+						Object.assign(cProps, listCellDictionary[column.type], {
+							columnData: column.data,
+						});
+					}
+					if (column.hideHeader) {
+						cProps.disableSort = true;
+						cProps.headerRenderer = HiddenHeader;
+					}
+					return <VirtualizedList.Content key={index} {...cProps} />;
+				})}
 		</VirtualizedList>
 	);
 }
