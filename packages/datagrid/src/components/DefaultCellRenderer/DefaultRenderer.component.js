d7f15d78f290524e71f83691d5628a75de53906a
Jean-Alain Ré <jare@talend.com>
2018-06-06 09:42:35 +0200

    fix test

--- packages/datagrid/src/components/DefaultCellRenderer/DefaultRenderer.component.js
+++ packages/datagrid/src/components/DefaultCellRenderer/DefaultRenderer.component.js
@@ -2,14 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import DefaultValueRenderer from './DefaultValueRenderer.component';
-import FormatValue, { hasWhiteSpaceCharacters } from './FormatValue.component';
 
 export default function DefaultRenderer({ data }) {
-	if (!hasWhiteSpaceCharacters(data.value)) {
-		return <DefaultValueRenderer label={data.value} />;
-	}
-
-	return <DefaultValueRenderer contentRenderer={() => <FormatValue value={data.value} />} />;
+	return <DefaultValueRenderer value={data.value} />;
 }
 
 DefaultRenderer.propTypes = {
