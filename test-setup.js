f5d5c692f175870f50e15ff5ce7234f9ee45ff46
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-06-11 16:32:30 +0200

    fix: typo

--- test-setup.js
+++ test-setup.js
@@ -4,7 +4,7 @@ import { configure } from 'enzyme';
 import AdapterReact15 from 'enzyme-adapter-react-15';
 import AdapterReact16 from 'enzyme-adapter-react-16';
 
-if (process.env.REACT_VERSION === 15) {
+if (process.env.REACT_VERSION.startsWith('^15.')) {
 	configure({ adapter: new AdapterReact15() });
 } else {
 	configure({ adapter: new AdapterReact16() });
