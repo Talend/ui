2b9ea7f556763e782a5dbc0efdc88550b1d3e1b2
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-06-11 18:21:43 +0200

    fix: make test-setup more strong on REACT_VERSION

--- test-setup.js
+++ test-setup.js
@@ -4,10 +4,21 @@ import { configure } from 'enzyme';
 import AdapterReact15 from 'enzyme-adapter-react-15';
 import AdapterReact16 from 'enzyme-adapter-react-16';
 
-if (process.env.REACT_VERSION.startsWith('^15.')) {
+function getMajorVersion() {
+	if (!process.env.REACT_VERSION) {
+		return '16';
+	}
+	return process.env.REACT_VERSION.replace('^', '').split('.')[0];
+}
+
+const REACT_VERSION = getMajorVersion();
+
+if (REACT_VERSION === '15') {
 	configure({ adapter: new AdapterReact15() });
-} else {
+} else if (REACT_VERSION === '16') {
 	configure({ adapter: new AdapterReact16() });
+} else {
+	throw new Error(`Unsupported version of React: ${REACT_VERSION}`);
 }
 
 const fetch = jest.fn(
