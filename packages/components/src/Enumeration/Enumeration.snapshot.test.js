254ff986de48f4ac4593dff1b2670047d78c9b36
Jimmy Somsanith <jsomsanith@talend.com>
2018-06-12 16:40:43 +0200

    Remove unused import

--- packages/components/src/Enumeration/Enumeration.snapshot.test.js
+++ packages/components/src/Enumeration/Enumeration.snapshot.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Enumeration from './Enumeration.component';
 import toJsonWithoutI18n from '../../test/props-without-i18n';
