2a2c7672872476b1ca0f768cfcf95d20e4eb54c0
Jérémie DESCOTTES <jdescottes@talend.com>
2018-06-12 16:25:53 +0200

    feat: Month picker buttons and style

--- packages/components/src/DateTimePickers/MonthPicker/MonthPicker.component.js
+++ packages/components/src/DateTimePickers/MonthPicker/MonthPicker.component.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import theme from './MonthPicker.scss';
+import { ActionButton } from '../../Actions';
 import { chunk } from 'lodash';
 
 function MonthPicker(props) {
 	const months = [
-		'Jan',
-		'Fev',
+		'Janvier',
+		'Février',
 		'Mars',
-		'Avr',
+		'Avril',
 		'Mai',
 		'Juin',
-		'Juil',
-		'Aout',
-		'Sept',
-		'Oct',
-		'Nov',
-		'Dec',
+		'Juillet',
+		'Août',
+		'Septembre',
+		'Octobre',
+		'Novembre',
+		'Décembre',
 	];
 
 	const monthsRows = chunk(months, 3);
+	const activeMonth = 'Septembre';
+
+	function getClassIfActive(m) {
+		return activeMonth === m ? theme.active : undefined;
+	}
 
 	return (
 		<div className={theme.container}>
 			{monthsRows.map((monthsRow, i) =>
 				<div className={theme.row} key={i}>
 					{monthsRow.map((month, j) =>
-						<div className={theme.month} key={j}>
-							{month}
-						</div>
+						<ActionButton
+							key={j}
+							label={month}
+							link
+							className={classNames(
+								theme.month,
+								getClassIfActive(month),
+							)}
+						/>
 					)}
 				</div>
 			)}
