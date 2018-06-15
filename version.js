97cd985ad11ef2e31f25bf9799e5842a01564296
Jimmy Somsanith <jsomsanith@talend.com>
2018-06-15 09:14:25 +0200

    Reorganize libs

--- version.js
+++ version.js
@@ -67,45 +67,34 @@ const ADDONS = {
 	'redux-undo': 'beta',
 	'redux-saga': '^0.15.4',
 	'react-addons-perf': '^15.4.2',
-	'react-autowhatever': '10.1.0',
-	'react-debounce-input': '3.1.0',
 	'react-immutable-proptypes': '^2.1.0',
-	'react-jsonschema-form': '0.51.0',
 	'react-tap-event-plugin': '^2.0.0',
-	'react-virtualized': '9.10.1',
 	slugify: '^1.1.0',
 	'whatwg-fetch': '^2.0.3',
 };
 
 const VERSIONS = Object.assign({}, ADDONS, {
-	// deps
+	// deps: js libs
 	ajv: '^6.2.1',
 	'bootstrap-sass': '3.3.7',
 	'bson-objectid': '^1.1.5',
 	classnames: '^2.2.5',
-	'd3-shape': '1.2.0',
 	keycode: '^2.2.0',
 	lodash: '^4.17.4',
 	immutable: '^3.8.1',
 	invariant: '^2.2.2',
 	'prop-types': '^15.5.10',
 	react: REACT_VERSION,
-	'react-ace': '5.2.0',
-	'react-bootstrap': '0.31.5',
 	'react-dom': REACT_VERSION,
 	i18next: '^9.0.0',
 	'i18next-parser': '^0.13.0',
-	'rc-slider': '8.4.1',
-	'rc-tooltip': '3.7.0',
 	'react-i18next': '^7.6.1',
 	'react-redux': '^5.0.7',
 	'react-router': '^3.2.0',
 	'react-router-redux': '^4.0.8',
 	'react-test-renderer': REACT_VERSION,
 	'react-transition-group': '^2.3.1',
-	'react-virtualized': '9.10.1',
 	reselect: '^2.5.4',
-
 	redux: '^3.7.2',
 	'redux-batched-actions': '^0.2.0',
 	'redux-logger': '^3.0.6',
@@ -113,6 +102,17 @@ const VERSIONS = Object.assign({}, ADDONS, {
 	'redux-thunk': '^2.2.0',
 	uuid: '^3.0.1', // prefer bson-objectid
 	tv4: '^1.3.0',
+
+	// deps: libs that interact with the DOM
+	'd3-shape': '1.2.0',
+	'react-ace': '5.2.0',
+	'react-bootstrap': '0.31.5',
+	'rc-slider': '8.4.1',
+	'rc-tooltip': '3.7.0',
+	'react-autowhatever': '10.1.0',
+	'react-debounce-input': '3.1.0',
+	'react-jsonschema-form': '0.51.0',
+	'react-virtualized': '9.10.1',
 
 	// script dep
 	deepmerge: '^1.5.1',
@@ -151,7 +151,6 @@ const VERSIONS = Object.assign({}, ADDONS, {
 	'react-storybook-cmf': '^0.4.0',
 	'react-stub-context': '^0.7.0',
 	rimraf: '^2.6.1',
-	storyshots: '3.2.2',
 
 	// webpack
 	'babel-loader': '^7.1.2',
