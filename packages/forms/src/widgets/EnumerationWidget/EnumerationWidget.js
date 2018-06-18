842fe0b7620ed1f6610efa6b12a1377dc569afbd
Sébastien Romain <sromain@tlnd-sromain.local>
2018-06-18 11:28:36 +0200

    update ( for now )

--- packages/forms/src/widgets/EnumerationWidget/EnumerationWidget.js
+++ packages/forms/src/widgets/EnumerationWidget/EnumerationWidget.js
@@ -854,7 +854,9 @@ class EnumerationForm extends React.Component {
 			// checking if the value already exist
 			const valueExist = this.valueAlreadyExist(value, prevState);
 			const [validateAndAddAction, validateAction, abortAction] = prevState.headerInput;
+			// in this case, we could have the loading state that implied we have just one icon
 			if (!validateAction && !abortAction) {
+				// returning null in setState prevent re-rendering
 				return null;
 			}
 			validateAndAddAction.disabled = value === '' || valueExist;
