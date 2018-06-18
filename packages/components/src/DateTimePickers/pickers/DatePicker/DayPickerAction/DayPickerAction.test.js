f2ee99763547f4f445419eccca3fa6166149f8fd
Jérémie DESCOTTES <jdescottes@talend.com>
2018-06-18 11:49:54 +0200

    fix: More meaningfull tests

--- packages/components/src/DateTimePickers/pickers/DatePicker/DayPickerAction/DayPickerAction.test.js
+++ packages/components/src/DateTimePickers/pickers/DatePicker/DayPickerAction/DayPickerAction.test.js
@@ -4,10 +4,55 @@ import { shallow } from 'enzyme';
 import DayPickerAction from './DayPickerAction.component';
 
 describe('DayPickerAction', () => {
-	it('should render', () => {
+	it('should render a simple day', () => {
 		// When
 		const wrapper = shallow(
-			<DayPickerAction />
+			<DayPickerAction
+				label="36"
+				className="my-day-picker-class"
+			/>
+		);
+
+		// Then
+		expect(wrapper.getElement()).toMatchSnapshot();
+	});
+
+	it('should render a disabled day', () => {
+		// When
+		const wrapper = shallow(
+			<DayPickerAction
+				label="36"
+				className="my-day-picker-class"
+				isDisabledDay
+			/>
+		);
+
+		// Then
+		expect(wrapper.getElement()).toMatchSnapshot();
+	});
+
+	it('should render a today day', () => {
+		// When
+		const wrapper = shallow(
+			<DayPickerAction
+				label="36"
+				className="my-day-picker-class"
+				isCurrentDay
+			/>
+		);
+
+		// Then
+		expect(wrapper.getElement()).toMatchSnapshot();
+	});
+
+	it('should render a selected day', () => {
+		// When
+		const wrapper = shallow(
+			<DayPickerAction
+				label="36"
+				className="my-day-picker-class"
+				isSelectedDay
+			/>
 		);
 
 		// Then
