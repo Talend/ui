73158cda65284e3fe823dca112e285175aeab468
Jérémie DESCOTTES <jdescottes@talend.com>
2018-06-19 10:56:52 +0200

    chore: Remove now useless stories

--- packages/components/stories/DateTimePicker.js
+++ packages/components/stories/DateTimePicker.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { IconsProvider } from '../src/index';
 
-import DateTimePicker, {
-	DateTimeView,
-	MonthYearView,
-} from '../src/DateTimePickers';
+import DateTimePicker from '../src/DateTimePickers';
 
 
 storiesOf('DateTimePicker', module)
@@ -15,24 +12,6 @@ storiesOf('DateTimePicker', module)
 			<IconsProvider />
 			<div style={{ width: '320px', border: '1px solid black' }}>
 				<DateTimePicker />
-			</div>
-		</div>
-	))
-	.add('Date/Time view structure', () => (
-		<div>
-			<h1>Date/Time structure</h1>
-			<IconsProvider />
-			<div style={{ width: '320px', height: '25rem', border: '1px solid black' }}>
-				<DateTimeView />
-			</div>
-		</div>
-	))
-	.add('Month/Year view structure', () => (
-		<div>
-			<h1>Month/Year structure</h1>
-			<IconsProvider />
-			<div style={{ width: '320px', height: '25rem', border: '1px solid black' }}>
-				<MonthYearView />
 			</div>
 		</div>
 	));
