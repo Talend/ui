3db85ee4b18ffb0e8283b5b4cfaf36fe19d2cbb9
Vladimir Koval <vladimmirkoval@gmail.com>
2018-06-21 18:09:08 +0300

    add toJS functionality to get data from components also, not anly from collections

---
+++ packages/cmf/src/selectors/components.js
@@ -0,0 +1,41 @@
+import getToJSMemoized from './toJS';
+
+/**
+ * return a collection or subset of a collection from a cmf store
+ * this function returns collection from cmf.components, where the local state of some components
+ * @param {Object} state
+ * @param {String or Array<String>} collectionPath
+ * @example
+ *  get('foo.bar', true) === state.cmf.components.getIn(['foo', 'bar'], true)
+ */
+export function get(state, collectionPath, defaultValue) {
+	let path;
+	if (typeof collectionPath === 'string') {
+		path = collectionPath.split('.');
+	} else if (Array.isArray(collectionPath)) {
+		path = collectionPath;
+	}
+	if (path) {
+		return state.cmf.components.getIn(path, defaultValue);
+	}
+	throw Error(`Type mismatch: collectionPath should be a string or an array of string
+got ${collectionPath}`);
+}
+
+const selectors = {};
+
+export function toJS(state, path) {
+	const joinedPath = Array.isArray(path) ? path.join('.') : path;
+	if (!selectors[joinedPath]) {
+		selectors[joinedPath] = getToJSMemoized(calledState => get(calledState, path));
+	}
+	return selectors[joinedPath](state);
+}
+
+export function getFormDataToJS(state, formId) {
+	return toJS(state, `Container(Form).${formId}.data`);
+}
+
+export function getFormData(state, formId, defaultValue) {
+	return get(state, `Container(Form).${formId}.data`, defaultValue);
+}
--- packages/cmf/src/selectors/index.js
+++ packages/cmf/src/selectors/index.js
@@ -1,9 +1,11 @@
 import * as collections from './collections';
+import * as components from './components';
 import * as router from './router';
 import toJS from './toJS';
 
 export default {
 	collections,
+	components,
 	router,
 	toJS,
 };
