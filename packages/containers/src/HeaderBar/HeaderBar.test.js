08a6c1595f296a6599857a0e94579d22fd7712f2
Steven Gangnant <sgangnant@talend.com>
2018-06-21 16:29:35 +0200

    chore(HeaderBarContainer): Fixed unit tests

--- packages/containers/src/HeaderBar/HeaderBar.test.js
+++ packages/containers/src/HeaderBar/HeaderBar.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Provider } from '@talend/react-cmf/lib/mock';
-import { Map } from 'immutable';
+import { Map, List } from 'immutable';
 
-import Container, { DEFAULT_STATE } from './HeaderBar.container';
+import Container from './HeaderBar.container';
 import Connected, {
 	mapStateToProps,
 } from './HeaderBar.connect';
@@ -22,18 +21,39 @@ describe('Connected HeaderBar', () => {
 		expect(Connected.displayName).toBe(`Connect(CMF(${Container.displayName}))`);
 		expect(Connected.WrappedComponent).toBe(Container);
 	});
+
 	it('should mapStateToProps', () => {
 		const state = {
 			cmf: {
-				components: new Map({
+				collections: new Map({
 					HeaderBar: {
-						HeaderBar: DEFAULT_STATE.toJS(),
+						[Container.PRODUCTS_COLLECTION_ID]: new List(),
 					},
 				}),
 			},
 		};
-		const props = mapStateToProps(state);
-		expect(typeof props).toBe('object');
+		const ownProps = {};
+
+		const props = mapStateToProps(state, ownProps);
+
+		expect(props).toEqual({});
+	});
+
+	it('should mapStateToProps when products have been already fetched', () => {
+		const apps = [{ url: 'foobar' }];
+		const state = {
+			cmf: {
+				collections: new Map({
+					[Container.PRODUCTS_COLLECTION_ID]: new List(apps),
+				}),
+			},
+		};
+		const ownProps = {};
+
+		const { products: { items } } = mapStateToProps(state, ownProps);
+
+		expect(items.length).toEqual(apps.length);
+		expect(Object.keys(items[0])).toEqual(['onClickDispatch', 'url']);
 	});
 });
 
