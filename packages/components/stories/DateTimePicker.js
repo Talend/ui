4fd7943eec3d071262a44c42beecfb54135154bd
Jérémie DESCOTTES <jdescottes@talend.com>
2018-06-22 12:01:54 +0200

    chore: Story context infos

--- packages/components/stories/DateTimePicker.js
+++ packages/components/stories/DateTimePicker.js
@@ -6,10 +6,17 @@ import DateTimePicker from '../src/DateTimePickers';
 
 
 storiesOf('DateTimePicker', module)
-	.add('Full DateTimePicker structure', () => (
+	.add('Full DateTimePicker', () => (
 		<div>
-			<h1>DateTimePicker structure</h1>
+			<h1>DateTimePicker</h1>
 			<IconsProvider />
+			<ul>
+				<li>Width is defined by the parent (here fixed to 320px) but is responsive </li>
+				<li>Height is responsive relatively to the default font-size</li>
+				<li>The outer border style (black) is here just as visual
+					shape indication, it's not part of the component rendered</li>
+			</ul>
+
 			<div style={{ width: '320px', border: '1px solid black' }}>
 				<DateTimePicker />
 			</div>
