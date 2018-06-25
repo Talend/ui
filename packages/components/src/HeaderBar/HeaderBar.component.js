72ea621bb4f7e79575c7999bdff4d665091ab09d
Jimmy Somsanith <jsomsanith@talend.com>
2018-06-25 09:44:06 +0200

    Test

--- packages/components/src/HeaderBar/HeaderBar.component.js
+++ packages/components/src/HeaderBar/HeaderBar.component.js
@@ -279,14 +279,13 @@ function HeaderBar(props) {
 				{props.help && (
 					<Components.Help getComponent={props.getComponent} {...props.help} t={props.t} />
 				)}
-				{!props.user &&
-					props.information && (
-						<Components.Information
-							getComponent={props.getComponent}
-							{...props.information}
-							t={props.t}
-						/>
-					)}
+				{!props.user && props.information && (
+					<Components.Information
+						getComponent={props.getComponent}
+						{...props.information}
+						t={props.t}
+					/>
+				)}
 				{props.user && (
 					<Components.User getComponent={props.getComponent} {...props.user} t={props.t} />
 				)}
