4f61afc49d1239c862070b97c3b861610f121115
Jérémie DESCOTTES <jdescottes@talend.com>
2018-06-25 14:50:41 +0200

    feat: Dynamic calendar header

--- packages/components/src/DateTimePickers/pickers/DatePicker/DatePicker.component.js
+++ packages/components/src/DateTimePickers/pickers/DatePicker/DatePicker.component.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import memoize from 'lodash/memoize';
 import { chunk } from 'lodash';
+import format from 'date-fns/format';
 import endOfMonth from 'date-fns/end_of_month';
 import startOfWeek from 'date-fns/start_of_week';
 import getDate from 'date-fns/get_date';
+import setDay from 'date-fns/set_day';
 import getMonth from 'date-fns/get_month';
 import isSameDay from 'date-fns/is_same_day';
 import differenceInCalendarWeeks from 'date-fns/difference_in_calendar_weeks';
@@ -14,10 +16,11 @@ import addDays from 'date-fns/add_days';
 import theme from './DatePicker.scss';
 import DayPickerAction from './DayPickerAction';
 
+const firstDayOfweek = 1;
+
 class DatePicker extends React.Component {
 
 	static buildWeeks(year, monthIndex) {
-		const firstDayOfweek = 1;
 		const firstDateOfMonth = new Date(year, monthIndex);
 		const firstDateOfCalendar = startOfWeek(firstDateOfMonth, {
 			weekStartsOn: firstDayOfweek,
@@ -39,7 +42,13 @@ class DatePicker extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+		const baseDate = new Date(0);
+
+		this.dayNames = (new Array(7))
+			.fill(0)
+			.map((_, i) => (i + firstDayOfweek) % 7)
+			.map(dayOfWeek => setDay(baseDate, dayOfWeek))
+			.map(headerDate => format(headerDate, 'dddd'));
 
 		this.getWeeks = memoize(DatePicker.buildWeeks, (year, monthIndex) => `${year}-${monthIndex}`);
 
--- packages/components/src/DateTimePickers/pickers/DatePicker/DatePicker.test.js
+++ packages/components/src/DateTimePickers/pickers/DatePicker/DatePicker.test.js
@@ -20,135 +20,259 @@ describe('DatePicker', () => {
 		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 
-	describe('first date in the correct day column (Monday as first column)', () => {
-		function getFirstRowCellText(wrapper, column) {
-			return wrapper
-				.find('.theme-calendar-body-row')
-				.first()
-				.find('.theme-calendar-item')
-				.at(column - 1)
-				.find('DayPickerAction')
-				.prop('label');
-		}
+	describe('body calendar', () => {
+		describe('first date in the correct day column (Monday as first column)', () => {
+			function getFirstRowCellText(wrapper, column) {
+				return wrapper
+					.find('.theme-calendar-body-row')
+					.first()
+					.find('.theme-calendar-item')
+					.at(column - 1)
+					.find('DayPickerAction')
+					.prop('label');
+			}
 
-		it('should have monday in the 1st column for january 2018', () => {
-			const calendar = {
-				year: 2018,
-				monthIndex: 0,
-			};
+			it('should have monday in the 1st column for january 2018', () => {
+				const calendar = {
+					year: 2018,
+					monthIndex: 0,
+				};
 
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
 
-			expect(getFirstRowCellText(wrapper, 1)).toBe('1');
+				expect(getFirstRowCellText(wrapper, 1)).toBe('1');
+			});
+
+			it('should have thursday in the 4th column for march 2018 ', () => {
+				const calendar = {
+					year: 2018,
+					monthIndex: 2,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				expect(getFirstRowCellText(wrapper, 4)).toBe('1');
+			});
+
+			it('should have sunday in the 7th column for april 2018', () => {
+				const calendar = {
+					year: 2018,
+					monthIndex: 3,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				expect(getFirstRowCellText(wrapper, 7)).toBe('1');
+			});
 		});
 
-		it('should have thursday in the 4th column for march 2018 ', () => {
-			const calendar = {
-				year: 2018,
-				monthIndex: 2,
-			};
 
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
+		describe('last date in the correct day column (Monday as first column)', () => {
+			function getLastRowCellText(wrapper, column) {
+				return wrapper
+					.find('.theme-calendar-body-row')
+					.last()
+					.find('.theme-calendar-item')
+					.at(column - 1)
+					.find('DayPickerAction')
+					.prop('label');
+			}
 
-			expect(getFirstRowCellText(wrapper, 4)).toBe('1');
+			it('should have monday in the 1st column for april 2018', () => {
+				const calendar = {
+					year: 2018,
+					monthIndex: 3,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				expect(getLastRowCellText(wrapper, 1)).toBe('30');
+			});
+
+			it('should have thursday in the 4th column for may 2018', () => {
+				const calendar = {
+					year: 2018,
+					monthIndex: 4,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				expect(getLastRowCellText(wrapper, 4)).toBe('31');
+			});
+
+			it('should have sunday in the 7th column for september 2018', () => {
+				const calendar = {
+					year: 2018,
+					monthIndex: 8,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				expect(getLastRowCellText(wrapper, 7)).toBe('30');
+			});
 		});
 
-		it('should have sunday in the 7th column for april 2018', () => {
-			const calendar = {
-				year: 2018,
-				monthIndex: 3,
-			};
+		describe('number of weeks displayed', () => {
+			function nbOfWeeksRendered(wrapper) {
+				return wrapper.find('.theme-calendar-body-row').length;
+			}
 
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
+			it('should have 4 weeks for february 2010', () => {
+				const calendar = {
+					year: 2010,
+					monthIndex: 1,
+				};
 
-			expect(getFirstRowCellText(wrapper, 7)).toBe('1');
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				expect(nbOfWeeksRendered(wrapper)).toBe(4);
+			});
+
+			it('should have 5 weeks for june 2018', () => {
+				const calendar = {
+					year: 2018,
+					monthIndex: 5,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				expect(nbOfWeeksRendered(wrapper)).toBe(5);
+			});
+
+			it('should have 6 weeks for october 2017', () => {
+				const calendar = {
+					year: 2017,
+					monthIndex: 9,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				expect(nbOfWeeksRendered(wrapper)).toBe(6);
+			});
+		});
+
+		describe('contiguous ordered numbers', () => {
+			function getDayNumbers(wrapper) {
+				return wrapper
+					.find('.theme-calendar-body-row .theme-calendar-item')
+					.find('DayPickerAction')
+					.map(action => action.prop('label'))
+					.map(label => parseInt(label, 10));
+			}
+
+			function ascSorting(a, b) {
+				return a - b;
+			}
+
+			it('should have contiguous numbers for july 2018', () => {
+				const calendar = {
+					year: 2018,
+					monthIndex: 6,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				const days = getDayNumbers(wrapper);
+				const copy = [...days];
+				days.sort(ascSorting);
+				expect(days).toEqual(copy);
+				expect(days[0]).toBe(1);
+				expect(days).toHaveLength(31);
+			});
+
+			it('should have contiguous numbers for february 2016', () => {
+				const calendar = {
+					year: 2016,
+					monthIndex: 1,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				const days = getDayNumbers(wrapper);
+				const copy = [...days];
+				days.sort(ascSorting);
+				expect(days).toEqual(copy);
+				expect(days[0]).toBe(1);
+				expect(days).toHaveLength(29);
+			});
+
+			it('should have contiguous numbers for february 2017', () => {
+				const calendar = {
+					year: 2017,
+					monthIndex: 1,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				const days = getDayNumbers(wrapper);
+				const copy = [...days];
+				days.sort(ascSorting);
+				expect(days).toEqual(copy);
+				expect(days[0]).toBe(1);
+				expect(days).toHaveLength(28);
+			});
+
+			it('should have contiguous numbers for november 2022', () => {
+				const calendar = {
+					year: 2022,
+					monthIndex: 10,
+				};
+
+				const wrapper = shallow(<DatePicker
+					calendar={calendar}
+					currentDate={currentDate}
+				/>);
+
+				const days = getDayNumbers(wrapper);
+				const copy = [...days];
+				days.sort(ascSorting);
+				expect(days).toEqual(copy);
+				expect(days[0]).toBe(1);
+				expect(days).toHaveLength(30);
+			});
 		});
 	});
 
+	describe('header calendar', () => {
+		it('should display the exact days sequence', () => {
+			const sequence = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-	describe('last date in the correct day column (Monday as first column)', () => {
-		function getLastRowCellText(wrapper, column) {
-			return wrapper
-				.find('.theme-calendar-body-row')
-				.last()
-				.find('.theme-calendar-item')
-				.at(column - 1)
-				.find('DayPickerAction')
-				.prop('label');
-		}
-
-		it('should have monday in the 1st column for april 2018', () => {
-			const calendar = {
-				year: 2018,
-				monthIndex: 3,
-			};
-
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			expect(getLastRowCellText(wrapper, 1)).toBe('30');
-		});
-
-		it('should have thursday in the 4th column for may 2018', () => {
-			const calendar = {
-				year: 2018,
-				monthIndex: 4,
-			};
-
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			expect(getLastRowCellText(wrapper, 4)).toBe('31');
-		});
-
-		it('should have sunday in the 7th column for september 2018', () => {
-			const calendar = {
-				year: 2018,
-				monthIndex: 8,
-			};
-
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			expect(getLastRowCellText(wrapper, 7)).toBe('30');
-		});
-	});
-
-	describe('number of weeks displayed', () => {
-		function nbOfWeeksRendered(wrapper) {
-			return wrapper.find('.theme-calendar-body-row').length;
-		}
-
-		it('should have 4 weeks for february 2010', () => {
-			const calendar = {
-				year: 2010,
-				monthIndex: 1,
-			};
-
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			expect(nbOfWeeksRendered(wrapper)).toBe(4);
-		});
-
-		it('should have 5 weeks for june 2018', () => {
 			const calendar = {
 				year: 2018,
 				monthIndex: 5,
@@ -159,111 +283,13 @@ describe('DatePicker', () => {
 				currentDate={currentDate}
 			/>);
 
-			expect(nbOfWeeksRendered(wrapper)).toBe(5);
-		});
+			const sequenceExpected = sequence.map(day => day[0].toUpperCase());
 
-		it('should have 6 weeks for october 2017', () => {
-			const calendar = {
-				year: 2017,
-				monthIndex: 9,
-			};
+			const sequenceRendered = wrapper
+				.find('.theme-calendar-header-row .theme-calendar-item')
+				.map(item => item.text());
 
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			expect(nbOfWeeksRendered(wrapper)).toBe(6);
-		});
-	});
-
-	describe('contiguous ordered numbers', () => {
-		function getDayNumbers(wrapper) {
-			return wrapper
-				.find('.theme-calendar-body-row .theme-calendar-item')
-				.find('DayPickerAction')
-				.map(action => action.prop('label'))
-				.map(label => parseInt(label, 10));
-		}
-
-		function ascSorting(a, b) {
-			return a - b;
-		}
-
-		it('should have contiguous numbers for july 2018', () => {
-			const calendar = {
-				year: 2018,
-				monthIndex: 6,
-			};
-
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			const days = getDayNumbers(wrapper);
-			const copy = [...days];
-			days.sort(ascSorting);
-			expect(days).toEqual(copy);
-			expect(days[0]).toBe(1);
-			expect(days).toHaveLength(31);
-		});
-
-		it('should have contiguous numbers for february 2016', () => {
-			const calendar = {
-				year: 2016,
-				monthIndex: 1,
-			};
-
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			const days = getDayNumbers(wrapper);
-			const copy = [...days];
-			days.sort(ascSorting);
-			expect(days).toEqual(copy);
-			expect(days[0]).toBe(1);
-			expect(days).toHaveLength(29);
-		});
-
-		it('should have contiguous numbers for february 2017', () => {
-			const calendar = {
-				year: 2017,
-				monthIndex: 1,
-			};
-
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			const days = getDayNumbers(wrapper);
-			const copy = [...days];
-			days.sort(ascSorting);
-			expect(days).toEqual(copy);
-			expect(days[0]).toBe(1);
-			expect(days).toHaveLength(28);
-		});
-
-		it('should have contiguous numbers for november 2022', () => {
-			const calendar = {
-				year: 2022,
-				monthIndex: 10,
-			};
-
-			const wrapper = shallow(<DatePicker
-				calendar={calendar}
-				currentDate={currentDate}
-			/>);
-
-			const days = getDayNumbers(wrapper);
-			const copy = [...days];
-			days.sort(ascSorting);
-			expect(days).toEqual(copy);
-			expect(days[0]).toBe(1);
-			expect(days).toHaveLength(30);
+			expect(sequenceRendered).toEqual(sequenceExpected);
 		});
 	});
 });
