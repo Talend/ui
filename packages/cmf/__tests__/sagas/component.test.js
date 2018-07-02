0bc78bedebb3aa0ae7581b5fa61ee2c63ca1c320
Jean-Alain Ré <jare@talend.com>
2018-07-02 18:09:19 +0200

    fix review

--- packages/cmf/__tests__/sagas/component.test.js
+++ packages/cmf/__tests__/sagas/component.test.js
@@ -5,7 +5,7 @@ import { onSagaStart, handle } from '../../src/sagas/component';
 import CONST from '../../src/constant';
 
 describe('sagas.component', () => {
-	it('should cancel one saga with the good componentID', () => {
+	it('should cancel one saga ', () => {
 		// given
 		const testAction = { type: 'TEST', saga: 'my-saga', event: { componentId: 42 } };
 		function* saga() {}
@@ -17,11 +17,34 @@ describe('sagas.component', () => {
 
 		// then
 		expect(gen.next().value).toEqual(fork(saga, undefined));
-		expect(gen.next(task).value).toEqual(take(`${CONST.WILL_UNMOUNT_SAGA_STOP}_my-saga`));
-		expect(gen.next({ event: { componentId: 41 } }).value).toEqual(
-			take(`${CONST.WILL_UNMOUNT_SAGA_STOP}_my-saga`),
-		);
-		expect(gen.next({ event: { componentId: 42 } }).value).toEqual(cancel(task));
+		const next = gen.next(task).value;
+		expect(next.TAKE).toBeDefined();
+		expect(
+			next.TAKE.pattern({
+				type: `${CONST.WILL_UNMOUNT_SAGA_STOP}_my-saga`,
+				event: {
+					componentId: 41,
+				},
+			}),
+		).toBeFalsy();
+		expect(
+			next.TAKE.pattern({
+				type: `${CONST.WILL_UNMOUNT_SAGA_STOP}_my-saga2`,
+				event: {
+					componentId: 42,
+				},
+			}),
+		).toBeFalsy();
+		expect(
+			next.TAKE.pattern({
+				type: `${CONST.WILL_UNMOUNT_SAGA_STOP}_my-saga`,
+				event: {
+					componentId: 42,
+				},
+			}),
+		).toBeTruthy();
+		expect(gen.next().value).toEqual(cancel(task));
+		expect(gen.next().done).toBe(true);
 	});
 
 	it('should onSagaStart forks action.saga with params and waits for unmount cancelling*', () => {
@@ -41,7 +64,8 @@ describe('sagas.component', () => {
 
 		// then
 		expect(gen.next().value).toEqual(fork(saga, { myProps: 'MyProps' }));
-		expect(gen.next(task).value).toEqual(take(`${CONST.WILL_UNMOUNT_SAGA_STOP}_my-saga`));
+		const next = gen.next(task).value;
+		expect(next.TAKE).toBeDefined();
 		expect(gen.next({ event: { componentId: 42 } }).value).toEqual(cancel(task));
 	});
 
