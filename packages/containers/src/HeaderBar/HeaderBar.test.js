c72a2aea492ac2eb3d6e055d567538e330b50bec
Jean-Alain Ré <jare@talend.com>
2018-07-04 09:49:48 +0200

    improve test

--- packages/containers/src/HeaderBar/HeaderBar.test.js
+++ packages/containers/src/HeaderBar/HeaderBar.test.js
@@ -13,44 +13,32 @@ describe('Container HeaderBar', () => {
 
 	it('should render HeaderBar container', () => {
 		const wrapper = shallow(<Container {...containerProps} />);
-		expect(
-			wrapper
-				.dive()
-				.dive()
-				.getElement(),
-		).toMatchSnapshot();
+		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 
 	it('should render HeaderBar container with a list of items', () => {
 		const props = {
-			productsItems: [{ foo: 'bar' }],
 			...containerProps,
+			productsItems: [{ foo: 'bar' }],
+			state: new Map({
+				productsFetchState: Constants.FETCH_PRODUCTS_SUCCESS,
+			}),
 		};
 
 		const wrapper = shallow(<Container {...props} />);
-		expect(
-			wrapper
-				.dive()
-				.dive()
-				.getElement(),
-		).toMatchSnapshot();
+		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 
 	it('should render HeaderBar container while fetching items', () => {
 		const props = {
 			...containerProps,
 			state: new Map({
-				get: jest.fn().mockReturnValue(Constants.FETCHING_PRODUCTS),
+				productsFetchState: Constants.FETCHING_PRODUCTS,
 			}),
 		};
 
 		const wrapper = shallow(<Container {...props} />);
-		expect(
-			wrapper
-				.dive()
-				.dive()
-				.getElement(),
-		).toMatchSnapshot();
+		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 });
 
