9ecba633012bf0b5a614c5d7f0b662721f7f6858
Fabien Rassinier <frassinier@talend.com>
2018-07-12 14:38:31 +0200

    feat(cmf/expressions): add oneOf expression

---
+++ packages/cmf/src/expressions/oneOf.js
@@ -0,0 +1,13 @@
+import _get from 'lodash/get';
+import Immutable from 'immutable';
+
+export default function getOneOfFunction(statePath) {
+	return function includes({ context }, immutablePath, values) {
+		if (!Array.isArray(values)) {
+			throw new Error("You should pass an array of values to check if one of them is present");
+		}
+		const arr = _get(context.store.getState(), statePath, new Immutable.Map())
+			.getIn(immutablePath.split('.'), new Immutable.List());
+		return values.some(value => arr.includes(value));
+	};
+}
