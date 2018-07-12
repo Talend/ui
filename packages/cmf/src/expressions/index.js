8f5cfc17fab47f9652e47c56b0e9de09d1c99529
Fabien Rassinier <frassinier@talend.com>
2018-07-12 16:21:28 +0200

    feat(cmf/expressions): add oneOf expression (#1536)
    **What is the problem this PR is trying to solve?**
    I can check if an array includes 1 value but I can't check one of several values.
    It's an issue for permissions.
    
    **What is the chosen solution to this problem?**
    Implement `cmf.collections.oneOf` expression to test several values. 👌 
    
    **Please check if the PR fulfills these requirements**
    
    * [x] The PR commit message follows our [guidelines](https://github.com/talend/tools/blob/master/tools-root-github/CONTRIBUTING.md)
    * [x] Tests for the changes have been added (for bug fixes / features) And [non reg](./screenshots.md) done before need review
    * [x] Docs have been added / updated (for bug fixes / features)
    * [ ] Related design / discussions / pages (not in jira), if any, are all linked or available in the PR
    
    <!-- You can add more checkboxes here -->
    
    **[ ] This PR introduces a breaking change**
    
    <!-- if the PR introduces a breaking change, add the description here. So when you merge this PR, add this description into the [breaking change wiki](https://github.com/Talend/ui/wiki/BREAKING-CHANGE) in the next version -->
    
    <!-- **Original Template** -->
    
    <!-- https://github.com/Talend/tools/blob/master/tools-root-github/.github/PULL_REQUEST_TEMPLATE.md -->

--- packages/cmf/src/expressions/index.js
+++ packages/cmf/src/expressions/index.js
@@ -1,5 +1,6 @@
 import getInState from './getInState';
 import includes from './includes';
+import oneOf from './oneOf';
 import * as router from './router';
 
 export default {
@@ -7,6 +8,7 @@ export default {
 	'cmf.components.get': getInState('cmf.components'),
 	'cmf.collections.includes': includes('cmf.collections'),
 	'cmf.components.includes': includes('cmf.components'),
+	'cmf.collections.oneOf': oneOf('cmf.collections'),
 	'cmf.router.matchPath': router.matchPath,
 	'cmf.router.location': router.location,
 };
