1f35541545becedd3b0d06e78070e8cea43b897a
travis <travis>
2018-07-12 13:04:25 +0000

    test(ci): prettier

--- packages/cmf/src/expressions/oneOf.js
+++ packages/cmf/src/expressions/oneOf.js
@@ -4,10 +4,12 @@ import Immutable from 'immutable';
 export default function getOneOfFunction(statePath) {
 	return function includes({ context }, immutablePath, values) {
 		if (!Array.isArray(values)) {
-			throw new Error("You should pass an array of values to check if one of them is present");
+			throw new Error('You should pass an array of values to check if one of them is present');
 		}
-		const arr = _get(context.store.getState(), statePath, new Immutable.Map())
-			.getIn(immutablePath.split('.'), new Immutable.List());
+		const arr = _get(context.store.getState(), statePath, new Immutable.Map()).getIn(
+			immutablePath.split('.'),
+			new Immutable.List(),
+		);
 		return values.some(value => arr.includes(value));
 	};
 }
