c73fcb5f14f1a6fc73981247dca6e3e2f318a838
Jérémie DESCOTTES <jdescottes@talend.com>
2018-07-13 16:31:27 +0200

    chore: Tests naming

--- packages/components/src/DateTimePickers/pickers/IncrementableScrollList/IncrementableScrollList.test.js
+++ packages/components/src/DateTimePickers/pickers/IncrementableScrollList/IncrementableScrollList.test.js
@@ -93,7 +93,7 @@ describe('IncrementableScrollList', () => {
 	});
 
 	describe('keep track of first visible row index', () => {
-		it('should initialized first visible row index with 0 when "initialIndex" is no set', () => {
+		it('should initialize first visible row index with 0 when "initialIndex" is no set', () => {
 			const wrapper = shallow(
 				<IncrementableScrollList
 					items={[]}
@@ -104,7 +104,7 @@ describe('IncrementableScrollList', () => {
 			expect(wrapper.state('startIndex')).toBe(0);
 		});
 
-		it('should initialized first visible row index based on "initialIndex"', () => {
+		it('should initialize first visible row index based on "initialIndex"', () => {
 			const wrapper = shallow(
 				<IncrementableScrollList
 					items={[]}
@@ -134,7 +134,7 @@ describe('IncrementableScrollList', () => {
 		});
 	});
 
-	it('should ask to scroll to the previous 5 items when click in the top button action', () => {
+	it('should scroll to the previous 5 items when clicking on the top button', () => {
 		const items = Array(100).fill();
 
 		const wrapper = shallow(
@@ -161,7 +161,7 @@ describe('IncrementableScrollList', () => {
 		expect(scrollToRow).toHaveBeenCalledWith(48);
 	});
 
-	it('should ask to scroll to the next 5 items when click in the bottom button action', () => {
+	it('should scroll to the next 5 items when clicking on the bottom button', () => {
 		const items = Array(100).fill();
 
 		const wrapper = shallow(
@@ -188,7 +188,7 @@ describe('IncrementableScrollList', () => {
 		expect(scrollToRow).toHaveBeenCalledWith(58);
 	});
 
-	it('should ask for the minimum index available when scrolling previous items while at a range of it lower than the display items range', () => {
+	it('should scroll to the minimum item available when clicking on the top button while at a range of it lower than the display items range', () => {
 		const items = Array(58).fill();
 
 		const wrapper = shallow(
@@ -215,7 +215,7 @@ describe('IncrementableScrollList', () => {
 		expect(scrollToRow).toHaveBeenCalledWith(0);
 	});
 
-	it('should ask for the maximum index available when scrolling next items while at a range of it lower than the display items range', () => {
+	it('should scroll to the maximum item available when clicking on the bottom button while at a range of it lower than the display items range', () => {
 		const items = Array(42).fill();
 
 		const wrapper = shallow(
@@ -242,7 +242,7 @@ describe('IncrementableScrollList', () => {
 		expect(scrollToRow).toHaveBeenCalledWith(37);
 	});
 
-	it('should not ask for index changing when scrolling previous items while already on minimum index', () => {
+	it('should not scroll when clicking on the top button while already on minimum item', () => {
 		const items = Array(98).fill();
 
 		const wrapper = shallow(
@@ -269,7 +269,7 @@ describe('IncrementableScrollList', () => {
 		expect(scrollToRow).not.toHaveBeenCalled();
 	});
 
-	it('should not ask for index changing when scrolling next items while already on maximum index', () => {
+	it('should not scroll when clicking on the bottom button while already on maximum item', () => {
 		const items = Array(96).fill();
 
 		const wrapper = shallow(
