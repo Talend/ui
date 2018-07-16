28635a095e6f190fa47b05d061794d75d4c20c04
Jimmy Somsanith <jsomsanith@talend.com>
2018-07-16 11:14:14 +0200

    Fallback to textarea

--- packages/forms/src/UIForm/fields/Code/Code.component.js
+++ packages/forms/src/UIForm/fields/Code/Code.component.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import FieldTemplate from '../FieldTemplate';
+import TextArea from '../TextArea';
 
 const DEFAULT_SET_OPTIONS = {
 	enableBasicAutocompletion: true,
@@ -8,87 +9,96 @@ const DEFAULT_SET_OPTIONS = {
 	enableSnippets: true,
 };
 
-export default class Code extends React.Component {
-	constructor(props) {
-		super(props);
-		this.onChange = this.onChange.bind(this);
-		this.onFinish = this.onFinish.bind(this);
-		this.onLoad = this.onLoad.bind(this);
+let CodeWidget = TextArea;
+
+try {
+	// eslint-disable-next-line global-require,import/no-extraneous-dependencies
+	const AceEditor = require('react-ace').default;
+
+	class Code extends React.Component {
+		constructor(props) {
+			super(props);
+			this.onChange = this.onChange.bind(this);
+			this.onFinish = this.onFinish.bind(this);
+			this.onLoad = this.onLoad.bind(this);
+		}
+
+		onChange(value, event) {
+			this.props.onChange(event, { schema: this.props.schema, value });
+		}
+
+		onFinish(event) {
+			this.props.onFinish(event, { schema: this.props.schema });
+		}
+
+		onLoad(editor) {
+			editor.textInput.getElement().setAttribute('id', this.props.id);
+		}
+
+		render() {
+			const { id, isValid, errorMessage, schema, value } = this.props;
+			const { autoFocus, description, disabled = false, options, readOnly = false, title } = schema;
+
+			return (
+				<FieldTemplate
+					description={description}
+					errorMessage={errorMessage}
+					id={id}
+					isValid={isValid}
+					label={title}
+					required={schema.required}
+				>
+					<AceEditor
+						className="tf-widget-code form-control"
+						disabled={disabled}
+						focus={autoFocus}
+						name={`${id}_wrapper`}
+						enableSnippets
+						mode={options && options.language}
+						onBlur={this.onFinish}
+						onLoad={this.onLoad}
+						onChange={this.onChange}
+						readOnly={readOnly}
+						setOptions={DEFAULT_SET_OPTIONS}
+						showGutter={false}
+						showPrintMargin={false}
+						theme="chrome"
+						value={value}
+						width="auto"
+						{...options}
+					/>
+				</FieldTemplate>
+			);
+		}
+	}
+	if (process.env.NODE_ENV !== 'production') {
+		Code.propTypes = {
+			id: PropTypes.string,
+			isValid: PropTypes.bool,
+			errorMessage: PropTypes.string,
+			onChange: PropTypes.func.isRequired,
+			onFinish: PropTypes.func.isRequired,
+			schema: PropTypes.shape({
+				autoFocus: PropTypes.bool,
+				description: PropTypes.string,
+				disabled: PropTypes.bool,
+				options: PropTypes.object,
+				readOnly: PropTypes.bool,
+				title: PropTypes.string,
+				type: PropTypes.string,
+			}),
+			value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+		};
 	}
 
-	onChange(value, event) {
-		this.props.onChange(event, { schema: this.props.schema, value });
-	}
-
-	onFinish(event) {
-		this.props.onFinish(event, { schema: this.props.schema });
-	}
-
-	onLoad(editor) {
-		editor.textInput.getElement().setAttribute('id', this.props.id);
-	}
-
-	render() {
-		const { id, isValid, errorMessage, schema, value } = this.props;
-		const { autoFocus, description, disabled = false, options, readOnly = false, title } = schema;
-
-		// eslint-disable-next-line global-require,import/no-extraneous-dependencies
-		const AceEditor = require('react-ace').default;
-
-		return (
-			<FieldTemplate
-				description={description}
-				errorMessage={errorMessage}
-				id={id}
-				isValid={isValid}
-				label={title}
-				required={schema.required}
-			>
-				<AceEditor
-					className="tf-widget-code form-control"
-					disabled={disabled}
-					focus={autoFocus}
-					name={`${id}_wrapper`}
-					enableSnippets
-					mode={options && options.language}
-					onBlur={this.onFinish}
-					onLoad={this.onLoad}
-					onChange={this.onChange}
-					readOnly={readOnly}
-					setOptions={DEFAULT_SET_OPTIONS}
-					showGutter={false}
-					showPrintMargin={false}
-					theme="chrome"
-					value={value}
-					width="auto"
-					{...options}
-				/>
-			</FieldTemplate>
-		);
-	}
-}
-
-if (process.env.NODE_ENV !== 'production') {
-	Code.propTypes = {
-		id: PropTypes.string,
-		isValid: PropTypes.bool,
-		errorMessage: PropTypes.string,
-		onChange: PropTypes.func.isRequired,
-		onFinish: PropTypes.func.isRequired,
-		schema: PropTypes.shape({
-			autoFocus: PropTypes.bool,
-			description: PropTypes.string,
-			disabled: PropTypes.bool,
-			options: PropTypes.object,
-			readOnly: PropTypes.bool,
-			title: PropTypes.string,
-			type: PropTypes.string,
-		}),
-		value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	Code.defaultProps = {
+		isValid: true,
+		schema: {},
 	};
+
+	CodeWidget = Code;
+} catch (error) {
+	console.warn('CodeWidget react-ace not found, fallback to Textarea');
 }
 
-Code.defaultProps = {
-	isValid: true,
-	schema: {},
-};
+export default CodeWidget;
