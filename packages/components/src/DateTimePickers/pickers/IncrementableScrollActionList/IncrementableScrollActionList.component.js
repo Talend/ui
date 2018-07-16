d0a1320a094b35458dd49a6b29195db30bbc4e19
Jérémie DESCOTTES <jdescottes@talend.com>
2018-07-16 15:08:49 +0200

    feat: Implement the IncrementableScrollActionList wrapper

--- packages/components/src/DateTimePickers/pickers/IncrementableScrollActionList/IncrementableScrollActionList.component.js
+++ packages/components/src/DateTimePickers/pickers/IncrementableScrollActionList/IncrementableScrollActionList.component.js
@@ -1,20 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import IncrementableScrollList from './IncrementableScrollList';
+import PickerAction from '../../PickerAction';
 
 class IncrementableScrollActionList extends React.Component {
-	static propTypes = {
-		name: PropTypes.string,
-	};
 
 	constructor(props) {
 		super(props);
+
+		const middleInitialIndex = this.props.items.findIndex(
+			item => item.id === this.props.initialMiddleVisibleItemId);
+
+		this.initialIndex = middleInitialIndex === -1
+			? 0
+			: Math.max(middleInitialIndex - 2, 0);
+	}
+
+	isSelected(id) {
+		return id === this.props.selectedItemId;
 	}
 
 	render() {
+		const itemRenderer = item => {
+			const { id, label } = item;
+			return (
+				<PickerAction
+					aria-label={`Select '${label}'`}
+					isSelected={this.isSelected(id)}
+					label={label}
+					onClick={() => this.props.onSelect(item)}
+				/>
+			);
+		};
+
 		return (
-			<div>{this.props.name}</div>
+			<IncrementableScrollList
+				items={this.props.items}
+				initialIndex={this.initialIndex}
+				itemRenderer={itemRenderer}
+			/>
 		);
 	}
 }
+
+IncrementableScrollActionList.propTypes = {
+	items: PropTypes.arrayOf(PropTypes.shape({
+		id: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.number,
+		]).isRequired,
+		label: PropTypes.string.isRequired,
+	})).isRequired,
+	initialMiddleVisibleItemId: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.number,
+	]),
+	selectedItemId: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.number,
+	]),
+	onSelect: PropTypes.func.isRequired,
+};
 
 export default IncrementableScrollActionList;
