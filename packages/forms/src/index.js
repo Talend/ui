d5cded1adb748ac6c371015192c635fcb2ee5184
travis <travis>
2018-07-18 09:10:36 +0000

    test(ci): prettier

--- packages/forms/src/index.js
+++ packages/forms/src/index.js
@@ -1,8 +1,6 @@
 import { I18N_DOMAIN_FORMS } from './constants';
 import FormSwitcher from './FormSwitcher';
-import {
-    customWidgets,
-} from './Form';
+import { customWidgets } from './Form';
 
 export default FormSwitcher;
 export { I18N_DOMAIN_FORMS, customWidgets };
