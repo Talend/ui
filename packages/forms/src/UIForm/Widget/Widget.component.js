cb30fb512bf7670a297af93e247e9f3593ba575f
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-07-18 15:02:46 +0200

    splitting more condition and widget.component to isolate both logics

--- packages/forms/src/UIForm/Widget/Widget.component.js
+++ packages/forms/src/UIForm/Widget/Widget.component.js
@@ -1,55 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import get from 'lodash/get';
-import includes from 'lodash/includes';
 import { sfPath } from '@talend/json-schema-form-core';
 
 import defaultWidgets from '../utils/widgets';
 import { getError } from '../utils/errors';
 import { getValue } from '../utils/properties';
-import getConditionValue from './condition';
-
-function evaluateCondition(properties, condition) {
-	const value = get(properties, condition.path);
-	const actual = getConditionValue(value, condition.strategy);
-	return (condition.shouldBe !== false) === includes(condition.values, actual);
-}
-
-/**
- * Evaluate a list (array) of conditions against a contextual set of values (properties).
- * Condition specification is done through objects with the following attributes:
- * <ul>
- *   <li><em>values</em> (required): list of values compared to the evaluated value</li>
- *   <li><em>path</em> (required): the path used to extract the value from properties
- *   to use to compare to values</li>
- *   <li><em>shouldBe</em> (optional): should the evaluation activate this widget when
- *   true (default) or false (when shouldBe = true).</li>
- *   <li><em>strategy</em> (optional): by default the property value extraction
- *   just uses the path, but for some cases like array you can want some more advanced strategy
- *   like extracting the length. For such cases, there are some <code>evaluationStrategy</code>.
- *   Currently you can set this property to <code>length</code> to evaluate the length of an array
- *   when the extracted instance is an array, otherwise it will set the value to zero. Other
- *   strategy values will just return false.</li>
- * </ul>
- *
- * @example {
- *   values:["A", "B"],
- *   path: "someProp.someArray",
- *   shouldBe: true,
- *   evaluationStrategy: "length"
- * }
- *
- * The combination of the conditions is done through an <code>AND</code>
- * whereas, for a single condition, the test against <code>values</code>
- * is an <code>includes</code> (<code>OR</code>).
- *
- * @param properties source of the value provider to evaluate conditions.
- * @param conditions array of conditions to evaluate.
- * @returns true if the conditions are met, false otherwise.
- */
-function shouldRender(conditions, properties) {
-	return !conditions || conditions.every(cond => evaluateCondition(properties, cond));
-}
+import shouldRender from './condition';
 
 export default function Widget(props) {
 	const { conditions, key, options, type, validationMessage, widget } = props.schema;
