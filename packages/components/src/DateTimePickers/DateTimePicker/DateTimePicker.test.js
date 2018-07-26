76f2ef6abe733f5da45e791ad8dbe3aa0516f806
Jérémie DESCOTTES <jdescottes@talend.com>
2018-07-26 17:47:44 +0200

    chore: Remove useless test for now

--- packages/components/src/DateTimePickers/DateTimePicker/DateTimePicker.test.js
+++ packages/components/src/DateTimePickers/DateTimePicker/DateTimePicker.test.js
@@ -158,30 +158,6 @@ describe('DateTimePicker', () => {
 		expect(wrapper.state('selectedTime')).toBe(initTime);
 	});
 
-	// TODO: Enable when the submit button will be implemented
-	it.skip('should give back "date" and "time" selected if selected when submited', () => {
-		const date = new Date(2018, 1, 1);
-		const time = 1000;
-		const onSubmit = jest.fn();
-		const wrapper = shallow(<DateTimePicker
-			selectedDate={date}
-			onSubmit={onSubmit}
-		/>);
-
-		wrapper.setState({
-			isDateTimeView: true,
-		});
-
-		// TODO: Switch with the future internal submit button simulation directly
-		const dateTimeView = wrapper.find(DateTimeView);
-		dateTimeView.prop('onSelectTime')(time);
-
-		expect(onSubmit).toHaveBeenCalledWith({
-			date,
-			time,
-		});
-	});
-
 	it('should auto submit when a date or time is selected and both are selected at this time', () => {
 		const date = new Date(2018, 1, 1);
 		const time = 1000;
