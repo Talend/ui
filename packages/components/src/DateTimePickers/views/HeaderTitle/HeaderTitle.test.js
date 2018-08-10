ca1858bc9e0fa06cee975c22129d183eff711fa7
Jérémie Descottes <38074242+jdescottes-talend@users.noreply.github.com>
2018-08-10 13:57:07 +0200

    feat(components): date picker draft (#1581)

---
+++ packages/components/src/DateTimePickers/views/HeaderTitle/HeaderTitle.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import HeaderTitle from './HeaderTitle.component';
+
+describe('HeaderTitle', () => {
+	it('should render a span', () => {
+		// When
+		const wrapper = shallow(<HeaderTitle monthIndex={8} year={2012} />);
+
+		// Then
+		expect(wrapper.name()).toEqual('span');
+		expect(wrapper.getElement()).toMatchSnapshot();
+	});
+
+	it('should render a button', () => {
+		// When
+		const wrapper = shallow(
+			<HeaderTitle monthIndex={8} year={2012} button={{ whateverButtonProp: 'whateverValue' }} />,
+		);
+
+		// Then
+		expect(wrapper.name()).toEqual('button');
+		expect(wrapper.getElement()).toMatchSnapshot();
+	});
+
+	it('should render the correct date and format', () => {
+		const wrapperSpan = shallow(<HeaderTitle monthIndex={2} year={2001} />);
+		const wrapperButton = shallow(<HeaderTitle monthIndex={11} year={2002} />);
+
+		expect(wrapperSpan.text()).toBe('March 2001');
+		expect(wrapperButton.text()).toBe('December 2002');
+	});
+});
