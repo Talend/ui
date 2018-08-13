fa37f468cd79e0b842e651aeb8f0e608574dd52a
Jean-Alain Ré <jare@talend.com>
2018-08-13 17:22:13 +0200

    fix error

--- packages/icons/src/extract.js
+++ packages/icons/src/extract.js
@@ -6,15 +6,14 @@ function extractFiles(folder) {
 	const dir = path.join(__dirname, folder);
 
 	// case insensitive sort for windows users
-	return
-		fs.readdirSync(dir)
+	return fs.readdirSync(dir)
 		.sort(function(a, b) {
 			return a.localeCompare(b, { sensitivity: 'base' });
 		})
 		.reduce(
 			(state, file) => Object.assign(state, {[file.split('.svg')[0]]: fs.readFileSync(path.resolve(dir, file))}),
 			{}
-		)
+		);
 }
 
 exports.default = extractFiles;
