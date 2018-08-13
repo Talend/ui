8cde76e2320809f8666f33300e510c57e4a6effd
smouillour <smouillour@talend.com>
2018-08-13 12:05:24 +0200

    review round 2

--- packages/components/src/Enumeration/Header/Header.component.js
+++ packages/components/src/Enumeration/Header/Header.component.js
@@ -36,11 +36,8 @@ function getAction(action, index) {
 		<Action
 			{...action}
 			key={`${index}-enum-header-action`}
-			label={action.label}
-			icon={action.icon}
 			onClick={onClick}
 			btooltipPlacement="bottom"
-			inProgress={action.inProgress}
 			hideLabel
 			link
 		/>
