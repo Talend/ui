fea81f5b7d3cdff25acc1ab9fe517d2079b1f652
Andreas Nilsson <anilsson@talend.com>
2018-08-24 16:14:07 +0200

    feat(local-libs-webpack-plugin): add package and mainSrc field

---
+++ packages/local-libs-webpack-plugin/README.md
@@ -0,0 +1,50 @@
+# @talend/local-libs-webpack-plugin
+
+Easier local development on libraries. Skip prepublish, npm link, and copying libs into node_modules. 
+
+**This plugin is meant to be used in development only**
+
+LocalLibsWebpackPlugin will let you use npm modules locally, without using npm link or modifying import paths. For best experience the library you want to use should have a `"mainSrc"` field in package.json, specifying where the **source** entry file is. `"main"` usually refer to a dist or lib folder. 
+
+# Install
+`$ yarn add --dev @talend/local-libs-webpack-plugin`
+
+# Usage
+
+webpack.config.js
+```js
+const LocalLibsWebpackPlugin = require('@talend/local-libs-webpack-plugin');
+
+module.exports = {
+	plugins: [
+		new LocalLibsWebpackPlugin: [
+			'../relative/path/to/package.json',
+			'/can/also/use/lerna.json',
+		],
+	],
+};
+```
+
+It can also be useful to use webpack's env variables for conditional options:
+
+Example command: `$ yarn start --env mylib myotherlib`
+
+webpack.config.js
+```js
+const LocalLibsWebpackPlugin = require('@talend/local-libs-webpack-plugin');
+
+module.exports = {
+	plugins: [
+		new LocalLibsWebpackPlugin: {
+			'../mylib/package.json': env.mylib,
+			'../myotherlib/lerna.json': env.myotherlib,
+		},
+	],
+};
+```
+
+## LICENSE
+
+Copyright (c) 2006-2016 Talend
+
+Licensed under the Apache V2 License
---
+++ packages/local-libs-webpack-plugin/index.js
@@ -0,0 +1,112 @@
+const path = require('path');
+
+function getJson(packagePath) {
+	return require(path.resolve(packagePath));
+}
+
+function isLerna(packagePath) {
+	if (packagePath.includes('lerna.json')) {
+		return true;
+	}
+	return false;
+}
+
+function pathWithoutFilename(filePath) {
+	if (indexOf('/') > 0) {
+		return filePath.substr(0, filePath.lastIndexOf('/'));
+	}
+	if (indexOf('.') === -1) {
+		return filePath;
+	}
+	return '';
+}
+
+function getPackageJsonInfo(packagePath) {
+	const packageJson = getJson(packagePath);
+	return {
+		name: packageJson.name,
+		path: path.resolve(packagePath.replace('/package.json', '')),
+		mainSrc: pathWithoutFilename(packageJson.mainSrc),
+		main: pathWithoutFilename(packageJson.main),
+		peerDependencies: packageJson.peerDependencies || {},
+	};
+}
+
+/**
+ * `mylib/lib/somefile` -> `mylib/src/somefile`
+ */
+function libToSrcPath(libPath) {
+	return libPath.replace(`${lib.name}/${lib.main}`, `${lib.name}/${lib.mainSrc}`);
+}
+
+function getLinkedLibs(paths) {
+	const linkedLibs = [];
+	paths.forEach(packagePath => {
+		if (isLerna(packagePath)) {
+			getJson(packagePath).packages.forEach(subPackagePath => {
+				const packageJsonPath = packagePath.replace('lerna.json', `${subPackagePath}/package.json`);
+				linkedLibs.push(getPackageJsonInfo(packageJsonPath));
+			});
+		} else {
+			linkedLibs.push(getPackageJsonInfo(packagePath));
+		}
+	});
+	return linkedLibs;
+}
+
+class LocalLibsWebpackPlugin {
+	constructor(options = []) {
+		if (Array.isArray(options)) {
+			this.options = options;
+		} else {
+			this.options = Object.entries(options)
+				.map(([key, value]) => {
+					if (value === true) {
+						return key;
+					}
+				})
+				.filter(option => option !== undefined);
+		}
+	}
+
+	apply(compiler) {
+		const linkedLibs = getLinkedLibs(this.options);
+		if (!linkedLibs.length) {
+			return;
+		}
+
+		// use src files instead of lib files (if mainSrc field exist in package.json)
+		// this is useful when you don't want to run prepublish after every change
+		compiler.options.resolve.mainFields = ['mainSrc', 'browser', 'module', 'main'];
+
+		// peerDependencies should use project's node_modules - not the library's
+		// this will avoid multiple instances of react issue for example
+		linkedLibs.forEach(lib => {
+			Object.keys(lib.peerDependencies).forEach(peerDependency => {
+				compiler.options.resolve.alias[peerDependency] = path.resolve('node_modules', peerDependency);
+			})
+		});
+		compiler.options.resolve.alias.react = path.resolve('node_modules/react');
+		compiler.options.resolve.alias['react-dom'] = path.resolve('node_modules/react-dom');
+
+		// Look in webpack repo (NormalModuleReplacementPlugin) how to convert this to webpack 4 when needed
+		compiler.plugin('normal-module-factory', nmf => {
+			nmf.plugin('before-resolve', (result, callback) => {
+				if (!result) {
+					return callback();
+				}
+				linkedLibs.forEach(lib => {
+					if (result.request.startsWith(lib.name)) {
+						if (lib.mainSrc && lib.main) {
+							result.request = libToSrcPath(result.request);
+						}
+						result.request = result.request.replace(lib.name, lib.path);
+					}
+				});
+				return callback(null, result);
+			});
+		});
+	}
+}
+
+module.exports = LocalLibsWebpackPlugin;
