4f0d38f78cb2ee1f0adfb3fade1d5277be93207e
Jimmy Somsanith <jsomsanith@talend.com>
2018-09-05 14:58:16 +0200

    feat(List/Large): improve a11y

--- packages/components/src/VirtualizedList/RowLarge/RowLarge.component.js
+++ packages/components/src/VirtualizedList/RowLarge/RowLarge.component.js
@@ -1,7 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
-import { extractSpecialFields, getId, getLabel, getRowData, renderCell } from '../utils/gridrow';
+import {
+	extractSpecialFields,
+	getCellData,
+	getId,
+	getLabel,
+	getRowData,
+	renderCell,
+} from '../utils/gridrow';
 
 import { listTypes } from '../utils/constants';
 import rowThemes from './RowThemes';
@@ -12,55 +19,82 @@ const { LARGE } = listTypes;
 /**
  * Row renderer that displays a Large item
  */
-function RowLarge({ className, index, key, parent, style }) {
-	const { titleField, selectionField, otherFields } = extractSpecialFields(parent);
-
-	const parentId = getId(parent);
-	const id = parentId && `${parentId}-${index}`;
-	const titleCell = titleField && renderCell(index, parent, titleField, LARGE);
-	const selectionCell = selectionField && renderCell(index, parent, selectionField);
-	const rowData = getRowData(parent, index);
-	const otherCellsListItems = otherFields.map((field, fieldIndex) => {
-		const cellContent = renderCell(index, parent, field);
-		const tooltip = typeof cellContent === 'string' ? cellContent : null;
-		const label = getLabel(field);
+class RowLarge extends React.Component {
+	shouldComponentUpdate(nextProps) {
 		return (
-			<li key={fieldIndex}>
-				{label && <span className={theme['field-label']}>{label}: </span>}
-				<span className={theme['field-value']} title={tooltip}>
-					{cellContent}
-				</span>
-			</li>
+			this.props.className !== nextProps.className ||
+			this.props.index !== nextProps.index ||
+			this.props.key !== nextProps.key ||
+			this.props.parent !== nextProps.parent ||
+			this.props.style !== nextProps.style
 		);
-	});
-
-	let onRowClick;
-	let onRowDoubleClick;
-	if (parent.props.onRowClick) {
-		onRowClick = event => parent.props.onRowClick({ event, rowData });
-	}
-	if (parent.props.onRowDoubleClick) {
-		onRowDoubleClick = event => parent.props.onRowDoubleClick({ event, rowData });
 	}
 
-	return (
-		<div
-			className={classNames('tc-list-item', rowThemes, rowData.className)}
-			key={key}
-			role="button"
-			onClick={onRowClick}
-			onDoubleClick={onRowDoubleClick}
-			style={style}
-		>
-			<div className={`tc-list-large-row ${theme['inner-box']} ${className}`} id={id}>
-				<div className={theme.header}>
-					{titleCell}
-					{selectionCell}
+	render() {
+		const { className, index, key, parent, style } = this.props;
+		const { titleField, selectionField, otherFields } = extractSpecialFields(parent);
+
+		const parentId = getId(parent);
+		const id = parentId && `${parentId}-${index}`;
+		const titleCell = titleField && renderCell(index, parent, titleField, LARGE);
+		const selectionCell = selectionField && renderCell(index, parent, selectionField);
+		const rowData = getRowData(parent, index);
+		const otherCellsListItems = (
+			<dl>
+				{otherFields.map((field, fieldIndex) => {
+					if (fieldIndex > 0) {
+						return null;
+					}
+					const cellContent = renderCell(index, parent, field);
+					const tooltip = typeof cellContent === 'string' ? cellContent : null;
+					const label = getLabel(field);
+					return [
+						<dt key={fieldIndex}>
+							{label && <span className={theme['field-label']}>{label}: </span>}
+						</dt>,
+						<dd className={theme['field-value']} title={tooltip}>
+							{cellContent}
+						</dd>,
+					];
+				})}
+			</dl>
+		);
+
+		let onRowClick;
+		let onRowDoubleClick;
+		if (parent.props.onRowClick) {
+			onRowClick = event => parent.props.onRowClick({ event, rowData });
+		}
+		if (parent.props.onRowDoubleClick) {
+			onRowDoubleClick = event => parent.props.onRowDoubleClick({ event, rowData });
+		}
+
+		return (
+			<div role="row" aria-rowindex={index + 1}>
+				<div
+					role="gridcell"
+					aria-colindex={1}
+					aria-label={titleField && getCellData(titleField, parent, index)}
+				>
+					<div
+						className={classNames('tc-list-item', rowThemes, rowData.className)}
+						key={key}
+						role="button"
+						onClick={onRowClick}
+						onDoubleClick={onRowDoubleClick}
+						style={style}
+						tabIndex="0"
+					>
+						{selectionCell}
+						<dl className={`tc-list-large-row ${theme['inner-box']} ${className}`} id={id}>
+							<dt>{titleCell}</dt>
+							<dd>{otherCellsListItems}</dd>
+						</dl>
+					</div>
 				</div>
-				<ul className={theme.content}>{otherCellsListItems}</ul>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 RowLarge.displayName = 'VirtualizedList(RowLarge)';
--- packages/components/src/VirtualizedList/RowLarge/index.js
+++ packages/components/src/VirtualizedList/RowLarge/index.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import RowLarge from './RowLarge.component';
 import { listTypes } from '../utils/constants';
 
 export const rowType = listTypes.LARGE;
-export default RowLarge;
+export default props => <RowLarge {...props} />;
