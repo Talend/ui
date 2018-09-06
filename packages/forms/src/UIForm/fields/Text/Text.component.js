fd83e2154f8d6dbdd96ca64cdc1294bb0ec49471
Steven Gangnant <sgangnant@talend.com>
2018-09-06 17:12:34 +0200

    feat(Text) Overlay support

--- packages/forms/src/UIForm/fields/Text/Text.component.js
+++ packages/forms/src/UIForm/fields/Text/Text.component.js
@@ -1,12 +1,23 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
+
 import FieldTemplate from '../FieldTemplate';
 import { generateDescriptionId, generateErrorId } from '../../Message/generateId';
-
 import { convertValue } from '../../utils/properties';
 
 export default function Text(props) {
-	const { id, isValid, errorMessage, onChange, onFinish, schema, value } = props;
+	const {
+		id,
+		isValid, errorMessage,
+		onChange,
+		onFinish,
+		schema,
+		value,
+		overlayComponent,
+		overlayPlacement,
+	} = props;
+
 	const {
 		autoFocus,
 		description,
@@ -23,6 +34,35 @@ export default function Text(props) {
 	const descriptionId = generateDescriptionId(id);
 	const errorId = generateErrorId(id);
 
+	const input = (<input
+		id={id}
+		autoFocus={autoFocus}
+		className="form-control"
+		disabled={disabled}
+		onBlur={event => onFinish(event, { schema })}
+		onChange={event => onChange(event, { schema, value: convertValue(type, event.target.value) })}
+		placeholder={placeholder}
+		readOnly={readOnly}
+		type={type}
+		value={value}
+		aria-invalid={!isValid} // eslint-disable-line jsx-a11y/aria-proptypes
+		aria-required={schema.required}
+		aria-describedby={`${descriptionId} ${errorId}`}
+	/>);
+
+	let field;
+
+	if (overlayComponent) {
+		const overlayProps = {
+			overlay: <Popover>{overlayComponent}</Popover>,
+			placement: overlayPlacement,
+		};
+
+		field = <OverlayTrigger {...overlayProps}>{input}</OverlayTrigger>;
+	} else {
+		field = input;
+	}
+
 	return (
 		<FieldTemplate
 			description={description}
@@ -35,24 +75,7 @@ export default function Text(props) {
 			labelAfter
 			required={schema.required}
 		>
-			<input
-				id={id}
-				autoFocus={autoFocus}
-				className="form-control"
-				disabled={disabled}
-				onBlur={event => onFinish(event, { schema })}
-				onChange={event =>
-					onChange(event, { schema, value: convertValue(type, event.target.value) })
-				}
-				placeholder={placeholder}
-				readOnly={readOnly}
-				type={type}
-				value={value}
-				// eslint-disable-next-line jsx-a11y/aria-proptypes
-				aria-invalid={!isValid}
-				aria-required={schema.required}
-				aria-describedby={`${descriptionId} ${errorId}`}
-			/>
+			{field}
 		</FieldTemplate>
 	);
 }
@@ -74,6 +97,8 @@ if (process.env.NODE_ENV !== 'production') {
 			type: PropTypes.string,
 		}),
 		value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+		overlayComponent: PropTypes.element,
+		overlayPlacement: OverlayTrigger.propTypes.placement,
 	};
 }
 
@@ -81,4 +106,5 @@ Text.defaultProps = {
 	isValid: true,
 	schema: {},
 	value: '',
+	overlayPlacement: 'right',
 };
