9134ddd383448343f3efacbe97e44dda9ba619ee
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2018-09-24 12:03:45 +0200

    doc: update cqrs README

--- packages/cmf-cqrs/src/index.js
+++ packages/cmf-cqrs/src/index.js
@@ -5,5 +5,6 @@ import reducers from './reducers';
 import { ackProcessed } from './reducers/ack';
 import * as constants from './constants';
 import sagas from './sagas';
+import cmfModule from './cmfModule';
 
-export { actions, ACKDispatcher, constants, middlewares, reducers, sagas, ackProcessed };
+export { actions, ACKDispatcher, cmfModule, constants, middlewares, reducers, sagas, ackProcessed };
