4d3d8c33209d2bc6f09b99808694473015a77bb1
Jimmy Somsanith <jsomsanith@talend.com>
2018-09-26 15:06:10 +0200

    Refactor

--- packages/forms/src/UIForm/Widget/Widget.component.js
+++ packages/forms/src/UIForm/Widget/Widget.component.js
@@ -20,84 +20,11 @@ function getWidget(displayMode, widgetId, customWidgets) {
 	return widget;
 }
 
-/**
- * if in the path [] appears it will be populated
- * with current key indices value.
- */
-function valorizeArrayIndices(path, key) {
-	if (!path || !key || !Array.isArray(key)) {
-		return path;
-	}
-	let keyIndex = 0;
-	return path.split('.').reduce((acc, current) => {
-		if (acc) {
-			acc += '.';
-		}
-		if (current.endsWith('[]')) {
-			acc += `${current.substring(0, current.length - 2)}.${key[keyIndex + 1]}`;
-			keyIndex += 2;
-		} else {
-			acc += current;
-			keyIndex += 1;
-		}
-		return acc;
-	}, '');
-}
-
-/**
- * For all "var" condition, populate generic indices ([]) from key indices.
- */
-function mapConditionItem(item, key) {
-	if (item.var) {
-		return {
-			...item,
-			var: valorizeArrayIndices(item.var, key),
-		};
-	}
-	if (Array.isArray(item)) {
-		return item.map(it => mapConditionVars(it, key));
-	}
-	return mapConditionVars(item, key);
-}
-
-/**
- * Ensure generic indices ([]) are populated from the key.
- * It is a recursive implementation to support any kind of condition.
- * WARN: this is an internal here, don't export it anyhow.
- */
-function mapConditionVars(condition, key) {
-	if (!condition) {
-		return condition;
-	}
-
-	const conditionType = typeof condition;
-	switch (conditionType) {
-		// primitive are passthrough, we just want to update "var" values
-		case 'boolean':
-		case 'number':
-		case 'string':
-			return condition;
-
-		// an object so browse it recursively to replace var values
-		default:
-			return Object.keys(condition).reduce((acc, attribute) => {
-				const value = condition[attribute];
-				if (Array.isArray(value)) {
-					acc[attribute] = value.map(it => mapConditionItem(it, key));
-				} else {
-					acc[attribute] = mapConditionItem(value, key);
-				}
-				return acc;
-			}, {});
-	}
-}
-
 export default function Widget(props) {
 	const { condition, key, options, type, validationMessage, widget, displayMode } = props.schema;
 	const widgetId = widget || type;
-	const runtimeCondition = mapConditionVars(condition, key);
 
-	if (widgetId === 'hidden' || !shouldRender(runtimeCondition, props.properties)) {
+	if (widgetId === 'hidden' || !shouldRender(condition, props.properties, key)) {
 		return null;
 	}
 
