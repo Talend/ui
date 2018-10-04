2fd74d0bf6eb15f12831ad0fda7481da71e54cc4
Jimmy Somsanith <jsomsanith@talend.com>
2018-10-04 13:30:14 +0000

    fix(ComponentForm): flatten use option to include objects (#1739)

--- packages/containers/src/ComponentForm/kit/flatten.js
+++ packages/containers/src/ComponentForm/kit/flatten.js
@@ -15,31 +15,39 @@
  *  limitations under the License.
  */
 
-function step(anything, key, payload) {
+function step(anything, key, payload, options = {}) {
 	// on native values, we just add it in the payload
 	if (typeof anything !== 'object') {
 		payload[key] = anything;
 	} else {
-		// for objects (including arrays), we flatten it. The we store it in 2 forms.
-		// Let's say that we have a key 'my-object'.
-		// - Form 1: { 'my-object' : {<flattenedObject>} }
-		// - Form 2: { 'my-object.key1': value1, my-object.key2: value2, ... }
+		// For objects (including arrays), we flatten it.
+		// We store it in 2 forms depending on options.
+		//
+		// Let's say that we have an array { 'my-array': ['tata', 'toto'] }.
+		// - Form 1: { 'my-array' : { '[0]': 'tata', '[1]': 'toto' } }
+		// - Form 2: { 'my-array[0]': 'tata', 'my-array[1]': 'toto' }
+		//
+		// And for objects { 'my-object': { first: 'tata', second: 'toto' } }.
+		// - Form 1: { 'my-object' : { '.first': 'tata', '.second': 'toto' } }
+		// - Form 2: { 'my-object.first': 'tata', 'my-object.second': 'toto' }
 		const subPayload = {};
 		if (Array.isArray(anything)) {
 			anything.forEach((item, index) => {
 				const itemKey = `[${index}]`;
-				step(item, itemKey, subPayload);
+				step(item, itemKey, subPayload, options);
 			});
 		} else {
 			Object.keys(anything).forEach(nextKey => {
 				const itemKey = `.${nextKey}`;
 				const item = anything[nextKey];
-				step(item, itemKey, subPayload);
+				step(item, itemKey, subPayload, options);
 			});
 		}
 
 		// Form 1: { 'my-object' : {<flattenedObject>} }
-		payload[key] = subPayload;
+		if (options.includeObjects) {
+			payload[key] = subPayload;
+		}
 		// Form 2: { 'my-object.key1': value1, my-object.key2: value2, ... }
 		Object.keys(subPayload).forEach(subKey => {
 			payload[`${key}${subKey}`] = subPayload[subKey];
@@ -51,14 +59,15 @@ function step(anything, key, payload) {
  * flatten an object means each keys are a jsonpath.
  * jsperf: https://jsperf.com/talend-flatten
  * @param {object} obj the source object
+ * @param {object} options
  * @return {object} flatten object
  * @example
  * flatten({ level1: { level2: 'foo' }})
  * // { 'level1.level2': 'foo' }
  */
-export default function flatten(obj) {
+export default function flatten(obj, options) {
 	return Object.keys(obj).reduce((accu, key) => {
-		step(obj[key], key, accu);
+		step(obj[key], key, accu, options);
 		return accu;
 	}, {});
 }
