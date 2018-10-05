6418549a332c556db164fd588dcbb1ce4157cc26
Laurent Maillet <lmaillet@talend.com>
2018-10-05 16:15:21 +0200

    TUI-224: function to class component

--- packages/forms/src/UIForm/fields/File/File.component.js
+++ packages/forms/src/UIForm/fields/File/File.component.js
@@ -2,92 +2,84 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import FieldTemplate from '../FieldTemplate';
 import { generateDescriptionId, generateErrorId } from '../../Message/generateId';
+import { translate } from 'react-i18next';
 
+import { I18N_DOMAIN_FORMS } from '../../../constants';
 import theme from './File.scss';
 
-function loadFileContent(onChange, event, schema) {
-	const fileList = event.target.files;
-	// TODO manage multiple files
-	const file = fileList[0];
-	const reader = new FileReader();
-	reader.onload = () => {
-		onChange(event, { schema, value: reader.result });
-		// TODO update ui
-		console.log(file.name);
+class FileWidget extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { fileReplaceValue: '' };
 	}
-	reader.readAsDataURL(file);
-}
 
-export default function File(props) {
-	const { id, isValid, errorMessage, onChange, onFinish, schema, value } = props;
-	const {
-		autoFocus,
-		description,
-		disabled = false,
-		placeholder,
-		readOnly = false,
-		title,
-	} = schema;
+	loadFileContent = (onChange, event, schema) => {
+		const fileList = event.target.files;
+		// TODO manage multiple files
+		const file = fileList[0];
+		const reader = new FileReader();
+		reader.onload = () => {
+			onChange(event, { schema, value: reader.result });
+			this.state.fileReplaceValue = file.name;
+		}
+		reader.readAsDataURL(file);
+	}
 
-	const descriptionId = generateDescriptionId(id);
-	const errorId = generateErrorId(id);
-	// let fileReplaceValue = '';
-
-	// const loadFileContent = event => {
-	// 	const fileList = event.target.files;
-	// 	// TODO manage multiple files
-	// 	const file = fileList[0];
-	// 	const reader = new FileReader();
-	// 	reader.onload = () => {
-	// 		onChange(event, { schema, value: reader.result });
-	// 		// TODO update ui
-	// 		console.log(file.name);
-	// 		fileReplaceValue = file.name;
-	// 	}
-	// 	reader.readAsDataURL(file);
-	// }
-
-	return (
-		<FieldTemplate
-			description={description}
-			descriptionId={descriptionId}
-			errorId={errorId}
-			errorMessage={errorMessage}
-			id={id}
-			isValid={isValid}
-			label={title}
-			labelAfter={false}
-			required={schema.required}
-		>
-			<div className={theme.file}>
-				<input
-					id={id}
-					autoFocus={autoFocus}
-					className="form-control"
-					disabled={disabled}
-					onBlur={event => onFinish(event, { schema })}
-					onChange={event => loadFileContent(onChange, event, schema)}
-					placeholder={placeholder}
-					readOnly={readOnly}
-					type="file"
-					// eslint-disable-next-line jsx-a11y/aria-proptypes
-					aria-invalid={!isValid}
-					aria-required={schema.required}
-					aria-describedby={`${descriptionId} ${errorId}`}
-				/>
-				<input
-					id={`input-${id}`}
-					name={`input-${id}`}
-					className={`form-control ${theme['file-replace']}`}
-					// value={fileReplaceValue}
-				/>
-			</div>
-		</FieldTemplate>
-	);
+	render() {
+		const { id, isValid, errorMessage, onChange, onFinish, schema, value } = this.props;
+		const {
+			autoFocus,
+			description,
+			disabled = false,
+			placeholder,
+			readOnly = false,
+			title,
+			required,
+		} = schema;
+		const descriptionId = generateDescriptionId(id);
+		const errorId = generateErrorId(id);
+		return (
+			<FieldTemplate
+				description={description}
+				descriptionId={descriptionId}
+				errorId={errorId}
+				errorMessage={errorMessage}
+				id={id}
+				isValid={isValid}
+				label={title}
+				labelAfter={false}
+				required={required}
+			>
+				<div className={theme.file}>
+					<input
+						id={id}
+						autoFocus={autoFocus}
+						className="form-control"
+						disabled={disabled}
+						onBlur={event => onFinish(event, { schema })}
+						onChange={event => this.loadFileContent(onChange, event, schema)}
+						placeholder={placeholder}
+						readOnly={readOnly}
+						type="file"
+						// eslint-disable-next-line jsx-a11y/aria-proptypes
+						aria-invalid={!isValid}
+						aria-required={schema.required}
+						aria-describedby={`${descriptionId} ${errorId}`}
+					/>
+					<input
+						id={`input-${id}`}
+						name={`input-${id}`}
+						className={`form-control ${theme['file-replace']}`}
+						value={this.state.fileReplaceValue}
+					/>
+				</div>
+			</FieldTemplate>
+		);
+	}
 }
 
 if (process.env.NODE_ENV !== 'production') {
-	File.propTypes = {
+	FileWidget.propTypes = {
 		id: PropTypes.string,
 		isValid: PropTypes.bool,
 		errorMessage: PropTypes.string,
@@ -106,8 +98,12 @@ if (process.env.NODE_ENV !== 'production') {
 	};
 }
 
-File.defaultProps = {
+FileWidget.defaultProps = {
 	isValid: true,
 	schema: {},
 	value: '',
 };
+
+export { FileWidget };
+
+export default translate(I18N_DOMAIN_FORMS)(FileWidget);
