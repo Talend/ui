f103c6882fc7feef20d56e9125363a58dc08ad34
sgendre <sgendre@talend.com>
2018-10-11 16:13:25 +0200

    type converter

--- packages/containers/sandbox/mockBackend/mock/kit/index.js
+++ packages/containers/sandbox/mockBackend/mock/kit/index.js
@@ -3,4 +3,5 @@ module.exports = {
 	titleMap: require('./titleMap.json'),
 	validation: require('./validation.json'),
 	fieldselector: require('./fieldselector.json'),
+	typeconverter: require('./typeconverter.json'),
 };
