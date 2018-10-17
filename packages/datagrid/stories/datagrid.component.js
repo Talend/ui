b142e9015f1ed402a1749c7a0f41de1a7d8f8319
Jean-Alain Ré <jare@talend.com>
2018-10-17 09:55:39 +0200

    update story to check performance issue

--- packages/datagrid/stories/datagrid.component.js
+++ packages/datagrid/stories/datagrid.component.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from '@storybook/addon-a11y';
 import { IconsProvider } from '@talend/react-components';
+import random from 'lodash/random';
+import differenceBy from 'lodash/differenceBy';
 
 import DataGrid from '../src/components/';
 import DefaultRenderer from '../src/components/DefaultCellRenderer/DefaultRenderer.component';
@@ -32,8 +34,8 @@ function getComponent(component) {
 	}
 }
 
-function forceRedrawRows(props, oldProps) {
-	return props.rowData[0].loading !== oldProps.rowData[0].loading;
+function forceRedrawRows(newProps, oldProps) {
+	return differenceBy(newProps.rowData, oldProps.rowData, 'loading').length;
 }
 
 sample.data[0].value.field0.value = `﻿﻿﻿﻿﻿﻿﻿  loreum lo
@@ -152,33 +154,110 @@ storiesOf('Component Datagrid')
 		return <WithLayout />;
 	})
 	.add('dynamic change data', () => {
+		const LOADING_ITEM = {
+			loading: true,
+			value: {},
+		};
+
+		function getItemWithRandomValue() {
+			return {
+				loading: false,
+				value: {
+					field2: {
+						value: random(0, 10000000),
+						quality: 1,
+					},
+					field8: {
+						value: random(0, 10000000),
+						quality: 0,
+					},
+					field5: {
+						value: random(0, 10000000),
+						quality: -1,
+					},
+					field4: {
+						value: random(0, 10000000),
+						quality: 1,
+					},
+					field7: {
+						value: random(0, 10000000),
+						quality: 1,
+					},
+					field3: {
+						value: random(0, 10000000),
+						quality: 1,
+					},
+					field1: {
+						value: random(0, 10000000),
+						quality: 1,
+					},
+					field0: {
+						value: `Aéroport ${random(0, 10000000)}`,
+						quality: 1,
+					},
+					field9: {
+						value: random(0, 10000000),
+						quality: 1,
+					},
+					field6: {
+						value: random(0, 10000000),
+						quality: 1,
+					},
+				},
+				quality: 1,
+			};
+		}
+		const ADD_ITEMS_NUMBER = 1;
 		class WithLayout extends React.Component {
 			constructor() {
 				super();
-				this.changeState = this.changeState.bind(this);
+				this.addLoadingsItems = this.addLoadingsItems.bind(this);
+				this.terminateItems = this.terminateItems.bind(this);
 				const datagridSample = Object.assign({}, sample);
-				datagridSample.data = [
-					{
-						value: {},
-						loading: true,
-					},
-				];
-				this.state = { sample: datagridSample };
+				datagridSample.data = new Array(ADD_ITEMS_NUMBER).fill().map(getItemWithRandomValue);
+				this.state = { sample: datagridSample, loading: false, index: 1 };
 			}
 
-			changeState() {
-				const datagridSample = Object.assign({}, this.state.sample);
-				datagridSample.data[0] = sample.data[0];
+			terminateItems() {
+				this.setState(prevState => {
+					const datagridSample = Object.assign({}, prevState.sample);
+					datagridSample.data.splice(
+						(prevState.index - 1) * ADD_ITEMS_NUMBER,
+						ADD_ITEMS_NUMBER,
+						...new Array(ADD_ITEMS_NUMBER).fill().map(getItemWithRandomValue),
+					);
 
-				this.setState({
-					sample: datagridSample,
+					return {
+						sample: datagridSample,
+						loading: !prevState.loading,
+					};
 				});
+			}
+
+			addLoadingsItems() {
+				const datagridSample = Object.assign({}, this.state.sample);
+				datagridSample.data = datagridSample.data.concat(
+					new Array(ADD_ITEMS_NUMBER).fill(LOADING_ITEM),
+				);
+
+				this.setState(prevState => ({
+					index: prevState.index + 1,
+					sample: datagridSample,
+					loading: !prevState.loading,
+				}));
+
+				setTimeout(this.terminateItems, 10);
 			}
 
 			render() {
 				return (
 					<div style={{ height: '100vh' }}>
-						<input type="button" value="changestatus" onClick={this.changeState} />
+						<input
+							type="button"
+							value={`add ${ADD_ITEMS_NUMBER} items`}
+							onClick={this.addLoadingsItems}
+							disabled={this.state.loading}
+						/>
 						Number of data : {this.state.sample.data.length}
 						<IconsProvider />
 						<DataGrid
