8a7007e74f5ef507a9c30f1127e6a79d22318e76
Jimmy Somsanith <jsomsanith@talend.com>
2018-10-29 17:10:10 +0100

    Add jsdoc

--- packages/forms/src/UIForm/utils/array.js
+++ packages/forms/src/UIForm/utils/array.js
@@ -2,6 +2,43 @@ function arrayStartsWith(prefix, arrayToCheck) {
 	return prefix.every((next, index) => arrayToCheck[index] === next);
 }
 
+/**
+ * Insert array element index in all nested schema key
+ *
+ * Example with array key ['my', 'array'] :
+ * {
+ * 		key: ['my', 'array', ''],
+ * 		widget: 'fieldset',
+ * 		items: [
+ * 			{
+ * 				key: ['my', 'item', ''],
+ *				widget: 'CustomFieldset',
+ *				items: [
+ *					{ key: ['my', 'array', '', path] },
+ *					{ key: ['my', 'array', '', name] },
+ *					{ key: ['other'] },
+ *				]
+ * 			}
+ * 		],
+ * }
+ *
+ * Result
+ * {
+ * 		key: ['my', 'array', index], // inject index in current element key
+ * 		widget: 'fieldset',
+ * 		items: [
+ * 			{
+ * 				key: ['my', 'array', index], // inject index in nster element child
+ *				widget: 'CustomFieldset',
+ *				items: [
+ *					{ key: ['my', 'array', index, path] }, // inject index in nested element child
+ *					{ key: ['my', 'array', index, name] },
+ *					{ key: ['other'] }, // don't touch any element with a different key than the array
+ *				]
+ * 			}
+ * 		],
+ * },
+ */
 function adaptArrayItemKey(arraySchema, item, itemIndex) {
 	const arrayKey = arraySchema.key;
 	const itemKey = item.key;
@@ -29,10 +66,24 @@ function adaptArrayItemKey(arraySchema, item, itemIndex) {
 }
 
 /**
- * Build the array content.
+ * Adapt array element items with the element index
+ * Example:
+ * Array schema
+ * {
+ * 		items: [
+ *			{ key: ['my', 'array', '', name], widget: 'text' },
+ *			{ key: ['my', 'array', '', path], widget: 'text' },
+ *		]
+ * }
+ *
+ * Result
+ * [
+ *		{ key: ['my', 'array', index, 'name'], widget: 'text' }, // insert nested index
+ *		{ key: ['my', 'array', index, 'path'], widget: 'text' }, // insert nested index
+ * ],
+ *
  * @param arraySchema
  * @param elementIndex
- * @returns {*}
  */
 export function getArrayElementItems(arraySchema, elementIndex) {
 	return arraySchema.items.map(item => adaptArrayItemKey(arraySchema, item, elementIndex));
@@ -40,6 +91,25 @@ export function getArrayElementItems(arraySchema, elementIndex) {
 
 /**
  * Build schema for an array element, based on array schema
+ * Example:
+ * Array schema
+ * {
+ * 		key: ['my', 'array'],
+ *		items: [
+ *			{ key: ['my', 'array', '', name], widget: 'text' },
+ *			{ key: ['my', 'array', '', path], widget: 'text' },
+ *		]
+ * }
+ * Result
+ * {
+ * 		key: ['my', 'array', index], // insert index
+ *		items: [
+ *			{ key: ['my', 'array', index, 'name'], widget: 'text' }, // insert nested index
+ *			{ key: ['my', 'array', index, 'path'], widget: 'text' }, // insert nested index
+ *		],
+ *		widget: 'fieldset', // insert item widget
+ * }
+ *
  * @param arraySchema The array schema
  * @param elementIndex The index of the element in the array
  */
