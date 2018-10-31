746e903749ee7f82f4e128fc63406cf3b84c203d
travis <travis>
2018-10-31 09:16:35 +0000

    chore(ci): prettier

--- packages/forms/src/UIForm/fields/Code/Code.component.js
+++ packages/forms/src/UIForm/fields/Code/Code.component.js
@@ -172,7 +172,8 @@ try {
 
 	CodeWidget = Code;
 } catch (error) {
-	class WrappedTextArea extends React.PureComponent { // eslint-disable-line react/no-multi-comp
+	class WrappedTextArea extends React.PureComponent {
+		// eslint-disable-line react/no-multi-comp
 		constructor() {
 			super();
 			// eslint-disable-next-line no-console
