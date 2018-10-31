b645cd21befcd561e8aca2090931664134471c06
Steven Gangnant <38908846+sgangnant@users.noreply.github.com>
2018-10-31 10:23:04 +0100

    Oops

--- packages/forms/src/UIForm/fields/Code/Code.component.js
+++ packages/forms/src/UIForm/fields/Code/Code.component.js
@@ -172,8 +172,7 @@ try {
 
 	CodeWidget = Code;
 } catch (error) {
-	// eslint-disable-line react/no-multi-comp
-	class WrappedTextArea extends React.PureComponent {	
+	class WrappedTextArea extends React.PureComponent { // eslint-disable-line react/no-multi-comp
 		constructor() {
 			super();
 			// eslint-disable-next-line no-console
