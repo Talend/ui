aff8b005d4bfdcd4eeb4b38f8e770daaeb7c09b5
Andreas Nilsson <anilsson@talend.com>
2018-11-05 09:43:27 +0100

    add tests and refactor for easier testing

--- packages/local-libs-webpack-plugin/index.js
+++ packages/local-libs-webpack-plugin/index.js
@@ -1,141 +1,41 @@
-const path = require('path');
-
-function getJson(filePath) {
-	return require(path.resolve(filePath)); // eslint-disable-line global-require
-}
-
-function isLerna(filePath) {
-	if (filePath.includes('lerna.json')) {
-		return true;
-	}
-	return false;
-}
-
-/**
- * `mylib/lib/package.json` -> `mylib/lib`
- */
-
-function pathWithoutFilename(filePath = '') {
-	if (filePath.indexOf('/') > 0) {
-		return filePath.substr(0, filePath.lastIndexOf('/'));
-	}
-	if (filePath.indexOf('.') === -1) {
-		return filePath;
-	}
-	return '';
-}
-
-function getPackageJsonInfo(packagePath) {
-	const packageJson = getJson(packagePath);
-	return {
-		name: packageJson.name,
-		path: path.resolve(packagePath.replace('/package.json', '')),
-		mainSrc: pathWithoutFilename(packageJson.mainSrc),
-		main: pathWithoutFilename(packageJson.main),
-		peerDependencies: packageJson.peerDependencies || {},
-	};
-}
-
-/**
- * `mylib/lib/somefile` -> `mylib/src/somefile`
- */
-function libToSrcPath(libPath, lib) {
-	return libPath.replace(`${lib.name}/${lib.main}`, `${lib.name}/${lib.mainSrc}`);
-}
-
-function getLinkedLibs(paths) {
-	const linkedLibs = [];
-	paths.forEach(packagePath => {
-		if (isLerna(packagePath)) {
-			getJson(packagePath).packages.forEach(subPackagePath => {
-				const packageJsonPath = packagePath.replace('lerna.json', `${subPackagePath}/package.json`);
-				linkedLibs.push(getPackageJsonInfo(packageJsonPath));
-			});
-		} else {
-			linkedLibs.push(getPackageJsonInfo(packagePath));
-		}
-	});
-	return linkedLibs;
-}
+const formatOptions = require('./lib/formatOptions');
+const getLinkedLibs = require('./lib/getLinkedLibs');
+const ensureOptionsKeys = require('./lib/ensureOptionsKeys');
+const addAliases = require('./lib/addAliases');
+const convertRequests = require('./lib/convertRequests');
 
 class LocalLibsWebpackPlugin {
-	constructor(options = []) {
-		if (Array.isArray(options)) {
-			this.options = options;
-		} else {
-			this.options = Object.entries(options)
-				.filter(([key, value]) => {
-					if (value === true && key !== undefined) {
-						return true;
-					}
-					return false;
-				})
-				.map(([key]) => key);
-		}
+	constructor(options) {
+		this.options = formatOptions(options);
 	}
 
 	apply(compiler) {
-		/* eslint-disable no-param-reassign */
 		const linkedLibs = getLinkedLibs(this.options);
 		if (!linkedLibs.length) {
 			return;
 		}
 
-		if (!compiler.options.resolve) {
-			compiler.options.resolve = {};
-		}
-
-		if (!compiler.options.resolve.alias) {
-			compiler.options.resolve.alias = {};
-		}
-
-		// peerDependencies should use project's node_modules - not the library's
-		// this will avoid issues like "multiple instances of react running"
-		linkedLibs.forEach(lib => {
-			Object.keys(lib.peerDependencies).forEach(peerDependency => {
-				compiler.options.resolve.alias[peerDependency] = path.resolve('./node_modules', peerDependency);
-			});
-		});
-
-		// Add the linked libs last - to override if any of them are also listed as peerDependencies
-		linkedLibs.forEach(lib => {
-			compiler.options.resolve.alias[lib.name] = path.resolve(lib.path);
-		});
+		/* eslint-disable no-param-reassign */
+		compiler.options = ensureOptionsKeys(compiler.options);
+		compiler.options.resolve.alias = addAliases(linkedLibs, compiler.options.resolve.alias);
+		/* eslint-enable no-param-reassign */
 
 		// Look in webpack repo (NormalModuleReplacementPlugin) how to convert to webpack 4 when needed
+		// This plugin is necessary to handle all special imports, for example long import paths or imports from inside a linked lib
 		compiler.plugin('normal-module-factory', nmf => {
 			nmf.plugin('before-resolve', (result, callback) => {
 				if (!result) {
 					return callback();
 				}
-				linkedLibs.forEach(lib => {
-					if (result.request.startsWith(lib.name) && result.request[lib.name.length] === '/') {
-						// convert paths to use mainSrc (should also work on long import paths)
-						// Examples: '@talend/somelib', '@talend/somelib/some/path'
-						// '@talend/someLib2' must not match '@talend/someLib'
-						if (lib.mainSrc && lib.main) {
-							result.request = libToSrcPath(result.request, lib);
-						}
-						result.request = result.request.replace(lib.name, lib.path);
-					}
-
-					if (result.request.startsWith('.') && result.context.includes(lib.path)) {
-						// Example request: './SomeComponent'
-						// First we add the context (full absolute path), then transform to mainSrc path
-						const absolutePath = path.resolve(result.context, result.request);
-						result.request = absolutePath.replace(`${lib.path}/${lib.main}`, `${lib.path}/${lib.mainSrc}`);
-					}
-				});
+				result.request = convertRequests(result.request, result.context, linkedLibs);
 				return callback(null, result);
 			});
 		});
 
 		console.log('LocalLibsWebpackPlugin: Link the following libs:'); // eslint-disable-line no-console
-
-		Object.keys(compiler.options.resolve.alias).forEach(item => {
-			console.log(`Link "${item}" to: "${compiler.options.resolve.alias[item]}"`); // eslint-disable-line no-console
-		});
-		/* eslint-enable no-param-reassign */
+		Object.keys(compiler.options.resolve.alias).forEach(item => (
+			console.log(`Link "${item}" to: "${compiler.options.resolve.alias[item]}"`) // eslint-disable-line no-console
+		));
 	}
 }
 
---
+++ packages/local-libs-webpack-plugin/test/pathWithoutFilename.test.js
@@ -0,0 +1,17 @@
+import pathWithoutFilename from '../lib/pathWithoutFilename';
+
+describe('pathWithoutFilename', () => {
+	it('should remove the file part from a path', () => {
+		expect(pathWithoutFilename('mylib/lib/index.js')).toEqual('mylib/lib');
+		expect(pathWithoutFilename('mylib/lib/.js')).toEqual('mylib/lib');
+		expect(pathWithoutFilename('index.js')).toEqual('');
+		expect(pathWithoutFilename('/index.js')).toEqual('');
+	});
+
+	xit('should not modify paths without json extension', () => {
+		expect(pathWithoutFilename('mylib/lib/package')).toEqual('mylib/lib/package');
+		expect(pathWithoutFilename('mylib/lib/package/')).toEqual('mylib/lib/package/');
+		expect(pathWithoutFilename('mylib/lib/package/json')).toEqual('mylib/lib/package/json');
+		expect(pathWithoutFilename('mylib/lib/package.')).toEqual('mylib/lib/package.');
+	});
+});
