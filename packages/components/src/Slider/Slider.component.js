2a876a25f8aad88d3efaed08e9bb3a9c503b046d
travis <travis>
2018-11-06 14:50:34 +0000

    chore(ci): prettier

--- packages/components/src/Slider/Slider.component.js
+++ packages/components/src/Slider/Slider.component.js
@@ -219,7 +219,7 @@ class Slider extends React.Component {
 		} = this.props;
 		const noValue = value === null || value === undefined;
 		return (
-			<div style={{ display: 'flex', flexDirection: 'column'}}>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
 				<div
 					className={classnames(theme['tc-slider'], 'tc-slider')}
 					style={{ width: '98%', alignSelf: 'center' }}
