f8266e1f3b96873e99fc928dd85841f5353b53e4
Jean-Alain Ré <jare@talend.com>
2018-11-07 13:41:15 +0100

    remove comment

--- .travis/after_success_e2e.sh
+++ .travis/after_success_e2e.sh
@@ -4,13 +4,9 @@ set -e
 echo "E2E TESTS"
 cd packages/components/e2e/component-objects
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_BRANCH" == 'master' ]; then
-	# check master
-	#launch cypress
 	CYPRESS_baseUrl=http://talend.surge.sh/components yarn run e2e
 	echo "✓ E2E tests on talend.surge.sh"
 elif [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
-	#check on branch
-	#launch cypress
 	CYPRESS_baseUrl=$TRAVIS_PULL_REQUEST.talend.surge.sh/components yarn run e2e
 else
     echo "✓ No storybook to test"
