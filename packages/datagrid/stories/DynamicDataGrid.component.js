944e0143f925103d90fa7735a707df4023c3d9fc
Jean-Alain Ré <jare@talend.com>
2018-11-19 15:11:58 +0100

    export DynamicDatagrid

---
+++ packages/datagrid/stories/DynamicDataGrid.component.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import random from 'lodash/random';
+import { IconsProvider } from '@talend/react-components';
+
+import DataGrid from '../src/components/';
+import DefaultRenderer from '../src/components/DefaultCellRenderer/DefaultRenderer.component';
+import DefaultIntCellRenderer from '../src/components/DefaultIntCellRenderer';
+import DefaultPinHeaderRenderer from '../src/components/DefaultPinHeaderRenderer';
+import DefaultCellRenderer from '../src/components/DefaultCellRenderer';
+import DefaultHeaderRenderer from '../src/components/DefaultHeaderRenderer';
+import sample from './sample.json';
+
+const ADD_ITEMS_NUMBER = 4;
+const LOADING_ITEM = {
+	loading: true,
+	value: {},
+};
+
+export function getComponent(component) {
+	switch (component) {
+		case 'DefaultIntCellRenderer':
+			return DefaultIntCellRenderer;
+		case 'DefaultHeaderRenderer':
+			return DefaultHeaderRenderer;
+		case 'DefaultPinHeaderRenderer':
+			return DefaultPinHeaderRenderer;
+		case 'DefaultCellRenderer':
+			return DefaultCellRenderer;
+		case 'DefaultStringCellRenderer':
+			return DefaultRenderer;
+		default:
+			console.error(component);
+	}
+}
+
+function getItemWithRandomValue() {
+	return {
+		loading: false,
+		value: {
+			// field2: {
+			// 	value: random(0, 10000000),
+			// 	quality: 1,
+			// },
+			// field8: {
+			// 	value: random(0, 10000000),
+			// 	quality: 0,
+			// },
+			// field5: {
+			// 	value: random(0, 10000000),
+			// 	quality: -1,
+			// },
+			// field4: {
+			// 	value: random(0, 10000000),
+			// 	quality: 1,
+			// },
+			// field7: {
+			// 	value: random(0, 10000000),
+			// 	quality: 1,
+			// },
+			// field3: {
+			// 	value: random(0, 10000000),
+			// 	quality: 1,
+			// },
+			// field1: {
+			// 	value: random(0, 10000000),
+			// 	quality: 1,
+			// },
+			field0: {
+				value: `Aéroport ${random(0, 10000000)}`,
+				quality: 1,
+			},
+			// field9: {
+			// 	value: random(0, 10000000),
+			// 	quality: 1,
+			// },
+			// field6: {
+			// 	value: random(0, 10000000),
+			// 	quality: 1,
+			// },
+		},
+		quality: 1,
+	};
+}
+
+export default class DynamicDataGrid extends React.Component {
+	constructor() {
+		super();
+		this.addLoadingsItems = this.addLoadingsItems.bind(this);
+		this.terminateItems = this.terminateItems.bind(this);
+		const datagridSample = Object.assign({}, sample);
+		datagridSample.schema.fields = [datagridSample.schema.fields[0]];
+		datagridSample.data = new Array(ADD_ITEMS_NUMBER).fill(LOADING_ITEM);
+		this.state = { sample: datagridSample, loading: true, index: 1 };
+
+		setTimeout(this.terminateItems, 1000);
+	}
+
+	terminateItems() {
+		this.setState(prevState => {
+			const datagridSample = Object.assign({}, prevState.sample);
+			datagridSample.data.splice(
+				(prevState.index - 1) * ADD_ITEMS_NUMBER,
+				ADD_ITEMS_NUMBER,
+				...new Array(ADD_ITEMS_NUMBER).fill().map(getItemWithRandomValue),
+			);
+
+			return {
+				sample: datagridSample,
+				loading: !prevState.loading,
+			};
+		});
+	}
+
+	addLoadingsItems() {
+		const datagridSample = Object.assign({}, this.state.sample);
+		datagridSample.data = datagridSample.data.concat(
+			new Array(ADD_ITEMS_NUMBER).fill(LOADING_ITEM),
+		);
+
+		this.setState(prevState => ({
+			index: prevState.index + 1,
+			sample: datagridSample,
+			loading: !prevState.loading,
+		}));
+
+		setTimeout(this.terminateItems, 1000);
+	}
+
+	render() {
+		return (
+			<div style={{ height: '100vh' }}>
+				<input
+					type="button"
+					value={`add ${ADD_ITEMS_NUMBER} items`}
+					onClick={this.addLoadingsItems}
+					disabled={this.state.loading}
+				/>
+				Number of data : {this.state.sample.data.length}
+				<IconsProvider />
+				<DataGrid
+					data={this.state.sample}
+					getComponent={getComponent}
+					rowSelection="multiple"
+					forceRedrawRows={() => true}
+				/>
+			</div>
+		);
+	}
+}
--- packages/datagrid/stories/datagrid.component.js
+++ packages/datagrid/stories/datagrid.component.js
@@ -2,42 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { checkA11y } from '@storybook/addon-a11y';
 import { IconsProvider } from '@talend/react-components';
-import random from 'lodash/random';
-import differenceBy from 'lodash/differenceBy';
 
 import DataGrid from '../src/components/';
-import DefaultRenderer from '../src/components/DefaultCellRenderer/DefaultRenderer.component';
-import DefaultIntCellRenderer from '../src/components/DefaultIntCellRenderer';
-import DefaultPinHeaderRenderer from '../src/components/DefaultPinHeaderRenderer';
-import DefaultCellRenderer from '../src/components/DefaultCellRenderer';
-import DefaultHeaderRenderer from '../src/components/DefaultHeaderRenderer';
 
-import serializer from '../src/components/DatasetSerializer';
+import DynamicDataGrid, { getComponent } from './DynamicDataGrid.component';
 import sample from './sample.json';
 import sample2 from './sample2.json';
 import sample3 from './sample3.json';
-
-function getComponent(component) {
-	switch (component) {
-		case 'DefaultIntCellRenderer':
-			return DefaultIntCellRenderer;
-		case 'DefaultHeaderRenderer':
-			return DefaultHeaderRenderer;
-		case 'DefaultPinHeaderRenderer':
-			return DefaultPinHeaderRenderer;
-		case 'DefaultCellRenderer':
-			return DefaultCellRenderer;
-		case 'DefaultStringCellRenderer':
-			return DefaultRenderer;
-		default:
-			console.error(component);
-	}
-}
-
-function forceRedrawRows(newProps, oldProps) {
-	return true;
-	//return differenceBy(newProps.rowData, oldProps.rowData, 'loading').length;
-}
 
 sample.data[0].value.field0.value = `﻿﻿﻿﻿﻿﻿﻿  loreum lo
 psum	 	 `;
@@ -154,126 +125,4 @@ storiesOf('Component Datagrid')
 		}
 		return <WithLayout />;
 	})
-	.add('dynamic change data', () => {
-		const LOADING_ITEM = {
-			loading: true,
-			value: {},
-		};
-
-		function getItemWithRandomValue() {
-			return {
-				loading: false,
-				value: {
-					// field2: {
-					// 	value: random(0, 10000000),
-					// 	quality: 1,
-					// },
-					// field8: {
-					// 	value: random(0, 10000000),
-					// 	quality: 0,
-					// },
-					// field5: {
-					// 	value: random(0, 10000000),
-					// 	quality: -1,
-					// },
-					// field4: {
-					// 	value: random(0, 10000000),
-					// 	quality: 1,
-					// },
-					// field7: {
-					// 	value: random(0, 10000000),
-					// 	quality: 1,
-					// },
-					// field3: {
-					// 	value: random(0, 10000000),
-					// 	quality: 1,
-					// },
-					// field1: {
-					// 	value: random(0, 10000000),
-					// 	quality: 1,
-					// },
-					field0: {
-						value: `Aéroport ${random(0, 10000000)}`,
-						quality: 1,
-					},
-					// field9: {
-					// 	value: random(0, 10000000),
-					// 	quality: 1,
-					// },
-					// field6: {
-					// 	value: random(0, 10000000),
-					// 	quality: 1,
-					// },
-				},
-				quality: 1,
-			};
-		}
-		const ADD_ITEMS_NUMBER = 4;
-		class WithLayout extends React.Component {
-			constructor() {
-				super();
-				this.addLoadingsItems = this.addLoadingsItems.bind(this);
-				this.terminateItems = this.terminateItems.bind(this);
-				const datagridSample = Object.assign({}, sample);
-				datagridSample.schema.fields = [datagridSample.schema.fields[0]];
-				datagridSample.data = new Array(ADD_ITEMS_NUMBER).fill(LOADING_ITEM);
-				this.state = { sample: datagridSample, loading: false, index: 1 };
-			}
-
-			terminateItems() {
-				this.setState(prevState => {
-					const datagridSample = Object.assign({}, prevState.sample);
-					datagridSample.data.splice(
-						(prevState.index - 1) * ADD_ITEMS_NUMBER,
-						ADD_ITEMS_NUMBER,
-						...new Array(ADD_ITEMS_NUMBER).fill().map(getItemWithRandomValue),
-					);
-
-					// datagridSample.data = [getItemWithRandomValue(), getItemWithRandomValue()];
-					console.log(datagridSample);
-
-					return {
-						sample: datagridSample,
-						loading: !prevState.loading,
-					};
-				});
-			}
-
-			addLoadingsItems() {
-				const datagridSample = Object.assign({}, this.state.sample);
-				datagridSample.data = datagridSample.data.concat(
-					new Array(ADD_ITEMS_NUMBER).fill(LOADING_ITEM),
-				);
-
-				this.setState(prevState => ({
-					index: prevState.index + 1,
-					sample: datagridSample,
-					loading: !prevState.loading,
-				}));
-
-				setTimeout(this.terminateItems, 1000);
-			}
-
-			render() {
-				return (
-					<div style={{ height: '100vh' }}>
-						<input
-							type="button"
-							value={`add ${ADD_ITEMS_NUMBER} items`}
-							onClick={this.addLoadingsItems}
-							disabled={this.state.loading}
-						/>
-						Number of data : {this.state.sample.data.length}
-						<IconsProvider />
-						<DataGrid
-							data={this.state.sample}
-							getComponent={getComponent}
-							rowSelection="multiple"
-							forceRedrawRows={() => true}
-						/>
-					</div>
-				);
-			}
-		}
-		return <WithLayout />;
-	});
+	.add('dynamic change data', () => <DynamicDataGrid />);
