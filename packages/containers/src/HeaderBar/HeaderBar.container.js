3de888068841831deb13db111ae9fe6ab46f1a7b
Steven Gangnant <sgangnant@talend.com>
2018-11-22 12:01:56 +0100

    Satisfy lint

--- packages/containers/src/HeaderBar/HeaderBar.container.js
+++ packages/containers/src/HeaderBar/HeaderBar.container.js
@@ -57,9 +57,14 @@ class HeaderBar extends React.Component {
 		if (hasFetchedProducts && productsFromState) {
 			const productsFromProps = props.products || {};
 
-			const itemsFromProps = (productsFromProps.items ? props.products.items : []).map(item =>
-				item.dispatch ? { ...item, onClickDispatch: item.dispatch } : item,
-			);
+			const itemsFromProps = (productsFromProps.items ? props.products.items : [])
+				.map(item => {
+					if (item.dispatch) {
+						return { ...item, onClickDispatch: item.dispatch };
+					}
+
+					return item;
+				});
 
 			const items = [
 				...itemsFromProps,
