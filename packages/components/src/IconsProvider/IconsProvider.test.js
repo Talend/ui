e992a95d85ed41bc5318b447b4c4855668688bc7
jmfrancois <jmfrancois@talend.com>
2018-11-22 16:47:44 +0100

    chore: will do that later

--- packages/components/src/IconsProvider/IconsProvider.test.js
+++ packages/components/src/IconsProvider/IconsProvider.test.js
@@ -2,31 +2,11 @@ import React from 'react';
 import { mount } from 'enzyme';
 import IconsProvider from './IconsProvider.component';
 
-const fs = require('fs');
-const path = require('path');
-
-const VIEW_BOXES = ['0 0 16 16', '0 0 32 32', '0 0 50 50'];
-
 describe('IconsProvider', () => {
 	it('should render default talend-icons', () => {
-		const output = [];
 		const wrapper = mount(<IconsProvider />);
 		const symbols = wrapper.find('symbol');
 		expect(symbols.length).toBeGreaterThan(200);
-		symbols.forEach(symbol => {
-			const id = symbol.props().id;
-			const viewBox = symbol.props().children.props.viewBox;
-			const type = symbol.props().children.props.children.type;
-			expect(id).toContain('talend-');
-			if (VIEW_BOXES.indexOf(viewBox) === -1) {
-				// eslint-disable-next-line no-console
-				output.push(`* ${id} icon do not follow the rule for the viewBox`);
-			}
-			if (type !== 'g') {
-				output.push(`* ${id} should have a group as first children`);
-			}
-		});
-		fs.writeFileSync(path.join(__dirname, '../../../../output/icons.lint.txt'), output.join('\n'));
 	});
 
 	it('should render default talend-icons and custom icons defined on icons prop', () => {
