a25d0268e6d5cce2360a081e5225674bfef27f49
Jimmy Somsanith <jsomsanith@talend.com>
2018-11-28 11:49:14 +0100

    chore(DatePicker): refactor year picker (#1852)

--- packages/components/src/DateTimePickers/generator.js
+++ packages/components/src/DateTimePickers/generator.js
@@ -4,6 +4,7 @@ import differenceInCalendarWeeks from 'date-fns/difference_in_calendar_weeks';
 import addMonths from 'date-fns/add_months';
 import endOfMonth from 'date-fns/end_of_month';
 import format from 'date-fns/format';
+import getYear from 'date-fns/get_year';
 import setDay from 'date-fns/set_day';
 import startOfWeek from 'date-fns/start_of_week';
 import getLocale from '../DateFnsLocale/locale';
@@ -59,4 +60,18 @@ export function buildMonths(chunkSize) {
 			name: format(addMonths(new Date(0), index), 'MMMM', pickerLocale),
 		}));
 	return chunk(months, chunkSize);
+}
+
+/**
+ * Generate a years window, centered on the current year by default, or the provided one
+ */
+export function buildYears(middle, window = 3) {
+	const middleYear = middle === undefined ? getYear(new Date()) : middle;
+	const start = middleYear - window;
+	const end = middleYear + window;
+	const years = [];
+	for (let i = start; i <= end; i += 1) {
+		years.push(i);
+	}
+	return years;
 }
