0a042212010f3da8f7135eb80e842a2b5f6efd92
Sébastien Romain <sromain@tlnd-sromain.local>
2018-12-02 20:15:51 +0100

    feat(componentForm): dispatch failed event when submit is failed

--- packages/containers/src/ComponentForm/ComponentForm.sagas.js
+++ packages/containers/src/ComponentForm/ComponentForm.sagas.js
@@ -75,13 +75,18 @@ function* onFormSubmit(componentId, submitURL, action) {
 	}
 	const { response, data } = yield call(cmf.sagas.http.post, submitURL, action.properties);
 	if (!response.ok) {
-		return;
+		yield put({
+			type: Component.ON_SUBMIT_FAILED,
+			data,
+			componentId,
+		});
+	} else {
+		yield put({
+			type: Component.ON_SUBMIT_SUCCEED,
+			data,
+			componentId,
+		});
 	}
-	yield put({
-		type: Component.ON_SUBMIT_SUCCEED,
-		data,
-		componentId,
-	});
 }
 
 /**
