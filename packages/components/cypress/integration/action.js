4391ececaf547d290aaebb953efe8d97abea6201
Jean-Alain Ré <jare@talend.com>
2018-12-12 16:56:08 +0100

    test with cypress dashboard

--- packages/components/cypress/integration/action.js
+++ packages/components/cypress/integration/action.js
@@ -1,9 +1,8 @@
 /* global context, cy */
+
 context('Actions', () => {
 	beforeEach(() => {
-		cy.visit(
-			'/iframe.html?selectedKind=Action&selectedStory=Disable%20the%20buttons&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel',
-		);
+		cy.visit('/iframe.html?selectedKind=Action&selectedStory=Disable%20the%20buttons');
 	});
 
 	it('should active/inactive a button', () => {
