a54f61dd59f3b5f15830d97104c516e09bb2e5e3
Jimmy Somsanith <jsomsanith@talend.com>
2018-12-14 13:54:35 +0100

    Continue script

--- packages/scripts/internationalization/scripts/to-github.js
+++ packages/scripts/internationalization/scripts/to-github.js
@@ -1,4 +1,4 @@
-/* eslint-disable global-require */
+/* eslint-disable global-require,no-console */
 const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
@@ -6,6 +6,16 @@ const xmlParser = require('xml2json');
 const spawn = require('cross-spawn');
 
 const error = require('../common/error');
+
+function printSection(title) {
+	console.log('\n------------------------------');
+	console.log(`-- ${title}`);
+	console.log('------------------------------');
+}
+
+function printSuccess(text) {
+	console.log(`✔ ${text}`);
+}
 
 function getGithubVariables() {
 	const { GITHUB_LOGIN, GITHUB_TOKEN } = process.env;
@@ -25,51 +35,8 @@ function getGithubVariables() {
 	return { login: GITHUB_LOGIN, token: GITHUB_TOKEN };
 }
 
-const VERSION_REGEX = /^([0-9]+\.[0-9]+).*/;
-function getVersion() {
-	const lernaJsonPath = path.join(process.cwd(), 'lerna.json');
-	const packageJsonPath = path.join(process.cwd(), 'package.json');
-	const pomXmlPath = path.join(process.cwd(), 'pom.xml');
-
-	if (fs.existsSync(lernaJsonPath)) {
-		const { version } = require(lernaJsonPath);
-		const match = version.match(VERSION_REGEX);
-		if (match) {
-			return match[1];
-		}
-	}
-
-	if (fs.existsSync(packageJsonPath)) {
-		const { version } = require(packageJsonPath);
-		const match = version.match(VERSION_REGEX);
-		if (match) {
-			return match[1];
-		}
-	}
-
-	if (fs.existsSync(pomXmlPath)) {
-		const data = fs.readFileSync(pomXmlPath);
-		const { version } = xmlParser.toJson(data);
-		const match = version.match(VERSION_REGEX);
-		if (match) {
-			return match[1];
-		}
-	}
-
-	error(`
-		talend-scripts can't determine your project version. Possible reasons : 
-			* no package.json or pom.xml in this directory
-			* no version in package.json or pom.xml
-	`);
-}
-
-function toGithub({ load, github }) {
-	const { login, token } = getGithubVariables();
-	const { url } = github;
-	const { project, target } = load;
-
-	// check if i18n files are here or ask user to perform download
-	const i18nFolder = path.join(process.cwd(), target, 'i18n');
+function checkI18nFolder(i18nFolder) {
+	printSection('Check i18n files');
 	if (
 		!fs.existsSync(i18nFolder) || // folder doesn't exist
 		!fs.lstatSync(i18nFolder).isDirectory() || // is not a folder
@@ -79,48 +46,120 @@ function toGithub({ load, github }) {
 			"talend-scripts i18n-to-github" need to consume files from xtm.
 			 Please run "talend-scripts download before"
 		`);
+	} else {
+		printSuccess(`${i18nFolder} found and contains files to add.`);
 	}
+}
+
+function getVersion() {
+	printSection('Extract version');
+	const lernaJsonPath = path.join(process.cwd(), 'lerna.json');
+	const packageJsonPath = path.join(process.cwd(), 'package.json');
+	const pomXmlPath = path.join(process.cwd(), 'pom.xml');
+	const VERSION_REGEX = /^([0-9]+\.[0-9]+).*/;
+
+	let extractedVersion;
+	let source;
+	if (fs.existsSync(lernaJsonPath)) {
+		const { version } = require(lernaJsonPath);
+		const match = version.match(VERSION_REGEX);
+		if (match) {
+			source = 'lerna.json';
+			extractedVersion = match[1];
+		}
+	} else if (!extractedVersion && fs.existsSync(packageJsonPath)) {
+		const { version } = require(packageJsonPath);
+		const match = version.match(VERSION_REGEX);
+		if (match) {
+			source = 'package.json';
+			extractedVersion = match[1];
+		}
+	} else if (!extractedVersion && fs.existsSync(pomXmlPath)) {
+		const data = fs.readFileSync(pomXmlPath);
+		const { version } = xmlParser.toJson(data);
+		const match = version.match(VERSION_REGEX);
+		if (match) {
+			source = 'pom.xml';
+			extractedVersion = match[1];
+		}
+	}
+
+	if (!extractedVersion) {
+		error(`
+			talend-scripts can't determine your project version. Possible reasons : 
+				* no package.json or pom.xml in this directory
+				* no version in package.json or pom.xml
+		`);
+	}
+
+	printSuccess(`Version extracted from ${source}: ${extractedVersion}`);
+	return extractedVersion;
+}
+
+function cloneLocalesRepo(githubUrl, localesRepoPath) {
+	printSection('Cloned repository');
+	rimraf.sync(localesRepoPath);
+	const { status } = spawn.sync('git', ['clone', githubUrl, localesRepoPath], { stdio: 'inherit' });
+	if (status !== 0) {
+		error(`Pull repository failed: git clone ${githubUrl} ${localesRepoPath}`);
+	} else {
+		printSuccess(`Repositiory cloned to ${localesRepoPath}`);
+	}
+}
+
+function toGithub({ load, github }) {
+	const { login, token } = getGithubVariables();
+	const { url } = github;
+	const { project, target } = load;
+
+	// check if i18n files are here or ask user to perform download
+	const i18nFolder = path.join(process.cwd(), target, 'i18n');
+	checkI18nFolder(i18nFolder);
 
 	// extract version (major.minor)
 	const version = getVersion();
-	const branch = `${project}/${version}`;
-	const githubUrl = url.replace('https://github.com', `https://${login}:${token}@github.com`);
-
-	console.log({
-		login,
-		token,
-		project,
-		githubUrl,
-		version,
-		branch,
-	});
+	const tagName = `${project}/${version}`;
+	const branchName = `release/${tagName}`;
 
 	// pull repo using token
+	const githubUrl = url.replace('https://github.com', `https://${login}:${token}@github.com`);
 	const tmpFolderPath = path.join(process.cwd(), 'tmp');
 	const localesRepoPath = path.join(tmpFolderPath, 'locales');
-	if (!fs.existsSync(tmpFolderPath)) {
-		fs.mkdirSync(tmpFolderPath);
-	}
-	rimraf.sync(localesRepoPath);
-	spawn.sync('git', ['clone', githubUrl, localesRepoPath], { stdio: 'inherit' });
-	spawn.sync('cd', [localesRepoPath], { stdio: 'inherit' });
+	cloneLocalesRepo(githubUrl, localesRepoPath);
+	const repoCmdContext = { stdio: 'inherit', cwd: localesRepoPath };
 
 	// pull or create branch
 	const branchTestCode = spawn.sync(
 		'git',
-		['ls-remote', '--exit-code', '--heads', localesRepoPath, branch],
+		['ls-remote', '--exit-code', '--heads', githubUrl, branchName],
 		{ stdio: 'inherit' },
 	);
-	if (branchTestCode === 2) {
-		spawn.sync('git', ['checkout', '-b', branch], { stdio: 'inherit' });
+	if (branchTestCode.status === 2) {
+		spawn.sync('git', ['checkout', '-b', branchName], repoCmdContext);
 	} else {
-		spawn.sync('git', ['checkout', branch], { stdio: 'inherit' });
+		spawn.sync('git', ['checkout', branchName], repoCmdContext);
 	}
 
 	// copy files (overwrite)
+	spawn.sync('cp', ['-r', `${i18nFolder}/.`, localesRepoPath], { stdio: 'inherit' });
 
-	// check if there are changes
-	// commit / push / tag if there are
+	// commit / tag / push
+	console.log('git add .');
+	spawn.sync('git', ['add', '.'], repoCmdContext);
+	console.log(`git commit -m 'feat(${project}): update locales files for ${version}'`);
+	spawn.sync(
+		'git',
+		['commit', '-m', `feat(${project}): update locales files for ${version}`],
+		repoCmdContext,
+	);
+	console.log(`git tag ${tagName} -f`);
+	spawn.sync('git', ['tag', tagName, '-f'], repoCmdContext);
+	console.log(`git push origin ${branchName}`);
+	spawn.sync('git', ['push', 'origin', branchName], repoCmdContext);
+	console.log(`git push origin :refs/tags/${tagName}`);
+	spawn.sync('git', ['push', 'origin', `:refs/tags/${tagName}`], repoCmdContext);
+	console.log('git push --tags');
+	spawn.sync('git', ['push', '--tags'], repoCmdContext);
 }
 
 module.exports = toGithub;
