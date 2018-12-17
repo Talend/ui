1cbb826fa0073828d169adc8e4c42bb9229a234e
jmfrancois <jmfrancois@talend.com>
2018-12-17 16:43:53 +0100

    doc: update homepage

--- packages/cmf/website/pages/en/index.js
+++ packages/cmf/website/pages/en/index.js
@@ -13,186 +13,206 @@ const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
-class HomeSplash extends React.Component {
-	render() {
-		const { siteConfig, language = '' } = this.props;
-		const { baseUrl, docsUrl } = siteConfig;
-		const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-		const langPart = `${language ? `${language}/` : ''}`;
-		const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
-
-		const SplashContainer = props => (
-			<div className="homeContainer">
-				<div className="homeSplashFade">
-					<div className="wrapper homeWrapper">{props.children}</div>
-				</div>
+function SplashContainer(props) {
+	return (
+		<div className="homeContainer">
+			<div className="homeSplashFade">
+				<div className="wrapper homeWrapper">{props.children}</div>
 			</div>
-		);
-
-		const Logo = props => (
-			<div className="projectLogo">
-				<img src={props.img_src} alt="Project Logo" />
-			</div>
-		);
-
-		const ProjectTitle = () => (
-			<h2 className="projectTitle">
-				{siteConfig.title}
-				<small>{siteConfig.tagline}</small>
-			</h2>
-		);
-
-		const PromoSection = props => (
-			<div className="section promoSection">
-				<div className="promoRow">
-					<div className="pluginRowBlock">{props.children}</div>
-				</div>
-			</div>
-		);
-
-		const Button = props => (
-			<div className="pluginWrapper buttonWrapper">
-				<a className="button" href={props.href} target={props.target}>
-					{props.children}
-				</a>
-			</div>
-		);
-
-		return (
-			<SplashContainer>
-				<Logo img_src={`${baseUrl}img/docusaurus.svg`} />
-				<div className="inner">
-					<ProjectTitle siteConfig={siteConfig} />
-					<PromoSection>
-						<Button href="#try">Try It Out</Button>
-						<Button href={docUrl('doc1.html')}>Example Link</Button>
-						<Button href={docUrl('howto.html')}>Example Link 2</Button>
-					</PromoSection>
-				</div>
-			</SplashContainer>
-		);
-	}
+		</div>
+	);
 }
 
-class Index extends React.Component {
-	render() {
-		const { config: siteConfig, language = '' } = this.props;
-		const { baseUrl } = siteConfig;
+function Logo({ img_src }) {
+	return (
+		<div className="projectLogo">
+			<img src={img_src} alt="Project Logo" />
+		</div>
+	);
+}
 
-		const Block = props => (
-			<Container padding={['bottom', 'top']} id={props.id} background={props.background}>
-				<GridBlock align="center" contents={props.children} layout={props.layout} />
-			</Container>
-		);
+function ProjectTitle({ siteConfig }) {
+	return (
+		<h2 className="projectTitle">
+			{siteConfig.title}
+			<small>{siteConfig.tagline}</small>
+		</h2>
+	);
+}
 
-		const FeatureCallout = () => (
-			<div className="productShowcaseSection paddingBottom" style={{ textAlign: 'center' }}>
-				<h2>Feature Callout</h2>
-				<MarkdownBlock>These are features of this project</MarkdownBlock>
+function PromoSection({ children }) {
+	return (
+		<div className="section promoSection">
+			<div className="promoRow">
+				<div className="pluginRowBlock">{children}</div>
 			</div>
-		);
+		</div>
+	);
+}
 
-		const TryOut = () => (
-			<Block id="try">
-				{[
-					{
-						content: 'Talk about trying this out',
-						image: `${baseUrl}img/docusaurus.svg`,
-						imageAlign: 'left',
-						title: 'Try it Out',
-					},
-				]}
-			</Block>
-		);
+function Button(props) {
+	return (
+		<div className="pluginWrapper buttonWrapper">
+			<a className="button" href={props.href} target={props.target}>
+				{props.children}
+			</a>
+		</div>
+	);
+}
 
-		const Description = () => (
-			<Block background="dark">
-				{[
-					{
-						content: 'This is another description of how this project is useful',
-						image: `${baseUrl}img/docusaurus.svg`,
-						imageAlign: 'right',
-						title: 'Description',
-					},
-				]}
-			</Block>
-		);
+function HomeSplash(props) {
+	const { siteConfig, language = '' } = props;
+	const { baseUrl, docsUrl } = siteConfig;
+	const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+	const langPart = `${language ? `${language}/` : ''}`;
+	const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
 
-		const LearnHow = () => (
-			<Block background="light">
-				{[
-					{
-						content: 'Talk about learning how to use this',
-						image: `${baseUrl}img/docusaurus.svg`,
-						imageAlign: 'right',
-						title: 'Learn How',
-					},
-				]}
-			</Block>
-		);
 
-		const Features = () => (
-			<Block layout="fourColumn">
-				{[
-					{
-						content: 'This is the content of my feature',
-						image: `${baseUrl}img/docusaurus.svg`,
-						imageAlign: 'top',
-						title: 'Feature One',
-					},
-					{
-						content: 'The content of my second feature',
-						image: `${baseUrl}img/docusaurus.svg`,
-						imageAlign: 'top',
-						title: 'Feature Two',
-					},
-				]}
-			</Block>
-		);
+	return (
+		<SplashContainer>
+			<Logo img_src={`${baseUrl}img/docusaurus.svg`} />
+			<div className="inner">
+				<ProjectTitle siteConfig={siteConfig} />
+				<PromoSection>
+					<Button siteConfig={siteConfig} href="#try">Try It Out</Button>
+					<Button siteConfig={siteConfig} href={docUrl('doc1.html')}>Example Link</Button>
+					<Button siteConfig={siteConfig} href={docUrl('howto.html')}>Example Link 2</Button>
+				</PromoSection>
+			</div>
+		</SplashContainer>
+	);
+}
 
-		const Showcase = () => {
-			if ((siteConfig.users || []).length === 0) {
-				return null;
-			}
+function Block(props) {
+	return (
+		<Container padding={['bottom', 'top']} id={props.id} background={props.background}>
+			<GridBlock align={props.align} contents={props.children} layout={props.layout} />
+		</Container>
+	);
+}
+Block.defaultProps = {
+	align: 'center',
+};
 
-			const showcase = siteConfig.users
-				.filter(user => user.pinned)
-				.map(user => (
-					<a href={user.infoLink} key={user.infoLink}>
-						<img src={user.image} alt={user.caption} title={user.caption} />
-					</a>
-				));
+function FeatureCallout() {
+	return (
+		<div className="productShowcaseSection paddingBottom" style={{ textAlign: 'center' }}>
+			<h2>Feature Callout</h2>
+			<MarkdownBlock>These are features of this project</MarkdownBlock>
+		</div>
+	);
+}
 
-			const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
+function Index(props) {
+	const { config: siteConfig, language = '' } = props;
+	const { baseUrl } = siteConfig;
 
-			return (
-				<div className="productShowcaseSection paddingBottom">
-					<h2>Who is Using This?</h2>
-					<p>This project is used by all these people</p>
-					<div className="logos">{showcase}</div>
-					<div className="more-users">
-						<a className="button" href={pageUrl('users.html')}>
-							More {siteConfig.title} Users
-						</a>
-					</div>
-				</div>
-			);
-		};
+	const TryOut = () => (
+		<Block id="try" align="left">
+			{[
+				{
+					title: 'Try out CMF',
+					content: `
+\`\`\`
+npm install -g yo generator-talend
+yo talend:react-cmf
+\`\`\`
+
+					`,
+					image: `${baseUrl}img/try.png`,
+					imageAlign: 'left',
+				},
+			]}
+		</Block>
+	);
+
+	const Description = () => (
+		<Block background="dark">
+			{[
+				{
+					content: 'This is another description of how this project is useful',
+					image: `${baseUrl}img/docusaurus.svg`,
+					imageAlign: 'right',
+					title: 'Description',
+				},
+			]}
+		</Block>
+	);
+
+	const LearnHow = () => (
+		<Block background="light">
+			{[
+				{
+					content: 'Talk about learning how to use this',
+					image: `${baseUrl}img/docusaurus.svg`,
+					imageAlign: 'right',
+					title: 'Learn How',
+				},
+			]}
+		</Block>
+	);
+
+	const Features = () => (
+		<Block layout="fourColumn">
+			{[
+				{
+					title: 'Settings',
+					content: 'cmfConnect()(Component) and your Component bring powerfull tools. [Read more](/docs/core-settings)',
+					image: `${baseUrl}img/docusaurus.svg`,
+					imageAlign: 'top',
+				},
+				{
+					title: 'Registry for DI',
+					content: 'start your app in just two lines of code and bring redux for free [Read more](/docs/core-registry)',
+					image: `${baseUrl}img/docusaurus.svg`,
+					imageAlign: 'top',
+				},
+			]}
+		</Block>
+	);
+
+	const Showcase = () => {
+		if ((siteConfig.users || []).length === 0) {
+			return null;
+		}
+
+		const showcase = siteConfig.users
+			.filter(user => user.pinned)
+			.map(user => (
+				<a href={user.infoLink} key={user.infoLink}>
+					<img src={user.image} alt={user.caption} title={user.caption} />
+				</a>
+			));
+
+		const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
 
 		return (
-			<div>
-				<HomeSplash siteConfig={siteConfig} language={language} />
-				<div className="mainContainer">
-					<Features />
-					<FeatureCallout />
-					<LearnHow />
-					<TryOut />
-					<Description />
-					<Showcase />
+			<div className="productShowcaseSection paddingBottom">
+				<h2>Who is Using This?</h2>
+				<p>This project is used by all these people</p>
+				<div className="logos">{showcase}</div>
+				<div className="more-users">
+					<a className="button" href={pageUrl('users.html')}>
+						More {siteConfig.title} Users
+					</a>
 				</div>
 			</div>
 		);
-	}
+	};
+
+	return (
+		<div>
+			<HomeSplash siteConfig={siteConfig} language={language} />
+			<div className="mainContainer">
+				<Features />
+				<FeatureCallout />
+				<LearnHow />
+				<TryOut />
+				<Description />
+				<Showcase />
+			</div>
+		</div>
+	);
 }
 
 module.exports = Index;
