06f8cf8eee312ac7ed18dff6f1b5d775b7950c8e
Nicolas Comont <ncomont@talend.com>
2019-01-09 16:23:05 +0100

    fix: resourcepicker coverage

--- packages/components/src/ResourcePicker/ResourcePicker.snap.test.js
+++ packages/components/src/ResourcePicker/ResourcePicker.snap.test.js
@@ -3,54 +3,63 @@ import { shallow } from 'enzyme';
 
 import ResourcePicker from './ResourcePicker.component';
 
+
+const collection = [
+	{
+		id: 0,
+		name: 'Title with few actions',
+		modified: '2016-09-22',
+		author: 'Jean-Pierre DUPONT',
+		display: 'text',
+		icon: 'talend-file-xls-o',
+	},
+	{
+		id: 1,
+		name: 'Title with lot of actions',
+		modified: '2016-09-22',
+		display: 'text',
+		icon: 'talend-file-xls-o',
+	},
+	{
+		id: 2,
+		name: 'Title with persistant actions',
+		modified: '2016-09-22',
+		author: 'Jean-Pierre DUPONT',
+		display: 'text',
+		icon: 'talend-file-xls-o',
+	},
+];
+
 describe('ResourcePicker component snaps', () => {
-	describe('renderers', () => {
-		it('should render ResourcePicker without any Resource', () => {
-			const props = {
-				toolbar: {},
-				collection: [],
-			};
+	it('should render ResourcePicker without any Resource', () => {
+		const props = {
+			toolbar: {},
+			collection: [],
+		};
 
-			const wrapper = shallow(<ResourcePicker {...props} />);
+		const wrapper = shallow(<ResourcePicker {...props} />);
 
-			expect(wrapper.getElement()).toMatchSnapshot();
-		});
+		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 
-	describe('renderers', () => {
-		it('should render ResourcePicker with some Resources', () => {
-			const props = {
-				toolbar: {},
-				collection: [
-					{
-						id: 0,
-						name: 'Title with few actions',
-						modified: '2016-09-22',
-						author: 'Jean-Pierre DUPONT',
-						display: 'text',
-						icon: 'talend-file-xls-o',
-					},
-					{
-						id: 1,
-						name: 'Title with lot of actions',
-						modified: '2016-09-22',
-						display: 'text',
-						icon: 'talend-file-xls-o',
-					},
-					{
-						id: 2,
-						name: 'Title with persistant actions',
-						modified: '2016-09-22',
-						author: 'Jean-Pierre DUPONT',
-						display: 'text',
-						icon: 'talend-file-xls-o',
-					},
-				],
-			};
+	it('should render ResourcePicker with some Resources', () => {
+		const props = {
+			toolbar: {},
+			collection,
+		};
 
-			const wrapper = shallow(<ResourcePicker {...props} />);
+		const wrapper = shallow(<ResourcePicker {...props} />);
 
-			expect(wrapper.getElement()).toMatchSnapshot();
-		});
+		expect(wrapper.getElement()).toMatchSnapshot();
+	});
+
+	it('should render ResourcePicker without toolbar', () => {
+		const props = {
+			collection,
+		};
+
+		const wrapper = shallow(<ResourcePicker {...props} />);
+
+		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 });
