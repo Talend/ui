d9fa92aa242429fa898df3d7c51847e69f731b03
travis <travis>
2019-01-09 15:52:32 +0000

    chore(ci): prettier

--- packages/components/src/ResourcePicker/Toolbar/SortOptions/SortOptions.test.js
+++ packages/components/src/ResourcePicker/Toolbar/SortOptions/SortOptions.test.js
@@ -3,7 +3,6 @@ import { Button } from 'react-bootstrap';
 import { mount } from 'enzyme';
 import SortOptions, { TYPES } from './SortOptions.component';
 
-
 describe('SortOptions', () => {
 	it('should trigger onChange callback with the new state on click', () => {
 		const onChange = jest.fn();
@@ -14,14 +13,21 @@ describe('SortOptions', () => {
 				dateAsc
 				icon={'talend-sort-desc'}
 				types={[TYPES.NAME, TYPES.DATE]}
-			/>);
+			/>,
+		);
 
 		expect(onChange).not.toBeCalled();
 
-		wrapper.find(Button).at(0).simulate('click');
+		wrapper
+			.find(Button)
+			.at(0)
+			.simulate('click');
 		expect(onChange).toBeCalledWith(TYPES.NAME, true);
 
-		wrapper.find(Button).at(1).simulate('click');
+		wrapper
+			.find(Button)
+			.at(1)
+			.simulate('click');
 		expect(onChange).toBeCalledWith(TYPES.DATE, false);
 	});
 });
