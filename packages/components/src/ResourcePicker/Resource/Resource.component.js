4376d285b6274fc4d85204dcb443e1a22e8ff3ae
Nicolas Comont <ncomont@talend.com>
2019-01-10 14:20:07 +0100

    fix: follow review

--- packages/components/src/ResourcePicker/Resource/Resource.component.js
+++ packages/components/src/ResourcePicker/Resource/Resource.component.js
@@ -11,6 +11,7 @@ import Icon from '../../Icon';
 
 import theme from './Resource.scss';
 
+
 const FLAGS = {
 	CERTIFIED: 'talend-badge',
 	FAVORITE: 'talend-star',
@@ -56,9 +57,14 @@ function Resource({ parent, index, style, t }) {
 				</small>
 			</div>
 			<div className={classNames('flags-container', theme['flags-container'])}>
-				{flags.map(flag => (
-					<Icon name={FLAGS[flag]} />
-				))}
+				{
+					flags.map(flag => (
+						<Icon
+							className={classNames(theme.flag)}
+							name={FLAGS[flag]}
+						/>
+					))
+				}
 			</div>
 		</div>
 	);
