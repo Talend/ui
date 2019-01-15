0ac071cb5ccdffbb8731d9bf70497d0603d7a013
jlame <jlame@talend.com>
2019-01-15 16:36:42 +0100

    reverting viewlayout update

--- packages/components/src/DateTimePickers/views/ViewLayout/ViewLayout.component.js
+++ packages/components/src/DateTimePickers/views/ViewLayout/ViewLayout.component.js
@@ -14,7 +14,6 @@ function ViewLayout(props) {
 				<div className={classNames(theme['element-container'], theme.right)}>{rightElement}</div>
 			</div>
 			<div className={classNames(theme['element-container'], theme.body)}>{props.bodyElement}</div>
-			<div className={theme.footer}>{props.footerElement}</div>
 		</div>
 	);
 }
@@ -26,7 +25,6 @@ ViewLayout.propTypes = {
 		rightElement: PropTypes.element,
 	}).isRequired,
 	bodyElement: PropTypes.element.isRequired,
-	footerElement: PropTypes.element,
 };
 
 export default ViewLayout;
