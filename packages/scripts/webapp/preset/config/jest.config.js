cb7ae00bd9153ee264b494df60f5737d403b9c60
jmfrancois <jmfrancois@talend.com>
2019-01-31 10:49:45 +0100

    chore: update

--- packages/scripts/webapp/preset/config/jest.config.js
+++ packages/scripts/webapp/preset/config/jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
 	moduleNameMapper: {
 		'^.+\\.(css|scss)$': '<rootDir>/node_modules/@talend/scripts/preset/config/test/style-mock.js',
 	},
-	setupTestFrameworkScriptFile: '<rootDir>/node_modules/@talend/scripts/preset/config/test/test-setup.js',
+	setupFilesAfterEnv: ['<rootDir>/node_modules/@talend/scripts/preset/config/test/test-setup.js'],
 	transform: { '^.+\\.js$': '<rootDir>/node_modules/@talend/scripts/preset/config/test/jest-preprocess.js' },
 };
