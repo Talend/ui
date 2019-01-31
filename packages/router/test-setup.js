83703ca0554dbd8055487c850166de92473da01a
jmfrancois <jmfrancois@talend.com>
2019-01-31 10:56:13 +0100

    chore: revert enzyme change in test setup

--- packages/router/test-setup.js
+++ packages/router/test-setup.js
@@ -8,6 +8,23 @@
 import '@babel/polyfill';
 
 import { configure } from 'enzyme';
-import AdapterReact from 'enzyme-adapter-react-16';
+
+function getMajorVersion() {
+	if (!process.env.REACT_VERSION) {
+		return '16';
+	}
+	return process.env.REACT_VERSION.replace('^', '').split('.')[0];
+}
+
+const REACT_VERSION = getMajorVersion();
+
+let AdapterReact;
+if (REACT_VERSION === '15') {
+	AdapterReact = require('enzyme-adapter-react-15');
+} else if (REACT_VERSION === '16') {
+	AdapterReact = require('enzyme-adapter-react-16');
+} else {
+	throw new Error(`Unsupported version of React: ${REACT_VERSION}`);
+}
 
 configure({ adapter: new AdapterReact() });
