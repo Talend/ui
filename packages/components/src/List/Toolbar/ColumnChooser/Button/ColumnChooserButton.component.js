20f6b5d260e66079c4cf63d2c1943fe72cac5d34
nicomaligne <nmaligne@gmail.com>
2019-02-04 16:37:40 +0100

    clean button first draft

--- packages/components/src/List/Toolbar/ColumnChooser/Button/ColumnChooserButton.component.js
+++ packages/components/src/List/Toolbar/ColumnChooser/Button/ColumnChooserButton.component.js
@@ -3,30 +3,7 @@ import ColumnChooserModal from '../Modal/ColumnChooserModal.component';
 import ActionButton from '../../../../Actions/ActionButton';
 
 export default class ColumnChosser extends React.Component {
-	state = {
-		opened: false,
-	};
-
-	/*
-	onClick = event => {
-		if (this.props.onClick) {
-			this.props.onClick(event, this.getState());
-		}
-		this.setState(prevState => {
-			return {
-				opened: !prevState.opened,
-			};
-		});
-	};
-	*/
-	getState() {
-		return {
-			opened: this.state.opened || this.props.opened,
-		};
-	}
-
 	render() {
-		const props = this.getState();
 		return (
 			<ActionButton
 				id="button-column-chooser"
