78a745b9e6b41ffa6e80aa1a28d2ec4ceb73b66b
Jérémie DESCOTTES <jdescottes@talend.com>
2019-02-04 17:45:23 +0100

    fix: path to preset without 'webapp' sub directory

--- packages/scripts/webapp/preset/config/jest.config.js
+++ packages/scripts/webapp/preset/config/jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
 	rootDir: process.cwd(),
 	testRegex: 'src/.*\\.test.js$',
 	moduleNameMapper: {
-		'^.+\\.(css|scss)$': '<rootDir>/node_modules/@talend/scripts/preset/config/test/style-mock.js',
+		'^.+\\.(css|scss)$': '<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/style-mock.js',
 	},
-	setupTestFrameworkScriptFile: '<rootDir>/node_modules/@talend/scripts/preset/config/test/test-setup.js',
-	transform: { '^.+\\.js$': '<rootDir>/node_modules/@talend/scripts/preset/config/test/jest-preprocess.js' },
+	setupTestFrameworkScriptFile: '<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/test-setup.js',
+	transform: { '^.+\\.js$': '<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/jest-preprocess.js' },
 };
