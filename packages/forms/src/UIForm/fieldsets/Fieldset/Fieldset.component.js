50ef795df2d7eecfad13f6ad290f5e7be6df5278
Jimmy Somsanith <jsomsanith@talend.com>
2019-02-04 14:33:18 +0100

    fix(Tabs): hide tab panes legends

--- packages/forms/src/UIForm/fieldsets/Fieldset/Fieldset.component.js
+++ packages/forms/src/UIForm/fieldsets/Fieldset/Fieldset.component.js
@@ -1,21 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import classnames from 'classnames';
 import Widget from '../../Widget';
 
 export default function Fieldset(props) {
 	const { schema, ...restProps } = props;
-	const { title, items } = schema;
+	const { title, items, options } = schema;
 
 	return (
 		<fieldset className="form-group">
-			{title && (<legend>{title}</legend>)}
-			{items.map((itemSchema, index) => (
-				<Widget
-					{...restProps}
-					key={index}
-					schema={itemSchema}
-				/>
-			))}
+			<legend className={classnames({ 'sr-only': options && options.hideTitle })}>{title}</legend>
+			{items.map((itemSchema, index) => <Widget {...restProps} key={index} schema={itemSchema} />)}
 		</fieldset>
 	);
 }
@@ -23,7 +18,7 @@ export default function Fieldset(props) {
 if (process.env.NODE_ENV !== 'production') {
 	Fieldset.propTypes = {
 		schema: PropTypes.shape({
-			title: PropTypes.string,
+			title: PropTypes.string.isRequired,
 			items: PropTypes.array.isRequired,
 		}).isRequired,
 	};
