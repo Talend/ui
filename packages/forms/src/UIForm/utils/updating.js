4ddc26814e8506b7a926d16994cfe59431a7eb9b
jmfrancois <jmfrancois@talend.com>
2019-02-08 15:28:47 +0100

    test: add

--- packages/forms/src/UIForm/utils/updating.js
+++ packages/forms/src/UIForm/utils/updating.js
@@ -1,9 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export function isUpdating(data = [], schema) {
-	if (!Array.isArray(data)) {
-		return data;
-	}
-	if (!schema) {
+	if (!Array.isArray(data) || !schema || !schema.key) {
 		return false;
 	}
 	// we need to support current and parent path
