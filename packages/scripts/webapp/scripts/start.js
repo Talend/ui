273e97d3a8c9a076db00a3957771fe474edea0e5
Jimmy Somsanith <jsomsanith@talend.com>
2019-02-21 15:26:30 +0100

    feat(Scripts): analyze option and spread user cli options (#2049)

--- packages/scripts/webapp/scripts/start.js
+++ packages/scripts/webapp/scripts/start.js
@@ -3,16 +3,18 @@ const { hereRelative, resolveBin } = require('../utils/path-resolver');
 
 const webpackDevServer = resolveBin('webpack-dev-server');
 
-module.exports = function start(env) {
+module.exports = function start(env, _, options) {
 	return spawn.sync(
 		webpackDevServer,
 		[
 			'--inline',
-			'--config', hereRelative(__dirname, '../config/webpack.config.js'),
-			'--content-base', 'build/',
+			'--config',
+			hereRelative(__dirname, '../config/webpack.config.js'),
 			'--open',
 			'--progress',
+			'--display-error-details',
+			...options,
 		],
-		{ stdio: 'inherit', env }
+		{ stdio: 'inherit', env },
 	);
 };
