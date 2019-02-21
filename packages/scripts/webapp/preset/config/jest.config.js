6f29d0ad7854123e673f97a94512db4332f89078
Jimmy Somsanith <jsomsanith@talend.com>
2019-02-21 22:55:12 +0100

    feat(Scripts): complete jest config inn talend preset

--- packages/scripts/webapp/preset/config/jest.config.js
+++ packages/scripts/webapp/preset/config/jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-	rootDir: process.cwd(),
-	testRegex: 'src/.*\\.test.js$',
 	moduleNameMapper: {
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttjest-preprocess.jsf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/file-mock.js',
 		'^.+\\.(css|scss)$':
 			'<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/style-mock.js',
 	},
+	rootDir: process.cwd(),
 	setupFilesAfterEnv: [
 		'<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/test-setup.js',
 	],
+	testEnvironment: 'jest-environment-jsdom-global',
+	testRegex: 'src/.*\\.test.js$',
 	transform: {
 		'^.+\\.js$':
 			'<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/jest-preprocess.js',
