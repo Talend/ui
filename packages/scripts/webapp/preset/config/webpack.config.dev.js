5fcebf49bc9c7f37c3d7501299c5236c1809ae86
Sébastien Romain <sromain@talend.com>
2019-02-21 09:59:41 +0100

    Update packages/scripts/webapp/preset/config/webpack.config.dev.js
    Co-Authored-By: jsomsanith <jsomsanith@talend.com>

--- packages/scripts/webapp/preset/config/webpack.config.dev.js
+++ packages/scripts/webapp/preset/config/webpack.config.dev.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = ({ getUserConfig }) => {
 	const plugins = [];
-	if (getUserConfig('cmf') === true) {
+	if (getUserConfig('cmf')) {
 		// eslint-disable-next-line global-require,import/newline-after-import
 		const ReactCMFWebpackPlugin = require('@talend/react-cmf-webpack-plugin');
 		plugins.push(new ReactCMFWebpackPlugin({ watch: true }));
