fcffe215403600aad6a6ccbb36aafb3d4f785365
Jimmy Somsanith <jsomsanith@talend.com>
2019-02-21 23:09:16 +0100

    Update jest.config.js

--- packages/scripts/webapp/preset/config/jest.config.js
+++ packages/scripts/webapp/preset/config/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	moduleNameMapper: {
-		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttjest-preprocess.jsf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|woff|woff2)$':
 			'<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/file-mock.js',
 		'^.+\\.(css|scss)$':
 			'<rootDir>/node_modules/@talend/scripts/webapp/preset/config/test/style-mock.js',
