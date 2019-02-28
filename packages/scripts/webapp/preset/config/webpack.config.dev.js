cbdc64ea13e9e57d9b8ffd8e2d67cbc83d01c78e
Jimmy Somsanith <jsomsanith@talend.com>
2019-02-28 17:40:39 +0100

    Refacto

--- packages/scripts/webapp/preset/config/webpack.config.dev.js
+++ packages/scripts/webapp/preset/config/webpack.config.dev.js
@@ -1,26 +1,16 @@
 const path = require('path');
 
-module.exports = ({ getUserConfig }) => {
-	const plugins = [];
-	if (getUserConfig('cmf')) {
-		// eslint-disable-next-line global-require,import/newline-after-import
-		const ReactCMFWebpackPlugin = require('@talend/react-cmf-webpack-plugin');
-		plugins.push(new ReactCMFWebpackPlugin({ watch: true }));
-	}
-
-	return {
-		mode: 'development',
-		watchOptions: {
-			aggregateTimeout: 300,
-			poll: 1000,
-		},
-		devServer: {
-			port: 3000,
-			stats: 'errors-only',
-			historyApiFallback: true,
-			contentBase: path.join(process.cwd(), '/dist'),
-		},
-		plugins,
-		devtool: 'source-map',
-	};
-};
+module.exports = () => ({
+	mode: 'development',
+	watchOptions: {
+		aggregateTimeout: 300,
+		poll: 1000,
+	},
+	devServer: {
+		port: 3000,
+		stats: 'errors-only',
+		historyApiFallback: true,
+		contentBase: path.join(process.cwd(), '/dist'),
+	},
+	devtool: 'source-map',
+});
--- packages/scripts/webapp/preset/config/webpack.config.js
+++ packages/scripts/webapp/preset/config/webpack.config.js
@@ -9,10 +9,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
-const PostCssSafeParser = require('postcss-safe-parser');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
+const ReactCMFWebpackPlugin = require('@talend/react-cmf-webpack-plugin');
 const TalendHTML = require('@talend/html-webpack-plugin');
 const AppLoader = require('@talend/react-components/lib/AppLoader/constant').default;
 const DEFAULT_APP_LOADER_ICON =
@@ -195,6 +193,7 @@ module.exports = ({ getUserConfig, mode }) => {
 	const appLoaderIcon = getUserConfig(['html', 'appLoaderIcon'], DEFAULT_APP_LOADER_ICON);
 	const userSassData = getUserConfig('sass');
 	const userCopyConfig = getUserConfig('copy');
+	const cmf = getUserConfig('cmf');
 	const { theme } = userSassData;
 
 	const sassData = getSassData(userSassData);
@@ -291,71 +290,9 @@ module.exports = ({ getUserConfig, mode }) => {
 			new TalendHTML({ appLoaderIcon }),
 			new CopyWebpackPlugin(getCopyConfig(userCopyConfig)),
 			new webpack.BannerPlugin({ banner: LICENSE_BANNER }),
+			cmf && new ReactCMFWebpackPlugin({ watch: mode === 'development' }),
 		],
 		optimization: {
-			minimize: mode === 'production',
-			minimizer: [
-				// This is only used in production mode
-				new TerserPlugin({
-					terserOptions: {
-						parse: {
-							// we want terser to parse ecma 8 code. However, we don't want it
-							// to apply any minfication steps that turns valid ecma 5 code
-							// into invalid ecma 5 code. This is why the 'compress' and 'output'
-							// sections only apply transformations that are ecma 5 safe
-							// https://github.com/facebook/create-react-app/pull/4234
-							ecma: 8,
-						},
-						compress: {
-							ecma: 5,
-							warnings: false,
-							// Disabled because of an issue with Uglify breaking seemingly valid code:
-							// https://github.com/facebook/create-react-app/issues/2376
-							// Pending further investigation:
-							// https://github.com/mishoo/UglifyJS2/issues/2011
-							comparisons: false,
-							// Disabled because of an issue with Terser breaking valid code:
-							// https://github.com/facebook/create-react-app/issues/5250
-							// Pending futher investigation:
-							// https://github.com/terser-js/terser/issues/120
-							inline: 2,
-						},
-						mangle: {
-							safari10: true,
-						},
-						output: {
-							ecma: 5,
-							comments: false,
-							// Turned on because emoji and regex is not minified properly using default
-							// https://github.com/facebook/create-react-app/issues/2488
-							ascii_only: true,
-						},
-					},
-					// Use multi-process parallel running to improve the build speed
-					// Default number of concurrent runs: os.cpus().length - 1
-					parallel: true,
-					// Enable file caching
-					cache: true,
-					sourceMap: true,
-				}),
-				// This is only used in production mode
-				new OptimizeCSSAssetsPlugin({
-					cssProcessorOptions: {
-						parser: PostCssSafeParser,
-						map:
-							mode === 'production'
-								? {
-										// `inline: false` forces the sourcemap to be output into a
-										// separate file
-										inline: false,
-										// `annotation: true` appends the sourceMappingURL to the end of
-										// the css file, helping the browser find the sourcemap
-										annotation: true,
-								  }
-								: false,
-					},
-				}),
-			],
 			// Automatically split vendor and commons
 			// https://twitter.com/wSokra/status/969633336732905474
 			// https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
