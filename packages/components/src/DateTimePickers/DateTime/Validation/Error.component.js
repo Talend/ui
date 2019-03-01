d42e859a7881d34d9124a9ec41d10a364370e100
yonmey <yonmey@gmail.com>
2019-03-01 11:07:41 +0100

    remove translation method

--- packages/components/src/DateTimePickers/DateTime/Validation/Error.component.js
+++ packages/components/src/DateTimePickers/DateTime/Validation/Error.component.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import getErrorMessage from '../error-messages';
 
 export default function Error({ hidden, errors, id }) {
 	const classNames = classnames({ 'sr-only': hidden });
 	return (
 		<div id={id} className={classNames}>
 			{errors.map((error, index) => (
-				<span key={index}>{getErrorMessage(error.message)}</span>
+				<span key={index}>{error.message}</span>
 			))}
 		</div>
 	);
