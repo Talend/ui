3dc6bcdcd5c50ec25c65d423506b370df5609e0d
travis <travis>
2019-03-07 00:06:00 +0000

    chore(ci): prettier

--- packages/components/src/GuidedTour/GuidedTour.component.js
+++ packages/components/src/GuidedTour/GuidedTour.component.js
@@ -6,14 +6,7 @@ import classNames from 'classnames';
 import theme from './GuidedTour.scss';
 
 function GuidedTour({ onRequestClose, isOpen = true, steps }) {
-
-	return (
-		<Tour
-			steps={steps}
-			isOpen={isOpen}
-			onRequestClose={onRequestClose}
-		/>
-	);
+	return <Tour steps={steps} isOpen={isOpen} onRequestClose={onRequestClose} />;
 }
 
 GuidedTour.displayName = 'GuidedTour';
@@ -22,18 +15,17 @@ if (process.env.NODE_ENV !== 'production') {
 	GuidedTour.propTypes = {
 		// id: PropTypes.string,
 		// className: PropTypes.string,
-		steps: PropTypes.arrayOf(PropTypes.shape({
-			selector: PropTypes.string,
-			content: PropTypes.oneOfType([
-				PropTypes.node,
-				PropTypes.element,
-				PropTypes.func,
-			]).isRequired,
-			position: PropTypes.oneOf(['top', 'right', 'bottom', 'left', 'center']),
-			action: PropTypes.func,
-			style: PropTypes.object,
-			stepInteraction: PropTypes.bool,
-		})),
+		steps: PropTypes.arrayOf(
+			PropTypes.shape({
+				selector: PropTypes.string,
+				content: PropTypes.oneOfType([PropTypes.node, PropTypes.element, PropTypes.func])
+					.isRequired,
+				position: PropTypes.oneOf(['top', 'right', 'bottom', 'left', 'center']),
+				action: PropTypes.func,
+				style: PropTypes.object,
+				stepInteraction: PropTypes.bool,
+			}),
+		),
 	};
 }
 
