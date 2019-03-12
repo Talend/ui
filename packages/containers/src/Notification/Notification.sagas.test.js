450ec5aa350ac6dbb318b301b907d67491241767
Jérémie DESCOTTES <jdescottes@talend.com>
2019-03-12 15:01:36 +0100

    test: Add saga test case

---
+++ packages/containers/src/Notification/Notification.sagas.test.js
@@ -0,0 +1,56 @@
+import Immutable from 'immutable';
+import { runSaga } from 'redux-saga';
+import { onAddNotification } from './Notification.sagas';
+
+describe('Notification sagas', () => {
+	describe('onAddNotification', () => {
+		const onAddNotificationAction = {
+			notification: {
+				message: 'WHATEVER_MESSAGE',
+			},
+		};
+
+		it('should dispatch the component state updated with the new notification', async () => {
+			const dispatched = [];
+
+			await runSaga({
+				dispatch: a => dispatched.push(a),
+				getState: () => ({
+					cmf: {
+						components: Immutable.fromJS({
+							'Container(Notification)': {
+								Notification: {
+									notifications: [],
+								},
+							},
+						}),
+					},
+				}),
+			}, onAddNotification, onAddNotificationAction).done;
+
+			// Convert first, the half immutable payload to a full one then back to a full js one
+			const actions = Immutable
+				.fromJS(dispatched)
+				.toJS();
+
+			expect(actions[0]).toEqual({
+				type: 'Container(Notification).setState',
+				cmf: expect.objectContaining({
+					componentState: expect.objectContaining({
+						componentName: 'Container(Notification)',
+						componentState: expect.objectContaining({
+							notifications: [
+								expect.objectContaining({
+									id: expect.anything(),
+									message: 'WHATEVER_MESSAGE',
+								}),
+							],
+						}),
+						key: 'Notification',
+						type: 'REACT_CMF.COMPONENT_MERGE_STATE',
+					}),
+				}),
+			});
+		});
+	});
+});
