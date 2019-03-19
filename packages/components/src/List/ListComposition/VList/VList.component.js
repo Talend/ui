c749a7fa224d3f4a4a7187d62d72cd05d7369a39
Jimmy Somsanith <jsomsanith@talend.com>
2019-03-19 16:51:20 +0100

    Add default prop in VList

--- packages/components/src/List/ListComposition/VList/VList.component.js
+++ packages/components/src/List/ListComposition/VList/VList.component.js
@@ -4,14 +4,8 @@ import { useListContext } from '../context';
 import VirtualizedList from '../../../VirtualizedList';
 
 function VList(props) {
-	const { displayMode, collection } = useListContext();
-	return (
-		<VirtualizedList
-			collection={collection}
-			type={displayMode && displayMode.toUpperCase()}
-			{...props}
-		/>
-	);
+	const { displayMode = 'table', collection } = useListContext();
+	return <VirtualizedList collection={collection} type={displayMode.toUpperCase()} {...props} />;
 }
 Object.entries({ ...VirtualizedList }).forEach(([key, value]) => {
 	VList[key] = value;
