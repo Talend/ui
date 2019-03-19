83e978396a7ad7a43fe3f9f9878907121f8dc8fe
Jimmy Somsanith <jsomsanith@talend.com>
2019-03-19 10:58:22 +0100

    Update DisplayMode after review

--- packages/components/src/List/ListComposition/DisplayMode/DisplayMode.component.js
+++ packages/components/src/List/ListComposition/DisplayMode/DisplayMode.component.js
@@ -4,7 +4,7 @@ import { translate } from 'react-i18next';
 import { Navbar, NavDropdown, Nav, MenuItem } from 'react-bootstrap';
 import uuid from 'uuid';
 
-import { ListContext } from '../context';
+import { useListContext } from '../context';
 import Icon from '../../../Icon';
 import getDefaultT from '../../../translate';
 import I18N_DOMAIN_COMPONENTS from '../../../constants';
@@ -20,90 +20,67 @@ function getIcon(selected) {
 	}
 }
 
-class DisplayMode extends React.Component {
-	static displayName = 'List.DisplayMode';
-	static propTypes = {
-		displayModes: PropTypes.arrayOf(PropTypes.string),
-		id: PropTypes.string,
-		mode: PropTypes.string,
-		onChange: PropTypes.func.isRequired,
-		t: PropTypes.func,
-	};
-	static defaultProps = {
-		id: uuid.v4(),
-		displayModes: ['table', 'large'],
-		mode: 'table',
-		t: getDefaultT(),
-	};
-
-	getLabel(mode) {
-		const { t } = this.props;
-		switch (mode) {
-			case 'table':
-				return t('LIST_SELECT_DISPLAY_MODE_TABLE', { defaultValue: 'Table' });
-			case 'large':
-				return t('LIST_SELECT_DISPLAY_MODE_LARGE', { defaultValue: 'Expanded' });
-			default:
-				return t('LIST_SELECT_DISPLAY_MODE_TABLE', { defaultValue: 'Table' });
-		}
+function getLabel(option, t) {
+	switch (option) {
+		case 'table':
+			return t('LIST_SELECT_DISPLAY_MODE_TABLE', { defaultValue: 'Table' });
+		case 'large':
+			return t('LIST_SELECT_DISPLAY_MODE_LARGE', { defaultValue: 'Expanded' });
+		default:
+			return t('LIST_SELECT_DISPLAY_MODE_TABLE', { defaultValue: 'Table' });
 	}
+}
 
-	getMenuItems() {
-		return this.props.displayModes.map(option => {
-			const displayModeLabel = this.getLabel(option);
-			const { id, t } = this.props;
-			return (
-				<MenuItem
-					id={id && `${id}-${option}`}
-					key={option}
-					eventKey={option}
-					aria-label={t('LIST_SELECT_DISPLAY_MODE', {
-						defaultValue: 'Set {{displayMode}} as current display mode.',
-						displayMode: displayModeLabel,
+function DisplayMode(props) {
+	const { displayMode, onDisplayModeChange } = useListContext();
+	const { id, displayModes, mode = displayMode, onChange = onDisplayModeChange, t } = props;
+	return (
+		<React.Fragment>
+			<Navbar.Text>
+				<label htmlFor={id}>{t('LIST_TOOLBAR_DISPLAY', { defaultValue: 'Display:' })}</label>
+			</Navbar.Text>
+			<Nav>
+				<NavDropdown
+					id={id}
+					title={<Icon name={getIcon(mode)} />}
+					onSelect={(value, event) => onChange(event, value)}
+					aria-label={t('LIST_CHANGE_DISPLAY_MODE', {
+						defaultValue: 'Change display mode. Current display mode: {{displayMode}}.',
+						displayMode: mode,
 					})}
 				>
-					<Icon name={getIcon(option)} />
-					{displayModeLabel}
-				</MenuItem>
-			);
-		});
-	}
-
-	render() {
-		const { id, mode, onChange, t } = this.props;
-		const items = this.getMenuItems();
-		return (
-			<React.Fragment>
-				<Navbar.Text>
-					<label htmlFor={id}>{t('LIST_TOOLBAR_DISPLAY', { defaultValue: 'Display:' })}</label>
-				</Navbar.Text>
-				<Nav>
-					<NavDropdown
-						id={id}
-						title={<Icon name={getIcon(mode)} />}
-						onSelect={(value, event) => onChange(event, value)}
-						aria-label={t('LIST_CHANGE_DISPLAY_MODE', {
-							defaultValue: 'Change display mode. Current display mode: {{displayMode}}.',
-							displayMode: mode,
-						})}
-					>
-						{items}
-					</NavDropdown>
-				</Nav>
-			</React.Fragment>
-		);
-	}
-}
-
-function ContextualDisplayMode(props) {
-	return (
-		<ListContext.Consumer>
-			{({ displayMode, onDisplayModeChange }) => (
-				<DisplayMode mode={displayMode} onChange={onDisplayModeChange} {...props} />
-			)}
-		</ListContext.Consumer>
+					{displayModes.map(option => (
+						<MenuItem
+							id={`${id}-${option}`}
+							key={option}
+							eventKey={option}
+							aria-label={t('LIST_SELECT_DISPLAY_MODE', {
+								defaultValue: 'Set {{displayMode}} as current display mode.',
+								displayMode: getLabel(option, t),
+							})}
+						>
+							<Icon name={getIcon(option)} />
+							{getLabel(option, t)}
+						</MenuItem>
+					))}
+				</NavDropdown>
+			</Nav>
+		</React.Fragment>
 	);
 }
-ContextualDisplayMode.displayName = 'ListContext(List.DisplayMode)';
+DisplayMode.displayName = 'List.DisplayMode';
+DisplayMode.defaultProps = {
+	id: uuid.v4(),
+	displayModes: ['table', 'large'],
+	mode: 'table',
+	t: getDefaultT(),
+};
+DisplayMode.propTypes = {
+	displayModes: PropTypes.arrayOf(PropTypes.string),
+	id: PropTypes.string,
+	mode: PropTypes.string,
+	onChange: PropTypes.func.isRequired,
+	t: PropTypes.func,
+};
 
-export default translate(I18N_DOMAIN_COMPONENTS)(ContextualDisplayMode);
+export default translate(I18N_DOMAIN_COMPONENTS)(DisplayMode);
