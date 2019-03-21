1c69dd6a3fd43b791e1d832e7a3e24fd231f3982
Jimmy Somsanith <jsomsanith@talend.com>
2019-03-21 15:32:31 +0100

    fix(Scripts): custom .babelrc file load

--- packages/scripts/webapp/preset/config/babel-resolver.js
+++ packages/scripts/webapp/preset/config/babel-resolver.js
@@ -1,13 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
-function getBabelConfigPath() {
-	const userBabelrcPath = `${process.cwd()}/.babelrc`;
-	if (!fs.existsSync(userBabelrcPath)) {
-		return path.join(__dirname, '.babelrc.json');
-	}
-
-	const babelrc = JSON.parse(fs.readFileSync(userBabelrcPath, 'utf8'));
+function checkBabelJsonExtension(babelConfigJsonPath) {
+	const babelrc = JSON.parse(fs.readFileSync(babelConfigJsonPath, 'utf8'));
 	const babelrcExtends = '@talend/scripts/webapp/preset/config/.babelrc.json';
 	if (babelrc.extends !== babelrcExtends) {
 		throw new Error(`
@@ -15,12 +10,36 @@ function getBabelConfigPath() {
 				{ "extends": "${babelrcExtends}" }
 		`);
 	}
-	return userBabelrcPath;
+}
+
+function getBabelConfigPath() {
+	const userBabelrcPath = `${process.cwd()}/.babelrc`;
+	const userBabelrcJsonPath = `${process.cwd()}/.babelrc.json`;
+	const userBabelrcConfigJsPath = `${process.cwd()}/babelrc.config.js`;
+
+	if (fs.existsSync(userBabelrcPath)) {
+		checkBabelJsonExtension(userBabelrcPath);
+		return userBabelrcPath;
+	} else if (fs.existsSync(userBabelrcJsonPath)) {
+		checkBabelJsonExtension(userBabelrcJsonPath);
+		return userBabelrcJsonPath;
+	} else if (fs.existsSync(userBabelrcConfigJsPath)) {
+		return userBabelrcConfigJsPath;
+	} else {
+		return path.join(__dirname, '.babelrc.json');
+	}
 }
 
 function getBabelConfig() {
+	const babelConfigPath = getBabelConfigPath();
+
+	// .babelrc is a json, but without explicit .json extension, node require() tries to parse js
+	if (babelConfigPath.endsWith('.babelrc')) {
+		return JSON.parse(fs.readFileSync(babelConfigPath, 'utf8'));
+	}
+
 	// eslint-disable-next-line global-require
-	return require(getBabelConfigPath());
+	return require(babelConfigPath);
 }
 
 module.exports = {
