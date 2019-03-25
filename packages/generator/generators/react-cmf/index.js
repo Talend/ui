0b536f9cb547945516f47803a28e4def888d3026
Jimmy Somsanith <jsomsanith@talend.com>
2019-03-25 14:21:37 +0100

    fix(generator): react-cmf app

--- packages/generator/generators/react-cmf/index.js
+++ packages/generator/generators/react-cmf/index.js
@@ -42,7 +42,7 @@ module.exports = class CMFAppGenerator extends Generator {
 	}
 
 	writing() {
-		const fileToCopy = ['src', 'cmf.json', 'talend-scripts.json', 'test-setup.js'];
+		const fileToCopy = ['src', 'cmf.json', '.eslintrc'];
 		const tplToCopy = ['package.json'];
 		fileToCopy.forEach(name => {
 			this.fs.copy(this.templatePath(name), this.destinationPath(name));
