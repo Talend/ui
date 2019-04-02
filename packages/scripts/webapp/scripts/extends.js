dbbb400716e087cd4a6ac75fc265c5a20bbb232f
Jimmy Somsanith <jsomsanith@talend.com>
2019-04-02 16:52:36 +0200

    Refacto

--- packages/scripts/webapp/scripts/extends.js
+++ packages/scripts/webapp/scripts/extends.js
@@ -2,22 +2,39 @@ const fs = require('fs');
 const path = require('path');
 const template = require('lodash.template');
 const { getPreset } = require('../utils/preset');
+const { getUserConfigFile } = require('../utils/env');
 
-const jestExtendsTemplate = template(`const { defaults } = require('<%= jestConfigRelativePath %>');
+const jestExtendsTemplate = template(`const defaults = require('<%= presetConfigRelativePath %>');
 
 module.exports = {
 	...defaults,
 
 	// add/change default config here
-};`);
+};
+`);
 
 const eslintExtendsTemplate = template(`{
-  "extends": "./<%= eslintConfigRelativePath %>"
-}`);
+  "extends": "./<%= presetConfigRelativePath %>"
+}
+`);
 
 const babelExtendsTemplate = template(`{
-  "extends": "./<%= babelConfigRelativePath %>"
-}`);
+  "extends": "<%= presetConfigRelativePath %>"
+}
+`);
+
+function generateConfigFile({ configFileNames, defaultConfigFilePath, generateContent }) {
+	const userConfigFilePath = getUserConfigFile(configFileNames);
+	if (userConfigFilePath) {
+		const fileName = path.basename(userConfigFilePath);
+		console.log(`❌ ${fileName} already exists in your project folder. Skip extension creation.`);
+		return;
+	}
+
+	fs.writeFileSync(defaultConfigFilePath, generateContent());
+	const fileName = path.basename(defaultConfigFilePath);
+	console.log(`✅ ${fileName} created.`);
+}
 
 module.exports = function test(env, presetApi) {
 	const presetName = presetApi.getUserConfig(['preset'], 'talend');
@@ -25,53 +42,41 @@ module.exports = function test(env, presetApi) {
 	const rootPath = process.cwd();
 	const nodeModulesPath = path.join(rootPath, 'node_modules');
 
-	const userJestPath = path.join(rootPath, 'jest.config.js');
-	if (!fs.existsSync(userJestPath)) {
-		const jestConfigPath = preset.getJestConfigurationPath(presetApi);
-		const jestConfigRelativePath = path.relative(nodeModulesPath, jestConfigPath);
-		const jestExtendsCode = jestExtendsTemplate({ jestConfigRelativePath });
-		fs.writeFileSync(userJestPath, jestExtendsCode);
-		console.log('✅ jest.config.js created.');
-	} else {
-		console.log(
-			'❌ jest.config.js already exists in your project folder. Skip jest extension creation.',
-		);
-	}
-	// TODO: make jest get the one from root if present
+	generateConfigFile({
+		configFileNames: ['jest.config.js'],
+		defaultConfigFilePath: path.join(rootPath, 'jest.config.js'),
+		generateContent() {
+			const presetConfigPath = preset.getJestConfigurationPath(presetApi);
+			const presetConfigRelativePath = path.relative(nodeModulesPath, presetConfigPath);
+			return jestExtendsTemplate({ presetConfigRelativePath });
+		},
+	});
 
-	const userEslintPath = path.join(rootPath, '.eslintrc');
-	if (!fs.existsSync(userEslintPath)) {
-		const eslintConfigPath = preset.getEslintConfigurationPath(presetApi);
-		const eslintConfigRelativePath = path.relative(rootPath, eslintConfigPath);
-		const eslintExtendsCode = eslintExtendsTemplate({ eslintConfigRelativePath });
-		fs.writeFileSync(userEslintPath, eslintExtendsCode);
-		console.log('✅ .eslintrc created.');
-	} else {
-		console.log(
-			'❌ .eslintrc already exists in your project folder. Skip eslint extension creation.',
-		);
-	}
-	// TODO: make eslint get the one from root if present
+	generateConfigFile({
+		configFileNames: [
+			'.eslintrc.js',
+			'.eslintrc.yaml',
+			'.eslintrc.yml',
+			'.eslintrc.json',
+			'.eslintrc',
+		],
+		defaultConfigFilePath: path.join(rootPath, '.eslintrc'),
+		generateContent() {
+			const presetConfigPath = preset.getEslintConfigurationPath(presetApi);
+			const presetConfigRelativePath = path.relative(rootPath, presetConfigPath);
+			return eslintExtendsTemplate({ presetConfigRelativePath });
+		},
+	});
 
-	const userBabelrcPath = path.join(process.cwd(), '.babelrc');
-	const userBabelrcJsonPath = path.join(process.cwd(), '.babelrc.json');
-	const userBabelJsPath = path.join(process.cwd(), 'babel.config.js');
-	if (
-		!fs.existsSync(userBabelrcPath) &&
-		!fs.existsSync(userBabelrcJsonPath) &&
-		!fs.existsSync(userBabelJsPath)
-	) {
-		const babelConfigPath = preset.getBabelConfigurationPath(presetApi);
-		const babelConfigRelativePath = path.relative(rootPath, babelConfigPath);
-		const babelExtendsCode = babelExtendsTemplate({ babelConfigRelativePath });
-		fs.writeFileSync(userBabelrcJsonPath, babelExtendsCode);
-		console.log('✅ .babelrc.json created.');
-	} else {
-		console.log(
-			'❌ (.babelrc | .babelrc.json | babel.config.js) already exists in your project folder. Skip babel extension creation.',
-		);
-	}
-	// TODO: move (in fact copy) babelrc resolver to build-lib.js
+	generateConfigFile({
+		configFileNames: ['.babelrc', '.babelrc.json', 'babel.config.js'],
+		defaultConfigFilePath: path.join(rootPath, '.babelrc.json'),
+		generateContent() {
+			const presetConfigPath = preset.getBabelConfigurationPath(presetApi);
+			const presetConfigRelativePath = path.relative(nodeModulesPath, presetConfigPath);
+			return babelExtendsTemplate({ presetConfigRelativePath });
+		},
+	});
 
 	return { status: 0 };
 };
