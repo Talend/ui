da75f0b3fc72adadd37cb4bbd4ee84989957679d
Jimmy Somsanith <jsomsanith@talend.com>
2019-04-10 17:06:15 +0200

    Revert a conf

--- packages/scripts/webapp/preset/config/karma/karma.conf.js
+++ packages/scripts/webapp/preset/config/karma/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function(config) {
 		},
 
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress'],
+		reporters: ['jasmine-diff', 'progress'],
 
 		// web server port
 		port: 9876,
