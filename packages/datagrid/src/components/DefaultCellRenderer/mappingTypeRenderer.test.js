752cd9a78edd56f35049923ad7637db2ffdd5f9f
Jean-Alain Ré <jare@talend.com>
2019-04-18 14:31:02 +0200

    update test

--- packages/datagrid/src/components/DefaultCellRenderer/mappingTypeRenderer.test.js
+++ packages/datagrid/src/components/DefaultCellRenderer/mappingTypeRenderer.test.js
@@ -9,6 +9,10 @@ describe('#avrorender', () => {
 		expect(getTypeRenderer({ type: 'long', logicalType: 'timestamp-millis' })).toBe('date');
 	});
 
+	it('should return the mapping corresponding to the logical type', () => {
+		expect(getTypeRenderer({ type: 'int', logicalType: 'timestamp-millis' })).toBe('int');
+	});
+
 	it('should return the type if no mapping', () => {
 		expect(getTypeRenderer({ type: 'string', logicalType: '' })).toBe('string');
 	});
