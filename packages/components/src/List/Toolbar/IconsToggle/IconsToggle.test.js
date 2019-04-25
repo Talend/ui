abe085f792261159db74b69af811afa28615aa10
Yan Wang <ywang@talend.com>
2019-04-25 15:07:23 +0800

    fix tests for IconsToggle

--- packages/components/src/List/Toolbar/IconsToggle/IconsToggle.test.js
+++ packages/components/src/List/Toolbar/IconsToggle/IconsToggle.test.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import DisplayModeToggle from './DisplayModeToggle.component';
+import IconsToggle from './IconsToggle.component';
 
 const props = {
 	onChange: jest.fn(),
+	options: [{ name: 'table', icon: 'talend-table', label: 'table', araiLabel: 'table view' },
+		{ name: 'large', icon: 'talend-expanded', label: 'large', araiLabel: 'large view' },
+	],
+	selected: 'table',
 };
-describe('DisplayModeToggle', () => {
-	it('should render', () => {
+describe('IconsToggle', () => {
+	it('should render a display mode toggle', () => {
 		// when
-		const wrapper = mount(<DisplayModeToggle {...props} />);
+		const wrapper = mount(<IconsToggle {...props} />);
 
 		// then
 		expect(toJson(wrapper)).toMatchSnapshot();
 	});
 	it('should render table mode selected', () => {
 		// when
-		const wrapper = mount(<DisplayModeToggle {...props} mode="table" />);
+		const wrapper = mount(<IconsToggle {...props} />);
 
 		// then
 		expect(wrapper.find('ActionIconToggle').at(0).prop('active')).toEqual(true);
 	});
 	it('should call onChange when change display mode', () => {
 		// when
-		const wrapper = mount(<DisplayModeToggle {...props} mode="table" />);
+		const wrapper = mount(<IconsToggle {...props} />);
 		wrapper.find('ActionIconToggle').at(1).simulate('click');
 
 		// then
