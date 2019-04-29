0d28dc2e3af4d15440c6dc81375df18c667ea7f4
jlame <jlame@talend.com>
2019-04-29 15:07:29 +0200

    snapshot

--- packages/components/src/GridLayout/Tile/Tile.component.test.js
+++ packages/components/src/GridLayout/Tile/Tile.component.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import Tile from './index';
 
 describe('Grid tiles', () => {
@@ -9,7 +10,7 @@ describe('Grid tiles', () => {
 		const wrapper = mount(<Tile.Container>my container</Tile.Container>);
 
 		// then
-		expect(wrapper.find('Tile').getElement()).toMatchSnapshot();
+		expect(toJson(wrapper)).toMatchSnapshot();
 		// expect(wrapper.getElement()).toHaveLength(1);
 	});
 });
