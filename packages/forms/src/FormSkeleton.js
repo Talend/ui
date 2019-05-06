389af5ac8d6e720443b3df5d312187c614b7cf2b
romainseb <sebastien.romain@gmail.com>
2019-05-06 08:43:03 +0200

    update skeleton in forms

--- packages/forms/src/FormSkeleton.js
+++ packages/forms/src/FormSkeleton.js
@@ -6,14 +6,14 @@ export default function FormSkeleton() {
 	return (
 		<div className={`${theme.container} tc-skeleton-heartbeat`} aria-busy>
 			<div className={theme['form-content']}>
-				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} width={600} />
-				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} width={200} />
-				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} width={600} />
-				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} width={200} />
-				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} width={600} />
-				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} width={200} />
-				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} width={600} />
-				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} width={200} />
+				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} size={Skeleton.SIZES.large} />
+				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} className="skeleton-fit-content" />
+				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} size={Skeleton.SIZES.large} />
+				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} className="skeleton-fit-content" />
+				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} size={Skeleton.SIZES.large} />
+				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} className="skeleton-fit-content" />
+				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} size={Skeleton.SIZES.large} />
+				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} className="skeleton-fit-content" />
 			</div>
 			<div className={theme.submit}>
 				<div className={theme['submit-wrapper']}>
