254d57333466a1d68f8680768a3659f5b2c0d212
Jimmy Somsanith <jsomsanith@talend.com>
2019-05-07 17:36:09 +0200

    Position

--- packages/components/src/ActionIntercom/Intercom.component.js
+++ packages/components/src/ActionIntercom/Intercom.component.js
@@ -1,8 +1,7 @@
-import React, { useState, useLayoutEffect, useRef } from 'react';
+import React, { useState, useLayoutEffect, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Icon from '../Icon';
-// import { ActionButton } from '../Actions';
 import IntercomService from './service';
 import getDefaultT from '../translate';
 
@@ -11,51 +10,18 @@ import theme from './Intercom.scss';
 export default function Intercom({ id, className, config, t }) {
 	const [show, setShow] = useState(false);
 	const ref = useRef(null);
-	useLayoutEffect(
+	useEffect(
 		() => {
-			let intercomContainer;
-			let customStyle = document.createElement('style');
-			const callback = () => {
-				let time = 0;
-				const interval = setInterval(() => {
-					time += 1;
-					intercomContainer = document.querySelector('#intercom-container');
-					if (intercomContainer) {
-						const { bottom, left, right } = ref.current.getBoundingClientRect();
-						const x = (left + right) / 2;
-						const y = bottom;
-						customStyle = document.createElement('style');
-						customStyle.appendChild(
-							document.createTextNode(`
-							.intercom-namespace .intercom-app div.intercom-messenger-frame {
-								top: ${y}px;
-								left: calc(${x}px - 376px / 2);
-								margin-top: 2rem;
-							}
-						`),
-						);
-						intercomContainer.appendChild(customStyle);
-					}
-
-					if (time === 10 || intercomContainer) {
-						clearInterval(interval);
-					}
-				}, 1000);
-			};
-
-			IntercomService.boot(`#${id}`, { vertical_padding: 70, ...config }, callback);
+			IntercomService.boot(`#${id}`, { vertical_padding: 70, ...config });
 			IntercomService.onShow(setShow.bind(null, true));
 			IntercomService.onHide(setShow.bind(null, false));
 
-			return () => {
-				IntercomService.shutdown();
-				if (customStyle) {
-					intercomContainer.removeChild(customStyle);
-				}
-			};
+			return IntercomService.shutdown;
 		},
 		[config, setShow],
 	);
+
+	useLayoutEffect(() => IntercomService.setPosition(ref.current), [ref.current]);
 
 	const label = show
 		? t('TC_INTERCOM_CLOSE', { defaultValue: 'Close support messenger.' })
@@ -64,24 +30,12 @@ export default function Intercom({ id, className, config, t }) {
 	const icon = show ? 'talend-cross' : 'talend-bubbles';
 
 	return (
-		/*
-		<ActionButton
-			buttonRef={ref}
-			id={id}
-			className={className}
-			label={label}
-			icon={icon}
-			tooltipPlacement="bottom"
-			hideLabel
-			link
-		/>
-		*/
 		<button
 			ref={ref}
 			id={id}
 			title={label}
 			className={classnames('btn', 'btn-link', 'tc-intercom', theme['tc-intercom'], className, {
-				[theme['open']]: show,
+				[theme.open]: show,
 			})}
 		>
 			<Icon name={icon} />
--- packages/components/src/ActionIntercom/service.js
+++ packages/components/src/ActionIntercom/service.js
@@ -29,22 +29,13 @@ function init(config) {
 	}
 }
 
-function boot(widgetId, config = {}, callback) {
+function boot(widgetId, config = {}) {
 	init(config);
 	intercom('boot', {
 		...config,
 		widget: { activator: widgetId },
 		hide_default_launcher: true,
 	});
-
-	if (callback) {
-		const interval = setInterval(() => {
-			if (intercom && intercom.booted) {
-				clearInterval(interval);
-				callback();
-			}
-		}, 100);
-	}
 }
 
 function update(config) {
@@ -67,6 +58,57 @@ function onUnreadCountChange() {
 	intercom('onUnreadCountChange');
 }
 
+const INTERCOM_MAIN_FRAME_SELECTOR =
+	'.intercom-namespace .intercom-app div.intercom-messenger-frame';
+const INTERCOM_MAIN_FRAME_WIDTH = 376;
+function setPosition(domElement) {
+	let intercomContainer;
+	let customStyle;
+	let time = 0;
+
+	const readyInterval = setInterval(() => {
+		time += 1;
+		if (time === 10) {
+			clearInterval(readyInterval);
+			return;
+		}
+
+		intercomContainer = document.querySelector('#intercom-container');
+		if (!intercomContainer) {
+			return;
+		}
+
+		const { bottom, left, right } = domElement.getBoundingClientRect();
+		const intercomRight = Math.max(
+			window.innerWidth /* window */ -
+			(right - (right - left) / 2) /* right position of trigger element center */ -
+				INTERCOM_MAIN_FRAME_WIDTH / 2 /* center intercom window  */,
+			0,
+		);
+		const intercomTop = bottom;
+
+		customStyle = document.createElement('style');
+		customStyle.appendChild(
+			document.createTextNode(`
+			${INTERCOM_MAIN_FRAME_SELECTOR} {
+				top: ${intercomTop}px;
+				right: ${intercomRight}px;
+				margin-top: 2rem;
+			}
+		`),
+		);
+		intercomContainer.appendChild(customStyle);
+		clearInterval(readyInterval);
+	}, 500);
+
+	return function clearIntercomPosition() {
+		clearInterval(readyInterval);
+		if (intercomContainer && customStyle) {
+			intercomContainer.removeChild(customStyle);
+		}
+	};
+}
+
 export default {
 	init,
 	boot,
@@ -75,4 +117,5 @@ export default {
 	onHide,
 	onShow,
 	onUnreadCountChange,
+	setPosition,
 };
