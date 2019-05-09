32dc0293fe24ba683c3c09616da766702ff7dd6d
Steven Gangnant <sgangnant@talend.com>
2019-05-09 15:11:24 +0200

    feat(components/ListComposition): Text filter feature

---
+++ packages/components/src/List/ListComposition/TextFilter/TextFilter.component.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import isUndefined from 'lodash/isUndefined';
+import { useListContext } from '../context';
+import FilterBar from '../../../FilterBar';
+
+function TextFilter(props) {
+	const { docked: dockedProp, onChange, onToggle, initialDocked, ...restProps } = props;
+	const { textFilter, setTextFilter } = useListContext();
+	const [docked, setDocked] = useState(initialDocked);
+
+	const filterBarProps = {
+		value: textFilter,
+		// onChange is mapped to onFilter for consistency with sortBy, displayMode ...
+		onFilter: (event, value) => {
+			if (onChange) {
+				onChange(event, value);
+			} else {
+				setTextFilter(value);
+			}
+		},
+		docked: isUndefined(dockedProp) ? docked : dockedProp,
+		onToggle: (event, value) => {
+			if (onToggle) {
+				onToggle(event, value);
+			} else {
+				setDocked(!docked);
+			}
+		},
+	};
+
+	return <FilterBar {...filterBarProps} {...restProps} />;
+}
+
+TextFilter.defaultProps = {
+	initialDocked: true,
+};
+
+if (process.env.NODE_ENV !== 'production') {
+	TextFilter.propTypes = {
+		docked: PropTypes.bool,
+		initialDocked: PropTypes.initialDocked,
+		onChange: PropTypes.func,
+		onToggle: PropTypes.func,
+	};
+}
+
+export default TextFilter;
