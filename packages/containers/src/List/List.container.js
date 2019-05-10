dcccec9acd398359316a8cf9ff7b7e443b63adf5
travis <travis>
2019-05-10 06:49:25 +0000

    chore(ci): prettier

--- packages/containers/src/List/List.container.js
+++ packages/containers/src/List/List.container.js
@@ -73,7 +73,7 @@ export function getItems(context, props) {
 			persistentActions: getActionsProps(
 				context,
 				get(props, 'actions.persistentItemsActions', []),
-				item
+				item,
 			),
 		}),
 	);
