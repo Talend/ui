cc8abb5ad86352dc8276a834e06b96c5018c7e9f
Steven Gangnant <sgangnant@talend.com>
2019-05-15 09:50:55 +0200

    Linter

--- packages/components/src/VirtualizedList/ListTable/ListTable.component.js
+++ packages/components/src/VirtualizedList/ListTable/ListTable.component.js
@@ -20,12 +20,14 @@ function ListTableRowRenderer(props) {
 
 	const columnsProp = isEmpty(rowData)
 		? [
-				columns.map(column => (
+			columns.map(
+				column => (
 					<div key={column.key} {...column.props}>
 						<Skeleton type="text" size="xlarge" />
 					</div>
-				)),
-		  ]
+				)
+			),
+		]
 		: columns;
 
 	return <DefaultTableRowRenderer {...props} columns={columnsProp} />;
