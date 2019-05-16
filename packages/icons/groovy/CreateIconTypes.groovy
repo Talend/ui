2508515970616fa7251901df8ddb270ac3d0fba1
Anas Khabali <akhabali@talend.com>
2019-05-16 14:27:14 +0200

    add license header and remove lombok dependency

--- packages/icons/groovy/CreateIconTypes.groovy
+++ packages/icons/groovy/CreateIconTypes.groovy
@@ -23,20 +23,38 @@ allIcons.sort()
 
 def iconJava = new File(project.basedir, 'src/main/java/org/talend/icons/Type.java')
 validateIcons(iconJava, allIcons)
-iconJava.text = """package org.talend.icons;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+iconJava.text = """/*
+  Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+  <p>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  <p>
+  http://www.apache.org/licenses/LICENSE-2.0
+  <p>
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+package org.talend.icons;
 
 /**
  * See http://talend.surge.sh/icons/
  */
-@RequiredArgsConstructor
 enum Type {
 ${generateEnumValues(allIcons, iconsDeprecated)}
     
-    @Getter
     private final String key;
+ 
+    Type(String key) {
+        this.key = key;
+    }
+    
+    public String getKey() {
+        return key;
+    }
 }
 """
 
--- packages/icons/src/main/java/org/talend/icons/UIIcon.java
+++ packages/icons/src/main/java/org/talend/icons/UIIcon.java
@@ -1,17 +1,17 @@
-/**
- * Copyright (C) 2006-2019 Talend Inc. - www.talend.com
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+  Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+  <p>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  <p>
+  http://www.apache.org/licenses/LICENSE-2.0
+  <p>
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  */
 package org.talend.icons;
 
@@ -24,16 +24,13 @@ import static java.lang.annotation.ElementType.PACKAGE;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-/**
- * Associate an icon to a component, only supported on `@PartitionMapper`, `@Processor` and `@Emitter` classes
- */
 @Icon
 @Target({ TYPE, PACKAGE })
 @Retention(RUNTIME)
 public @interface UIIcon {
 
     /**
-     * @return the icon to associate to the component.
+     * @return the icon to be used.
      */
     Type value();
 }
