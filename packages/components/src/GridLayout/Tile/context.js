34fd896e32d7a71472b4bcbe73ae9a20b399eee9
jlame <jlame@talend.com>
2019-06-04 14:14:40 +0200

    lint + review comment

--- packages/components/src/GridLayout/Tile/context.js
+++ packages/components/src/GridLayout/Tile/context.js
@@ -1,14 +1,13 @@
 import React, { useContext } from 'react';
+import invariant from 'invariant';
+
 
 // eslint-disable-next-line import/prefer-default-export
 export const TileContext = React.createContext();
 
 export function useTileContext() {
 	const context = useContext(TileContext);
-	if (!context) {
-		throw Error(
-			'@talend/react-components > Tile: you are using a sub component out of Tile.Container.',
-		);
-	}
+	invariant(context,
+		'@talend/react-components > Tile: you are using a sub component out of Tile.Container.');
 	return context;
 }
