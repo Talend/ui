55019cbf486fc4b77c7913a7f6cd010fa8817ee8
smouillour <smouillour@talend.com>
2019-06-12 09:42:31 +0200

    fix review

--- packages/components/src/Badge/Badge.component.js
+++ packages/components/src/Badge/Badge.component.js
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import classNames from 'classnames';
 import { translate } from 'react-i18next';
 
 import I18N_DOMAIN_COMPONENTS from '../constants';
 import Action from '../Actions/Action';
 import getDefaultT from '../translate';
-import theme from './Badge.scss';
 import Icon from '../Icon';
 import TooltipTrigger from '../TooltipTrigger';
+import badgeCssModule from './Badge.scss';
+import { getTheme } from '../theme';
+
+const theme = getTheme(badgeCssModule);
+
+const SIZES = {
+	large: 'large',
+	small: 'small',
+};
 
 function Badge({
 	id,
@@ -21,47 +28,34 @@ function Badge({
 	t,
 	style,
 	className,
-	display = 'large',
+	display = SIZES.large,
 	aslink,
 	edit,
 	icon,
 	white,
 }) {
-	const displayClass = display === 'small' ? 'tc-badge-display-small' : 'tc-badge-display-large';
+	const displayClass =
+		display === SIZES.small ? 'tc-badge-display-small' : 'tc-badge-display-large';
 
-	const containerClasses = classNames(
-		'tc-badge',
-		theme['tc-badge'],
-		selected && ['tc-badge-selected', theme['tc-badge-selected']],
-		disabled && ['tc-badge-disabled', theme['tc-badge-disabled']],
-		!onDelete && ['tc-badge-readonly', theme['tc-badge-readonly']],
-		[displayClass, theme[displayClass]],
-		aslink && ['tc-badge-aslink', theme['tc-badge-aslink']],
-		edit && ['tc-badge-edit', theme['tc-badge-edit']],
-		className,
-	);
-	const badgeClasses = classNames('tc-badge-button', theme['tc-badge-button']);
-	const badgeWhiteClasses = classNames(
-		'tc-badge-button',
-		'tc-badge-white',
-		theme['tc-badge-button'],
-		theme['tc-badge-white'],
-	);
-	const categoryClasses = classNames('tc-badge-category', theme['tc-badge-category']);
-	const separatorClasses = classNames('tc-badge-separator', theme['tc-badge-separator']);
-	const separatorIconClasses = classNames(
-		'tc-badge-separator',
-		'tc-badge-separator-icon',
-		theme['tc-badge-separator'],
-		theme['tc-badge-separator-icon'],
-	);
-	const labelClasses = classNames('tc-badge-label', theme['tc-badge-label']);
-	const labelTextClasses = classNames('tc-badge-label-text', theme['tc-badge-label-text']);
-	const labelTextWithCategClasses = classNames(
-		'tc-badge-label-text-with-categ',
-		theme['tc-badge-label-text-with-categ'],
-	);
-	const labelIconClasses = classNames('tc-badge-label-icon', theme['tc-badge-label-icon']);
+	const containerClasses = theme('tc-badge', displayClass, className, {
+		'tc-badge-selected': selected,
+		'tc-badge-disabled': disabled,
+		'tc-badge-readonly': !onDelete,
+		'tc-badge-aslink': aslink,
+		'tc-badge-edit': edit,
+	});
+	const badgeClasses = theme('tc-badge-button', {
+		'tc-badge-white': white,
+	});
+	const categoryClasses = theme('tc-badge-category');
+	const separatorClasses = theme('tc-badge-separator');
+	const separatorIconClasses = theme('tc-badge-separator', 'tc-badge-separator-icon');
+	const labelClasses = theme('tc-badge-label');
+	const labelTextClasses = theme({
+		'tc-badge-label-text': !(!aslink && category),
+		'tc-badge-label-text-with-categ': !aslink && category,
+	});
+	const labelIconClasses = theme('tc-badge-label-icon');
 
 	const children = [
 		category && (
@@ -74,10 +68,7 @@ function Badge({
 		category && <span className={separatorClasses} />,
 		<div className={labelClasses}>
 			<TooltipTrigger label={label} tooltipPlacement="top">
-				<span
-					key="label"
-					className={!aslink && category ? labelTextWithCategClasses : labelTextClasses}
-				>
+				<span key="label" className={labelTextClasses}>
 					{label}
 				</span>
 			</TooltipTrigger>
@@ -93,7 +84,7 @@ function Badge({
 				onClick={onDelete}
 				disabled={disabled}
 				icon={'talend-cross'}
-				className={classNames('tc-badge-delete-icon', theme['tc-badge-delete-icon'])}
+				className={theme('tc-badge-delete-icon')}
 				link
 				role="button"
 			/>
@@ -101,7 +92,7 @@ function Badge({
 	];
 	const badgeProps = {
 		id: id && `tc-badge-select-${id}`,
-		className: white ? badgeWhiteClasses : badgeClasses,
+		className: badgeClasses,
 		children,
 	};
 
@@ -127,7 +118,7 @@ Badge.propTypes = {
 	t: PropTypes.func.isRequired,
 	style: PropTypes.object, // eslint-disable-line react/forbid-prop-types
 	className: PropTypes.string,
-	display: PropTypes.string,
+	display: PropTypes.oneOf([SIZES.small, SIZES.large]),
 	aslink: PropTypes.bool,
 	edit: PropTypes.bool,
 	white: PropTypes.bool,
