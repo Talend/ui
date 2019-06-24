d8bfafc5016c9a873eae71f05302d2dff2a87493
jmfrancois <jmfrancois@talend.com>
2019-06-24 14:27:52 +0200

    fix: add deprecated support
    You just have to add process.env.FORM_MOZ in webpack

--- packages/forms/src/FormSwitcher.js
+++ packages/forms/src/FormSwitcher.js
@@ -3,13 +3,32 @@ import PropTypes from 'prop-types';
 import { UIForm } from './UIForm';
 import FormSkeleton from './FormSkeleton';
 
+let Form = () => (
+	<div className="alert alert-danger">
+		Error: You should compile using FORM_MOZ to use deprecated Form
+	</div>
+);
+
+if (process.env.FORM_MOZ) {
+	// eslint-disable-next-line global-require
+	Form = require('./deprecated/Form');
+	// eslint-disable-next-line no-console
+	console.warn('FORM_MOZ is deprecated and will be removed in UI 4.x');
+}
+
 export default function FormSwitcher(props) {
 	if (props.loading) {
 		return <FormSkeleton />;
+	}
+	if (props.data && !Array.isArray(props.data.uiSchema)) {
+		return <Form {...props} />;
 	}
 	return <UIForm {...props} />;
 }
 
 FormSwitcher.propTypes = {
 	loading: PropTypes.bool,
+	data: PropTypes.shape({
+		uiSchema: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+	}),
 };
