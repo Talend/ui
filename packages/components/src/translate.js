58918f30f8da6c5ba2ceda99b70b5439fcbabca4
Ré Jean-Alain <jare@talend.com>
2019-07-04 15:34:51 +0200

    remove comments

--- packages/components/src/translate.js
+++ packages/components/src/translate.js
@@ -1,11 +1,6 @@
 import i18next, { createInstance } from 'i18next';
 import { setI18n, getI18n } from 'react-i18next';
 
-/* if i18next didn't provide by the parent app,
- i18next.t(I18N_TEST_KEy, { defaultValue: 'VALUE' }) return undefined.
-
- So, for this module works without a provided i18next, we create a default
-*/
 if (!getI18n()) {
 	console.warn('@talend/react-components used without i18n host.');
 	setI18n(createInstance({}, () => {}));
