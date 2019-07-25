ba2c407769cad46d986c2077e30cbe7f19f8dbcc
Jimmy Somsanith <jimmy.somsanith@gmail.com>
2019-07-25 17:18:58 +0200

    Adapt travis

--- .travis/after_success_deploy.sh
+++ .travis/after_success_deploy.sh
@@ -6,7 +6,6 @@ if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_BRANCH" == 'master' ]; th
 	surge --project .static --domain "talend.surge.sh"
 	echo "✓ Deploy to talend.surge.sh"
 elif [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
-    yarn chroma
     surge --project .static --domain "$TRAVIS_PULL_REQUEST.talend.surge.sh"
     echo "✓ Deploy PR#$TRAVIS_PULL_REQUEST to $TRAVIS_PULL_REQUEST.talend.surge.sh"
     curl -f -XPOST -H "Authorization: token $GH_TOKEN" https://api.github.com/repos/Talend/ui/issues/$TRAVIS_PULL_REQUEST/comments -d "{\"body\": \":octocat: [Demo is available here](http://$TRAVIS_PULL_REQUEST.talend.surge.sh) \"}"
