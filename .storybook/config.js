165908f93347362ddab5a13263e2058713d80089
Jimmy Somsanith <jimmy.somsanith@gmail.com>
2019-07-25 17:46:25 +0200

    SB Docs

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,10 +3,15 @@ import 'focus-outline-manager';
 import 'storybook-chroma';
 import { load, addDecorator, addParameters } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
+import { DocsPage } from '@storybook/addon-docs/blocks';
 
 import './i18n';
 
 addDecorator(withA11y);
+
+addParameters({
+	docs: DocsPage,
+});
 
 // automatically import all files ending in *.stories.js
 load(require.context('../packages/components/stories', true, /\.stories\.js$/), module);
