9a6496f1cf114eaeebddaa8e1d68a6de00d709ac
nicomaligne <nmaligne@talend.com>
2019-07-29 14:16:23 +0200

    enhance context

--- packages/components/src/List/Toolbar/ColumnChooserButton/ColumnChooser/columnChooser.context.js
+++ packages/components/src/List/Toolbar/ColumnChooserButton/ColumnChooser/columnChooser.context.js
@@ -1,6 +1,17 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 
 const columnChooserContext = createContext();
 
-// eslint-disable-next-line import/prefer-default-export
-export { columnChooserContext };
+export const useColumnChooserContext = () => {
+	const context = useContext(columnChooserContext);
+	if (!context) {
+		throw new Error(
+			'[columnChooserContext]: you are using some column chooser components outside the column chooser context',
+		);
+	}
+	return context;
+};
+
+const ColumnChooserProvider = columnChooserContext.Provider;
+
+export { columnChooserContext, ColumnChooserProvider };
