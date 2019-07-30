8c3b22c9c9355a053c873241116e718fd1099a5e
jlame <jlame@talend.com>
2019-07-30 15:08:27 +0200

    removing unused import

--- packages/components/src/FilterBar/FilterBar.component.js
+++ packages/components/src/FilterBar/FilterBar.component.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 import DebounceInput from 'react-debounce-input';
 import FormControl from 'react-bootstrap/lib/FormControl';
