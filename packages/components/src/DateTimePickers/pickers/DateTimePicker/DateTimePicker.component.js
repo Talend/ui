e543196160c35ac974346b52bdb12c871f34277c
jillyan <zyan@talend.com>
2019-08-09 14:34:09 +0800

    move onClickToday before setView

--- packages/components/src/DateTimePickers/pickers/DateTimePicker/DateTimePicker.component.js
+++ packages/components/src/DateTimePickers/pickers/DateTimePicker/DateTimePicker.component.js
@@ -121,6 +121,16 @@ class DateTimePicker extends React.Component {
 		this.setState({ allowFocus: value });
 	}
 
+    onClickToday(event) {
+        const now = new Date();
+        if (!this.state.isDateTimeView) {
+            this.onSelectCalendarYear(event, getYear(now));
+            this.onSelectCalendarMonth(event, getMonth(now));
+            this.setView(true);
+        }
+        this.onSelectDate(event, startOfDay(now));
+    }
+
 	setView(isDateTimeView) {
 		this.setState({ isDateTimeView }, () => {
 			focusOnCalendar(this.pickerRef);
@@ -133,16 +143,6 @@ class DateTimePicker extends React.Component {
 			time: this.state.selectedTime,
 			field,
 		});
-	}
-
-	onClickToday(event) {
-		const now = new Date();
-		if (!this.state.isDateTimeView) {
-			this.onSelectCalendarYear(event, getYear(now));
-			this.onSelectCalendarMonth(event, getMonth(now));
-			this.setView(true);
-		}
-		this.onSelectDate(event, startOfDay(now));
 	}
 
 	render() {
