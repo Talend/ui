fe41aea0de19bbc0d512f7e18b80f83d42913c79
Steven Gangnant <sgangnant@talend.com>
2019-08-22 09:39:41 +0200

    Fixed date/date-fns in tests

--- packages/components/src/RelativeDate/RelativeDate.snap.test.js
+++ packages/components/src/RelativeDate/RelativeDate.snap.test.js
@@ -3,17 +3,26 @@ import { shallow } from 'enzyme';
 
 import RelativeDate from './RelativeDate.component';
 
+jest.mock('date-fns', () => ({
+	distanceInWordsToNow: () => 'a year ago',
+	format: date => `[${date} formatted with date-fns]`,
+}));
+
 describe('RelativeDate', () => {
 	const props = {
 		date: '2019-08-21 12:00:00',
 	};
+
+	afterAll(() => {
+		jest.unmock('date-fns');
+	});
 
 	it('should render RelativeDate component the simple way', () => {
 		// when
 		const wrapper = shallow(<RelativeDate {...props} />);
 
 		// then
-		expect(wrapper.dive().getElement()).toMatchSnapshot();
+		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 
 	it('should render RelativeDate component with an icon', () => {
@@ -21,6 +30,6 @@ describe('RelativeDate', () => {
 		const wrapper = shallow(<RelativeDate {...props} withIcon />);
 
 		// then
-		expect(wrapper.dive().getElement()).toMatchSnapshot();
+		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 });
