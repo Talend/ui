b7e6af59b45307d52bf8d850b29a4403e8e9e811
Yan Wang <ywang@talend.com>
2019-08-26 10:23:52 +0800

    extends DateTIme.Picker

--- packages/components/src/DateTimePickers/DateTime/Picker/Picker.component.js
+++ packages/components/src/DateTimePickers/DateTime/Picker/Picker.component.js
@@ -1,20 +1,35 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 
 import { DateTimeContext } from '../Context';
 import DateTimePicker from '../../pickers/DateTimePicker';
+import TimePicker from '../../pickers/TimePicker';
 
 export default function Picker(props) {
-	const { datetime, pickerManagement } = useContext(DateTimeContext);
+	const {
+		datetime,
+		pickerManagement,
+		datePickerManagement,
+		timePickerManagement,
+	} = useContext(DateTimeContext);
+	const PickerComponent = props.part === 'date' ? DateTimePicker : TimePicker;
+	const partPickerManagement = props.part === 'date' ? datePickerManagement : timePickerManagement;
 	return (
-		<DateTimePicker
+		<PickerComponent
 			manageFocus
 			selection={{
 				date: datetime.date,
 				time: datetime.time,
 			}}
 			{...pickerManagement}
+			{...partPickerManagement}
 			{...props}
 		/>
 	);
 }
+
+Picker.propTypes = {
+	part: PropTypes.oneOf(['date', 'time']),
+};
+
 Picker.displayName = 'DateTime.Picker';
