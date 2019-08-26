b5eeeb9190413a7b27a613aad527a62bc7d7d647
Jimmy Somsanith <jimmy.somsanith@gmail.com>
2019-08-26 17:38:25 +0200

    feat(ListComposition): collection selection hook

---
+++ packages/components/src/List/ListComposition/Manager/hooks/useCollectionSelection.hook.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export default function useCollectionSelection(
+	collection = [],
+	initialSelectedIds = [],
+	idKey = 'id',
+) {
+	const [selectedIds, setSelectedIds] = useState(initialSelectedIds);
+
+	function isSelected(item) {
+		const itemId = item[idKey];
+		return selectedIds.some(itemKey => itemKey === itemId);
+	}
+
+	function onToggleItem(item) {
+		const itemId = item[idKey];
+		const dataIndex = selectedIds.indexOf(itemId);
+		const newSelectedIds = selectedIds.slice(0);
+		if (dataIndex > -1) {
+			newSelectedIds.splice(dataIndex, 1);
+		} else {
+			newSelectedIds.push(itemId);
+		}
+		setSelectedIds(newSelectedIds);
+	}
+
+	function onToggleAll() {
+		if (collection.length === selectedIds.length) {
+			setSelectedIds([]);
+		} else {
+			setSelectedIds(collection.map(item => item[idKey]));
+		}
+	}
+
+	return {
+		isSelected,
+		allIsSelected: selectedIds.length > 0 && selectedIds.length === collection.length,
+		selectedIds,
+		onToggleAll,
+		onToggleItem,
+	};
+}
