b5f0408acf718ba93a92b3fbd7216850a7cd5944
Jimmy Somsanith <jimmy.somsanith@gmail.com>
2019-08-27 11:48:55 +0200

    feat(ListComposition): hook to insert actions into collection

--- packages/components/src/List/ListComposition/Manager/ListManager.component.js
+++ packages/components/src/List/ListComposition/Manager/ListManager.component.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
 import useLocalStorage from 'react-use/lib/useLocalStorage';
