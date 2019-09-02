27028163d9ffd617d4a1b65ad3552fea2762ae33
Yan Wang <ywang@talend.com>
2019-09-02 09:53:41 +0800

    merge Manager into InputTimePicker

--- packages/components/src/DateTimePickers/InputTimePicker/InputTimePicker.component.js
+++ packages/components/src/DateTimePickers/InputTimePicker/InputTimePicker.component.js
@@ -10,17 +10,9 @@ import Time from '../Time';
 import theme from './InputTimePicker.scss';
 import useInputPickerHandlers from '../hooks/useInputPickerHandlers';
 
+const PROPS_TO_OMIT_FOR_INPUT = ['id', 'required', 'value', 'useSeconds', 'onBlur', 'onChange'];
 
-const PROPS_TO_OMIT_FOR_INPUT = [
-	'id',
-	'required',
-	'value',
-	'useSeconds',
-	'onBlur',
-	'onChange',
-];
-
-function InputTimePicker(props) {
+export default function InputTimePicker(props) {
 	const popoverId = `time-picker-${props.id || uuid.v4()}`;
 
 	const inputRef = useRef(null);
@@ -32,51 +24,48 @@ function InputTimePicker(props) {
 
 	const inputProps = omit(props, PROPS_TO_OMIT_FOR_INPUT);
 	const timePicker = [
-		<Time.Input
-			{...inputProps}
-			id={`${props.id}-input`}
-			key="input"
-			inputRef={inputRef}
-		/>,
-			handlers.showPicker && (
-				<Popper
-					key="popper"
-					modifiers={{
-						hide: {
-							enabled: false,
-						},
-						preventOverflow: {
-							enabled: false,
-						},
-					}}
-					placement={handlers.getPopperPlacement(inputRef.current)}
-					positionFixed
-					referenceElement={inputRef.current}
-				>
-					{({ ref, style }) => (
-						<div id={popoverId} className={theme.popper} style={style} ref={ref}>
-							<Time.Picker
-								{...props}
-								onChange={(...args) => {
-									handlers.onChange(...args, inputRef.current);
-								}}
-							/>
-						</div>
-					)}
-				</Popper>
+		<Time.Input {...inputProps} id={`${props.id}-input`} key="input" inputRef={inputRef} />,
+		handlers.showPicker && (
+			<Popper
+				key="popper"
+				modifiers={{
+					hide: {
+						enabled: false,
+					},
+					preventOverflow: {
+						enabled: false,
+					},
+				}}
+				placement={handlers.getPopperPlacement(inputRef.current)}
+				positionFixed
+				referenceElement={inputRef.current}
+			>
+				{({ ref, style }) => (
+					<div id={popoverId} className={theme.popper} style={style} ref={ref}>
+						<Time.Picker
+							{...props}
+							onChange={(...args) => {
+								handlers.onChange(...args, inputRef.current);
+							}}
+						/>
+					</div>
+				)}
+			</Popper>
 		),
 	].filter(Boolean);
 	return (
-		<FocusManager
-			divRef={containerRef}
-			onFocusIn={handlers.onFocus}
-			onFocusOut={handlers.onBlur}
-			onKeyDown={event => {
-				handlers.onKeyDown(event, inputRef.current);
-			}}
-		>
-			{timePicker}
-		</FocusManager>
+		<Time.Manager value={props.value} useSeconds={props.useSeconds} onChange={props.onChange}>
+			<FocusManager
+				divRef={containerRef}
+				onFocusIn={handlers.onFocus}
+				onFocusOut={handlers.onBlur}
+				onKeyDown={event => {
+					handlers.onKeyDown(event, inputRef.current);
+				}}
+			>
+				{timePicker}
+			</FocusManager>
+		</Time.Manager>
 	);
 }
 
@@ -87,29 +76,9 @@ InputTimePicker.propTypes = {
 	useSeconds: PropTypes.bool,
 	onChange: PropTypes.func,
 	onBlur: PropTypes.func,
+	value: PropTypes.string,
 };
 
 InputTimePicker.defaultProps = {
-	useSeconds: false,
-};
-
-export default function ContexualInputTimePicker(props) {
-	return (
-		<Time.Manager
-			value={props.value}
-			useSeconds={props.useSeconds}
-			onChange={props.onChange}
-		>
-			<InputTimePicker {...props} />
-		</Time.Manager>
-	);
-}
-
-ContexualInputTimePicker.displayName = 'ContexualInputTimePicker';
-ContexualInputTimePicker.propTypes = {
-	...InputTimePicker.propTypes,
-	value: PropTypes.string,
-};
-ContexualInputTimePicker.defaultProps = {
 	useSeconds: false,
 };
