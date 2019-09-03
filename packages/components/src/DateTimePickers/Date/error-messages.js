c3c87368c3ae50f0dff755750d87966c8d9fa62f
Yan Wang <ywang@talend.com>
2019-09-03 13:46:53 +0800

    udpate error

--- packages/components/src/DateTimePickers/Date/error-messages.js
+++ packages/components/src/DateTimePickers/Date/error-messages.js
@@ -3,22 +3,6 @@ import i18next from 'i18next';
 
 export default function getErrorMessage(key) {
 	switch (key) {
-		case 'INVALID_HOUR_EMPTY':
-			return i18next.t('INVALID_HOUR_EMPTY', { defaultValue: 'Hour is required' });
-		case 'INVALID_HOUR_NUMBER':
-			return i18next.t('INVALID_HOUR_NUMBER', { defaultValue: 'Hour must be between 00 and 23' });
-		case 'INVALID_MINUTES_EMPTY':
-			return i18next.t('INVALID_MINUTES_EMPTY', { defaultValue: 'Minutes are required' });
-		case 'INVALID_MINUTES_NUMBER':
-			return i18next.t('INVALID_MINUTES_NUMBER', {
-				defaultValue: 'Minutes value must be between 00 and 59',
-			});
-		case 'INVALID_SECONDS_EMPTY':
-			return i18next.t('INVALID_SECONDS_EMPTY', { defaultValue: 'Seconds are required' });
-		case 'INVALID_SECONDS_NUMBER':
-			return i18next.t('INVALID_SECONDS_NUMBER', {
-				defaultValue: 'Seconds value must be between 00 and 59',
-			});
 		case 'INVALID_DATE_FORMAT':
 			return i18next.t('INVALID_DATE_FORMAT', { defaultValue: 'Date format is invalid' });
 		case 'INVALID_MONTH_NUMBER':
