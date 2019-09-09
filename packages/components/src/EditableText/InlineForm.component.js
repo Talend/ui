7e3141f1c135334024ec26ab45f5b7be4dd038b9
jlame <jlame@talend.com>
2019-09-09 15:57:24 +0200

    chore(EditableText): submit on blur

--- packages/components/src/EditableText/InlineForm.component.js
+++ packages/components/src/EditableText/InlineForm.component.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import keycode from 'keycode';
 import { Action } from '../Actions';
+import FocusManager from '../FocusManager';
 import theme from './EditableText.scss';
 import getDefaultT from '../translate';
 
@@ -63,8 +64,10 @@ class InlineForm extends React.Component {
 
 	onSubmit(event) {
 		event.preventDefault();
-		if (this.state.value.trim().length !== 0 || !this.props.required) {
-			this.props.onSubmit(event, { value: this.state.value, props: this.props });
+		if (!event.target || event.target.tagName !== 'INPUT') {
+			if (this.state.value.trim().length !== 0 || !this.props.required) {
+				this.props.onSubmit(event, { value: this.state.value, props: this.props });
+			}
 		}
 	}
 
@@ -81,67 +84,70 @@ class InlineForm extends React.Component {
 		const notFilled = required && this.state.value.trim().length === 0;
 		const notValid = notFilled || !!errorMessage;
 		return (
-			<form
-				onSubmit={this.onSubmit}
-				className={classNames(theme['tc-editable-text-form'], 'tc-editable-text-form')}
-			>
-				<div className={classNames('form-group', { 'has-error': notValid })}>
-					<input
-						ref={this.selectInput}
-						id="inputTitle"
-						type="text"
-						placeholder={placeholder}
-						className={classNames(
-							theme['tc-editable-text-form-input'],
-							'tc-editable-text-form-input',
-							'form-control',
-						)}
-						onChange={this.onChange}
-						value={this.state.value}
-						onKeyDown={this.onKeyDown}
-						onBlur={this.onSubmit}
-					/>
-					{errorMessage && <p className="help-block text-danger">{errorMessage}</p>}
-				</div>
-				<div
-					className={classNames(
-						theme['tc-editable-text-form-buttons'],
-						'tc-editable-text-form-buttons',
-					)}
+			<FocusManager onFocusOut={this.onSubmit}>
+				<form
+					onSubmit={this.onSubmit}
+					className={classNames(theme['tc-editable-text-form'], 'tc-editable-text-form')}
 				>
-					<Action
-						name="action-cancel-title"
-						label={t('CANCEL_TOOLTIP', { defaultValue: 'Cancel' })}
-						icon="talend-cross"
-						onClick={this.onCancel}
-						bsStyle="link"
+
+					<div className={classNames('form-group', { 'has-error': notValid })}>
+						<input
+							ref={this.selectInput}
+							id="inputTitle"
+							type="text"
+							placeholder={placeholder}
+							className={classNames(
+								theme['tc-editable-text-form-input'],
+								'tc-editable-text-form-input',
+								'form-control',
+							)}
+							onChange={this.onChange}
+							value={this.state.value}
+							onKeyDown={this.onKeyDown}
+							onBlur={this.onSubmit}
+						/>
+						{errorMessage && <p className="help-block text-danger">{errorMessage}</p>}
+					</div>
+					<div
 						className={classNames(
-							theme['tc-editable-text-form-buttons-icon'],
-							'tc-editable-text-form-buttons-icon',
-							theme['tc-editable-text-form-buttons-cancel'],
-							'tc-editable-text-form-buttons-cancel',
+							theme['tc-editable-text-form-buttons'],
+							'tc-editable-text-form-buttons',
 						)}
-						hideLabel
-						data-feature={feature && `${feature}.cancel`}
-					/>
-					<Action
-						type="submit"
-						name="action-submit-title"
-						label={t('SUBMIT_TOOLTIP', { defaultValue: 'Submit' })}
-						icon="talend-check"
-						bsStyle="link"
-						className={classNames(
-							theme['tc-editable-text-form-buttons-icon'],
-							'tc-editable-text-form-buttons-icon',
-							theme['tc-editable-text-form-buttons-submit'],
-							'tc-editable-text-form-buttons-submit',
-						)}
-						disabled={notFilled}
-						hideLabel
-						data-feature={feature && `${feature}.submit`}
-					/>
-				</div>
-			</form>
+					>
+						<Action
+							name="action-cancel-title"
+							label={t('CANCEL_TOOLTIP', { defaultValue: 'Cancel' })}
+							icon="talend-cross"
+							onClick={this.onCancel}
+							bsStyle="link"
+							className={classNames(
+								theme['tc-editable-text-form-buttons-icon'],
+								'tc-editable-text-form-buttons-icon',
+								theme['tc-editable-text-form-buttons-cancel'],
+								'tc-editable-text-form-buttons-cancel',
+							)}
+							hideLabel
+							data-feature={feature && `${feature}.cancel`}
+						/>
+						<Action
+							type="submit"
+							name="action-submit-title"
+							label={t('SUBMIT_TOOLTIP', { defaultValue: 'Submit' })}
+							icon="talend-check"
+							bsStyle="link"
+							className={classNames(
+								theme['tc-editable-text-form-buttons-icon'],
+								'tc-editable-text-form-buttons-icon',
+								theme['tc-editable-text-form-buttons-submit'],
+								'tc-editable-text-form-buttons-submit',
+							)}
+							disabled={notFilled}
+							hideLabel
+							data-feature={feature && `${feature}.submit`}
+						/>
+					</div>
+				</form>
+			</FocusManager>
 		);
 	}
 }
