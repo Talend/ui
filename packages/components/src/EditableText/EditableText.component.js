d7deeed536a376fb345dd15e5f73a7aa9335a02e
travis <travis>
2019-09-11 10:38:33 +0000

    chore(ci): prettier

--- packages/components/src/EditableText/EditableText.component.js
+++ packages/components/src/EditableText/EditableText.component.js
@@ -16,7 +16,11 @@ export function PlainTextTitle({ componentClass, onEdit, disabled, text, inProgr
 	const ComponentClass = componentClass;
 	return (
 		<div className={theme['tc-editable-text-title']}>
-			<TooltipTrigger label={text} tooltipPlacement="bottom" className={'tc-editable-text-wording-wrapper'}>
+			<TooltipTrigger
+				label={text}
+				tooltipPlacement="bottom"
+				className={'tc-editable-text-wording-wrapper'}
+			>
 				<ComponentClass
 					className={classNames(theme['tc-editable-text-wording'], 'tc-editable-text-wording')}
 					onDoubleClick={isDisabled ? undefined : onEdit}
@@ -30,7 +34,12 @@ export function PlainTextTitle({ componentClass, onEdit, disabled, text, inProgr
 				icon="talend-pencil"
 				onClick={onEdit}
 				bsStyle="link"
-				className={classNames(theme['tc-editable-text-pencil'], 'tc-editable-text-pencil', 'sr-only', 'sr-only-focusable')}
+				className={classNames(
+					theme['tc-editable-text-pencil'],
+					'tc-editable-text-pencil',
+					'sr-only',
+					'sr-only-focusable',
+				)}
 				disabled={disabled || inProgress}
 				hideLabel
 				data-feature={feature}
