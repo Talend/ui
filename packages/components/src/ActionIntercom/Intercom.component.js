6a2d0152531243090d812c911b392ed3f581d897
max <mkondzera@talend.com>
2019-09-23 15:15:16 +0300

    fix

--- packages/components/src/ActionIntercom/Intercom.component.js
+++ packages/components/src/ActionIntercom/Intercom.component.js
@@ -39,7 +39,7 @@ function Intercom({ id, className, config, t, tooltipPlacement }) {
 		: t('TC_INTERCOM_OPEN', { defaultValue: 'Chat with Talend Support' });
 
 	return (
-		<TooltipTrigger label={label} tooltipPlacement={'top-right'}>
+		<TooltipTrigger label={label} tooltipPlacement={tooltipPlacement}>
 			<button
 				ref={ref}
 				id={id}
