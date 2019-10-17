2fbf2efeb6b0d9fc02c74805257b7b18d86eef06
sgendre <sgendre@talend.com>
2019-10-17 17:31:24 +0200

    Virtualized tree WIP

--- packages/components/src/TreeView/TreeView.component.js
+++ packages/components/src/TreeView/TreeView.component.js
@@ -1,5 +1,10 @@
+import VTree from 'react-virtualized-tree';
+import 'react-virtualized/styles.css';
+import 'react-virtualized-tree/lib/main.css';
+
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+
 import classNames from 'classnames';
 import { Action } from '../';
 import TreeViewItem from './TreeViewItem/';
@@ -88,6 +93,98 @@ if (process.env.NODE_ENV !== 'production') {
  * <TreeView {...defaultProps} />
  *
  */
+export const COLLAPSED_CHILDREN = {
+	id: 3,
+	name: 'Leaf 3',
+	state: {
+		expanded: false,
+		favorite: true,
+		deletable: true,
+	},
+	children: [
+		{
+			id: 'c-3',
+			name: 'Leaf 3 Child',
+			state: {},
+		},
+	],
+};
+
+export const EXPANDED_CHILDREN = {
+	id: 2,
+	name: 'Leaf 2',
+	state: {
+		expanded: true,
+		deletable: true,
+	},
+	children: [
+		COLLAPSED_CHILDREN,
+		{
+			id: 4,
+			name: 'Leaf 4',
+		},
+	],
+};
+
+export const EXPANDED_NODE_IN_ROOT = {
+	id: 0,
+	name: 'Leaf 1',
+	state: {
+		expanded: true,
+	},
+	children: [
+		EXPANDED_CHILDREN,
+		{
+			id: 5,
+			name: 'Leaf 5',
+		},
+	],
+};
+
+export const COLLAPSED_NODE_IN_ROOT = {
+	id: 1,
+	name: 'Leaf 6',
+	state: {
+		expanded: false,
+		deletable: true,
+	},
+	children: [
+		{
+			id: 6,
+			name: 'Leaf 7',
+			state: {
+				expanded: false,
+			},
+			children: [
+				{
+					id: 7,
+					name: 'Leaf 8',
+				},
+				{
+					id: 8,
+					name: 'Leaf 9',
+				},
+			],
+		},
+		{
+			id: 9,
+			name: 'Leaf 10',
+		},
+	],
+};
+
+export const DELETABLE_IN_ROOT = {
+	id: 'z',
+	name: 'Leaf z',
+	state: {
+		deletable: true,
+		favorite: true,
+	},
+};
+
+export const DELETABLE_CHILDREN = EXPANDED_CHILDREN;
+
+export const Nodes = [EXPANDED_NODE_IN_ROOT, COLLAPSED_NODE_IN_ROOT, DELETABLE_IN_ROOT];
 function TreeView(props) {
 	const {
 		id,
@@ -97,35 +194,90 @@ function TreeView(props) {
 		onToggle,
 		className,
 		selectedId,
-		style,
 		headerRenderer,
 		...rest
 	} = props;
+	const containerStyle = props.style;
+	const [nodes, setNodes] = useState([]);
 	const titleId = id && `${id}-title`;
+
+	function nodeNameRenderer(rendererProps) {
+		const {
+			node: { name },
+			children,
+		} = rendererProps;
+		return (
+			<span>
+				{name}
+				{children}
+			</span>
+		);
+	}
+
+	function handleChange(_nodes) {
+		setNodes(_nodes);
+	}
+
+	function createNodeRenderer() {
+		return (nodeDisplay = this.state.nodeDisplay, props) => {
+			const [nextNode, ...remainingNodes] = nodeDisplay;
+
+			if (remainingNodes.length === 0) {
+				return this.renderNodeDisplay(nextNode, props);
+			}
+
+			return this.renderNodeDisplay(
+				nextNode,
+				props,
+				this.createNodeRenderer(remainingNodes, props),
+			);
+		};
+	}
+
+	//	style={containerStyle}
+
 	return (
-		<div className={classNames('tc-treeview', theme['tc-treeview'], className)} style={style}>
+		<div
+			className={classNames('tc-treeview', theme['tc-treeview'], className)}
+			style={{ height: 500, position: 'relative' }}
+		>
 			<TreeViewHeader {...rest} id={id} titleId={titleId} headerRenderer={headerRenderer} />
-			<ul className={theme['tc-treeview-list']} role="tree" aria-labelledby={titleId}>
-				{structure.map((item, i) => (
-					<TreeViewItem
-						id={id && `${id}-${i}`}
-						item={item}
-						siblings={structure}
-						onKeyDown={onKeyDown}
-						onSelect={onSelect}
-						onToggle={onToggle}
-						key={i}
-						index={i + 1}
-						selectedId={selectedId}
-						level={1}
-						hideFolderIcon
-					/>
-				))}
-			</ul>
+			HERE
+			<VTree
+				nodes={Nodes}
+				height={500}
+				width={500}
+				defaultHeight={500}
+				defaultWidth={500}
+				onChange={handleChange}
+			>
+				{({ style, node, ...p }) => <div style={style}>{nodeNameRenderer({ node, p })}</div>}
+			</VTree>
 		</div>
 	);
 }
+//			<ul className={theme['tc-treeview-list']} role="tree" aria-labelledby={titleId}></ul>
+// createNodeRenderer([NodeNameRenderer], p)
 
+/*
+				{structure.map((item, i) => (
+
+				))}
+
+<TreeViewItem
+	id={id && `${id}-${i}`}
+	item={item}
+	siblings={structure}
+	onKeyDown={onKeyDown}
+	onSelect={onSelect}
+	onToggle={onToggle}
+	key={i}
+	index={i + 1}
+	selectedId={selectedId}
+	level={1}
+	hideFolderIcon
+/>
+*/
 TreeView.displayName = 'TreeView';
 
 TreeView.defaultProps = {
