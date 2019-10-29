db4b547ea982467f9b47760e21e35022a85d051e
Yan Wang <ywang@talend.com>
2019-10-29 14:39:17 +0800

    revert unnecessary changes

--- packages/components/src/DateTimePickers/Date/Picker/Picker.component.js
+++ packages/components/src/DateTimePickers/Date/Picker/Picker.component.js
@@ -5,8 +5,6 @@ import CalendarPicker from '../../pickers/CalendarPicker';
 
 export default function Picker(props) {
 	const { value, pickerManagement } = useContext(DateContext);
-	return (
-		<CalendarPicker manageFocus selectedDate={value.date} {...pickerManagement} {...props} from />
-	);
+	return <CalendarPicker manageFocus selectedDate={value.date} {...pickerManagement} {...props} />;
 }
 Picker.displayName = 'Date.Picker';
