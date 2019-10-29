e58cd0672d48a06349d802749276cef6523a8519
Fabien Rassinier <frassinier@talend.com>
2019-10-29 09:26:13 +0100

    fix(containers/breadcrumbs): loading state

--- packages/containers/src/Breadcrumbs/Breadcrumbs.connect.js
+++ packages/containers/src/Breadcrumbs/Breadcrumbs.connect.js
@@ -3,14 +3,14 @@ import { cmfConnect } from '@talend/react-cmf';
 import { Map } from 'immutable';
 import Breadcrumbs from '@talend/react-components/lib/Breadcrumbs';
 
-const DEFAULT_STATE = new Map();
+const DEFAULT_STATE = Map();
 
 export function ContainerBreadcrumbs(props) {
 	const state = props.state || DEFAULT_STATE;
 	const items = state.get('items', props.items);
 	const newProps = {
-		id: state.get('id', props.id),
-		items: items.map(item => ({
+		...props,
+		items: items && items.map(item => ({
 			...item,
 			onClick: (event, data) => props.dispatchActionCreator(item.actionCreator, event, data),
 		})),
@@ -27,7 +27,7 @@ ContainerBreadcrumbs.propTypes = {
 };
 
 export default cmfConnect({
-	defaultState: new Map({ items: [], maxItems: 10 }),
+	defaultState: Map({ items: [], maxItems: 10 }),
 	omitCMFProps: true,
 	withComponentRegistry: true,
 	withDispatch: true,
