c68fa0782393463b1ed41b54f0516d02ff1e0447
Yan Wang <ywang@talend.com>
2019-11-08 17:44:00 +0800

    Update packages/components/src/DateTimePickers/DateRange/Inputs/Inputs.component.js
    Co-Authored-By: Jimmy Somsanith <jsomsanith@talend.com>

--- packages/components/src/DateTimePickers/DateRange/Inputs/Inputs.component.js
+++ packages/components/src/DateTimePickers/DateRange/Inputs/Inputs.component.js
@@ -61,7 +61,7 @@ function Inputs(props) {
 			<Icon name="talend-arrow-right" className={theme.icon} />
 		</span>,
 		<div>
-			<label htmlFor={`${props.id}-end-date-input`} className="control-label">{t('DATE_RANGE_TO', { defaultValue: 'To' })}</label>
+			<label htmlFor={`${props.id}-end-date-input`} className="control-label">{t('TC_DATE_PICKER__RANGE_TO', { defaultValue: 'To' })}</label>
 			<InputSizer placeholder={placeholder} inputText={endDate.value ? endDate.textInput : ''}>
 				{width => (
 					<DebounceInput
