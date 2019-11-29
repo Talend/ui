2d5ddcfb3a9d0a8b667a6a0a61ca392d87d0b5ba
Fabien Rassinier <frassinier@talend.com>
2019-11-29 10:31:51 +0100

    chore: upgrade Storybook to 5.2.6

--- versions/dependencies.js
+++ versions/dependencies.js
@@ -71,12 +71,12 @@ module.exports = {
 	deepmerge: '^1.5.1',
 
 	// dev deps
-	'@storybook/react': '^5.1.0',
-	'@storybook/addon-a11y': '^5.1.0',
-	'@storybook/addon-actions': '^5.1.0',
-	'@storybook/addon-info': '^5.1.0',
-	'@storybook/addon-knobs': '^5.1.0',
-	'@storybook/addons': '^5.1.0',
+	'@storybook/react': '^5.2.6',
+	'@storybook/addon-a11y': '^5.2.6',
+	'@storybook/addon-actions': '^5.2.6',
+	'@storybook/addon-info': '^5.2.6',
+	'@storybook/addon-knobs': '^5.2.6',
+	'@storybook/addons': '^5.2.6',
 	autoprefixer: '^7.1.4',
 	'babel-eslint': '^10.0.1',
 	'babel-jest': JEST_VERSION,
