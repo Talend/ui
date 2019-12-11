fde071e0007147e32a4b2d4756ee57613f9045f2
Yan Wang <ywang@talend.com>
2019-12-11 10:30:29 +0800

    validate range

--- packages/components/src/DateTimePickers/DateTimeRange/Manager/Manager.component.js
+++ packages/components/src/DateTimePickers/DateTimeRange/Manager/Manager.component.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import isBefore from 'date-fns/is_before';
+
 import { DateTimeRangeContext } from '../Context';
+import getErrorMessage from '../../shared/error-messages';
+
+export function DateTimeRangePickerException(code, message) {
+	this.message = getErrorMessage(message);
+	this.code = code;
+}
 
 function DateTimeRangeManager(props) {
 	const initialState = {
@@ -8,16 +16,42 @@ function DateTimeRangeManager(props) {
 		endDateTime: props.endDateTime,
 	};
 	const [state, setState] = useState(initialState);
+
+	function onChange(event, nextState, origin) {
+		const errors = [...(nextState.errors || [])];
+		const { startDateTime, endDateTime } = nextState;
+
+		if (startDateTime && endDateTime) {
+			if (!isBefore(startDateTime, endDateTime)) {
+				errors.push(
+					new DateTimeRangePickerException(
+						'INVALID_RANGE_START_AFTER_END',
+						'INVALID_RANGE_START_AFTER_END',
+					),
+				);
+			}
+		}
+		const payload = {
+			nextState,
+			origin,
+			errors,
+			errorMessage: errors[0] ? errors[0].message : null,
+		};
+		props.onChange(event, payload);
+	}
+
 	function onStartChange(event, payload) {
-		const nextState = { ...state, startDateTime: payload.datetime };
+		const nextState = { ...state, startDateTime: payload.datetime, errors: payload.errors };
 		setState(nextState);
-		props.onChange(nextState);
+		onChange(event, nextState, 'RANGE_START');
 	}
+
 	function onEndChange(event, payload) {
-		const nextState = { ...state, endDateTime: payload.datetime };
+		const nextState = { ...state, endDateTime: payload.datetime, errors: payload.errors };
 		setState(nextState);
-		props.onChange(nextState);
+		onChange(event, nextState, 'RANGE_END');
 	}
+
 	return (
 		<DateTimeRangeContext.Provider
 			value={{
