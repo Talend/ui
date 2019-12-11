de8233e3c4bdb2ff8827a8c82a9156e087bbdcec
Yan Wang <ywang@talend.com>
2019-12-11 11:00:01 +0800

    add labels

--- packages/components/src/DateTimePickers/InputDateTimeRangePicker/InputDateTimeRangePicker.component.js
+++ packages/components/src/DateTimePickers/InputDateTimeRangePicker/InputDateTimeRangePicker.component.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import Icon from '../../Icon';
 
 import InputDateTimePicker from '../InputDateTimePicker';
 import DateTimeRange from '../DateTimeRange';
 import { DateTimeRangeContext } from '../DateTimeRange/Context';
+
+import getDefaultT from '../../translate';
 
 import theme from './InputDateTimeRangePicker.scss';
 
@@ -19,26 +22,40 @@ function InputDateTimeRangePicker(props) {
 		>
 			<DateTimeRangeContext.Consumer>
 				{({ startDateTime, endDateTime, onStartChange, onEndChange }) => (
-					<div className={theme['range-picker']}>
-						<InputDateTimePicker
-							selectedDateTime={startDateTime}
-							endDate={endDateTime}
-							onChange={onStartChange}
-						/>
+					<div className={classnames(theme['range-picker'], 'range-picker')}>
+						<div>
+							<label htmlFor={props.id} className="control-label">
+								{props.t('TC_DATE_PICKER_RANGE_FROM', { defaultValue: 'From' })}
+							</label>
+							<InputDateTimePicker
+								selectedDateTime={startDateTime}
+								endDate={endDateTime}
+								onChange={onStartChange}
+							/>
+						</div>
 						<span className={theme.arrow}>
 							<Icon name="talend-arrow-right" className={theme.icon} />
 						</span>
-						<InputDateTimePicker
-							selectedDateTime={endDateTime}
-							startDate={startDateTime}
-							onChange={onEndChange}
-						/>
+						<div>
+							<label htmlFor={props.id} className="control-label">
+								{props.t('TC_DATE_PICKER__RANGE_TO', { defaultValue: 'To' })}
+							</label>
+							<InputDateTimePicker
+								selectedDateTime={endDateTime}
+								startDate={startDateTime}
+								onChange={onEndChange}
+							/>
+						</div>
 					</div>
 				)}
 			</DateTimeRangeContext.Consumer>
 		</DateTimeRange.Manager>
 	);
 }
+InputDateTimeRangePicker.defaultProps = {
+	dateFormat: 'YYYY-MM-DD',
+	t: getDefaultT(),
+};
 
 InputDateTimeRangePicker.propTypes = {
 	// id: PropTypes.string.isRequired,
@@ -55,7 +72,7 @@ InputDateTimeRangePicker.propTypes = {
 		PropTypes.number,
 		PropTypes.string,
 	]),
-	// t: PropTypes.func.isRequired,
+	t: PropTypes.func.isRequired,
 };
 
 export default InputDateTimeRangePicker;
