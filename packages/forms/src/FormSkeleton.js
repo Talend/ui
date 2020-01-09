8493e3555f3d02fe4074cdaf87c3788cb2765242
Fabien Rassinier <frassinier@talend.com>
2020-01-09 10:40:34 +0100

    feat(components/Drawers): style

--- packages/forms/src/FormSkeleton.js
+++ packages/forms/src/FormSkeleton.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Skeleton from '@talend/react-components/lib/Skeleton';
 import theme from './FormSkeleton.scss';
 
-export default function FormSkeleton() {
+export default function FormSkeleton({ displayMode }) {
 	return (
 		<div className={`${theme.container} tc-skeleton-heartbeat`} aria-busy>
 			<div className={theme['form-content']}>
@@ -15,12 +16,16 @@ export default function FormSkeleton() {
 				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} size={Skeleton.SIZES.large} />
 				<Skeleton heartbeat={false} type={Skeleton.TYPES.text} className="skeleton-fit-content" />
 			</div>
-			<div className={theme.submit}>
+			{displayMode !== 'text' && <div className={theme.submit}>
 				<div className={theme['submit-wrapper']}>
 					<Skeleton heartbeat={false} type={Skeleton.TYPES.button} />
 					<Skeleton heartbeat={false} type={Skeleton.TYPES.button} />
 				</div>
-			</div>
+			</div>}
 		</div>
 	);
 }
+
+FormSkeleton.propTypes = {
+	displayMode: PropTypes.string,
+};
