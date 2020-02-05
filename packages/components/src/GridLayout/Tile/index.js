c937927a5a764b5b3f90637adf9bf51554e1fe56
Nicolas Comont <ncomont@talend.com>
2020-02-05 11:42:30 +0100

    feat(component/gridlayout): add footer

--- packages/components/src/GridLayout/Tile/index.js
+++ packages/components/src/GridLayout/Tile/index.js
@@ -1,12 +1,14 @@
 import Container from './Tile.component';
 import Body from './Body/TileBody.component';
 import Header from './Header/TileHeader.component';
+import Footer from './Footer/TileFooter.component';
 import Skeleton from './Skeleton/SkeletonTile.component';
 import { useTileContext } from './context';
 
 export default {
 	Body,
 	Header,
+	Footer,
 	Container,
 	Skeleton,
 	useTileContext,
