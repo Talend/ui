705412048d2fbc40913b546fd0262c4f8b1da7a4
Mathieu Huchet <mhuchet@talend.com>
2020-02-12 13:42:10 +0100

    Review

--- packages/components/src/VirtualizedList/CellLabel/CellLabel.test.js
+++ packages/components/src/VirtualizedList/CellLabel/CellLabel.test.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import CellLabel from './CellLabel.component';
 
 describe('CellLabel', () => {
-	it('should render label', () => {
+	it('should default render', () => {
+		// given
+		const label = 'my label';
 		// when
-		const wrapper = shallow(<CellLabel cellData="label" rowIndex={25} />);
+		const wrapper = mount(<CellLabel cellData={label} rowIndex={25} />);
+		// then
+		expect(
+			wrapper
+				.find('.label')
+				.at(0)
+				.text(),
+		).toBe(label);
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	it('should render a label info', () => {
+		// given
+		const label = 'my label';
+		// when
+		const wrapper = shallow(<CellLabel cellData={label} rowIndex={25} />);
 
 		// then
+		expect(
+			wrapper
+				.find('.label-info')
+				.at(0)
+				.text(),
+		).toBe(label);
 		expect(wrapper.getElement()).toMatchSnapshot();
 	});
 });
