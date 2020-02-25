7134c4f9fb4ffffaee63f6882008a1e9361017ca
Fabien Rassinier <frassinier@talend.com>
2020-02-25 18:31:41 +0100

    chore(i18n): Add language switcher and locales for stories

--- packages/datagrid/stories/config/i18n.js
+++ packages/datagrid/stories/config/i18n.js
@@ -1,63 +1,25 @@
-import React from 'react';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import I18N_DOMAIN_DATAGRID from '../../src/constant';
+
+import { namespaces as tuiNamespaces } from '@talend/locales-tui/namespaces';
+import { locales as tuiLocales } from '@talend/locales-tui/locales';
 
 i18n.use(initReactI18next).init({
-	lng: 'en',
-	resources: {
-		en: {
-			[I18N_DOMAIN_DATAGRID]: {
-				QUALITY_BAR_INVALID: '{{count}} invalid value ({{percentage}}%)',
-				QUALITY_BAR_INVALID_plural: '{{count}} invalid values ({{percentage}}%)',
-				QUALITY_BAR_EMPTY: '{{count}} empty value ({{percentage}}%)',
-				QUALITY_BAR_EMPTY_plural: '{{count}} empty values ({{percentage}}%)',
-				QUALITY_BAR_VALID: '{{count}} valid value ({{percentage}}%)',
-				QUALITY_BAR_VALID_plural: '{{count}} valid values ({{percentage}}%)',
-				QUALITY_INDICATOR_INVALID_VALUE: 'Invalid value',
-			},
-		},
-		fr: {
-			[I18N_DOMAIN_DATAGRID]: {
-				QUALITY_BAR_INVALID: '{{count}} valeur invalide ({{percentage}}%)',
-				QUALITY_BAR_INVALID_plural: '{{count}} valeurs invalides ({{percentage}}%)',
-				QUALITY_BAR_EMPTY: '{{count}} valeur vide ({{percentage}}%)',
-				QUALITY_BAR_EMPTY_plural: '{{count}} valeurs vides ({{percentage}}%)',
-				QUALITY_BAR_VALID: '{{count}} valeur valide ({{percentage}}%)',
-				QUALITY_BAR_VALID_plural: '{{count}} valeurs valides ({{percentage}}%)',
-				QUALITY_INDICATOR_INVALID_VALUE: 'Valeur invalide',
-			},
+	debug: true,
+	defaultNS: 'tui-datagrid',
+	interpolation: {
+		format: (value, format) => {
+			if (value && format === 'lowercase') return value.toLocaleLowerCase();
+			if (value && format === 'uppercase') return value.toLocaleUpperCase();
+			return value;
 		},
 	},
-	debug: false,
+	ns: [...tuiNamespaces],
+	resources: tuiLocales,
 	wait: true, // globally set to wait for loaded translations in translate hoc
 });
 
-export const LanguageSwitcher = () => {
-	const style = {
-		position: 'fixed',
-		bottom: 0,
-		width: '100vw',
-		textAlign: 'center',
-		zIndex: 1,
-	};
-
-	function renderBtn(locale, isDefault) {
-		return (
-			<button className="btn" onClick={() => i18n.changeLanguage(locale)}>
-				{locale} {isDefault && '(default)'}
-			</button>
-		);
-	}
-
-	return (
-		<nav style={style}>
-			<div className="btn-group">
-				{renderBtn('en', true)}
-				{renderBtn('fr')}
-			</div>
-		</nav>
-	);
-};
+window.i18n = i18n;
 
 export default i18n;
+
--- packages/datagrid/stories/datagrid.container.js
+++ packages/datagrid/stories/datagrid.container.js
@@ -9,7 +9,6 @@ import api from '@talend/react-cmf';
 import DataGrid from '../src/';
 import register from '../src/register';
 import theme from '../src/components/DataGrid/DataGrid.scss';
-import { LanguageSwitcher } from './config/i18n';
 import sample from './sample.json';
 import sampleRenderer from './sampleRenderer.json';
 
@@ -217,7 +216,6 @@ const options = {
 storiesOf('Container Datagrid', module)
 	.addDecorator(story => (
 		<div>
-			<LanguageSwitcher />
 			{story()}
 		</div>
 	))
