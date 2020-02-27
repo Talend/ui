a7b7c7a3f2cd683b43a34e461af5b03e9cc1167c
Fabien Rassinier <frassinier@talend.com>
2020-02-27 15:16:02 +0100

    chore(i18n): Add language switcher and locales for stories

---
+++ .storybook/i18n.js
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import { namespaces as tuiNamespaces } from '@talend/locales-tui/namespaces';
+import { locales as tuiLocales } from '@talend/locales-tui/locales';
+
+i18n.use(initReactI18next).init({
+    debug: true,
+    interpolation: {
+        format: (value, format) => {
+            if (value && format === 'lowercase') return value.toLocaleLowerCase();
+            if (value && format === 'uppercase') return value.toLocaleUpperCase();
+            return value;
+        },
+    },
+    ns: [...tuiNamespaces],
+    resources: tuiLocales,
+    wait: true, // globally set to wait for loaded translations in translate hoc
+});
+
+export default i18n;
