baf978b4c896128e24aa20f326931c2e3de18cbd
jsomsanith-tlnd <jsomsanith@talend.com>
2020-03-09 16:44:45 +0100

    Change ci

--- .travis/chroma.sh
+++ .travis/chroma.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 echo "CHROMA"
-if [[ $TRAVIS_EVENT_TYPE == 'pull_request' &&  $TRAVIS_PULL_REQUEST_SLUG == $TRAVIS_REPO_SLUG ]];
+if [[ $TRAVIS_EVENT_TYPE != 'pull_request' ||  $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG ]];
 then
-    # https://docs.chromaticqa.com/setup_ci#configuring-specific-ci-services
-    echo "✓ No chroma to publish: it's an internal PR event, we publish only on push. For more details: https://docs.chromaticqa.com/setup_ci#configuring-specific-ci-services"
-    exit 0;
+    yarn chroma
+    echo "✓ Chroma published for PR"
 fi
 
-yarn chroma
-echo "✓ Chroma published for PR"
+ # https://docs.chromaticqa.com/setup_ci#configuring-specific-ci-services
+echo "✓ No chroma to publish: it's an internal PR event, we publish only on push. For more details: https://docs.chromaticqa.com/setup_ci#configuring-specific-ci-services"
+exit 0;
+
 
 # if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_BRANCH" == 'master' ]; then
 # 	yarn chroma --auto-accept-changes
