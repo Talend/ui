145082135a8df4ab27620776d076f4fda1fbd8a5
jsomsanith-tlnd <jsomsanith@talend.com>
2020-03-09 16:24:36 +0100

    change

--- .travis/chroma.sh
+++ .travis/chroma.sh
@@ -7,10 +7,16 @@ then
     echo "✓ No chroma to publish: it's an internal PR event, we publish only on push. For more details: https://docs.chromaticqa.com/setup_ci#configuring-specific-ci-services"
     exit 0;
 fi
-if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_BRANCH" == 'master' ]; then
-	yarn chroma --auto-accept-changes
-elif [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
-    yarn chroma
-else
-    echo "✓ No chroma to publish"
-fi
+
+yarn chroma
+echo "✓ Chroma published for PR"
+
+# if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ "$TRAVIS_BRANCH" == 'master' ]; then
+# 	yarn chroma --auto-accept-changes
+#     echo "✓ Chroma published for master with all changes automatically accepted"
+# elif [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
+#     yarn chroma
+#     echo "✓ Chroma published for PR"
+# else
+#     echo "✓ No chroma to publish"
+# fi
