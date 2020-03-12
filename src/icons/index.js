1efbcd1206cda6bb88c4c0afc7b905df8b32195c
Fabien Rassinier <frassinier@talend.com>
2020-03-12 11:23:27 +0100

    feat: add icons

---
+++ src/icons/index.js
@@ -0,0 +1,15 @@
+import caret from './caret.svg';
+import checkCircle from './check-circle.svg';
+import cross from './cross.svg';
+import information from './information.svg';
+import plus from './plus.svg';
+import warning from './warning.svg';
+
+export default {
+    caret,
+    checkCircle,
+    cross,
+    information,
+    plus,
+    warning,
+};
