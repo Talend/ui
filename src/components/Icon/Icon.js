dbb5514af2d2c68ce98b786ff126f0708a65620f
Fabien Rassinier <frassinier@talend.com>
2020-03-14 00:03:35 +0100

    chore: add Icon story

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SVG from 'react-inlinesvg';
+
 import icons from '../../icons';
 
 function isRequired() {
@@ -7,9 +9,13 @@ function isRequired() {
 }
 
 // TODO https://medium.com/@allalmohamedlamine/react-best-way-of-importing-svg-the-how-and-why-f7c968272dd9
-export default function Icon({ name = isRequired() }) {
+export default function Icon({ name = isRequired(), ...rest }) {
 	if (!Object.keys(icons).find(iconName => iconName === name)) {
 		return null;
 	}
-	return <SVG src={icons[name]} />;
+	return <SVG src={icons[name]} {...rest} />;
 }
+
+Icon.propTypes = {
+	name: PropTypes.string.isRequired,
+};
