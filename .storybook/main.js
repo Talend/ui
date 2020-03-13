a04e704b8ffedbf973114e876063c2f43097c3f1
Fabien Rassinier <frassinier@talend.com>
2020-03-14 00:27:20 +0100

    chore: add knobs addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
-	addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+	addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs/register'],
 	presets: ['@storybook/addon-docs/preset'],
 };
--- src/components/Alert/docs/Alert.stories.js
+++ src/components/Alert/docs/Alert.stories.js
@@ -1,44 +1,64 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 import Alert from './../index';
 
 export default {
 	title: 'Components|Alert',
-
 	parameters: {
 		component: Alert,
 	},
+	decorators: [withKnobs],
 };
 
 export const basic = () => (
-	<Alert title={'title'} description={'lorem ipsum dolor sit amet'} link={<a href={'#'}>link</a>} />
+	<Alert
+		title={text('title', 'Lorem ipsum')}
+		description={text(
+			'description',
+			'dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim posuere ac.',
+		)}
+		link={<a href={'#'}>{text('link', 'See more')}</a>}
+	/>
 );
 export const information = () => (
 	<Alert.Information
-		title={'title'}
-		description={'lorem ipsum dolor sit amet'}
-		link={<a href={'#'}>link</a>}
+		title={text('title', 'Lorem ipsum')}
+		description={text(
+			'description',
+			'dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim posuere ac.',
+		)}
+		link={<a href={'#'}>{text('link', 'See more')}</a>}
 	/>
 );
 export const success = () => (
 	<Alert.Success
-		title={'title'}
-		description={'lorem ipsum dolor sit amet'}
-		link={<a href={'#'}>link</a>}
+		title={text('title', 'Lorem ipsum')}
+		description={text(
+			'description',
+			'dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim posuere ac.',
+		)}
+		link={<a href={'#'}>{text('link', 'See more')}</a>}
 	/>
 );
 export const warning = () => (
 	<Alert.Warning
-		title={'title'}
-		description={'lorem ipsum dolor sit amet'}
-		link={<a href={'#'}>link</a>}
+		title={text('title', 'Lorem ipsum')}
+		description={text(
+			'description',
+			'dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim posuere ac.',
+		)}
+		link={<a href={'#'}>{text('link', 'See more')}</a>}
 	/>
 );
 export const destructive = () => (
 	<Alert.Destructive
-		title={'title'}
-		description={'lorem ipsum dolor sit amet'}
-		link={<a href={'#'}>link</a>}
+		title={text('title', 'Lorem ipsum')}
+		description={text(
+			'description',
+			'dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim posuere ac.',
+		)}
+		link={<a href={'#'}>{text('link', 'See more')}</a>}
 	/>
 );
