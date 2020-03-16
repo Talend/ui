1981f745310792903481c5b79791415c67bb1038
Fabien Rassinier <frassinier@talend.com>
2020-03-16 23:44:36 +0100

    chore: add custom storyook theme

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js'],
-	addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs/register'],
+	addons: ['@storybook/addon-actions', '@storybook/addon-knobs/register', '@storybook/addon-links'],
 	presets: ['@storybook/addon-docs/preset'],
 };
