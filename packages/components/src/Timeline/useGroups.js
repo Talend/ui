c4d6d307965425fad19287993c578b78257838c1
jsomsanith-tlnd <jsomsanith@talend.com>
2020-03-25 09:45:59 +0100

    fix: group data level takes first level available

--- packages/components/src/Timeline/useGroups.js
+++ packages/components/src/Timeline/useGroups.js
@@ -1,14 +1,24 @@
 import { useMemo } from 'react';
+import get from 'lodash/get';
+
+const timesOverlap = (time1, time2) =>
+	(!time1.end && !time2.end) || // 2 running
+	(!time1.end && time1.start <= time2.end) || // first is running
+	(!time2.end && time2.start <= time1.end) || // second is running
+	(time1.start >= time2.start && time1.start <= time2.end) || // first starts during second run
+	(time1.end >= time2.start && time1.end <= time2.end) || // first ends during second run
+	(time2.start >= time1.start && time2.start <= time1.end) || // second starts during first run
+	(time2.end >= time1.start && time2.end <= time1.end); // second ends during first run
 
 export default function useGroups(data, { groupIdName, groupLabelName, startName, endName }) {
 	return useMemo(() => {
 		const groupsDict = data.reduce((accu, item) => {
-			const groupId = item[groupIdName] || 'default';
+			const groupId = get(item, groupIdName) || 'default';
 			let group = accu[groupId];
 			if (!group) {
 				group = {
 					id: groupId,
-					label: item[groupLabelName] || 'Default',
+					label: get(item, groupLabelName) || 'Default',
 					items: [],
 				};
 				accu[groupId] = group;
@@ -19,23 +29,30 @@ export default function useGroups(data, { groupIdName, groupLabelName, startName
 
 		const groups = Object.values(groupsDict);
 		groups.forEach(group => {
-			group.items.sort((item1, item2) => item1[startName] - item2[startName]);
+			group.items.sort((item1, item2) => get(item1, startName) - get(item2, startName));
 
 			let maxLevel = 0;
-			group.items = group.items.map((item, index) => {
-				const itemStart = item[startName];
+			const itemsWithLevel = [];
+			group.items.forEach(item => {
+				const itemStart = get(item, startName);
+				const itemEnd = get(item, endName);
+				const itemsThatOverlap = itemsWithLevel.filter(cursor =>
+					timesOverlap(
+						{ start: itemStart, end: itemEnd },
+						{ start: get(cursor, startName), end: get(cursor, endName) },
+					),
+				);
+
 				let itemLevel = 0;
-				for (let i = 0; i < index; i++) {
-					const cursor = group.items[i];
-					if (itemStart >= cursor[startName] && itemStart < cursor[endName]) {
-						itemLevel++;
-					}
+				while (itemsThatOverlap.some(({ _timelineLevel }) => _timelineLevel === itemLevel)) {
+					itemLevel++;
 				}
 
 				maxLevel = Math.max(maxLevel, itemLevel);
 
-				return itemLevel === 0 ? item : { ...item, _timelineLevel: itemLevel };
+				itemsWithLevel.push({ ...item, _timelineLevel: itemLevel });
 			});
+			group.items = itemsWithLevel;
 			group.maxLevel = maxLevel;
 		});
 
