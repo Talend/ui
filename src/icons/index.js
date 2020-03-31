eb758a7582caa78818d0da9ee0b5b0a719007024
Monia Huet <mhuet@talend.com>
2020-03-31 17:36:19 +0200

    improve alert message guideline

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,14 +1,14 @@
 import caret from './caret.svg';
-import checkCircle from './check-circle.svg';
 import cross from './cross.svg';
 import information from './information.svg';
+import check from './check.svg';
 import plus from './plus.svg';
 import warning from './warning.svg';
 
 export default {
 	caret,
-	checkCircle,
 	cross,
+	check,
 	information,
 	plus,
 	warning,
