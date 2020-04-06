894a51e3d8c0856202ffe5052af73faec44c1438
Gwendal Jouannic <gjouannic@talend.com>
2020-04-06 11:43:41 +0200

    fix eslint issue

--- packages/stepper/src/service/Stepper.utils.js
+++ packages/stepper/src/service/Stepper.utils.js
@@ -1,9 +1,8 @@
-import { LOADING_STEP_STATUSES } from '../Stepper.constants';
-
 /**
  * This function returns the key to point the loading step in the store
  * It's based on the resourceType / resourceId
  * internally usage only in the loading step service
  * @param {object} action redux action
  */
+// eslint-disable-next-line
 export const getStepperKey = action => `${action.resourceType}-${action.resourceId}`;
