7f0c5df28968fe8253d5db28e0564826833144f7
Sébastien Romain <sebastien.romain@gmail.com>
2020-04-12 20:53:25 +0200

    one way to do it

--- packages/cmf/src/cmfModule.js
+++ packages/cmf/src/cmfModule.js
@@ -1,22 +1,29 @@
 import merge from './cmfModule.merge';
 
+async function getModule(module) {
+	const { asyncInit, ...syncModule } = module;
+	if (asyncInit) {
+		const asyncModule = await asyncInit();
+		return {
+			...syncModule,
+			...asyncModule,
+		};
+	}
+	return syncModule;
+}
+
 async function find(options, buff = []) {
 	if (options.modules) {
-		for await (let current of options.modules) {
-			if (typeof module === 'function') {
-				current = await current();
-			}
-
+		for await (const current of options.modules) {
 			if (!current.id) {
 				throw new Error('a cmf.module must have an id');
 			}
-			if (buff.some(({ id }) => current.id === id)) {
-				console.warn(
-					`cmf.bootstrap: 2 modules have the same id ${current.id}. This duplicated module will be skipped.`,
-				);
-			} else {
-				buff.push(current);
-				await find(current, buff);
+
+			// prevent module duplication
+			if (!buff.some(({ id }) => current.id === id)) {
+				const module = await getModule(current);
+				buff.push(module);
+				await find(module, buff);
 			}
 		}
 	}
