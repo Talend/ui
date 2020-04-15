cd89c37e9358b47e6f5b64d12ef10e1824e8e842
Fabien Rassinier <frassinier@talend.com>
2020-04-15 23:53:05 +0200

    fix: Storybook on IE11

--- .storybook/sortStories.js
+++ .storybook/sortStories.js
@@ -14,7 +14,7 @@ const categories = [
 
 addParameters({
 	options: {
-		storySort: (a, b) => {
+		storySort: function(a, b) {
 			const aCat = a[1].kind ? a[1].kind.split('/')[0] : '';
 			const bCat = b[1].kind ? b[1].kind.split('/')[0] : '';
 			const aLevel = categories.indexOf(aCat);
