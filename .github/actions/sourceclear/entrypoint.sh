87e85fe9275fbd3dae6d852147dfb612de06c4db
jsomsanith-tlnd <jsomsanith@talend.com>
2020-04-16 10:23:22 +0200

    Change list, switch back to push event to test, need revert on that

--- .github/actions/sourceclear/entrypoint.sh
+++ .github/actions/sourceclear/entrypoint.sh
@@ -5,64 +5,24 @@ echo "Script saved to srcclr.sh"
 chmod a+x srcclr.sh
 echo "Script execution rights added"
 
-# monorepo
+packages="cmf,cmf-cqrs,cmf-webpack-plugin,components,containers,datagrid,forms,icons,sagas,stepper,theme,faceted-search,router,html-webpack-plugin"
+
+# set comma as internal field separator for the string list
+Field_Separator=$IFS 
+IFS=,
+
+# scan each folder
 echo "Starting scan on . (root) ..."
 ./srcclr.sh scan .
 echo "Scan completed on . (root)"
 
-echo "Starting scan on ./packages/cmf ..."
-./srcclr.sh scan ./packages/cmf
-echo "Scan completed on ./packages/cmf"
+for folder in $packages;
+do
+    echo "Starting scan on ./packages/$folder ..."
+    ./srcclr.sh scan ./packages/$folder
+    echo "Scan completed on ./packages/$folder"
+done
 
-echo "Starting scan on ./packages/cmf-cqrs ..."
-./srcclr.sh scan ./packages/cmf-cqrs
-echo "Scan completed on ./packages/cmf-cqrs"
+# set back default field separator
+IFS=$Field_Separator
 
-echo "Starting scan on ./packages/cmf-webpack-plugin ..."
-./srcclr.sh scan ./packages/cmf-webpack-plugin
-echo "Scan completed on ./packages/cmf-webpack-plugin"
-
-echo "Starting scan on ./packages/components ..."
-./srcclr.sh scan ./packages/components
-echo "Scan completed on ./packages/components"
-
-echo "Starting scan on ./packages/containers ..."
-./srcclr.sh scan ./packages/containers
-echo "Scan completed on ./packages/containers"
-
-echo "Starting scan on ./packages/datagrid ..."
-./srcclr.sh scan ./packages/datagrid
-echo "Scan completed on ./packages/datagrid"
-
-echo "Starting scan on ./packages/forms ..."
-./srcclr.sh scan ./packages/forms
-echo "Scan completed on ./packages/forms"
-
-echo "Starting scan on ./packages/icons ..."
-./srcclr.sh scan ./packages/icons
-echo "Scan completed on ./packages/icons"
-
-echo "Starting scan on ./packages/saga ..."
-./srcclr.sh scan ./packages/saga
-echo "Scan completed on ./packages/saga"
-
-echo "Starting scan on ./packages/stepper ..."
-./srcclr.sh scan ./packages/stepper
-echo "Scan completed on ./packages/stepper"
-
-echo "Starting scan on ./packages/theme ..."
-./srcclr.sh scan ./packages/theme
-echo "Scan completed on ./packages/theme"
-
-# non monorepo
-echo "Starting scan on ./packages/faceted-search ..."
-./srcclr.sh scan ./packages/faceted-search
-echo "Scan completed on ./packages/faceted-search"
-
-echo "Starting scan on ./packages/router ..."
-./srcclr.sh scan ./packages/router
-echo "Scan completed on ./packages/router"
-
-echo "Starting scan on ./packages/html-webpack-plugin ..."
-./srcclr.sh scan ./packages/html-webpack-plugin
-echo "Scan completed on ./packages/html-webpack-plugin"
