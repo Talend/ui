6808efdd4c1d9d6563824ad0d317d070624fa308
Ré Jean-Alain <jare@talend.com>
2020-04-20 15:29:41 +0200

    fix review

--- packages/components/src/AppGuidedTour/DemoContentStep.component.js
+++ packages/components/src/AppGuidedTour/DemoContentStep.component.js
@@ -18,7 +18,7 @@ export default function DemoContentStep({ demoContentSteps }) {
 				<Icon name="talend-info-circle" />
 				{i18n.t('tui-components:DEMO_CONTENT_LOADING_MESSAGE', {
 					defaultValue:
-						'Loading may take a few minutes to complete. Bear with us. Content is loading. Isn\'t it time for tea or coffee?',
+						"Loading may take a few minutes to complete. Bear with us. Content is loading. Isn't it time for tea or coffee?",
 				})}
 			</p>
 			<Stepper steps={demoContentSteps} />
