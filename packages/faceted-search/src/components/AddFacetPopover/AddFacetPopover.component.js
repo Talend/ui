2d1a6e066354f09b937bba9e2df51e42cb68cfbe
Vincent Bocquet <vbocquet@talend.com>
2020-04-20 15:44:25 +0200

    chore(faceted-search): Update changelog

--- packages/faceted-search/src/components/AddFacetPopover/AddFacetPopover.component.js
+++ packages/faceted-search/src/components/AddFacetPopover/AddFacetPopover.component.js
@@ -90,7 +90,6 @@ const AddFacetRow = ({ badgeDefinition, id, label, onClick, isFocusable, badges,
 				role="button"
 				tabIndex={getTabIndex(isFocusable)}
 				disabled={isDisabled}
-				data-feature={'klsdjfklsjdflk'}
 			>
 				<div className={theme('tc-add-facet-popover-row-text')}>{label}</div>
 			</Button>
