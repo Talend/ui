804ab8a7b4bab9ba66d0fd3f0cbb5cbad40d09d9
jsomsanith-tlnd <jsomsanith@talend.com>
2020-04-27 10:26:17 +0200

    eslint output

--- .travis/after_success_lint.sh
+++ .travis/after_success_lint.sh
@@ -3,14 +3,14 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
 	echo "Linting JavaScript files"
-	(lerna exec --scope=@talend/react-sagas -- npm run lint:es | awk '{if(NR>2)print}') 1> output/sagas.eslint.txt
-	(lerna exec --scope=@talend/react-datagrid -- npm run lint:es | awk '{if(NR>2)print}') 1> output/datagrid.eslint.txt
-	(lerna exec --scope=@talend/react-stepper -- npm run lint:es | awk '{if(NR>2)print}') 1> output/stepper.eslint.txt
-	(lerna exec --scope=@talend/react-cmf -- npm run lint:es | awk '{if(NR>2)print}') 1> output/cmf.eslint.txt 2>/dev/null
-	(lerna exec --scope=@talend/react-cmf-webpack-plugin -- npm run lint:es | awk '{if(NR>2)print}') 1> output/cmf-webpack-plugin.eslint.txt 2>/dev/null
-	(lerna exec --scope=@talend/react-components -- npm run lint:es | awk '{if(NR>2)print}') 1> output/components.eslint.txt 2>/dev/null
-	(lerna exec --scope=@talend/react-containers -- npm run lint:es | awk '{if(NR>2)print}') 1> output/containers.eslint.txt 2>/dev/null
-	(lerna exec --scope=@talend/react-forms -- npm run lint:es | awk '{if(NR>2)print}') 1> output/forms.eslint.txt 2>/dev/null
+	(lerna exec --scope=@talend/react-sagas -- npm run lint:es -o output/sagas.eslint.txt)
+	(lerna exec --scope=@talend/react-datagrid -- npm run lint:es -o output/datagrid.eslint.txt)
+	(lerna exec --scope=@talend/react-stepper -- npm run lint:es -o output/stepper.eslint.txt)
+	(lerna exec --scope=@talend/react-cmf -- npm run lint:es -o output/cmf.eslint.txt)
+	(lerna exec --scope=@talend/react-cmf-webpack-plugin -- npm run lint:es -o output/cmf-webpack-plugin.eslint.txt)
+	(lerna exec --scope=@talend/react-components -- npm run lint:es -o output/components.eslint.txt)
+	(lerna exec --scope=@talend/react-containers -- npm run lint:es -o output/containers.eslint.txt)
+	(lerna exec --scope=@talend/react-forms -- npm run lint:es -o output/forms.eslint.txt)
 	# not in lerna
 	cd packages/router/ && (yarn run lint:es | awk '{if(NR>2)print}') 1> ../../output/router.eslint.txt 2>/dev/null && cd ../../
 
