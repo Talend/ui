d2c484cf3c732e23a8837d39e4e40754b2d2c1a1
yatheesh <ybaskaran@talend.com>
2020-04-29 00:26:24 +0200

    fix(TCCUI-135) - show date value if specified without time

--- packages/components/src/DateTimePickers/DateTime/datetime-extraction.js
+++ packages/components/src/DateTimePickers/DateTime/datetime-extraction.js
@@ -6,8 +6,6 @@ import getErrorMessage from '../shared/error-messages';
 import { convertDateToTimezone, extractDateOnly } from '../Date/date-extraction';
 import { checkTime, pad, timeToStr, strToTime } from '../Time/time-extraction';
 
-const splitDateAndTimePartsRegex = new RegExp(/^\s*(.*)\s+((.*):(.*)(:.*)?)\s*$/);
-
 const INTERNAL_INVALID_DATE = new Date('INTERNAL_INVALID_DATE');
 
 export function DateTimePickerException(code, message) {
@@ -162,15 +160,12 @@ function extractPartsFromTextInput(textInput, options) {
 	let errors = [];
 
 	try {
-		const splitMatches = textInput.match(splitDateAndTimePartsRegex) || [];
-		if (!splitMatches.length) {
-			throw new DateTimePickerException('DATETIME_INVALID_FORMAT', 'DATETIME_INVALID_FORMAT');
-		} else {
-			date = splitMatches[1];
-			time = splitMatches[2];
-			datetime = dateAndTimeToDateTime(date, time, options);
-		}
+		const splitMatches = textInput.split(/\s/);
+		date = splitMatches[0];
+		time = splitMatches[1];
+		datetime = dateAndTimeToDateTime(date, time, options);
 	} catch (error) {
+		datetime = INTERNAL_INVALID_DATE;
 		errors = [error];
 	}
 
