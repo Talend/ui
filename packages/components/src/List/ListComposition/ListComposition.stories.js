4b3225555ca3ed6e643727607cb7f1eb643717b9
Sébastien Romain <sromain@talend.com>
2020-04-30 15:31:16 +0200

    feat(List): add IconTextCell column (#2806)

--- packages/components/src/List/ListComposition/ListComposition.stories.js
+++ packages/components/src/List/ListComposition/ListComposition.stories.js
@@ -26,6 +26,7 @@ function CustomList(props) {
 		<List.VList id="my-vlist" {...props}>
 			<List.VList.Text label="Id" dataKey="id" />
 			<List.VList.Title label="Name" dataKey="name" columnData={titleProps} />
+			<List.VList.IconText label="IconText" dataKey="iconAndText" />
 			<List.VList.Boolean label="Valid" dataKey="isValid" />
 			<List.VList.Boolean
 				label="ValidWithIcon"
