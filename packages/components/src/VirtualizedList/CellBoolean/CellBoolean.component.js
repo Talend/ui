13835a46215709c492d55368138687c36ef5d37b
Sébastien Romain <sromain@talend.com>
2020-04-30 07:50:17 +0200

    feat(List): Boolean Cell with Icon (#2803)

--- packages/components/src/VirtualizedList/CellBoolean/CellBoolean.component.js
+++ packages/components/src/VirtualizedList/CellBoolean/CellBoolean.component.js
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import classnames from 'classnames';
 import { withTranslation } from 'react-i18next';
+import Icon from '../../Icon';
 
 import I18N_DOMAIN_COMPONENTS from '../../constants';
 import getDefaultT from '../../translate';
+import { getTheme } from '../../theme';
 
-import Action from '../../Actions/Action';
-import styles from './CellBoolean.scss';
+import theme from './CellBoolean.scss';
+
+const css = getTheme(theme);
+export const DISPLAY_MODE = {
+	TEXT: 'TEXT',
+	ICON: 'ICON',
+};
 
 /**
  * Cell renderer that displays a boolean
@@ -18,13 +24,27 @@ class CellBoolean extends React.Component {
 	}
 
 	render() {
-		const { cellData, t } = this.props;
+		const { cellData, t, columnData } = this.props;
+
+		if (columnData.displayMode === DISPLAY_MODE.ICON) {
+			if (cellData) {
+				return (
+					<div className={css('cell-boolean-container')}>
+						<Icon
+							name="talend-check-circle"
+							title={t('CAD_LIST_MANDATORY_REQUIRED', { defaultValue: 'Required' })}
+						/>
+					</div>
+				);
+			}
+			return null;
+		}
 
 		return (
-			<div className={classnames('cell-boolean-container', styles['cell-boolean-container'])}>
+			<React.Fragment>
 				{cellData === true && t('BOOLEAN_VALUE_TRUE', { defaultValue: 'Yes' })}
 				{cellData === false && t('BOOLEAN_VALUE_FALSE', { defaultValue: 'No' })}
-			</div>
+			</React.Fragment>
 		);
 	}
 }
@@ -34,10 +54,16 @@ CellBoolean.propTypes = {
 	// The cell value : props.rowData[props.dataKey]
 	cellData: PropTypes.bool,
 	t: PropTypes.func,
+	columnData: PropTypes.shape({
+		displayMode: PropTypes.oneOf(Object.values(DISPLAY_MODE)),
+	}).isRequired,
 };
 
 CellBoolean.defaultProps = {
 	t: getDefaultT(),
+	columnData: {
+		displayMode: DISPLAY_MODE.TEXT,
+	},
 };
 
 export default withTranslation(I18N_DOMAIN_COMPONENTS)(CellBoolean);
