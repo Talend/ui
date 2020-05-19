1a76ae9147c5c79b52b4aae62de8872b72cd2a2d
Sébastien Romain <sebastien.romain@gmail.com>
2020-05-19 13:53:54 +0200

    add unit tests

--- packages/forms/src/widgets/templates/FieldTemplate/FieldTemplate.test.js
+++ packages/forms/src/widgets/templates/FieldTemplate/FieldTemplate.test.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import FieldTemplate from './FieldTemplate.component';
 
 describe('FieldTemplate', () => {
 	it('should render the common widget markup', () => {
 		// given
+		const props = {
+			descriptionId: 'test-description',
+			errorId: 'test-error',
+			label: 'Test',
+			description: 'This is the description',
+			error: 'This is the error',
+		};
 		// when
-		// then: a snapshot is sufficient
+		const wrapper = mount(
+			<FieldTemplate {...props}>
+				<input name="test" />
+			</FieldTemplate>,
+		);
+		// then
+		expect(wrapper.html()).toMatchSnapshot();
 	});
 
 	it('should hide description when there is an error message', () => {
 		// given
+		const props = {
+			descriptionId: 'test-description',
+			errorId: 'test-error',
+			label: 'Test',
+			description: 'This is the description',
+			error: 'This is the error',
+		};
 		// when
+		const wrapper = mount(
+			<FieldTemplate {...props}>
+				<input name="test" />
+			</FieldTemplate>,
+		);
 		// then
-		// wrapper.find(descriptionId) has class sr-only
+		const descBlock = wrapper.find('.help-block').at(0);
+		expect(descBlock.text()).toEqual('This is the description');
+		expect(descBlock.getDOMNode().getAttribute('class')).toEqual('help-block sr-only');
+
+		const errorBlock = wrapper.find('.help-block').at(1);
+		expect(errorBlock.text()).toEqual('This is the error');
+		expect(errorBlock.getDOMNode().getAttribute('class')).toEqual('help-block');
 	});
 });
