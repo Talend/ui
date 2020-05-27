ce4e1eee950afbd7c031305434b327b61043541e
Jimmy Somsanith <jimmy.somsanith@gmail.com>
2020-05-27 16:08:49 +0200

    Remove jest config

--- packages/components/jest.config.js
+++
@@ -1,6 +0,0 @@
-const defaults = require('../../node_modules/@talend/scripts-config-jest/jest.config.js');
-
-module.exports = {
-	...defaults,
-	setupFilesAfterEnv: [...defaults.setupFilesAfterEnv, '../../test-setup.js'],
-};
