904efd405526377b8b7991fcfd5c33e35b4de075
Sébastien Romain <sebastien.romain@gmail.com>
2020-06-01 16:26:14 +0200

    beh

---
+++ packages/http/README.md
@@ -0,0 +1,1 @@
+# @talend/http
---
+++ packages/http/src/index.js
@@ -0,0 +1,5 @@
+import { http } from './http.async';
+import { generators } from './http.generators';
+import { setDefaultConfig, setDefaultLanguage } from './http.common';
+
+export { http, generators, setDefaultConfig, setDefaultLanguage };
