9fab63309b7cdbc7184afd91c8a47c885da07c63
Fabien Rassinier <frassinier@talend.com>
2020-06-11 18:19:46 +0200

    feature: design tokens doc

--- src/tokens/docs/DesignTokens.component.js
+++ src/tokens/docs/DesignTokens.component.js
@@ -1,4 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+	table {
+		border: 1px solid;
+		border-collapse: collapse;
+		border-radius: 0.75rem;
+		text-align: left;
+		overflow: hidden;
+	}
+
+	thead {
+		color: white;
+		background: #333;
+	}
+
+	th,
+	td {
+		padding: 1rem 2rem;
+		width: 50%;
+	}
+
+	tbody tr:nth-child(even) {
+		background-color: #efefef99;
+	}
+`;
 
 function DesignTokens({ tokens }) {
 	const designTokens = Object.entries(tokens);
@@ -8,22 +34,24 @@ function DesignTokens({ tokens }) {
 	}
 
 	return (
-		<table>
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Value</th>
-				</tr>
-			</thead>
-			<tbody>
-				{designTokens.map(([name, value], index) => (
-					<tr key={index}>
-						<td>{name}</td>
-						<td>{value}</td>
+		<Div>
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Value</th>
 					</tr>
-				))}
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					{designTokens.map(([name, value], index) => (
+						<tr key={index}>
+							<td>{name}</td>
+							<td>{value}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</Div>
 	);
 }
 
