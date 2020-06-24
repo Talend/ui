e98b088d7f1e186a0e62a7bd0e9a65be12cf0cee
Jimmy Somsanith <jsomsanith@talend.com>
2020-06-24 14:50:12 +0200

    chore: make eslint and demo works (#2867)

--- packages/icons/webpack.config.js
+++ packages/icons/webpack.config.js
@@ -1,29 +1,26 @@
 const path = require('path');
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	mode: "production",
+	mode: 'production',
 	context: path.resolve(__dirname),
 	entry: './index.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.join(__dirname, 'dist')
+		path: path.join(__dirname, 'dist'),
 	},
 	module: {
 		rules: [
 			{
 				test: /\.font\.(js)$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'webfonts-loader',
-				],
-			}, {
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'webfonts-loader'],
+			},
+			{
 				test: /\.(woff|eot|ttf|svg)$/,
-				loader: 'url-loader'
-			}
-		]
+				loader: 'url-loader',
+			},
+		],
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
@@ -32,6 +29,6 @@ module.exports = {
 		}),
 	],
 	node: {
-		fs: 'empty'
-	}
+		fs: 'empty',
+	},
 };
