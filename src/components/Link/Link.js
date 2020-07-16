b61b7488c3358df4589cad5f70fc7508d889f13b
Fabien Rassinier <frassinier@talend.com>
2020-07-16 17:29:16 +0200

    feat: link

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,14 +1,80 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { shade } from 'polished';
 import { Button } from 'reakit';
 import tokens from '../../tokens';
+import Icon from '../Icon/Icon';
 
-const ButtonAsAnchor = styled(Button)`
-	color: ${tokens.colors.lochmara};
-`;
+const ButtonAsAnchor = styled(Button)(
+	({ theme }) => `
+	display: inline-flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${theme.colors.activeColor};
+	
+	&:hover,
+	&:active {
+		.link__text {
+			text-decoration: underline;
+		}
+	}
+	
+	&:hover {
+		color: ${shade(0.2, theme.colors.activeColor)};
 
-function Link({ href = '#', ...rest }) {
-	return <ButtonAsAnchor as="a" href={href} {...rest} />;
+		.link__icon {
+			fill: ${shade(0.2, theme.colors.activeColor)};
+		}
+	}
+	
+	&:active {
+		color: ${shade(0.4, theme.colors.activeColor)};
+
+		.link__icon {
+			fill: ${shade(0.4, theme.colors.activeColor)};
+		}
+	}
+	
+	&.link--disabled {
+		cursor: not-allowed;
+		opacity: ${tokens.opacity.disabled};
+		
+		.link__text {
+			text-decoration: none;
+		}
+	}
+	
+	.link__icon {
+		display: inline-block;
+		margin-right: ${tokens.space.smaller};
+		width: ${tokens.sizes.smallerr};
+		fill: ${theme.colors.activeColor};
+  		
+  		&--external {
+			margin-right: 0;
+			margin-left: ${tokens.space.smaller};
+		}
+	}
+`,
+);
+
+function Link({ children, disabled, href = '#', target, icon, ...rest }) {
+	const isExternal = target?.toLocaleLowerCase().includes('blank');
+	return (
+		<ButtonAsAnchor
+			as="a"
+			className={`link ${disabled ? 'link--disabled' : ''} ${isExternal ? 'link--external' : ''}`}
+			href={!disabled ? href : null}
+			rel={isExternal ? 'noopener noreferrer' : null}
+			target={target}
+			aria-disabled={disabled ? 'true' : null}
+			{...rest}
+		>
+			{icon && <Icon className="link__icon" name={icon} />}
+			<span className="link__text">{children}</span>
+			{isExternal && <Icon className="link__icon link__icon--external" name="link" />}
+		</ButtonAsAnchor>
+	);
 }
 
 export default Link;
--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,11 @@
+import arrowLeft from './arrow-left.svg';
 import caret from './caret.svg';
 import check from './check.svg';
 import cross from './cross.svg';
 import eye from './eye.svg';
 import eyeSlash from './eye-slash.svg';
 import information from './information.svg';
+import link from './link.svg';
 import minus from './minus.svg';
 import plus from './plus.svg';
 import search from './search.svg';
@@ -12,12 +14,14 @@ import validate from './validate.svg';
 import warning from './warning.svg';
 
 export default {
+	arrowLeft,
 	caret,
 	cross,
 	check,
 	eye,
 	eyeSlash,
 	information,
+	link,
 	minus,
 	plus,
 	search,
