0bce973fa8225083be87a9e5100d8294301f9a8e
Fabien Rassinier <frassinier@talend.com>
2020-07-21 09:34:02 +0200

    chore: docs components

---
+++ src/docs/Table.component.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTable = styled.table`
+	border: 1px solid;
+	border-collapse: collapse;
+	border-radius: 0.75rem;
+	text-align: left;
+	overflow: hidden;
+
+	thead {
+		color: white;
+		background: #333;
+	}
+
+	th,
+	td {
+		padding: 1rem 2rem;
+		width: 50%;
+	}
+
+	tbody tr:nth-child(even) {
+		background-color: #efefef99;
+	}
+`;
+
+function Table({ data }) {
+	const entries = Object.entries(data);
+
+	if (!entries.length) {
+		return null;
+	}
+
+	return (
+		<StyledTable>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Value</th>
+				</tr>
+			</thead>
+			<tbody>
+				{entries.map(([name, value], index) => (
+					<tr key={index}>
+						<td>{name}</td>
+						<td>{value}</td>
+					</tr>
+				))}
+			</tbody>
+		</StyledTable>
+	);
+}
+
+export default Table;
--- src/docs/index.js
+++ src/docs/index.js
@@ -1,5 +1,6 @@
 import Col from './Col.component';
+import Table from './Table.component';
 import Row from './Row.component';
 import TableOfContent from './TableOfContent.component';
 
-export { Col, Row, TableOfContent };
+export { Col, Row, Table, TableOfContent };
