90489548ae9c9bb0bf9a52fec1b60d3c3582cb36
Fabien Rassinier <frassinier@talend.com>
2020-07-22 11:00:13 +0200

    doc: fix toc for safari and add scroll-to-top btn

--- .storybook/docs/TableOfContents.tsx
+++ .storybook/docs/TableOfContents.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { Button, VisuallyHidden } from 'reakit';
 
 const Nav = styled.nav`
 	position: fixed;
@@ -37,7 +38,7 @@ const Nav = styled.nav`
 		}
 
 		.toc-list-item {
-			opacity: 0.5;
+			opacity: 0.6;
 
 			&:before {
 				content: none;
@@ -45,6 +46,37 @@ const Nav = styled.nav`
 		}
 	}
 `;
+
+const NavHeader = styled.header`
+	font-weight: bold;
+`;
+
+const ScrollToTopButton = styled(Button)`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 2rem;
+	width: 2rem;
+	position: fixed;
+	right: 5rem;
+	bottom: 2rem;
+	color: white;
+	background: rgba(0, 0, 0, 0.65);
+	border-radius: 50%;
+
+	&:hover {
+		background: rgba(0, 0, 0, 1);
+	}
+`;
+
+function BackToTop() {
+	return (
+		<ScrollToTopButton onClick={() => window.scroll({ top: 0, left: 0, behavior: 'smooth' })}>
+			<span aria-hidden={true}>↑</span>
+			<VisuallyHidden>Back to top</VisuallyHidden>
+		</ScrollToTopButton>
+	);
+}
 
 function TableOfContents() {
 	React.useLayoutEffect(() => {
@@ -55,9 +87,7 @@ function TableOfContents() {
 			headingSelector: 'h2, h3, h4, h5',
 			onClick: (event) => {
 				event.preventDefault();
-				const element = document.getElementById(event.currentTarget.hash.substr(1));
-				element.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
-				element.focus();
+				document.getElementById(event.currentTarget.hash.substr(1)).focus();
 			},
 		});
 		// @ts-ignore
@@ -65,12 +95,11 @@ function TableOfContents() {
 	}, []);
 
 	return (
-		<>
-			<Nav>
-				<strong>Table of content</strong>
-				<div className="js-toc"></div>
-			</Nav>
-		</>
+		<Nav>
+			<NavHeader>Table of contents</NavHeader>
+			<div className="js-toc"></div>
+			<BackToTop />
+		</Nav>
 	);
 }
 
