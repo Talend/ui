cf17f75f9a6d9672c7205d3f56f76f8e14bc2d37
Fabien Rassinier <frassinier@talend.com>
2020-07-22 10:35:30 +0200

    docs: reorg themes doc

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,9 @@
 module.exports = {
 	stories: [
 		'../src/Welcome.stories.mdx',
-		'../src/tokens/docs/Tokens.stories.mdx',
-		'../src/themes/docs/Themes.stories.mdx',
+		'../src/tokens/docs/*.stories.mdx',
+		'../src/themes/docs/Light.stories.mdx',
+		'../src/themes/docs/*.stories.mdx',
 		'../src/**/*.stories.mdx',
 		'../src/**/*.stories.js',
 	],
