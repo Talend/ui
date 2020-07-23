eb42be03ad135245dcbadb5b74a925fd2d865c77
Fabien Rassinier <frassinier@talend.com>
2020-07-23 18:10:23 +0200

    feat: extract tooltip style

---
+++ src/components/Tooltip/Tooltip.style.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import {
+	Tooltip as ReakitTooltip,
+	TooltipArrow as ReakitTooltipArrow,
+	TooltipReference as ReakitTooltipReference,
+} from 'reakit/Tooltip';
+import tokens from '../../tokens';
+
+export const Tooltip = styled(ReakitTooltip)`
+	max-width: 25rem;
+	padding-left: ${tokens.space.smaller};
+	padding-right: ${tokens.space.smaller};
+	font-size: 1.2rem;
+	color: ${tokens.colors.white};
+	background: ${tokens.colors.doveGray};
+	border-radius: ${tokens.radii.rectRadius};
+	z-index: ${tokens.zIndices.tooltips};
+`;
+
+export const TooltipReference = styled(ReakitTooltipReference)`
+	display: inline-block;
+`;
+
+export const TooltipArrow = styled(ReakitTooltipArrow)`
+	svg {
+		fill: ${tokens.colors.doveGray};
+	}
+`;
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,33 +1,7 @@
 import * as React from 'react';
-import styled from 'styled-components';
-import {
-	Tooltip as ReakitTooltip,
-	TooltipArrow,
-	TooltipReference,
-	useTooltipState,
-} from 'reakit/Tooltip';
-import tokens from '../../tokens';
+import { useTooltipState } from 'reakit/Tooltip';
 
-const StyledTooltipReference = styled(TooltipReference)`
-	display: inline-block;
-`;
-
-const StyledTooltipArrow = styled(TooltipArrow)`
-	svg {
-		fill: ${tokens.colors.doveGray};
-	}
-`;
-
-const StyledTooltip = styled(ReakitTooltip)`
-	max-width: 25rem;
-	padding-left: ${tokens.space.smaller};
-	padding-right: ${tokens.space.smaller};
-	font-size: 1.2rem;
-	color: ${tokens.colors.white};
-	background: ${tokens.colors.doveGray};
-	border-radius: ${tokens.radii.rectRadius};
-	z-index: ${tokens.zIndices.tooltips};
-`;
+import * as S from './Tooltip.style';
 
 type Placement =
 	| 'auto-start'
@@ -62,14 +36,14 @@ const Tooltip = React.forwardRef(
 		});
 		return (
 			<>
-				<StyledTooltipReference {...tooltipState} {...children.props} ref={ref}>
-					{(referenceProps) => React.cloneElement(children, referenceProps)}
-				</StyledTooltipReference>
+				<S.TooltipReference {...tooltipState} {...children.props} ref={ref}>
+					{referenceProps => React.cloneElement(children, referenceProps)}
+				</S.TooltipReference>
 				{title && (
-					<StyledTooltip {...tooltipState} {...rest}>
-						<StyledTooltipArrow {...tooltipState} />
+					<S.Tooltip {...tooltipState} {...rest}>
+						<S.TooltipArrow {...tooltipState} />
 						{title}
-					</StyledTooltip>
+					</S.Tooltip>
 				)}
 			</>
 		);
