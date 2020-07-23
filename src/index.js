9079915ff81de278db17bd6c5aaefefc013096ef
Jimmy Somsanith <jsomsanith@talend.com>
2020-07-23 09:59:01 +0200

    chore: build lib and umd (#10)
    * chore: build lib and umd
    
    * chore: let tailwind for preview for now
    
    * fix: make Icon className optional
    
    * fix: InlineMessage withtout Tailwind
    
    * rename umd
    
    Co-authored-by: Jimmy Somsanith <jimmy.somsanith@gmail.com>
    Co-authored-by: Fabien Rassinier <frassinier@talend.com>

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Button from './components/Button';
+import Dropdown from './components/Dropdown';
+import Form from './components/Form';
+import Icon from './components/Icon';
+import InlineMessage from './components/InlineMessage';
+import Link from './components/Link';
+import Loading from './components/Loading';
+import Tooltip from './components/Tooltip';
+
+export { Button, Dropdown, Form, Icon, InlineMessage, Link, Loading, Tooltip };
