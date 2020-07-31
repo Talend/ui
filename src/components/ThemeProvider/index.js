ba58deb0772e8a4b146b5b248db09d0b1b1b95a8
Fabien Rassinier <frassinier@talend.com>
2020-07-31 16:29:25 +0200

    chore: add temp globalStyle

--- src/components/ThemeProvider/index.js
+++ src/components/ThemeProvider/index.js
@@ -1,3 +1,31 @@
-import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+const GlobalStyle = createGlobalStyle(
+	({ theme }) => `
 
-export { ThemeProvider };
+	html {
+		/* 1rem = 10px */
+		font-size: 62.5%;
+	}
+
+	body {
+		margin: 0;
+		padding: 0;
+        font-size: 14px;
+	}
+
+	.sb-show-main,
+	.sbdocs.sbdocs-preview {
+		background: ${theme.colors.backgroundColor};
+	}
+
+	:focus,
+	button:focus {
+		outline: 0.3rem solid ${theme.colors.focusColor};
+	}
+
+	.focus-outline-hidden *:focus {
+		outline: none;
+	}
+`,
+);
+export { ThemeProvider, GlobalStyle };
