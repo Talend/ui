8d0235c1a43fa7fa08cb08c0b6ccac39cd6c953f
Fabien Rassinier <frassinier@talend.com>
2020-09-10 11:37:20 +0200

    chore: Storybook manager style

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,11 +4,11 @@ import logo from './logo.svg';
 export default create({
 	base: 'light',
 
-	colorPrimary: '#7bb2ff',
-	colorSecondary: '#1c44be',
+	colorPrimary: '#19426c',
+	colorSecondary: '#19426c',
 
 	// UI
-	appBg: 'rgba(255,255,255,0.9)',
+	appBg: '#f3f3f3',
 	appContentBg: 'white',
 	appBorderColor: '#C6C6C6',
 	appBorderRadius: 4,
@@ -24,7 +24,7 @@ export default create({
 	// Toolbar default and active colors
 	barTextColor: '#555555',
 	barSelectedColor: '#202020',
-	barBg: '#white',
+	barBg: 'white',
 
 	// Form colors
 	inputBg: 'white',
