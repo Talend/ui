44195a32b2839c75f88af8cdd15d8bcbf518a378
Fabien Rassinier <frassinier@talend.com>
2020-09-11 02:23:54 +0200

    fix: separate sb style from themeprovider

--- src/components/ThemeProvider/index.js
+++ src/components/ThemeProvider/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { normalize } from 'polished';
 
-import light from '../../themes/light.theme';
+import defaultTheme from '../../themes';
 
 const GlobalStyle = createGlobalStyle(
-	({ theme }) => `
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Inconsolata:wght@300;400;500;600;700;800;900');
-    
+	({ theme }) => `  
 	${normalize()};
+
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Inconsolata:wght@300;400;500;600;700;800;900');
 
 	*, ::after, ::before {
 		box-sizing: border-box;
@@ -34,13 +34,24 @@ const GlobalStyle = createGlobalStyle(
 	.focus-outline-hidden *:focus {
 		outline: none;
 	}
+	
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+    
+    a {
+        text-decoration: none;
+	}
 `,
 );
 
-const TalendThemeProvider = ({ theme = light, children }) => {
-	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-};
-
-TalendThemeProvider.GlobalStyle = GlobalStyle;
+const TalendThemeProvider = ({ theme = defaultTheme, children }) => (
+	<ThemeProvider theme={theme}>
+		<GlobalStyle />
+		{children}
+	</ThemeProvider>
+);
 
 export default TalendThemeProvider;
