6816a16ca9bf9bc582999636efc2089e6ba7d6b0
jmfrancois <jmfrancois@talend.com>
2020-09-22 17:30:04 +0200

    feat: add http constants

--- packages/cmf/src/middlewares/http/index.js
+++ packages/cmf/src/middlewares/http/index.js
@@ -1,6 +1,7 @@
 import { httpMiddleware } from './middleware';
 import * as csrf from './csrfHandling';
 import { OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT, HTTP_METHODS } from './constants';
+import * as constants from './constants';
 
 httpMiddleware.OPTIONS = OPTIONS;
 httpMiddleware.GET = GET;
@@ -12,6 +13,7 @@ httpMiddleware.TRACE = TRACE;
 httpMiddleware.CONNECT = CONNECT;
 httpMiddleware.HTTP_METHODS = HTTP_METHODS;
 httpMiddleware.csrf = csrf;
+httpMiddleware.constants = constants;
 
 export default httpMiddleware;
 
