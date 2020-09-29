b0803b5766413cfef3e2c4c5d4088a2bbe4daf83
jmfrancois <jmfrancois@talend.com>
2020-09-29 17:44:04 +0200

    feat: add some compat

--- packages/components/src/IconsProvider/IconsProvider.component.js
+++ packages/components/src/IconsProvider/IconsProvider.component.js
@@ -5,25 +5,9 @@ const DEFAULT_BUNDLES = [
 	'/all.svg',
 ];
 
-const talendIcons = {};
-
-const context = {
-	ids: [],
-	// default no op for testing (case of Icon call without IconsProvider)
-	get: () => { },
-};
-
 // TODO 6.0: do not export this
 export function getIconHREF(name) {
-	if (context.ids.indexOf(name) !== -1) {
-		return `#${name}`;
-	}
-	let href = context.get(name);
-	if (!href) {
-		// backward compatibility for test
-		href = `#${name}`;
-	}
-	return href;
+	return `#${name}`;
 }
 
 const FETCHING = { urls: [] };
@@ -65,11 +49,7 @@ function addBundle(response) {
  * @example
 <IconsProvider />
  */
-function IconsProvider({ bundles = DEFAULT_BUNDLES, defaultIcons = talendIcons, icons = {}, getIconHref = () => { } }) {
-	const iconset = Object.assign({}, defaultIcons, icons);
-	const ids = Object.keys(iconset);
-	context.ids = ids;
-	context.get = getIconHref;
+function IconsProvider({ bundles = DEFAULT_BUNDLES }) {
 	React.useEffect(() => {
 		if (Array.isArray(bundles)) {
 			bundles.filter(url => {
@@ -81,26 +61,14 @@ function IconsProvider({ bundles = DEFAULT_BUNDLES, defaultIcons = talendIcons, 
 				});
 			});
 		}
-	}, []);
-	return (
-		<svg xmlns="http://www.w3.org/2000/svg" focusable="false" className="sr-only">
-			{ids.map((id, index) => (
-				<symbol key={index} id={id}>
-					{iconset[id]}
-				</symbol>
-			))}
-		</svg>
-	);
+	}, [bundles]);
+	return null;
 }
 
 IconsProvider.displayName = 'IconsProvider';
-IconsProvider.context = context;
+IconsProvider.getIconHREF = getIconHREF;
 IconsProvider.propTypes = {
-	defaultIcons: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-	icons: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-	getIconHref: PropTypes.func, // eslint-disable-line react/forbid-prop-types
 	bundles: PropTypes.arrayOf(PropTypes.string),
 };
 
-IconsProvider.getIconHREF = getIconHREF;
 export default IconsProvider;
--- packages/components/src/IconsProvider/IconsProvider.test.js
+++ packages/components/src/IconsProvider/IconsProvider.test.js
@@ -12,25 +12,4 @@ describe('IconsProvider', () => {
 		expect(symbols.filter(symbol => symbol.props().id === 'custom')).not.toBeUndefined();
 	});
 
-	it('should support defaultIcons props', () => {
-		const defaultIcons = {
-			default: <svg id="OverrideDefaultIcon" />,
-		};
-		const wrapper = mount(<IconsProvider defaultIcons={defaultIcons} />);
-		const symbols = wrapper.find('symbol');
-		expect(symbols.length).toBe(1);
-		expect(symbols.filter(symbol => symbol.props().id === 'default')).not.toBeUndefined();
-	});
-
-	it('should support defaultIcons props and render custom icon', () => {
-		const defaultIcons = {
-			default: <svg id="OverrideDefaultIcon" />,
-		};
-		const customIcons = {
-			custom: <svg id="customIcon" />,
-		};
-		const wrapper = mount(<IconsProvider defaultIcons={defaultIcons} icons={customIcons} />);
-		const symbols = wrapper.find('symbol');
-		expect(symbols.length).toBe(2);
-	});
 });
