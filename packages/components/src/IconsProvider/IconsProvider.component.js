32fda6b4edc452e6819ed7b5641cc4108ed860f7
travis <travis>
2020-10-01 14:59:12 +0000

    chore(ci): prettier

--- packages/components/src/IconsProvider/IconsProvider.component.js
+++ packages/components/src/IconsProvider/IconsProvider.component.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { react as talendIcons, info  } from '@talend/icons';
-
+import { react as talendIcons, info } from '@talend/icons';
 
 const context = {
 	ids: [],
