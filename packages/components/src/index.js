0d0f54c23bd9b0a8ee3e26c89c9dadff823d5545
Jimmy Somsanith <jimmy.somsanith@gmail.com>
2020-10-02 14:56:19 +0200

    chore: expose getTheme in index

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -72,8 +72,8 @@ import TooltipTrigger from './TooltipTrigger';
 import TreeView from './TreeView';
 import Typeahead from './Typeahead';
 import VirtualizedList from './VirtualizedList';
-
 import WithDrawer from './WithDrawer';
+import getTheme from './theme';
 
 // TODO 6.0: remove those imports
 import HeaderTitle from './HeaderTitle';
@@ -225,6 +225,7 @@ export {
 	Typeahead,
 	VirtualizedList,
 	WithDrawer,
+	getTheme,
 	// TODO 6.0: remove from here
 	RichError,
 	RichLayout,
