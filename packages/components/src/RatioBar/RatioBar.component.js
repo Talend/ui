49541e90b1c101a8e81894ab595dd5fec5226c97
Sébastien Romain <sebastien.romain@gmail.com>
2020-10-06 08:49:54 +0200

    component

--- packages/components/src/RatioBar/RatioBar.component.js
+++ packages/components/src/RatioBar/RatioBar.component.js
@@ -1,1 +1,25 @@
-export function RatioBar() {}
+import React from 'react';
+import PropTypes from 'prop-types';
+import { RatioBarComposition } from './RatioBarComposition.component';
+import { EmptyLine, FilledLine } from './RatioBarLines.component';
+import { getTheme } from '../theme';
+import qualityBarTheme from './RatioBar.scss';
+
+const theme = getTheme(qualityBarTheme);
+
+export function RatioBar({ amount, total }) {
+	return (
+		<RatioBarComposition>
+			<FilledLine percentage={(amount / total) * 100} value={amount} />
+			<EmptyLine percentage={(1 - amount / total) * 100} value={total - amount} />
+			<div className={theme('tc-ratio-bar-counter')}>
+				{amount}/{total}
+			</div>
+		</RatioBarComposition>
+	);
+}
+
+RatioBar.propTypes = {
+	amount: PropTypes.number.isRequired,
+	total: PropTypes.number.isRequired,
+};
--- packages/components/src/RatioBar/RatioBar.stories.js
+++ packages/components/src/RatioBar/RatioBar.stories.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { RatioBar } from './RatioBar.component';
+
 import { RatioBarComposition } from './RatioBarComposition.component';
-import { FilledLine, EmptyLine } from './RatioBarLines.component';
+import {
+	QualityEmptyLine,
+	QualityInvalidLine,
+	QualityValidLine,
+} from '../QualityBar/QualityRatioBar.component';
 
 const t = () => 'test';
 
@@ -11,9 +17,24 @@ stories
 	.addDecorator(story => <div className="col-lg-offset-2 col-lg-8">{story()}</div>)
 	.add('RatioBar default', () => (
 		<div>
-			<RatioBarComposition>
-				<FilledLine percentage={30} t={t} value={30}></FilledLine>
-				<EmptyLine percentage={70} t={t} value={70}></EmptyLine>
-			</RatioBarComposition>
+			<section>
+				<header>Ratio Bar</header>
+				<div>
+					<RatioBar amount={10} total={12} />
+					<RatioBar amount={0} total={12} />
+					<RatioBar amount={12} total={12} />
+					<RatioBar amount={532} total={1000} />
+				</div>
+			</section>
+			<section>
+				<header>Quality Bar</header>
+				<div>
+					<RatioBarComposition>
+						<QualityEmptyLine value={30} percentage={30} t={t} />
+						<QualityInvalidLine value={40} percentage={40} t={t} />
+						<QualityValidLine value={30} percentage={30} t={t} />
+					</RatioBarComposition>
+				</div>
+			</section>
 		</div>
 	));
