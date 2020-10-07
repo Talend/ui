b5b5608d3eb003c5df5e55e9edfdec018cfff8af
Sébastien Romain <sebastien.romain@gmail.com>
2020-10-07 09:51:45 +0200

    update

--- packages/components/src/RatioBar/RatioBar.component.js
+++ packages/components/src/RatioBar/RatioBar.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Trans } from 'react-i18next';
 import { RatioBarComposition } from './RatioBarComposition.component';
 import { EmptyLine, FilledLine } from './RatioBarLines.component';
 import { getTheme } from '../theme';
@@ -7,14 +8,55 @@ import qualityBarTheme from './RatioBar.scss';
 
 const theme = getTheme(qualityBarTheme);
 
+function getFilledValues(amount, total) {
+	if (!amount || amount < 0) {
+		return { percentage: 0, amount: 0 };
+	}
+
+	if (amount > total) {
+		return { percentage: 100, amount };
+	}
+
+	return { percentage: (amount / total) * 100, amount };
+}
+
+function getEmptyValues(amount, total) {
+	if (!amount || amount < 0) {
+		return { percentage: 100, amount: total };
+	}
+	if (amount > total) {
+		return { percentage: 0, amount: 0 };
+	}
+
+	return { percentage: (1 - amount / total) * 100, amount: total - amount };
+}
+
+function getLabel(amount, total) {
+	if (!amount) {
+		return (
+			<div className={theme('tc-ratio-bar-counter')}>
+				<Trans i18nKey="tui-components:NA">
+					<strong>N</strong>/A
+				</Trans>
+			</div>
+		);
+	}
+	return (
+		<div className={theme('tc-ratio-bar-counter')}>
+			<strong>{amount}</strong>/{total}
+		</div>
+	);
+}
+
 export function RatioBar({ amount, total }) {
+	const filled = getFilledValues(amount, total);
+	const empty = getEmptyValues(amount, total);
+
 	return (
 		<RatioBarComposition>
-			<FilledLine percentage={(amount / total) * 100} value={amount} />
-			<EmptyLine percentage={(1 - amount / total) * 100} value={total - amount} />
-			<div className={theme('tc-ratio-bar-counter')}>
-				{amount}/{total}
-			</div>
+			<FilledLine percentage={filled.percentage} value={filled.amount} />
+			<EmptyLine percentage={empty.percentage} value={empty.amount} />
+			{getLabel(amount, total)}
 		</RatioBarComposition>
 	);
 }
