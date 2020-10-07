53c43add245ae6ab9021a8e59c3fe3ce347170d5
Volodymyr Koval <vladimmirkoval@gmail.com>
2020-10-07 16:11:35 +0300

    feat(TMC-22913/Status): enhancing status component (#2982)
    * change status component according to new design
    
    * chore(ci): prettier
    
    * fix snapshots
    
    * center icon
    
    * move specific color shades
    
    * refactor variables
    
    Co-authored-by: travis <travis>

--- packages/components/src/Status/Status.component.js
+++ packages/components/src/Status/Status.component.js
@@ -105,12 +105,16 @@ export function Status({ status, label, icon, actions, progress }) {
 		css[getbsStyleFromStatus(status)],
 	);
 
+	const labelClassNames = classNames(
+		css['tc-status-label'],
+		'tc-status-label',
+		css[getbsStyleFromStatus(status)],
+	);
+
 	return (
 		<div role="status" className={rootClassnames}>
 			<span className={iconClassnames}>{renderIcon(status, icon, progress)}</span>
-			<span className={classNames(css['tc-status-label'], 'tc-status-label')}>
-				{renderLabel(status, label)}
-			</span>
+			<span className={labelClassNames}>{renderLabel(status, label)}</span>
 			<Actions
 				actions={actions}
 				className={classNames(css['tc-status-actions'], 'tc-status-actions')}
