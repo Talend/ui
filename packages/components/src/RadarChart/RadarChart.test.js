74d6d7188e28d734d1cca1035caf4ec63b2c2d71
Ollie Williams <66147232+olliew-talend@users.noreply.github.com>
2020-10-12 10:23:55 +0200

    feat(components/RadarChart): Interactive RadarChart axis dots (#2986)

--- packages/components/src/RadarChart/RadarChart.test.js
+++ packages/components/src/RadarChart/RadarChart.test.js
@@ -73,6 +73,42 @@ describe('RadarChart', () => {
 		// then
 		expect(props.clickMock).toHaveBeenCalled();
 	});
+	it('should render a chart with clickable dots', () => {
+		const props = {
+			data: [
+				{ axis: 'Validity', A: 4 },
+				{ axis: 'Social curation', A: 3 },
+				{ axis: 'Completeness', A: 2 },
+				{ axis: 'Discoverability', A: 1 },
+				{ axis: 'Usage', A: 1 },
+			],
+			domain: [0, 5],
+			dataKey: 'axis',
+			activeAxis: 2,
+			clickMock: jest.fn(),
+		};
+
+		// when
+		const wrapper = mount(
+			<RadarChart data={props.data} domain={props.domain}>
+				<RadarChart.PolarAngleAxis dataKey={props.dataKey} />
+				<RadarChart.Radar
+					dataKey="A"
+					dot={<RadarChart.DotWithClick activeAxis={2} onClick={props.clickMock} />}
+					fill="#19426c"
+					fillOpacity={0.1}
+					name="Trust score"
+					stroke="#19426c"
+				/>
+			</RadarChart>,
+		);
+
+		// when
+		wrapper.find('.recharts-dot').at(0).simulate('click');
+
+		// then
+		expect(props.clickMock).toHaveBeenCalled();
+	});
 	it('should render a chart with custom dots', () => {
 		const props = {
 			data: [
