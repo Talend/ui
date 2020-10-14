e0b47ad3dd616f7371e8ba737a08f31b281de2a3
Fabien Rassinier <frassinier@talend.com>
2020-10-14 14:11:56 +0200

    feat: use images direclty from Figma

---
+++ src/docs/FigmaImage.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import * as Figma from 'figma-js';
+
+const client = Figma.Client({
+	personalAccessToken: process.env.FIGMA_ACCESS_TOKEN,
+});
+
+const Figure = styled.figure`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+function getMetadata(url) {
+	const parsedUrl = new URL(url);
+	return {
+		projectId: parsedUrl.pathname.split('/')[2],
+		nodeId: parsedUrl.searchParams.get("node-id"), 
+	};
+}
+
+const FigmaImage = ({ src, alt, ...rest }) => {
+	const [data, setData] = React.useState();
+
+	React.useEffect(() => {
+		const { projectId, nodeId } = getMetadata(src); 
+		client
+			.fileImages(projectId, {
+				ids: [nodeId],
+			})
+			.then(({ data }) => setData(data));
+	}, [src]);
+
+	if (!data) {
+		return <span>Fetching data from Figma server...</span>;
+	}
+
+	return Object.values(data.images).map((image, index) => (
+		<Figure key={index}>
+			<img src={image} alt={alt} {...rest} />
+		</Figure>
+	));
+};
+
+export default FigmaImage;
--- src/docs/index.js
+++ src/docs/index.js
@@ -1,9 +1,10 @@
 import Card from './Card.component';
 import Col from './Col.component';
 import ColorSwatches from './ColorSwatches.component';
+import FigmaImage from './FigmaImage';
 import Grid from './Grid.component';
 import Table from './Table.component';
 import Row from './Row.component';
 import Use from './Use.component';
 
-export { Card, Col, ColorSwatches, Grid, Table, Row, Use };
+export { Card, Col, ColorSwatches, FigmaImage, Grid, Table, Row, Use };
