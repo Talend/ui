33afe6d53c6fbb425e67f1cd37d1ed2ad6467fca
jsomsanith-tlnd <jsomsanith@talend.com>
2020-10-15 15:21:50 +0200

    feat(containers): exportrt ComponentForm > fields

--- packages/containers/src/ComponentForm/index.js
+++ packages/containers/src/ComponentForm/index.js
@@ -3,10 +3,12 @@ import sagas from './ComponentForm.sagas';
 import * as ComponentFormSelectors from './ComponentForm.selectors';
 import * as ComponentFormActions from './ComponentForm.actions';
 import kit from './kit';
+import fields from './fields';
 
 ComponentForm.sagas = sagas;
 ComponentForm.selectors = ComponentFormSelectors;
 ComponentForm.actions = ComponentFormActions;
 ComponentForm.kit = kit;
+ComponentForm.fields = fields;
 
 export default ComponentForm;
