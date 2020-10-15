48ef9ed2f78297cc5ba9c4eeb06c3b19ffea61b1
jsomsanith-tlnd <jsomsanith@talend.com>
2020-10-15 16:44:41 +0200

    Expose some missing things

--- packages/containers/src/ComponentForm/index.js
+++ packages/containers/src/ComponentForm/index.js
@@ -1,11 +1,12 @@
 import ComponentForm from './ComponentForm.component';
-import sagas from './ComponentForm.sagas';
+import sagas, * as internalSagas from './ComponentForm.sagas';
 import * as ComponentFormSelectors from './ComponentForm.selectors';
 import * as ComponentFormActions from './ComponentForm.actions';
 import kit from './kit';
 import fields from './fields';
 
 ComponentForm.sagas = sagas;
+ComponentForm.internalSagas = internalSagas;
 ComponentForm.selectors = ComponentFormSelectors;
 ComponentForm.actions = ComponentFormActions;
 ComponentForm.kit = kit;
