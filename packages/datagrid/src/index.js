123830a5ac91375485e910984fd5e239ac6c4017
jsomsanith-tlnd <jsomsanith@talend.com>
2020-10-15 15:52:08 +0200

    chore: expose constantts and quality bar in datagrid

--- packages/datagrid/src/index.js
+++ packages/datagrid/src/index.js
@@ -1,3 +1,8 @@
 import DataGrid from './containers';
+import QualityBar from './components/DefaultHeaderRenderer/QualityBar.component';
+import constants from './constants';
+
+DataGrid.constants = constants;
+DataGrid.QualityBar = QualityBar;
 
 export default DataGrid;
