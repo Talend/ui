4c92519b54e41281132cafa45be3459760135725
Sébastien Romain <sebastien.romain@gmail.com>
2020-10-20 08:00:09 +0200

    import export

--- packages/components/src/RatioBar/index.js
+++ packages/components/src/RatioBar/index.js
@@ -1,3 +1,5 @@
 import { RatioBar } from './RatioBar.component';
+import { RatioBarComposition, RatioBarLine } from './RatioBarComposition.component';
 
 export default RatioBar;
+export { RatioBarComposition, RatioBarLine };
