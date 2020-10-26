acb5208689e54b6299214095396e3af3d7aaf50b
travis <travis>
2020-10-26 13:19:51 +0000

    chore(ci): prettier

--- packages/components/src/DateTimePickers/shared/InputSizer/InputSizer.component.js
+++ packages/components/src/DateTimePickers/shared/InputSizer/InputSizer.component.js
@@ -39,7 +39,7 @@ InputSizer.propTypes = {
 	placeholder: PropTypes.string,
 	inputText: PropTypes.string,
 	children: PropTypes.func.isRequired,
-    minWidth: PropTypes.number,
+	minWidth: PropTypes.number,
 };
 
 export default InputSizer;
