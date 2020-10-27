ac1f7558a5cdebd1eff62a3cedda551da25cc43a
jsomsanith-tlnd <jsomsanith@talend.com>
2020-10-27 17:42:47 +0100

    chore: revert unwanted changes

--- packages/components/src/Slider/index.js
+++ packages/components/src/Slider/index.js
@@ -1,8 +1,3 @@
-import Slider, { SLIDER_MODE } from './Slider.component';
-
-Slider.MODES = SLIDER_MODE;
+import Slider from './Slider.component';
 
 export default Slider;
-
-// TODO 6.0 remove this export it is now attached to the Slider component
-export { SLIDER_MODE };
