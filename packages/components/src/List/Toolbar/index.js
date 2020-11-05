5f7a7032f60c5d640b2d579ea9ef073e946fd198
jmfrancois <jmfrancois@talend.com>
2020-11-05 19:03:22 +0100

    feat: add export of ListToolbar

--- packages/components/src/List/Toolbar/index.js
+++ packages/components/src/List/Toolbar/index.js
@@ -1,3 +1,17 @@
 import Toolbar from './Toolbar.component';
+import ColumnChooserButton from './ColumnChooserButton';
+import DisplayModeToggle from './DisplayModeToggle';
+import ItemsNumber from './ItemsNumber';
+import Label from './Label';
+import Pagination from './Pagination';
+import SelectAll from './SelectAll';
+import SelectSortBy from './SelectSortBy';
 
+Toolbar.Pagination = Pagination;
+Toolbar.ColumnChooserButton = ColumnChooserButton;
+Toolbar.DisplayModeToggle = DisplayModeToggle;
+Toolbar.ItemsNumber = ItemsNumber;
+Toolbar.Label = Label;
+Toolbar.SelectAll = SelectAll;
+Toolbar.SelectSortBy = SelectSortBy;
 export default Toolbar;
