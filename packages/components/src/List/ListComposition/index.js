08b169bdae137741b28ecaaba564958d2853d6ee
Jimmy Somsanith <jimmy.somsanith@gmail.com>
2020-11-10 10:07:02 +0100

    chore: expose List constants

--- packages/components/src/List/ListComposition/index.js
+++ packages/components/src/List/ListComposition/index.js
@@ -8,20 +8,24 @@ import SortBy from './SortBy';
 import TextFilter from './TextFilter';
 import Toolbar from './Toolbar';
 import VList from './VList';
+import SelectAll from '../Toolbar/SelectAll'
 import { sortCollection, useCollectionSort } from './Manager/hooks/useCollectionSort.hook';
 import { filterCollection, useCollectionFilter } from './Manager/hooks/useCollectionFilter.hook';
 import useCollectionSelection from './Manager/hooks/useCollectionSelection.hook';
 import useCollectionActions from './Manager/hooks/useCollectionActions.hook';
+import * as constants from './constants';
 
 export default {
+	constants,
+	ColumnChooser,
 	DisplayMode,
 	ItemsNumber,
 	LazyLoadingList,
 	InfiniteScrollList: LazyLoadingList,
 	Manager,
+	SelectAll,
 	SortBy,
 	TextFilter,
-	ColumnChooser,
 	Toolbar,
 	VList,
 };
