b71e01878b04938e080b270671bc00e5a6c38776
jmfrancois <jmfrancois@talend.com>
2020-11-12 17:00:01 +0100

    fix: inject icon do not use xlinkhref

--- packages/components/src/Icon/Icon.component.js
+++ packages/components/src/Icon/Icon.component.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classnames from 'classnames';
 
 import theme from './Icon.scss';
+import IconsProvider from '../IconsProvider';
 
 const FA_TRANSFORMS = {
 	spin: 'fa-spin',
@@ -71,8 +72,11 @@ function Icon({ className, name, title, transform, onClick, ...props }) {
 		if (ref.current && isRemoteSVG) {
 			// eslint-disable-next-line no-param-reassign
 			ref.current.innerHTML = content;
+		} else if (ref.current && !isRemoteSVG) {
+			IconsProvider.injectIcon(name, ref.current);
 		}
-	}, [isRemoteSVG, ref, content]);
+	}, [isRemoteSVG, ref, content, name]);
+
 	const accessibility = {
 		focusable: 'false', // IE11
 		'aria-hidden': 'true',
@@ -101,9 +105,7 @@ function Icon({ className, name, title, transform, onClick, ...props }) {
 	);
 
 	let iconElement = (
-		<svg name={name} className={classname} ref={ref} {...accessibility} {...props}>
-			{isRemote ? undefined : <use xlinkHref={`#${name}`} />}
-		</svg>
+		<svg name={name} className={classname} ref={ref} {...accessibility} {...props} />
 	);
 	if (isRemote && content && !isRemoteSVG) {
 		const classNames = classnames(theme['tc-icon'], 'tc-icon', className);
