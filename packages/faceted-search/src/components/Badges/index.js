d9f3a514a6ce6544399cbcf750cfdc681d04c680
Sébastien Romain <sromain@talend.com>
2020-11-12 12:04:06 +0100

    chore: remove faceted-search package (#3015)

--- packages/faceted-search/src/components/Badges/index.js
+++
@@ -1,5 +0,0 @@
-import { BadgeText } from './BadgeText';
-import { BadgeSlider } from './BadgeSlider';
-import { BadgeOverlay } from './BadgeOverlay';
-
-export { BadgeText, BadgeSlider, BadgeOverlay };
