5e0aa7f399f68e97e6176289615b8aeccd710872
Fabien Rassinier <frassinier@talend.com>
2020-12-17 21:59:28 +0100

    doc: FigmaLink

--- src/docs/index.js
+++ src/docs/index.js
@@ -2,9 +2,10 @@ import Card from './Card.component';
 import Col from './Col.component';
 import ColorSwatches from './ColorSwatches.component';
 import FigmaImage from './FigmaImage';
+import FigmaLink from './FigmaLink';
 import Grid from './Grid.component';
 import Table from './Table.component';
 import Row from './Row.component';
 import Use from './Use.component';
 
-export { Card, Col, ColorSwatches, FigmaImage, Grid, Table, Row, Use };
+export { Card, Col, ColorSwatches, FigmaImage, FigmaLink, Grid, Table, Row, Use };
