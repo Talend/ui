bd55673ac437d0fefc261eb41cfebe20cbf7c4a8
Steven Gangnant <sgangnant@talend.com>
2020-12-17 16:46:58 +0100

    Use primitive type

--- packages/validation/src/methods.ts
+++ packages/validation/src/methods.ts
@@ -3,43 +3,43 @@ import { NAME, EMAIL, DOMAIN, PHONE } from './regexp';
 /**
  * Check that a given value is a valid first name
  * @param {String} value
- * @returns {Boolean}
+ * @returns {boolean}
  */
-export function validFirstName(value: string): Boolean {
+export function validFirstName(value: string): boolean {
 	return NAME.test(value);
 }
 /**
  * Check that a given value is a valid last name
  * @param {String} value
- * @returns {Boolean}
+ * @returns {boolean}
  */
-export function validLastName(value: string): Boolean {
+export function validLastName(value: string): boolean {
 	return NAME.test(value);
 }
 
 /**
  * Check that a given value is a value email address
  * @param {String} value
- * @returns {Boolean}
+ * @returns {boolean}
  */
-export function validEmail(value: string): Boolean {
+export function validEmail(value: string): boolean {
 	return EMAIL.test(value);
 }
 
 /**
  * Check that a given value is a valid domain
  * @param {String} value
- * @returns {Boolean}
+ * @returns {boolean}
  */
-export function validDomain(value: string): Boolean {
+export function validDomain(value: string): boolean {
 	return DOMAIN.test(value);
 }
 
 /**
  * Check that a given value is a value phone number
  * @param {String} value
- * @returns {Boolean}
+ * @returns {boolean}
  */
-export function validPhone(value: string): Boolean {
+export function validPhone(value: string): boolean {
 	return PHONE.test(value);
 }
