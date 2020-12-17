7c1e5eaaa4e92436115f568725968ff8bc56987c
Fabien Rassinier <frassinier@talend.com>
2020-12-17 11:10:22 +0100

    fix: tokens import

--- src/components/Form/Label/Label.js
+++ src/components/Form/Label/Label.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { tokens } from '../../../tokens';
+import tokens from '../../../tokens';
 
 const StyledLabel = styled.label(
 	({ theme }) => `
