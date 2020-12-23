f30ce041e5792733a20cf7cac79300ac4b7fea1a
jmfrancois <jmfrancois@talend.com>
2020-12-23 11:33:44 +0100

    feat(cmf): expose hook to get context

---
+++ packages/cmf/src/useContext.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactReduxContext } from 'react-redux';
+import { RegistryContext } from '../lib/RegistryProvider';
+
+export function useCMFContext() {
+    const store = React.useContext(ReactReduxContext).store;
+    const registry = React.useContext(RegistryContext);
+    return {
+        store,
+        registry,
+    };
+}
