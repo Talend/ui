22c269346e87925677af662d8a513e5863f17720
Fabien Rassinier <frassinier@talend.com>
2021-01-12 00:35:25 +0100

    feat: textarea style

--- src/components/Form/Field/Textarea/Textarea.style.ts
+++ src/components/Form/Field/Textarea/Textarea.style.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
+import tokens from '../../../../tokens';
 
 export const Textarea = styled.textarea`
 	padding: 1rem;
+	line-height: ${tokens.lineHeights.textarea};
 	min-height: 9.6rem;
 	resize: vertical;
+	white-space: initial;
+	overflow: auto;
 `;
