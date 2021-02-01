15be806621790bc0dbac79d469d4073630c03d38
jlame <jlame@talend.com>
2021-02-01 20:11:18 +0100

    +story

--- packages/components/src/DateTimePickers/LegacyDateTimePickers/LegacyDateTimePicker.stories.js
+++ packages/components/src/DateTimePickers/LegacyDateTimePickers/LegacyDateTimePicker.stories.js
@@ -33,6 +33,19 @@ storiesOf('Deprecated/LegacyDteTimePicker', module).add('Legacy - form mode', ()
 				selectedDateTime={'::00'}
 				formMode
 			/>
+
+			<InputDateTimePicker
+				id="my-date-picker3"
+				name="Datetime"
+				onBlur={action('onBlur')}
+				onChange={action('onChange')}
+				useTime
+				required={false}
+				useSeconds
+				hybridMode
+				selectedDateTime={'2012-12-12'}
+				formMode
+			/>
 		</div>
 	</React.Fragment>
 ));
