22d0c50589d02afa492f9766cedfbdd2571edf18
jsomsanith-tlnd <jsomsanith@talend.com>
2021-02-24 16:01:57 +0100

    export redirect component

--- packages/router-bridge/src/index.js
+++ packages/router-bridge/src/index.js
@@ -1,13 +1,14 @@
-import { history, Switch, Route, Router, Link, useParams, useRouteMatch } from './router';
+import { history, Switch, Route, Router, Link, Redirect, useParams, useRouteMatch } from './router';
 import { push, replace } from './redux';
 
 export {
 	// react router v5 only, otherwise those are empty functions or components
 	history,
-	Switch,
+	Link,
+	Redirect,
 	Route,
 	Router,
-	Link,
+	Switch,
 	useParams,
 	useRouteMatch,
 	// bridge to connected-react-router or cmf-router
--- packages/router-bridge/src/router/index.js
+++ packages/router-bridge/src/router/index.js
@@ -1,8 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies, global-require */
+import React from 'react';
+
 let reactRouterV5;
 let Switch = () => null;
 let Route = () => null;
 let Router = () => null;
 let Link = () => null;
+let Redirect = () => null;
 
 let useParams = () => {};
 let useRouteMatch = () => {};
@@ -18,6 +22,7 @@ try {
 	Switch = reactRouterV5.Switch;
 	Route = reactRouterV5.Route;
 	Link = reactRouterV5.Link;
+	Redirect = reactRouterV5.Redirect;
 	useParams = reactRouterV5.useParams;
 	useRouteMatch = reactRouterV5.useRouteMatch;
 
@@ -32,4 +37,4 @@ try {
 }
 
 //TODO: export all V5 content before the overrides
-export { history, Switch, Route, Router, Link, useParams, useRouteMatch, isV5 };
+export { history, Switch, Route, Router, Link, Redirect, useParams, useRouteMatch, isV5 };
