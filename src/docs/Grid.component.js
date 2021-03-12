09f0a6ef9ee9800587a50ba65e5693d9e1cf0701
Fabien Rassinier <frassinier@talend.com>
2021-03-12 11:47:32 +0100

    doc(grid): reduce gap (#76)
    * chore(babel): revert to previous config
    
    * doc(grid): reduce gap

--- src/docs/Grid.component.js
+++ src/docs/Grid.component.js
@@ -2,16 +2,14 @@ import styled from 'styled-components';
 
 import tokens from '../tokens';
 
-const Grid = styled.div(
-	({ columns = 3 }) => `
+const Grid = styled.div`
 	display: grid;
-    grid-template-columns: repeat(${columns}, minmax(25rem, 1fr));
-    gap: 2rem 10rem;
+	grid-template-columns: repeat(${({ columns = 3 }) => columns}, minmax(25rem, 1fr));
+	gap: 2.5rem 5rem;
 
-    @media only screen and (max-width: ${tokens.breakpoints.m}) {
-        grid-template-columns: 1fr;
-    }
-`,
-);
+	@media only screen and (max-width: ${tokens.breakpoints.m}) {
+		grid-template-columns: 1fr;
+	}
+`;
 
 export default Grid;
