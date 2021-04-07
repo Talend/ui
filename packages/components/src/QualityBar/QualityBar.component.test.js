a906e15fc4907a7405a0817590c94c3244726f07
jlame <jlame@talend.com>
2021-04-07 12:26:16 +0200

    add test

--- packages/components/src/QualityBar/QualityBar.component.test.js
+++ packages/components/src/QualityBar/QualityBar.component.test.js
@@ -4,7 +4,24 @@ import { QualityBar } from './QualityBar.component';
 
 describe('QualityBar', () => {
 	describe('QualityBar component', () => {
-		it('should render an chart', () => {
+		it('should render a chart', () => {
+			// given
+			const props = {
+				valid: 523,
+				invalid: 123,
+				empty: 332,
+			};
+			// when
+			const wrapper = shallow(<QualityBar {...props} />);
+			// then
+			expect(wrapper.find('QualityInvalidLine').props().percentage).toBe(12.6);
+			expect(wrapper.find('QualityInvalidLine').props().value).toBe(123);
+			expect(wrapper.find('QualityEmptyLine').props().percentage).toBe(33.9);
+			expect(wrapper.find('QualityEmptyLine').props().value).toBe(332);
+			expect(wrapper.find('QualityValidLine').props().percentage).toBe(53.5);
+			expect(wrapper.find('QualityValidLine').props().value).toBe(523);
+		});
+		it('should render a chart with non applicable', () => {
 			// given
 			const props = {
 				valid: 523,
