d8d2ea644461a1bd865100341df61347a1a5da9d
Fabien Rassinier <frassinier@talend.com>
2021-04-13 11:56:15 +0200

    fix(ThemeProvider): Open Sans should be Source Sans Pro (#106)

--- src/components/ThemeProvider/index.js
+++ src/components/ThemeProvider/index.js
@@ -18,7 +18,7 @@ const GlobalStyle = createGlobalStyle`
 	body {
 		margin: 0;
 		padding: 0;
-		font-family: 'Open Sans', sans-serif;
+		font-family: 'Source Sans Pro', sans-serif;
 		font-size: 14px;
 		color: ${({ theme }) => theme.colors.textColor};
 		background: ${({ theme }) => theme.colors.backgroundColor};
@@ -46,7 +46,7 @@ const ThemeContext = React.createContext({});
 
 const TalendThemeProvider = ({ theme = defaultTheme, children }) => {
 	useGoogleFont(
-		'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&family=Inconsolata:wght@300;400;500;600;700;800;900&display=swap',
+		'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&family=Inconsolata:wght@300;400;500;600;700;800;900&display=swap',
 	);
 	const [selectedTheme, setSelectedTheme] = useState(theme);
 	React.useEffect(() => {
