54fb6ae4407e2a5a64753a77f721bd4c027e9257
jmfrancois <jmfrancois@talend.com>
2021-05-24 15:30:50 +0200

    chore: refactor to make it a fn

--- packages/forms/src/UIForm/fields/Code/Code.component.js
+++ packages/forms/src/UIForm/fields/Code/Code.component.js
@@ -1,13 +1,25 @@
 import PropTypes from 'prop-types';
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { withTranslation } from 'react-i18next';
 import keyCode from 'keycode';
-import { importFromCDN } from '@talend/react-components';
+import { importFromCDN, Skeleton } from '@talend/react-components';
 import FieldTemplate from '../FieldTemplate';
 
 import { generateId, generateDescriptionId, generateErrorId } from '../../Message/generateId';
 import getDefaultT from '../../../translate';
 import { I18N_DOMAIN_FORMS } from '../../../constants';
+
+function CodeSkeleton() {
+	return (
+		<div>
+			<Skeleton type={Skeleton.TYPES.text} size={Skeleton.SIZES.small} />
+			<Skeleton type={Skeleton.TYPES.text} size={Skeleton.SIZES.large} />
+			<Skeleton type={Skeleton.TYPES.text} size={Skeleton.SIZES.large} />
+			<Skeleton type={Skeleton.TYPES.text} size={Skeleton.SIZES.large} />
+			<Skeleton type={Skeleton.TYPES.button} />
+		</div>
+	);
+}
 
 const AceEditor = lazy(() =>
 	importFromCDN({
@@ -24,133 +36,110 @@ const DEFAULT_SET_OPTIONS = {
 	enableSnippets: true,
 };
 
-// eslint-disable-next-line react/no-multi-comp
-class Code extends React.Component {
-	constructor(props) {
-		super(props);
-		this.onChange = this.onChange.bind(this);
-		this.onFinish = this.onFinish.bind(this);
-		this.onLoad = this.onLoad.bind(this);
-		this.onBlur = this.onBlur.bind(this);
-		this.onKeyDown = this.onKeyDown.bind(this);
+function Code(props) {
+	const { id, isValid, errorMessage, schema, value, valueIsUpdating, t } = props;
+	const { autoFocus, description, options, readOnly = false, title } = schema;
+	const descriptionId = generateDescriptionId(id);
+	const errorId = generateErrorId(id);
+	const instructionsId = generateId(id, 'instructions');
+	const containerRef = React.useRef(null);
+	const [editor, setEditor] = React.useState(null);
 
-		// this hold the last time the ESC is pressed to offer an escape solution from keyboard trap
-		this.lastEsc = null;
-	}
-
-	componentDidUpdate() {
-		this.attachTextareaAttributes();
-	}
-
-	onChange(value, event) {
-		this.props.onChange(event, { schema: this.props.schema, value });
-	}
-
-	onFinish(event) {
-		this.props.onFinish(event, { schema: this.props.schema });
-	}
-
-	onKeyDown(event) {
-		if (event.keyCode === keyCode.codes.esc) {
-			const now = Date.now();
-			if (this.lastEsc && this.lastEsc - now < 1000) {
-				this.lastEsc = null;
-				this.ref.focus();
-				this.editor.textInput.getElement().setAttribute('tabindex', -1);
-			} else {
-				this.lastEsc = now;
-			}
-		} else {
-			this.lastEsc = null;
-		}
-	}
-
-	onBlur() {
-		this.editor.textInput.getElement().removeAttribute('tabindex');
-	}
-
-	onLoad(editor) {
-		this.editor = editor;
-		this.attachTextareaAttributes();
-	}
-
-	attachTextareaAttributes() {
-		if (this.editor) {
-			const textarea = this.editor.textInput.getElement();
-			textarea.setAttribute('id', this.props.id);
+	useEffect(() => {
+		if (editor) {
+			const textarea = editor.textInput.getElement();
+			textarea.setAttribute('id', id);
 			textarea.setAttribute(
 				'aria-describedby',
-				`${this.ids.instructionsId} ${this.ids.descriptionId} ${this.ids.errorId}`,
+				`${instructionsId} ${descriptionId} ${errorId}`,
 			);
+		}
+	}, [editor, instructionsId, descriptionId, errorId, id]);
+
+	function onChange(value, event) {
+		props.onChange(event, { schema: props.schema, value });
+	}
+
+	function onFinish(event) {
+		props.onFinish(event, { schema: props.schema });
+	}
+
+	function onKeyDown(event) {
+		if (event.keyCode === keyCode.codes.esc) {
+			const now = Date.now();
+
+			if (containerRef.current.lastEsc && containerRef.current.lastEsc - now < 1000) {
+				containerRef.current.lastEsc = null;
+				containerRef.current.focus();
+				editor.textInput.getElement().setAttribute('tabindex', -1);
+			} else {
+				containerRef.current.lastEsc = now;
+			}
+		} else {
+			containerRef.current.lastEsc = null;
 		}
 	}
 
-	render() {
-		const { id, isValid, errorMessage, schema, value, valueIsUpdating, t } = this.props;
-		const { autoFocus, description, options, readOnly = false, title } = schema;
-		const descriptionId = generateDescriptionId(id);
-		const errorId = generateErrorId(id);
-		const instructionsId = generateId(id, 'instructions');
-		this.ids = {
-			descriptionId,
-			errorId,
-			instructionsId,
-		};
-
-		return (
-			<FieldTemplate
-				description={description}
-				descriptionId={descriptionId}
-				errorId={errorId}
-				errorMessage={errorMessage}
-				id={id}
-				isValid={isValid}
-				label={title}
-				required={schema.required}
-				valueIsUpdating={valueIsUpdating}
-			>
-				<div // eslint-disable-line jsx-a11y/no-static-element-interactions
-					id={id && `${id}-editor-container`}
-					onBlur={this.onBlur}
-					onKeyDown={this.onKeyDown}
-					ref={ref => {
-						this.ref = ref;
-					}}
-					tabIndex="-1"
-				>
-					<div id={instructionsId} className="sr-only">
-						{t('TF_CODE_ESCAPE', {
-							defaultValue: 'To focus out of the editor, press ESC key twice.',
-						})}
-					</div>
-					<Suspense fallback={<div>Loading...</div>}>
-						<AceEditor
-							key="ace"
-							className="tf-widget-code form-control"
-							editorProps={{ $blockScrolling: Infinity }} // https://github.com/securingsincity/react-ace/issues/29
-							focus={autoFocus}
-							name={`${id}_wrapper`}
-							mode={options && options.language}
-							onBlur={this.onFinish}
-							onLoad={this.onLoad}
-							onChange={this.onChange}
-							// disabled is not supported by ace use readonly
-							// https://github.com/ajaxorg/ace/issues/406
-							readOnly={readOnly || schema.disabled || valueIsUpdating}
-							setOptions={DEFAULT_SET_OPTIONS}
-							showGutter={false}
-							showPrintMargin={false}
-							theme="chrome"
-							value={value}
-							width="auto"
-							{...options}
-						/>
-					</Suspense>
-				</div>
-			</FieldTemplate>
-		);
+	function onBlur() {
+		editor.textInput.getElement().removeAttribute('tabindex');
 	}
+
+	function onLoad(ace) {
+		setEditor(ace);
+	}
+
+	return (
+		<FieldTemplate
+			description={description}
+			descriptionId={descriptionId}
+			errorId={errorId}
+			errorMessage={errorMessage}
+			id={id}
+			isValid={isValid}
+			label={title}
+			required={schema.required}
+			valueIsUpdating={valueIsUpdating}
+		>
+			<div // eslint-disable-line jsx-a11y/no-static-element-interactions
+				id={id && `${id}-editor-container`}
+				onBlur={onBlur}
+				onKeyDown={onKeyDown}
+				ref={containerRef}
+				tabIndex="-1"
+			>
+				<div id={instructionsId} className="sr-only">
+					{t('TF_CODE_ESCAPE', {
+						defaultValue: 'To focus out of the editor, press ESC key twice.',
+					})}
+				</div>
+				<Suspense fallback={<CodeSkeleton />}>
+					<AceEditor
+						key="ace"
+						className="tf-widget-code form-control"
+						editorProps={{ $blockScrolling: Infinity }} // https://github.com/securingsincity/react-ace/issues/29
+						focus={autoFocus}
+						name={`${id}_wrapper`}
+						mode={options && options.language}
+						onBlur={onFinish}
+						onLoad={onLoad}
+						onChange={onChange}
+						// disabled is not supported by ace use readonly
+						// https://github.com/ajaxorg/ace/issues/406
+						readOnly={readOnly || schema.disabled || valueIsUpdating}
+						setOptions={DEFAULT_SET_OPTIONS}
+						showGutter={false}
+						showPrintMargin={false}
+						theme="chrome"
+						value={value}
+						width="auto"
+						{...options}
+					/>
+				</Suspense>
+			</div>
+		</FieldTemplate>
+	);
 }
+
 if (process.env.NODE_ENV !== 'production') {
 	Code.propTypes = {
 		id: PropTypes.string,
