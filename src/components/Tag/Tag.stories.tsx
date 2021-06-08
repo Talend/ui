aa79f526402985355fec9cc14e3abdd85c5ac1a3
Fabien Rassinier <frassinier@talend.com>
2021-06-08 15:08:14 +0200

    doc(Tag): stories

---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Tag from './Tag';
+
+export const Template = ({ variant, ...rest }) => {
+	const Component = Tag[variant] || Tag;
+	Component.displayName = variant ? `Tag.${variant}` : 'Tag';
+	return <Component {...rest} />;
+};
+
+const defaultProps = {
+	children: 'Example',
+};
+
+export const Default = Template.bind({});
+Default.args = defaultProps;
+
+export const Information = Template.bind({});
+Information.args = {
+	...defaultProps,
+	variant: 'Information',
+};
+
+export const Success = Template.bind({});
+Success.args = { ...defaultProps, variant: 'Success' };
+
+export const Warning = Template.bind({});
+Warning.args = { ...defaultProps, variant: 'Warning' };
+
+export const Destructive = Template.bind({});
+Destructive.args = { ...defaultProps, variant: 'Destructive' };
+
+export const Ellipsis = Template.bind({});
+Ellipsis.args = {
+	...defaultProps,
+	children: 'Lorem ipsum dolor sit amet',
+};
