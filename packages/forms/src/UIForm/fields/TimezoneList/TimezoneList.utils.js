7e5b4d1f006517d72be858ea77b2826467bcb453
Steven Gangnant <sgangnant@talend.com>
2021-06-09 13:39:54 +0200

    Use utils

--- packages/forms/src/UIForm/fields/TimezoneList/TimezoneList.utils.js
+++ packages/forms/src/UIForm/fields/TimezoneList/TimezoneList.utils.js
@@ -1,67 +1,11 @@
 import get from 'lodash/get';
-
 import frTimezones from 'cldr-dates-full/main/fr/timeZoneNames.json';
 import enTimezones from 'cldr-dates-full/main/en/timeZoneNames.json';
 import jaTimezones from 'cldr-dates-full/main/ja/timeZoneNames.json';
 import deTimezones from 'cldr-dates-full/main/de/timeZoneNames.json';
+import { date } from '@talend/utils';
 
 const TIMEZONES = { fr: frTimezones, en: enTimezones, ja: jaTimezones, de: deTimezones };
-
-/**
- * Get the offset between a timezone and the UTC time (in minutes)
- * @param {String} timeZone Timezone IANA name
- * @returns {Number}
- * 
- * @see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
- */
-export function getTimezoneUTCOffset(timeZone) {
-	// Build localized formats for UTC and the target timezone
-	const formatOptions = {
-		hourCycle: 'h23',
-		year: 'numeric',
-		month: 'numeric',
-		day: 'numeric',
-		hour: 'numeric',
-		minute: 'numeric'
-	};
-
-	const locale = 'en-US';
-	const utcFormat = new Intl.DateTimeFormat(locale, { ...formatOptions, timeZone: 'Etc/UTC' });
-	const timezoneFormat = new Intl.DateTimeFormat(locale, { ...formatOptions, timeZone });
-
-	// Create the same date in UTC timezone and the target timezone
-	const date = new Date();
-	const utcDate = new Date(utcFormat.format(date));
-	const timezoneDate = new Date(timezoneFormat.format(date));
-
-	// Compute delta between dates
-	return (timezoneDate - utcDate) / (1000 * 60);
-}
-
-/**
- * Ensure a numeric value is expressed on two digits (01, 02, 10 ...)
- * @param {Number} value
- * @returns {String}
- */
-function padTwoDigits(value) {
-	return value > 9 ? value : `0${value}`;
-}
-
-/**
- * Format an UTC offset from minutes to [+/-][HH][separator][mm]
- * @param {Number} offset UTC offset
- * @param {String} separator Separator between hours and minutes
- * @returns {String} Formatted UTC offset
- */
-function formatUtcOffset(offset, separator) {
-	const sign = offset >= 0 ? '+' : '-';
-
-	const absoluteOffset = Math.abs(offset);
-	const min = absoluteOffset % 60;
-	const hours = (absoluteOffset - min) / 60;
-
-	return `${sign}${padTwoDigits(hours)}${separator}${padTwoDigits(min)}`;
-}
 
 /**
  * Get the sorted list of timezones in a given language.
@@ -80,8 +24,8 @@ export function getTimezones(lang, valueType = 'string') {
 	 */
 	const getTimezoneInfo = timezone => {
 		const timezoneName = get(zones, `${timezone.replaceAll('/', '.')}.exemplarCity`, timezone);
-		const offset = getTimezoneUTCOffset(timezone);
-		const name = `(${formatUtcOffset(offset, ':')}) ${timezoneName}`;
+		const offset = date.getTimezoneUTCOffset(timezone);
+		const name = `(${date.formatUtcOffset(offset, ':')}) ${timezoneName}`;
 
 		const timezoneInfo = {
 			timezone,
