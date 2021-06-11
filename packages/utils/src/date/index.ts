bb93661c490eaeab7862a22243b93a0f9c0b2932
Steven Gangnant <38908846+sgangnant@users.noreply.github.com>
2021-06-11 11:39:07 +0200

    feat(utils/date): Expose getUTCOffset (#3329)

--- packages/utils/src/date/index.ts
+++ packages/utils/src/date/index.ts
@@ -14,10 +14,9 @@ interface ConversionOptions {
  *
  * @see https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
  */
-function getUTCOffset(timeZone: string): number {
+export function getUTCOffset(timeZone: string): number {
 	// Build localized formats for UTC and the target timezone
 	const formatOptions: Intl.DateTimeFormatOptions = {
-		hourCycle: 'h23',
 		year: 'numeric',
 		month: 'numeric',
 		day: 'numeric',
@@ -44,7 +43,7 @@ function getUTCOffset(timeZone: string): number {
  * @param {string} separator Separator between hours and minutes
  * @returns {string} Formatted UTC offset
  */
-function formatUtcOffset(offset: number, separator: string): string {
+function formatUTCOffset(offset: number, separator: string): string {
 	const sign = offset >= 0 ? '+' : '-';
 
 	const absoluteOffset = Math.abs(offset);
@@ -71,7 +70,7 @@ function formatTimeZoneTokens(dateFormat: string, timeZone: string): string {
 	return dateFormat.replace(/z|ZZ?/g, match => {
 		const offset = getUTCOffset(timeZone);
 		const separator = match === 'Z' ? ':' : '';
-		return formatUtcOffset(offset, separator);
+		return formatUTCOffset(offset, separator);
 	});
 }
 
@@ -158,4 +157,11 @@ export function timeZoneExists(timeZone: string): boolean {
 	}
 }
 
-export default { formatToTimeZone, convertToLocalTime, convertToTimeZone, convertToUTC, timeZoneExists };
+export default {
+	convertToLocalTime,
+	convertToTimeZone,
+	convertToUTC,
+	formatToTimeZone,
+	getUTCOffset,
+	timeZoneExists,
+};
