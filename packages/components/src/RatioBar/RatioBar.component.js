7dec167450ff354e804ef3368e1465a4722f624f
Nicolas Comont <ncomont@talend.com>
2021-06-18 14:18:10 +0200

    feat(components/ratiobar): add errors count

--- packages/components/src/RatioBar/RatioBar.component.js
+++ packages/components/src/RatioBar/RatioBar.component.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Trans } from 'react-i18next';
 import { RatioBarComposition } from './RatioBarComposition.component';
-import { EmptyLine, FilledLine } from './RatioBarLines.component';
+import { EmptyLine, FilledLine, ErrorLine } from './RatioBarLines.component';
 import { getTheme } from '../theme';
 import ratioBarTheme from './RatioBar.scss';
 
 const theme = getTheme(ratioBarTheme);
 
-function getFilledValues(amount, total) {
+function getFilledValues(amount, total, errors) {
 	if (!amount || amount < 0) {
 		return { percentage: 0, amount: 0 };
 	}
@@ -18,6 +18,17 @@ function getFilledValues(amount, total) {
 	}
 
 	return { percentage: (amount / total) * 100, amount };
+}
+function getErrorValues(amount, total, errors) {
+	if (!errors || errors < 0) {
+		return { percentage: 0, amount: 0 };
+	}
+
+	if (errors > total) {
+		return { percentage: 100, amount };
+	}
+
+	return { percentage: (errors / total) * 100, amount };
 }
 
 function getEmptyValues(amount, total) {
@@ -31,7 +42,7 @@ function getEmptyValues(amount, total) {
 	return { percentage: (1 - amount / total) * 100, amount: total - amount };
 }
 
-function getLabel(amount, total) {
+function getLabel(amount, errors, total) {
 	if (!amount && amount !== 0) {
 		return (
 			<div className={theme('tc-ratio-bar-counter')}>
@@ -43,26 +54,29 @@ function getLabel(amount, total) {
 	}
 	return (
 		<div className={theme('tc-ratio-bar-counter')}>
-			<strong>{amount}</strong>/{total}
+			<strong>{amount + errors}</strong>/{total}
 		</div>
 	);
 }
 
-export function RatioBar({ amount, total, hideLabel = false }) {
+export function RatioBar({ amount, total, errors = 0, hideLabel = false }) {
 	const filled = getFilledValues(amount, total);
-	const empty = getEmptyValues(amount, total);
+	const error = getFilledValues(errors, total);
+	const empty = getEmptyValues(amount + errors, total);
 
 	return (
 		<RatioBarComposition>
 			<FilledLine percentage={filled.percentage} value={filled.amount} />
+			<ErrorLine percentage={error.percentage} value={error.amount} />
 			<EmptyLine percentage={empty.percentage} value={empty.amount} />
-			{!hideLabel && getLabel(amount, total)}
+			{!hideLabel && getLabel(amount, errors, total)}
 		</RatioBarComposition>
 	);
 }
 
 RatioBar.propTypes = {
 	amount: PropTypes.number,
+	errors: PropTypes.number,
 	total: PropTypes.number.isRequired,
 	hideLabel: PropTypes.bool,
 };
