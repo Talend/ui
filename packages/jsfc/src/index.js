fe4af8c5ddef639e869f3e3e867b451aa9fbfd52
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2021-08-20 18:02:39 +0200

    chore(ARCH-364): use changeset to release (#3328)
    * update to lerna 4
    * Switch to lerna in independant mode.
    * Install changeset
    * Update cache in gh actions
    
    Side effects:
    
    * Rewire tsconfig, eslint
    * Align package.json scripts
    * Align jest and typescript dependendencies
    * Update some test snapshots (dataviz from components update)
    * Update jsfc to fit in ui-scripts and integrate the result in forms (will need a release)

--- packages/jsfc/src/index.js
+++ packages/jsfc/src/index.js
@@ -1,7 +1,7 @@
+import tv4index from 'tv4';
 import * as schemaDefaultsImp from './lib/schema-defaults';
 import * as sfPathImp from './lib/sf-path';
 import canonicalTitleMapImp from './lib/canonical-title-map';
-
 export { merge } from './lib/merge';
 export { select } from './lib/select';
 export { jsonref } from './lib/resolve';
@@ -11,3 +11,4 @@ export { validate } from './lib/validate';
 export const sfPath = sfPathImp;
 export const schemaDefaults = schemaDefaultsImp;
 export const canonicalTitleMap = canonicalTitleMapImp;
+export const tv4 = tv4index;
