1500ef9c10c9618a5275a7baedd43507bf9632d3
Artem <akaseka@talend.com>
2021-10-01 10:17:43 +0300

    add a condition for props

--- packages/forms/src/UIForm/fields/MultiSelectTag/MultiSelectTag.component.js
+++ packages/forms/src/UIForm/fields/MultiSelectTag/MultiSelectTag.component.js
@@ -244,7 +244,7 @@ export default class MultiSelectTag extends React.Component {
 				<div className={`${theme.wrapper} form-control`}>
 					{this.props.value.map((val, index) => {
 						const label = getLabel(this.getTitleMap(), val, names[index]);
-						const badgeProps = { label, key: index, dataTest: `${schema.dataTest || 'multiselectTag'}.${index}` };
+						const badgeProps = { label, key: index, dataTest: schema.dataTest && `${schema.dataTest}.${index}`};
 						if (!schema.readOnly && !schema.disabled) {
 							badgeProps.onDelete = event => this.onRemoveTag(event, index);
 						}
