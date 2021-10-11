a6f84be552991f53f1d9b34249b3298d8f1af827
jmfrancois <jmfrancois@talend.com>
2021-10-11 09:16:45 +0200

    chore: align deps and integrate test:demo

--- packages/faceted-search/.storybook/main.js
+++ packages/faceted-search/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', 'storybook-addon-i18next'],
+	stories: ['../stories/**/*.stories.js'],
+	addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
--- packages/faceted-search/.storybook/preview.js
+++ packages/faceted-search/.storybook/preview.js
@@ -1,32 +1,20 @@
-import React from "react";
-import { addDecorator } from "@storybook/react";
-import { withI18next } from "storybook-addon-i18next";
-import { locales } from "@talend/locales-tui-faceted-search/locales";
-import IconProvider from "@talend/react-components/lib/IconsProvider";
-
-import i18n from "./i18n";
-
-const languages = {};
-Object.keys(locales).forEach((key) => (languages[key] = key));
-console.log(locales);
-addDecorator(
-  withI18next({
-    i18n,
-    languages,
-  })
-);
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import IconProvider from '@talend/react-components/lib/IconsProvider';
 
 addDecorator(storyFn => (
-  <div
-    style={{
-      height: '100%',
-      width: '100%',
-      overflow: 'auto',
-      padding: '3rem',
-      backgroundColor: 'rgba(145, 209, 237, 0.1)',
-    }}
-  >
-    <IconProvider bundles={['https://statics-dev.cloud.talend.com/@talend/icons/6.4.0/dist/svg-bundle/all.svg']}/>
-    {storyFn()}
-  </div>
+	<div
+		style={{
+			height: '100%',
+			width: '100%',
+			overflow: 'auto',
+			padding: '3rem',
+			backgroundColor: 'rgba(145, 209, 237, 0.1)',
+		}}
+	>
+		<IconProvider
+			bundles={['https://statics-dev.cloud.talend.com/@talend/icons/6.4.0/dist/svg-bundle/all.svg']}
+		/>
+		{storyFn()}
+	</div>
 ));
