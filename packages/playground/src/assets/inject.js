4de9ab0f1f26a88096f6b9d96c6f412b375eadcf
jmfrancois <jmfrancois@talend.com>
2021-10-20 14:39:33 +0200

    fix: init playground

--- packages/playground/src/assets/inject.js
+++ packages/playground/src/assets/inject.js
@@ -1,25 +1,28 @@
 // custom inject to UMD from surge and the others from unpkg
 
-const base = document.querySelector('base');
-
-const preparebaseUrl = url => (base ? url.slice(1) : url);
-
-function prepareUrl(builtUrl) {
-	const url = preparebaseUrl(builtUrl);
-
-	if (url.startsWith('/cdn')) {
-		if (url.startsWith('/cdn/@talend')) {
-			let newUrl = url.split('/');
-			newUrl.splice(4, 1); // remove version
-			newUrl = newUrl.join('/').replace('/cdn', document.baseURI);
-			// newUrl = newUrl.replace(window.location.origin, 'http://3428.talend.surge.sh');
-			console.log(`playground inject debug: replace ${url} by  ${newUrl}`);
-			return newUrl;
+(function () {
+	function prepareUrl(url) {
+		let newUrl;
+		if (url.startsWith('/cdn')) {
+			if (url.startsWith('/cdn/@talend')) {
+				// code specific target to surge
+				newUrl = url.split('/');
+				newUrl.splice(4, 1); // remove version
+				newUrl = newUrl.join('/').replace('/cdn', '');
+				// newUrl = newUrl.replace(window.location.origin, 'http://3428.talend.surge.sh');
+			} else {
+				newUrl = url.replace('/cdn', 'https://statics.cloud.talend.com');
+			}
+		} else if (url.startsWith('/')) {
+			// basename is added by ui-scripts
+			newUrl = url.replace('/', `${window.basename}/`);
 		}
-		return url.replace('/cdn', 'https://statics.cloud.talend.com');
+		if (newUrl) {
+			console.log(`playground inject debug: replace ${url} by  ${newUrl}`);
+		}
+		return newUrl || url;
 	}
-	return url;
-}
 
-window.talendAddStyles(window.cssFiles, prepareUrl);
-window.talendAddScripts(window.jsFiles, prepareUrl);
+	window.talendAddStyles(window.cssFiles, prepareUrl);
+	window.talendAddScripts(window.jsFiles, prepareUrl);
+})();
