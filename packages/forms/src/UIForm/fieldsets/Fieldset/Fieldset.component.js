0707e402d59ff49660b91c5df9731dce168836f3
gjou-tlnd <gjouannic@talend.com>
2021-10-25 11:25:27 +0200

    fix(forms): do not render empty fieldsets

--- packages/forms/src/UIForm/fieldsets/Fieldset/Fieldset.component.js
+++ packages/forms/src/UIForm/fieldsets/Fieldset/Fieldset.component.js
@@ -2,19 +2,24 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classnames from 'classnames';
 import Widget from '../../Widget';
+import shouldRender from '../../utils/condition';
 
 export default function Fieldset(props) {
 	const { schema, ...restProps } = props;
 	const { title, items, options } = schema;
 
-	return items.length ? (
+	const widgets = items.map((itemSchema, index) =>
+		shouldRender(itemSchema.condition, itemSchema.properties, index) ? (
+			<Widget {...restProps} key={index} schema={itemSchema} />
+		) : null,
+	);
+
+	return widgets.some(React.isValidElement) ? (
 		<fieldset className="form-group">
 			{title && (
 				<legend className={classnames({ 'sr-only': options && options.hideTitle })}>{title}</legend>
 			)}
-			{items.map((itemSchema, index) => (
-				<Widget {...restProps} key={index} schema={itemSchema} />
-			))}
+			{widgets}
 		</fieldset>
 	) : null;
 }
