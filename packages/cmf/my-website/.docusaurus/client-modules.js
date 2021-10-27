5567dd3cace350300718627a55ca9dd6402c4b10
jmfrancois <jmfrancois@talend.com>
2021-10-27 16:28:09 +0200

    chore: remove not used stuff

--- packages/cmf/my-website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/jmfrancois/github/talend/ui/packages/cmf/my-website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jmfrancois/github/talend/ui/packages/cmf/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jmfrancois/github/talend/ui/packages/cmf/my-website/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/jmfrancois/github/talend/ui/packages/cmf/my-website/src/css/custom.css"),
-];
