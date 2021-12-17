48dfe89bcd6ed6a8bc26d1c7cdcb0073afb2fd57
Benrajalu <rajalubenoit@gmail.com>
2021-12-17 10:27:30 +0100

    feat(Stack): Stack outline behavior

--- packages/design-system/src/components/Stack/Stack.tsx
+++ packages/design-system/src/components/Stack/Stack.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './Stack.module.scss';
 
-type StackProps = {
-    as?: 'div' | 'ul' | 'ol';
-    children: React.ReactElement;
+export const justifyOptions = {
+    start: 'justify-start',
+    end: 'justify-end',
+    center: 'justify-center',
+    spaceBetween: 'justify-space-between',
+    spaceAround: 'justify-space-around',
+    spaceEvenly: 'justify-space-evenly',
 };
 
-function Stack({ as = 'div', children}: StackProps){
+export type StackProps = {
+    as?: 'div' | 'ul' | 'ol';
+    children: React.ReactElement;
+    justify?: keyof typeof justifyOptions;
+};
+
+function Stack({ as = 'div', children, justify = 'start'}: StackProps){
     const TagType = as;
     const childrenArray = React.Children.toArray(children);
     return (
-        <TagType>
+        <TagType className={classnames(
+            styles.stack,
+            styles[justifyOptions[justify]]
+        )}
+        >
             {childrenArray.map(child => (
                 <div key={child.key}>
                     {React.cloneElement(child)}
