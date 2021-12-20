aa5223cf38676689f1d7a9581e537ad85cff7a95
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2021-12-20 15:57:16 +0100

    chore(ARCH-404/CI): decrease time needed (#3564)
    what has been done:
    
    * add option to choose if executed in // in workspace script
    * rewrite size check to commit a file (keeping threshold to 100 bytes) (Minus 10 minutes on test ci)
    * move playground outside of test:demo so pre-release and test:demo can be run in // (minus 5 minutes on pr-demo)
    * enforce cache on sub node_modules which contains the .cache for webpack (should decrease by two) so test:demo took 5 minutes !
    
    So we have almost divided it by 2

---
+++ size.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const run = require('./run');
+const originalSize = require('./size.json');
+
+run({ name: 'yarn', args: ['workspaces', '--silent', 'info'] })
+	.then(info => JSON.parse(info))
+	.then(workspaceInfo => {
+		let changed = false;
+		const sizes = Object.values(workspaceInfo).reduce((acc, value) => {
+			const dist = path.join(value.location, 'dist');
+			if (fs.existsSync(path.join(value.location, 'dist'))) {
+				fs.readdirSync(dist)
+					.filter(f => f.endsWith('.json') || f.endsWith('.js') || f.endsWith('.css'))
+					.forEach(f => {
+						const filePath = path.join(dist, f);
+						const size = fs.statSync(filePath).size;
+						const diff = Math.abs(originalSize[filePath] - size);
+						if (diff > (process.env.THRESHOLD || 100)) {
+							changed = true;
+							acc[filePath] = size;
+						} else {
+							acc[filePath] = originalSize[filePath];
+						}
+					});
+			}
+			return acc;
+		}, {});
+		if (changed) {
+			fs.writeFileSync('./size.json', JSON.stringify(sizes));
+		}
+	});
