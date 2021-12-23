cb5caeea13c85181830a154699d3092b3ec89bbc
Fabien Rassinier <frassinier@talend.com>
2021-12-23 22:50:08 +0100

    feat(design-system/link): css module

--- packages/design-system/src/components/Link/Link.tsx
+++ packages/design-system/src/components/Link/Link.tsx
@@ -1,25 +1,39 @@
 import { IconName } from '@talend/icons';
-import React from 'react';
+import React, {
+	AnchorHTMLAttributes,
+	cloneElement,
+	ComponentType,
+	createElement,
+	forwardRef,
+	PropsWithRef,
+	ReactElement,
+	Ref,
+	useCallback,
+	useMemo,
+} from 'react';
+
+import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { Icon } from '../Icon/Icon';
-import * as S from './Link.style';
 
-export type LinkProps = React.AnchorHTMLAttributes<any> & {
+import style from './Link.module.scss';
+
+export type LinkProps = AnchorHTMLAttributes<any> & {
 	/** The icon to display before */
-	icon?: IconName | React.ReactElement;
+	icon?: IconName | ReactElement;
 	/** The icon to display before */
-	iconBefore?: IconName | React.ReactElement;
+	iconBefore?: IconName | ReactElement;
 	/** The icon to display after */
-	iconAfter?: IconName | React.ReactElement;
+	iconAfter?: IconName | ReactElement;
 	/** if the link is disabled */
 	disabled?: boolean;
 	/** if the link is external but the icon must not be shown */
 	hideExternalIcon?: boolean;
 };
 
-export type StyledLink = { as?: React.ComponentType<any> | string } & React.PropsWithRef<LinkProps>;
+export type StyledLink = { as?: ComponentType<any> | string } & PropsWithRef<LinkProps>;
 
-const Link = React.forwardRef(
+const Link = forwardRef(
 	(
 		{
 			className,
@@ -35,21 +49,21 @@ const Link = React.forwardRef(
 			as = 'a',
 			...rest
 		}: StyledLink,
-		ref: React.Ref<any>,
+		ref: Ref<any>,
 	) => {
 		const { t } = useTranslation();
-		const isBlank: boolean = React.useMemo(
+		const isBlank: boolean = useMemo(
 			() => !!target && !['_self', '_parent', '_top'].includes(target.toLowerCase()),
 			[target],
 		);
-		const isExternal = React.useMemo(() => {
+		const isExternal = useMemo(() => {
 			if (!href) {
 				return false;
 			}
 			return /^https?:\/\//i.test(href) && new URL(href).host !== location.host;
 		}, [href]);
 
-		const getTitle = React.useCallback(() => {
+		const getTitle = useCallback(() => {
 			if (disabled && title) {
 				return t('LINK_DISABLED_TITLE', {
 					title,
@@ -75,39 +89,54 @@ const Link = React.forwardRef(
 			return title;
 		}, [disabled, title, isExternal, isBlank]);
 
-		return (
-			<S.Link
-				rel={isBlank ? 'noopener noreferrer' : undefined}
-				target={target}
-				{...rest}
-				href={!disabled ? href : undefined}
-				className={`link ${disabled ? 'link--disabled' : ''} ${className || ''}`}
-				title={getTitle()}
-				aria-disabled={disabled}
-				ref={ref}
-				as={as}
-			>
+		return createElement(
+			as,
+			{
+				rel: isBlank ? 'noopener noreferrer' : undefined,
+				target,
+				...rest,
+				href: !disabled ? href : undefined,
+				className: classNames(style.link, { [style.linkDisabled]: disabled }),
+				title: getTitle(),
+				'aria-disabled': disabled,
+				ref,
+			},
+			<>
 				{icon &&
 					(typeof icon === 'string' ? (
-						<Icon className="link__icon link__icon--before" name={icon} />
+						<Icon className={classNames(style.link__Icon, style.link__iconBefore)} name={icon} />
 					) : (
-						React.cloneElement(icon, {
-							className: `${icon.props?.className} link__icon link__icon--before`,
+						cloneElement(icon, {
+							className: classNames(
+								icon.props?.className,
+								style.link__icon,
+								style.link__iconBefore,
+							),
 						})
 					))}
-				<span className="link__text">{children}</span>
+				<span className={style.link__text}>{children}</span>
 				{isExternal && !hideExternalIcon && (
-					<Icon className="link__icon link__icon--external" name="talend-link" />
+					<Icon
+						className={classNames(style.link__Icon, style.link__iconExternal)}
+						name="talend-link"
+					/>
 				)}
 				{iconAfter &&
 					(typeof iconAfter === 'string' ? (
-						<Icon className="link__icon link__icon--after" name={iconAfter} />
+						<Icon
+							className={classNames(style.link__Icon, style.link__iconAfter)}
+							name={iconAfter}
+						/>
 					) : (
-						React.cloneElement(iconAfter, {
-							className: `${iconAfter.props?.className} link__icon link__icon--after`,
+						cloneElement(iconAfter, {
+							className: classNames(
+								iconAfter.props?.className,
+								style.link__icon,
+								style.link__iconAfter,
+							),
 						})
 					))}
-			</S.Link>
+			</>,
 		);
 	},
 );
