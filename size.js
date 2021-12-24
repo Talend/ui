7b750aa4763044e9730dcff305ea383624b5795a
jmfrancois <jmfrancois@talend.com>
2021-12-24 11:37:20 +0100

    chore(ARCH-404): fix CI about bundle sizes

--- size.js
+++ size.js
@@ -3,20 +3,25 @@ const path = require('path');
 const run = require('./run');
 const originalSize = require('./size.json');
 
+let THRESHOLD = 100;
+if (process.env.THRESHOLD) {
+	THRESHOLD = parseInt(process.env.THRESHOLD, 10);
+}
+
 run({ name: 'yarn', args: ['workspaces', '--silent', 'info'] })
 	.then(info => JSON.parse(info))
 	.then(workspaceInfo => {
 		let changed = false;
 		const sizes = Object.values(workspaceInfo).reduce((acc, value) => {
 			const dist = path.join(value.location, 'dist');
-			if (fs.existsSync(path.join(value.location, 'dist'))) {
+			if (fs.existsSync(dist)) {
 				fs.readdirSync(dist)
 					.filter(f => f.endsWith('.json') || f.endsWith('.js') || f.endsWith('.css'))
 					.forEach(f => {
 						const filePath = path.join(dist, f);
 						const size = fs.statSync(filePath).size;
-						const diff = Math.abs(originalSize[filePath] - size);
-						if (diff > (process.env.THRESHOLD || 100)) {
+						const diff = Math.abs((originalSize[filePath] || 0) - size);
+						if (diff > THRESHOLD) {
 							changed = true;
 							acc[filePath] = size;
 						} else {
@@ -27,6 +32,6 @@ run({ name: 'yarn', args: ['workspaces', '--silent', 'info'] })
 			return acc;
 		}, {});
 		if (changed) {
-			fs.writeFileSync('./size.json', JSON.stringify(sizes));
+			fs.writeFileSync('./size.json', JSON.stringify(sizes, null, 2));
 		}
 	});
