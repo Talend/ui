4f720743f2c9bfcc3d55bf06f125c11fb4df7d9f
jmfrancois <jmfrancois@talend.com>
2021-12-24 10:57:39 +0100

    chore: add logs

--- size.js
+++ size.js
@@ -9,15 +9,17 @@ run({ name: 'yarn', args: ['workspaces', '--silent', 'info'] })
 		let changed = false;
 		const sizes = Object.values(workspaceInfo).reduce((acc, value) => {
 			const dist = path.join(value.location, 'dist');
-			if (fs.existsSync(path.join(value.location, 'dist'))) {
+			if (fs.existsSync(dist)) {
 				fs.readdirSync(dist)
 					.filter(f => f.endsWith('.json') || f.endsWith('.js') || f.endsWith('.css'))
 					.forEach(f => {
 						const filePath = path.join(dist, f);
 						const size = fs.statSync(filePath).size;
 						const diff = Math.abs(originalSize[filePath] - size);
+						console.log('size diff', diff, size);
 						if (diff > (process.env.THRESHOLD || 100)) {
 							changed = true;
+							console.log('changed');
 							acc[filePath] = size;
 						} else {
 							acc[filePath] = originalSize[filePath];
@@ -27,6 +29,7 @@ run({ name: 'yarn', args: ['workspaces', '--silent', 'info'] })
 			return acc;
 		}, {});
 		if (changed) {
+			console.log('save');
 			fs.writeFileSync('./size.json', JSON.stringify(sizes, null, 2));
 		}
 	});
