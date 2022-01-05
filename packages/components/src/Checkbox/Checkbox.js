0f9408d0f3b0484526b6cdf26084009ac1a9924c
Fabien Rassinier <frassinier@talend.com>
2022-01-05 23:39:42 +0100

    fix(components): Toggle should reuse Checkbox

--- packages/components/src/Checkbox/Checkbox.js
+++ packages/components/src/Checkbox/Checkbox.js
@@ -1,5 +1,63 @@
-import Toggle from '../Toggle/Toggle.component';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import omit from 'lodash/omit';
 
-export default function Checkbox(params) {
-	return new Toggle({ className: 'checkbox', ...params });
+import theme from '../Toggle/Toggle.scss';
+
+export default function Checkbox({ id, className, label, intermediate, ...props }) {
+	let dataFeature;
+	let dataChecked = 0;
+
+	if (!props.disabled && props['data-feature']) {
+		dataFeature = props['data-feature'];
+		dataFeature += props.checked ? '.disable' : '.enable';
+	}
+
+	if (intermediate) {
+		dataChecked = 1;
+	} else if (props.checked) {
+		dataChecked = 2;
+	}
+
+	return (
+		<div
+			className={classNames('checkbox tc-toggle', theme['tc-toggle'], className, {
+				[theme['tc-toggle-disabled']]: props.disabled,
+				'tc-toggle-disabled': props.disabled,
+			})}
+		>
+			<label htmlFor={id} data-feature={dataFeature}>
+				<input
+					type="checkbox"
+					id={id}
+					data-checked={dataChecked}
+					{...omit(props, 'data-feature')}
+				/>
+				<span>{label}</span>
+			</label>
+		</div>
+	);
 }
+
+Checkbox.displayName = 'Checkbox';
+
+Checkbox.defaultProps = {
+	disabled: false,
+	checked: false,
+	intermediate: false,
+	label: '',
+};
+
+Checkbox.propTypes = {
+	id: PropTypes.string.isRequired,
+	label: PropTypes.string,
+	onChange: PropTypes.func.isRequired,
+	onBlur: PropTypes.func,
+	checked: PropTypes.bool,
+	autoFocus: PropTypes.bool,
+	disabled: PropTypes.bool,
+	intermediate: PropTypes.bool,
+	className: PropTypes.string,
+	'data-feature': PropTypes.string,
+};
