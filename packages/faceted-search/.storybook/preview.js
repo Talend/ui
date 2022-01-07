58e1ee5d92f7ca2dadb3f2bd3838b2055b37c3f1
Fabien <frassinier@talend.com>
2022-01-07 09:17:01 +0100

    chore(faceted-search): init Storybook documentation (#3572)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/faceted-search/.storybook/preview.js
+++ packages/faceted-search/.storybook/preview.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
-import IconProvider from '@talend/react-components/lib/IconsProvider';
+import { I18nextProvider } from 'react-i18next';
+import { IconsProvider } from '@talend/design-system';
 
-addDecorator(storyFn => (
-	<div
-		style={{
-			height: '100%',
-			width: '100%',
-			overflow: 'auto',
-			padding: '3rem',
-			backgroundColor: 'rgba(145, 209, 237, 0.1)',
-		}}
-	>
-		<IconProvider
-			bundles={['https://statics-dev.cloud.talend.com/@talend/icons/6.4.0/dist/svg-bundle/all.svg']}
-		/>
-		<React.Suspense fallback={null}>{storyFn()}</React.Suspense>
-	</div>
-));
+import i18n from './i18n';
+
+export const globalTypes = {
+	locale: {
+		name: 'Locale',
+		defaultValue: 'en',
+		toolbar: {
+			icon: 'globe',
+			items: [
+				{ value: 'zh', title: 'Chinese' },
+				{ value: 'en', title: 'English' },
+				{ value: 'fr', title: 'French' },
+				{ value: 'de', title: 'German' },
+				{ value: 'ja', title: 'Japanese' },
+			],
+		},
+	},
+};
+
+export const decorators = [
+	(Story, context) => {
+		i18n.changeLanguage(context.globals?.locale);
+		return (
+			<div
+				style={{
+					height: '100%',
+					width: '100%',
+					overflow: 'auto',
+					padding: '3rem',
+					backgroundColor: 'rgba(145, 209, 237, 0.1)',
+				}}
+			>
+				{' '}
+				<I18nextProvider i18n={i18n}>
+					<IconsProvider bundles={['https://unpkg.com/@talend/icons/dist/svg-bundle/all.svg']} />
+					<Story {...context} />
+				</I18nextProvider>
+			</div>
+		);
+	},
+];
