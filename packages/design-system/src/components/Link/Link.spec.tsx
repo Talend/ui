1ec435bb9bcdd55ad77ef0d28d88e3e379721963
Fabien <frassinier@talend.com>
2022-01-12 14:01:02 +0100

    feat(Design-System/Link): CSS Modules (#3589)
    Co-authored-by: Benrajalu <rajalubenoit@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/design-system/src/components/Link/Link.spec.tsx
+++ packages/design-system/src/components/Link/Link.spec.tsx
@@ -10,22 +10,22 @@ const { Default, WithIcon, External, TargetBlank } = composeStories(Stories);
 context('<Link />', () => {
 	it('should render', () => {
 		cy.mount(<Default />);
-		cy.get('.link').should('have.text', 'Link example');
+		cy.get('a').should('have.text', 'Link example');
 	});
 
 	it('should render icon', () => {
 		cy.mount(<WithIcon />);
-		cy.get('.link .link__icon').should('have.attr', 'name', 'talend-info-circle');
+		cy.get('a svg').should('have.attr', 'name', 'talend-info-circle');
 	});
 
 	it('should render external', () => {
 		cy.mount(<External />);
-		cy.get('.link .link__icon').should('have.attr', 'name', 'talend-link');
+		cy.get('a svg').should('have.attr', 'name', 'talend-link');
 	});
 
 	it('should render target blank', () => {
 		cy.mount(<TargetBlank />);
-		cy.get('.link')
+		cy.get('a')
 			.should('have.attr', 'title', 'Open in a new tab')
 			.should('have.attr', 'target', '_blank')
 			.should('have.attr', 'rel', 'noopener noreferrer');
