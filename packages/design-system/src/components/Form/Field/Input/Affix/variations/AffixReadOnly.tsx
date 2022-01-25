2f5f046c06503fee92d838b7be77de135f3ea2ea
Benrajalu <rajalubenoit@gmail.com>
2022-01-25 15:18:52 +0100

    fix(ButtonIcon): TUX-1059 - Removing old Button.Icon and Toggle instances (#3632)
    * fix(scripts): env var in npm scripts for windows (#3618)
    
    * Fix test and postinstall for Win users
    
    * Don't use cross-env twice
    
    * feat(ButtonIcon): Creating new ButtonIcon component + doc (#3621)
    
    * feat(ButtonIcon): Initial documentation
    
    * feat(ButtonIcon): Component API outlines
    
    * feat(ButtonIcon): API fixes
    
    * feat(ButtonIcon): Styles and stories
    
    * feat(ButtonIcon): Export, remove Toggle and Button.Icon components
    
    * Revert "feat(ButtonIcon): Export, remove Toggle and Button.Icon components"
    
    This reverts commit 365516012c077ec0b4ac74853d9dabe866b34140.
    
    * feat(ButtonIcon): Export
    
    * doc(Migration): Documented breaking change in migraton
    
    * feat(ButtonIcon): Testing Toggle logic
    
    * feat(ButtonIcon): Testing Toggle logic + fixing Link test
    
    * feat(ButtonIcon): review
    
    * feat(ButtonIcon): Fixed dark mode toggle for CSS modules
    
    * fix(scripts): env var in npm scripts for windows (#3618)
    
    * Fix test and postinstall for Win users
    
    * Don't use cross-env twice
    
    * fix(ButtonIcon): Initial removal of previous Toggle and Button.Icon components
    
    * fix(ButtonIcon): Style corrections for Input.File and Input.Copy
    
    * fix(ButtonIcon): Style corrections for InlineEditing
    
    * fix(ButtonIcon): Style corrections for Menu component
    
    * fix(ButtonIcon): style review
    
    * fix(ButtonIcon): review fixes
    
    Co-authored-by: Laurent Maillet <35027619+lmaillet@users.noreply.github.com>

---
+++ packages/design-system/src/components/Form/Field/Input/Affix/variations/AffixReadOnly.tsx
@@ -0,0 +1,48 @@
+import React, { forwardRef, Ref, HTMLAttributes } from 'react';
+import { IconName } from '@talend/icons';
+
+import { Icon } from '../../../../../Icon/Icon';
+import { StackHorizontal } from '../../../../../Stack';
+import VisuallyHidden from '../../../../../VisuallyHidden';
+
+import styles from '../Affix.module.scss';
+
+type CommonAffixReadOnlyPropTypes = {
+	label: string;
+};
+
+type AffixReadOnlyHideTextProps = {
+	hideText: true;
+	icon: IconName;
+};
+
+type AffixReadOnlyShowTextProps = {
+	hideText: false;
+	icon?: IconName;
+};
+
+export type AffixReadOnlyPropTypes = Omit<HTMLAttributes<HTMLSpanElement>, 'className' | 'style'> &
+	CommonAffixReadOnlyPropTypes &
+	(AffixReadOnlyHideTextProps | AffixReadOnlyShowTextProps);
+
+const AffixReadOnly = forwardRef(
+	(
+		{ label, icon, hideText = false, ...rest }: AffixReadOnlyPropTypes,
+		ref: Ref<HTMLSpanElement>,
+	) => {
+		return (
+			<span ref={ref} {...rest} className={styles.affix}>
+				<StackHorizontal gap="XXS" as="span" display="inline" justify="center" align="center">
+					{icon && (
+						<span className={styles.affix__icon}>
+							<Icon name={icon} />
+						</span>
+					)}
+					{!hideText ? label : <VisuallyHidden>{label}</VisuallyHidden>}
+				</StackHorizontal>
+			</span>
+		);
+	},
+);
+
+export default AffixReadOnly;
