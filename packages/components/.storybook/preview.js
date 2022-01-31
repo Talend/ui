314481ff562584fb7f9a279e276210eec5e77303
jsomsanith-tlnd <jsomsanith@talend.com>
2022-01-31 09:58:08 +0100

    Continue

---
+++ packages/components/.storybook/preview.js
@@ -0,0 +1,17 @@
+export const parameters = {
+	options: {
+		storySort: {
+			order: [
+				'Design Principles',
+				'Writing Principles',
+				'Navigation',
+				'Layout',
+				'Buttons',
+				'Messaging & Communication',
+				'Form',
+				'Data',
+				'Deprecated',
+			],
+		},
+	},
+};
