6af49359f0b235f0264245cacc684078a656f666
jmfrancois <jmfrancois@talend.com>
2022-02-10 10:04:26 +0100

    test: add faceted search playground

---
+++ packages/playground/src/app/components/FacetedSearch.js
@@ -0,0 +1,70 @@
+import FacetedSearch from '@talend/react-faceted-search';
+import Layout from '@talend/react-components/lib/Layout';
+import SidePanel from '@talend/react-containers/lib/SidePanel';
+import HeaderBar from '@talend/react-containers/lib/HeaderBar';
+import * as badges from '@talend/react-faceted-search/stories/badgesDefinitions';
+
+function action(msg) {
+	return (...args) => console.log(msg, ...args);
+}
+
+const badgesDefinitions = Object.values(badges);
+console.log(badgesDefinitions);
+// [
+// 	badgeAll,
+// 	badgeName,
+// 	badgeConnectionName,
+// 	badgeAuthor,
+// 	badgeConnectionType,
+// 	badgeTags,
+// 	badgePrice,
+// 	badgeValid,
+// 	badgeEmpty,
+// 	badgeInvalid,
+// 	badgeCreationDate,
+// ];
+
+const callbacks = {
+	getTags: () =>
+		new Promise(resolve =>
+			setTimeout(resolve, 2000, [
+				'clean',
+				'production',
+				'last chunk',
+				'salesforce',
+				'outdated',
+				'extracted',
+				'security',
+				'in processing',
+				'deep learning',
+				'sql',
+				'cluster',
+				'visualization',
+				'analytics',
+				'users',
+				'warehouse',
+				'api',
+			]),
+		),
+};
+
+export function FacetedSearchPlayground() {
+	return (
+		<Layout mode="TwoColumns" one={<SidePanel />} header={<HeaderBar />}>
+			<FacetedSearch.Faceted>
+				{currentFacetedMode =>
+					(currentFacetedMode === FacetedSearch.constants.FACETED_MODE.ADVANCED && (
+						<FacetedSearch.AdvancedSearch onSubmit={action('onSubmit')} />
+					)) ||
+					(currentFacetedMode === FacetedSearch.constants.FACETED_MODE.BASIC && (
+						<FacetedSearch.BasicSearch
+							badgesDefinitions={badgesDefinitions}
+							callbacks={callbacks}
+							onSubmit={action('onSubmit')}
+						/>
+					))
+				}
+			</FacetedSearch.Faceted>
+		</Layout>
+	);
+}
--- packages/playground/src/app/index.js
+++ packages/playground/src/app/index.js
@@ -13,6 +13,7 @@ import ComponentForm from '@talend/react-containers/lib/ComponentForm';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import ComponentFormSandbox from './components/ComponentFormSandbox';
+import { FacetedSearchPlayground } from './components/FacetedSearch';
 
 import { LeaguesList } from './components/List';
 import { Dataviz } from './components/Dataviz';
@@ -45,7 +46,14 @@ function IconsProvider() {
 }
 
 const app = {
-	components: { ComponentForm, ComponentFormSandbox, LeaguesList, IconsProvider, Dataviz },
+	components: {
+		ComponentForm,
+		ComponentFormSandbox,
+		FacetedSearch: FacetedSearchPlayground,
+		LeaguesList,
+		IconsProvider,
+		Dataviz,
+	},
 	settingsURL: `${basename || ''}/settings.json`,
 	actionCreators: actions,
 	middlewares: [],
