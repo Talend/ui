775ff083652ff486f36b22ba92995a2067130bd2
Jimmy Somsanith <jsomsanith@talend.com>
2022-02-18 10:27:01 +0100

    Update prettier.sh

--- prettier.sh
+++ prettier.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-modified_files=($(git diff --name-only HEAD $(git merge-base HEAD master)))
+modified_files=$(git diff --name-only HEAD $(git merge-base HEAD master))
 for i in ${modified_files[@]}; do
   if [[ $i == *.js || $i == *.scss || $i == *.json ]]; then
     prettier --config .prettierrc ${i} --write
