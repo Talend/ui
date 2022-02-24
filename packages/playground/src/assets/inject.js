fb5e77ddfe7e20e92108beca30dbff09d1fa1cb6
jmfrancois <jmfrancois@talend.com>
2022-02-24 10:20:52 +0100

    fix(playground): remove integrity on local to support refresh

--- packages/playground/src/assets/inject.js
+++ packages/playground/src/assets/inject.js
@@ -3,6 +3,27 @@
 (function () {
 	const CDN_URL_REGEX = /^(\/?.*\/cdn)\//;
 
+	const PKGS = [
+		'@talend/design-system',
+		'@talend/design-tokens',
+		'@talend/react-components',
+		'@talend/react-containers',
+		'@talend/react-cmf',
+		'@talend/react-cmf-router',
+		'@talend/react-dataviz',
+		'@talend/react-faceted-search',
+		'@talend/react-datagrid',
+		'@talend/react-forms',
+		'@talend/bootstrap-theme',
+		'@talend/icons',
+	];
+	function removeIntegrityOnDev(info) {
+		if (PKGS.includes(info.name)) {
+			// eslint-disable-next-line no-param-reassign
+			delete info.integrity;
+		}
+		return info;
+	}
 	function prepareUrl(url) {
 		let newUrl;
 		const m = CDN_URL_REGEX.exec(url);
@@ -20,6 +41,6 @@
 		}
 		return 'https://statics.cloud.talend.com';
 	};
-	window.talendAddStyles(window.Talend.cssBuild, prepareUrl);
-	window.talendAddScripts(window.Talend.build, prepareUrl);
+	window.talendAddStyles(window.Talend.cssBuild.map(removeIntegrityOnDev), prepareUrl);
+	window.talendAddScripts(window.Talend.build.map(removeIntegrityOnDev), prepareUrl);
 })();
