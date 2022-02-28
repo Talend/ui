52ea432ffb486c19520f4b3370e7193f9714d5ce
jsomsanith-tlnd <jsomsanith@talend.com>
2022-02-28 14:15:10 +0100

    revert some things

---
+++ .storybook/config.js
@@ -0,0 +1,20 @@
+import { configure, addDecorator, addParameters } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+import { locales as tuiLocales } from '@talend/locales-tui/locales';
+
+import 'focus-outline-manager';
+import './sortStories';
+
+const languages = {};
+Object.keys(tuiLocales).forEach(key => (languages[key] = key));
+addDecorator(withA11y);
+
+configure(
+	[
+		require.context('../packages/components/src', true, /\.stories\.js$/),
+		require.context('../packages/datagrid/src', true, /\.stories\.js$/),
+	],
+	module,
+);
+
+addParameters({ layout: 'fullscreen' });
