d54c6254633aa23c4b5a79d495d19cb1dc10370a
jmfrancois <jmfrancois@talend.com>
2022-03-01 14:33:47 +0100

    chore: fix i18n with new args

--- packages/components/.storybook/i18n.js
+++ packages/components/.storybook/i18n.js
@@ -1,6 +1,6 @@
 import { namespaces as tuiNamespaces } from '@talend/locales-tui-components/namespaces';
 
 export const namespaces = [tuiNamespaces];
-export const LOCALES_MAP = {
+export const remoteLocalesMap = {
 	'tui-components': 'https://unpkg.com/@talend/locales-tui-components/locales/{{lng}}/{{ns}}.json',
 };
--- packages/containers/.storybook/i18n.js
+++ packages/containers/.storybook/i18n.js
@@ -1,5 +1,10 @@
-import { namespaces as tuiNamespaces } from '@talend/locales-tui-containers/namespaces';
-import { locales as tuiLocales } from '@talend/locales-tui-containers/locales';
+import { namespaces as componentsNamespaces } from '@talend/locales-tui-components/namespaces';
+import { namespaces as formsNamespaces } from '@talend/locales-tui-forms/namespaces';
+import { namespaces as containersNamespaces } from '@talend/locales-tui-containers/namespaces';
 
-export const namespaces = [tuiNamespaces];
-export const locales = [tuiLocales];
+export const namespaces = [componentsNamespaces, containersNamespaces, formsNamespaces];
+export const remoteLocalesMap = {
+	'tui-components': 'https://unpkg.com/@talend/locales-tui-components/locales/{{lng}}/{{ns}}.json',
+	'tui-containers': 'https://unpkg.com/@talend/locales-tui-containers/locales/{{lng}}/{{ns}}.json',
+	'tui-forms': 'https://unpkg.com/@talend/locales-tui-forms/locales/{{lng}}/{{ns}}.json',
+};
