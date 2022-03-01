588f1189835243f712799f83a2ff77fbba728ad4
jsomsanith-tlnd <jsomsanith@talend.com>
2022-03-01 16:06:38 +0100

    fix ci

--- packages/components/.storybook/i18n.js
+++ packages/components/.storybook/i18n.js
@@ -1,6 +1,8 @@
 import { namespaces as tuiNamespaces } from '@talend/locales-tui-components/namespaces';
+import { namespaces as dsNamespaces } from '@talend/locales-design-system/namespaces';
 
-export const namespaces = [tuiNamespaces];
+export const namespaces = [...tuiNamespaces, ...dsNamespaces];
 export const remoteLocalesMap = {
 	'tui-components': 'https://unpkg.com/@talend/locales-tui-components/locales/{{lng}}/{{ns}}.json',
+	'design-system': 'https://unpkg.com/@talend/locales-design-system/locales/{{lng}}/{{ns}}.json',
 };
