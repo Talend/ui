19efeac67a6b3ab18c997e6b0219931298bcf0f2
jmfrancois <jmfrancois@talend.com>
2022-03-01 11:48:21 +0100

    chore: upgrade scripts

--- packages/components/.storybook/i18n.js
+++ packages/components/.storybook/i18n.js
@@ -1,5 +1,6 @@
 import { namespaces as tuiNamespaces } from '@talend/locales-tui-components/namespaces';
-import { locales as tuiLocales } from '@talend/locales-tui-components/locales';
 
 export const namespaces = [tuiNamespaces];
-export const locales = [tuiLocales];
+export const LOCALES_MAP = {
+	'tui-components': 'https://unpkg.com/@talend/locales-tui-components/locales/{{lng}}/{{ns}}.json',
+};
