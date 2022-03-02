965c5d508152e4d723505234517ae4576b9852bf
jmfrancois <jmfrancois@talend.com>
2022-03-02 17:18:28 +0100

    feat: add getUMD transpile from resolver

--- packages/babel-plugin-assets-api-inject-version/__fixtures__/first/output.js
+++ packages/babel-plugin-assets-api-inject-version/__fixtures__/first/output.js
@@ -2,7 +2,6 @@
 
 /* eslint-disable import/no-extraneous-dependencies */
 import assetsAPI from '@talend/assets-api';
-
 assetsAPI.getURL('/', 'react', '16.14.0');
 assetsAPI.getJSON('/foo.json', 'react', '16.14.0');
 assetsAPI.getUMD('react', '16.14.0', 'React', '/umd/react.production.min.js');
