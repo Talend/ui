526858423307a5df5440bd3d44877f7b3faa0df9
jmfrancois <jmfrancois@talend.com>
2022-03-03 15:31:05 +0100

    chore: remove babel plugin

--- packages/babel-plugin-assets-api/__fixtures__/first/code.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/* eslint-disable import/no-extraneous-dependencies */
-import assetsAPI from '@talend/assets-api';
-
-assetsAPI.getURL('/', 'react');
-assetsAPI.getJSON('/foo.json', 'react');
-assetsAPI.getUMD('react');
--- packages/babel-plugin-assets-api/__fixtures__/first/output.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/* eslint-disable import/no-extraneous-dependencies */
-import assetsAPI from '@talend/assets-api';
-assetsAPI.getURL('/', 'react', '16.14.0');
-assetsAPI.getJSON('/foo.json', 'react', '16.14.0');
-assetsAPI.getUMD('react', '16.14.0', 'React', '/umd/react.production.min.js');
