354354fe40d1fc9d4a55ba5427f42455116bd57e
jsomsanith-tlnd <jsomsanith@talend.com>
2022-03-03 14:13:48 +0100

    fixes

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -1,3 +1,15 @@
+import { namespaces as tuiNamespaces } from '@talend/locales-tui-components/namespaces';
+import { namespaces as dsNamespaces } from '@talend/locales-design-system/namespaces';
+
+export const i18n = {
+	namespaces: [...tuiNamespaces, ...dsNamespaces],
+	remoteLocalesMap: {
+		'tui-components':
+			'https://unpkg.com/@talend/locales-tui-components/locales/{{lng}}/{{ns}}.json',
+		'design-system': 'https://unpkg.com/@talend/locales-design-system/locales/{{lng}}/{{ns}}.json',
+	},
+};
+
 export const parameters = {
 	options: {
 		storySort: {
