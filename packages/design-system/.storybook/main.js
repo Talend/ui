b33ba9322192f6686330a24929bc598953a71863
Fabien <frassinier@talend.com>
2022-03-10 11:54:12 +0100

    doc(design-system): disable remark plugin

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,26 +1,26 @@
 const path = require('path');
-const visit = require('unist-util-visit');
+// const visit = require('unist-util-visit');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
-const tokens = require('../lib/tokens').default;
+// const tokens = require('../lib/tokens').default;
 
-const getValue = (path, obj) => path.split('.').reduce((acc, c) => acc && acc[c], obj);
-const getTokenValue = path => getValue(path, tokens);
-const designTokensPlugin = () => tree =>
-	visit(tree, 'text', node => {
-		const regex = /\$([\w+\.]+\w+)/g;
-		const nodeValue = node.value;
-		let results;
-		while ((results = regex.exec(nodeValue)) !== null) {
-			if (getTokenValue(results[1])) {
-				node.type = 'html';
-				node.value = node.value.replace(
-					results[0],
-					`<abbr title="${getTokenValue(results[1])}">${results[0]}</abbr>`,
-				);
-			}
-		}
-	});
+// const getValue = (path, obj) => path.split('.').reduce((acc, c) => acc && acc[c], obj);
+// const getTokenValue = path => getValue(path, tokens);
+// const designTokensPlugin = () => tree =>
+// 	visit(tree, 'text', node => {
+//		const regex = /\$([\w+\.]+\w+)/g;
+//		const nodeValue = node.value;
+//		let results;
+//		while ((results = regex.exec(nodeValue)) !== null) {
+//			if (getTokenValue(results[1])) {
+//				node.type = 'html';
+//				node.value = node.value.replace(
+//					results[0],
+//					`<abbr title="${getTokenValue(results[1])}">${results[0]}</abbr>`,
+//				);
+//			}
+//		}
+//	});
 
 module.exports = {
 	features: {
@@ -90,17 +90,17 @@ module.exports = {
 	},
 	webpackFinal: async config => {
 		config.entry.unshift('core-js');
-		config.module.rules.map(rule => {
-			if (rule.use && rule.use.some(use => use.loader && use.loader.includes('@mdx-js'))) {
-				return rule.use.map(use => {
-					if (use.options && use.options.remarkPlugins) {
-						use.options.remarkPlugins.push(designTokensPlugin);
-					}
-					return use;
-				});
-			}
-			return rule;
-		});
+		//config.module.rules.map(rule => {
+		//	if (rule.use && rule.use.some(use => use.loader && use.loader.includes('@mdx-js'))) {
+		//		return rule.use.map(use => {
+		//			if (use.options && use.options.remarkPlugins) {
+		//				use.options.remarkPlugins.push(designTokensPlugin);
+		//			}
+		//			return use;
+		//		});
+		//	}
+		//	return rule;
+		//});
 
 		config.plugins.push(
 			new BrowserSyncPlugin({
