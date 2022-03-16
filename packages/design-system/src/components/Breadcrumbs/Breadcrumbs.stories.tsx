323069364bdb1e317ba14c35cc15baa749d79ebb
Benrajalu <rajalubenoit@gmail.com>
2022-03-16 17:34:30 +0100

    doc(Breadcrumbs): Updated MDX

--- packages/design-system/src/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ packages/design-system/src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import Breadcrumbs from './Breadcrumbs';
 
 export default {
@@ -50,4 +51,46 @@ export const Advanced = () => (
 			},
 		]}
 	/>
+);
+
+export const Usage = () => (
+	<BrowserRouter>
+		<Breadcrumbs
+			items={[
+				{
+					label: 'Link example',
+					as: <Link to="/documentation" />,
+				},
+				{
+					label: 'Other Link example',
+					as: <Link to="/documentation" />,
+				},
+			]}
+		/>
+	</BrowserRouter>
+);
+
+export const FullWidth = () => (
+	<BrowserRouter>
+		<Breadcrumbs
+			items={[
+				{
+					label: 'Link example with a label that is too long',
+					as: <Link to="/documentation" />,
+				},
+				{
+					label: 'Link example with a label that is still too long',
+					as: <Link to="/documentation" />,
+				},
+				{
+					label: 'Link example with a label that is yet again too long',
+					href: '/documentation',
+				},
+				{
+					label: 'Link example with a label that is still and forever too long',
+					href: '/documentation',
+				},
+			]}
+		/>
+	</BrowserRouter>
 );
--- packages/design-system/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ packages/design-system/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -7,70 +7,92 @@ import { ButtonTertiary } from '../Button';
 import { StackHorizontal } from '../Stack';
 import Divider from '../Divider';
 
-type BreadcrumbsItem = {
+type BreadcrumbsLink = {
 	label: string;
 	href: string;
 	target?: string;
-}[];
+};
+
+type BreadcrumbsRouterLink = {
+	label: string;
+	target?: string;
+	as: ReactElement;
+};
+
+type BreadcrumbsItem = (BreadcrumbsRouterLink | BreadcrumbsLink)[];
 
 type BreadCrumbsProps = Omit<HTMLAttributes<HTMLElement>, 'className'> & {
-	as?: 'a' | ReactElement;
 	items: BreadcrumbsItem;
 };
 
 const maxBreadcrumbsItemLength = 4;
 
-const Breadcrumbs = forwardRef(
-	({ as = 'a', items, ...rest }: BreadCrumbsProps, ref: Ref<HTMLElement>) => {
-		const buildEntries = () => {
-			if (items.length > maxBreadcrumbsItemLength) {
-				const origin = items[0];
-				const suffix = items.slice(-2);
-				const collapsed = items.slice(1, items.length - 2);
+const Breadcrumbs = forwardRef(({ items, ...rest }: BreadCrumbsProps, ref: Ref<HTMLElement>) => {
+	const buildEntries = () => {
+		if (items.length > maxBreadcrumbsItemLength) {
+			const origin = items[0];
+			const suffix = items.slice(-2);
+			const collapsed = items.slice(1, items.length - 2);
 
-				return (
-					<>
-						<li className={styles.entry}>
-							<StackHorizontal gap="S" align="center" wrap="nowrap">
-								<Link href={origin.href} as={as} target={origin.target} data-ellipsis>
+			return (
+				<>
+					<li className={styles.entry}>
+						<StackHorizontal gap="S" align="center" wrap="nowrap">
+							{'href' in origin ? (
+								<Link href={origin.href} target={origin.target} data-ellipsis>
 									{origin.label}
 								</Link>
-								<span aria-hidden className={styles.divider}>
-									<Divider orientation="vertical" />
-								</span>
-							</StackHorizontal>
-						</li>
+							) : (
+								<Link as={origin.as} target={origin.target} data-ellipsis>
+									{origin.label}
+								</Link>
+							)}
 
-						<li className={styles.entry}>
-							<StackHorizontal gap="S" align="center" wrap="nowrap">
-								<Dropdown
-									aria-label="Collapsed links"
-									items={collapsed.map(collapsedLinks => ({
-										href: collapsedLinks.href,
+							<span aria-hidden className={styles.divider}>
+								<Divider orientation="vertical" />
+							</span>
+						</StackHorizontal>
+					</li>
+
+					<li className={styles.entry}>
+						<StackHorizontal gap="S" align="center" wrap="nowrap">
+							<Dropdown
+								aria-label="Collapsed links"
+								items={collapsed.map(collapsedLinks => {
+									if ('href' in collapsedLinks) {
+										return {
+											href: collapsedLinks.href,
+											label: collapsedLinks.label,
+											target: collapsedLinks.target,
+											type: 'link',
+										};
+									}
+									return {
 										label: collapsedLinks.label,
 										target: collapsedLinks.target,
 										type: 'link',
-										as,
-									}))}
-								>
-									<ButtonTertiary isDropdown size="S" onClick={() => {}}>
-										...
-									</ButtonTertiary>
-								</Dropdown>
-								<span aria-hidden className={styles.divider}>
-									<Divider orientation="vertical" />
-								</span>
-							</StackHorizontal>
-						</li>
+										as: collapsedLinks.as,
+									};
+								})}
+							>
+								<ButtonTertiary isDropdown size="S" onClick={() => {}}>
+									...
+								</ButtonTertiary>
+							</Dropdown>
+							<span aria-hidden className={styles.divider}>
+								<Divider orientation="vertical" />
+							</span>
+						</StackHorizontal>
+					</li>
 
-						{suffix.map((entry, index) => {
+					{suffix.map((entry, index) => {
+						if ('href' in entry) {
 							const { label, ...entryProps } = entry;
 							return (
 								<li className={styles.entry} key={`${label}-${index}`}>
 									<StackHorizontal gap="S" align="center" wrap="nowrap">
 										<Link
 											{...entryProps}
-											as={as}
 											aria-current={index === suffix.length - 1 ? 'page' : undefined}
 											data-ellipsis
 										>
@@ -82,18 +104,57 @@ const Breadcrumbs = forwardRef(
 									</StackHorizontal>
 								</li>
 							);
-						})}
-					</>
+						}
+						const { label, as, ...entryProps } = entry;
+						return (
+							<li className={styles.entry} key={`${label}-${index}`}>
+								<StackHorizontal gap="S" align="center" wrap="nowrap">
+									<Link
+										{...entryProps}
+										as={as}
+										aria-current={index === suffix.length - 1 ? 'page' : undefined}
+										data-ellipsis
+									>
+										{label}
+									</Link>
+									<span aria-hidden className={styles.divider}>
+										<Divider orientation="vertical" />
+									</span>
+								</StackHorizontal>
+							</li>
+						);
+					})}
+				</>
+			);
+		}
+
+		return items.map((entry, index) => {
+			if ('href' in entry) {
+				return (
+					<li className={styles.entry} key={`${entry.label}-${index}`}>
+						<StackHorizontal gap="S" align="center" wrap="nowrap">
+							<Link
+								data-ellipsis
+								href={entry.href}
+								aria-current={index === items.length - 1 ? 'page' : undefined}
+							>
+								{entry.label}
+							</Link>
+							<span aria-hidden className={styles.divider}>
+								<Divider orientation="vertical" />
+							</span>
+						</StackHorizontal>
+					</li>
 				);
 			}
 
-			return items.map((entry, index) => (
-				<li className={styles.entry} key={entry.href}>
+			return (
+				<li className={styles.entry} key={`${entry.label}-${index}`}>
 					<StackHorizontal gap="S" align="center" wrap="nowrap">
 						<Link
-							href={entry.href}
-							as={as}
+							as={entry.as}
 							aria-current={index === items.length - 1 ? 'page' : undefined}
+							data-ellipsis
 						>
 							{entry.label}
 						</Link>
@@ -102,17 +163,17 @@ const Breadcrumbs = forwardRef(
 						</span>
 					</StackHorizontal>
 				</li>
-			));
-		};
+			);
+		});
+	};
 
-		return (
-			<nav {...rest} className={styles.breadcrumbs} ref={ref} aria-label="breadcrumb">
-				<StackHorizontal gap="S" as="ul" justify="start" align="center" role="list">
-					{buildEntries()}
-				</StackHorizontal>
-			</nav>
-		);
-	},
-);
+	return (
+		<nav {...rest} className={styles.breadcrumbs} ref={ref} aria-label="breadcrumb">
+			<StackHorizontal gap="S" as="ul" justify="start" align="center" role="list">
+				{buildEntries()}
+			</StackHorizontal>
+		</nav>
+	);
+});
 
 export default Breadcrumbs;
