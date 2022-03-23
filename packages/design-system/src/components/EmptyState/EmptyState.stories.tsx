1347110832a9ddf33de239cf317ab745fdf94904
Benoit Rajalu <rajalubenoit@gmail.com>
2022-03-23 14:03:23 +0100

    feat(EmptyStates): Illustrations

--- packages/design-system/src/components/EmptyState/EmptyState.stories.tsx
+++ packages/design-system/src/components/EmptyState/EmptyState.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ComponentStory } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import EmptyState from '.';
 
 export default {
@@ -10,8 +11,19 @@ const Template: ComponentStory<typeof EmptyState> = args => {
 	return <EmptyState {...args} />;
 };
 
-export const Default = Template.bind({});
-Default.args = {
-	title: 'No preparations yet',
-	description: 'Add a preparation to clean, format, and transform data priori to processing',
+export const Icon = Template.bind({});
+Icon.args = {
+	title: 'No preparations yet.',
+	description: 'Add a preparation to clean, format, and transform data prior to processing.',
+	variant: 'icon',
+	docLinkURL: 'https://talend.com',
+};
+
+export const Spot = Template.bind({});
+Spot.args = {
+	title: 'No preparations yet.',
+	description: 'Add a preparation to clean, format, and transform data prior to processing.',
+	variant: 'spot',
+	docLinkURL: 'https://talend.com',
+	callback: { label: 'Create a dataset', action: () => action('clicked'), icon: 'talend-plus' },
 };
