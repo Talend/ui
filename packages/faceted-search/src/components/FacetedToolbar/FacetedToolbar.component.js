8341b8fb120a416ac0d5a35796d17bc48246c4f4
AdelineMrtn <adeline.martineau0@gmail.com>
2022-04-07 11:22:44 +0200

    chore:refactoring Form.Switch component

--- packages/faceted-search/src/components/FacetedToolbar/FacetedToolbar.component.js
+++ packages/faceted-search/src/components/FacetedToolbar/FacetedToolbar.component.js
@@ -9,13 +9,13 @@ import cssModule from './FacetedToolbar.scss';
 const theme = getTheme(cssModule);
 
 const SwitchFacetedMode = ({ facetedMode, onChange, t }) => (
-	<form>
+	<Form>
 		<Form.Switch
 			label={t('FACETED_SEARCH_QUERY', 'Query')}
 			onChange={() => onChange(facetedMode === FACETED_MODE.BASIC ? FACETED_MODE.ADVANCED : FACETED_MODE.BASIC)}
-			dataFeature={facetedMode === FACETED_MODE.BASIC ? USAGE_TRACKING_TAGS.ADVANCED : USAGE_TRACKING_TAGS.BASIC}
+			dataFeature={facetedMode === FACETED_MODE.BASIC ? USAGE_TRACKING_TAGS.BASIC : USAGE_TRACKING_TAGS.ADVANCED}
 		/>
-	</form>
+	</Form>
 );
 
 SwitchFacetedMode.propTypes = {
