2e19164bfa6c0ff4b5cb8237143a8d892dda95ec
Benrajalu <rajalubenoit@gmail.com>
2022-04-14 14:17:44 +0200

    feat(design-system): Empty States (#3781)
    * feat(EmptyStates): Initial commit
    
    * feat(EmptyStates): Illustration pool
    
    * feat(EmptyStates): Illustrations
    
    * feat(EmptyStates): Split empty states in variants
    
    * feat(EmptyStates): review
    
    * Update EmptyState.stories.mdx
    
    * Update EmptyState.stories.mdx
    
    * Update EmptyState.stories.mdx
    
    * chore(design-tokens): update tokens
    
    * Update EmptyState.stories.mdx
    
    * chore(design-system): Empty state doc correction
    
    * Update EmptyState.stories.mdx
    
    * Update EmptyState.stories.mdx
    
    * chore(design-system): Empty state text correction
    
    * feat(design-system): Empty state illustration corrections
    
    * feat(design-system): Empty state description madatory in two cases
    
    * feat(design-system): Final story
    
    * feat(design-system): Export component to package
    
    * feat(design-system): review fixes
    
    * feat(design-system): Updated illustration colors
    
    * feat(design-system): props renaming
    
    * Update EmptyState.stories.mdx
    
    Co-authored-by: HM <hmassy@talend.com>

---
+++ packages/design-system/src/components/EmptyState/index.ts
@@ -0,0 +1,7 @@
+import EmptyStateLarge from './variants/EmptyStateLarge';
+import EmptyStateMedium from './variants/EmptyStateMedium';
+import EmptyStateSmall from './variants/EmptyStateSmall';
+import EmptyState from './EmptyState';
+
+export default EmptyState;
+export { EmptyStateLarge, EmptyStateMedium, EmptyStateSmall };
