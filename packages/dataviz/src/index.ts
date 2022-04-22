8431b257687f27be3bfb220100f88b14c2ed5ae7
Guillaume NICOLAS <guin56@gmail.com>
2022-04-22 11:31:44 +0200

    feat(TDC-6145): export LineChart component and types

--- packages/dataviz/src/index.ts
+++ packages/dataviz/src/index.ts
@@ -4,14 +4,28 @@ export { default as VerticalChartFilter } from './components/ChartPanel/Vertical
 export { default as GeoChart } from './components/GeoChart';
 export { default as TooltipContent } from './components/TooltipContent/TooltipContent.component';
 export { default as BoxPlot } from './components/BoxPlot';
+export { default as LineChart } from './components/LineChart';
 export * from './components/BarChart/barChart.tooltip';
 export * from './components/GeoChart/GeoChart.utils';
 export * from './components/RangeFilter/handlers';
 
 export type { HorizontalBarChartProps } from './components/BarChart/HorizontalBarChart';
-export type { VerticalBarChartEntry, VerticalBarChartProps } from './components/BarChart/VerticalBarChart';
+export type {
+	VerticalBarChartEntry,
+	VerticalBarChartProps,
+} from './components/BarChart/VerticalBarChart';
 export type { VerticalChartFilterProps } from './components/ChartPanel/VerticalChartFilter';
 export type { GeoChartProps } from './components/GeoChart';
-export type { TooltipEntry, TooltipContentProps } from './components/TooltipContent/TooltipContent.component';
+export type {
+	TooltipEntry,
+	TooltipContentProps,
+} from './components/TooltipContent/TooltipContent.component';
 export type { BoxPlotProps, BoxPlotData } from './components/BoxPlot';
+export type {
+	LineChartEntry,
+	LineChartOptions,
+	LineOptions,
+	LineStatus,
+	LineChartProps,
+} from './components/LineChart';
 export type { Range } from './types';
