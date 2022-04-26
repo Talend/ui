f89507650b42cd705d859553256ab6d5e81c7451
Romain GUILLAUME <rguillaume@talend.com>
2022-04-26 10:55:15 +0200

    fix: datagrid build package

--- .changeset/hungry-cougars-marry.md
+++
@@ -1,6 +0,0 @@
----
-'@talend/react-components': patch
-'@talend/react-datagrid': patch
----
-
-feat(components/QualityBar): rework quality bar with design tokens
--- packages/datagrid/src/index.js
+++ packages/datagrid/src/index.js
@@ -1,11 +1,9 @@
 import DataGrid from './containers';
-import QualityBar from './components/DefaultHeaderRenderer/QualityBar.component';
 import * as constants from './constants';
 import cmfModule from './cmfModule';
 import * as components from './components';
 
 DataGrid.constants = constants;
-DataGrid.QualityBar = QualityBar;
 DataGrid.components = components;
 DataGrid.cmfModule = cmfModule;
 
