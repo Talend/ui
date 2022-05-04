f57e9bfc7342faff3ae27e6c8c8d7d714a04ff12
jmfrancois <jmfrancois@talend.com>
2022-05-04 15:47:10 +0200

    fix: add missing package

---
+++ packages/monaco-editor/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import MonacoEditor from 'react-monaco-editor';
+// import * as monaco from 'monaco-editor';
+
+export default function Monaco(props) {
+	<MonacoEditor {...props} />;
+}
