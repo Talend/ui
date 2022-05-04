8e95aab34c8496a2ef0142b7e7c8ff8a7c03c782
Steven Gangnant <38908846+sgangnant@users.noreply.github.com>
2022-05-04 10:14:08 +0200

    feat(design-system/Modal): Modal component (#3914)

--- packages/design-system/src/components/Modal/index.ts
+++ packages/design-system/src/components/Modal/index.ts
@@ -1,20 +1,2 @@
-import Modal, { useModalState, ModalDisclosure, ModalDialog } from './Modal';
-import * as S from './Modal.style';
+export { default } from './Modal';
 
-const ModalComponent = Modal as typeof Modal & {
-	useDialogState: typeof useModalState;
-	Disclosure: typeof ModalDisclosure;
-	Dialog: typeof ModalDialog;
-	Body: typeof S.Dialog;
-	Heading: typeof S.DialogHeading;
-	Buttons: typeof S.DialogButtons;
-};
-
-ModalComponent.useDialogState = useModalState;
-ModalComponent.Disclosure = ModalDisclosure;
-ModalComponent.Dialog = ModalDialog;
-ModalComponent.Body = S.Dialog;
-ModalComponent.Heading = S.DialogHeading;
-ModalComponent.Buttons = S.DialogButtons;
-
-export default ModalComponent;
