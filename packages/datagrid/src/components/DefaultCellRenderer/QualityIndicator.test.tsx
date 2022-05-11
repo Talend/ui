4119033fd341430817600181c4d8cbd01b87d0be
gjou-tlnd <58977230+gjou-tlnd@users.noreply.github.com>
2022-05-11 17:45:59 +0200

    chore(datagrid): Rewrite QualityIndicator in typescript (#4003)
    * chore(datagrid): Boot rtl config & continue ts migration
    
    * review

--- packages/datagrid/src/components/DefaultCellRenderer/QualityIndicator.test.tsx
+++ packages/datagrid/src/components/DefaultCellRenderer/QualityIndicator.test.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
 import QualityIndicator from './QualityIndicator.component';
 import { QUALITY_INVALID_KEY, QUALITY_VALID_KEY } from '../../constants';
 
 describe('#QualityIndicator', () => {
 	it('should render when quality index is QUALITY_INVALID_KEY', () => {
-		const wrapper = shallow(<QualityIndicator qualityIndex={QUALITY_INVALID_KEY} />);
+		render(<QualityIndicator qualityIndex={QUALITY_INVALID_KEY} />);
 
-		expect(wrapper.getElement()).toMatchSnapshot();
+		expect(screen.getByTitle('Invalid value')).toBeInTheDocument();
 	});
 
 	it('should not render when quality index is different of QUALITY_INVALID_KEY', () => {
-		const wrapper = shallow(<QualityIndicator qualityIndex={QUALITY_VALID_KEY} />);
+		render(<QualityIndicator qualityIndex={QUALITY_VALID_KEY} />);
 
-		expect(wrapper.getElement()).toMatchSnapshot();
+		expect(screen.queryByTitle('Invalid value')).not.toBeInTheDocument();
 	});
 });
---
+++ packages/datagrid/src/components/PinHeaderRenderer/index.ts
@@ -0,0 +1,1 @@
+export { default, PIN_HEADER_RENDERER_COMPONENT } from './PinHeaderRenderer.component';
