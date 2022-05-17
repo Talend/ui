793d363f58e0b4373530dbdf6d2f674ce81bd298
Benoit Rajalu <rajalubenoit@gmail.com>
2022-05-17 15:19:31 +0200

    feat(design-system): Form - Select styles using icon

--- packages/design-system/src/components/Form/Primitives/Select/Select.tsx
+++ packages/design-system/src/components/Form/Primitives/Select/Select.tsx
@@ -8,6 +8,7 @@ type SelectProps = Omit<SelectHTMLAttributes<any>, 'prefix'> & {
 } & AffixesProps;
 
 import styles from './Select.module.scss';
+import { Icon } from '../../../Icon/Icon';
 
 const Select = forwardRef((props: SelectProps, ref: Ref<HTMLSelectElement>) => {
 	const {
@@ -23,20 +24,25 @@ const Select = forwardRef((props: SelectProps, ref: Ref<HTMLSelectElement>) => {
 	} = props;
 	return (
 		<InputWrapper prefix={prefix} suffix={suffix} readOnly={!!readOnly} disabled={!!disabled}>
-			<select
-				{...rest}
-				disabled={disabled || readOnly}
-				ref={ref}
-				className={classnames(styles.select, { [styles.select_readOnly]: readOnly }, className)}
-			>
-				{placeholder && (
-					<option disabled selected>
-						{placeholder}
-					</option>
-				)}
-				{!required && <option value=""></option>}
-				{children}
-			</select>
+			<div className={styles.select__wrapper}>
+				<select
+					{...rest}
+					disabled={disabled || readOnly}
+					ref={ref}
+					className={classnames(styles.select, { [styles.select_readOnly]: readOnly }, className)}
+				>
+					{placeholder && (
+						<option disabled selected>
+							{placeholder}
+						</option>
+					)}
+					{!required && <option value=""></option>}
+					{children}
+				</select>
+				<div className={styles.select__icon}>
+					<Icon name="talend-caret-down" />
+				</div>
+			</div>
 		</InputWrapper>
 	);
 });
