0e4cfbaa8d9046bf44a1e359ba781536d728234a
gjou-tlnd <58977230+gjou-tlnd@users.noreply.github.com>
2022-06-03 15:36:07 +0200

    feat(Skeleton): Add new input variant (#4072)
    * feat(Skeleton): Add new input variant
    
    * fix mdx
    
    * fix stack type
    
    * Update packages/design-system/src/components/Skeleton/variations/SkeletonInput.module.scss
    
    Co-authored-by: Steven Gangnant <38908846+sgangnant@users.noreply.github.com>
    
    Co-authored-by: Steven Gangnant <38908846+sgangnant@users.noreply.github.com>

---
+++ .changeset/twelve-frogs-compete.md
@@ -0,0 +1,5 @@
+---
+'@talend/design-system': minor
+---
+
+Add new input variant
---
+++ packages/design-system/src/components/Skeleton/variations/SkeletonInput.tsx
@@ -0,0 +1,20 @@
+import { StackVertical } from '../../Stack';
+import SkeletonPrimitive, { SkeletonPrimitiveProps } from '../Primitive/Skeleton.Primitive';
+import styles from './SkeletonInput.module.scss';
+import SkeletonParagraph from './SkeletonParagraph';
+import React, { forwardRef, Ref } from 'react';
+
+export type SkeletonInputProps = Omit<SkeletonPrimitiveProps, 'className'>;
+
+const SkeletonInput = forwardRef((props: SkeletonInputProps, ref: Ref<HTMLSpanElement>) => {
+	return (
+		<StackVertical as="span" gap="XXS" {...props} ref={ref}>
+			<span className={styles['skeleton-input__label']}>
+				<SkeletonParagraph size="S" />
+			</span>
+			<SkeletonPrimitive isBlock className={styles.skeletonInput} />
+		</StackVertical>
+	);
+});
+
+export default SkeletonInput;
