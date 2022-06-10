82922882d9b8ca6f27430cab7a7ebc946f46161a
jmfrancois <jmfrancois@talend.com>
2022-06-10 11:03:43 +0200

    fix: add compat

--- packages/cmf/scripts/cmf-settings.js
+++ packages/cmf/scripts/cmf-settings.js
@@ -1,1 +1,13 @@
-require('@talend/scripts-cmf/cmf-settings');
+#!/usr/bin/env node
+
+const program = require('commander'); // eslint-disable-line import/no-extraneous-dependencies
+const merge = require('@talend/scripts-cmf/cmf-settings.merge');
+
+program
+	.version('0.0.2')
+	.option('-d, --dev', 'dev sources instead of sources')
+	.option('-q, --quiet', 'display nothing')
+	.option('-r, --recursive', 'allow recursive search for json files')
+	.parse(process.argv);
+
+merge(program, process.exit);
