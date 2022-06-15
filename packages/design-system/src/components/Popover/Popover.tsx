a90ec37482d8101d315e5fb18272f23356fef4e6
Sébastien Romain <sromain@talend.com>
2022-06-15 10:50:19 +0200

    feat(design-system): allow to control popover (#4121)
    * feat(design-system): allow to control popover
    
    * add z-index
    
    * review

--- packages/design-system/src/components/Popover/Popover.tsx
+++ packages/design-system/src/components/Popover/Popover.tsx
@@ -1,9 +1,11 @@
-import React, { HTMLAttributes, ReactElement, ReactNode } from 'react';
+import React, { HTMLAttributes, ReactElement, ReactNode, cloneElement } from 'react';
+import tokens from '@talend/design-tokens';
 import {
 	usePopoverState,
 	Popover as ReakitPopover,
 	PopoverArrow as ReakitPopoverArrow,
 	PopoverDisclosure as ReakitPopoverDisclosure,
+	PopoverStateReturn,
 } from 'reakit';
 import Clickable from '../Clickable';
 import { Placement } from '../Tooltip/Tooltip';
@@ -13,24 +15,38 @@ import style from './Popover.module.scss';
 
 const ANIMATION_DURATION = 150; // Sync with @talend/design-token animations duration
 
+export type PopoverChildren = ReactNode | ((popover: PopoverStateReturn) => ReactNode);
+
 export type PopoverPropsType = HTMLAttributes<HTMLDivElement> & {
 	disclosure: ReactElement<typeof Clickable>;
-	children: ReactNode | ReactNode[];
+	children: PopoverChildren | PopoverChildren[];
 	position?: Placement;
+	zIndex?: string | number;
 } & DataAttributes;
 
-function Popover({ disclosure, position = 'auto', ...props }: PopoverPropsType) {
+function Popover({
+	disclosure,
+	position = 'auto',
+	zIndex = tokens.coralElevationLayerStandardFront,
+	...props
+}: PopoverPropsType) {
 	const popover = usePopoverState({ animated: ANIMATION_DURATION, placement: position });
+	const children = Array.isArray(props.children) ? props.children : [props.children];
 
 	return (
 		<>
 			<ReakitPopoverDisclosure {...popover}>
-				{disclosureProps => React.cloneElement(disclosure, disclosureProps)}
+				{disclosureProps => cloneElement(disclosure, disclosureProps)}
 			</ReakitPopoverDisclosure>
-			<ReakitPopover {...popover} {...props}>
+			<ReakitPopover {...popover} {...props} style={{ zIndex }}>
 				<div className={style.popover__animated}>
 					<ReakitPopoverArrow {...popover} className={style.popover__arrow} />
-					{props.children}
+					{children.map(child => {
+						if (typeof child === 'function') {
+							return child(popover);
+						}
+						return child;
+					})}
 				</div>
 			</ReakitPopover>
 		</>
