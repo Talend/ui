2203c4831a10e324cbeb52e448feb68a1f06cb05
romainseb <sromain@talend.com>
2022-06-15 09:02:07 +0200

    feat(design-system): allow to control popover

---
+++ .changeset/weak-beers-worry.md
@@ -0,0 +1,5 @@
+---
+'@talend/design-system': minor
+---
+
+feat(design-system): Allow children as function for popover component in order to access the popover state
--- packages/design-system/src/components/Popover/Popover.tsx
+++ packages/design-system/src/components/Popover/Popover.tsx
@@ -1,9 +1,16 @@
-import React, { HTMLAttributes, ReactElement, ReactNode } from 'react';
+import React, {
+	HTMLAttributes,
+	ReactElement,
+	ReactNode,
+	cloneElement,
+	isValidElement,
+} from 'react';
 import {
 	usePopoverState,
 	Popover as ReakitPopover,
 	PopoverArrow as ReakitPopoverArrow,
 	PopoverDisclosure as ReakitPopoverDisclosure,
+	PopoverStateReturn,
 } from 'reakit';
 import Clickable from '../Clickable';
 import { Placement } from '../Tooltip/Tooltip';
@@ -13,24 +20,32 @@ import style from './Popover.module.scss';
 
 const ANIMATION_DURATION = 150; // Sync with @talend/design-token animations duration
 
+export type PopoverChildren = ReactNode | ((popover: PopoverStateReturn) => ReactNode);
+
 export type PopoverPropsType = HTMLAttributes<HTMLDivElement> & {
 	disclosure: ReactElement<typeof Clickable>;
-	children: ReactNode | ReactNode[];
+	children: PopoverChildren | PopoverChildren[];
 	position?: Placement;
 } & DataAttributes;
 
 function Popover({ disclosure, position = 'auto', ...props }: PopoverPropsType) {
 	const popover = usePopoverState({ animated: ANIMATION_DURATION, placement: position });
+	const children = Array.isArray(props.children) ? props.children : [props.children];
 
 	return (
 		<>
 			<ReakitPopoverDisclosure {...popover}>
-				{disclosureProps => React.cloneElement(disclosure, disclosureProps)}
+				{disclosureProps => cloneElement(disclosure, disclosureProps)}
 			</ReakitPopoverDisclosure>
 			<ReakitPopover {...popover} {...props}>
 				<div className={style.popover__animated}>
 					<ReakitPopoverArrow {...popover} className={style.popover__arrow} />
-					{props.children}
+					{children.map(child => {
+						if (typeof child === 'function') {
+							return child(popover);
+						}
+						return child;
+					})}
 				</div>
 			</ReakitPopover>
 		</>
