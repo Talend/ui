adc0924d7e8325b5f0b6fae0614ffe9348f1ad0f
Benoit Rajalu <rajalubenoit@gmail.com>
2022-06-16 18:20:54 +0200

    feat(design-system/TUX-1178/styles): First batch of styles

--- packages/storybook-docs/webpack.custom.js
+++ packages/storybook-docs/webpack.custom.js
@@ -1,19 +1,37 @@
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
 module.exports = {
     entry: './src/globalStyles.scss',
     output: {
-        path: '/dist',
+        path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'globalStyles.min.css',
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
                 use: [
+                    { loader: MiniCssExtractPlugin.loader, options: { esModule: false } },
+                    { loader: 'css-loader', options: { sourceMap: true } },
                     {
-                        loader: 'file-loader',
-                        options: { outputPath: 'css/', name: '[name].min.css'}
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()],
+                            sourceMap: true,
+                        },
                     },
-                    'sass-loader'
+                    { loader: 'resolve-url-loader', options: { sourceMap: true } },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true },
+                    }
                 ]
             }
         ]
