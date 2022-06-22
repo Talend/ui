3422f271f2ced7a828ac48c49024a4ad6f0e1b1a
Sébastien Romain <sromain@talend.com>
2022-06-22 15:18:00 +0200

    feat(components): new icons in old buttons (#4141)

---
+++ .changeset/old-socks-obey.md
@@ -0,0 +1,5 @@
+---
+'@talend/react-components': minor
+---
+
+feat(components): allow to use new icons in legacy buttons
--- packages/components/src/ActionList/ActionList.stories.js
+++ packages/components/src/ActionList/ActionList.stories.js
@@ -11,7 +11,7 @@ const actions = [
 	},
 	{
 		label: 'Favorite datasets of the year 2019',
-		icon: 'talend-star',
+		iconName: 'star',
 		'data-feature': 'actionlist.item',
 		onClick: action('Favorite clicked'),
 		beta: true,
