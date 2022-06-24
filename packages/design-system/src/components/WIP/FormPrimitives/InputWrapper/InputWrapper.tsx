f074bee3a5836ca7d5a7a4708553764921f83296
Benoit Rajalu <rajalubenoit@gmail.com>
2022-06-24 10:59:00 +0200

    chore(design-system/WIP): Documenting what's WIP better

---
+++ packages/design-system/src/components/WIP/FormPrimitives/InputWrapper/InputWrapper.tsx
@@ -0,0 +1,75 @@
+import React, { forwardRef, ReactElement, Ref } from 'react';
+import { isElement } from 'react-is';
+import { AffixButtonPropsType } from '../../../Form/FieldGroup/Affix/variations/AffixButton';
+import { AffixReadOnlyPropsType } from '../../../Form/FieldGroup/Affix/variations/AffixReadOnly';
+import { AffixButton, AffixReadOnly } from '../../../Form/FieldGroup/Affix';
+
+import styles from './InputWrapper.module.scss';
+import classnames from 'classnames';
+import { FieldStatusProps } from '../Field/Field';
+
+type AffixProps =
+	| ({ type: 'button' } & AffixButtonPropsType)
+	| ({ type: 'text' } & AffixReadOnlyPropsType)
+	| ReactElement;
+
+export type AffixesProps = {
+	prefix?: AffixProps;
+	suffix?: AffixProps;
+};
+
+type InputWrapperProps = {
+	children: ReactElement;
+	disabled?: boolean;
+	readOnly?: boolean;
+} & AffixesProps &
+	Omit<FieldStatusProps, 'errorMessage'>;
+
+function buildAffix(affixProps: AffixProps) {
+	if (isElement(affixProps)) {
+		return affixProps;
+	}
+
+	if (affixProps.type === 'button') {
+		const { type, children, ...rest } = affixProps;
+		return <AffixButton {...rest}>{children}</AffixButton>;
+	}
+
+	if (affixProps.type === 'text') {
+		const { type, children, ...rest } = affixProps;
+		return <AffixReadOnly {...rest}>{children}</AffixReadOnly>;
+	}
+
+	return <></>;
+}
+
+const InputWrapper = forwardRef((props: InputWrapperProps, ref: Ref<HTMLDivElement>) => {
+	const {
+		children,
+		prefix,
+		suffix,
+		disabled = false,
+		readOnly = false,
+		hasError = false,
+		...rest
+	} = props;
+	return (
+		<div
+			{...rest}
+			ref={ref}
+			className={classnames(styles.inputShell, {
+				[styles.inputShell_disabled]: disabled,
+				[styles.inputShell_readOnly]: readOnly,
+				[styles.inputShell_borderError]: hasError,
+			})}
+		>
+			{prefix && buildAffix(prefix)}
+			{children}
+			{suffix && buildAffix(suffix)}
+		</div>
+	);
+});
+
+InputWrapper.displayName = 'InputWrapper';
+
+export default InputWrapper;
