913a9205ef51d408e62e73c63458139a8d5895cd
jmfrancois <jmfrancois@talend.com>
2022-07-04 17:13:44 +0200

    test: fix dataviz

--- packages/dataviz/src/components/GeoChart/GeoChart.test.js
+++ packages/dataviz/src/components/GeoChart/GeoChart.test.js
@@ -1,54 +1,58 @@
+/* eslint-disable import/no-unresolved */
+jest.mock('@talend/assets-api', () => ({
+	getJSON: async url => {
+		return new Promise(resolve => resolve(require(url.replace('/dist', '../../..'))));
+	},
+	getURL: jest.fn(() => '/url'),
+}));
+
 import React from 'react';
-import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
+
 import GeoChart from './GeoChart.component';
 import { getGeoChartConfig } from './GeoChart.utils';
 import styles from './GeoChart.scss';
 
-function getTopo(url) {
-	const last = url.split('/').pop();
-	return require(`../../../assets/maps/${last}`);
-}
-
 describe('GeoChart component', () => {
 	let defaultProps;
-	let originalfetch;
 	beforeEach(async () => {
 		jest.resetAllMocks();
-		originalfetch = global.fetch;
-		global.fetch = jest.fn(url => ({ ok: true, json: () => Promise.resolve(getTopo(url)) }));
 		defaultProps = {
 			data: [
 				{ key: 'Occitanie', value: 10 },
 				{ key: 'Martinique', value: 20 },
 			],
 			columnName: 'name',
-			chartConfig: await getGeoChartConfig('FR_REGION'),
 			onSelection: jest.fn(),
 		};
 	});
-	afterEach(() => {
-		global.fetch = originalfetch;
+
+	it('Should match data', async () => {
+		const chartConfig = await getGeoChartConfig('FR_REGION');
+		render(<GeoChart {...defaultProps} chartConfig={chartConfig} />);
+
+		expect(document.querySelectorAll(`.${styles['geo-chart__feature']}`)).toHaveLength(18);
+		expect(document.querySelectorAll(`.${styles['geo-chart__feature--disabled']}`)).toHaveLength(
+			16,
+		);
+		expect(document.querySelectorAll('[data-key="Occitanie"][data-value="10"]')).toHaveLength(1);
 	});
 
-	it('Should match data', () => {
-		const component = mount(<GeoChart {...defaultProps} />).render();
-
-		expect(component.find(`.${styles['geo-chart__feature']}`)).toHaveLength(18);
-		expect(component.find(`.${styles['geo-chart__feature--disabled']}`)).toHaveLength(16);
-		expect(component.find('[data-key="Occitanie"][data-value="10"]')).toHaveLength(1);
-	});
-
-	it('Should be case and punctuation insensitive', () => {
-		const component = mount(
-			<GeoChart {...defaultProps} data={[{ key: 'Occi tanié', value: 10 }]} />,
-		).render();
-
-		expect(component.find('[data-key="Occi tanié"][data-value="10"]')).toHaveLength(1);
+	it('Should be case and punctuation insensitive', async () => {
+		const chartConfig = await getGeoChartConfig('FR_REGION');
+		render(
+			<GeoChart
+				{...defaultProps}
+				chartConfig={chartConfig}
+				data={[{ key: 'Occi tanié', value: 10 }]}
+			/>,
+		);
+		expect(document.querySelectorAll('[data-key="Occi tanié"][data-value="10"]')).toHaveLength(1);
 	});
 
 	it('Should match data in configured properties only', async () => {
 		const chartConfig = await getGeoChartConfig('US_STATE_CODE');
-		const component = mount(
+		render(
 			<GeoChart
 				{...defaultProps}
 				chartConfig={chartConfig}
@@ -57,10 +61,9 @@ describe('GeoChart component', () => {
 					{ key: 'New York', value: 10 },
 				]}
 			/>,
-		).render();
-		expect(global.fetch).toHaveBeenCalled();
+		);
 
-		expect(component.find('[data-key="TX"]')).toHaveLength(1);
-		expect(component.find('[data-key="New York"]')).toHaveLength(0);
+		expect(document.querySelectorAll('[data-key="TX"]')).toHaveLength(1);
+		expect(document.querySelectorAll('[data-key="New York"]')).toHaveLength(0);
 	});
 });
