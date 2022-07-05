9d23d1104e3105fd35ccf7e2c709ed28fa3d00ca
Benoit Rajalu <rajalubenoit@gmail.com>
2022-07-05 16:41:30 +0200

    fix(storybook-doc/config): Updating custom webpack config

--- packages/storybook-docs/webpack.custom.js
+++ packages/storybook-docs/webpack.custom.js
@@ -3,43 +3,45 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: { globalStyles: './src/globalStyles.scss', managerStyles: './src/managerStyles.scss' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].min.css',
-            chunkFilename: "[name].min.css",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: MiniCssExtractPlugin.loader, options: { esModule: false } },
-                    { loader: 'css-loader', options: { sourceMap: true } },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [autoprefixer()],
-                            sourceMap: true,
-                        },
-                    },
-                    { loader: 'resolve-url-loader', options: { sourceMap: true } },
-                    {
-                        loader: 'sass-loader',
-                        options: { sourceMap: true },
-                    }
-                ]
-            }
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-    },
+	entry: { globalStyles: './src/globalStyles.scss', managerStyles: './src/managerStyles.scss' },
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].min.css',
+			chunkFilename: '[name].min.css',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: [
+					{ loader: MiniCssExtractPlugin.loader, options: { esModule: false } },
+					{ loader: 'css-loader', options: { sourceMap: true } },
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: ['autoprefixer'],
+								sourceMap: true,
+							},
+						},
+					},
+					{ loader: 'resolve-url-loader', options: { sourceMap: true } },
+					{
+						loader: 'sass-loader',
+						options: { sourceMap: true },
+					},
+				],
+			},
+		],
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
 };
