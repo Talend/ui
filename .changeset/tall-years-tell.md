bdd46b1663b870d7ffee8a12f7e59433fd7542fb
jmfrancois <jmfrancois@talend.com>
2022-07-12 09:36:36 +0200

    chore: re re move

--- .changeset/tall-years-tell.md
+++
@@ -1,5 +0,0 @@
----
-'@talend/scripts-config-ng-webpack': patch
----
-
-fix: make it work with webpack 5
--- tools/scripts-config-ng-webpack/test/fixtures/app/components/foo/foo-module.js
+++
@@ -1,8 +0,0 @@
-import Foo from './foo.component';
-import angular from 'angular';
-
-const MODULE_NAME = 'myApp.foo';
-
-angular.module(MODULE_NAME, []).component('foo', Foo);
-
-export default MODULE_NAME;
