473513fccda0bee1ef1fb7ae019a4e52b42e7752
Guillaume NICOLAS <guin56@gmail.com>
2022-07-20 15:11:26 +0200

    fix but very slow

--- packages/components/src/List/ListComposition/Manager/hooks/useColumnsVisibility.hook.js
+++ packages/components/src/List/ListComposition/Manager/hooks/useColumnsVisibility.hook.js
@@ -1,4 +1,3 @@
-import { difference } from 'lodash';
 import { useMemo, useState } from 'react';
 
 export function useColumnsVisibility(storageKey, initialVisibleColumns = []) {
@@ -8,25 +7,26 @@ export function useColumnsVisibility(storageKey, initialVisibleColumns = []) {
 			const columnsVisibility = localStorage.getItem(storageKey);
 			visibleColumns = columnsVisibility && JSON.parse(columnsVisibility);
 		}
-		if (!visibleColumns)
-			visibleColumns = initialVisibleColumns.map(key => ({ key, visible: true }));
 		return visibleColumns;
 	});
 
 	const visibleColumns = useMemo(
-		() => columnsVisibility.filter(({ visible }) => !!visible).map(({ key }) => key),
+		() => columnsVisibility?.filter(({ visible }) => !!visible).map(({ key }) => key),
 		[columnsVisibility],
 	);
 
 	const setVisibleColumns = (columns, nextVisibleColumns) => {
-		if (storageKey && columns.length) {
+		if (columns.length) {
 			const nextColumnsVisibility = columns.map(({ dataKey }) => ({
 				key: dataKey,
-				visible:
-					(nextVisibleColumns || visibleColumns).includes(dataKey) ||
-					!columnsVisibility.find(({ key }) => key === dataKey),
+				visible: !visibleColumns
+					? initialVisibleColumns.length
+						? initialVisibleColumns.includes(dataKey)
+						: true
+					: (nextVisibleColumns || visibleColumns).includes(dataKey) ||
+					  !columnsVisibility.find(({ key }) => key === dataKey),
 			}));
-			localStorage.setItem(storageKey, JSON.stringify(nextColumnsVisibility));
+			if (storageKey) localStorage.setItem(storageKey, JSON.stringify(nextColumnsVisibility));
 			_setColumnsVisibility(nextColumnsVisibility);
 		}
 	};
