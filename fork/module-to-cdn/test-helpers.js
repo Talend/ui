5a2a40c851a63db1162ed32b9147147f0cd34b7f
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2022-07-22 12:34:22 +0200

    Update test-helpers.js

--- fork/module-to-cdn/test-helpers.js
+++ fork/module-to-cdn/test-helpers.js
@@ -1,7 +1,7 @@
 function assertStyleVersionPatterns(config) {
     Object.keys(config)
         .filter(importPath => config[importPath].hasOwnProperty('style-versions'))
-        .reduce((acc, importPath) => {
+        .forEach((acc, importPath) => {
             const patterns = Object.keys(config[importPath].versions);
             const stylePatterns = Object.keys(config[importPath]['style-versions']);
             expect(patterns).toEqual(expect.arrayContaining(stylePatterns));
