b4f626a4487daf0d25446e32c272961ac4306b70
jmainguytalend <jmainguy@talend.com>
2022-09-01 11:20:13 +0200

    feat(CDM-237) Add const to export Tag variations through string array

---
+++ .changeset/popular-schools-remember.md
@@ -0,0 +1,5 @@
+---
+'@talend/design-system': minor
+---
+
+Add const to export Tag variations through string array
--- packages/design-system/src/components/Tag/Tag.tsx
+++ packages/design-system/src/components/Tag/Tag.tsx
@@ -19,6 +19,9 @@ export enum TagVariant {
 	beta = 'beta',
 }
 
+// This const allows JS component to type props with restricted list of tag variations
+export const TagVariants = Object.values(TagVariant);
+
 type TagProps = Omit<PrimitiveTagProps, 'className'> & {
 	/**
 	 * Tag variation depending on its semantic
