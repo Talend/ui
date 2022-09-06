def6151749f28eb1b5dddfd58ba0e04e1a7b6485
Benoit Rajalu <rajalubenoit@gmail.com>
2022-09-06 14:41:15 +0200

    feat(design-system/NO-TICKET): Fixing Popover stories and styles

--- packages/design-system/src/components/WIP/Popover/Popover.tsx
+++ packages/design-system/src/components/WIP/Popover/Popover.tsx
@@ -1,24 +1,29 @@
-import React, { HTMLAttributes, ReactElement, ReactNode, cloneElement } from 'react';
+import React, { cloneElement, HTMLAttributes, ReactElement, ReactNode } from 'react';
 import tokens from '@talend/design-tokens';
 import {
-	usePopoverState,
 	Popover as ReakitPopover,
 	PopoverArrow as ReakitPopoverArrow,
 	PopoverDisclosure as ReakitPopoverDisclosure,
+	PopoverDisclosureHTMLProps,
 	PopoverStateReturn,
+	usePopoverState,
 } from 'reakit';
 import Clickable from '../../Clickable';
 import { Placement } from '../../Tooltip/Tooltip';
 import { DataAttributes } from '../../../types';
 
 import style from './Popover.module.scss';
+import classnames from 'classnames';
 
 const ANIMATION_DURATION = 150; // Sync with @talend/design-token animations duration
 
 export type PopoverChildren = ReactNode | ((popover: PopoverStateReturn) => ReactNode);
+export type PopoverDisclosure =
+	| ReactElement<typeof Clickable>
+	| ((disclosureProps: PopoverDisclosureHTMLProps) => ReactNode);
 
 export type PopoverPropsType = HTMLAttributes<HTMLDivElement> & {
-	disclosure: ReactElement<typeof Clickable>;
+	disclosure: PopoverDisclosure;
 	children: PopoverChildren | PopoverChildren[];
 	position?: Placement;
 	zIndex?: string | number;
@@ -44,23 +49,31 @@ function Popover({
 	return (
 		<>
 			<ReakitPopoverDisclosure {...popover}>
-				{disclosureProps => cloneElement(disclosure, disclosureProps)}
+				{disclosureProps => {
+					if (typeof disclosure === 'function') {
+						return disclosure(disclosureProps);
+					}
+					return cloneElement(disclosure, disclosureProps);
+				}}
 			</ReakitPopoverDisclosure>
 			<ReakitPopover
 				{...popover}
 				{...props}
 				style={{
 					zIndex,
-					padding: hasPadding ? tokens.coralSpacingM : 0,
 				}}
 			>
-				<div className={style.popover__animated}>
+				<div
+					className={classnames(style.popover__animated, {
+						[style.popover__animated_withPadding]: hasPadding,
+					})}
+				>
 					<ReakitPopoverArrow {...popover} className={style.popover__arrow} />
-					{children.map(child => {
+					{children.map((child, index) => {
 						if (typeof child === 'function') {
-							return child(popover);
+							return <div key={`popover-child-${index}`}>{child(popover)}</div>;
 						}
-						return child;
+						return <div key={`popover-child-${index}`}>{child}</div>;
 					})}
 				</div>
 			</ReakitPopover>
