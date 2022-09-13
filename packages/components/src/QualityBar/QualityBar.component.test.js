4beeaba94ee877be1eec907a9c0f4e9927782ddd
Steven Gangnant <sgangnant@talend.com>
2022-09-13 13:56:49 +0200

    shallow

--- packages/components/src/QualityBar/QualityBar.component.test.js
+++ packages/components/src/QualityBar/QualityBar.component.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { QualityBar } from './QualityBar.component';
 
 describe('QualityBar', () => {
