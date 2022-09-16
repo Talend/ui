aee4711a8ed00c043c6866495d9dc5f8b2bd8e5e
cbaune-talend <cbaune@talend.com>
2022-09-16 08:50:38 +0200

    feat(TUX-1038): accordion - add tests & doc

---
+++ packages/design-system/src/components/WIP/Accordion/Accordion.spec.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { composeStories } from '@storybook/testing-react';
+
+import * as Stories from './Accordion.stories';
+
+const { WithAction, AccordionWrapper, DisabledPanel } = composeStories(Stories);
+
+context('<CollapsiblePanel />', () => {
+	it('should render header', () => {
+		cy.mount(<WithAction />);
+		cy.getByTest('panel.header').should('be.visible');
+		cy.getByTest('panel.section').should('not.exist');
+	});
+
+	it('should expand and collapse', () => {
+		cy.mount(<WithAction />);
+		cy.get('#actionCollapsible_control').click();
+		cy.getByTest('panel.section').should('be.visible');
+		cy.get('#actionCollapsible_control').click();
+		cy.getByTest('panel.section').should('not.exist');
+	});
+
+	it('should hide chevron and action when disabled', () => {
+		cy.mount(<DisabledPanel />);
+		cy.get('#actionCollapsible_control').should('not.exist');
+		cy.get('#actionCollapsible_control').should('not.exist');
+	});
+
+	it('should display action toolip', () => {
+		cy.mount(<WithAction />);
+		cy.getByTest('action.button')
+			.focus()
+			.should('have.attr', 'aria-describedby')
+			.then(describedBy => cy.get(`#${describedBy}`).should('have.text', 'action tooltip'));
+	});
+
+	it('should managed expanded state with accordion', () => {
+		cy.mount(<AccordionWrapper />);
+		cy.get('#SimpleAccordion-p1_control').click().should('have.attr', 'aria-expanded', 'true');
+		cy.get('#SimpleAccordion-p2_control').click().should('have.attr', 'aria-expanded', 'true');
+		cy.get('#SimpleAccordion-p1_control').should('have.attr', 'aria-expanded', 'false');
+	});
+});
--- packages/design-system/src/components/WIP/Accordion/Accordion.stories.tsx
+++ packages/design-system/src/components/WIP/Accordion/Accordion.stories.tsx
@@ -1,44 +1,124 @@
 import React from 'react';
+import { Story } from '@storybook/react';
 
 import { TagSuccess } from '../../Tag';
 
 import Accordion from './Accordion';
 import CollapsiblePanel from './Primitive/CollapsiblePanel';
+import { StackVertical } from '../../Stack';
 
 export default { component: Accordion };
 
+const SampleParagraph = () => (
+	<p>
+		Quisque efficitur, magna sit amet tempor malesuada, orci mauris vestibulum enim, quis gravida
+		est urna et ipsum. Nunc rutrum, magna id fermentum dignissim, magna sem volutpat risus, ut
+		ultrices ipsum lacus vitae sapien. Curabitur sodales risus ac nibh efficitur, dapibus posuere
+		ipsum bibendum. Proin erat ipsum, tempus in aliquet sed, auctor id sem. Maecenas ultrices, magna
+		vitae pretium condimentum, ipsum lectus hendrerit est, a ultrices lacus odio in mi. Phasellus
+		accumsan diam in metus dictum ultrices. In hac habitasse platea dictumst. Curabitur vestibulum
+		vitae libero sit amet blandit. Nulla bibendum sollicitudin dolor at vehicula. Morbi quis viverra
+		velit, eget ornare velit. Praesent porttitor sagittis nulla non vehicula. u
+	</p>
+);
+
 export const SimpleCollapsiblePanel = () => (
-	<div style={{ maxWidth: '50rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
+	<div style={{ maxWidth: '80rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
 		<CollapsiblePanel a11yId="simpleCollapsible" title="Simple panel">
-			<p> this is a test pharagraph</p>
+			<SampleParagraph />
 		</CollapsiblePanel>
 	</div>
 );
 
-export const SimpleAccordion = () => (
-	<Accordion>
-		<CollapsiblePanel a11yId="SimpleAccordion-p1" title="Simple accordion 1">
-			<p> this is a test pharagraph</p>
+export const DisabledPanel = {
+	render: (props: Story) => (
+		<div style={{ maxWidth: '80rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
+			<CollapsiblePanel
+				{...props}
+				a11yId="disabledCollapsible"
+				title="disabled panel"
+				action={{
+					icon: 'plus',
+					tooltip: 'action tooltip',
+					callback: () => window.alert('action callback'),
+				}}
+				disabled
+			>
+				<SampleParagraph />
+			</CollapsiblePanel>
+		</div>
+	),
+};
+
+export const SmallPanel = () => (
+	<div style={{ maxWidth: '80rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
+		<CollapsiblePanel a11yId="smallCollapsible" title="small panel" size="S">
+			<SampleParagraph />
 		</CollapsiblePanel>
-		<CollapsiblePanel
-			a11yId="SimpleAccordion-p2"
-			title="Simple accordion 2"
-			action={{ icon: 'plus', tooltip: 'test tooltip', callback: () => window.alert('test') }}
-		>
-			<p> this is a test pharagraph</p>
-		</CollapsiblePanel>
-	</Accordion>
+	</div>
 );
 
 export const WithMetadata = () => (
-	<div style={{ maxWidth: '50rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
+	<div style={{ maxWidth: '80rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
 		<CollapsiblePanel
 			a11yId="WithMetadata"
 			title="Simple panel with metadata"
 			metadata={['Duration : 3sec', <TagSuccess key="toto">Succes</TagSuccess>]}
-			action={{ icon: 'plus', tooltip: 'test tooltip', callback: () => window.alert('test') }}
 		>
-			<p> this is a test pharagraph</p>
+			<SampleParagraph />
 		</CollapsiblePanel>
+	</div>
+);
+
+export const WithAction = {
+	render: (props: Story) => (
+		<div style={{ maxWidth: '80rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
+			<CollapsiblePanel
+				{...props}
+				a11yId="actionCollapsible"
+				title="panel with action"
+				action={{
+					icon: 'plus',
+					tooltip: 'action tooltip',
+					callback: () => window.alert('action callback'),
+				}}
+			>
+				<SampleParagraph />
+			</CollapsiblePanel>
+		</div>
+	),
+};
+
+export const AccordionWrapper = {
+	render: (props: Story) => (
+		<div style={{ maxWidth: '80rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
+			<Accordion {...props}>
+				<CollapsiblePanel a11yId="SimpleAccordion-p1" title="first panel">
+					<SampleParagraph />
+				</CollapsiblePanel>
+				<CollapsiblePanel a11yId="SimpleAccordion-p2" title="second panel">
+					<SampleParagraph />
+				</CollapsiblePanel>
+				<CollapsiblePanel a11yId="SimpleAccordion-p3" title="third panel">
+					<SampleParagraph />
+				</CollapsiblePanel>
+			</Accordion>
+		</div>
+	),
+};
+
+export const GroupOfSimplePanels = () => (
+	<div style={{ maxWidth: '80rem', marginLeft: 'auto', marginRight: 'auto', padding: '3rem' }}>
+		<StackVertical gap={'S'} align="stretch">
+			<CollapsiblePanel a11yId="SimplePanel-p1" title="first panel">
+				<SampleParagraph />
+			</CollapsiblePanel>
+			<CollapsiblePanel a11yId="SimplePanel-p2" title="second panel">
+				<SampleParagraph />
+			</CollapsiblePanel>
+			<CollapsiblePanel a11yId="SimplePanel-p3" title="third panel">
+				<SampleParagraph />
+			</CollapsiblePanel>
+		</StackVertical>
 	</div>
 );
