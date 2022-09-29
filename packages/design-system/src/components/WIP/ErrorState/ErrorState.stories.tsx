60deab44ad164c55750797cf804419fbe7da3fed
Steven Gangnant <sgangnant@talend.com>
2022-09-29 16:46:05 +0200

    Stories writings

--- packages/design-system/src/components/WIP/ErrorState/ErrorState.stories.tsx
+++ packages/design-system/src/components/WIP/ErrorState/ErrorState.stories.tsx
@@ -5,22 +5,44 @@ import Link from '../../Link';
 
 import ErrorState from './ErrorState';
 
-// Default props
-const title = "Couldn't load your data";
-const description = "We couldn't retrieve your data, you should try later";
-const link = (
-	<span>
-		read more <Link href="https://www.talend.com">here</Link>
-	</span>
-);
-const actionProp = { children: 'Try again', onClick: action('actionClicked') };
-
-// Stories
 export default { component: ErrorState };
 
-export const DefaultStory = () => <ErrorState {...{ title, description }} />;
-export const WithLink = () => <ErrorState {...{ title, description, link }} />;
-export const WithAction = () => <ErrorState {...{ title, description, action: actionProp }} />;
+export const DefaultStory = () => (
+	<ErrorState
+		title="Couldn't load your data"
+		description="We couldn't retrieve your data, you should try later"
+	/>
+);
+
+export const WithLink = () => (
+	<ErrorState
+		title="Couldn't load your data"
+		description="We couldn't retrieve your data, you should try later"
+		link={
+			<span>
+				read more <Link href="https://www.talend.com">here</Link>
+			</span>
+		}
+	/>
+);
+
+export const WithAction = () => (
+	<ErrorState
+		title="Couldn't load your data"
+		description="We couldn't retrieve your data, you should try later"
+		action={{ children: 'Try again', onClick: action('actionClicked') }}
+	/>
+);
+
 export const WithActionAndLink = () => (
-	<ErrorState {...{ title, description, link, action: actionProp }} />
+	<ErrorState
+		title="Couldn't load your data"
+		description="We couldn't retrieve your data, you should try later"
+		action={{ children: 'Try again', onClick: action('actionClicked') }}
+		link={
+			<span>
+				read more <Link href="https://www.talend.com">here</Link>
+			</span>
+		}
+	/>
 );
