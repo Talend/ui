abe81c238a4eaa6965e3c62570b58a140e624596
romainseb <sromain@talend.com>
2022-11-02 16:51:25 +0100

    update doc

--- packages/design-system/src/components/Message/Message.stories.tsx
+++ packages/design-system/src/components/Message/Message.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { ComponentStory } from '@storybook/react';
 
 import {
 	MessagePrimitive,
@@ -19,21 +20,6 @@ import { MessageCollectionWarning } from './variations/MessageCollectionWarning'
 import { MessageCollectionInformation } from './variations/MessageCollectionInformation';
 
 export default { component: MessagePrimitive };
-
-const defaultMessageCollectionProps: SharedMessageCollectionProps = {
-	title: 'Lorem ipsum',
-	description:
-		'dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim tposuere ac.',
-	action: { children: 'See all (3)', onClick: action('action clicked') },
-	additionalActions: {
-		'aria-label': 'Additional actions',
-		items: [
-			{ label: 'Select all', type: 'button', onClick: action('select all clicked') },
-			{ label: 'Dismiss', type: 'button', onClick: action('dismiss clicked') },
-			{ label: 'Delete', type: 'button', onClick: action('delete clicked') },
-		],
-	},
-};
 
 const defaultMessageProps: SharedMessageProps = {
 	title: 'Type incompatibilities',
@@ -55,6 +41,68 @@ export const DefaultMessageDemo = () => (
 		<MessageInformation {...defaultMessageProps} />
 	</StackHorizontal>
 );
+
+export const WithPropVariation = () => (
+	<StackHorizontal gap="M">
+		<MessageSuccess link={defaultMessageProps.link} description={defaultMessageProps.description}>
+			<TagBeta>Beta</TagBeta>
+		</MessageSuccess>
+		<MessageDestructive
+			description={defaultMessageProps.description}
+			action={defaultMessageProps.action}
+		/>
+		<MessageWarning link={defaultMessageProps.link} description={defaultMessageProps.description} />
+		<MessageInformation
+			title={defaultMessageProps.title}
+			description={defaultMessageProps.description}
+		/>
+	</StackHorizontal>
+);
+
+const MessageInformationTemplate: ComponentStory<typeof MessageInformation> = args => {
+	return <MessageInformation {...args} />;
+};
+
+export const MessageInformationTemplateStory = MessageInformationTemplate.bind({});
+MessageInformationTemplateStory.argTypes = {
+	action: {
+		control: { type: 'object' },
+		defaultValue: { children: 'See', onClick: () => {} },
+	},
+	title: {
+		control: { type: 'text' },
+		defaultValue: 'Information Title',
+	},
+	description: {
+		control: { type: 'text' },
+		defaultValue: 'Maybe resolve this issue before doing anything else',
+	},
+	link: {
+		control: { type: 'object' },
+		defaultValue: { href: 'https://talend.com', children: 'Learn more' },
+	},
+	children: {
+		control: { type: 'text' },
+		defaultValue: undefined,
+		description: 'optional',
+	},
+};
+
+// Message Collection
+
+const defaultMessageCollectionProps: SharedMessageCollectionProps = {
+	title: 'Lorem ipsum',
+	description: 'Try resolving it this way or consult the documentation for more info.',
+	action: { children: 'See all (3)', onClick: action('action clicked') },
+	additionalActions: {
+		'aria-label': 'Additional actions',
+		items: [
+			{ label: 'Select all', type: 'button', onClick: action('select all clicked') },
+			{ label: 'Dismiss', type: 'button', onClick: action('dismiss clicked') },
+			{ label: 'Delete', type: 'button', onClick: action('delete clicked') },
+		],
+	},
+};
 
 export const DefaultMessageCollectionDemo = () => (
 	<StackHorizontal gap="M">
@@ -81,22 +129,53 @@ export const DefaultMessageCollectionDemo = () => (
 	</StackHorizontal>
 );
 
-export const WithPropVariation = () => (
+export const MessageCollectionWithPropVariation = () => (
 	<StackHorizontal gap="M">
-		<MessageSuccess
-			link={{ href: 'https://talend.com', children: 'Learn more' }}
-			description="dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim tposuere ac."
-		>
-			<TagBeta>Beta</TagBeta>
-		</MessageSuccess>
-		<MessageDestructive description="dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim tposuere ac." />
-		<MessageWarning
-			link={{ href: 'https://talend.com', children: 'Learn more' }}
-			description="dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim tposuere ac."
+		<MessageCollectionSuccess
+			action={defaultMessageCollectionProps.action}
+			title="Success"
+			description={defaultMessageCollectionProps.description}
 		/>
-		<MessageInformation
-			title="Lorem ipsum"
-			description="dolor sit amet, consectetur adipiscing elit. Integer gravida orci lacus, vel convallis enim tposuere ac."
+		<MessageCollectionWarning
+			title="Warning"
+			description={defaultMessageCollectionProps.description}
+			action={defaultMessageCollectionProps.action}
+			additionalActions={defaultMessageCollectionProps.additionalActions}
 		/>
 	</StackHorizontal>
 );
+
+const MessageCollectionInformationTemplate: ComponentStory<
+	typeof MessageCollectionInformation
+> = args => {
+	return <MessageCollectionInformation {...args} />;
+};
+
+export const MessageCollectionInformationTemplateStory = MessageCollectionInformationTemplate.bind(
+	{},
+);
+MessageCollectionInformationTemplateStory.argTypes = {
+	action: {
+		control: { type: 'object' },
+		defaultValue: { children: 'See', onClick: () => {} },
+	},
+	title: {
+		control: { type: 'text' },
+		defaultValue: 'Information Title',
+	},
+	description: {
+		control: { type: 'text' },
+		defaultValue: 'Maybe resolve this issue before doing anything else',
+	},
+	additionalActions: {
+		control: { type: 'object' },
+		defaultValue: {
+			'aria-label': 'Additional actions',
+			items: [
+				{ label: 'Select all', type: 'button', onClick: action('select all clicked') },
+				{ label: 'Dismiss', type: 'button', onClick: action('dismiss clicked') },
+				{ label: 'Delete', type: 'button', onClick: action('delete clicked') },
+			],
+		},
+	},
+};
