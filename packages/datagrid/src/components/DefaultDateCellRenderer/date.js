b3d97ee5f1628862df6e06c87cd6c3720713f594
Sébastien Romain <sromain@talend.com>
2022-11-09 10:32:38 +0100

    Update packages/datagrid/src/components/DefaultDateCellRenderer/date.js
    Co-authored-by: Jean-Michel FRANCOIS <jmfrancois@talend.com>

--- packages/datagrid/src/components/DefaultDateCellRenderer/date.js
+++ packages/datagrid/src/components/DefaultDateCellRenderer/date.js
@@ -5,7 +5,7 @@ export function dateToString(value) {
 	}
 
 	try {
-		return new Date(parseInt(value)).toISOString();
+		return new Date(parseInt(value, 10)).toISOString();
 	} catch (e) {
 		return value;
 	}
