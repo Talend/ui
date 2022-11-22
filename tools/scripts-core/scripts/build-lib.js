2800883812b2b2bea16e5b5a4b7329397b8efd1c
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2022-11-22 14:57:41 +0100

    feat(TDOPS-3184): add --watch support on build:lib and build:ts:lib (#4456)

--- tools/scripts-core/scripts/build-lib.js
+++ tools/scripts-core/scripts/build-lib.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const path = require('path');
 const spawn = require('cross-spawn');
 const rimraf = require('rimraf');
@@ -18,32 +19,66 @@ module.exports = function build(env, presetApi, options) {
 	const srcFolder = path.join(process.cwd(), 'src');
 	const targetFolder = path.join(process.cwd(), 'lib');
 
-	console.log(`Removing target folder (${targetFolder})...`);
-	rimraf.sync(targetFolder);
+	if (!options.includes('--watch')) {
+		console.log(`Removing target folder (${targetFolder})...`);
+		rimraf.sync(targetFolder);
+	}
+	const babelPromise = () =>
+		new Promise((resolve, reject) => {
+			console.log('Compiling with babel...');
+			const babelSpawn = spawn(
+				babel,
+				[
+					'--config-file',
+					babelConfigPath,
+					'-d',
+					targetFolder,
+					srcFolder,
+					'--source-maps',
+					'--ignore',
+					'**/*.test.js,**/*.stories.js',
+					...options,
+				],
+				{
+					stdio: 'inherit',
+					env,
+				},
+			);
+			babelSpawn.on('exit', status => {
+				if (parseInt(status, 10) !== 0) {
+					console.error(`Babel exit error: ${status}`);
+					reject(new Error(status));
+				} else {
+					console.log(`Babel exit: ${status}`);
+					resolve({ status });
+				}
+			});
+		});
 
-	console.log('Compiling with babel...');
-	spawn.sync(
-		babel,
-		[
-			'--config-file',
-			babelConfigPath,
-			'-d',
-			targetFolder,
-			srcFolder,
-			'--source-maps',
-			'--ignore',
-			'**/*.test.js,**/*.stories.js',
-			...options,
-		],
-		{
-			stdio: 'inherit',
-			env,
-		},
-	);
+	const copyPromise = () =>
+		new Promise((resolve, reject) => {
+			if (options.includes('--watch')) {
+				const evtEmitter = cpx.watch(`${srcFolder}/**/*.{scss,json}`, targetFolder);
+				evtEmitter.on('watch-error', err => {
+					reject(err);
+				});
+				evtEmitter.on('copy', e => {
+					console.log('copy', e.srcPath, e.dstPath);
+				});
+			} else {
+				console.log('Copying assets...');
+				cpx.copy(`${srcFolder}/**/*.{scss,json}`, targetFolder, err => {
+					if (err) {
+						console.error(err);
+						reject(error);
+					} else {
+						resolve();
+					}
+				});
+			}
+		});
 
-	console.log('Copying assets...');
-	cpx.copySync(`${srcFolder}/**/*.{scss,json}`, targetFolder);
-
-	console.log('🎉 Build complete');
-	return { status: 0 };
+	return Promise.all([babelPromise(), copyPromise()]).then(() => {
+		console.log('🎉 Build complete');
+	});
 };
--- tools/scripts-core/scripts/build-ts-lib.js
+++ tools/scripts-core/scripts/build-ts-lib.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const fs = require('fs');
 const path = require('path');
 const spawn = require('cross-spawn');
@@ -25,77 +26,103 @@ module.exports = function build(env, presetApi, options) {
 	const srcFolder = path.join(process.cwd(), 'src');
 	const targetFolder = path.join(process.cwd(), 'lib');
 
-	console.log(`Removing target folder (${targetFolder})...`);
-	rimraf.sync(targetFolder);
+	if (!options.includes('--watch')) {
+		console.log(`Removing target folder (${targetFolder})...`);
+		rimraf.sync(targetFolder);
+	}
 
-	console.log('Building with babel, generating definition types with tsc...');
-	const babelPromise = new Promise((resolve, reject) => {
-		const babelSpawn = spawn(
-			babel,
-			[
-				'--config-file',
-				babelConfigPath,
-				'-d',
-				targetFolder,
-				srcFolder,
-				'--source-maps',
-				'--ignore',
-				// @see https://github.com/babel/babel/issues/12008
-				'**/*.test.js,**/*.test.ts,**/*.test.tsx,**/*.spec.js,**/*.spec.ts,**/*.spec.tsx,**/*.stories.js,**/*.stories.ts,**/*.stories.tsx',
-				'--extensions',
-				'.js,.ts,.tsx,.jsx',
-				...options,
-			],
-			{ stdio: 'inherit', env },
-		);
+	const babelPromise = () =>
+		new Promise((resolve, reject) => {
+			console.log('Building with babel, generating definition types with tsc...');
+			const babelSpawn = spawn(
+				babel,
+				[
+					'--config-file',
+					babelConfigPath,
+					'-d',
+					targetFolder,
+					srcFolder,
+					'--source-maps',
+					'--ignore',
+					// @see https://github.com/babel/babel/issues/12008
+					'**/*.test.js,**/*.test.ts,**/*.test.tsx,**/*.spec.js,**/*.spec.ts,**/*.spec.tsx,**/*.stories.js,**/*.stories.ts,**/*.stories.tsx',
+					'--extensions',
+					'.js,.ts,.tsx,.jsx',
+					...options,
+				],
+				{ stdio: 'inherit', env },
+			);
 
-		babelSpawn.on('exit', status => {
-			if (parseInt(status, 10) !== 0) {
-				console.error(`Babel exit error: ${status}`);
-				reject(new Error(status));
-			} else {
-				console.log('Copying assets...');
-				cpx.copySync(`${srcFolder}/**/*.{scss,json}`, targetFolder);
-
-				console.log(`Babel exit: ${status}`);
-				resolve({ status });
-			}
-		});
-	});
-	const tscPromise = new Promise((resolve, reject) => {
-		const tscSpawn = spawn(
-			tsc,
-			['--emitDeclarationOnly', '--project', tscConfigPath, '--outDir', targetFolder, ...options],
-			{ stdio: 'inherit', env },
-		);
-
-		tscSpawn.on('exit', status => {
-			if (parseInt(status, 10) !== 0) {
-				console.error(`TSC exit error: ${status}`);
-				reject(new Error(status));
-			} else {
-				console.log(`TSC exit: ${status}`);
-				const pkgPath = path.join(process.cwd(), 'package.json');
-				const types = JSON.parse(fs.readFileSync(pkgPath))?.types;
-				if (!types) {
-					const msg = `Entry "types", referencing your declaration file (index.d.ts), must be defined in ${pkgPath}`;
-					console.error(msg);
-					reject(new Error(msg));
+			babelSpawn.on('exit', status => {
+				if (parseInt(status, 10) !== 0) {
+					console.error(`Babel exit error: ${status}`);
+					reject(new Error(status));
 				} else {
-					const absoluteTypes = path.join(process.cwd(), types);
-					if (!fs.existsSync(absoluteTypes)) {
-						const msg = `Declaration file, referenced in package.json, not found ${absoluteTypes}`;
+					console.log(`Babel exit: ${status}`);
+					resolve({ status });
+				}
+			});
+		});
+
+	const tscPromise = () =>
+		new Promise((resolve, reject) => {
+			const tscSpawn = spawn(
+				tsc,
+				['--emitDeclarationOnly', '--project', tscConfigPath, '--outDir', targetFolder, ...options],
+				{ stdio: 'inherit', env },
+			);
+
+			tscSpawn.on('exit', status => {
+				if (parseInt(status, 10) !== 0) {
+					console.error(`TSC exit error: ${status}`);
+					reject(new Error(status));
+				} else {
+					console.log(`TSC exit: ${status}`);
+					const pkgPath = path.join(process.cwd(), 'package.json');
+					const types = JSON.parse(fs.readFileSync(pkgPath))?.types;
+					if (!types) {
+						const msg = `Entry "types", referencing your declaration file (index.d.ts), must be defined in ${pkgPath}`;
 						console.error(msg);
 						reject(new Error(msg));
 					} else {
-						resolve({ status });
+						const absoluteTypes = path.join(process.cwd(), types);
+						if (!fs.existsSync(absoluteTypes)) {
+							const msg = `Declaration file, referenced in package.json, not found ${absoluteTypes}`;
+							console.error(msg);
+							reject(new Error(msg));
+						} else {
+							resolve({ status });
+						}
 					}
 				}
+			});
+		});
+
+	const copyPromise = () =>
+		new Promise((resolve, reject) => {
+			if (options.includes('--watch')) {
+				const evtEmitter = cpx.watch(`${srcFolder}/**/*.{scss,json}`, targetFolder);
+				evtEmitter.on('watch-error', err => {
+					reject(err);
+				});
+				evtEmitter.on('copy', e => {
+					console.log('copy', e.srcPath, e.dstPath);
+				});
+			} else {
+				console.log('Copying assets...');
+
+				cpx.copy(`${srcFolder}/**/*.{scss,json}`, targetFolder, err => {
+					if (err) {
+						console.error(err);
+						reject(error);
+					} else {
+						resolve();
+					}
+				});
 			}
 		});
-	});
 
-	return Promise.all([babelPromise, tscPromise]).then(() => {
+	return Promise.all([babelPromise(), tscPromise(), copyPromise()]).then(() => {
 		console.log('🎉 Build complete');
 	});
 };
