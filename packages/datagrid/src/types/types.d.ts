082e5ed275b70cc6d2a915a1a2f950b5d34e14bc
Steven Gangnant <38908846+sgangnant@users.noreply.github.com>
2022-12-01 15:54:22 +0100

    chore(datagrid): Drop package (#4477)

--- packages/datagrid/src/types/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.scss' {
-	const content: { [className: string]: string };
-	export default content;
-}
-
-declare module '@talend/react-bootstrap';
-declare module '@talend/react-components' {
-	export * from '@talend/react-components/types';
-}
