0ca76a0af23f14d2b8540eea14f2ddecf9f38d71
jmfrancois <jmfrancois@talend.com>
2022-12-12 14:40:48 +0100

    fix: link

--- packages/forms/src/UIForm/fields/Text/Text.component.js
+++ packages/forms/src/UIForm/fields/Text/Text.component.js
@@ -59,7 +59,7 @@ export default function Text(props) {
 				aria-invalid={!isValid}
 				aria-required={get(schema, 'required')}
 				aria-describedby={`${descriptionId} ${errorId}`}
-				link={link && <Link {...omit(link, ['label'])}> {link.label} </Link>}
+				link={link && omit(link, ['label'])}
 			/>
 		);
 	}
