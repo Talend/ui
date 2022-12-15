ed49c79e8207709b16abef667bd8eb947893972b
aghomari <ghomari.professionnel@gmail.com>
2022-12-15 09:18:08 +0100

    TDS-5962: fix not translated "Select All"
    use the hook directly in the target component

--- packages/components/src/List/List.component.js
+++ packages/components/src/List/List.component.js
@@ -7,8 +7,6 @@ import Toolbar from './Toolbar';
 import ListToVirtualizedList from './ListToVirtualizedList';
 import theme from './List.module.scss';
 import Inject from '../Inject';
-import { useTranslation } from 'react-i18next';
-import I18N_DOMAIN_COMPONENTS from '../constants';
 
 const SelectAll = Toolbar.SelectAll;
 
@@ -138,7 +136,6 @@ function List({
 }) {
 	const classnames = classNames('tc-list', theme.list);
 	const injected = Inject.all(getComponent, omit(components, ['toolbar', 'list']));
-	const { t } = useTranslation(I18N_DOMAIN_COMPONENTS);
 	let selectAllCheckbox;
 	if (
 		displayMode !== 'table' &&
@@ -168,7 +165,7 @@ function List({
 				components={components}
 			/>
 			{injected('after-toolbar')}
-			{selectAllCheckbox && <SelectAll {...selectAllCheckbox} t={t} />}
+			{selectAllCheckbox && <SelectAll {...selectAllCheckbox} />}
 			{injected('before-list-wrapper')}
 			<div className="tc-list-display-virtualized">
 				{injected('before-list')}
