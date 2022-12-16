1ce5a1f8c32fa93828b8bf2cab70c34ae8d6bbff
cbaune-talend <81923357+cbaune-talend@users.noreply.github.com>
2022-12-16 09:41:00 +0100

    feat(TUX-1221): add openInNewTab option to LinkAsButton (#4496)

---
+++ packages/design-system/src/components/LinkAsButton/LinkAsButton.spec.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react';
+
+import { LinkAsButton } from './';
+
+context('<LinkAsButton />', () => {
+	it('should render', () => {
+		cy.mount(<LinkAsButton data-testid="my.link">Link example</LinkAsButton>);
+		cy.getByTestId('my.link').should('have.text', 'Link example');
+	});
+
+	it('should render icon before', () => {
+		cy.mount(<LinkAsButton icon="information-filled">Link example</LinkAsButton>);
+		cy.getByTest('link.icon.before').should('be.visible');
+	});
+
+	it('should render external', () => {
+		cy.mount(<LinkAsButton openInNewTab>Link example</LinkAsButton>);
+		cy.getByTest('link.icon.external').should('be.visible');
+	});
+});
