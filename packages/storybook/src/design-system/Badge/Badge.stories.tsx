8d29cfd215f7ff4dcba9de4b1449416333ebe9c8
jmainguytalend <jmainguy@talend.com>
2022-12-23 15:54:52 +0100

    Update storybook

--- packages/storybook/src/design-system/Badge/Badge.stories.tsx
+++ packages/storybook/src/design-system/Badge/Badge.stories.tsx
@@ -16,39 +16,56 @@ export default {
 export const StoryBadgeValue = () => (
 	<StackVertical gap="S" justify="spaceBetween">
 		<StackHorizontal align="center" gap="S" justify="spaceBetween">
-			Writable
-			<BadgeValue name="Wonderful" value="Feature" />
+			Component Badge w/ variant "badge"
+			<Badge label="Wonderful" value={['Feature']} variant="badge" />
 		</StackHorizontal>
 
 		<StackHorizontal align="center" gap="S" justify="spaceBetween">
-			Read-only
-			<BadgeValue isReadOnly name="Wonderful" value="Feature" />
+			Variant component BadgeValue
+			<BadgeValue label="Wonderful" value={['Feature']} />
+		</StackHorizontal>
+
+		<StackHorizontal align="center" gap="S" justify="spaceBetween">
+			Variant component BadgeValue w/ multi value
+			<BadgeValue label="Wonderful" value={['Feature', 'Item', 'Component']} />
 		</StackHorizontal>
 	</StackVertical>
 );
 
-export const StoryBadgeTag = () => <BadgeTag name="Delightful" />;
+export const StoryBadgeTag = () => (
+	<StackVertical gap="S" justify="spaceBetween">
+		<StackHorizontal align="center" gap="S" justify="spaceBetween">
+			Component Badge w/ variant "tag"
+			<Badge label="Delightful" variant="tag" />
+		</StackHorizontal>
+
+		<StackHorizontal align="center" gap="S" justify="spaceBetween">
+			Variant component BadgeTag
+			<BadgeTag label="Delightful" />
+		</StackHorizontal>
+	</StackVertical>
+);
 
 export const StoryBadgeDropdown = () => (
 	<StackVertical gap="S" justify="spaceBetween">
 		<StackHorizontal align="center" gap="S" justify="spaceBetween">
-			Writable
-			<BadgeDropdown
-				name="Awesome"
+			Component Badge w/ variant "dropdown"
+			<Badge
+				label="Awesome"
 				selectedId="3"
 				value={[
 					{ id: '1', label: 'Feature' },
 					{ id: '2', label: 'Item' },
 					{ id: '3', label: 'Component' },
 				]}
+				variant="dropdown"
 			/>
 		</StackHorizontal>
 
 		<StackHorizontal align="center" gap="S" justify="spaceBetween">
-			Read-only
+			Variant component BadgeDropdown
 			<BadgeDropdown
-				isReadOnly
-				name="Awesome"
+				label="Awesome"
 				selectedId="3"
 				value={[
 					{ id: '1', label: 'Feature' },
@@ -63,22 +80,22 @@ export const StoryBadgeDropdown = () => (
 export const StoryBadgePopover = () => (
 	<StackVertical gap="S" justify="spaceBetween">
 		<StackHorizontal align="center" gap="S" justify="spaceBetween">
-			Writable
-			<BadgePopover
-				name="Marvellous"
+			Component Badge w/ variant "popover"
+			<Badge
+				label="Marvellous"
 				value={[
 					{ id: '1', label: 'Feature', onClick: () => {} },
 					{ id: '2', label: 'Item', onClick: () => {} },
 					{ id: '3', label: 'Component', onClick: () => {} },
 				]}
+				variant="popover"
 			/>
 		</StackHorizontal>
 
 		<StackHorizontal align="center" gap="S" justify="spaceBetween">
-			Read-only
+			Variant component BadgePopover
 			<BadgePopover
-				isReadOnly
-				name="Marvellous"
+				label="Marvellous"
 				value={[
 					{ id: '1', label: 'Feature', onClick: () => {} },
 					{ id: '2', label: 'Item', onClick: () => {} },
