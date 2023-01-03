1312720b70e17dba57f6964ea247221397cf3d8b
jmfrancois <jmfrancois@talend.com>
2023-01-03 11:39:22 +0100

    fix: eslint in vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 const config = {
 	root: true,
-	extends: ['./node_modules/@talend/scripts-config-eslint/.eslintrc'],
-};
-
-if (process.argv.join(' ').includes('.ts')) {
-	// switch to ts config which is the .eslintrc.js
-	config.extends = ['./node_modules/@talend/scripts-config-eslint/.eslintrc.js'];
-	config.parser = '@typescript-eslint/parser';
-	config.parserOptions = {
+	extends: ['./node_modules/@talend/scripts-config-eslint/.eslintrc.js'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
 		tsconfigRootDir: __dirname,
 		project: 'tsconfig.json',
-	};
-}
+	},
+};
 
 module.exports = config;
