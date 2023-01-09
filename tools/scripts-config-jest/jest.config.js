7bf092816f3d73358870052f673d13b615f33058
jmfrancois <jmfrancois@talend.com>
2023-01-09 09:51:06 +0100

    chore: revert change in jest

--- tools/scripts-config-jest/jest.config.js
+++ tools/scripts-config-jest/jest.config.js
@@ -51,10 +51,10 @@ module.exports = {
 	rootDir: process.cwd(),
 	setupFilesAfterEnv: [path.join(__dirname, 'test-setup.js')],
 	testEnvironment: 'jest-environment-jsdom-global',
-	testRegex: 'src/.*\\.test.(js|ejs|cjs|ts|tsx)$',
+	testRegex: 'src/.*\\.test.(js|ts|tsx)$',
 	transform: {
 		// match mjs js jsx ts tsx
-		'^.+\\.[me]?[jt]sx?$': ['babel-jest', { configFile: getBabelConfigPath() }],
+		'^.+\\.m?[jt]sx?$': ['babel-jest', { configFile: getBabelConfigPath() }],
 	},
 	// stop ignore node_modules transform since d3 and others start to put es6 as main of packages
 	transformIgnorePatterns: [
