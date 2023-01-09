5ca4da2381ccc4feba550288be1f1ddaa2d306ef
jmfrancois <jmfrancois@talend.com>
2023-01-09 13:50:59 +0100

    chore: remove useless api

--- tools/scripts-core/utils/preset.js
+++ tools/scripts-core/utils/preset.js
@@ -1,6 +1,4 @@
 /* eslint-disable import/extensions */
-import fs from 'fs';
-import path from 'path';
 import { createUserConfigGetter } from './env.js';
 
 /**
@@ -13,35 +11,6 @@ export function getPresetApi(env = process.env) {
 	return {
 		mode,
 		getUserConfig,
-	};
-}
-
-function hasDep(pkg, name) {
-	let found = false;
-	if (pkg.dependencies) {
-		found = found || !!pkg.dependencies[name];
-	}
-	if (pkg.devDependencies) {
-		found = found || !!pkg.devDependencies[name];
-	}
-	if (pkg.peerDependencies) {
-		found = found || !!pkg.peerDependencies[name];
-	}
-	return found;
-}
-
-export function getPresetEnv() {
-	const isApp = fs.existsSync(path.join(process.cwd(), 'src/app'));
-	const pkgJSON = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json')));
-	const isAngular = hasDep(pkgJSON, 'angular');
-	const isReact = hasDep(pkgJSON, 'react');
-	const isPublic = pkgJSON?.publishConfig?.access === 'public';
-	return {
-		isLib: !isApp,
-		isApp,
-		isReact,
-		isAngular,
-		isPublic,
 	};
 }
 
