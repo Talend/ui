b4781ec7a3aa453d1135d07caabbff4886dbfe01
jmfrancois <jmfrancois@talend.com>
2023-01-12 10:25:26 +0100

    chore: beta 25, fix binary breaking change

--- tools/scripts-core/scripts/build-storybook.js
+++ tools/scripts-core/scripts/build-storybook.js
@@ -2,14 +2,14 @@ const spawn = require('cross-spawn');
 const { resolveBin } = require('../utils/path-resolver');
 const { getPreset } = require('../utils/preset');
 
-const buildStorybook = resolveBin('build-storybook');
+const buildStorybook = resolveBin('storybook');
 
 module.exports = function build(env, presetApi, options) {
 	const presetName = presetApi.getUserConfig(['preset'], '@talend/scripts-preset-react-lib');
 	const preset = getPreset(presetName);
 	const sbConfigPath = preset.getStorybookConfigurationPath(presetApi);
 
-	return spawn.sync(buildStorybook, ['-c', sbConfigPath].concat(options), {
+	return spawn.sync(buildStorybook, ['build', '-c', sbConfigPath].concat(options), {
 		stdio: 'inherit',
 		env,
 	});
--- tools/scripts-core/scripts/start-storybook.js
+++ tools/scripts-core/scripts/start-storybook.js
@@ -2,14 +2,14 @@ const spawn = require('cross-spawn');
 const { resolveBin } = require('../utils/path-resolver');
 const { getPreset } = require('../utils/preset');
 
-const startStorybook = resolveBin('start-storybook');
+const startStorybook = resolveBin('storybook');
 
 module.exports = function start(env, presetApi, options) {
 	const presetName = presetApi.getUserConfig(['preset'], '@talend/scripts-preset-react-lib');
 	const preset = getPreset(presetName);
 	const sbConfigPath = preset.getStorybookConfigurationPath(presetApi);
 
-	return spawn.sync(startStorybook, ['-c', sbConfigPath].concat(options), {
+	return spawn.sync(startStorybook, ['dev', '-c', sbConfigPath].concat(options), {
 		stdio: 'inherit',
 		env,
 	});
