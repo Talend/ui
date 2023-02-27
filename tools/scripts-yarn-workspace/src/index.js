ae19faf969a38ef67668dbd17265bf086d605dd9
jmfrancois <jmfrancois@talend.com>
2023-02-27 11:35:28 +0100

    feat: add lint merge-report script

--- tools/scripts-yarn-workspace/src/index.js
+++ tools/scripts-yarn-workspace/src/index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import { workspaceRun } from './workspace-run.js';
-
+import { mergeReport } from './merge-report.js';
 const command = process.argv[2];
 const args = process.argv.slice(3);
 
 if (command === 'run') {
 	workspaceRun(args);
+} else if (command === 'merge-report') {
+	mergeReport(args);
 } else {
 	console.error(`Command ${command} not found`);
 	process.exit(1);
---
+++ tools/scripts-yarn-workspace/src/merge-report.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-param-reassign */
+import fs from 'fs';
+import { run } from './run.js';
+
+const reports = ['eslint-report.json', 'stylelint-report.json'];
+
+let buff = [];
+
+function transform(item) {
+	if (item.source && !item.filePath) {
+		item.filePath = item.source;
+		delete item.source;
+	}
+	if (item.warnings && !item.messages) {
+		item.messages = item.warnings.map(w => ({
+			...w,
+			severity: 1,
+			message: w.text,
+			ruleId: w.rule,
+		}));
+		item.warningCount = item.warnings.length;
+		delete item.warning;
+	} else if (item.messages) {
+		item.messages = item.messages.map(w => ({ ...w, severity: 1 }));
+		item.warningCount += item.errorCount;
+		item.errorCount = 0;
+	}
+	return item;
+}
+
+export function mergeReport(args) {
+	console.log('###', args);
+	const info = run({ name: 'yarn', args: ['workspaces', '--silent', 'info'] }).then(info =>
+		JSON.parse(info),
+	);
+	const diff = run({
+		name: 'git',
+		args: ['diff', '--name-only', `origin/${process.argv[2]}`, `origin/${process.argv[3]}`],
+	})
+		.then(out =>
+			out
+				.split('\n')
+				.map(str => str.trim())
+				.filter(Boolean),
+		)
+		.catch(e => console.error(e));
+	Promise.all([info, diff]).then(results => {
+		const [infos, files] = results;
+		// eslint-disable-next-line no-console
+		console.log({ files });
+		function onlyIfInDiff(lint) {
+			return !!files.find(f => lint.filePath.endsWith(`/${f}`));
+		}
+		Object.keys(infos).forEach(pkg => {
+			reports.forEach(report => {
+				const fpath = `${infos[pkg].location}/${report}`;
+				if (fs.existsSync(fpath)) {
+					try {
+						buff = buff.concat(
+							JSON.parse(fs.readFileSync(fpath)).map(transform).filter(onlyIfInDiff),
+						);
+					} catch (e) {
+						console.error(e);
+					}
+				}
+			});
+		});
+		const target = `${process.cwd()}/eslint-report.json`;
+
+		// eslint-disable-next-line no-console
+		console.log(`report merge into ${target}`);
+		fs.writeFileSync(target, JSON.stringify(buff, null, 2));
+	});
+}
