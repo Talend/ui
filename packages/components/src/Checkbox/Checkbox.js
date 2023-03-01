e3259ee02a93d8dce7a8b37317336ddb44d714d4
Volodymyr Koval <vladimmirkoval@gmail.com>
2023-03-01 12:26:40 +0200

    feat(TDOPS-3583): link and data tracking attribute in checkbox

--- packages/components/src/Checkbox/Checkbox.js
+++ packages/components/src/Checkbox/Checkbox.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import omit from 'lodash/omit';
 
-export default function Checkbox({ id, className, label, intermediate, ...props }) {
+export default function Checkbox({ id, className, label, link, intermediate, ...props }) {
 	let dataFeature;
 	let dataChecked = 0;
-
+	console.log('!!!!!!!!!!!!! ', props);
 	if (!props.disabled && props['data-feature']) {
 		dataFeature = props['data-feature'];
 		dataFeature += props.checked ? '.disable' : '.enable';
@@ -33,9 +33,11 @@ export default function Checkbox({ id, className, label, intermediate, ...props 
 					type="checkbox"
 					id={id}
 					data-checked={dataChecked}
+					data-tracking={`${props['data-tracking']}-${props.checked ? 'on' : 'off'}`}
 					{...omit(props, 'data-feature')}
 				/>
 				<span>{label}</span>
+				{link}
 			</label>
 		</div>
 	);
@@ -53,6 +55,7 @@ Checkbox.defaultProps = {
 Checkbox.propTypes = {
 	id: PropTypes.string.isRequired,
 	label: PropTypes.node,
+	link: PropTypes.node,
 	onChange: PropTypes.func.isRequired,
 	onBlur: PropTypes.func,
 	checked: PropTypes.bool,
@@ -60,5 +63,6 @@ Checkbox.propTypes = {
 	disabled: PropTypes.bool,
 	intermediate: PropTypes.bool,
 	className: PropTypes.string,
+	'data-tracking': PropTypes.string,
 	'data-feature': PropTypes.string,
 };
