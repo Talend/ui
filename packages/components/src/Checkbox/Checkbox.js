ca299e189ca64d1a92d17d08fe4e3416a00b9a12
Volodymyr Koval <vladimmirkoval@gmail.com>
2023-03-01 15:21:10 +0200

    feat(TDOPS-3583): link and data tracking attribute in checkbox

--- packages/components/src/Checkbox/Checkbox.js
+++ packages/components/src/Checkbox/Checkbox.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import omit from 'lodash/omit';
 
-export default function Checkbox({ id, className, label, link, intermediate, ...props }) {
+export default function Checkbox({ id, className, label, intermediate, ...props }) {
 	let dataFeature;
 	let dataChecked = 0;
-	console.log('!!!!!!!!!!!!! ', props);
+	const dataTracking = `${props['data-tracking']}-${props.checked ? 'on' : 'off'}`;
+
 	if (!props.disabled && props['data-feature']) {
 		dataFeature = props['data-feature'];
 		dataFeature += props.checked ? '.disable' : '.enable';
@@ -33,11 +34,10 @@ export default function Checkbox({ id, className, label, link, intermediate, ...
 					type="checkbox"
 					id={id}
 					data-checked={dataChecked}
-					data-tracking={`${props['data-tracking']}-${props.checked ? 'on' : 'off'}`}
-					{...omit(props, 'data-feature')}
+					data-tracking={dataTracking}
+					{...omit(props, ['data-feature', 'data-tracking'])}
 				/>
 				<span>{label}</span>
-				{link}
 			</label>
 		</div>
 	);
@@ -55,7 +55,6 @@ Checkbox.defaultProps = {
 Checkbox.propTypes = {
 	id: PropTypes.string.isRequired,
 	label: PropTypes.node,
-	link: PropTypes.node,
 	onChange: PropTypes.func.isRequired,
 	onBlur: PropTypes.func,
 	checked: PropTypes.bool,
