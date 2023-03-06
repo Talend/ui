82466531045c5c585783b15a61748cee3ada1d43
jmfrancois <jmfrancois@talend.com>
2023-03-06 18:02:10 +0100

    chore: cleanup

--- packages/components/src/Actions/ActionIconToggle/ActionIconToggle.component.test.js
+++ packages/components/src/Actions/ActionIconToggle/ActionIconToggle.component.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ActionIconToggle from './ActionIconToggle.component';
-import Icon from '../../Icon';
 
 jest.unmock('@talend/design-system');
 
