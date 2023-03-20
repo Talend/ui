426fa4772699d538efa19f90c29024e0a095d457
jmfrancois <jmfrancois@talend.com>
2023-03-20 17:37:42 +0100

    chore: re-add snapshot

--- packages/components/src/Badge/BadgeComposition/BadgeIcon/BadgeIcon.component.test.js
+++ packages/components/src/Badge/BadgeComposition/BadgeIcon/BadgeIcon.component.test.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import BadgeIcon from './BadgeIcon.component';
+import Icon from '../../../Icon';
 jest.unmock('@talend/design-system');
 
+jest.mock('../../../Icon', () => {
+	return jest.fn(({ name, className }) => <span role="img" name={name} className={className} />);
+});
 describe('BadgeIcon', () => {
 	it('should default render', () => {
 		// given
 		const name = 'my icon name';
 		// when
-		render(<BadgeIcon name={name} />);
+		const { baseElement } = render(<BadgeIcon name={name} />);
 		// then
-		expect(document.querySelector(`[name="${name}"]`)).toBeInTheDocument();
+		expect(Icon).toHaveBeenCalledWith(
+			{
+				className: 'tc-badge-label-icon theme-tc-badge-label-icon',
+				name: 'my icon name',
+			},
+			expect.anything(),
+		);
+		expect(baseElement).toMatchSnapshot();
 	});
 });
--- packages/components/src/Badge/BadgeComposition/BadgeSeparator/BadgeSeparator.component.test.js
+++ packages/components/src/Badge/BadgeComposition/BadgeSeparator/BadgeSeparator.component.test.js
@@ -5,16 +5,18 @@ import BadgeSeparator from './BadgeSeparator.component';
 describe('BadgeSeparator', () => {
 	it('should default render', () => {
 		// when
-		render(<BadgeSeparator />);
+		const { baseElement } = render(<BadgeSeparator />);
 		// then
 		expect(document.querySelector('.tc-badge-separator')).toBeInTheDocument();
+		expect(baseElement).toMatchSnapshot();
 	});
 	it('should render with icon separator class', () => {
 		// given
 		const iconSeparator = true;
 		// when
-		render(<BadgeSeparator iconSeparator={iconSeparator} />);
+		const { baseElement } = render(<BadgeSeparator iconSeparator={iconSeparator} />);
 		// then
 		expect(document.querySelector('.tc-badge-separator-icon')).toBeInTheDocument();
+		expect(baseElement).toMatchSnapshot();
 	});
 });
