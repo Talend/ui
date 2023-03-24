f47e34dd012c8cabf24a4f9f3c40018752827a7d
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2023-03-24 14:36:17 +0100

    feat(ARCH-662/CMF): provide API for RLT (#4654)

---
+++ packages/cmf/src/mock/rtl.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import bootstrap from '../bootstrap';
+import registry from '../registry';
+
+export async function prepareCMF(jsx, opts = {}) {
+	// reset global registry
+	const reg = registry.getRegistry();
+	reg._registry = {};
+	reg._isLocked = false;
+
+	function Wrapper() {
+		return jsx;
+	}
+	const config = await bootstrap({
+		RootComponent: Wrapper,
+		render: false,
+		modules: [opts?.cmfModule || { id: 'empty' }],
+	});
+	config.saga.run();
+	return (
+		<config.App
+			store={config.store}
+			loading={opts.AppLoader}
+			withSettings={!!opts.settingsURL}
+			registry={registry.getRegistry()}
+		>
+			{jsx}
+		</config.App>
+	);
+}
