f9fab6d99c62d63fb637dc1f12b54ba4012b78b2
jmfrancois <jmfrancois@talend.com>
2023-03-29 11:20:01 +0200

    chore: remove some useless comments

--- packages/design-system/cypress/support/commands.ts
+++ packages/design-system/cypress/support/commands.ts
@@ -15,15 +15,4 @@ Cypress.Commands.add('mount', (component, options) => {
 	return mount(wrapper, options);
 });
 
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
-
 import '@testing-library/cypress/add-commands';
