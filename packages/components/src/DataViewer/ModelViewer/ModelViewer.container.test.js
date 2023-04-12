71e5aea755551d3d6336ad3a9c3922a72f325924
jmfrancois <jmfrancois@talend.com>
2023-04-12 18:04:32 +0200

    test: fix container

--- packages/components/src/DataViewer/ModelViewer/ModelViewer.container.test.js
+++ packages/components/src/DataViewer/ModelViewer/ModelViewer.container.test.js
@@ -1,9 +1,11 @@
 import { render } from '@testing-library/react';
 import Container from './ModelViewer.container';
+import hierarchicSample from '../sample.raw.json';
 
 describe('I18n', () => {
 	it('should render', () => {
-		const { container } = render(<Container value={[]} />);
+		const isUnion = item => Array.isArray(item.type);
+		const { container } = render(<Container isUnion={isUnion} sample={hierarchicSample} />);
 		expect(container.firstChild).toMatchSnapshot();
 	});
 });
