fc90d83902ac6d52a6e2126560231fe8e34bd39c
jmfrancois <jmfrancois@talend.com>
2023-04-25 16:36:17 +0200

    test: rewrite

--- packages/components/src/DateTimePickers/Date/Manager/Manager.component.test.js
+++ packages/components/src/DateTimePickers/Date/Manager/Manager.component.test.js
@@ -1,7 +1,5 @@
-import { shallow, mount } from 'enzyme';
 import { render, screen } from '@testing-library/react';
-import cases from 'jest-in-case';
-import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 
 import Manager from './Manager.component';
 import { DateContext } from '../Context';
@@ -9,8 +7,25 @@ import { DateContext } from '../Context';
 const DEFAULT_ID = 'DEFAULT_ID';
 
 function DateConsumerDiv(props) {
-	return <div data-testid="DateConsumerDiv" data-props={JSON.stringify(props)} />;
+	return (
+		<div data-testid="DateConsumerDiv" data-props={JSON.stringify(props)}>
+			<input
+				type="text"
+				data-testid="DateConsumerDivInput"
+				value={props.value.textInput}
+				onChange={props.inputManagement.onChange}
+			/>
+			<button
+				type="submit"
+				data-testid="DateConsumerDivSubmit"
+				onClick={e => props.pickerManagement.onSubmit(e, props.value)}
+			>
+				Submit
+			</button>
+		</div>
+	);
 }
+
 // eslint-disable-next-line react/prop-types
 function DateConsumer() {
 	return (
@@ -47,183 +62,162 @@ describe('Date.Manager', () => {
 	});
 
 	describe('value management', () => {
-		cases(
-			'initial state',
-			({ initialDate, expectedTextInput, expectedDate }) => {
-				// when
-				const wrapper = mount(
-					<Manager id={DEFAULT_ID} value={initialDate}>
-						<DateConsumer />
-					</Manager>,
-				);
-
-				// then
-				const contextValue = wrapper.find('DateConsumerDiv').props();
-				expect(contextValue.value.textInput).toBe(expectedTextInput);
-				expect(contextValue.value.date).toEqual(expectedDate);
+		test.each([
+			{
+				name: 'should init default state',
+				initialDate: undefined,
+				expectedTextInput: '',
+				expectedDate: undefined,
 			},
-			[
-				{
-					name: 'should init default state',
-					initialDate: undefined,
-					expectedTextInput: '',
-					expectedDate: undefined,
-				},
-				{
-					name: 'should init default state from props invalid date',
-					initialDate: new Date(''), // invalid date
-					expectedTextInput: '',
-					expectedDate: undefined,
-				},
-				{
-					name: 'should init state from props',
-					initialDate: new Date(2015, 3, 4),
-					expectedTextInput: '2015-04-04',
-					expectedDate: new Date(2015, 3, 4),
-				},
-			],
-		);
-		cases(
-			'props update should update state',
-			({ initialDate, newDate, expectedTextInput, expectedDate }) => {
-				// given
-				const wrapper = mount(
-					<Manager id={DEFAULT_ID} value={initialDate}>
-						<DateConsumer />
-					</Manager>,
-				);
-
-				// when
-				act(() => {
-					wrapper.setProps({
-						value: newDate,
-					});
-				});
-				wrapper.update();
-
-				// then
-				const contextValue = wrapper.find('DateConsumerDiv').props();
-				expect(contextValue.value.textInput).toBe(expectedTextInput);
-				expect(contextValue.value.date).toEqual(expectedDate);
+			{
+				name: 'should init default state from props invalid date',
+				initialDate: new Date(''), // invalid date
+				expectedTextInput: '',
+				expectedDate: undefined,
 			},
-			[
-				{
-					name: 'from undefined props',
-					initialDate: new Date(),
-					newDate: undefined,
-					expectedTextInput: '',
-					expectedDate: undefined,
-				},
-				{
-					name: 'from props invalid date',
-					initialDate: new Date(),
-					newDate: new Date(''), // invalid date
-					expectedTextInput: '',
-					expectedDate: undefined,
-				},
-				{
-					name: 'from props valid date',
-					initialDate: new Date(),
-					newDate: new Date(2015, 3, 4),
-					expectedTextInput: '2015-04-04',
-					expectedDate: new Date(2015, 3, 4),
-				},
-			],
-		);
-
-		cases(
-			'props update should NOT update state',
-			({ initialDate, newDate }) => {
-				// given
-				const wrapper = mount(
-					<Manager id={DEFAULT_ID} value={initialDate}>
-						<DateConsumer />
-					</Manager>,
-				);
-				const previousState = wrapper.find('DateConsumerDiv').props('value');
-
-				// when
-				act(() => {
-					wrapper.setProps({
-						value: newDate,
-					});
-				});
-				wrapper.update();
-
-				// then
-				const nextState = wrapper.find('DateConsumerDiv').props('value');
-				expect(previousState.textInput).toBe(nextState.textInput);
-				expect(previousState.date).toBe(nextState.date);
+			{
+				name: 'should init state from props',
+				initialDate: new Date(2015, 3, 4),
+				expectedTextInput: '2015-04-04',
+				expectedDate: '2015-04-03T22:00:00.000Z',
 			},
-			[
-				{
-					name: "when date ref doesn't change",
-					initialDate: undefined,
-					newDate: undefined,
-				},
-				{
-					name: 'when the dates are equals',
-					initialDate: new Date(2015, 1, 5),
-					newDate: new Date(2015, 1, 5),
-				},
-			],
-		);
-
-		describe('input change', () => {
-			cases(
-				'should update picker',
-				({ textInput, expectedDate, dateFormat }) => {
-					// given
-					const event = { target: { value: textInput } };
-					const wrapper = mount(
-						<Manager id={DEFAULT_ID} dateFormat={dateFormat}>
-							<DateConsumer />
-						</Manager>,
-					);
-
-					// when
-					act(() => {
-						wrapper.find('DateConsumerDiv').prop('inputManagement').onChange(event);
-					});
-					wrapper.update();
-
-					// then
-					const value = wrapper.find('DateConsumerDiv').prop('value');
-					expect(value.textInput).toBe(textInput);
-
-					const { date } = value;
-					expect(date).toEqual(expectedDate);
-				},
-				[
-					{
-						name: 'with valid date',
-						textInput: '2015-01-15',
-						expectedDate: new Date(2015, 0, 15),
-					},
-					{
-						name: 'with invalid date',
-						textInput: '2015aze-01-15',
-						expectedDate: undefined,
-					},
-					{
-						name: 'with empty string',
-						textInput: '',
-						expectedDate: undefined,
-					},
-					{
-						name: 'with custom date format',
-						textInput: '15/01/2015',
-						expectedDate: new Date(2015, 0, 15),
-						dateFormat: 'DD/MM/YYYY',
-					},
-				],
+		])('$name', ({ initialDate, expectedTextInput, expectedDate }) => {
+			// when
+			render(
+				<Manager id={DEFAULT_ID} value={initialDate}>
+					<DateConsumer />
+				</Manager>,
 			);
 
-			it('should trigger props.onChange with valid date', () => {
+			// then
+			expect(screen.getByTestId('DateConsumerDiv')).toBeVisible();
+			const props = JSON.parse(screen.getByTestId('DateConsumerDiv').dataset.props);
+			expect(props.value.textInput).toBe(expectedTextInput);
+			expect(props.value.date).toBe(expectedDate);
+		});
+		test.each([
+			{
+				name: 'from undefined props',
+				initialDate: new Date(),
+				newDate: undefined,
+				expectedTextInput: '',
+				expectedDate: undefined,
+			},
+			{
+				name: 'from props invalid date',
+				initialDate: new Date(),
+				newDate: new Date(''), // invalid date
+				expectedTextInput: '',
+				expectedDate: undefined,
+			},
+			{
+				name: 'from props valid date',
+				initialDate: new Date(),
+				newDate: new Date(2015, 3, 4),
+				expectedTextInput: '2015-04-04',
+				expectedDate: '2015-04-03T22:00:00.000Z',
+			},
+		])('$name', ({ initialDate, newDate, expectedTextInput, expectedDate }) => {
+			// given
+			const { rerender } = render(
+				<Manager id={DEFAULT_ID} value={initialDate}>
+					<DateConsumer />
+				</Manager>,
+			);
+
+			// when
+			rerender(
+				<Manager id={DEFAULT_ID} value={newDate}>
+					<DateConsumer />
+				</Manager>,
+			);
+
+			// then
+			const props = JSON.parse(screen.getByTestId('DateConsumerDiv').dataset.props);
+			expect(props.value.textInput).toBe(expectedTextInput);
+			expect(props.value.date).toEqual(expectedDate);
+		});
+
+		test.each([
+			{
+				name: "when date ref doesn't change",
+				initialDate: undefined,
+				newDate: undefined,
+			},
+			{
+				name: 'when the dates are equals',
+				initialDate: new Date(2015, 1, 5),
+				newDate: new Date(2015, 1, 5),
+			},
+		])('$name', ({ initialDate, newDate }) => {
+			// given
+			const { rerender } = render(
+				<Manager id={DEFAULT_ID} value={initialDate}>
+					<DateConsumer />
+				</Manager>,
+			);
+			const prevProps = JSON.parse(screen.getByTestId('DateConsumerDiv').dataset.props);
+
+			// when
+			rerender(
+				<Manager id={DEFAULT_ID} value={newDate}>
+					<DateConsumer />
+				</Manager>,
+			);
+
+			// then
+			const props = JSON.parse(screen.getByTestId('DateConsumerDiv').dataset.props);
+			expect(prevProps.value.textInput).toBe(props.value.textInput);
+			expect(prevProps.value.date).toBe(props.value.date);
+		});
+
+		describe('input change', () => {
+			test.each([
+				{
+					name: 'with valid date',
+					textInput: '2015-01-15',
+					expectedDate: '2015-01-14T23:00:00.000Z',
+				},
+				{
+					name: 'with invalid date',
+					textInput: '2015aze-01-15',
+					expectedDate: undefined,
+				},
+				{
+					name: 'with empty string',
+					textInput: ' ',
+					expectedDate: undefined,
+				},
+				{
+					name: 'with custom date format',
+					textInput: '15/01/2015',
+					expectedDate: '2015-01-14T23:00:00.000Z',
+					dateFormat: 'DD/MM/YYYY',
+				},
+			])('$name', async ({ textInput, expectedDate, dateFormat }) => {
+				// given
+				render(
+					<Manager id={DEFAULT_ID} dateFormat={dateFormat}>
+						<DateConsumer />
+					</Manager>,
+				);
+
+				// when
+				await userEvent.click(screen.getByTestId('DateConsumerDivInput'));
+				await userEvent.keyboard(textInput);
+
+				// then
+				const props = JSON.parse(screen.getByTestId('DateConsumerDiv').dataset.props);
+				expect(props.value.textInput).toBe(textInput);
+
+				const { date } = props.value;
+				expect(date).toEqual(expectedDate);
+			});
+
+			it('should trigger props.onChange with valid date', async () => {
 				// given
 				const onChange = jest.fn();
-				const event = { target: { value: '2015-01-15' } };
-				const wrapper = mount(
+				render(
 					<Manager id={DEFAULT_ID} onChange={onChange}>
 						<DateConsumer />
 					</Manager>,
@@ -231,13 +225,11 @@ describe('Date.Manager', () => {
 				expect(onChange).not.toBeCalled();
 
 				// when
-				act(() => {
-					wrapper.find('DateConsumerDiv').prop('inputManagement').onChange(event);
-				});
-				wrapper.update();
+				await userEvent.click(screen.getByTestId('DateConsumerDivInput'));
+				await userEvent.keyboard('2015-01-15');
 
 				// then
-				expect(onChange).toBeCalledWith(event, {
+				expect(onChange).toBeCalledWith(expect.anything(), {
 					date: new Date(2015, 0, 15),
 					origin: 'INPUT',
 					textInput: '2015-01-15',
@@ -246,11 +238,10 @@ describe('Date.Manager', () => {
 				});
 			});
 
-			it('should trigger props.onChange with invalid date', () => {
+			it('should trigger props.onChange with invalid date', async () => {
 				// given
 				const onChange = jest.fn();
-				const event = { target: { value: '2015aze-01-15' } };
-				const wrapper = mount(
+				render(
 					<Manager id={DEFAULT_ID} onChange={onChange}>
 						<DateConsumer />
 					</Manager>,
@@ -258,15 +249,18 @@ describe('Date.Manager', () => {
 				expect(onChange).not.toBeCalled();
 
 				// when
-				act(() => {
-					wrapper.find('DateConsumerDiv').prop('inputManagement').onChange(event);
-				});
-				wrapper.update();
+				await userEvent.click(screen.getByTestId('DateConsumerDivInput'));
+				await userEvent.keyboard('2015-01-15');
 
 				// then
 				expect(onChange).toBeCalled();
 				const args = onChange.mock.calls[0];
-				expect(args[0]).toBe(event);
+				expect(args[0]).toMatchObject({
+					type: 'change',
+					target: expect.anything({
+						value: '2015-01-15',
+					}),
+				});
 				expect(args[1].errorMessage).toBe('Date format is invalid');
 				expect(args[1].errors).toEqual([
 					{ code: 'INVALID_DATE_FORMAT', message: 'Date format is invalid' },
@@ -277,48 +271,39 @@ describe('Date.Manager', () => {
 		});
 
 		describe('picker change', () => {
-			cases(
-				'should update input',
-				({ date, expectedTextInput, dateFormat }) => {
-					// given
-					const event = { preventDefault: () => {} };
-					const wrapper = mount(
-						<Manager id={DEFAULT_ID} dateFormat={dateFormat}>
-							<DateConsumer />
-						</Manager>,
-					);
-
-					// when
-					act(() => {
-						wrapper.find('DateConsumerDiv').prop('pickerManagement').onSubmit(event, { date });
-					});
-					wrapper.update();
-
-					// then
-					const dateValue = wrapper.find('DateConsumerDiv').prop('value');
-					expect(dateValue.textInput).toBe(expectedTextInput);
-					expect(dateValue.date).toEqual(date);
+			test.each([
+				{
+					name: 'with valid date',
+					date: new Date(2015, 0, 15),
+					expectedTextInput: '2015-01-15',
 				},
-				[
-					{
-						name: 'with valid date',
-						date: new Date(2015, 0, 15),
-						expectedTextInput: '2015-01-15',
-					},
-					{
-						name: 'with custom date format',
-						date: new Date(2015, 0, 15),
-						dateFormat: 'DD/MM/YYYY',
-						expectedTextInput: '15/01/2015',
-					},
-				],
-			);
+				{
+					name: 'with custom date format',
+					date: new Date(2015, 0, 15),
+					dateFormat: 'DD/MM/YYYY',
+					expectedTextInput: '15/01/2015',
+				},
+			])('$name', async ({ date, expectedTextInput, dateFormat }) => {
+				// given
+				render(
+					<Manager id={DEFAULT_ID} dateFormat={dateFormat} value={date}>
+						<DateConsumer />
+					</Manager>,
+				);
 
-			it('should trigger props.onChange with valid date', () => {
+				// when
+				await userEvent.click(screen.getByRole('button'));
+
+				// then
+				const props = JSON.parse(screen.getByTestId('DateConsumerDiv').dataset.props);
+				expect(props.value.textInput).toBe(expectedTextInput);
+			});
+
+			it('should trigger props.onChange with valid date', async () => {
 				// given
 				const onChange = jest.fn();
 				const event = { target: {}, preventDefault: () => {} };
-				const wrapper = mount(
+				render(
 					<Manager id={DEFAULT_ID} onChange={onChange}>
 						<DateConsumer />
 					</Manager>,
@@ -327,38 +312,41 @@ describe('Date.Manager', () => {
 				expect(onChange).not.toBeCalled();
 
 				// when
-				act(() => {
-					wrapper
-						.find('DateConsumerDiv')
-						.prop('pickerManagement')
-						.onSubmit(event, {
-							date: new Date(2015, 0, 15),
-						});
-				});
-				wrapper.update();
+				await userEvent.click(screen.getByTestId('DateConsumerDivInput'));
+				await userEvent.keyboard('2015-01-15');
+				await userEvent.click(screen.getByRole('button'));
+
 				// then
-				expect(onChange).toBeCalledWith(event, {
-					date: new Date(2015, 0, 15),
-					origin: 'PICKER',
-					textInput: '2015-01-15',
-					errors: [],
-					errorMessage: null,
-				});
+				expect(onChange).toBeCalledWith(
+					expect.anything({
+						type: 'change',
+						target: expect.anything({
+							value: '2015-01-15',
+						}),
+					}),
+					{
+						date: new Date(2015, 0, 15),
+						origin: 'PICKER',
+						textInput: '2015-01-15',
+						errors: [],
+						errorMessage: null,
+					},
+				);
 			});
 		});
 
 		describe('picker management', () => {
 			it('should pass date options', () => {
 				// when
-				const wrapper = mount(
+				render(
 					<Manager id={DEFAULT_ID} useUTC={false}>
 						<DateConsumer />
 					</Manager>,
 				);
 
 				// then
-				const { useUTC } = wrapper.find('DateConsumerDiv').prop('pickerManagement');
-				expect(useUTC).toBe(false);
+				const props = JSON.parse(screen.getByTestId('DateConsumerDiv').dataset.props);
+				expect(props.pickerManagement.useUTC).toBe(false);
 			});
 		});
 	});
