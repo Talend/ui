56948ce94c2d148c69ed94cd756d732b4bb5311f
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2023-05-05 16:15:59 +0200

    test(ARCH-482): Dialog, Drawer, EditableText, Enumeration (#4699)
    - rewrite tests using RTL of Dialog, Drawer, EditableText, Enumeration
    - refactor the EditableText component
    - refactor Enumeration

---
+++ .changeset/late-llamas-allow.md
@@ -0,0 +1,7 @@
+---
+'@talend/react-components': patch
+---
+
+- rewrite tests using RTL of Dialog, Drawer, EditableText, Enumeration
+- refactor the EditableText component
+- refactor Enumeration
--- packages/components/src/Enumeration/Items/Item/ItemEdit.test.js
+++ packages/components/src/Enumeration/Items/Item/ItemEdit.test.js
@@ -1,5 +1,5 @@
-import { mount } from 'enzyme';
-import { Button } from '@talend/react-bootstrap';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import ItemEdit from './ItemEdit.component';
 
@@ -9,24 +9,28 @@ const item = {
 	index: 0,
 	itemProps: {
 		key: 'values',
-		onSubmitItem: jest.fn(), // provided click callback
-		onAbortItem: jest.fn(), // provided click callback
+		onSubmitItem: jest.fn(),
+		onAbortItem: jest.fn(),
+		onChangeItem: jest.fn(),
 		actions: [
 			{
 				label: 'Validate',
 				id: 'validate',
-				onClick: jest.fn(), // provided click callback
+				onClick: jest.fn(),
 			},
 			{
 				label: 'Cancel',
 				id: 'cancel',
-				onClick: jest.fn(), // provided click callback
+				onClick: jest.fn(),
 			},
 		],
 	},
 };
 
 describe('Item', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
 	it('should display value with two buttons and trigger callback on button title click', () => {
 		// given
 		const props = {
@@ -37,16 +41,12 @@ describe('Item', () => {
 				},
 			},
 		};
-		const itemInstance = <ItemEdit {...props} />;
+		render(<ItemEdit {...props} />);
 
 		// when
-		const wrapper = mount(itemInstance);
-		const buttons = wrapper.find(Button);
-
-		buttons.at(1).simulate('click', { stopPropagation: () => {} });
+		userEvent.click(screen.getByLabelText('Cancel'));
 
 		// then
-		expect(buttons.length).toBe(2);
 		expect(props.item.itemProps.actions[1].onClick).toBeCalled();
 	});
 
@@ -61,28 +61,32 @@ describe('Item', () => {
 			},
 			itemProps: {
 				key: 'values',
-				onSubmitItem: jest.fn(), // provided click callback
-				onAbortItem: jest.fn(), // provided click callback
+				onSubmitItem: jest.fn(),
+				onAbortItem: jest.fn(),
+				onChangeItem: jest.fn(),
 				actions: [
 					{
 						label: 'Validate',
 						id: 'validate',
-						onClick: jest.fn(), // provided click callback
+						onClick: jest.fn(),
 					},
 					{
 						label: 'Cancel',
 						id: 'cancel',
-						onClick: jest.fn(), // provided click callback
+						onClick: jest.fn(),
 					},
 				],
 			},
 		};
 
-		const itemEditInstance = <ItemEdit {...props} />;
+		render(<ItemEdit {...props} />);
 
 		// when
-		const wrapper = mount(itemEditInstance);
-		wrapper.find('input').simulate('keyDown', { keyCode: 13, target: { value: 'my new title' } });
+		const input = screen.getAllByRole('gridcell')[0];
+		userEvent.click(input);
+		input.value = '';
+		userEvent.type(input, 'my new title');
+		userEvent.keyboard('{Enter}');
 
 		// then
 		expect(props.item.itemProps.onSubmitItem).toBeCalled();
@@ -101,28 +105,27 @@ describe('Item', () => {
 			},
 			itemProps: {
 				key: 'values',
-				onSubmitItem: jest.fn(), // provided click callback
-				onAbortItem: jest.fn(), // provided click callback
+				onSubmitItem: jest.fn(),
+				onAbortItem: jest.fn(),
 				actions: [
 					{
 						label: 'Validate',
 						id: 'validate',
-						onClick: jest.fn(), // provided click callback
+						onClick: jest.fn(),
 					},
 					{
 						label: 'Cancel',
 						id: 'cancel',
-						onClick: jest.fn(), // provided click callback
+						onClick: jest.fn(),
 					},
 				],
 			},
 		};
 
-		const itemEditInstance = <ItemEdit {...props} />;
-
 		// when
-		const wrapper = mount(itemEditInstance);
-		wrapper.find('input').simulate('keyDown', { keyCode: 27 });
+		render(<ItemEdit {...props} />);
+		userEvent.click(screen.getAllByRole('gridcell')[0]);
+		userEvent.keyboard('{Escape}');
 
 		// then
 		expect(props.item.itemProps.onAbortItem).toBeCalled();
