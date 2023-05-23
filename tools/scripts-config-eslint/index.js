c384e717e9c76d80678db918cb52b1bc6ec807de
jmfrancois <jmfrancois@talend.com>
2023-05-23 10:56:37 +0200

    chore: lint

--- tools/scripts-config-eslint/index.js
+++ tools/scripts-config-eslint/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const utils = require('@talend/scripts-utils');
+
 const cwd = process.cwd();
 const isTS = utils.fs.tsConfig();
 const commentsRegex = /\/\/.*/g;
