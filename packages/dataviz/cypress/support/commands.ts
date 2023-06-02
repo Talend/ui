3e002959493e2cdd2859134aca0d75de0176715e
jmfrancois <jmfrancois@talend.com>
2023-06-02 15:48:41 +0200

    chore: add missing snap

--- packages/dataviz/cypress/support/commands.ts
+++ packages/dataviz/cypress/support/commands.ts
@@ -2,15 +2,8 @@
 /// <reference types="cypress" />
 
 import { mount } from 'cypress/react';
-// import IconsProvider from '../../src/components/IconsProvider';
 
 Cypress.Commands.add('mount', (component, options) => {
-	// const wrapped = (
-	// 	<React.Fragement>
-	// 		<IconsProvider />
-	// 		{component}
-	// 	</React.Fragement>
-	// );
 	const wrapper = component;
 	return mount(wrapper, options);
 });
