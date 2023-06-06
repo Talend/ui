b39dcf8bbef2919813932cfae31b444c782f03cb
jmfrancois <jmfrancois@talend.com>
2023-06-06 14:47:40 +0200

    test: migrate tests of bootstrap

--- fork/react-bootstrap/src/Radio.test.js
+++ fork/react-bootstrap/src/Radio.test.js
@@ -1,66 +1,67 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { screen, render } from '@testing-library/react';
 
-import Radio from '../src/Radio';
+import Radio from './Radio';
 
 import { shouldWarn } from './helpers';
 
 describe('<Radio>', () => {
-  xit('should render correctly', () => {
-    const wrapper = shallow(
+  it('should render correctly', () => {
+    render(
       <Radio name="foo" checked className="my-radio">
         My label
       </Radio>
     );
 
-    wrapper
-      .assertSingle('div.radio.my-radio')
-      .assertSingle('input[type="radio"][name="foo"][checked]');
+    expect(document.querySelector('div.radio.my-radio')).toBeVisible();
+    expect(
+      document.querySelector('input[type="radio"][name="foo"][checked]')
+    ).toBeVisible();
 
-    wrapper.assertSingle('label').text().should.equal('My label');
+    expect(screen.getByText('My label')).toBeInTheDocument();
   });
 
-  xit('should support inline', () => {
-    const wrapper = shallow(
-      <Radio inline name="foo" className="my-radio">
-        My label
-      </Radio>
-    );
+  // xit('should support inline', () => {
+  //   const wrapper = shallow(
+  //     <Radio inline name="foo" className="my-radio">
+  //       My label
+  //     </Radio>
+  //   );
 
-    wrapper
-      .assertSingle('label.radio-inline.my-radio')
-      .assertSingle('input[type="radio"][name="foo"]');
+  //   wrapper
+  //     .assertSingle('label.radio-inline.my-radio')
+  //     .assertSingle('input[type="radio"][name="foo"]');
 
-    wrapper.assertSingle('label').text().should.equal('My label');
-  });
+  //   wrapper.assertSingle('label').text().should.equal('My label');
+  // });
 
-  xit('should support validation state', () => {
-    shallow(<Radio validationState="success" />).assertSingle('.has-success');
-  });
+  // xit('should support validation state', () => {
+  //   shallow(<Radio validationState="success" />).assertSingle('.has-success');
+  // });
 
-  xit('should not support validation state when inline', () => {
-    shouldWarn('ignored');
+  // xit('should not support validation state when inline', () => {
+  //   shouldWarn('ignored');
 
-    shallow(<Radio inline validationState="success" />)
-      .find('.has-success')
-      .should.have.length(0);
-  });
+  //   shallow(<Radio inline validationState="success" />)
+  //     .find('.has-success')
+  //     .should.have.length(0);
+  // });
 
-  xit('should support inputRef', () => {
-    class Container extends React.Component {
-      render() {
-        return (
-          <Radio
-            inputRef={(ref) => {
-              this.input = ref;
-            }}
-          />
-        );
-      }
-    }
+  // xit('should support inputRef', () => {
+  //   class Container extends React.Component {
+  //     render() {
+  //       return (
+  //         <Radio
+  //           inputRef={(ref) => {
+  //             this.input = ref;
+  //           }}
+  //         />
+  //       );
+  //     }
+  //   }
 
-    const instance = mount(<Container />).instance();
+  //   const instance = mount(<Container />).instance();
 
-    expect(instance.input.tagName).to.equal('INPUT');
-  });
+  //   expect(instance.input.tagName).to.equal('INPUT');
+  // });
 });
