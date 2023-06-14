6dd6da6addfbb13c308c2b009b9485064ba16247
Samuel Gendre <sgendre@talend.com>
2023-06-14 17:23:19 +0200

    lokks quite ok

--- packages/flow-designer/src/components/grid/Grid.component.tsx
+++ packages/flow-designer/src/components/grid/Grid.component.tsx
@@ -4,15 +4,20 @@ import get from 'lodash/get';
 import { GRID_SIZE } from '../../constants/flowdesigner.constants';
 import { Transform } from '../../customTypings/index.d';
 
+const size = (4 * 5) / 3;
+const halfSize = size / 2;
+
 function Grid({ transformData }: { transformData?: Transform }) {
 	const largeGridSize = GRID_SIZE * get(transformData, 'k', 1);
+	const halfCrossSize = halfSize * get(transformData, 'k', 1);
+
 	return (
 		<g>
 			<defs>
 				<pattern
 					id="grid"
 					fill="none"
-					stroke="#BFBDBD"
+					stroke="#8F8F8F"
 					strokeWidth="0.5"
 					x={get(transformData, 'x')}
 					y={get(transformData, 'y')}
@@ -20,8 +25,10 @@ function Grid({ transformData }: { transformData?: Transform }) {
 					height={largeGridSize}
 					patternUnits="userSpaceOnUse"
 				>
-					<rect width={largeGridSize} height={largeGridSize} />
-					<path d={`M ${largeGridSize} 0 L 0 0 0 ${largeGridSize}`} />
+					<path d={`M 0 0 V ${halfCrossSize}`} />
+					<path d={`M 0 0 H ${halfCrossSize}`} />
+					<line x1="0" y1={`${largeGridSize}`} x2="0" y2={`${largeGridSize - halfCrossSize}`} />
+					<line x1={`${largeGridSize}`} y1="0" x2={`${largeGridSize - halfCrossSize}`} y2="0" />
 				</pattern>
 			</defs>
 			<rect
