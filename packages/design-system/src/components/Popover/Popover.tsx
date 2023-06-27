4db53be27decb54b2e8c7625183df4abcbff4aea
jmfrancois <jmfrancois@talend.com>
2023-06-27 14:05:48 +0200

    fix: use portal if isFixed

--- packages/design-system/src/components/Popover/Popover.tsx
+++ packages/design-system/src/components/Popover/Popover.tsx
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import type { ReactNode } from 'react';
 import tokens from '@talend/design-tokens';
 
-import { Placement, FloatingArrow } from '@floating-ui/react';
+import { Placement, FloatingArrow, FloatingPortal } from '@floating-ui/react';
 import { usePopover } from './usePopover';
 import { Disclosure } from '../Disclosure/Disclosure';
 import theme from './Popover.module.scss';
@@ -13,6 +13,7 @@ interface PopoverOptions {
 	placement?: Placement;
 	modal?: boolean;
 	open?: boolean;
+	isFixed?: boolean;
 	onOpenChange?: (open: boolean) => void;
 }
 
@@ -20,6 +21,7 @@ export function Popover({
 	children,
 	disclosure,
 	modal = true,
+	isFixed = false,
 	...restOptions
 }: {
 	children: ReactNode;
@@ -33,28 +35,34 @@ export function Popover({
 	if (typeof children === 'function') {
 		childrenRendered = children(popover);
 	}
+	let content = (
+		<div
+			ref={popover.refs.setFloating}
+			className={theme.popover}
+			style={{ ...popover.floatingStyles, display: popover.open ? 'block' : 'none' }}
+			aria-labelledby={popover.labelId}
+			aria-describedby={popover.descriptionId}
+			{...popover.getFloatingProps(restOptions)}
+		>
+			<FloatingArrow
+				ref={arrowRef}
+				context={popover.context}
+				strokeWidth={1}
+				stroke={tokens.coralColorIllustrationShadow}
+				fill={tokens.coralColorNeutralBackground}
+			/>
+			{childrenRendered}
+		</div>
+	);
+	if (isFixed) {
+		content = <FloatingPortal>${content}</FloatingPortal>;
+	}
 	return (
 		<>
 			<Disclosure popref={popover.refs.setReference} {...popover.getReferenceProps()}>
 				{disclosure}
 			</Disclosure>
-			<div
-				ref={popover.refs.setFloating}
-				className={theme.popover}
-				style={{ ...popover.floatingStyles, display: popover.open ? 'block' : 'none' }}
-				aria-labelledby={popover.labelId}
-				aria-describedby={popover.descriptionId}
-				{...popover.getFloatingProps(restOptions)}
-			>
-				<FloatingArrow
-					ref={arrowRef}
-					context={popover.context}
-					strokeWidth={1}
-					stroke={tokens.coralColorIllustrationShadow}
-					fill={tokens.coralColorNeutralBackground}
-				/>
-				{childrenRendered}
-			</div>
+			{content}
 		</>
 	);
 }
