57d412281c3aa59ddd41e078bc9cb80738dfbeec
jmainguytalend <jmainguy@talend.com>
2023-07-11 18:02:33 +0200

    Fix combobox behaviour

--- packages/design-system/src/components/Combobox/Combobox.tsx
+++ packages/design-system/src/components/Combobox/Combobox.tsx
@@ -1,8 +1,18 @@
-import { useState, useCallback, useEffect } from 'react';
+import {
+	useState,
+	useCallback,
+	useEffect,
+	useRef,
+	DetailedHTMLProps,
+	HTMLAttributes,
+	FocusEvent,
+} from 'react';
 import { useTranslation } from 'react-i18next';
-import { I18N_DOMAIN_DESIGN_SYSTEM } from '../constants';
-import styles from './Combobox.module.scss';
+
 import { useId } from '../../useId';
+import { I18N_DOMAIN_DESIGN_SYSTEM } from '../constants';
+
+import styles from './Combobox.module.scss';
 
 export type ComboboxProps = {
 	id?: string;
@@ -23,6 +33,9 @@ const Combobox = ({ values, ...rest }: ComboboxProps) => {
 			setShow(false);
 		}
 	}, []);
+
+	let comboboxRef: HTMLDivElement | null = null;
+
 	// const combobox = useReakitComboboxState({
 	// 	autoSelect: true,
 	// 	inline: true,
@@ -42,8 +55,22 @@ const Combobox = ({ values, ...rest }: ComboboxProps) => {
 		}
 	}, [value, values]);
 
+	const onFocusOut = (event: FocusEvent<HTMLDivElement, Element>) => {
+		// Check if where user clicks out is part of combobox
+		let relatedTarget = event.relatedTarget;
+		while (relatedTarget !== null && relatedTarget !== comboboxRef) {
+			relatedTarget = relatedTarget.parentElement;
+		}
+
+		// If relatedTarget is null, it means we reach top level component without finding combobox div
+		// So we are sure user doesn't click in part of combobox
+		if (relatedTarget === null) {
+			setShow(false);
+		}
+	};
+
 	return (
-		<div className={styles.combobox}>
+		<div className={styles.combobox} ref={ref => (comboboxRef = ref)} onBlur={onFocusOut}>
 			<input
 				className={styles.combobox__input}
 				id={id}
@@ -52,6 +79,9 @@ const Combobox = ({ values, ...rest }: ComboboxProps) => {
 				aria-expanded={show}
 				onKeyDown={onKeydown}
 				onFocus={() => setShow(true)}
+				onClick={() => {
+					if (!show) setShow(true);
+				}}
 				aria-controls={boxId}
 				value={value}
 				placeholder={t('COMBOBOX_PLACEHOLDER', 'Search')}
