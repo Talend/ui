5e33f54460918af1a973e6b65e13a83f3967c59b
jmfrancois <jmfrancois@talend.com>
2023-07-12 16:04:47 +0200

    chore: move theme switcher to globals

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -42,17 +42,6 @@ const TokenOrder = [
 export { i18n };
 
 export const globalTypes = {
-	theme: {
-		name: 'Theme',
-		description: 'Choose a theme to apply to the design system',
-		toolbar: {
-			icon: 'paintbrush',
-			items: [
-				{ value: 'light', left: '⚪️', title: 'Default theme' },
-				{ value: 'dark', left: '⚫️', title: 'Dark theme' },
-			],
-		},
-	},
 	locale: {
 		name: 'Locale',
 		defaultValue: 'en',
@@ -104,21 +93,21 @@ export const parameters = {
 			// const { id, parameters, globals, title } = props.context;
 			// not shure about this but this is where i found the id, title and parameters variables
 			// globals is still missing
-			const { id, parameters, globals, title } = props.context?.attachedCSFFile?.meta;
+			const { id, parameters, title } = props.context.attachedCSFFile?.meta;
+			const theme = props.context.store.globals.globals.theme;
 
 			const hasDarkTheme = title.toLocaleLowerCase().includes('dark');
 
-			useEffect(() => {
-				channel.emit(UPDATE_GLOBALS, {
-					globals: { theme: hasDarkMode ? 'dark' : 'light' },
-				});
-			}, [hasDarkMode]);
+			// useEffect(() => {
+			// 	channel.emit(UPDATE_GLOBALS, {
+			// 		globals: { theme: hasDarkMode ? 'dark' : 'light' },
+			// 	});
+			// }, [hasDarkMode]);
 
 			useEffect(() => {
 				channel.emit('SET_STATUSES_BY_PAGE', statusByPage);
 			}, [statusByPage]);
 
-			const { theme } = globals;
 			useEffect(() => {
 				const hasDarkModeFromToolbar = theme === 'dark';
 				if (hasDarkModeFromToolbar != hasDarkMode) {
@@ -176,33 +165,31 @@ export const parameters = {
 
 						<TableOfContents>
 							{isDesignSystemElementPage && (
-								<ThemeProvider theme={light}>
-									<StackVertical
-										gap="XXS"
-										padding={{ top: 'XS', left: '0', right: '0', bottom: '0' }}
-									>
-										<Divider />
-										<Form.ToggleSwitch
-											label={'Dark mode'}
-											onChange={() => {
-												setDarkMode(!hasDarkMode);
-											}}
-											checked={hasDarkMode}
-										/>
-										<Form.ToggleSwitch
-											label={'Bootstrap stylesheet'}
-											onChange={() => setBootstrapStylesheet(!hasBootstrapStylesheet)}
-											checked={!!hasBootstrapStylesheet}
-										/>
-										{/*
+								<StackVertical
+									gap="XXS"
+									padding={{ top: 'XS', left: '0', right: '0', bottom: '0' }}
+								>
+									<Divider />
+									<Form.ToggleSwitch
+										label={'Dark mode'}
+										onChange={() => {
+											setDarkMode(!hasDarkMode);
+										}}
+										checked={hasDarkMode}
+									/>
+									<Form.ToggleSwitch
+										label={'Bootstrap stylesheet'}
+										onChange={() => setBootstrapStylesheet(!hasBootstrapStylesheet)}
+										checked={!!hasBootstrapStylesheet}
+									/>
+									{/*
 										<Form.Switch
 											label={'Figma iframes'}
 											onChange={() => setFigmaIframe(!hasFigmaIframe)}
 											checked={!!hasFigmaIframe}
 										/>
 										*/}
-									</StackVertical>
-								</ThemeProvider>
+								</StackVertical>
 							)}
 						</TableOfContents>
 
@@ -215,9 +202,7 @@ export const parameters = {
 							</Badges>
 						)}
 
-						<ThemeProvider theme={hasDarkMode ? dark : light}>
-							<DocsContainer {...props} />
-						</ThemeProvider>
+						<DocsContainer {...props} />
 
 						<BackToTop />
 					</DarkThemeWrapper>
--- tools/scripts-config-storybook-lib/.storybook-templates/preview.js
+++ tools/scripts-config-storybook-lib/.storybook-templates/preview.js
@@ -29,6 +29,18 @@ if (cmf) {
 
 const defaultPreview = {
 	globalTypes: {
+		theme: {
+			name: 'Theme',
+			description: 'Choose a theme to apply to the design system',
+			defaultValue: 'light',
+			toolbar: {
+				icon: 'paintbrush',
+				items: [
+					{ value: 'light', left: '⚪️', title: 'Default theme' },
+					{ value: 'dark', left: '⚫️', title: 'Dark theme' },
+				],
+			},
+		},
 		locale: {
 			name: 'Locale',
 			defaultValue: 'en',
@@ -62,7 +74,8 @@ const defaultPreview = {
 					key: 'icons-provider-decorator'
 				}),
 				React.createElement(ThemeProvider, {
-					key: 'theme-provider-decorator'
+					key: 'theme-provider-decorator',
+					theme: context.globals.theme,
 				}, storyElement)
 			];
 		},
