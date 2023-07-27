b7be4657a32f2e4cb82cd1f54ab75f9b196a7d8d
Guillaume NICOLAS <guin56@gmail.com>
2023-07-27 18:10:20 +0200

    fix link to tokens and theme switcher example

--- packages/storybook/src/tokens/ThemeWithTokens.tsx
+++ packages/storybook/src/tokens/ThemeWithTokens.tsx
@@ -30,7 +30,7 @@ export default function ThemeWithTokens() {
 				</p>
 			</StackVertical>
 			<StackHorizontal gap="M" align="center" justify="center">
-				<div>
+				<div data-theme={isDark ? 'dark' : 'light'}>
 					<div className={styles.card}>
 						<StackVertical gap="M" justify="stretch" align="stretch">
 							<StackHorizontal gap="M" justify="spaceBetween" align="center">
