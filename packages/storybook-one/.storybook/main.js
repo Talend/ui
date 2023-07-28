ca199c3ed3d5d891032bbc44f140a45c2c14efb9
jmfrancois <jmfrancois@talend.com>
2023-07-28 16:35:10 +0200

    chore: create the new one storybook

---
+++ packages/storybook-one/.storybook/main.js
@@ -0,0 +1,50 @@
+import path from 'path';
+const iconConfig = require('@talend/icons/.storybook/main.js');
+const rootPath = require.resolve('@talend/ui-storybook-one').replace('src/index.ts', '');
+const {
+	getCommonStyleLoaders,
+	getSassLoaders,
+	getJSAndTSLoader,
+	getSassData,
+	getAssetsRules,
+	getFileNameForExtension,
+} = require('@talend/scripts-config-react-webpack/config/webpack.config.common');
+
+const STORIES = [
+	// `${rootPath}../design-system/src/**/*.stories.@(js|tsx)`,
+	`${rootPath}../components/**/*.stories.@(js|tsx)`,
+	// `${rootPath}../forms/**/*.stories.@(js|tsx)`,
+	`${rootPath}../dataviz/src/**/*.stories.@(js|tsx)`,
+	// `${rootPath}../icons/stories/**/*.stories.@(js|tsx)`,
+];
+const monoRepoFixSourceMap = [
+	'../design-system/src',
+	'../components/src',
+	'../dataviz/src',
+	'../forms/src',
+	'../icons/src',
+];
+const srcDirectories = monoRepoFixSourceMap.map(src => path.resolve(process.cwd(), src));
+
+const config = {
+	stories: STORIES,
+	webpackFinal: async originalConfig => {
+		const config = await iconConfig.webpackFinal(originalConfig);
+		// weird, replace the loader of the current storybook which do not support mono repo
+		const rules = [
+			...config.module.rules.filter(rule => {
+				return !rule.test?.toString().includes('tsx?');
+			}),
+			{
+				test: /\.(js|ts|tsx)$/,
+				exclude: /node_modules/,
+				include: srcDirectories,
+				use: getJSAndTSLoader({}, true),
+			},
+		];
+		config.module.rules = rules;
+		return config;
+	},
+};
+
+export default config;
