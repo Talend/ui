83558a8e5b8003de2f2bb6fa886229677acfa722
jmfrancois <jmfrancois@talend.com>
2023-09-14 11:46:34 +0200

    feat: add transition

--- packages/design-system/src/components/Drawer/variants/FloatingDrawer/FloatingDrawer.tsx
+++ packages/design-system/src/components/Drawer/variants/FloatingDrawer/FloatingDrawer.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, cloneElement } from 'react';
 import type { ReactElement, ReactNode } from 'react';
+import { Transition } from 'react-transition-group';
 import { PrimitiveDrawer } from '../../Primitive/PrimitiveDrawer';
 
 import theme from './FloatingDrawer.module.scss';
@@ -22,14 +23,23 @@ export type FloatingDrawerProps = {
 	header?: ((dialog: DialogFnProps) => ReactNode) | ReactNode;
 	children: ((dialog: DialogFnProps) => ReactNode) | ReactNode;
 	footer?: ((dialog: DialogFnProps) => ReactNode) | ReactNode;
+	withTransition?: boolean;
 	onClose?: () => void;
 } & (WithDisclosure | Controlled);
 
 // backward compatibility
 export type DrawerProps = FloatingDrawerProps;
 
+const STYLES = {
+	entering: { transform: 'translateX(100%)' },
+	entered: { transform: 'translateX(0%)' },
+	exiting: { transform: 'translateX(100%)' },
+	exited: { transform: 'translateX(100%)' },
+};
+
 export const FloatingDrawer = ({
 	id,
+	withTransition = true,
 	disclosure,
 	header,
 	children,
@@ -39,6 +49,7 @@ export const FloatingDrawer = ({
 }: DrawerProps) => {
 	const uuid = useId(id, 'drawer');
 	const [isVisible, setVisible] = useState(visible === undefined ? false : visible);
+
 	// sync with the props
 	useEffect(() => {
 		if (visible !== undefined && visible !== isVisible) {
@@ -62,14 +73,34 @@ export const FloatingDrawer = ({
 		<>
 			{disclosure && <div>{cloneElement(disclosure, disclosureProps)}</div>}
 			{isVisible && (
-				<div data-test="drawer" id={uuid} role="dialog" className={theme.drawer}>
-					<PrimitiveDrawer
-						header={typeof header === 'function' ? header({ onClose: onCloseHandler }) : header}
-						footer={typeof footer === 'function' ? footer({ onClose: onCloseHandler }) : footer}
-					>
-						{typeof children === 'function' ? children({ onClose: onCloseHandler }) : children}
-					</PrimitiveDrawer>
-				</div>
+				<Transition in appear timeout={withTransition ? 500 : 0}>
+					{transitionState => {
+						const style = {
+							transform: 'translateX(100%)',
+							...STYLES[transitionState],
+						};
+						const childrenProps = {
+							onClose: onCloseHandler,
+						};
+						return (
+							<div
+								data-test="drawer"
+								data-testid="drawer"
+								id={uuid}
+								role="dialog"
+								className={theme.drawer}
+								style={style}
+							>
+								<PrimitiveDrawer
+									header={typeof header === 'function' ? header(childrenProps) : header}
+									footer={typeof footer === 'function' ? footer(childrenProps) : footer}
+								>
+									{typeof children === 'function' ? children(childrenProps) : children}
+								</PrimitiveDrawer>
+							</div>
+						);
+					}}
+				</Transition>
 			)}
 		</>
 	);
