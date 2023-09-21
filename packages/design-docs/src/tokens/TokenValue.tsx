aa2d28fbfdb3fa884d73a0e157009648ac131eaf
jmfrancois <jmfrancois@talend.com>
2023-09-21 15:52:54 +0200

    doc: continue on tokens

--- packages/design-docs/src/tokens/TokenValue.tsx
+++ packages/design-docs/src/tokens/TokenValue.tsx
@@ -1,5 +1,10 @@
-import { SizedIcon } from '@talend/design-system';
-import style from './TokenValue.module.scss';
+/* eslint-disable @talend/import-depth */
+import { ReactNode, useState } from 'react';
+import { SizedIcon, StackVertical, StackHorizontal, ButtonTertiary } from '@talend/design-system';
+import tokens from '@talend/design-tokens';
+import dictionary from '@talend/design-tokens/lib/light/dictionary';
+import * as utils from './TokenFormatter';
+import theme from './TokenValue.module.scss';
 
 type TokenValueProps = {
 	children: string;
@@ -20,13 +25,80 @@ function copy(svalue: string) {
 export function TokenValue({ children, lang }: TokenValueProps) {
 	return (
 		<button
-			className={style.btn}
+			className={theme.btn}
 			style={{ border: 'none', background: 'none' }}
 			onClick={() => copy(children)}
 		>
-			<span className={style.lang}>{lang}</span>
+			<span className={theme.lang}>{lang}</span>
 			<span>{children}</span>
 			<SizedIcon size="S" name="copy" />
 		</button>
+	);
+}
+
+type TokenListProps = {
+	style: (token: any) => React.CSSProperties;
+	type: string;
+	children: string | ((token: any) => ReactNode);
+	category?: string;
+	categories?: string[];
+};
+
+export function TokenList({ categories, category, style, type, children }: TokenListProps) {
+	const [search, setSearch] = useState(category);
+	const safeCategories = categories || [];
+	return (
+		<>
+			<div>
+				{safeCategories.map(categ => (
+					<ButtonTertiary key={categ} onClick={() => setSearch(categ)} disabled={categ === search}>
+						{categ}
+					</ButtonTertiary>
+				))}
+			</div>
+			<StackVertical gap="M">
+				{dictionary
+					.filter(t => {
+						const sameType = t.type === type;
+						const sameCategory = search
+							? utils.getDisplayName(t.name).split('/')[0] === search
+							: true;
+						return sameType && sameCategory;
+					})
+					.map(token => (
+						<StackHorizontal gap="M" align="stretch" key={token.name}>
+							<div
+								style={{
+									minHeight: '8rem',
+									width: 200,
+									verticalAlign: 'middle',
+									border: '1px solid #ccc',
+									borderRadius: tokens.coralRadiusM,
+									boxShadow: tokens.coralElevationShadowNeutralM,
+									padding: tokens.coralSpacingS,
+									...style(token),
+								}}
+							>
+								{typeof children === 'function' ? children(token) : children}
+							</div>
+							<dl style={{ margin: 0 }}>
+								<dt>{utils.getDisplayName(token.name)}</dt>
+								<dd style={{ width: 300 }}>{token.description}</dd>
+							</dl>
+							<ul style={{ listStyleType: 'none' }}>
+								<li>
+									<TokenValue lang="SCSS">{utils.getScssName(token.name)}</TokenValue>
+								</li>
+								<li>
+									<TokenValue lang="CSS">{utils.getCssName(token)}</TokenValue>
+								</li>
+								<li>
+									<TokenValue lang="JS">{`tokens.${token.name}`}</TokenValue>
+								</li>
+							</ul>
+						</StackHorizontal>
+					))}
+			</StackVertical>
+		</>
 	);
 }
