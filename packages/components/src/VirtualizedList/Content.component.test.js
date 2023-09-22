d8e1416abce1f54d71fe623323e322cab6e81921
Geoffroy Baccarini <gbaccarini@talend.com>
2023-09-22 11:47:17 +0200

    Cleanup

--- packages/components/src/VirtualizedList/Content.component.test.js
+++ packages/components/src/VirtualizedList/Content.component.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { defaultColumnConfiguration } from './Content.component';
-import userEvent from '@testing-library/user-event';
+
 jest.unmock('@talend/design-system');
 
 describe('CellLabel', () => {
