c2e4f94a77db251422c93b2f4a73a09601b86ebf
smouillour <smouillour@talend.com>
2023-10-12 23:21:59 +0200

    fix node_modules path, upgrade babel/core, keep jest only in scripts-core

--- tools/scripts-core/src/scripts/build-storybook.js
+++ tools/scripts-core/src/scripts/build-storybook.js
@@ -4,10 +4,13 @@ import { getStorybookConfiguration } from '../utils/storybook.js';
 export default async function build(env, presetApi, options) {
 	const sbConfigPath = getStorybookConfiguration(presetApi);
 	utils.pkg.checkPackageIsInstalled('@talend/scripts-config-storybook-lib');
-	const buildStorybook = utils.path.resolveBin('storybook');
 
-	return utils.process.spawn(buildStorybook, ['build', '-c', sbConfigPath].concat(options), {
-		stdio: 'inherit',
-		env,
-	});
+	return utils.process.spawn(
+		new URL(import.meta.resolve('storybook/index.js')).pathname,
+		['build', '-c', sbConfigPath].concat(options),
+		{
+			stdio: 'inherit',
+			env,
+		},
+	);
 }
