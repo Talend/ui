b1512ed102ddcdb6b2d064f7a0092648f22e1f83
smouillour <smouillour@talend.com>
2023-10-18 02:05:03 +0200

    set hoist to false and solve dependency issues

--- tools/scripts-core/src/scripts/build-lib-umd.js
+++ tools/scripts-core/src/scripts/build-lib-umd.js
@@ -2,11 +2,12 @@ import fs from 'fs';
 import * as utils from '@talend/scripts-utils';
 import { getUserConfigFile } from '../utils/env.js';
 
+const webpack = utils.path.resolveBin('webpack');
 const dirname = utils.path.getDirName(import.meta.url);
 
 async function buildUMD(env, presetApi, options = []) {
 	return utils.process.spawn(
-		new URL(import.meta.resolve('webpack/bin/webpack.js')).pathname,
+		webpack,
 		['--config', utils.path.hereRelative(dirname, '../config/webpack.config.js')].concat(options),
 		{ stdio: 'inherit', env },
 	);
