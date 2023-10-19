60da1435e0e55bf61f9ccf2bceae34eeffd8f901
jmfrancois <jmfrancois@talend.com>
2023-10-19 09:41:18 +0200

    chore: cleanup eslintrc

--- packages/design-system/src/components/Tabs/index.ts
+++ packages/design-system/src/components/Tabs/index.ts
@@ -1,10 +1,3 @@
-// import Tab from './Primitive/TabElement';
-// import Tabs from './variants/Tabs';
-// import TabsAsLinkList from './variants/TabsAsLinkList';
-// import TabsKit from './variants/TabsKit';
-
-// export { Tab, Tabs, TabsKit, TabsAsLinkList };
-
 export * from './Primitive/TabsProvider';
 export * from './Primitive/Tabs';
 export * from './Primitive/TabPanel';
