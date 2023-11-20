eb7f13260cfa620593eab12d446338e32ae94c9e
Sebastien LE MOUILLOUR <smouillour@talend.com>
2023-11-20 10:27:22 +0100

    fix: override root config of prettier if a config file exist in packages

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,21 @@
 module.exports = {
 	...require('@talend/scripts-config-prettier'),
+	overrides: [
+		{
+			files: 'fork/module-to-cdn/**/*',
+			...require('./fork/module-to-cdn/.prettierrc'),
+		},
+		{
+			files: 'packages/design-system/**/*',
+			...require('./packages/design-system/.prettierrc'),
+		},
+		{
+			files: 'packages/stepper/**/*',
+			...require('./packages/stepper/.prettierrc'),
+		},
+		{
+			files: 'packages/storybook-docs/**/*',
+			...require('./packages/storybook-docs/.prettierrc'),
+		},
+	],
 };
