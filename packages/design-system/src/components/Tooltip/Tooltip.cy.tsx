f43609cd243c3de4a675adba13802ccd78354c10
Gbacc <Gbacc@users.noreply.github.com>
2023-11-21 10:36:44 +0100

    fix(TDOPS-5671): fix tooltip display when empty title (#4999)
    * fix(TDOPS-5671): fix tooltip display when empty title
    
    * changeset
    
    * improve accessibility

--- packages/design-system/src/components/Tooltip/Tooltip.cy.tsx
+++ packages/design-system/src/components/Tooltip/Tooltip.cy.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable testing-library/prefer-screen-queries */
+
 /* eslint-disable testing-library/await-async-queries */
 import { Tooltip } from './Tooltip';
 
@@ -14,6 +15,18 @@ context('<Tooltip />', () => {
 			cy.findByTestId('my.tooltip').should('not.be.visible');
 			cy.get('button').click();
 			cy.findByTestId('my.tooltip').should('be.visible');
+		});
+
+		it('should not show a tooltip when empty title', () => {
+			cy.mount(
+				<Tooltip title="" data-testid="my.tooltip">
+					<button>button</button>
+				</Tooltip>,
+			);
+
+			cy.findByTestId('my.tooltip').should('not.exist');
+			cy.get('button').click();
+			cy.findByTestId('my.tooltip').should('not.exist');
 		});
 
 		it('Should be able to override baseId', () => {
