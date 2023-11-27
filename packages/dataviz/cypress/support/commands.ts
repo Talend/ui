c348b75fceb9273f1b25ea1a21d465bd8921ee98
Sebastien LE MOUILLOUR <32456736+smouillour@users.noreply.github.com>
2023-11-27 17:31:14 +0100

    Update packages/dataviz/cypress/support/commands.ts
    Co-authored-by: Sébastien Romain <sromain@talend.com>

--- packages/dataviz/cypress/support/commands.ts
+++ packages/dataviz/cypress/support/commands.ts
@@ -6,12 +6,6 @@ import { mount } from 'cypress/react';
 // import IconsProvider from '../../src/components/IconsProvider';
 
 Cypress.Commands.add('mount', (component, options) => {
-	// const wrapped = (
-	// 	<React.Fragement>
-	// 		<IconsProvider />
-	// 		{component}
-	// 	</React.Fragement>
-	// );
 	const wrapper = component;
 	return mount(wrapper, options);
 });
