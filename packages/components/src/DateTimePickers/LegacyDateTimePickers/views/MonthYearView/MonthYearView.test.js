a93932cd1b7e04c0d4b7c0997de2c6034adf2271
Alexandre Amalric <aamalric@talend.com>
2024-01-02 15:20:18 +0100

    fix(): Update tests with mocked date, again

--- packages/components/src/DateTimePickers/LegacyDateTimePickers/views/MonthYearView/MonthYearView.test.js
+++ packages/components/src/DateTimePickers/LegacyDateTimePickers/views/MonthYearView/MonthYearView.test.js
@@ -4,15 +4,8 @@ import userEvent from '@testing-library/user-event';
 import MonthYearView from './MonthYearView.component';
 
 describe('MonthYearView', () => {
-	beforeAll(() => {
-		jest.useFakeTimers().setSystemTime(new Date('2023-01-01'));
-	});
-
-	afterAll(() => {
-		jest.useRealTimers();
-	});
-
 	it('should render', () => {
+		jest.useFakeTimers().setSystemTime(new Date('2023-01-01'));
 		// when
 		const { container } = render(
 			<MonthYearView
@@ -27,9 +20,11 @@ describe('MonthYearView', () => {
 
 		// then
 		expect(container.firstChild).toMatchSnapshot();
+		jest.useRealTimers();
 	});
 
 	it('should manage tabIndex', () => {
+		jest.useFakeTimers().setSystemTime(new Date('2023-01-01'));
 		// given
 		const { rerender } = render(
 			<MonthYearView
@@ -56,6 +51,7 @@ describe('MonthYearView', () => {
 
 		// then
 		expect(screen.getByLabelText('Switch to date-and-time view')).toHaveAttribute('tabIndex', '0');
+		jest.useRealTimers();
 	});
 
 	it('should trigger props.onBackClick', async () => {
