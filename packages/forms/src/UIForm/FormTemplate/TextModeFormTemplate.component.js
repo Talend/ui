1e9859f11b929a27f492b598e0fd67147150ab08
Geoffroy Baccarini <gbaccarini@talend.com>
2024-02-07 11:42:18 +0100

    too much wrapping

--- packages/forms/src/UIForm/FormTemplate/TextModeFormTemplate.component.js
+++ packages/forms/src/UIForm/FormTemplate/TextModeFormTemplate.component.js
@@ -1,10 +1,8 @@
-import { StackVertical } from '@talend/design-system';
-
 export default function TextModeFormTemplate({ widgetsRenderer, children }) {
 	return (
-		<StackVertical gap="S">
+		<>
 			{widgetsRenderer()}
 			{children}
-		</StackVertical>
+		</>
 	);
 }
