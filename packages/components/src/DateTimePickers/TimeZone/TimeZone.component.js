bc3255032611aa47b93582aa5e1bcf3fc07af003
Geoffroy Baccarini <gbaccarini@talend.com>
2024-02-15 15:23:29 +0100

    small fix on styling

--- packages/components/src/DateTimePickers/TimeZone/TimeZone.component.js
+++ packages/components/src/DateTimePickers/TimeZone/TimeZone.component.js
@@ -7,7 +7,12 @@ import TooltipTrigger from '../../TooltipTrigger';
 function TimeZone(props) {
 	return (
 		<TooltipTrigger label={props.timezone} tooltipPlacement="top">
-			<StackVertical gap="0" display="inline" margin={{ left: 'XS', right: 0, top: 0, bottom: 0 }}>
+			<StackVertical
+				gap="0"
+				justify="center"
+				display="inline"
+				margin={{ left: 'XS', right: 0, top: 0, bottom: 0 }}
+			>
 				<SizedIcon
 					color="var(--coral-color-accent-icon, hsla(204, 88%, 40%, 1))"
 					name="information-filled"
