8d25e904887077784c1ed1083c4f444375d755c8
smouillour <sebastien.lemouillour@qlik.com>
2024-04-09 17:15:23 +0200

    fix sass + add alias

--- packages/playground/vite.config.js
+++ packages/playground/vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import fixReactVirtualized from 'esbuild-plugin-react-virtualized';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -13,7 +14,21 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			'~': path.resolve(__dirname, 'node_modules'),
+			'~@talend/bootstrap-theme/src/theme/guidelines': fileURLToPath(
+				import.meta.resolve('@talend/bootstrap-theme/src/theme/guidelines'),
+			),
+			'~@talend/design-tokens/lib/tokens': fileURLToPath(
+				import.meta.resolve('@talend/design-tokens/lib/tokens'),
+			),
+			'~@talend/bootstrap-sass/assets/stylesheets/bootstrap/mixins': fileURLToPath(
+				import.meta.resolve('@talend/bootstrap-sass/assets/stylesheets/bootstrap/mixins'),
+			),
+			'~@talend/bootstrap-theme/src/theme/animation': fileURLToPath(
+				import.meta.resolve('@talend/bootstrap-theme/src/theme/animation'),
+			),
+			'~@talend/design-tokens/lib/_tokens.scss': fileURLToPath(
+				import.meta.resolve('@talend/design-tokens/lib/_tokens.scss'),
+			),
 		},
 	},
 });
