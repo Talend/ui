2c349219489e2bfa0e07dc4419b33dab54ee9fa3
smouillour <sebastien.lemouillour@qlik.com>
2024-04-09 16:16:17 +0200

    Add esm in containers and components

--- packages/playground/vite.config.js
+++ packages/playground/vite.config.js
@@ -1,7 +1,19 @@
 import react from '@vitejs/plugin-react';
+import fixReactVirtualized from 'esbuild-plugin-react-virtualized';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [fixReactVirtualized],
+		},
+	},
+	resolve: {
+		alias: {
+			'~': path.resolve(__dirname, 'node_modules'),
+		},
+	},
 });
