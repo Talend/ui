0f9d131f13d2e19e207634770119991d305510d7
Jean-Michel FRANCOIS <jeanmichel.francois@qlik.com>
2026-03-06 15:28:36 +0100

    test(react-bootstrap): migrate package to vitest

---
+++ fork/react-bootstrap/src/Alert.test.jsx
@@ -0,0 +1,95 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Alert from './Alert';
+
+describe('<Alert>', () => {
+	it('Should output a alert with message', () => {
+		// when
+		render(
+			<Alert>
+				<strong>Message</strong>
+			</Alert>,
+		);
+
+		// then
+		expect(screen.getByRole('alert')).toBeInTheDocument();
+		expect(screen.getByText('Message')).toBeInTheDocument();
+	});
+
+	it('Should have bsType by default', () => {
+		// when
+		render(
+			<Alert>
+				<strong>Message</strong>
+			</Alert>,
+		);
+
+		// then
+		expect(screen.getByRole('alert')).toHaveClass('alert-info');
+	});
+
+	it('Should have dismissable style with onDismiss', () => {
+		// when
+		render(
+			<Alert onDismiss={vi.fn()}>
+				<strong>Message</strong>
+			</Alert>,
+		);
+
+		// then
+		expect(screen.getByRole('alert')).toHaveClass('alert-dismissable');
+	});
+
+	it('Should call onDismiss callback on dismiss click', async () => {
+		const user = userEvent.setup();
+
+		// given
+		const onDismiss = vi.fn();
+		render(
+			<Alert onDismiss={onDismiss} closeLabel="close">
+				<strong>Message</strong>
+			</Alert>,
+		);
+		expect(onDismiss).not.toHaveBeenCalled();
+
+		// when
+		await user.click(screen.getByRole('button', { name: 'close' }));
+
+		// then
+		expect(onDismiss).toHaveBeenCalled();
+	});
+
+	it('Should have a default bsStyle class', () => {
+		// when
+		render(<Alert>Message</Alert>);
+
+		// then
+		expect(screen.getByRole('alert')).toHaveClass('alert-info');
+	});
+
+	it('Should have use bsStyle class', () => {
+		// when
+		render(<Alert bsStyle="danger">Message</Alert>);
+
+		// then
+		expect(screen.getByRole('alert')).toHaveClass('alert-danger');
+	});
+
+	describe('Web Accessibility', () => {
+		it('Should call onDismiss callback when the sr-only dismiss link is activated', async () => {
+			const user = userEvent.setup();
+
+			// given
+			const onDismiss = vi.fn();
+			render(<Alert onDismiss={onDismiss}>Message</Alert>);
+			expect(onDismiss).not.toHaveBeenCalled();
+
+			// when
+			await user.click(screen.getByRole('button', { name: 'Close alert' }));
+
+			// then
+			expect(onDismiss).toHaveBeenCalled();
+		});
+	});
+});
---
+++ fork/react-bootstrap/src/InputGroup.test.jsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+
+import Button from './Button';
+import FormControl from './FormControl';
+import InputGroup from './InputGroup';
+
+describe('<InputGroup>', () => {
+	it('should render properly', () => {
+		render(
+			<InputGroup className="my-input-group">
+				<InputGroup.Addon className="my-addon">Foo</InputGroup.Addon>
+
+				<FormControl type="text" />
+
+				<InputGroup.Button className="my-button">
+					<Button>Bar</Button>
+				</InputGroup.Button>
+			</InputGroup>,
+		);
+		expect(document.querySelector('.input-group.my-input-group')).toBeVisible();
+
+		expect(document.querySelector('.input-group-addon.my-addon')).toHaveTextContent('Foo');
+
+		expect(document.querySelector('input.form-control[type="text"]')).toBeVisible();
+
+		// eslint-disable-next-line jest-dom/prefer-in-document
+		expect(screen.getAllByRole('button')).toHaveLength(1);
+	});
+
+	it('should support bsSize', () => {
+		render(<InputGroup bsSize="small" />);
+		expect(document.querySelector('.input-group.input-group-sm')).toBeVisible();
+	});
+});
