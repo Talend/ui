7e29994b9f7dffd467630046c9d79b71816b7c5f
Jean-Michel FRANCOIS <jeanmichel.francois@qlik.com>
2026-03-06 16:10:56 +0100

    test(faceted-search): migrate package to vitest

---
+++ packages/faceted-search/vitest.config.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [react({ include: /\.[jt]sx?$/ })],
+	esbuild: {
+		loader: 'tsx',
+		include: /src\/.*\.[jt]sx?$/,
+		exclude: [],
+		jsx: 'automatic',
+		tsconfigRaw: {
+			compilerOptions: {
+				jsx: 'react-jsx',
+			},
+		},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		env: {
+			TZ: 'UTC',
+		},
+		setupFiles: ['jest.setup.js'],
+		include: ['src/**/*.test.{js,jsx,ts,tsx}'],
+		exclude: ['lib/**', 'lib-esm/**'],
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json-summary'],
+		},
+	},
+});
