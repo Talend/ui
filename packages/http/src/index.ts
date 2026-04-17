0ce1fac34c997eaae8879920b6753d6802e02355
Volodymyr Koval <vladimmirkoval@gmail.com>
2026-04-17 15:27:06 +0300

    feat: export httpFetch to allow sending payload in delete request

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export { http } from './async';
+export { httpFetch } from './http.common';
 
 export * from './http.types';
 export * from './http.constants';
